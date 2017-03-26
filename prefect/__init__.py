4852bf686b46dd77c6efbd578e07f1ba6b5f470b
Jeremiah Lowin <jlowin@gmail.com>
2017-03-25 23:56:47 -0400

    Add utilities

--- prefect/__init__.py
+++ prefect/__init__.py
@@ -2,6 +2,8 @@ from prefect.configuration import config
 
 import prefect.exceptions
 import prefect.state
+import prefect.utilities
 import prefect.triggers
 import prefect.flow
 import prefect.task
+# import prefect.task_runner
