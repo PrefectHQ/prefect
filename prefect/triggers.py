15e2cfad0be4ad87e9279924555c1d303007e6f9
Jeremiah Lowin <jlowin@gmail.com>
2017-03-27 07:42:29 -0400

    Triggers receive dicts, not sets

--- prefect/triggers.py
+++ prefect/triggers.py
@@ -1,29 +1,30 @@
 """
 Triggers are functions that determine whether a task should run.
 
-Triggers are passed the task in question and a sequence of states from
-preceding tasks. Triggers raised TriggerFailed is they fail and return None
-otherwise.
+Triggers are passed the task in question and a dict of states from preceding
+tasks. The dict contains {task: state} pairs. Triggers raised TriggerFailed is
+they fail and return None otherwise.
 """
+
 import prefect.exceptions
 from prefect.state import State
 
-def all_success(task, preceding_states):
-    if not all([s == State.SUCCESS for s in preceding_states]):
+def all_success(task, preceding_states_dict):
+    if not all([s == State.SUCCESS for s in preceding_states_dict.values()]):
         raise prefect.exceptions.TriggerFailed(
             'All success: not all tasks succeeded')
 
-def all_failed(task, preceding_states):
-    if not all([s == State.FAILED for s in preceding_states]):
+def all_failed(task, preceding_states_dict):
+    if not all([s == State.FAILED for s in preceding_states_dict.values()]):
         raise prefect.exceptions.TriggerFailed(
             'All failed: not all tasks failed')
 
-def any_success(task, preceding_states):
-    if not any([s == State.SUCCESS for s in preceding_states]):
+def any_success(task, preceding_states_dict):
+    if not any([s == State.SUCCESS for s in preceding_states_dict.values()]):
         raise prefect.exceptions.TriggerFailed(
             'Any success: no tasks succeeded')
 
-def any_failed(task, preceding_states):
-    if not any([s == State.FAILED for s in preceding_states]):
+def any_failed(task, preceding_states_dict):
+    if not any([s == State.FAILED for s in preceding_states_dict.values()]):
         raise prefect.exceptions.TriggerFailed(
             'Any failed: no tasks failed')
