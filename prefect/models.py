a2ef47f9e5f98efd48b31dbda650de8a08632225
Jeremiah Lowin <jlowin@gmail.com>
2017-03-27 18:50:59 -0400

    Clean up Models

--- prefect/models.py
+++ prefect/models.py
@@ -1,34 +1,26 @@
 import datetime
 import inspect
 import itertools
-from mongoengine import Document, EmbeddedDocument, queryset_manager
-from mongoengine.fields import (
-    BooleanField,
-    DateTimeField,
-    DictField,
-    EmbeddedDocumentField,
-    IntField,
-    ListField,
-    MapField,
-    ReferenceField,
-    StringField,)
+from mongoengine import Document, EmbeddedDocument, queryset_manager, fields
 import prefect
 from prefect.state import State
 from prefect.utilities.schedules import Schedule
 from prefect.utilities.serialize import Serialized
 
+
 class FlowModel(Document):
-    _id = StringField(primary_key=True)
-    namespace = StringField(
+    _id = fields.StringField(primary_key=True)
+    namespace = fields.StringField(
         default=prefect.config.get('flows', 'default_namespace'))
-    name = StringField(required=True, unique_with='namespace')
-    version = StringField(default='1')
-    schedule = EmbeddedDocumentField(Schedule)
-    params = ListField(StringField, default=tuple())
-    active = BooleanField(
+    name = fields.StringField(required=True, unique_with='namespace')
+    version = fields.StringField(default='1')
+    schedule = fields.EmbeddedDocumentField(Schedule)
+    params = fields.ListField(fields.StringField, default=tuple())
+    active = fields.BooleanField(
         default=prefect.config.getboolean('flows', 'default_active'))
-    serialized = EmbeddedDocumentField(Serialized)
-    graph = MapField(ListField(StringField(), default=tuple()))
+    serialized = fields.EmbeddedDocumentField(Serialized)
+    graph = fields.MapField(
+        fields.ListField(fields.StringField(), default=tuple()))
 
     meta = {'collection': 'flowModels'}
 
@@ -52,69 +44,55 @@ class FlowModel(Document):
 
 
 class TaskModel(Document):
-    _id = StringField(primary_key=True)
-    name = StringField(required=True, unique_with='flow_id')
-    flow_id = StringField(required=True)
-    serialized = EmbeddedDocumentField(Serialized)
+    _id = fields.StringField(primary_key=True)
+    name = fields.StringField(required=True, unique_with='flow_id')
+    flow_id = fields.StringField(required=True)
+    serialized = fields.EmbeddedDocumentField(Serialized)
 
-    meta = {'collection': 'taskModels'}  #, 'indexes': ['flow']}
+    meta = {'collection': 'taskModels', 'indexes': ['flow_id']}
 
 
 class FlowRunModel(Document):
-    _id = StringField(primary_key=True)
-    flow = ReferenceField(FlowModel, required=True)
-    params = MapField(EmbeddedDocumentField(Serialized), default=lambda: dict())
-    generated_by = ReferenceField('TaskRunModel')
-    state = StringField(default=State.PENDING, choices=list(State.all_states()))
-    created = DateTimeField(default=lambda: datetime.datetime.utcnow())
-    started = DateTimeField()
-    finished = DateTimeField()
+    _id = fields.StringField(primary_key=True)
+    flow = fields.ReferenceField(FlowModel, required=True)
+    params = fields.MapField(
+        fields.EmbeddedDocumentField(Serialized), default=lambda: dict())
+    generated_by = fields.ReferenceField('TaskRunModel')
+    state = fields.StringField(
+        default=State.PENDING, choices=list(State.all_states()))
+    scheduled_start = fields.DateTimeField()
+    created = fields.DateTimeField(default=lambda: datetime.datetime.utcnow())
+    started = fields.DateTimeField()
+    finished = fields.DateTimeField()
 
-    meta = {'collection': 'flowRuns', 'indexes': ['state', 'created']}
+    meta = {
+        'collection': 'flowRuns',
+        'indexes': [
+            'state',
+            'created',
+            'flow',
+            'generated_by',
+            'scheduled_start',
+        ]
+    }
 
 
 class TaskRunModel(EmbeddedDocument):
-    _id = StringField(primary_key=True)
-    task = ReferenceField(TaskModel, required=True)
-    flow_run = ReferenceField(FlowRunModel)
-    state = StringField(default=State.NONE)
-    run_count = IntField(default=0)
-    scheduled_start = DateTimeField()
-    created = DateTimeField(default=lambda: datetime.datetime.utcnow())
-    started = DateTimeField()
-    finished = DateTimeField()
+    _id = fields.StringField(primary_key=True)
+    task = fields.ReferenceField(TaskModel, required=True)
+    run_id = fields.StringField(required=True)
+    state = fields.StringField(default=State.NONE)
+    run_count = fields.IntField(default=0)
+    scheduled_start = fields.DateTimeField()
+    created = fields.DateTimeField(default=lambda: datetime.datetime.utcnow())
+    started = fields.DateTimeField()
+    finished = fields.DateTimeField()
 
     meta = {
         'collection': 'taskRuns',
-        'indexes': ['flow_run', 'state', 'scheduled_start']
+        'indexes': ['task', 'run_id', 'state', 'scheduled_start']
     }
 
     @classmethod
     def from_task_and_flowrun(cls, task, flowrun):
         return cls.objects(task=task, flowrun=flowrun).first()
-
-
-#
-#
-# class Test(PrefectDocument):
-#     _id = StringField(primary_key=True)
-#     a=IntField()
-#     b=IntField()
-#     c=IntField()
-#     d=IntField()
-#     def __init__(self, a, b, **_mongo_kwargs):
-#         id = str(a + b)
-#         c = a + b
-#         super().__init__(id=id, a=a, b=b, **_mongo_kwargs)
-#
-# class Test(Document):
-#     _id = StringField(primary_key=True)
-#     a=IntField()
-#     b=IntField()
-#
-#     def __init__(self, a, b, **k):
-#         super().__init__(a=a, b=b, **k)
-#
-#     @property
-#     def id(self):
-#         return str(self.a + self.b)
