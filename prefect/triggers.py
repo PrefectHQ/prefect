a7a2d9eca03439c3c0d0db5355cf94f93975fc77
Jeremiah Lowin <jlowin@apache.org>
2017-04-26 15:28:16 -0400

    Preceding states is an iterable, not a dict

--- prefect/triggers.py
+++ prefect/triggers.py
@@ -1,10 +1,6 @@
 """
 Triggers are functions that determine if task state should change based on
 the state of preceding tasks.
-
-Trigger is passed a dict of {task : state} pairs for all preceding tasks. It
-is expected to adjust the current state as necessary.
-
 """
 
 from prefect import exceptions
@@ -15,10 +11,10 @@ def all_success(preceding_states):
     any skipped -> skip
     any unsuccessful -> fail
     """
-    if any(s.is_skipped() for s in preceding_states.values()):
+    if any(s.is_skipped() for s in preceding_states):
         raise exceptions.SKIP(
             'Trigger failed: at least one preceding task was skipped.')
-    elif not all(s.is_successful() for s in preceding_states.values()):
+    elif not all(s.is_successful() for s in preceding_states):
         raise exceptions.FAIL(
             'Trigger failed: not all preceding tasks were successful')
 
@@ -28,10 +24,10 @@ def all_failed(preceding_states):
     any skipped -> skip
     any successful -> fail
     """
-    if any(s.is_skipped() for s in preceding_states.values()):
+    if any(s.is_skipped() for s in preceding_states):
         raise exceptions.SKIP(
             'Trigger failed: at least one preceding task was skipped.')
-    elif not all(s.is_failed() for s in preceding_states.values()):
+    elif not all(s.is_failed() for s in preceding_states):
         raise exceptions.FAIL(
             'Trigger failed: not all preceding tasks failed.')
 
@@ -41,10 +37,10 @@ def any_success(preceding_states):
     all skipped -> skip
     none successful -> fail
     """
-    if all(s.is_skipped() for s in preceding_states.values()):
+    if all(s.is_skipped() for s in preceding_states):
         raise exceptions.SKIP(
             'Trigger failed: all preceding tasks were skipped.')
-    if not any(s.is_successful() for s in preceding_states.values()):
+    if not any(s.is_successful() for s in preceding_states):
         raise exceptions.FAIL(
             'Trigger failed: all preceding tasks failed.')
 
@@ -54,9 +50,9 @@ def any_failed(preceding_states):
     all skipped -> skip
     none failed -> fail
     """
-    if all(s.is_skipped() for s in preceding_states.values()):
+    if all(s.is_skipped() for s in preceding_states):
         raise exceptions.SKIP(
             'Trigger failed: all preceding tasks were skipped.')
-    if not any(s.is_failed() for s in preceding_states.values()):
+    if not any(s.is_failed() for s in preceding_states):
         raise exceptions.FAIL(
             'Trigger failed: all preceding tasks were successful.')
