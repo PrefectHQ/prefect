0847ebc9bcf3aa21548afa9afb2afbddd3f7cbdc
Jeremiah Lowin <jlowin@apache.org>
2017-11-14 15:18:12 -0500

    Add DummyTask

--- prefect/tasks/__init__.py
+++ prefect/tasks/__init__.py
@@ -1,3 +1,3 @@
 from prefect.task import Task
-from prefect.tasks.core import FunctionTask, FlowRunnerTask, as_task_class
+from prefect.tasks.core import DummyTask, FunctionTask, FlowRunnerTask, as_task_class
 import prefect.tasks.control_flow
