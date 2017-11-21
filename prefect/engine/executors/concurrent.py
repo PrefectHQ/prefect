9288d998da271f6c3b1e9b908774db215b041b2f
Jeremiah Lowin <jlowin@apache.org>
2017-11-21 15:42:21 -0500

    Refactor concurrent executor

--- prefect/engine/executors/concurrent.py
+++ prefect/engine/executors/concurrent.py
@@ -4,43 +4,50 @@ from contextlib import contextmanager
 from prefect.engine.executors import Executor
 
 
-class ThreadPoolExecutor:
+class ConcurrentExecutor(Executor):
+
+    def __init__(self):
+        self.executor = None
+        super().__init__()
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        state['executor'] = None
+        return state
+
+    def submit(self, fn, *args, **kwargs):
+        return self.executor.submit(fn, *args, **kwargs)
+
+    def wait(self, futures, timeout=None):
+        return concurrent.futures.wait(futures, timeout=timeout)
+
+
+class ThreadPoolExecutor(ConcurrentExecutor):
 
     def __init__(self, threads):
         self.threads = threads
         super().__init__()
 
     @contextmanager
-    def __call__(self, context, **kwargs):
+    def execution_context(self, **kwargs):
+        old_executor = self.executor
         with concurrent.futures.ThreadPoolExecutor(self.threads) as executor:
-            yield ConcurrentExecutor(executor=executor)
+            self.executor = executor
+            yield
+            self.executor = old_executor
 
 
-class ProcessPoolExecutor:
+class ProcessPoolExecutor(ConcurrentExecutor):
 
     def __init__(self, processes):
         self.processes = processes
+        self.executor = None
         super().__init__()
 
     @contextmanager
-    def __call__(self, context, **kwargs):
+    def execution_context(self, **kwargs):
+        old_executor = self.executor
         with concurrent.futures.ProcessPoolExecutor(self.processes) as executor:
-            yield ConcurrentExecutor(executor=executor)
-
-
-class ConcurrentExecutor(Executor):
-
-    def __init__(self, executor):
-        self.executor = executor
-
-    def submit(self, fn, *args, _client_kwargs=None, **kwargs):
-        """
-        Runs a function locally
-        """
-        return self.executor.submit(fn, *args, **kwargs)
-
-    def wait(self, futures, timeout=None):
-        """
-        Returns the provided futures
-        """
-        return concurrent.futures.wait(futures, timeout=timeout)
+            self.executor = executor
+            yield
+            self.executor = old_executor
