bf6ec4ec9a33d27b3c6e321b962ac70a9df72a55
Jeremiah Lowin <jlowin@apache.org>
2018-01-09 11:35:47 -0500

    Remove DummyTask and move FlowRunnerTask out of Core

--- prefect/tasks/__init__.py
+++ prefect/tasks/__init__.py
@@ -1,6 +1,6 @@
 from prefect.task import Task
 from prefect.tasks.core import (
-    ConstantTask, DummyTask, FunctionTask, FlowRunnerTask, as_task_class)
+    ConstantTask, FunctionTask, Parameter, as_task_class)
 import prefect.tasks.control_flow
 import prefect.tasks.notifications
 import prefect.tasks.strings
--- prefect/tasks/core/dummy_task.py
+++
@@ -1,9 +0,0 @@
-import prefect
-
-class DummyTask(prefect.Task):
-    """
-    A task that doesn't do anything but can be used as a placeholder.
-    """
-
-    def run(self):
-        pass
