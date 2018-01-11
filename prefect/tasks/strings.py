5bea86ff1269f41cf1ec1196b505b37bfae59b99
Jeremiah Lowin <jlowin@apache.org>
2018-01-11 18:30:25 -0500

    Framework Regex task

--- prefect/tasks/strings.py
+++ prefect/tasks/strings.py
@@ -1,10 +1,24 @@
+import re
 from prefect.task import Task
 
 class StringFormatterTask(Task):
+    """
+    This task contains a template which is formatted with the results of any
+    upstream tasks and returned.
+    """
 
-    def __init__(self, template, **kwargs):
-        self.template = template
+    def __init__(self, template=None, **kwargs):
+        self.template = template or ''
         super().__init__(**kwargs)
 
-    def run(self, **format_kwargs):
-        return self.template.format(**format_kwargs)
+    def run(self, template=None, **format_kwargs):
+        if template is None:
+            template = self.template
+        return template.format(**format_kwargs)
+
+# class MatchRegexTask(FunctionTask):
+#     """
+#     This task attempts to match a regex string against
+#     """
+#     def run(self, string, regex):
+#         re.
