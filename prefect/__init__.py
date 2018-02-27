c21d744c1543665633567a11b2fe17a6682b62a0
Jeremiah Lowin <jlowin@apache.org>
2018-02-27 18:36:17 -0500

    Run reset default flow during import

--- prefect/__init__.py
+++ prefect/__init__.py
@@ -1,17 +1,17 @@
 __version__ = '0.0'
 
 from prefect.configuration import config
-
 import prefect.utilities
 from prefect.context import Context
 import prefect.signals
 import prefect.serializers
 
 from prefect.core import Task, Flow, Parameter
-from prefect.tasks import FunctionTask
+import prefect.tasks
 import prefect.flows
+import prefect.engine
 from prefect.utilities.tasks import task
 
-import prefect.engine
-
 from prefect.client import Client
+
+prefect.utilities.flows.reset_default_flow()
