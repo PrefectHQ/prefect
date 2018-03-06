934e06385aace5c7d50393cf6bbb31a1f9779135
Jeremiah Lowin <jlowin@apache.org>
2018-03-06 10:35:34 -0500

    Sequence => VarArgsTask

--- prefect/tasks/collections.py
+++ prefect/tasks/collections.py
@@ -2,32 +2,36 @@ from prefect import Task
 from prefect.utilities.tasks import task_factory
 
 
-class Sequence(Task):
+class VarArgsTask(Task):
 
     def __call__(self, *args):
+        """
+        Accepts *args and transforms them into **kwargs
+        """
         kwargs = {'arg_{}'.format(i + 1): a for i, a in enumerate(args)}
         return super().__call__(**kwargs)
 
 
 @task_factory
-class List(Sequence):
+class List(VarArgsTask):
 
     def run(self, **task_results):
         return list(task_results.values())
 
 
 @task_factory
-class Set(Sequence):
+class Set(VarArgsTask):
 
     def run(self, **task_results):
         return set(task_results.values())
 
 
 @task_factory
-class Tuple(Sequence):
+class Tuple(VarArgsTask):
 
     def run(self, **task_results):
         return tuple(task_results.values())
+
 
 @task_factory
 class Dict(Task):
