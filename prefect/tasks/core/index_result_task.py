a2d7a5107a8d1a0a5703a6a4a34745c8293ade5d
Jeremiah Lowin <jlowin@apache.org>
2018-03-17 16:26:37 -0400

    Docstring

--- prefect/tasks/core/index_result_task.py
+++ prefect/tasks/core/index_result_task.py
@@ -2,6 +2,9 @@ import prefect
 
 
 class GetIndexTask(prefect.Task):
+    """
+    Gets a result from a specific index
+    """
 
     def __init__(self, index, **kwargs):
         self.index = index
