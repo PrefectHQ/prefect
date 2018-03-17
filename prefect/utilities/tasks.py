3823577839f07f221c868ba291d74628f5e99049
Jeremiah Lowin <jlowin@apache.org>
2018-03-17 16:27:36 -0400

    Reflect new TaskResult location

--- prefect/utilities/tasks.py
+++ prefect/utilities/tasks.py
@@ -7,14 +7,13 @@ def as_task_result(x):
     Wraps a function, collection, or constant with the appropriate Task type.
     """
     # utilities are imported first, so TaskResult must be imported here
-    TaskResult = prefect.core.flow.TaskResult
 
     # task objects
-    if isinstance(x, TaskResult):
+    if isinstance(x, prefect.flow.TaskResult):
         return x
 
     elif isinstance(x, prefect.Task):
-        return TaskResult(task=x, flow=None)
+        return prefect.flow.TaskResult(task=x, flow=None)
 
     # sequences
     elif isinstance(x, list):
@@ -31,11 +30,13 @@ def as_task_result(x):
 
     # functions
     elif callable(x):
-        return TaskResult(task=prefect.tasks.FunctionTask(fn=x), flow=None)
+        return prefect.flow.TaskResult(
+            task=prefect.tasks.core.function_task.FunctionTask(fn=x), flow=None)
 
     # constants
     else:
-        return TaskResult(task=prefect.tasks.Constant(value=x), flow=None)
+        return prefect.flow.TaskResult(
+            task=prefect.tasks.Constant(value=x), flow=None)
 
     # others - not reachable right now
     # else:
@@ -49,10 +50,12 @@ def task_factory(**task_init_kwargs):
     Task factories automatically instantiate the class and calls it on the
     provided arguments, returning a TaskResult.
     """
+
     @wrapt.decorator
     def inner(cls, instance, args, kwargs):
         task = cls(**task_init_kwargs)
         return task(*args, **kwargs)
+
     return inner
 
 
@@ -73,7 +76,8 @@ def task(**task_init_kwargs):
 
     @wrapt.decorator
     def inner(fn, instance, args, kwargs):
-        task = prefect.tasks.FunctionTask(fn=fn, **task_init_kwargs)
+        task = prefect.tasks.core.function_task.FunctionTask(
+            fn=fn, **task_init_kwargs)
         return task(*args, **kwargs)
 
     return inner
@@ -84,4 +88,4 @@ def get_task_by_id(id):
     Retrieves a task by its ID. This will only work for tasks that are alive
     in the current interpreter.
     """
-    return prefect.core.task.TASK_REGISTRY.get(id)
+    return prefect.task_module.TASK_REGISTRY.get(id)
