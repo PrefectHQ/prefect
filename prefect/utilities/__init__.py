96351ce4c14ea46f922250305748f3794cf8271e
Jeremiah Lowin <jlowin@apache.org>
2018-03-22 16:24:59 -0700

    Add task hashing algorithm

--- prefect/utilities/__init__.py
+++ prefect/utilities/__init__.py
@@ -1,6 +1,7 @@
 import prefect.utilities.collections
 import prefect.utilities.datetimes
 import prefect.utilities.flows
+import prefect.utilities.hashing
 import prefect.utilities.functions
 import prefect.utilities.json
 import prefect.utilities.strings
