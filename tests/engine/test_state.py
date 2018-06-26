014c97d45e24dfc1b785701732a8f38f0960db17
Jeremiah Lowin <jlowin@users.noreply.github.com>
2018-06-26 10:51:49 -0400

    Add __eq__() method to State
    Also make sure States are hashable

--- tests/engine/test_state.py
+++ tests/engine/test_state.py
@@ -10,17 +10,17 @@ class TestState:
     def test_default_state(self):
         assert State._default_state == State.PENDING  # pylint: disable=W0212
 
-    def test_create_no_args(self):
+    def test_create_state_with_no_args(self):
         state = State()
         assert state.state == State._default_state  # pylint: disable=W0212
         assert state.data is None
 
-    def test_create_state(self):
+    def test_create_state_with_no_data(self):
         state = State(State.RUNNING)
         assert state.state == State.RUNNING
         assert state.data is None
 
-    def test_create_state_and_data(self):
+    def test_create_state_with_state_and_data(self):
         data = {"hi": 5}
         state = State(State.RUNNING, data)
         assert state.state == State.RUNNING
@@ -62,6 +62,15 @@ class TestState:
         assert new_state.data == state.data
         assert new_state.timestamp == state.timestamp
 
+    def test_state_equality(self):
+        assert State() == State()
+        assert State(State.SUCCESS) == State(State.SUCCESS)
+        assert State(State.SUCCESS, data=1) == State(State.SUCCESS, data=1)
+        assert not State() == State(State.SUCCESS)
+        assert not State(State.SUCCESS, data=1) == State(State.SUCCESS, data=2)
+
+    def test_states_are_hashable(self):
+        assert {State(State.SUCCESS), State(State.FAILED)}
 
 class TestStateMethods:
     def test_state_type_methods_with_pending_state(self):
