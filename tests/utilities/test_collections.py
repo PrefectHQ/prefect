569dc65e9d93759660d7ba066d96d6f88d0fc927
Chris White <chris@prefect.io>
2018-06-28 14:31:42 -0400

    Initial commit of testing framework for new dotdict implementation

--- tests/utilities/test_collections.py
+++ tests/utilities/test_collections.py
@@ -1,6 +1,7 @@
 import pytest
 
 from prefect.utilities import collections
+from prefect.utilities.collections import DotDict
 
 
 @pytest.fixture
@@ -24,3 +25,26 @@ def test_nest_flattened_dict(nested_dict):
     flat = collections.dict_to_flatdict(nested_dict)
     nested = collections.flatdict_to_dict(flat)
     assert nested == nested_dict
+
+
+@pytest.fixture(params=[dict(another=500),
+                        DotDict(another=500)])
+def mutable_mapping(request):
+    "MutableMapping objects to test with"
+    return request.param
+
+
+class TestDotDict:
+    def test_initialization_with_kwargs(self):
+        d = DotDict(chris=10, attr='string', other=lambda x: {})
+
+    def test_initialization_with_mutable_mapping(self, mutable_mapping):
+        d = DotDict(mutable_mapping)
+
+    def test_update_with_kwargs(self):
+        d = DotDict(chris=10, attr='string', other=lambda x: {})
+        d.update(another=500)
+
+    def test_update_with_mutable_mapping(self, mutable_mapping):
+        d = DotDict({'chris': 10, 'attr': 'string', 'other': lambda x: {}})
+        d.update(mutable_mapping)
