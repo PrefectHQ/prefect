98d942d39b68ce5db4e8b5fc31086238685ec329
Jeremiah Lowin <jlowin@users.noreply.github.com>
2018-06-28 17:54:56 -0400

    create ETL example

---
+++ examples/etl.py
@@ -0,0 +1,20 @@
+from prefect import Task, Flow, task
+
+@task
+def extract():
+    return [1, 2, 3]
+
+@task
+def transform(x):
+    return [i * 10 for i in x]
+
+@task
+def load(y):
+    print('Received y: {}'.format(y))
+
+with Flow('ETL') as flow:
+    e = extract()
+    t = transform(e)
+    l = load(t)
+
+flow.run()
