9777e172603485576c9b3b09de821d21b2846372
Chris White <chris@prefect.io>
2018-07-18 10:59:29 -0700

    Add more nuanced test

--- tests/utilities/test_tests.py
+++ tests/utilities/test_tests.py
@@ -49,3 +49,16 @@ def test_raise_on_fail_works_at_the_task_level_with_signal():
         with raise_on_fail():
             taskrunner.run()
     assert "needs more blockchain!" in str(error)
+
+
+def test_core_code_errors_bubble_up(monkeypatch):
+    flow = Flow()
+    flow.add_task(MathTask())
+    class BadTaskRunner(TaskRunner):
+        def handle_fail(self, *args, **kwargs):
+            raise RuntimeError("I'm not cool with this.")
+    monkeypatch.setattr(prefect.engine, "TaskRunner", BadTaskRunner)
+    with pytest.raises(RuntimeError) as error:
+        with raise_on_fail():
+            flow.run()
+    assert "I'm not cool with this." in str(error)
