7dec7e78704372da019b0736dab877f28dc5e1b3
Chris White <chris@prefect.io>
2018-07-18 10:38:56 -0700

    Additional testing for task runners
    Addressing https://github.com/PrefectHQ/prefect/issues/29

---
+++ tests/utilities/test_tests.py
@@ -0,0 +1,51 @@
+import pytest
+
+import prefect
+
+from prefect.utilities.tests import raise_on_fail
+from prefect.core import Flow, Task
+from prefect.engine import TaskRunner
+
+
+class BusinessTask(Task):
+    def run(self):
+        raise prefect.signals.FAIL("needs more blockchain!")
+
+
+class MathTask(Task):
+    def run(self):
+        1 / 0
+
+
+def test_raise_on_fail_raises_basic_error():
+    flow = Flow()
+    task = MathTask()
+    flow.add_task(task)
+    with pytest.raises(ZeroDivisionError):
+        with raise_on_fail():
+            flow.run()
+
+
+def test_raise_on_fail_raises_basic_prefect_signal():
+    flow = Flow()
+    task = BusinessTask()
+    flow.add_task(task)
+    with pytest.raises(prefect.signals.FAIL) as error:
+        with raise_on_fail():
+            flow.run()
+    assert "needs more blockchain!" in str(error)
+
+
+def test_raise_on_fail_works_at_the_task_level_with_error():
+    taskrunner = TaskRunner(task=MathTask())
+    with pytest.raises(ZeroDivisionError):
+        with raise_on_fail():
+            taskrunner.run()
+
+
+def test_raise_on_fail_works_at_the_task_level_with_signal():
+    taskrunner = TaskRunner(task=BusinessTask())
+    with pytest.raises(prefect.signals.FAIL) as error:
+        with raise_on_fail():
+            taskrunner.run()
+    assert "needs more blockchain!" in str(error)
