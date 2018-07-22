fe0f3ab21a528a548557273860d8240cfa6c470b
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-07-22 16:20:18 -0400

    Unit tests for signals

--- tests/test_signals.py
+++
@@ -1,8 +0,0 @@
-from prefect.engine.signals import PrefectStateSignal
-
-
-def test_exceptions_are_displayed_with_messages():
-    err = PrefectStateSignal("you did something incorrectly")
-    assert "you did something incorrectly" in repr(err)
-    assert "PrefectStateSignal" in repr(err)
-
