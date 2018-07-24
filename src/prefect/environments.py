50cb53679d30e7f46ff67629d1e9ce0c45a83906
Josh Meek <josh@prefect.io>
2018-07-24 16:24:26 -0400

    Black formatting

--- src/prefect/environments.py
+++ src/prefect/environments.py
@@ -59,7 +59,7 @@ class Container(Environment):
         self.tag = tag
         self._python_dependencies = python_dependencies
         # self.client = docker.from_env()
-        self.client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+        self.client = docker.DockerClient(base_url="unix://var/run/docker.sock")
         self.running_container_id = None
 
         super().__init__(secrets=secrets)
