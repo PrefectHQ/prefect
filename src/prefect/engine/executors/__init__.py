47f0dc3957ee33b779599a365c4dad72929ac8ab
Chris White <chris@prefect.io>
2018-07-30 10:49:18 -0700

    Add first draft of dask executor

--- src/prefect/engine/executors/__init__.py
+++ src/prefect/engine/executors/__init__.py
@@ -1,3 +1,4 @@
 from prefect.engine.executors.base import Executor
 from prefect.engine.executors.local import LocalExecutor
+from prefect.engine.executors.dask import DaskExecutor
 from prefect.engine.executors.distributed import DistributedExecutor
---
+++ src/prefect/engine/executors/dask.py
@@ -0,0 +1,34 @@
+import dask
+import datetime
+import uuid
+from contextlib import contextmanager
+from functools import wraps
+from typing import Any, Callable, Dict, Iterable, List, TypeVar, Union
+
+import prefect
+from prefect.engine.executors.base import Executor
+from prefect.utilities.json import Serializable
+
+
+class DaskExecutor(Executor):
+    @contextmanager
+    def start(self) -> Iterable[None]:
+        """
+        Any initialization this executor needs to perform should be done in this
+        context manager, and torn down after yielding.
+        """
+        with dask.config.set(scheduler='synchronous') as cfg:
+            yield cfg
+
+    def submit(self, fn: Callable, *args: Any, **kwargs: Any) -> Any:
+        """
+        Submit a function to the executor for execution. Returns a future
+        """
+        return dask.delayed(fn)(*args, **kwargs)
+
+    def wait(self, futures: Iterable, timeout: datetime.timedelta = None) -> Iterable:
+        """
+        Resolves futures to their values. Blocks until the future is complete.
+        """
+        computed = dask.compute(futures)
+        return computed[0]
