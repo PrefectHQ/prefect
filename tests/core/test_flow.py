fd4b993a1df873ae42fea52098e702a6d24f5423
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-08-04 15:31:58 -0400

    Add projects to flows

--- tests/core/test_flow.py
+++ tests/core/test_flow.py
@@ -47,6 +47,13 @@ class TestCreateFlow:
         f2 = Flow(version="test")
         assert f2.version == "test"
 
+    def test_create_flow_with_project(self):
+        f1 = Flow()
+        assert f1.project == prefect.config.flows.default_project
+
+        f2 = Flow(project="test")
+        assert f2.project == "test"
+
     def test_create_flow_with_description(self):
         f1 = Flow()
         assert f1.description is None
