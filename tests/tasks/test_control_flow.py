ebd481f265a898c625e8ec52c861e13107c4b964
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-08-17 12:18:11 -0400

    Add merge task

--- tests/tasks/test_control_flow.py
+++ tests/tasks/test_control_flow.py
@@ -2,8 +2,9 @@ import pytest
 
 import prefect
 from prefect import Flow, Task, task
+from prefect.tasks.core.constants import Constant
 from prefect.engine.state import Skipped, Success
-from prefect.tasks.control_flow import ifelse, switch
+from prefect.tasks.control_flow import ifelse, switch, merge
 
 
 class Condition(Task):
@@ -65,7 +66,7 @@ def test_switch(condition_value):
         )
 
 
-def test_diamond_flow():
+def test_merging_diamond_flow():
     """
     Test a flow that branches into two separate chains that later merge back together.
 
@@ -81,8 +82,7 @@ def test_diamond_flow():
         flow.chain(*true_branch)
         flow.chain(*false_branch)
 
-        merge_task = SuccessTask(name="merge", skip_on_upstream_skip=False)
-        merge_task.set_dependencies(upstream_tasks=[true_branch[-1], false_branch[-1]])
+        merge_task = merge(true_branch[-1], false_branch[-1])
 
     with prefect.context(CONDITION=True):
         state = flow.run(return_tasks=flow.tasks)
@@ -92,3 +92,80 @@ def test_diamond_flow():
         for t in false_branch:
             assert isinstance(state.result[t], Skipped)
         assert isinstance(state.result[merge_task], Success)
+
+
+def test_list_of_tasks():
+    """
+    Test that a list of tasks can be set as a switch condition, with a warning.
+
+    The warning is raised because the tasks in the list will actually run
+    """
+
+    with Flow() as flow:
+        condition = Condition()
+        true_branch = [SuccessTask(), SuccessTask()]
+        false_branch = SuccessTask()
+        with pytest.warns(prefect.utilities.exceptions.PrefectWarning):
+            ifelse(condition, true_branch, false_branch)
+
+    with prefect.context(CONDITION=True):
+        state = flow.run(return_tasks=flow.tasks)
+
+        for t in true_branch:
+            assert isinstance(state.result[t], Success)
+        assert isinstance(state.result[false_branch], Skipped)
+
+    with prefect.context(CONDITION=False):
+        state = flow.run(return_tasks=flow.tasks)
+
+        for t in true_branch:
+            # the tasks in the list ran becuase they have no upstream dependencies.
+            assert isinstance(state.result[t], Success)
+        list_task = next(
+            t for t in flow.tasks if isinstance(t, prefect.tasks.core.collections.List)
+        )
+        # but the list itself skipped
+        assert isinstance(state.result[list_task], Skipped)
+        assert isinstance(state.result[false_branch], Success)
+
+
+def test_merge_with_upstream_skip_arg_raises_error():
+    with pytest.raises(ValueError) as exc:
+        prefect.tasks.control_flow.conditional.Merge(skip_on_upstream_skip=True)
+    assert "skip_on_upstream_skip=False" in str(exc.value)
+
+
+def test_merge_diamond_flow_with_results():
+    condition = Condition()
+    true_branch = Constant(1)
+    false_branch = Constant(0)
+
+    with Flow() as flow:
+        ifelse(condition, true_branch, false_branch)
+        merge_task = merge(true_branch, false_branch)
+
+    with prefect.context(CONDITION=True):
+        state = flow.run(return_tasks=flow.tasks)
+        assert state.result[merge_task].result == 1
+
+    with prefect.context(CONDITION=False):
+        state = flow.run(return_tasks=flow.tasks)
+        assert state.result[merge_task].result == 0
+
+    with prefect.context(CONDITION=None):
+        state = flow.run(return_tasks=flow.tasks)
+        assert state.result[merge_task].result is None
+
+def test_merge_with_list():
+    with Flow() as flow:
+        condition = Condition()
+        true_branch = prefect.utilities.tasks.as_task([Constant(1), Constant(2)])
+        false_branch = Constant(0)
+
+        with pytest.warns(prefect.utilities.exceptions.PrefectWarning):
+            ifelse(condition, true_branch, false_branch)
+        merge_task = merge(true_branch, false_branch)
+
+    with prefect.context(CONDITION=True):
+        state = flow.run(return_tasks=flow.tasks)
+        assert state.result[merge_task].result == [1, 2]
