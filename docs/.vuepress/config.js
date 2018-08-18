76cac480f502d1121fadf48404cf05f681248020
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-08-18 16:41:57 -0400

    Add license to docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,11 +5,15 @@ module.exports = {
         repo: 'prefecthq/prefect/tree/master/docs',
         editLinks: true,
         // repoLabel: 'GitHub',
-        nav: [{ text: "Overview", link: '/introduction.html' },
-            { text: "API", link: '/api/' }
+        nav: [
+            { text: "Overview", link: '/introduction.html' },
+            { text: "API", link: '/api/' },
+            { text: "License", link: '/license.html' }
         ],
         sidebar: {
-            '/api/': [{
+            '/api/': [
+                '/api/',
+                {
                     title: 'prefect',
                     collapsable: true,
                     children: ['environments', 'schedules', 'serializers', 'triggers']
@@ -41,13 +45,14 @@ module.exports = {
                     children: ['utilities/bokeh', 'utilities/collections', 'utilities/json', 'utilities/tasks']
                 },
             ],
+            '/license': [],
             '/': ['introduction', 'concepts', 'getting_started',
-                  {
+                {
                     title: 'Tutorials',
                     collapsable: true,
                     children: ['tutorials/', 'tutorials/task-retries', 'tutorials/triggers-and-references']
-                  },
-                  'comparisons'
+                },
+                'comparisons'
             ]
         }
     },
