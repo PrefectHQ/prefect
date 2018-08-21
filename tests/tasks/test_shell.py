7ddd1d4258351ec587f739e755abe5c248cff2c4
Chris White <chris@prefect.io>
2018-08-21 10:36:20 -0700

    Ensure multiline support

--- tests/tasks/test_shell.py
+++ tests/tasks/test_shell.py
@@ -10,16 +10,24 @@ def test_shell_initializes_and_runs_basic_cmd():
     f = Flow(tasks=[task])
     out = f.run(return_tasks=[task])
     assert out.is_successful()
-    assert out.result[task].result == b'hello world'
+    assert out.result[task].result == b"hello world"
 
 
 def test_shell_inherits_env():
     task = ShellTask(command="echo -n $MYTESTVAR")
     f = Flow(tasks=[task])
-    os.environ['MYTESTVAR'] = '42'
+    os.environ["MYTESTVAR"] = "42"
     out = f.run(return_tasks=[task])
     assert out.is_successful()
-    assert out.result[task].result == b'42'
+    assert out.result[task].result == b"42"
+
+
+def test_shell_task_accepts_env():
+    task = ShellTask(command="echo -n $MYTESTVAR", env=dict(MYTESTVAR="test"))
+    f = Flow(tasks=[task])
+    out = f.run(return_tasks=[task])
+    assert out.is_successful()
+    assert out.result[task].result == b"test"
 
 
 def test_shell_returns_errors_as_well():
@@ -27,4 +35,23 @@ def test_shell_returns_errors_as_well():
     f = Flow(tasks=[task])
     out = f.run(return_tasks=[task])
     assert out.is_successful()
-    assert out.result[task].result == b'ls: surely_a_dir_that_doesnt_exist: No such file or directory\n'
+    assert (
+        out.result[task].result
+        == b"ls: surely_a_dir_that_doesnt_exist: No such file or directory\n"
+    )
+
+
+def test_shell_initializes_and_runs_multiline_cmd():
+    cmd = """
+    for line in $(printenv)
+    do
+        echo "${line#*=}"
+    done
+    """
+    task = ShellTask(command=cmd, env={key: "test" for key in "abcdefgh"})
+    f = Flow(tasks=[task])
+    out = f.run(return_tasks=[task])
+    assert out.is_successful()
+    lines = out.result[task].result.decode().split("\n")
+    test_lines = [l for l in lines if l == "test"]
+    assert len(test_lines) == 8
