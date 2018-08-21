dae78379e1904e53c7e3266368f465c574003a80
Chris White <chris@prefect.io>
2018-08-21 16:07:40 -0700

    Move command and env to runtime args

--- tests/tasks/test_shell.py
+++ tests/tasks/test_shell.py
@@ -7,24 +7,24 @@ from prefect.tasks.core.shell import ShellTask
 
 
 def test_shell_initializes_and_runs_basic_cmd():
-    task = ShellTask(command="echo -n 'hello world'")
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="echo -n 'hello world'")
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     assert out.result[task].result == b"hello world"
 
 
 def test_shell_runs_other_shells():
-    task = ShellTask(command="echo -n $ZSH_NAME", shell="zsh")
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask(shell="zsh")(command="echo -n $ZSH_NAME")
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     assert out.result[task].result == b"zsh"
 
 
 def test_shell_inherits_env():
-    task = ShellTask(command="echo -n $MYTESTVAR")
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="echo -n $MYTESTVAR")
     os.environ["MYTESTVAR"] = "42"
     out = f.run(return_tasks=[task])
     assert out.is_successful()
@@ -32,24 +32,24 @@ def test_shell_inherits_env():
 
 
 def test_shell_task_accepts_env():
-    task = ShellTask(command="echo -n $MYTESTVAR", env=dict(MYTESTVAR="test"))
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="echo -n $MYTESTVAR", env=dict(MYTESTVAR="test"))
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     assert out.result[task].result == b"test"
 
 
 def test_shell_task_doesnt_inherit_if_env_is_provided():
-    task = ShellTask(command="echo -n $HOME", env=dict(MYTESTVAR="test"))
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="echo -n $HOME", env=dict(MYTESTVAR="test"))
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     assert out.result[task].result == b""
 
 
 def test_shell_returns_stderr_as_well():
-    task = ShellTask(command="ls surely_a_dir_that_doesnt_exist || exit 0")
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="ls surely_a_dir_that_doesnt_exist || exit 0")
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     assert "No such file or directory" in out.result[task].result.decode()
@@ -62,8 +62,8 @@ def test_shell_initializes_and_runs_multiline_cmd():
         echo "${line#*=}"
     done
     """
-    task = ShellTask(command=cmd, env={key: "test" for key in "abcdefgh"})
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command=cmd, env={key: "test" for key in "abcdefgh"})
     out = f.run(return_tasks=[task])
     assert out.is_successful()
     lines = out.result[task].result.decode().split("\n")
@@ -72,8 +72,8 @@ def test_shell_initializes_and_runs_multiline_cmd():
 
 
 def test_shell_task_raises_fail_if_cmd_fails():
-    task = ShellTask(command="ls surely_a_dir_that_doesnt_exist")
-    f = Flow(tasks=[task])
+    with Flow() as f:
+        task = ShellTask()(command="ls surely_a_dir_that_doesnt_exist")
     out = f.run(return_tasks=[task])
     assert out.is_failed()
     assert "Command failed with exit code" in str(out.result[task].message)
