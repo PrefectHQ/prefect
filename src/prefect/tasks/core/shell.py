f0f9eff7c471d8d702e63b92de7634e131c3ecf2
Chris White <chris@prefect.io>
2018-08-21 10:36:20 -0700

    Ensure multiline support

--- src/prefect/tasks/core/shell.py
+++ src/prefect/tasks/core/shell.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import prefect
@@ -7,11 +8,16 @@ class ShellTask(prefect.Task):
     """
     Task for running arbitrary shell commands.
     """
-    def __init__(self, command, **kwargs):
+
+    def __init__(self, command, env=None, **kwargs):
         self.command = command
+        self.env = env or dict()
         super().__init__(**kwargs)
 
     def run(self):
-        out = subprocess.check_output(["bash", "-c", self.command],
-                                      stderr=subprocess.STDOUT)
+        current_env = os.environ.copy()
+        current_env.update(self.env)
+        out = subprocess.check_output(
+            ["bash", "-c", self.command], stderr=subprocess.STDOUT, env=current_env
+        )
         return out
