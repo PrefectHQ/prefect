5379021b72c7a7c13304ffbe215545273d3f70aa
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-08-23 17:08:54 -0400

    Simplify signatures of TaskRunner methods

--- tests/engine/test_task_runner.py
+++ tests/engine/test_task_runner.py
@@ -197,12 +197,19 @@ class TestTaskRunner_get_pre_run_state:
     @pytest.mark.parametrize("state", [Pending(), Retrying(), Scheduled()])
     def test_returns_running_if_successful_with_pending_state(self, state):
         runner = TaskRunner(SuccessTask())
-        state = runner.get_pre_run_state(state=state)
+        state = runner.get_pre_run_state(
+            state=state, upstream_states=set(), ignore_trigger=False, inputs={}
+        )
         assert isinstance(state, Running)
 
     def test_ignores_cached_state_if_task_didnt_ask_for_it(self):
         runner = TaskRunner(SuccessTask())
-        state = runner.get_pre_run_state(state=CachedState(cached_result=4))
+        state = runner.get_pre_run_state(
+            state=CachedState(cached_result=4),
+            upstream_states=set(),
+            ignore_trigger=False,
+            inputs={},
+        )
         assert isinstance(state, Running)
 
     def test_returns_running_if_cached_state_with_expired_cache(self):
@@ -213,7 +220,10 @@ class TestTaskRunner_get_pre_run_state:
         )
         expiration = datetime.datetime.utcnow() - datetime.timedelta(days=1)
         state = runner.get_pre_run_state(
-            state=CachedState(cached_result=4, cached_result_expiration=expiration)
+            state=CachedState(cached_result=4, cached_result_expiration=expiration),
+            upstream_states=set(),
+            ignore_trigger=False,
+            inputs={},
         )
         assert isinstance(state, Running)
 
@@ -234,16 +244,18 @@ class TestTaskRunner_get_pre_run_state:
         expiration = datetime.datetime.utcnow() + datetime.timedelta(days=1)
         inputs = dict(x=2, y=1)
         params = dict(p="p", q=99)
-        state = runner.get_pre_run_state(
-            state=CachedState(
-                cached_parameters=params,
-                cached_inputs=inputs,
-                cached_result=4,
-                cached_result_expiration=expiration,
-            ),
-            inputs=inputs,
-            parameters=params,
-        )
+        with prefect.context(_parameters=params):
+            state = runner.get_pre_run_state(
+                state=CachedState(
+                    cached_parameters=params,
+                    cached_inputs=inputs,
+                    cached_result=4,
+                    cached_result_expiration=expiration,
+                ),
+                inputs=inputs,
+                upstream_states=set(),
+                ignore_trigger=False,
+            )
         assert isinstance(state, Success)
         assert state.result == 4
 
@@ -255,7 +267,9 @@ class TestTaskRunner_get_pre_run_state:
         )
         expiration = datetime.datetime.utcnow() + datetime.timedelta(days=1)
         cached_state = CachedState(cached_result=4, cached_result_expiration=expiration)
-        state = runner.get_pre_run_state(state=cached_state)
+        state = runner.get_pre_run_state(
+            state=cached_state, upstream_states=set(), ignore_trigger=False, inputs={}
+        )
         assert isinstance(state, Success)
         assert state.result == 4
         assert state.cached == cached_state
@@ -283,14 +297,17 @@ class TestTaskRunner_get_pre_run_state:
                 cached_result_expiration=expiration,
             ),
             inputs=dict(x=1),
-            parameters=dict(y=7),
+            upstream_states=set(),
+            ignore_trigger=False,
         )
         assert isinstance(state, Running)
 
     def test_returns_failed_with_internal_error(self):
         runner = TaskRunner(SuccessTask())
         # pass an invalid state to the function to see if the resulting errors are caught
-        state = runner.get_pre_run_state(state=1)
+        state = runner.get_pre_run_state(
+            state=1, upstream_states=set(), ignore_trigger=False, inputs={}
+        )
         assert isinstance(state, Failed)
         assert "object has no attribute" in str(state.message).lower()
 
@@ -298,7 +315,10 @@ class TestTaskRunner_get_pre_run_state:
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
             runner.get_pre_run_state(
-                state=Pending(), upstream_states={1: Pending(), 2: Success()}
+                state=Pending(),
+                upstream_states={Pending(), Success()},
+                ignore_trigger=False,
+                inputs={},
             )
         assert "upstream tasks are not finished" in str(exc.value).lower()
 
@@ -309,7 +329,10 @@ class TestTaskRunner_get_pre_run_state:
         task = SuccessTask(skip_on_upstream_skip=False)
         runner = TaskRunner(task)
         state = runner.get_pre_run_state(
-            state=Pending(), upstream_states={1: Skipped()}
+            state=Pending(),
+            upstream_states={Skipped()},
+            ignore_trigger=False,
+            inputs={},
         )
         assert isinstance(state, Running)
 
@@ -317,7 +340,10 @@ class TestTaskRunner_get_pre_run_state:
         task = SuccessTask(True)
         runner = TaskRunner(task)
         state = runner.get_pre_run_state(
-            state=Pending(), upstream_states={1: Skipped()}
+            state=Pending(),
+            upstream_states={Skipped()},
+            ignore_trigger=False,
+            inputs={},
         )
         assert isinstance(state, Skipped)
         assert "upstream task was skipped" in state.message.lower()
@@ -325,19 +351,25 @@ class TestTaskRunner_get_pre_run_state:
     def test_raises_triggerfail_if_trigger_returns_false(self):
         task = SuccessTask(trigger=lambda upstream_states: False)
         runner = TaskRunner(task)
-        state = runner.get_pre_run_state(state=Pending())
+        state = runner.get_pre_run_state(
+            state=Pending(), upstream_states=set(), ignore_trigger=False, inputs={}
+        )
         assert isinstance(state, TriggerFailed)
 
     def test_ignores_trigger(self):
         task = SuccessTask(trigger=lambda upstream_states: False)
         runner = TaskRunner(task)
-        state = runner.get_pre_run_state(state=Pending(), ignore_trigger=True)
+        state = runner.get_pre_run_state(
+            state=Pending(), ignore_trigger=True, upstream_states=set(), inputs={}
+        )
         assert isinstance(state, Running)
 
     def test_raises_dontrun_if_state_is_running(self):
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_pre_run_state(state=Running())
+            runner.get_pre_run_state(
+                state=Running(), upstream_states=set(), ignore_trigger=False, inputs={}
+            )
         assert "already running" in str(exc.value).lower()
 
     @pytest.mark.parametrize(
@@ -346,7 +378,9 @@ class TestTaskRunner_get_pre_run_state:
     def test_raises_dontrun_if_state_is_finished(self, state):
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_pre_run_state(state=state)
+            runner.get_pre_run_state(
+                state=state, upstream_states=set(), ignore_trigger=False, inputs={}
+            )
         assert "already finished" in str(exc.value).lower()
 
     def test_raises_dontrun_if_state_is_not_pending(self):
@@ -356,14 +390,18 @@ class TestTaskRunner_get_pre_run_state:
         """
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_pre_run_state(state=State())
+            runner.get_pre_run_state(
+                state=State(), upstream_states=set(), ignore_trigger=False, inputs={}
+            )
         assert "not ready to run" in str(exc.value).lower()
 
         class MyState(State):
             pass
 
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_pre_run_state(state=MyState())
+            runner.get_pre_run_state(
+                state=MyState(), upstream_states=set(), ignore_trigger=False, inputs={}
+            )
         assert "unrecognized" in str(exc.value).lower()
 
 
@@ -388,12 +426,12 @@ class TestTaskRunner_get_run_state:
     def test_raises_dontrun_if_state_is_not_running(self, state):
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_run_state(state=state)
+            runner.get_run_state(state=state, inputs={})
         assert "not in a running state" in str(exc.value).lower()
 
     def test_runs_task(self):
         runner = TaskRunner(SuccessTask())
-        state = runner.get_run_state(state=Running())
+        state = runner.get_run_state(state=Running(), inputs={})
         assert state == Success(result=1)
         assert "succeeded" in state.message.lower()
 
@@ -404,7 +442,7 @@ class TestTaskRunner_get_run_state:
 
     def test_fails_if_task_with_inputs_doesnt_receive_inputs(self):
         runner = TaskRunner(AddTask())
-        state = runner.get_run_state(state=Running())
+        state = runner.get_run_state(state=Running(), inputs={})
         assert isinstance(state, Failed)
         assert isinstance(state.message, TypeError)
         assert "required positional arguments" in str(state.message).lower()
@@ -415,9 +453,11 @@ class TestTaskRunner_get_run_state:
                 raise signals.DONTRUN()
 
         runner = TaskRunner(DontRunTask())
-        state = runner.get_run_state(state=Running())
+        state = runner.get_run_state(state=Running(), inputs={})
         assert isinstance(state, Skipped)
         assert "dontrun was raised" in str(state.message).lower()
+
+
 
     def test_ignores_cached_attribute_if_task_doesnt_ask_for_it(self):
         runner = TaskRunner(AddTask())
@@ -433,9 +473,10 @@ class TestTaskRunner_get_run_state:
     def test_sets_cached_attribute_if_task_requests(self):
         now = datetime.datetime.utcnow()
         runner = TaskRunner(AddTask(cache_for=datetime.timedelta(days=1)))
-        state = runner.get_run_state(
-            state=Running(), inputs=dict(x=1, y=2), parameters=dict(qq="time")
-        )
+        with prefect.context(_parameters=dict(qq="time")):
+            state = runner.get_run_state(
+                state=Running(), inputs=dict(x=1, y=2),
+            )
         cached = state.cached
         assert isinstance(cached, CachedState)
         assert cached.cached_result_expiration >= now + datetime.timedelta(hours=23)
@@ -453,7 +494,7 @@ class TestTaskRunner_get_post_run_state:
     def test_raises_dontrun_if_state_is_not_finished(self, state):
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_post_run_state(state=state)
+            runner.get_post_run_state(state=state, inputs={})
         assert "not in a finished state" in str(exc.value).lower()
 
     @pytest.mark.parametrize(
@@ -462,7 +503,7 @@ class TestTaskRunner_get_post_run_state:
     def test_raises_dontrun_if_state_is_finished_but_not_retry_eligable(self, state):
         runner = TaskRunner(SuccessTask())
         with pytest.raises(signals.DONTRUN) as exc:
-            runner.get_post_run_state(state=state)
+            runner.get_post_run_state(state=state, inputs={})
         assert "requires no further processing" in str(exc.value).lower()
 
     def test_returns_retry_if_failed_and_retry_eligable(self):
@@ -470,7 +511,7 @@ class TestTaskRunner_get_post_run_state:
             ErrorTask(max_retries=1, retry_delay=datetime.timedelta(minutes=1))
         )
         with prefect.context(_task_run_number=1):
-            state = runner.get_post_run_state(state=Failed())
+            state = runner.get_post_run_state(state=Failed(), inputs={})
         assert isinstance(state, Retrying)
         assert (state.scheduled_time - datetime.datetime.utcnow()) < datetime.timedelta(
             minutes=1
@@ -478,4 +519,4 @@ class TestTaskRunner_get_post_run_state:
 
         with prefect.context(_task_run_number=2):
             with pytest.raises(signals.DONTRUN):
-                runner.get_post_run_state(state=Failed())
+                runner.get_post_run_state(state=Failed(), inputs={})
