6fa2ef14c22ad2a3412c278de4b03e81491d4e1e
Chris White <chris@prefect.io>
2018-08-27 19:05:25 -0700

    Initial commit of refactor ideas
    Refactored

--- tests/core/test_task_map.py
+++ tests/core/test_task_map.py
@@ -44,7 +44,8 @@ def test_map_spawns_new_tasks():
     with Flow() as f:
         res = a.map(ll)
 
-    s = f.run(return_tasks=f.tasks)
+    with raise_on_exception():
+        s = f.run(return_tasks=f.tasks)
     slist = s.result[res]
     assert s.is_successful()
     assert isinstance(slist, list)
