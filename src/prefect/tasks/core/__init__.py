4d1dabe9c925952b4e72fac55b4b1f5da9b50778
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-09-01 12:15:22 -0400

    Add module docstring

--- src/prefect/tasks/core/__init__.py
+++ src/prefect/tasks/core/__init__.py
@@ -1,4 +1,7 @@
 # Licensed under LICENSE.md; also available at https://www.prefect.io/licenses/alpha-eula
+"""
+These Task classes are "core" in the sense that they are used elsewhere in Prefect.
+"""
 
 import prefect.tasks.core.collections
 import prefect.tasks.core.constants
