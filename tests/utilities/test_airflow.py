6f8561df351fdb2d609fadea24eb35054a950679
Chris White <chris@prefect.io>
2018-09-27 13:23:12 -0700

    Tweaks and more tests

--- tests/utilities/test_airflow.py
+++ tests/utilities/test_airflow.py
@@ -1,4 +1,5 @@
 import pytest
+
 try:
     import airflow
 except ImportError:
@@ -6,10 +7,6 @@ except ImportError:
 
 from prefect.engine.state import Success, Skipped, Failed
 from prefect.utilities.airflow_utils import AirFlow
-
-
-def assert_state_is():
-    pass
 
 
 def test_example_branch_operator():
@@ -32,8 +29,12 @@ def test_example_branch_operator():
     branch, state = run_branches.pop()
     assert state.is_successful()
     assert all([isinstance(s, Skipped) for t, s in branch_tasks if t != branch])
-    assert all([isinstance(s, Skipped) for t, s in follow_tasks if (branch.name not in t.name)])
-    assert all([isinstance(s, Success) for t, s in follow_tasks if (branch.name in t.name)])
+    assert all(
+        [isinstance(s, Skipped) for t, s in follow_tasks if (branch.name not in t.name)]
+    )
+    assert all(
+        [isinstance(s, Success) for t, s in follow_tasks if (branch.name in t.name)]
+    )
 
 
 def test_example_xcom():
@@ -42,12 +43,26 @@ def test_example_xcom():
     assert res.is_successful()
 
     for task, state in res.result.items():
-        if task.name == 'puller':
+        if task.name == "puller":
             assert state.is_successful()
             assert state.result is None
-        elif task.name == 'push':
+        elif task.name == "push":
             assert state.is_successful()
             assert state.result == [1, 2, 3]
-        if task.name == 'push_by_returning':
+        if task.name == "push_by_returning":
             assert state.is_successful()
-            assert state.result == {'a': 'b'}
+            assert state.result == {"a": "b"}
+
+
+def test_example_short_circuit_operator():
+    flow = AirFlow(dag_id="example_short_circuit_operator")
+    res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
+    assert res.is_successful()
+
+    for task, state in res.result.items():
+        if "condition" in task.name:
+            assert isinstance(state, Success)
+        elif "false" in task.name:
+            assert isinstance(state, Skipped)
+        elif "true" in task.name:
+            assert isinstance(state, Success)
