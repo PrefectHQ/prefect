579ed860f276f82786f6b0ecc824bb6e7f632468
Chris White <chris@prefect.io>
2018-10-03 08:47:45 -0700

    initial commit of jinja template task

---
+++ tests/tasks/test_strings.py
@@ -0,0 +1,47 @@
+import os
+import pytest
+import subprocess
+import tempfile
+
+from prefect import Flow
+from prefect.engine import signals
+from prefect.tasks.strings import StringFormatterTask, JinjaTemplateTask
+from prefect.utilities.tests import raise_on_exception
+
+
+def test_string_formatter_simply_formats():
+    task = StringFormatterTask(template="{name} is from {place}")
+    with Flow() as f:
+        ans = task(name="Ford", place="Betelgeuse")
+    res = f.run(return_tasks=[ans])
+    assert res.is_successful()
+    assert res.result[ans].result == "Ford is from Betelgeuse"
+
+
+def test_string_formatter_fails_in_expected_ways():
+    t1 = StringFormatterTask(template="{name} is from {place}")
+    t2 = StringFormatterTask(template="{0} is from {1}")
+    f = Flow(tasks=[t1, t2])
+    res = f.run(return_tasks=[t1, t2])
+
+    assert res.is_failed()
+    assert isinstance(res.result[t1].message, KeyError)
+    assert isinstance(res.result[t2].message, IndexError)
+
+
+def test_jinja_template_simply_formats():
+    task = JinjaTemplateTask(template="{{ name }} is from {{ place }}")
+    with Flow() as f:
+        ans = task(name="Ford", place="Betelgeuse")
+    res = f.run(return_tasks=[ans])
+    assert res.is_successful()
+    assert res.result[ans].result == "Ford is from Betelgeuse"
+
+
+def test_jinja_template_partially_formats():
+    task = JinjaTemplateTask(template="{{ name }} is from {{ place }}")
+    with Flow() as f:
+        ans = task(name="Ford")
+    res = f.run(return_tasks=[ans])
+    assert res.is_successful()
+    assert res.result[ans].result == "Ford is from "
