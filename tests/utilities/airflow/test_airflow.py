095d5c2ff9a82b03ac8723ef1503cf19e9139fc8
Chris White <chris@prefect.io>
2018-10-08 11:51:25 -0700

    Update tests so we can write our own DAGS for testing
    empty-message
    
    oops forgot to commit new location of test_airflow file

--- tests/utilities/airflow/test_airflow.py
+++ tests/utilities/airflow/test_airflow.py
@@ -11,14 +11,27 @@ from prefect.utilities.airflow_utils import AirFlow
 
 
 @pytest.fixture(scope="module")
-def airflow_db():
+def airflow_settings():
     with tempfile.NamedTemporaryFile(
         prefix="prefect-airflow", suffix="prefect-airflow-test.db"
     ) as tmp:
         env = os.environ.copy()
         env["AIRFLOW__CORE__SQL_ALCHEMY_CONN"] = "sqlite:///" + tmp.name
+        dag_folder = os.path.join(os.path.dirname(__file__), "dags")
+        env["AIRFLOW__CORE__DAGS_FOLDER"] = dag_folder
         status = subprocess.check_output(["bash", "-c", "airflow initdb"], env=env)
-        yield tmp.name
+        yield dict(db_file=tmp.name, dag_folder=dag_folder)
+
+
+def test_trigger_rules_dag(airflow_settings):
+    flow = AirFlow(dag_id="trigger_rules", **airflow_settings)
+    res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
+    assert res.is_failed()
+    for task, state in res.result.items():
+        if "3" in task.name:
+            assert isinstance(state, TriggerFailed)
+        else:
+            assert state.is_successful()
 
 
 @pytest.mark.airflow()
@@ -38,8 +51,8 @@ def test_airflow_creates_sqlite_db_if_none_provided():
 
 
 @pytest.mark.airflow()
-def test_example_branch_operator(airflow_db):
-    flow = AirFlow(dag_id="example_branch_operator", db_file=airflow_db)
+def test_example_branch_operator(airflow_settings):
+    flow = AirFlow(dag_id="example_branch_operator", **airflow_settings)
     res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
     assert res.is_successful()
 
@@ -67,8 +80,8 @@ def test_example_branch_operator(airflow_db):
 
 
 @pytest.mark.airflow()
-def test_example_xcom(airflow_db):
-    flow = AirFlow(dag_id="example_xcom", db_file=airflow_db)
+def test_example_xcom(airflow_settings):
+    flow = AirFlow(dag_id="example_xcom", **airflow_settings)
     res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
     assert res.is_successful()
 
@@ -85,8 +98,8 @@ def test_example_xcom(airflow_db):
 
 
 @pytest.mark.airflow()
-def test_example_short_circuit_operator(airflow_db):
-    flow = AirFlow(dag_id="example_short_circuit_operator", db_file=airflow_db)
+def test_example_short_circuit_operator(airflow_settings):
+    flow = AirFlow(dag_id="example_short_circuit_operator", **airflow_settings)
     res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
     assert res.is_successful()
 
@@ -100,8 +113,8 @@ def test_example_short_circuit_operator(airflow_db):
 
 
 @pytest.mark.airflow()
-def test_example_bash_operator(airflow_db):
-    flow = AirFlow(dag_id="example_bash_operator", db_file=airflow_db)
+def test_example_bash_operator(airflow_settings):
+    flow = AirFlow(dag_id="example_bash_operator", **airflow_settings)
     res = flow.run(execution_date="2018-09-20", return_tasks=flow.tasks)
     assert res.is_successful()
 
@@ -110,8 +123,8 @@ def test_example_bash_operator(airflow_db):
 
 
 @pytest.mark.airflow()
-def test_extending_airflow_dag_with_prefect_task(airflow_db):
-    flow = AirFlow(dag_id="example_bash_operator", db_file=airflow_db)
+def test_extending_airflow_dag_with_prefect_task(airflow_settings):
+    flow = AirFlow(dag_id="example_bash_operator", **airflow_settings)
     run_task0 = flow.get_tasks(name="runme_0")
     with flow:
         t1 = Task(trigger=triggers.all_failed)(upstream_tasks=[run_task0])
