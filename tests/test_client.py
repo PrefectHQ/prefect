76eae1e43a65c40bf4bf818d5a1048f4c9f03e21
Chris White <chris@prefect.io>
2018-10-11 10:23:11 -0700

    Secrets depend on config

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,6 +10,7 @@ from prefect.client import Secret
 def test_create_secret():
     secret = Secret(name="test")
     assert secret
+    assert secret._use_local_secrets is prefect.config.server.use_local_secrets
 
 
 def test_secret_get_none():
@@ -22,3 +23,10 @@ def test_secret_value_pulled_from_context():
     with prefect.context(_secrets=dict(test=42)):
         assert secret.get() == 42
     assert secret.get() is None
+
+
+def test_secret_value_depends_on_prefect_config():
+    secret = Secret(name="test")
+    secret._use_local_secrets = False
+    with prefect.context(_secrets=dict(test=42)):
+        assert secret.get() is None
