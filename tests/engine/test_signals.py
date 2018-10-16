40220eadb87c16452f6e31e1deafa3fef8b0e166
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-10-16 10:33:59 -0400

    Pass signal arguments to states

--- tests/engine/test_signals.py
+++ tests/engine/test_signals.py
@@ -1,3 +1,4 @@
+import datetime
 import pytest
 
 from prefect.engine.signals import (
@@ -33,6 +34,26 @@ def test_signals_create_states():
     assert exc.value.state.result is None
 
 
+def test_signals_pass_arguments_to_states():
+    with pytest.raises(PrefectStateSignal) as exc:
+        raise SUCCESS("you did it!", result=100)
+    assert exc.value.state.message is exc.value
+    assert exc.value.state.result == 100
+    assert str(exc.value.state.message) == "you did it!"
+
+
+def test_signals_dont_pass_invalid_arguments_to_states():
+    with pytest.raises(TypeError):
+        raise SUCCESS(bad_result=100)
+
+
+def test_retry_signals_can_set_retry_time():
+    date = datetime.datetime(2019, 1, 1)
+    with pytest.raises(PrefectStateSignal) as exc:
+        raise RETRY(start_time=date)
+    assert exc.value.state.start_time == date
+
+
 @pytest.mark.parametrize(
     "signal,state",
     [
@@ -50,3 +71,11 @@ def test_signals_creates_correct_states(signal, state):
     assert type(exc.value.state) is state
     assert exc.value.state.message is exc.value
     assert str(exc.value.state.message) == state.__name__
+
+
+def test_retry_signals_carry_default_retry_time_on_state():
+    with pytest.raises(Exception) as exc:
+        raise RETRY()
+    assert exc.value.state.start_time is not None
+    now = datetime.datetime.utcnow()
+    assert now - exc.value.state.start_time < datetime.timedelta(seconds=0.1)
