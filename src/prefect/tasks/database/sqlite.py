6d1782a324ce51b37d12d5102497a0aa28954ab5
Chris White <chris@prefect.io>
2018-10-25 15:39:20 -0700

    Fork into two distinct tasks

--- src/prefect/tasks/database/sqlite.py
+++ src/prefect/tasks/database/sqlite.py
@@ -7,18 +7,18 @@ import prefect
 from prefect import Task
 
 
-class SqliteTask(Task):
+class SQLiteQueryTask(Task):
     """
     Args:
-        - query (str, optional): the optional _default_ template string to render at runtime;
+        - db (str): the location of the database (.db) file
+        - query (str, optional): the optional _default_ query to execute at runtime;
             can also be provided as a keyword to `run`, which takes precendence over this default.
-        - db (str, optional): the optional _default_ template string to render at runtime;
-            can also be provided as a keyword to `run`, which takes precendence over this default.
+            Note that a query should consist of a _single SQL statement_.
         - **kwargs (optional): additional keyword arguments to pass to the
             standard Task constructor
     """
 
-    def __init__(self, query: str = None, db: str = ":memory:", **kwargs) -> None:
+    def __init__(self, db: str, query: str = None, **kwargs) -> None:
         self.db = db
         self.query = query
         super().__init__(**kwargs)
@@ -26,16 +26,45 @@ class SqliteTask(Task):
     def run(self, query: str = None):
         """
         Args:
-            - query (str, optional): the template string to render; if not
-                provided, `self.template` will be used
+            - query (str, optional): the optional query to execute at runtime;
+                if not provided, `self.query` will be used instead. Note that a query should consist of a _single SQL statement_.
 
         Returns:
-            - str: the rendered string
+            - [Any]: the results of the query
         """
         query = self.query if query is None else query
         with closing(sql.connect(self.db)) as conn:
             with closing(conn.cursor()) as cursor:
-                cursor.executescript(query)
+                cursor.execute(query)
                 out = cursor.fetchall()
                 conn.commit()
         return out
+
+
+class SQLiteScriptTask(Task):
+    """
+    Args:
+        - script (str, optional): the optional _default_ query string to render at runtime;
+            can also be provided as a keyword to `run`, which takes precendence over this default.
+        - db (str, optional): the optional _default_ template string to render at runtime;
+            can also be provided as a keyword to `run`, which takes precendence over this default.
+        - **kwargs (optional): additional keyword arguments to pass to the
+            standard Task constructor
+    """
+
+    def __init__(self, db: str, script: str = None, **kwargs) -> None:
+        self.db = db
+        self.script = script
+        super().__init__(**kwargs)
+
+    def run(self, script: str = None):
+        """
+        Args:
+            - query (str, optional): the optional query to execute at runtime;
+                if not provided, `self.query` will be used instead. Note that a query should consist of a _single SQL statement_.
+        """
+        script = self.script if script is None else script
+        with closing(sql.connect(self.db)) as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.executescript(script)
+                conn.commit()
