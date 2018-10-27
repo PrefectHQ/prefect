d58cf2f9a0877a3dee33986011c012059bd42fc1
Chris White <chris@prefect.io>
2018-10-27 14:52:12 -0700

    Remove unnecessary code example

--- docs/tokenizer.py
+++ docs/tokenizer.py
@@ -58,24 +58,6 @@ tokenizer_map = {
 }
 
 
-code = """
-from prefect import task, Task, Flow
-import random
-
-@task
-def random_number():
-    return random.randint(0, 100)
-
-@task
-def plus_one(x):
-    return x + 1
-
-with Flow('My Functional Flow') as flow:
-    r = random_number()
-    y = plus_one(x=r)
-"""
-
-
 def format_code(code):
     pp = Python3Lexer()
     tokens = pp.get_tokens(code)
