79185b5a437b11d0b50846e711abcc0823d2f401
Chris White <chris@prefect.io>
2018-11-03 18:00:31 -0700

    Engine is type checked and we ignore a lot

--- src/prefect/engine/cloud_handler.py
+++ src/prefect/engine/cloud_handler.py
@@ -11,10 +11,12 @@ from prefect.engine.state import State
 class CloudHandler:
     def __init__(self) -> None:
         if config.get("prefect_cloud", None):
-            self.load_prefect_config()
+            self.load_prefect_client()
 
-    def load_prefect_client(self):
-        client = Client(config.api_url, os.path.join(config.api_url, "graphql/"))
+    def load_prefect_client(self) -> None:
+        client = Client(  # type: ignore
+            config.api_url, os.path.join(config.api_url, "graphql/")
+        )
         client.login(email=config.email, password=config.password)
 
         self.states_gql = States(client=client)
@@ -28,7 +30,7 @@ class CloudHandler:
 
     def getFlowRunVersion(self) -> int:
         gql_output = self.states_gql.query_flow_run_version(self.flow_run_id)
-        return gql_output.flowRuns[0].version
+        return gql_output.flowRuns[0].version  # type: ignore
 
     def setTaskRunState(self, task_run_id: str, version: int, state: State) -> None:
         self.states_gql.set_task_run_from_serialized_state(task_run_id, version, state)
@@ -37,4 +39,4 @@ class CloudHandler:
         gql_output = self.states_gql.query_task_run_id_and_version(
             self.flow_run_id, task_id
         )
-        return gql_output.taskRuns[0]
+        return gql_output.taskRuns[0]  # type: ignore
