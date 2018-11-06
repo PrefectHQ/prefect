1a020452006138a60f61a5b836b533f966c89e5a
Chris White <chris@prefect.io>
2018-11-06 12:52:58 -0800

    Add some client tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,72 @@
+import pytest
+from unittest.mock import MagicMock
+
 import prefect
-from prefect.client import Secret
+from prefect.client import Client, Secret
+from prefect.utilities.tests import set_temporary_config
+
+
+#################################
+##### Client Tests
+#################################
+def test_client_initializes_from_kwargs():
+    client = Client(
+        api_server="api_server", graphql_server="graphql_server", token="token"
+    )
+    assert client._api_server == "api_server"
+    assert client._graphql_server == "graphql_server"
+    assert client._token == "token"
+
+
+def test_client_initializes_from_config():
+    with set_temporary_config("server.api_server", "api_server"):
+        with set_temporary_config("server.graphql_server", "graphql_server"):
+            with set_temporary_config("server.token", "token"):
+                client = Client()
+    assert client._api_server == "api_server"
+    assert client._graphql_server == "graphql_server"
+    assert client._token is None
+
+
+def test_client_initializes_from_context():
+    with set_temporary_config("server.api_server", None):
+        with set_temporary_config("server.graphql_server", None):
+            with set_temporary_config("server.token", None):
+                with prefect.context(
+                    api_server="api_server",
+                    graphql_server="graphql_server",
+                    token="token",
+                ):
+                    client = Client()
+
+    assert client._api_server == "api_server"
+    assert client._graphql_server == "graphql_server"
+    assert client._token == "token"
+
+
+def test_client_logs_in_and_saves_token(monkeypatch):
+    post = MagicMock(
+        return_value=MagicMock(
+            ok=True, json=MagicMock(return_value=dict(token="secret_token"))
+        )
+    )
+    monkeypatch.setattr("requests.post", post)
+    client = Client(api_server="http://my-server.foo")
+    client.login("test@example.com", "1234")
+    assert post.called
+    assert post.call_args[0][0] == "http://my-server.foo/login"
+    assert post.call_args[1]["auth"] == ("test@example.com", "1234")
+    assert client._token == "secret_token"
+
+
+def test_client_raises_if_login_fails(monkeypatch):
+    post = MagicMock(return_value=MagicMock(ok=False))
+    monkeypatch.setattr("requests.post", post)
+    client = Client(api_server="http://my-server.foo")
+    with pytest.raises(ValueError):
+        client.login("test@example.com", "1234")
+    assert post.called
+    assert post.call_args[0][0] == "http://my-server.foo/login"
 
 
 #################################
