0a1e0934a665282f84c3501ed6e20d4060d9a688
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-11-07 16:54:38 -0500

    Ensure directory exists

--- src/prefect/cli/__init__.py
+++ src/prefect/cli/__init__.py
@@ -43,6 +43,7 @@ def make_user_config():
 
     config_path = os.path.join(os.path.dirname(prefect.__file__), "config.toml")
     with open(config_path, "r") as standard_config:
+        os.makedirs(os.path.dirname(user_config_path), exist_ok=True)
         with open(user_config_path, "w") as user_config:
             user_config.write(standard_config.read())
 
