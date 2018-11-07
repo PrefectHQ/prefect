28011d294ba8f1ce2ddb2207132acf19560dc439
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-11-07 16:46:47 -0500

    Add CLI command to generate new config file

--- src/prefect/cli/__init__.py
+++ src/prefect/cli/__init__.py
@@ -28,3 +28,22 @@ def cli(registry_path=None, registry_encryption_key=None):
 
 cli.add_command(flows)
 cli.add_command(configure)
+
+
+@cli.command()
+def make_user_config():
+    """
+    Generates a user configuration file
+    """
+    user_config_path = prefect.config.get("user_config_path")
+    if not user_config_path:
+        raise ValueError("No user config path set!")
+    elif os.path.isfile(user_config_path):
+        raise ValueError("A file already exists at {}".format(user_config_path))
+
+    config_path = os.path.join(os.path.dirname(prefect.__file__), "config.toml")
+    with open(config_path, "r") as standard_config:
+        with open(user_config_path, "w") as user_config:
+            user_config.write(standard_config.read())
+
+    click.secho("Config created at {}".format(user_config_path), fg="green")
