a295771d1858a2010eab7201bfbd7278d13ca19c
Chris White <chris@prefect.io>
2018-11-07 08:42:48 -0800

    Logout and client based secrets

--- tests/test_client.py
+++ tests/test_client.py
@@ -13,15 +13,6 @@ from prefect.utilities.tests import set_temporary_config
 #################################
 
 
-def test_client_initializes_from_kwargs():
-    client = Client(
-        api_server="api_server", graphql_server="graphql_server", token="token"
-    )
-    assert client.api_server == "api_server"
-    assert client.graphql_server == "graphql_server"
-    assert client.token == "token"
-
-
 def test_client_initializes_from_config():
     with set_temporary_config("server.api_server", "api_server"):
         with set_temporary_config("server.graphql_server", "graphql_server"):
@@ -30,6 +21,16 @@ def test_client_initializes_from_config():
     assert client.api_server == "api_server"
     assert client.graphql_server == "graphql_server"
     assert client.token == "token"
+
+
+def test_client_token_initializes_from_kwarg():
+    with set_temporary_config("server.api_server", "api_server"):
+        with set_temporary_config("server.graphql_server", "graphql_server"):
+            with set_temporary_config("server.auth_token", "token"):
+                client = Client(token="init-token")
+    assert client.api_server == "api_server"
+    assert client.graphql_server == "graphql_server"
+    assert client.token == "init-token"
 
 
 def test_client_token_initializes_from_file(monkeypatch):
@@ -57,7 +58,8 @@ def test_client_logs_in_and_saves_token(monkeypatch):
     mock_file = mock_open()
     monkeypatch.setattr("builtins.open", mock_file)
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client()
     client.login("test@example.com", "1234")
     assert post.called
     assert post.call_args[0][0] == "http://my-server.foo/login"
@@ -73,7 +75,8 @@ def test_client_logs_out_and_deletes_auth_token(monkeypatch):
         )
     )
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client()
     client.login("test@example.com", "1234")
     assert os.path.exists("~/.prefect/.credentials/auth_token")
     with open("~/.prefect/.credentials/auth_token", "r") as f:
@@ -85,7 +88,8 @@ def test_client_logs_out_and_deletes_auth_token(monkeypatch):
 def test_client_raises_if_login_fails(monkeypatch):
     post = MagicMock(return_value=MagicMock(ok=False))
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client()
     with pytest.raises(ValueError):
         client.login("test@example.com", "1234")
     assert post.called
@@ -95,7 +99,8 @@ def test_client_raises_if_login_fails(monkeypatch):
 def test_client_posts_raises_with_no_token(monkeypatch):
     post = MagicMock()
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client()
     with pytest.raises(ValueError) as exc:
         result = client.post("/foo/bar")
     assert "Client.login" in str(exc.value)
@@ -106,7 +111,8 @@ def test_client_posts_to_api_server(monkeypatch):
         return_value=MagicMock(json=MagicMock(return_value=dict(success=True)))
     )
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo", token="secret_token")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client(token="secret_token")
     result = client.post("/foo/bar")
     assert result == {"success": True}
     assert post.called
@@ -123,7 +129,8 @@ def test_client_posts_retries_if_token_needs_refreshing(monkeypatch):
         )
     )
     monkeypatch.setattr("requests.post", post)
-    client = Client(api_server="http://my-server.foo", token="secret_token")
+    with set_temporary_config("server.api_server", "http://my-server.foo"):
+        client = Client(token="secret_token")
     with pytest.raises(requests.HTTPError) as exc:
         result = client.post("/foo/bar")
     assert exc.value is error
@@ -139,7 +146,8 @@ def test_client_posts_graphql_to_graphql_server(monkeypatch):
         )
     )
     monkeypatch.setattr("requests.post", post)
-    client = Client(graphql_server="http://my-server.foo/graphql", token="secret_token")
+    with set_temporary_config("server.graphql_server", "http://my-server.foo/graphql"):
+        client = Client(token="secret_token")
     result = client.graphql("{projects{name}}")
     assert result == {"success": True}
     assert post.called
@@ -156,7 +164,8 @@ def test_client_graphql_retries_if_token_needs_refreshing(monkeypatch):
         )
     )
     monkeypatch.setattr("requests.post", post)
-    client = Client(graphql_server="http://my-server.foo/graphql", token="secret_token")
+    with set_temporary_config("server.graphql_server", "http://my-server.foo/graphql"):
+        client = Client(token="secret_token")
     with pytest.raises(requests.HTTPError) as exc:
         result = client.graphql("{}")
     assert exc.value is error
@@ -191,4 +200,6 @@ def test_secret_value_depends_on_use_local_secrets(monkeypatch):
     secret = Secret(name="test")
     monkeypatch.setattr(prefect.config.server, "use_local_secrets", False)
     with prefect.context(_secrets=dict(test=42)):
-        assert secret.get() is None
+        with pytest.raises(ValueError) as exc:
+            secret.get()
+        assert "Client.login" in str(exc.value)
