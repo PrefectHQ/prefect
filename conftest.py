b9e810517df2dfdeb14a3740bdc0543b49a5c312
Chris White <chris@prefect.io>
2018-12-12 10:34:51 -0800

    Convert formatting flag to skip-formatting

--- conftest.py
+++ conftest.py
@@ -108,7 +108,7 @@ def pytest_addoption(parser):
         help="including this flag will attempt to ONLY run airflow compatibility tests",
     )
     parser.addoption(
-        "--formatting",
+        "--skip-formatting",
         action="store_true",
         dest="formatting",
         help="including this flag will attempt to ONLY run formatting tests",
@@ -121,7 +121,7 @@ def pytest_configure(config):
     )
     config.addinivalue_line(
         "markers",
-        "formatting: mark test to run only when --formatting flag is provided.",
+        "formatting: mark test as formatting to skip when --skip-formatting flag is provided.",
     )
 
 
@@ -140,14 +140,11 @@ def pytest_runtest_setup(item):
 
     formatting_mark = item.get_marker("formatting")
 
-    # if a test IS marked as "formattingflow" and the formattingflow flag IS NOT set, skip it
-    if formatting_mark is not None and item.config.getoption("--formatting") is False:
+    # if a test IS marked as "formatting" and the --skip-formatting flag IS set, skip it
+    if (
+        formatting_mark is not None
+        and item.config.getoption("--skip-formatting") is True
+    ):
         pytest.skip(
-            "formatting tests skipped by default unless --formatting flag provided to pytest."
-        )
-
-    # if a test IS NOT marked as formatting and the formatting flag IS set, skip it
-    elif formatting_mark is None and item.config.getoption("--formatting") is True:
-        pytest.skip(
-            "Non-formatting tests skipped because --formatting flag was provided."
+            "Formatting tests skipped because --skip-formatting flag was provided."
         )
