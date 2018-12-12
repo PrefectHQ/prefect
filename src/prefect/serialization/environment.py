04f092b5943e34feb0d3bb395b51cfd07e806f17
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-12-12 16:53:52 -0500

    Redo environments as composable, serializable objects

--- src/prefect/serialization/environment.py
+++ src/prefect/serialization/environment.py
@@ -1,9 +1,11 @@
 from typing import Any
 import marshmallow
 import prefect
-from marshmallow import fields
+from marshmallow import fields, post_load
+from prefect.utilities.collections import DotDict
 from prefect.utilities.serialization import (
     Bytes,
+    JSONCompatible,
     VersionedSchema,
     version,
     to_qualified_name,
@@ -17,6 +19,7 @@ class LocalEnvironmentSchema(VersionedSchema):
         object_class = prefect.environments.LocalEnvironment
 
     encryption_key = Bytes(allow_none=True)
+    serialized_flow = Bytes(allow_none=True)
 
 
 @version("0.3.3")
@@ -25,9 +28,10 @@ class ContainerEnvironmentSchema(VersionedSchema):
         object_class = prefect.environments.ContainerEnvironment
 
     base_image = fields.String(required=True)
-    registry_url = fields.String(allow_none=True)
+    registry_url = fields.String(required=True)
+    image_name = fields.String(allow_none=True)
+    image_tag = fields.String(allow_none=True)
     python_dependencies = fields.List(fields.String(), allow_none=True)
-    secrets = fields.List(fields.String(), allow_none=True)
 
 
 class EnvironmentSchema(OneOfSchema):
--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -1,12 +1,41 @@
 import os
 import pytest
 import subprocess
-
+import tempfile
 from click.testing import CliRunner
 
 import prefect
+import prefect.cli
 
 
 def test_cli_is_installed():
     output = subprocess.check_output(["prefect"])
     assert b"The Prefect CLI" in output
+
+
+def test_make_user_config():
+    with tempfile.TemporaryDirectory() as td:
+        path = os.path.join(td, "test-config.toml")
+        with prefect.utilities.tests.set_temporary_config("user_config_path", path):
+            CliRunner().invoke(prefect.cli.make_user_config)
+        with open(path, "r") as f:
+            assert f.read().startswith("# This is a user configuration file")
+
+
+def error_flow():
+    @prefect.task
+    def zero_error():
+        1 / 0
+
+    with prefect.Flow("error flow") as flow:
+        zero_error()
+
+    return flow
+
+
+def test_run_cli():
+    flow = error_flow()
+    with tempfile.NamedTemporaryFile() as tmp:
+        flow.to_environment_file(tmp.name)
+        result = CliRunner().invoke(prefect.cli.run, tmp.name)
+    assert 'Failed("Some reference tasks failed.")' in result.output
