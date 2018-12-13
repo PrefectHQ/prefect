b0f235e54cb17eacb4536e3e6e9acbd2084b9025
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2018-12-12 20:43:42 -0500

    Promote `set_temporary_config` from utilities.tests -> utilities.configuration

--- src/prefect/utilities/__init__.py
+++ src/prefect/utilities/__init__.py
@@ -2,7 +2,10 @@
 
 import prefect.utilities.logging
 import prefect.utilities.collections
+import prefect.utilities.configuration
 import prefect.utilities.datetimes
 import prefect.utilities.exceptions
+import prefect.utilities.graphql
 import prefect.utilities.notifications
+import prefect.utilities.serialization
 import prefect.utilities.tasks
