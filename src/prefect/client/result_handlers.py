a8205d15b8eb46a676a34a8e5b1ee79ad46f5eee
Chris White <chris@prefect.io>
2018-12-21 17:52:39 -0800

    Add some docs

--- src/prefect/client/result_handlers.py
+++ src/prefect/client/result_handlers.py
@@ -1,3 +1,8 @@
+"""
+Result Handlers provide the hooks that Prefect uses to store task results in production; a `ResultHandler` can be provided to a `Flow` at creation.
+
+Anytime a task needs its output or inputs stored, a result handler is used to determine where this data should be stored (and how it can be retrieved).
+"""
 import base64
 import cloudpickle
 import tempfile
@@ -17,11 +22,24 @@ class ResultHandler:
 
 
 class CloudResultHandler(ResultHandler):
+    """
+    Hook for storing and retrieving task results from Prefect cloud storage; the default result handler for Flows running on Cloud.
+    """
+
     def __init__(self) -> None:
         self.client = Client()
         self.result_handler_service = config.cloud.result_handler
 
     def deserialize(self, uri: str) -> Any:
+        """
+        Deserialize a result from the given URI location.
+
+        Args:
+            - uri (str): the path to the location of a serialized result
+
+        Returns:
+            - the deserialized result from the provided URI
+        """
         res = self.client.get("/", server=self.result_handler_service, **{"uri": uri})
 
         try:
@@ -32,6 +50,15 @@ class CloudResultHandler(ResultHandler):
         return return_val
 
     def serialize(self, result: Any) -> str:
+        """
+        Serialize the provided result to Prefect Cloud.
+
+        Args:
+            - result (Any): the result to serialize and store
+
+        Returns:
+            - str: the URI path to the serialized result in Cloud storage
+        """
         binary_data = base64.b64encode(cloudpickle.dumps(result)).decode()
         res = self.client.post(
             "/", server=self.result_handler_service, **{"result": binary_data}
