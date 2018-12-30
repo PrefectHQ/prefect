86e66b269e59e0b5391364bae77d1a0edf126f9b
Chris White <chris@prefect.io>
2018-12-29 21:07:00 -0800

    Fix containerenvironment COPY files

--- tests/test_environments.py
+++ tests/test_environments.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import os
+import shutil
 import tempfile
 
 import pytest
@@ -104,11 +105,54 @@ class TestContainerEnvironment:
 
             with tempfile.TemporaryDirectory(prefix="prefect-tests") as tmp:
                 container.create_dockerfile(Flow(), directory=tmp)
+
+                ## ensure create_dockerfile copied the files over
+                assert os.path.exists(os.path.join(tmp, base1))
+                assert os.path.exists(os.path.join(tmp, base2))
+
                 with open(os.path.join(tmp, "Dockerfile"), "r") as f:
                     dockerfile = f.read()
 
         assert "COPY {} /root/dockerconfig".format(base1) in dockerfile
         assert "COPY {} ./.secret_file".format(base2) in dockerfile
+
+    def test_create_dockerfile_with_copy_files_doesnt_raise_if_file_exists_and_is_same(
+        self
+    ):
+        with tempfile.NamedTemporaryFile() as t1, tempfile.NamedTemporaryFile() as t2:
+            container = ContainerEnvironment(
+                base_image="python:3.6",
+                registry_url="",
+                files={t1.name: "/root/dockerconfig", t2.name: "./.secret_file"},
+            )
+
+            base1, base2 = os.path.basename(t1.name), os.path.basename(t2.name)
+
+            with tempfile.TemporaryDirectory(prefix="prefect-tests") as tmp:
+                shutil.copy(t1.name, os.path.join(tmp, base1))
+                container.create_dockerfile(Flow(), directory=tmp)
+
+    def test_create_dockerfile_with_copy_files_raises_if_file_exists_and_different(
+        self
+    ):
+        with tempfile.NamedTemporaryFile() as t1, tempfile.NamedTemporaryFile() as t2:
+            container = ContainerEnvironment(
+                base_image="python:3.6",
+                registry_url="",
+                files={t1.name: "/root/dockerconfig", t2.name: "./.secret_file"},
+            )
+
+            base1, base2 = os.path.basename(t1.name), os.path.basename(t2.name)
+
+            with tempfile.TemporaryDirectory(prefix="prefect-tests") as tmp:
+                new_file = os.path.join(tmp, base1)
+                shutil.copy(t1.name, new_file)
+                with open(new_file, "w+") as f:
+                    f.write("a few lines\n")
+                with pytest.raises(ValueError) as exc:
+                    container.create_dockerfile(Flow(), directory=tmp)
+
+        assert "already exists" in str(exc.value)
 
     def test_init_with_copy_files_raises_informative_error_if_not_absolute(self):
         with pytest.raises(ValueError) as exc:
