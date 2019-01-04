8ecfa9064b804b1858a9eeb6c3a463aad0349eca
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-01-04 10:41:35 -0500

    Rename formatting test file to remove dunder

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -9,8 +9,9 @@ pytestmark = pytest.mark.formatting
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="Black requires Python 3.6+")
 def test_black_formatting():
-    # make sure we're in the right place
-    assert __file__.endswith("/tests/test__formatting.py")
+    # make sure we know what working directory we're in
+    assert __file__.endswith("/tests/test_formatting.py")
+
     prefect_dir = os.path.dirname(os.path.dirname(__file__))
     result = subprocess.call(shlex.split("black --check {}".format(prefect_dir)))
     assert result == 0, "Prefect repo did not pass Black formatting!"
@@ -18,8 +19,9 @@ def test_black_formatting():
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="mypy requires Python 3.6+")
 def test_mypy():
-    # make sure we're in the right place
-    assert __file__.endswith("/tests/test__formatting.py")
+    # make sure we know what working directory we're in
+    assert __file__.endswith("/tests/test_formatting.py")
+
     prefect_dir = os.path.dirname(os.path.dirname(__file__))
     prefect_src_dir = os.path.join(prefect_dir, "src/")
     config_file = os.path.join(prefect_dir, "setup.cfg")
