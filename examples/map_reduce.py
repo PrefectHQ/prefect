08d6f7d84514ec53ef6d302d7d83c67cfa8ae8f9
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-01-04 10:34:32 -0500

    Create map_reduce.py

---
+++ examples/map_reduce.py
@@ -0,0 +1,36 @@
+from prefect import Flow, task
+
+
+# ------------------------------------
+# define some tasks
+
+@task
+def numbers_task():
+    return [1, 2, 3]
+
+
+@task
+def map_task(x):
+    return x + 1
+
+
+@task
+def reduce_task(x):
+    return sum(x)
+
+
+# ------------------------------------
+# build a flow
+
+with Flow("Map / Reduce 🤓") as flow:
+    numbers = numbers_task()
+    first_map = map_task.map(numbers)
+    second_map = map_task.map(first_map)
+    reduction = reduce_task(second_map)
+
+
+# ------------------------------------
+# run the flow
+
+state = flow.run(return_tasks=flow.tasks)
+assert state.result[reduction].result == 12
