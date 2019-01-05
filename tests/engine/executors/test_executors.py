eb59de2006de6dad6d1f8c33c064a4de437d45c4
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-01-05 14:23:16 -0500

    Remove submit_with_context()

--- tests/engine/executors/test_executors.py
+++ tests/engine/executors/test_executors.py
@@ -26,15 +26,6 @@ class TestBaseExecutor:
         with pytest.raises(NotImplementedError):
             Executor().map(lambda: 1, upstream_states={})
 
-    def test_submit_with_context_requires_context_kwarg(self):
-        with pytest.raises(TypeError) as exc:
-            Executor().submit_with_context(lambda: 1)
-        assert "missing 1 required keyword-only argument: 'context'" in str(exc.value)
-
-    def test_submit_with_context_raises_notimplemented(self):
-        with pytest.raises(NotImplementedError):
-            Executor().submit_with_context(lambda: 1, context=None)
-
     def test_wait_raises_notimplemented(self):
         with pytest.raises(NotImplementedError):
             Executor().wait([1])
@@ -74,25 +65,6 @@ class TestSyncExecutor:
         assert SynchronousExecutor().submit(lambda x: x, 1).compute() == 1
         assert SynchronousExecutor().submit(lambda x: x, x=1).compute() == 1
         assert SynchronousExecutor().submit(lambda: prefect).compute() is prefect
-
-    def test_submit_with_context(self):
-        context_fn = lambda: prefect.context.get("abc")
-        context = dict(abc="abc")
-
-        assert SynchronousExecutor().submit(context_fn).compute() is None
-        with prefect.context(context):
-            assert SynchronousExecutor().submit(context_fn).compute() is None
-        assert (
-            SynchronousExecutor()
-            .submit_with_context(context_fn, context=context)
-            .compute()
-            == "abc"
-        )
-
-    def test_submit_with_context_requires_context_kwarg(self):
-        with pytest.raises(TypeError) as exc:
-            SynchronousExecutor().submit_with_context(lambda: 1)
-        assert "missing 1 required keyword-only argument: 'context'" in str(exc.value)
 
     def test_wait(self):
         e = SynchronousExecutor()
@@ -148,20 +120,6 @@ class TestLocalExecutor:
         assert LocalExecutor().submit(lambda x: x, 1) == 1
         assert LocalExecutor().submit(lambda x: x, x=1) == 1
         assert LocalExecutor().submit(lambda: prefect) is prefect
-
-    def test_submit_with_context(self):
-        context_fn = lambda: prefect.context.get("abc")
-        context = dict(abc="abc")
-
-        assert LocalExecutor().submit(context_fn) is None
-        with prefect.context(context):
-            assert LocalExecutor().submit(context_fn) == "abc"
-        assert LocalExecutor().submit_with_context(context_fn, context=context) == "abc"
-
-    def test_submit_with_context_requires_context_kwarg(self):
-        with pytest.raises(TypeError) as exc:
-            LocalExecutor().submit_with_context(lambda: 1)
-        assert "missing 1 required keyword-only argument: 'context'" in str(exc.value)
 
     def test_wait(self):
         """LocalExecutor's wait() method just returns its input"""
@@ -275,28 +233,6 @@ class TestDaskExecutor:
         assert "y" in computed
         assert computed["x"] == 3
         assert computed["y"] == 4
-
-    @pytest.mark.parametrize("executor", ["mproc", "mthread"], indirect=True)
-    def test_submit_with_context(self, executor):
-        context_fn = lambda: prefect.context.get("abc")
-        context = dict(abc="abc")
-
-        with executor.start():
-            assert executor.wait(executor.submit(context_fn)) is None
-            with prefect.context(context):
-                assert (
-                    executor.wait(executor.submit(context_fn)) is None
-                )  # not inherited to subprocesses / threads
-            fut = executor.submit_with_context(context_fn, context=context)
-            context.clear()
-            assert executor.wait(fut) == "abc"
-
-    @pytest.mark.parametrize("executor", ["mproc", "mthread"], indirect=True)
-    def test_submit_with_context_requires_context_kwarg(self, executor):
-        with pytest.raises(TypeError) as exc:
-            with executor.start():
-                executor.submit_with_context(lambda: 1)
-        assert "missing 1 required keyword-only argument: 'context'" in str(exc.value)
 
     @pytest.mark.parametrize("executor", ["mproc", "mthread"], indirect=True)
     def test_runs_in_parallel(self, executor):
