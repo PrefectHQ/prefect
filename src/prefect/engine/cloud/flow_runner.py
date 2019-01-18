3373ab77fdd3f1a1eb072c5b037392aa4104ae6b
Chris White <chris@prefect.io>
2019-01-18 11:44:05 -0800

    Add two logs to help us determine when state setting fails

--- src/prefect/engine/cloud/flow_runner.py
+++ src/prefect/engine/cloud/flow_runner.py
@@ -99,6 +99,9 @@ class CloudFlowRunner(FlowRunner):
                 result_handler=self.result_handler,
             )
         except Exception as exc:
+            self.logger.debug(
+                "Failed to set flow state with error: {}".format(repr(exc))
+            )
             raise ENDRUN(state=new_state)
 
         prefect.context.update(flow_run_version=version + 1)  # type: ignore
