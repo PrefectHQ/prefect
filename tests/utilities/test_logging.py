badd487932d6494f901d5b5e196c54822c4d5dc1
Chris White <chris@prefect.io>
2019-01-22 22:56:26 -0800

    Go back to simpler times and simpler testing

--- tests/utilities/test_logging.py
+++ tests/utilities/test_logging.py
@@ -1,5 +1,4 @@
 import logging
-from multiprocessing.pool import ThreadPool
 from unittest.mock import MagicMock
 
 from prefect import utilities
@@ -21,18 +20,13 @@ def test_root_logger_level_responds_to_config():
 
 
 def test_remote_handler_is_configured_for_cloud(monkeypatch):
-    starter = MagicMock()
-    listener = MagicMock(return_value=starter)
-    monkeypatch.setattr("prefect.utilities.logging.QueueListener", listener)
     try:
         with utilities.configuration.set_temporary_config(
             {"logging.log_to_cloud": True, "cloud.log": "http://foo.bar:1800/log"}
         ):
             logger = utilities.logging.configure_logging()
-            assert listener.called
-            remote_handler = listener.call_args[0][1]
-            assert remote_handler.logger_server == "http://foo.bar:1800/log"
-            assert starter.start.called
+            assert hasattr(logger.handlers[-1], "client")
+            assert logger.handlers[-1].logger_server == "http://foo.bar:1800/log"
     finally:
         # reset root_logger
         logger = utilities.logging.get_logger()
@@ -51,29 +45,3 @@ def test_get_logger_with_name_returns_child_logger():
 
     assert prefect_logger is child_logger
     assert prefect_logger is logging.getLogger("prefect").getChild("test")
-
-
-def test_loggers_can_be_pickled_and_still_ship_to_cloud(monkeypatch):
-    client = MagicMock()
-    monkeypatch.setattr("prefect.client.Client", MagicMock(return_value=client))
-
-    def can_i_speak(logger):
-        logger.critical("this is important")
-        was_called = logger.handlers[-1].client.post.called
-        return was_called
-
-    try:
-        with utilities.configuration.set_temporary_config(
-            {"logging.log_to_cloud": True, "cloud.log": "http://foo.bar:1800/log"}
-        ):
-            logger = utilities.logging.configure_logging()
-            pool = ThreadPool(processes=1)
-            result = pool.apply_async(can_i_speak, args=(logger,))
-            value = result.get()
-            assert value is True
-    finally:
-        # reset root_logger
-        logger = utilities.logging.get_logger()
-        for h in logger.handlers:
-            logger.removeHandler(h)
-        utilities.logging.configure_logging()
