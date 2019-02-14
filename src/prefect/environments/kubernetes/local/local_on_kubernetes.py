c3f1e3be34f80c5f7d44ae1e9be1f682516a85e2
Josh Meek <josh@prefect.io>
2019-02-14 11:57:55 -0500

    LocalOnKubernetes into work

--- src/prefect/environments/kubernetes/local/local_on_kubernetes.py
+++ src/prefect/environments/kubernetes/local/local_on_kubernetes.py
@@ -1,5 +1,19 @@
+import prefect
 from prefect.environments import Environment
 
 class LocalOnKubernetes(Environment):
     """"""
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def build(self, flow: "prefect.Flow") -> "Environment":
+        pass
+
+    def execute(self) -> None:
+        pass
+
+    def run(self) -> None:
+        pass
+
+    def setup(self) -> None:
+        pass
