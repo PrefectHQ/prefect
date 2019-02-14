919260d16bb03b41bbd5c8ce7e3d9a7beced2a1e
Josh Meek <josh@prefect.io>
2019-02-14 11:57:55 -0500

    LocalOnKubernetes into work

--- src/prefect/environments/environments.py
+++ src/prefect/environments/environments.py
@@ -1,10 +1,22 @@
+import json
+
+import prefect
 
 class Environment:
     """"""
     def __init__(self) -> None:
         pass
 
-    def build(self) -> "Environment":
+    def build(self, flow: "prefect.Flow") -> "Environment":
+        """
+        Builds the environment for a specific flow. A new environment is returned.
+
+        Args:
+            - flow (prefect.Flow): the Flow for which the environment will be built
+
+        Returns:
+            - Environment: a new environment that can run the provided Flow.
+        """
         raise NotImplementedError()
 
     def execute(self) -> None:
@@ -15,3 +27,23 @@ class Environment:
 
     def setup(self) -> None:
         raise NotImplementedError()
+
+    def serialize(self) -> dict:
+        """
+        Returns a serialized version of the Environment
+
+        Returns:
+            - dict: the serialized Environment
+        """
+        schema = prefect.serialization.environment.EnvironmentSchema()
+        return schema.dump(self)
+
+    def to_file(self, path: str) -> None:
+        """
+        Serialize the environment to a file.
+
+        Args:
+            - path (str): the file path to which the environment will be written
+        """
+        with open(path, "w") as f:
+            json.dump(self.serialize(), f)
