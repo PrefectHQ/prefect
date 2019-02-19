7eee6c9d2febb92564bbe323b3be689249867ebd
Josh Meek <josh@prefect.io>
2019-02-19 10:39:12 -0500

    Working LocalOnKubuernetes execute functionality

--- src/prefect/environments/kubernetes/local/local_on_kubernetes.py
+++ src/prefect/environments/kubernetes/local/local_on_kubernetes.py
@@ -1,16 +1,12 @@
-import prefect
-from prefect.environments import DockerEnvironment
-import filecmp
-import json
-import logging
-import os
-import shutil
-import tempfile
-import textwrap
-from typing import Iterable
+from os import path
 import uuid
 
 import docker
+from kubernetes import client, config
+import yaml
+
+import prefect
+from prefect.environments import DockerEnvironment
 
 
 class LocalOnKubernetesEnvironment(DockerEnvironment):
@@ -36,14 +32,58 @@ class LocalOnKubernetesEnvironment(DockerEnvironment):
             files=files,
         )
 
+        config.load_kube_config()  # OUT OF CLUSTER
+        # config.load_incluster_config() # IN CLUSTER
+
+        self.uuid_label = str(uuid.uuid4())
+
+    def _populate_yaml(self, yaml_obj: dict) -> dict:
+        # set identifier labels
+        yaml_obj["metadata"]["name"] = "prefect-local-job-{}".format(self.uuid_label)
+        yaml_obj["metadata"]["labels"]["identifier"] = self.uuid_label
+        yaml_obj["spec"]["template"]["metadata"]["labels"][
+            "identifier"
+        ] = self.uuid_label
+
+        # set environment variables
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][0][
+            "value"
+        ] = prefect.config.cloud.graphql
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][1][
+            "value"
+        ] = prefect.config.cloud.log
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][2][
+            "value"
+        ] = prefect.config.cloud.result_handler
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][3][
+            "value"
+        ] = prefect.config.cloud.auth_token
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][4][
+            "value"
+        ] = prefect.context.get("flow_run_id", "")
+
+        # set image
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["image"] = "{}:{}".format(
+            path.join(self.registry_url, self.image_name), self.image_tag
+        )
+
+        return yaml_obj
+
     def execute(self) -> None:
-        print("execute")
+        batch_client = client.BatchV1Api()
+
+        with open(path.join(path.dirname(__file__), "job.yaml")) as job_file:
+            job = yaml.safe_load(job_file)
+            job = self._populate_yaml(yaml_obj=job)
+
+            # Create Job
+            batch_client.create_namespaced_job(namespace="default", body=job)
 
     def run(self) -> None:
         pass
 
     def setup(self) -> None:
-        print("setup")
+        pass
 
     def build(
         self, flow: "prefect.Flow", push: bool = True
