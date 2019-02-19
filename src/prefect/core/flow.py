23a7fc32e62a700d3a4d0f8e1f6fd02a9fb66bd6
Josh Meek <josh@prefect.io>
2019-02-19 13:36:14 -0500

    Import issue w/ Environment

--- src/prefect/core/flow.py
+++ src/prefect/core/flow.py
@@ -32,7 +32,7 @@ from prefect.engine.result import NoResult
 from prefect.engine.result_handlers import ResultHandler
 from prefect.core.edge import Edge
 from prefect.core.task import Parameter, Task
-from prefect.environments import Environment
+from prefect.environments.environment import Environment
 from prefect.utilities import logging
 from prefect.utilities.notifications import callback_factory
 from prefect.utilities.serialization import to_qualified_name
