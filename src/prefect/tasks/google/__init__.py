7002d3fd3be43d7d2f13fdd30745d27816af385f
Chris White <chris@prefect.io>
2019-02-20 15:09:44 -0800

    Update google docstring

--- src/prefect/tasks/google/__init__.py
+++ src/prefect/tasks/google/__init__.py
@@ -1,6 +1,11 @@
 """
-Require a service account.  Recommend a defaultproject.
-Save blob in secret.  Default name.
+Tasks which interface with various components of Google Cloud Storage.
+
+Note that these tasks allow for a wide range of custom usage patterns, such as:
+
+- Initialize a task with all settings for one time use
+- Initialize a "template" task with default settings and override as needed
+- Create a custom Task which inherits from a Prefect Task and utilizes the Prefect boilerplate
 """
 import prefect.tasks.google.storage
 
