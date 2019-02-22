e903b10d80b3ff7d2735432ec5deb857144cf2c4
Josh Meek <josh@prefect.io>
2019-02-22 10:06:22 -0500

    Remove LocalOnKubernetesEnvironment

--- src/prefect/environments/kubernetes/__init__.py
+++ src/prefect/environments/kubernetes/__init__.py
@@ -1,8 +1,5 @@
 # Licensed under LICENSE.md; also available at https://www.prefect.io/licenses/alpha-eula
 
-from prefect.environments.kubernetes.local.local_on_kubernetes import (
-    LocalOnKubernetesEnvironment,
-)
 from prefect.environments.kubernetes.docker.docker_on_kubernetes import (
     DockerOnKubernetesEnvironment,
 )
--- src/prefect/environments/kubernetes/local/__init__.py
+++
@@ -1,1 +0,0 @@
-# Licensed under LICENSE.md; also available at https://www.prefect.io/licenses/alpha-eula
