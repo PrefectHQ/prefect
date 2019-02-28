8729c0c8ea3f805f935801a99782617ca9edd8f2
Chris White <chris@prefect.io>
2019-02-28 15:26:05 -0500

    Add a couple of tests for s3

--- tests/tasks/aws/test_s3.py
+++ tests/tasks/aws/test_s3.py
@@ -1,6 +1,9 @@
 import pytest
+from unittest.mock import MagicMock
 
+import prefect
 from prefect.tasks.aws import S3DownloadTask, S3UploadTask
+from prefect.utilities.configuration import set_temporary_config
 
 
 class TestS3DownloadTask:
@@ -19,6 +22,21 @@ class TestS3DownloadTask:
             task.run(key="")
         assert "bucket" in str(exc.value)
 
+    def test_creds_are_pulled_from_secret(self, monkeypatch):
+        task = S3DownloadTask(bucket="bob")
+        client = MagicMock()
+        boto3 = MagicMock(client=client)
+        monkeypatch.setattr("prefect.tasks.aws.s3.boto3", boto3)
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            with prefect.context(
+                secrets=dict(
+                    AWS_CREDENTIALS='{"ACCESS_KEY": "42", "SECRET_ACCESS_KEY": "99"}'
+                )
+            ):
+                task.run(key="")
+        kwargs = client.call_args[1]
+        assert kwargs == {"aws_access_key_id": "42", "aws_secret_access_key": "99"}
+
 
 class TestS3UploadTask:
     def test_initialization(self):
@@ -35,3 +53,18 @@ class TestS3UploadTask:
         with pytest.raises(ValueError) as exc:
             task.run(data="")
         assert "bucket" in str(exc.value)
+
+    def test_creds_are_pulled_from_secret(self, monkeypatch):
+        task = S3UploadTask(bucket="bob")
+        client = MagicMock()
+        boto3 = MagicMock(client=client)
+        monkeypatch.setattr("prefect.tasks.aws.s3.boto3", boto3)
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            with prefect.context(
+                secrets=dict(
+                    AWS_CREDENTIALS='{"ACCESS_KEY": "42", "SECRET_ACCESS_KEY": "99"}'
+                )
+            ):
+                task.run(data="")
+        kwargs = client.call_args[1]
+        assert kwargs == {"aws_access_key_id": "42", "aws_secret_access_key": "99"}
