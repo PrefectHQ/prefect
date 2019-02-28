adb07204e74d93bba0c678acda51c0013a1d11df
Josh Meek <josh@prefect.io>
2019-02-28 13:35:06 -0500

    Ironed out `execute`

--- src/prefect/environments/kubernetes/dask/dask_on_kubernetes.py
+++ src/prefect/environments/kubernetes/dask/dask_on_kubernetes.py
@@ -37,6 +37,7 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
         )
 
         self.identifier_label = str(uuid.uuid4())
+        self.scheduler_address = None
 
     def _populate_worker_pod_yaml(self, yaml_obj: dict) -> dict:
         """"""
@@ -64,6 +65,48 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
             self.image_tag,
         )
 
+        return yaml_obj
+
+    def _populate_job_yaml(self, yaml_obj: dict) -> dict:
+        """"""
+        # set identifier labels
+        yaml_obj["metadata"]["name"] = "prefect-local-job-{}".format(
+            self.identifier_label
+        )
+        yaml_obj["metadata"]["labels"]["identifier"] = self.identifier_label
+        yaml_obj["spec"]["template"]["metadata"]["labels"][
+            "identifier"
+        ] = self.identifier_label
+
+        # set environment variables
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][0][
+            "value"
+        ] = prefect.config.cloud.graphql
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][1][
+            "value"
+        ] = prefect.config.cloud.log
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][2][
+            "value"
+        ] = prefect.config.cloud.result_handler
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][3][
+            "value"
+        ] = prefect.config.cloud.auth_token
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][4][
+            "value"
+        ] = prefect.context.get("flow_run_id", "")
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][5][
+            "value"
+        ] = self.scheduler_address
+
+        # set image
+        yaml_obj["spec"]["template"]["spec"]["containers"][0]["image"] = "{}:{}".format(
+            path.join(self.registry_url, self.image_name),  # type: ignore
+            self.image_tag,
+        )
+
+        return yaml_obj
+
+
     def execute(self) -> None:
         """
         Create a single Kubernetes job on the default namespace that runs a flow. This also
@@ -78,7 +121,7 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
 
         with open(path.join(path.dirname(__file__), "job.yaml")) as job_file:
             job = yaml.safe_load(job_file)
-            job = self._populate_yaml(yaml_obj=job)
+            job = self._populate_job_yaml(yaml_obj=job)
 
             # Create Job
             batch_client.create_namespaced_job(namespace="default", body=job)
@@ -91,8 +134,10 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
             worker_pod = yaml.safe_load(pod_file)
             worker_pod = self._populate_worker_pod_yaml(yaml_obj=worker_pod)
 
-        cluster = KubeCluster.from_dict(worker_pod)
-        cluster.scale_up(1)
+            cluster = KubeCluster.from_dict(worker_pod)
+            cluster.scale_up(1)
+
+            self.scheduler_address = cluster.scheduler_address
 
         # Spin everything up, make sure the scheduler and worker start, block inside execute
 
