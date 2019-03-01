39d997539ceb43e45fc8f3d86b3c6c1f1f1a9926
Josh Meek <josh@prefect.io>
2019-03-01 13:27:12 -0500

    Switch to different execution model

--- src/prefect/environments/kubernetes/dask/dask_on_kubernetes.py
+++ src/prefect/environments/kubernetes/dask/dask_on_kubernetes.py
@@ -45,10 +45,6 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
         # set identifier labels
         yaml_obj["metadata"]["labels"]["identifier"] = self.identifier_label
 
-        # set environment variables
-        yaml_obj["spec"]["containers"][0]["args"][1] = "tcp://{}:8786".format(
-            self.scheduler_address
-        )
         yaml_obj["spec"]["containers"][0]["env"][0][
             "value"
         ] = prefect.config.cloud.graphql
@@ -64,9 +60,8 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
         )
 
         # set image
-        yaml_obj["spec"]["containers"][0]["image"] = "{}:{}".format(
-            path.join(self.registry_url, self.image_name),  # type: ignore
-            self.image_tag,
+        yaml_obj["spec"]["containers"][0]["image"] = prefect.context.get(
+            "image", "daskdev/dask:latest"
         )
 
         return yaml_obj
@@ -113,7 +108,13 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
         ] = prefect.context.get("flow_run_id", "")
         yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][5][
             "value"
-        ] = "{}:8786".format(self.scheduler_address)
+        ] = "{}:{}".format(
+            path.join(self.registry_url, self.image_name),  # type: ignore
+            self.image_tag,
+        )
+        # yaml_obj["spec"]["template"]["spec"]["containers"][0]["env"][5][
+        #     "value"
+        # ] = "{}:8786".format(self.scheduler_address)
 
         # set image
         yaml_obj["spec"]["template"]["spec"]["containers"][0]["image"] = "{}:{}".format(
@@ -139,6 +140,14 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
                 print("Dask job is", job.status)
                 return
 
+    def create_dask_cluster(self) -> None:
+        with open(path.join(path.dirname(__file__), "worker_pod.yaml")) as pod_file:
+            worker_pod = yaml.safe_load(pod_file)
+            worker_pod = self._populate_worker_pod_yaml(yaml_obj=worker_pod)
+
+            cluster = KubeCluster.from_dict(worker_pod)
+            cluster.scale_up(1)
+
     def execute(self) -> None:
         """
         Create a single Kubernetes job on the default namespace that runs a flow. This also
@@ -149,16 +158,16 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
         except config.config_exception.ConfigException:
             raise EnvironmentError("Environment not currently inside a cluster")
 
-        job_identifier = prefect.context.get("job_identifier", "")
-        service_name = "prefect-job-{}".format(job_identifier)
+        # job_identifier = prefect.context.get("job_identifier", "")
+        # service_name = "prefect-job-{}".format(job_identifier)
 
-        core_client = client.CoreV1Api()
-        service = core_client.read_namespaced_service(
-            namespace="default", name=service_name
-        )
+        # core_client = client.CoreV1Api()
+        # service = core_client.read_namespaced_service(
+        #     namespace="default", name=service_name
+        # )
 
-        self.scheduler_address = service.spec.cluster_ip
-        print("Scheduler address {}".format(self.scheduler_address))
+        # self.scheduler_address = service.spec.cluster_ip
+        # print("Scheduler address {}".format(self.scheduler_address))
 
         batch_client = client.BatchV1Api()
 
@@ -166,53 +175,52 @@ class DaskOnKubernetesEnvironment(DockerEnvironment):
             job = yaml.safe_load(job_file)
             job = self._populate_job_yaml(yaml_obj=job)
 
-            print(job)
-
             # Create Job
             batch_client.create_namespaced_job(namespace="default", body=job)
 
-        self._wait_for_dask_job()
+        # self._wait_for_dask_job()
 
     def setup(self) -> None:
         """"""
-        try:
-            config.load_incluster_config()
-        except config.config_exception.ConfigException:
-            raise EnvironmentError("Environment not currently inside a cluster")
+        pass
+        # try:
+        #     config.load_incluster_config()
+        # except config.config_exception.ConfigException:
+        #     raise EnvironmentError("Environment not currently inside a cluster")
 
-        # Make service
-        with open(
-            path.join(path.dirname(__file__), "scheduler_service.yaml")
-        ) as svc_file:
+        # # Make service
+        # with open(
+        #     path.join(path.dirname(__file__), "scheduler_service.yaml")
+        # ) as svc_file:
 
-            core_client = client.CoreV1Api()
+        #     core_client = client.CoreV1Api()
 
-            # Populate
-            scheduler_service = yaml.safe_load(svc_file)
-            scheduler_service = self._populate_scheduler_service_yaml(
-                yaml_obj=scheduler_service
-            )
+        #     # Populate
+        #     scheduler_service = yaml.safe_load(svc_file)
+        #     scheduler_service = self._populate_scheduler_service_yaml(
+        #         yaml_obj=scheduler_service
+        #     )
 
-            print(scheduler_service)
+        #     print(scheduler_service)
 
-            # Create
-            core_client.create_namespaced_service(
-                namespace="default", body=scheduler_service
-            )
+        #     # Create
+        #     core_client.create_namespaced_service(
+        #         namespace="default", body=scheduler_service
+        #     )
 
-            time.sleep(10)
+        #     time.sleep(10)
 
-            job_identifier = prefect.context.get("job_identifier", "")
-            service_name = "prefect-job-{}".format(job_identifier)
+        #     job_identifier = prefect.context.get("job_identifier", "")
+        #     service_name = "prefect-job-{}".format(job_identifier)
 
-            core_client = client.CoreV1Api()
-            service = core_client.read_namespaced_service(
-                namespace="default", name=service_name
-            )
+        #     core_client = client.CoreV1Api()
+        #     service = core_client.read_namespaced_service(
+        #         namespace="default", name=service_name
+        #     )
 
-            self.scheduler_address = service.spec.cluster_ip
+        #     self.scheduler_address = service.spec.cluster_ip
 
-            print("Address: {}".format(self.scheduler_address))
+        #     print("Address: {}".format(self.scheduler_address))
 
         # Make workers
         with open(path.join(path.dirname(__file__), "worker_pod.yaml")) as pod_file:
