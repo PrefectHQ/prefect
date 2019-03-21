0a42b8da2010c66c3c1fd44d48a5add4e7ce356a
Josh Meek <josh@prefect.io>
2019-03-21 10:22:50 -0400

    CreateNamespacedJob documentation

--- src/prefect/tasks/kubernetes/__init__.py
+++ src/prefect/tasks/kubernetes/__init__.py
@@ -1,4 +1,10 @@
 from prefect.tasks.kubernetes.deployment import CreateNamespacedDeploymentTask
-from prefect.tasks.kubernetes.job import CreateNamespacedJob
+from prefect.tasks.kubernetes.job import (
+    CreateNamespacedJob,
+    DeleteNamespacedJob,
+    ListNamespacedJob,
+    PatchNamespacedJob,
+    ReplaceNamespacedJob,
+)
 from prefect.tasks.kubernetes.pod import CreateNamespacedPodTask
 from prefect.tasks.kubernetes.service import CreateNamespacedServiceTask
