0b763437e280a90191b67e086b45dfa126a79413
Josh Meek <josh@prefect.io>
2019-03-20 22:08:25 -0400

    Working CreateNamespacedJobTask

--- src/prefect/tasks/kubernetes/job.py
+++ src/prefect/tasks/kubernetes/job.py
@@ -1,5 +1,56 @@
+from kubernetes import client, config
 
 from prefect import Task
+from prefect.client import Secret
+from prefect.utilities.tasks import defaults_from_attrs
+
 
 class CreateNamespacedJobTask(Task):
-    pass
+    def __init__(
+        self,
+        body: dict = None,
+        namespace: str = "default",
+        kube_kwargs: dict = None,
+        kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
+        **kwargs
+    ):
+        self.body = body or {}
+        self.namespace = namespace
+        self.kube_kwargs = kube_kwargs or {}
+        self.kubernetes_api_key_secret = kubernetes_api_key_secret
+
+        super().__init__(**kwargs)
+
+    @defaults_from_attrs(
+        "body", "namespace", "kube_kwargs", "kubernetes_api_key_secret"
+    )
+    def run(
+        self,
+        body: dict = None,
+        namespace: str = "default",
+        kube_kwargs: dict = None,
+        kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
+    ):
+
+        if not body:
+            raise ValueError("A dictionary representing a V1Job must be provided.")
+
+        kubernetes_api_key = Secret(kubernetes_api_key_secret).get()
+
+        if kubernetes_api_key:
+            configuration = client.Configuration()
+            configuration.api_key["authorization"] = kubernetes_api_key
+            self.client = client.BatchV1Api(kubernetes.client.ApiClient(configuration))
+        else:
+            try:
+                config.load_incluster_config()
+            except config.config_exception.ConfigException:
+                config.load_kube_config()
+
+            self.client = client.BatchV1Api()
+
+        self.body.update(body)
+
+        return self.client.create_namespaced_job(
+            namespace=namespace, body=self.body, **kube_kwargs
+        )
