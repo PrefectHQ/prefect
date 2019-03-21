8fe0e0828bdb868d8ca4b8e2e3a5e3e3ba27c397
Josh Meek <josh@prefect.io>
2019-03-20 22:14:03 -0400

    Remove Task on naming convention

--- src/prefect/tasks/kubernetes/__init__.py
+++ src/prefect/tasks/kubernetes/__init__.py
@@ -1,4 +1,4 @@
 from prefect.tasks.kubernetes.deployment import CreateNamespacedDeploymentTask
-from prefect.tasks.kubernetes.job import CreateNamespacedJobTask
+from prefect.tasks.kubernetes.job import CreateNamespacedJob
 from prefect.tasks.kubernetes.pod import CreateNamespacedPodTask
 from prefect.tasks.kubernetes.service import CreateNamespacedServiceTask
