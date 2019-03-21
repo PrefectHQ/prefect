1e0bd778ad993b39f78ab15d8d4576c59fead342
Chris White <chris@prefect.io>
2019-03-21 10:41:24 -0700

    Remove ignored coverage entries and update EmailTask

--- src/prefect/tasks/notifications/email_task.py
+++ src/prefect/tasks/notifications/email_task.py
@@ -1,38 +1,68 @@
 import smtplib
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from typing import Any
 
-import prefect
+from prefect import Task
+from prefect.client import Secret
+from prefect.utilities.tasks import defaults_from_attrs
 
-EMAIL_TEMPLATE = """
-    From: {email_from}
-    To: {email_to}
-    Subject: {subject}
 
-    {msg}
+class EmailTask(Task):
+    """
+    Task for sending email from an authenticated Gmail address.  For this task to function properly,
+    you must have the `"EMAIL_USERNAME"` and `"EMAIL_PASSWORD"` Prefect Secrets set.  It is recommended
+    you use a [Google App Password](https://support.google.com/accounts/answer/185833) for this purpose.
+
+    Args:
+        - subject (str, optional): the subject of the email; can also be provided at runtime
+        - msg (str, optional): the contents of the email; can also be provided at runtime
+        - email_to (str, optional): the destination email address to send the message to; can also
+            be provided at runtime
+        - **kwargs (Any, optional): additional keyword arguments to pass to the base Task initialization
     """
 
-
-class EmailTask(prefect.Task):
-    """
-    This task sends an email.
-    """
-
-    def __init__(self, username: str = None, password: str = None, **kwargs: Any):
-
-        self.username = username
-        self.password = password
-
+    def __init__(
+        self, subject: str = None, msg: str = None, email_to: str = None, **kwargs
+    ):
+        self.subject = subject
+        self.msg = msg
+        self.email_to = email_to
         super().__init__(**kwargs)
 
-    def run(  # type: ignore
-        self, subject=None, msg=None, email_from=None, email_to=None
-    ):
+    @defaults_from_attrs("subject", "msg", "email_to")
+    def run(self, subject: str = None, msg: str = None, email_to: str = None):
+        """
+        Run method which sends an email.
 
-        message = EMAIL_TEMPLATE.format(
-            email_from=email_from, email_to=email_to, subject=subject, msg=msg
-        )
+        Args:
+            - subject (str, optional): the subject of the email; defaults to the one provided
+                at initialization
+            - msg (str, optional): the contents of the email; defaults to the one provided
+                at initialization
+            - email_to (str, optional): the destination email address to send the message to;
+                defaults to the one provided at initialization
+
+        Returns:
+            - None
+        """
+
+        username = Secret("EMAIL_USERNAME").get()
+        password = Secret("EMAIL_PASSWORD").get()
+
+        contents = MIMEMultipart("alternative")
+        contents.attach(MIMEText(msg, "plain"))
+
+        contents["Subject"] = Header(subject, "UTF-8")
+        contents["From"] = "notifications@prefect.io"
+        contents["To"] = email_to
+
+        message = contents.as_string()
 
         server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-        server.ehlo()
-        server.login(self.username, self.password)
-        server.sendmail(from_addr=email_from, to_addrs=email_to, msg=message)
+        server.login(username, password)
+        try:
+            server.sendmail("notifications@prefect.io", email_to, message)
+        finally:
+            server.quit()
