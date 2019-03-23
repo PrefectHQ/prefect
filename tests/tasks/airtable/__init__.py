4e166359d6a82b5c8b032d08de96aa0ff21435af
Colin Carroll <colcarroll@gmail.com>
2019-03-23 15:40:51 -0400

    Bump to latest version of pytest 4.3
    This updates pytest to the latest version. It also updates the usage of
    `pytest.importorskip`, which previously caused errors when run, but may
    have been masked by skipping.

--- tests/tasks/airtable/__init__.py
+++ tests/tasks/airtable/__init__.py
@@ -1,3 +1,3 @@
 import pytest
 
-pytestmark = pytest.importorskip("airtable")
+airtable = pytest.importorskip("airtable")
--- tests/tasks/twitter/__init__.py
+++ tests/tasks/twitter/__init__.py
@@ -1,3 +1,3 @@
 import pytest
 
-pytestmark = pytest.importorskip("tweepy")
+tweepy = pytest.importorskip("tweepy")
