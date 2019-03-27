38385b8bcfaaaf838f0bd4bd73a704a2261d969f
Josh Meek <josh@prefect.io>
2019-03-27 13:19:03 -0400

    CreateContainer Docker task

--- src/prefect/tasks/docker/__init__.py
+++ src/prefect/tasks/docker/__init__.py
@@ -1,1 +1,10 @@
-from prefect.tasks.docker.images import ListImages
+from prefect.tasks.docker.images import (
+    BuildImage,
+    ListImages,
+    PullImage,
+    PushImage,
+    RemoveImage,
+    TagImage,
+)
+
+from prefect.tasks.docker.containers import CreateContainer
