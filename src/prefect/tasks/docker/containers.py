eb5107bd9e0a73e0c7513591f15ff4d7c76872c0
Josh Meek <josh@prefect.io>
2019-03-27 14:23:19 -0400

    StopContainer Docker task

--- src/prefect/tasks/docker/containers.py
+++ src/prefect/tasks/docker/containers.py
@@ -152,3 +152,54 @@ class StartContainer(Task):
         client = docker.APIClient(base_url=docker_server_url, version="auto")
 
         client.start(container_id)
+
+
+class StopContainer(Task):
+    """
+    Task for stopping a Docker container.
+    Note that all initialization arguments can optionally be provided or overwritten at runtime.
+
+    Args:
+        - container_id (str, optional): The id of a container to stop
+        - docker_server_url (str, optional): URL for the Docker server. Defaults to
+            `unix:///var/run/docker.sock` however other hosts such as `tcp://0.0.0.0:2375`
+            can be provided
+        - **kwargs (dict, optional): additional keyword arguments to pass to the Task
+            constructor
+    """
+
+    def __init__(
+        self,
+        container_id: str = None,
+        docker_server_url: str = "unix:///var/run/docker.sock",
+        **kwargs: Any
+    ):
+        self.container_id = container_id
+        self.docker_server_url = docker_server_url
+
+        super().__init__(**kwargs)
+
+    @defaults_from_attrs("container_id", "docker_server_url")
+    def run(
+        self,
+        container_id: str = None,
+        docker_server_url: str = "unix:///var/run/docker.sock",
+    ) -> None:
+        """
+        Task run method.
+
+        Args:
+            - container_id (str, optional): The id of a container to stop
+            - docker_server_url (str, optional): URL for the Docker server. Defaults to
+                `unix:///var/run/docker.sock` however other hosts such as `tcp://0.0.0.0:2375`
+                can be provided
+
+        Raises:
+            - ValueError: if `container_id` is `None`
+        """
+        if not container_id:
+            raise ValueError("A container id must be provided.")
+
+        client = docker.APIClient(base_url=docker_server_url, version="auto")
+
+        client.stop(container_id)
