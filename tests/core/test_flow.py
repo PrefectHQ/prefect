5269efa60e3562e913001c8161a02671b58c24d5
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-04-15 11:12:19 -0400

    Test that flows stop on schedule

--- tests/core/test_flow.py
+++ tests/core/test_flow.py
@@ -1409,6 +1409,30 @@ class TestFlowRunMethod:
         assert t.call_count == 2
 
 
+    def test_flow_dot_run_stops_on_schedule(self):
+        class MockSchedule(prefect.schedules.Schedule):
+            call_count = 0
+
+            def next(self, n):
+                if self.call_count < 1:
+                    self.call_count += 1
+                    return [pendulum.now("utc").add(seconds=0.05)]
+                else:
+                    return []
+
+        class StatefulTask(Task):
+            call_count = 0
+
+            def run(self):
+                self.call_count += 1
+
+        t = StatefulTask()
+        schedule = MockSchedule()
+        f = Flow(name="test", tasks=[t], schedule=schedule)
+        f.run()
+        assert t.call_count == 1
+
+
     def test_scheduled_runs_handle_retries(self):
         class MockSchedule(prefect.schedules.Schedule):
             call_count = 0
