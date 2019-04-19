fec3c3ee0856f38360b4b34dff564aba32472dd3
Chris White <chris@prefect.io>
2019-04-18 19:03:24 -0700

    Refactor new triggers to avoid curry for a better UX

--- src/prefect/triggers.py
+++ src/prefect/triggers.py
@@ -2,7 +2,6 @@
 Triggers are functions that determine if task state should change based on
 the state of preceding tasks.
 """
-from toolz import curry
 from typing import Callable, Set, Union
 
 from prefect import context
@@ -104,12 +103,9 @@ def any_failed(upstream_states: Set["state.State"]) -> bool:
     return True
 
 
-@curry
 def some_failed(
-    upstream_states: Set["state.State"],
-    at_least: Union[int, float] = None,
-    at_most: Union[int, float] = None,
-) -> bool:
+    at_least: Union[int, float] = None, at_most: Union[int, float] = None
+) -> Callable[[Set["state.State"]], bool]:
     """
     Runs if some amount of upstream tasks failed. This amount can be specified as an upper bound (`at_most`) or
     a lower bound (`at_least`), and can be provided as an absolute number or a percentage of upstream tasks.
@@ -123,34 +119,42 @@ def some_failed(
         - at_most (Union[int, float], optional): the maximum number of upstream failures to allow for
             this task to run.  If the provided number is less than 0, it will be interpreted as a percentage, otherwise as an
             absolute number.
-        - upstream_states (set[State]): the set of all upstream states
     """
 
-    # scale conversions
-    num_failed = len([s for s in upstream_states if s.is_failed()])
-    num_states = len(upstream_states)
-    if at_least is not None:
-        at_least = (num_states * at_least) if at_least < 1 else at_least
-    else:
-        at_least = 0
-    if at_most is not None:
-        at_most = (num_states * at_most) if at_most < 1 else at_most
-    else:
-        at_most = num_states
+    def _some_failed(upstream_states: Set["state.State"]) -> bool:
+        """
+        The underlying trigger function.
 
-    if not (at_least <= num_failed <= at_most):
-        raise signals.TRIGGERFAIL(
-            'Trigger was "some_failed" but thresholds were not met.'
-        )
-    return True
+        Args:
+            - upstream_states (set[State]): the set of all upstream states
+
+        Returns:
+            - bool: whether the trigger thresolds were met
+        """
+        # scale conversions
+        num_failed = len([s for s in upstream_states if s.is_failed()])
+        num_states = len(upstream_states)
+        if at_least is not None:
+            min_num = (num_states * at_least) if at_least < 1 else at_least
+        else:
+            min_num = 0
+        if at_most is not None:
+            max_num = (num_states * at_most) if at_most < 1 else at_most
+        else:
+            max_num = num_states
+
+        if not (min_num <= num_failed <= max_num):
+            raise signals.TRIGGERFAIL(
+                'Trigger was "some_failed" but thresholds were not met.'
+            )
+        return True
+
+    return _some_failed
 
 
-@curry
 def some_successful(
-    upstream_states: Set["state.State"],
-    at_least: Union[int, float] = None,
-    at_most: Union[int, float] = None,
-) -> bool:
+    at_least: Union[int, float] = None, at_most: Union[int, float] = None
+) -> Callable[[Set["state.State"]], bool]:
     """
     Runs if some amount of upstream tasks succeed. This amount can be specified as an upper bound (`at_most`) or
     a lower bound (`at_least`), and can be provided as an absolute number or a percentage of upstream tasks.
@@ -164,26 +168,37 @@ def some_successful(
         - at_most (Union[int, float], optional): the maximum number of upstream successes to allow for
             this task to run.  If the provided number is less than 0, it will be interpreted as a percentage, otherwise as an
             absolute number.
-        - upstream_states (set[State]): the set of all upstream states
     """
 
-    # scale conversions
-    num_success = len([s for s in upstream_states if s.is_successful()])
-    num_states = len(upstream_states)
-    if at_least is not None:
-        at_least = (num_states * at_least) if at_least < 1 else at_least
-    else:
-        at_least = 0
-    if at_most is not None:
-        at_most = (num_states * at_most) if at_most < 1 else at_most
-    else:
-        at_most = num_states
+    def _some_successful(upstream_states: Set["state.State"]) -> bool:
+        """
+        The underlying trigger function.
 
-    if not (at_least <= num_success <= at_most):
-        raise signals.TRIGGERFAIL(
-            'Trigger was "some_successful" but thresholds were not met.'
-        )
-    return True
+        Args:
+            - upstream_states (set[State]): the set of all upstream states
+
+        Returns:
+            - bool: whether the trigger thresolds were met
+        """
+        # scale conversions
+        num_success = len([s for s in upstream_states if s.is_successful()])
+        num_states = len(upstream_states)
+        if at_least is not None:
+            min_num = (num_states * at_least) if at_least < 1 else at_least
+        else:
+            min_num = 0
+        if at_most is not None:
+            max_num = (num_states * at_most) if at_most < 1 else at_most
+        else:
+            max_num = num_states
+
+        if not (min_num <= num_success <= max_num):
+            raise signals.TRIGGERFAIL(
+                'Trigger was "some_successful" but thresholds were not met.'
+            )
+        return True
+
+    return _some_successful
 
 
 # aliases
