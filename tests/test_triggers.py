7e2ab4e58e8093b7ce6e798b7302bfb33e71a8b8
Chris White <chris@prefect.io>
2019-04-18 18:07:41 -0700

    Implement new some_failed trigger

--- tests/test_triggers.py
+++ tests/test_triggers.py
@@ -160,3 +160,51 @@ def test_any_failed_with_some_failed_and_1_success():
 def test_any_failed_with_all_success():
     with pytest.raises(signals.TRIGGERFAIL):
         triggers.any_failed(generate_states(success=3))
+
+
+@pytest.mark.parametrize(
+    "states", [generate_states(failed=3), generate_states(failed=1, success=2)]
+)
+def test_some_failed_with_no_args(states):
+    assert triggers.some_failed(states)
+
+
+def test_some_failed_with_one_arg():
+    trigger = triggers.some_failed(at_least=4)
+    assert trigger(generate_states(failed=4, skipped=2))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=3))
+
+    trigger = triggers.some_failed(at_most=4)
+    assert trigger(generate_states(failed=4, skipped=2))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=5))
+
+
+@pytest.mark.parametrize("at_least,at_most", [(1, 1), (0.2, 1), (0.01, 20)])
+def test_some_failed_with_all_success(at_least, at_most):
+    trigger = triggers.some_failed(at_least=at_least, at_most=at_most)
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(success=3))
+
+
+def test_some_failed_does_the_math():
+    trigger = triggers.some_failed(at_least=0.1, at_most=2)
+    assert trigger(generate_states(failed=2, pending=18))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=1, pending=18))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=3, pending=18))
+
+    trigger = triggers.some_failed(at_least=2, at_most=0.1)
+    assert trigger(generate_states(failed=2, pending=18))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=1, pending=18))
+
+    with pytest.raises(signals.TRIGGERFAIL):
+        trigger(generate_states(failed=3, pending=18))
