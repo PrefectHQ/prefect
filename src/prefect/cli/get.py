155106d38ffddaba0992e7a8c0cfdec21aa5e0a2
Josh Meek <josh@prefect.io>
2019-04-19 13:47:52 -0400

    Added more options to get flows

--- src/prefect/cli/get.py
+++ src/prefect/cli/get.py
@@ -9,34 +9,51 @@ from prefect.utilities.graphql import with_args, EnumValue
 @click.group()
 def get():
     """
-    Get commands
+    Get commands that refer to querying Prefect Cloud metadata.
     """
     pass
 
 
 @get.command()
-@click.option("--name", "-n")
-def flows(name):
+@click.option("--name", "-n", help="A flow name to query.")
+@click.option("--version", "-v", type=int, help="A flow version to query.")
+@click.option("--project", "-p", help="The name of a project to query.")
+@click.option("--all-versions", is_flag=True, help="Query all flow versions.")
+def flows(name, version, project, all_versions):
     """
-    Get flows
+    Query information regarding your Prefect flows.
     """
+
+    distinct_on = EnumValue("name")
+    if all_versions:
+        distinct_on = None
+
     query = {
         "query": {
             with_args(
                 "flow",
                 {
-                    "where": {"name": {"_eq": name}},
+                    "where": {
+                        "_and": {
+                            "name": {"_eq": name},
+                            "version": {"_eq": version},
+                            "project": {"name": {"_eq": project}},
+                        }
+                    },
                     "order_by": {
                         "name": EnumValue("asc"),
                         "version": EnumValue("desc"),
                     },
+                    "distinct_on": distinct_on,
                 },
-            ): {"name": True, "version": True, "created": True}
+            ): {
+                "name": True,
+                "version": True,
+                "project": {"name": True},
+                "created": True,
+            }
         }
     }
-
-    # where_clause = {"where": {"name": {"_eq": name}}}
-    # query = {"query": {with_args("flow", where_clause): "id"}}
 
     result = Client().graphql(query)
 
@@ -46,7 +63,20 @@ def flows(name):
 
     for item in flow_data:
         output.append(
-            [item.name, item.version, pendulum.parse(item.created).diff_for_humans()]
+            [
+                item.name,
+                item.version,
+                item.project.name,
+                pendulum.parse(item.created).diff_for_humans(),
+            ]
         )
 
-    click.echo(tabulate(output, headers=["name", "version", "age"]))
+    click.echo(
+        tabulate(
+            output,
+            headers=["NAME", "VERSION", "PROJECT NAME", "AGE"],
+            tablefmt="plain",
+            numalign="left",
+            stralign="left",
+        )
+    )
