c61cd5c54e5b89aefc4fa53a327e9892e8d2f6f1
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-05-20 20:33:03 -0400

    Add new schedule classes

--- src/prefect/serialization/__init__.py
+++ src/prefect/serialization/__init__.py
@@ -1,6 +1,7 @@
 import prefect.serialization.schedule
 import prefect.serialization.task
 import prefect.serialization.edge
+
 import prefect.serialization.flow
 import prefect.serialization.state
 import prefect.serialization.environment
