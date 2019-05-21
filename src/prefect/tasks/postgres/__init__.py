057b942b5d57563ad82670fe6350e5bbac8ac5eb
zangell44 <zachary.james.angell@gmail.com>
2019-05-21 07:31:43 -0400

    Rename Postgres tasks for consistency
    Renaming the two tasks I created to be more consistent
    with general task naming format. (e.g. SQLiteQuery or
    LambdaCreate)

--- src/prefect/tasks/postgres/__init__.py
+++ src/prefect/tasks/postgres/__init__.py
@@ -4,7 +4,7 @@ the psycopg2 library.
 """
 
 try:
-    from prefect.tasks.postgres.postgres import ExecuteTask, FetchTask
+    from prefect.tasks.postgres.postgres import PostgresExecute, PostgresFetch
 except ImportError:
     raise ImportError(
         'Using `prefect.tasks.postgres` requires Prefect to be installed with the "postgres" extra.'
--- tests/tasks/postgres/test_postgres.py
+++ tests/tasks/postgres/test_postgres.py
@@ -1,33 +1,37 @@
 import pytest
 
-from prefect.tasks.postgres import ExecuteTask, FetchTask
+from prefect.tasks.postgres import PostgresExecute, PostgresFetch
 
 
-class TestExecuteTask:
+class TestPostgresExecute:
     def test_construction(self):
-        task = ExecuteTask(db_name="test", user="test", password="test", host="test")
+        task = PostgresExecute(
+            db_name="test", user="test", password="test", host="test"
+        )
         assert task.commit is False
 
     def test_query_string_must_be_provided(self):
-        task = ExecuteTask(db_name="test", user="test", password="test", host="test")
+        task = PostgresExecute(
+            db_name="test", user="test", password="test", host="test"
+        )
         with pytest.raises(ValueError) as exc:
             task.run()
         assert "A query string must be provided" == str(exc.value)
 
 
-class TestFetchTask:
+class TestPostgresFetch:
     def test_construction(self):
-        task = FetchTask(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
         assert task.fetch == "one"
 
     def test_query_string_must_be_provided(self):
-        task = FetchTask(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
         with pytest.raises(ValueError) as exc:
             task.run()
         assert "A query string must be provided" == str(exc.value)
 
     def test_bad_fetch_param_raises(self):
-        task = FetchTask(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
         with pytest.raises(ValueError) as exc:
             task.run(query="SELECT * FROM some_table", fetch="not a valid parameter")
         assert (
