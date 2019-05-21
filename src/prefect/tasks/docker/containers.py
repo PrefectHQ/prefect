9cdb38c404703c4e0ac505024bb7bdaf2d19b26c
Chris White <chris@prefect.io>
2019-05-21 12:37:52 -0700

    Add WaitOnContainer Task

--- src/prefect/tasks/docker/containers.py
+++ src/prefect/tasks/docker/containers.py
@@ -3,6 +3,7 @@ from typing import Any, Union
 import docker
 
 from prefect import Task
+from prefect.engine.signals import FAIL
 from prefect.utilities.tasks import defaults_from_attrs
 
 
@@ -299,3 +300,77 @@ class StopContainer(Task):
         client = docker.APIClient(base_url=docker_server_url, version="auto")
 
         client.stop(container=container_id)
+
+
+class WaitOnContainer(Task):
+    """
+    Task for waiting on an already started Docker container.
+    Note that all initialization arguments can optionally be provided or overwritten at runtime.
+
+    Args:
+        - container_id (str, optional): The id of a container to start
+        - docker_server_url (str, optional): URL for the Docker server. Defaults to
+            `unix:///var/run/docker.sock` however other hosts such as `tcp://0.0.0.0:2375`
+            can be provided
+        - raise_for_exit_code (bool, optional): whether to raise a `FAIL` signal for a nonzero exit code;
+            defaults to `True`
+        - **kwargs (dict, optional): additional keyword arguments to pass to the Task
+            constructor
+    """
+
+    def __init__(
+        self,
+        container_id: str = None,
+        docker_server_url: str = "unix:///var/run/docker.sock",
+        raise_for_exit_code: bool = True,
+        **kwargs: Any
+    ):
+        self.container_id = container_id
+        self.docker_server_url = docker_server_url
+        self.raise_for_exit_code = raise_for_exit_code
+
+        super().__init__(**kwargs)
+
+    @defaults_from_attrs("container_id", "docker_server_url", "raise_for_exit_code")
+    def run(
+        self,
+        container_id: str = None,
+        docker_server_url: str = "unix:///var/run/docker.sock",
+        raise_for_exit_code: bool = True,
+    ) -> None:
+        """
+        Task run method.
+
+        Args:
+            - container_id (str, optional): The id of a container to start
+            - docker_server_url (str, optional): URL for the Docker server. Defaults to
+                `unix:///var/run/docker.sock` however other hosts such as `tcp://0.0.0.0:2375`
+                can be provided
+            - raise_for_exit_code (bool, optional): whether to raise a `FAIL` signal for a nonzero exit code;
+                defaults to `True`
+
+        Returns:
+            - dict: a dictionary with `StatusCode` and `Error` keys
+
+        Raises:
+            - ValueError: if `container_id` is `None`
+            - FAIL: if `raise_for_exit_code` is `True` and the container exits with a nonzero exit code
+        """
+        if not container_id:
+            raise ValueError("A container id must be provided.")
+
+        client = docker.APIClient(base_url=docker_server_url, version="auto")
+
+        result = client.wait(container=container_id)
+        if raise_for_exit_code and (
+            (result.get("Error") is not None) or result.get("StatusCode")
+        ):
+            raise FAIL(
+                "{id} failed with exit code {code}: {msg}".format(
+                    id=container_id,
+                    code=result.get("StatusCode"),
+                    msg=result.get("Error"),
+                )
+            )
+
+        return result
