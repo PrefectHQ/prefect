b75acd90887064c3c6717f4afc6c72a86a79a5e2
Josh Meek <josh@prefect.io>
2019-05-29 12:57:59 -0400

    Add summarize CLI command tests

---
+++ tests/cli/test_summarize.py
@@ -0,0 +1,34 @@
+from unittest.mock import MagicMock
+
+import click
+from click.testing import CliRunner
+import requests
+
+import prefect
+from prefect.cli.summarize import summarize
+
+
+def test_summarize_init():
+    runner = CliRunner()
+    result = runner.invoke(summarize)
+    assert result.exit_code == 0
+    assert "Summarize Prefect Cloud metadata. *Not yet implemented*" in result.output
+
+
+def test_summarize_help():
+    runner = CliRunner()
+    result = runner.invoke(summarize, ["--help"])
+    assert result.exit_code == 0
+    assert "Summarize Prefect Cloud metadata. *Not yet implemented*" in result.output
+
+
+def test_summarize_flow_runs():
+    runner = CliRunner()
+    result = runner.invoke(summarize, ["flow-runs"])
+    assert result.exit_code == 0
+
+
+def test_summarize_projects():
+    runner = CliRunner()
+    result = runner.invoke(summarize, ["projects"])
+    assert result.exit_code == 0
