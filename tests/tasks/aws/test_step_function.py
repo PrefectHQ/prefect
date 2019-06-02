67cf39f0e7b5e58c8556ac2bb171986552315b23
zangell44 <42625717+zangell44@users.noreply.github.com>
2019-06-02 12:11:56 -0400

    Add tests for aws step functin task
    Basic tests have been added to ensure
    - construction suceeds with default params
    - initialization passes to the Task base class
    - AWS credentials are pulled from the prefect secret
    
    The StepActivate task was tested locally with live aws creds.
    
    Resolves #1012

---
+++ tests/tasks/aws/test_step_function.py
@@ -0,0 +1,35 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+import prefect
+from prefect.tasks.aws import StepActivate
+from prefect.utilities.configuration import set_temporary_config
+
+
+class TestStepActivate:
+    def test_initialization(self):
+        task = StepActivate(state_machine_arn="arn", execution_name="name")
+        assert task.aws_credentials_secret == "AWS_CREDENTIALS"
+
+    def test_initialization_passes_to_task_constructor(self):
+        task = StepActivate(
+            state_machine_arn="arn", execution_name="name", name="test", tags=["AWS"]
+        )
+        assert task.name == "test"
+        assert task.tags == {"AWS"}
+
+    def test_creds_are_pulled_from_secret(self, monkeypatch):
+        task = StepActivate(state_machine_arn="arn", execution_name="name")
+        client = MagicMock()
+        boto3 = MagicMock(client=client)
+        monkeypatch.setattr("prefect.tasks.aws.step_function.boto3", boto3)
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            with prefect.context(
+                secrets=dict(
+                    AWS_CREDENTIALS={"ACCESS_KEY": "42", "SECRET_ACCESS_KEY": "99"}
+                )
+            ):
+                task.run()
+        kwargs = client.call_args[1]
+        assert kwargs == {"aws_access_key_id": "42", "aws_secret_access_key": "99"}
