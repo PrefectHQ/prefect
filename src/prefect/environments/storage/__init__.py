e1524800ceba5c3e0bea624ee5ff2b528eb31f97
Josh Meek <josh@prefect.io>
2019-07-15 13:09:35 -0400

    Rename LocalStorage -> Local

--- src/prefect/environments/storage/__init__.py
+++ src/prefect/environments/storage/__init__.py
@@ -13,5 +13,5 @@ The Prefect Storage interface encapsulates logic for storing, serializing and ev
 from prefect.environments.storage.base import Storage
 from prefect.environments.storage.docker import Docker
 from prefect.environments.storage.bytes import Bytes
-from prefect.environments.storage.local import LocalStorage
+from prefect.environments.storage.local import Local
 from prefect.environments.storage.memory import Memory
