fdda64febdb217b54d1b41b3122789c0a477384b
Chris White <chris@prefect.io>
2019-07-21 12:07:57 -0700

    Remove unnecessary layer of import from remote environment

--- src/prefect/environments/execution/__init__.py
+++ src/prefect/environments/execution/__init__.py
@@ -1,2 +1,3 @@
 from prefect.environments.execution.base import Environment
 from prefect.environments.execution.local import LocalEnvironment
+from prefect.environments.execution.remote import RemoteEnvironment
