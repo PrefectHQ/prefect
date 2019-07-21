ebf5778d4b6973bed51fb5364fdb4536eacbd0e0
Chris White <chris@prefect.io>
2019-07-21 12:23:24 -0700

    Update and add some clarity in storage / execution documentation

--- src/prefect/environments/execution/__init__.py
+++ src/prefect/environments/execution/__init__.py
@@ -1,3 +1,9 @@
+"""
+Execution environments encapsulate the logic for where your Flow should execute in Prefect Cloud.
+
+Currently, we recommend all users deploy their Flow using the `RemoteEnvironment` configured with the
+appropriate choice of executor.
+"""
 from prefect.environments.execution.base import Environment
 from prefect.environments.execution.local import LocalEnvironment
 from prefect.environments.execution.remote import RemoteEnvironment
