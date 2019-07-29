7fd4b42536733e3fe4c806e68d1e0412126476cf
Chris White <chris@prefect.io>
2019-07-29 11:52:01 -0400

    Add docstring for lower_bounds helper script

--- lower_bounds.py
+++ lower_bounds.py
@@ -1,3 +1,11 @@
+"""
+This helper script compiles all of our lowest specified bounds
+on all base dependencies from our `requirements.txt` file into a new
+`lower_requirements.txt` file.  In this new requirements file all dependencies
+are pinned to their lowest allowed versions.  We use this
+new requirements file to test that we still support all of our
+lower bounds.
+"""
 import re
 
 
