b51c1d55c312c67deb6b9899520f01947a531395
Chris White <chris@prefect.io>
2019-08-08 11:29:34 -0700

    Update docker doc to call out registry differences

--- src/prefect/environments/storage/docker.py
+++ src/prefect/environments/storage/docker.py
@@ -27,7 +27,9 @@ class Docker(Storage):
     A user specifies a `registry_url`, `base_image` and other optional dependencies (e.g., `python_dependencies`)
     and `build()` will create a temporary Dockerfile that is used to build the image.
 
-    Note that the `base_image` must be capable of `pip` installing.
+    Note that the `base_image` must be capable of `pip` installing.  Note that registry behavior with respect to
+    image names can differ between providers - for example, Google's GCR registry allows for registry URLs of the form
+    `gcr.io/my-registry/subdir/my-image-name` whereas DockerHub requires the registry URL to be separate from the image name.
 
     Args:
         - registry_url (str, optional): URL of a registry to push the image to; image will not be pushed if not provided
