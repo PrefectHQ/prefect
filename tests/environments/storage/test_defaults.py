f4896ddc0c74b097e461a5547f0f5b4984c56ed3
Chris White <chris@prefect.io>
2019-08-08 17:54:45 -0700

    Add tests and fixes which were caught by tests

--- tests/environments/storage/test_defaults.py
+++ tests/environments/storage/test_defaults.py
@@ -1,5 +1,20 @@
 from prefect.environments import storage
+from prefect import utilities
 
 
 def test_default_storage():
     assert storage.get_default_storage_class() is storage.Docker
+
+
+def test_default_storage_responds_to_config():
+    with utilities.configuration.set_temporary_config(
+        {"flows.defaults.storage.default_class": "prefect.environments.storage.Memory"}
+    ):
+        assert storage.get_default_storage_class() is storage.Memory
+
+
+def test_default_storage_ignores_bad_config():
+    with utilities.configuration.set_temporary_config(
+        {"flows.defaults.storage.default_class": "FOOBAR"}
+    ):
+        assert storage.get_default_storage_class() is storage.Docker
