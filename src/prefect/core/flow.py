317c8f28cde75894d6f897e61bbe0db8bbbb2cde
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-08-28 20:58:55 -0400

    Hello darkness my old friend

--- src/prefect/core/flow.py
+++ src/prefect/core/flow.py
@@ -840,8 +840,10 @@ class Flow:
         prefect.context.setdefault("caches", {})
 
         # set context for this flow run
-        flow_run_context = kwargs.pop("context", {}).copy() # copy to avoid modification
-        flow_run_context.setdefault('scheduled_start_time', next_run_time)
+        flow_run_context = kwargs.pop(
+            "context", {}
+        ).copy()  # copy to avoid modification
+        flow_run_context.setdefault("scheduled_start_time", next_run_time)
 
         ## run this flow indefinitely, so long as its schedule has future dates
         while True:
