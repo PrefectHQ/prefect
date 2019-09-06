ffbeb79ff48d246d3c2892174e452383051bc33c
Chris White <chris@prefect.io>
2019-09-05 20:29:12 -0700

    Update formatting tests to use platform agnostic path manipulation

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -8,10 +8,13 @@ import pytest
 pytestmark = pytest.mark.formatting
 
 
+FILE_ENDING = os.path.join("tests", "test_formatting.py")
+
+
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="Black requires Python 3.6+")
 def test_black_formatting():
     # make sure we know what working directory we're in
-    assert __file__.endswith("/tests/test_formatting.py")
+    assert __file__.endswith(FILE_ENDING)
 
     prefect_dir = os.path.dirname(os.path.dirname(__file__))
     result = subprocess.call(shlex.split("black --check {}".format(prefect_dir)))
@@ -21,10 +24,10 @@ def test_black_formatting():
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="mypy requires Python 3.6+")
 def test_mypy():
     # make sure we know what working directory we're in
-    assert __file__.endswith("/tests/test_formatting.py")
+    assert __file__.endswith(FILE_ENDING)
 
     prefect_dir = os.path.dirname(os.path.dirname(__file__))
-    prefect_src_dir = os.path.join(prefect_dir, "src/")
+    prefect_src_dir = os.path.join(prefect_dir, "src")
     config_file = os.path.join(prefect_dir, "setup.cfg")
     result = subprocess.call(
         shlex.split("mypy --config-file {0} {1}".format(config_file, prefect_src_dir))
