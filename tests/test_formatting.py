f959676f70ad4f7d13e346fa6454a1d4fb25a0e3
Chris White <chris@prefect.io>
2019-09-05 20:34:47 -0700

    Add posix flag to formatting check subprocess calls

--- tests/test_formatting.py
+++ tests/test_formatting.py
@@ -9,6 +9,7 @@ pytestmark = pytest.mark.formatting
 
 
 FILE_ENDING = os.path.join("tests", "test_formatting.py")
+POSIX = sys.platform != "win32"
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="Black requires Python 3.6+")
@@ -17,7 +18,9 @@ def test_black_formatting():
     assert __file__.endswith(FILE_ENDING)
 
     prefect_dir = os.path.dirname(os.path.dirname(__file__))
-    result = subprocess.call(shlex.split("black --check {}".format(prefect_dir)))
+    result = subprocess.call(
+        shlex.split("black --check {}".format(prefect_dir), posix=POSIX)
+    )
     assert result == 0, "Prefect repo did not pass Black formatting!"
 
 
@@ -30,6 +33,9 @@ def test_mypy():
     prefect_src_dir = os.path.join(prefect_dir, "src")
     config_file = os.path.join(prefect_dir, "setup.cfg")
     result = subprocess.call(
-        shlex.split("mypy --config-file {0} {1}".format(config_file, prefect_src_dir))
+        shlex.split(
+            "mypy --config-file {0} {1}".format(config_file, prefect_src_dir),
+            posix=POSIX,
+        )
     )
     assert result == 0, "Prefect repo did not pass mypy type checks!"
