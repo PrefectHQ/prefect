b4e05c7b5999f04131bcc99909244f4f179900ef
Chris White <chris@prefect.io>
2019-09-10 12:19:57 -0700

    Switch order of merge dicts operation

--- src/prefect/utilities/context.py
+++ src/prefect/utilities/context.py
@@ -78,7 +78,7 @@ class Context(DotDict, threading.local):
         if "context" in config:
             self.update(config.context)
         if "config" in self:
-            new_config = merge_dicts(self["config"], config)
+            new_config = merge_dicts(config, self["config"])  # order matters
             self["config"] = as_nested_dict(new_config, dct_class=Config)
         else:
             self["config"] = config
@@ -100,7 +100,7 @@ class Context(DotDict, threading.local):
         try:
             new_context = Context(*args, **kwargs)
             if "config" in new_context:
-                new_config = merge_dicts(self.get("config", {}), new_context["config"])
+                new_config = merge_dicts(new_context["config"], self.get("config", {}))
                 new_context["config"] = as_nested_dict(new_config, dct_class=Config)
             self.update(new_context)  # type: ignore
             yield self
