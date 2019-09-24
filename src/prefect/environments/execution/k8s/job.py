31dd1a0a4dad698b929885e3fb1243f1bc78101b
Josh Meek <josh@prefect.io>
2019-09-24 14:58:42 -0400

    Update environment docstring to remove unused env var;

--- src/prefect/environments/execution/k8s/job.py
+++ src/prefect/environments/execution/k8s/job.py
@@ -23,8 +23,7 @@ class KubernetesJobEnvironment(Environment):
     PREFECT__CLOUD__GRAPHQL, PREFECT__CLOUD__AUTH_TOKEN, PREFECT__CONTEXT__FLOW_RUN_ID,
     PREFECT__CONTEXT__NAMESPACE, PREFECT__CONTEXT__IMAGE, PREFECT__CONTEXT__FLOW_FILE_PATH,
     PREFECT__CLOUD__USE_LOCAL_SECRETS, PREFECT__ENGINE__FLOW_RUNNER__DEFAULT_CLASS,
-    PREFECT__ENGINE__TASK_RUNNER__DEFAULT_CLASS, PREFECT__ENGINE__EXECUTOR__DEFAULT_CLASS,
-    PREFECT__LOGGING__LOG_TO_CLOUD
+    PREFECT__ENGINE__TASK_RUNNER__DEFAULT_CLASS, PREFECT__LOGGING__LOG_TO_CLOUD
 
     Additionally, the following command will be applied to the first container:
     $ /bin/sh -c 'python -c "from prefect.environments import KubernetesJobEnvironment; KubernetesJobEnvironment().run_flow()"'
