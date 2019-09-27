494d9972ad1f86e5e395471860d92b003188ca32
Josh Meek <josh@prefect.io>
2019-09-27 13:44:26 -0400

    Add tests for watch and logs run flags

--- tests/cli/test_run.py
+++ tests/cli/test_run.py
@@ -65,6 +65,119 @@ def test_run_cloud(monkeypatch):
         assert post.call_args[1]["json"]["query"].split() == query.split()
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 6), reason="3.5 does not preserve dictionary order"
+)
+def test_run_cloud_watch(monkeypatch):
+    post = MagicMock(
+        return_value=MagicMock(
+            json=MagicMock(
+                return_value=dict(
+                    data=dict(
+                        flow=[{"id": "flow"}],
+                        flow_run_by_pk=dict(
+                            states=[
+                                {"state": "Running", "timestamp": None},
+                                {"state": "Success", "timestamp": None},
+                            ]
+                        ),
+                    )
+                )
+            )
+        )
+    )
+    session = MagicMock()
+    session.return_value.post = post
+    monkeypatch.setattr("requests.Session", session)
+
+    create_flow_run = MagicMock(resurn_value="id")
+    monkeypatch.setattr(
+        "prefect.client.Client.create_flow_run", MagicMock(return_value=create_flow_run)
+    )
+
+    with set_temporary_config(
+        {"cloud.graphql": "http://my-cloud.foo", "cloud.auth_token": "secret_token"}
+    ):
+        runner = CliRunner()
+        result = runner.invoke(
+            run,
+            [
+                "cloud",
+                "--name",
+                "flow",
+                "--project",
+                "project",
+                "--version",
+                "2",
+                "--watch",
+            ],
+        )
+        assert result.exit_code == 0
+        assert "Running" in result.output
+        assert "Success" in result.output
+        assert post.called
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 6), reason="3.5 does not preserve dictionary order"
+)
+def test_run_cloud_logs(monkeypatch):
+    post = MagicMock(
+        return_value=MagicMock(
+            json=MagicMock(
+                return_value=dict(
+                    data=dict(
+                        flow=[{"id": "flow"}],
+                        flow_run=[
+                            {
+                                "logs": [
+                                    {
+                                        "timestamp": "test_timestamp",
+                                        "message": "test_message",
+                                        "level": "test_level",
+                                    }
+                                ]
+                            }
+                        ],
+                        flow_run_by_pk=dict(state="Success"),
+                    )
+                )
+            )
+        )
+    )
+    session = MagicMock()
+    session.return_value.post = post
+    monkeypatch.setattr("requests.Session", session)
+
+    create_flow_run = MagicMock(resurn_value="id")
+    monkeypatch.setattr(
+        "prefect.client.Client.create_flow_run", MagicMock(return_value=create_flow_run)
+    )
+
+    with set_temporary_config(
+        {"cloud.graphql": "http://my-cloud.foo", "cloud.auth_token": "secret_token"}
+    ):
+        runner = CliRunner()
+        result = runner.invoke(
+            run,
+            [
+                "cloud",
+                "--name",
+                "flow",
+                "--project",
+                "project",
+                "--version",
+                "2",
+                "--logs",
+            ],
+        )
+        assert result.exit_code == 0
+        assert "test_timestamp" in result.output
+        assert "test_message" in result.output
+        assert "test_level" in result.output
+        assert post.called
+
+
 def test_run_cloud_fails(monkeypatch):
     post = MagicMock(
         return_value=MagicMock(json=MagicMock(return_value=dict(data=dict(flow=[]))))
