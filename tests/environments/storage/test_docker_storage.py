1129130e4e61bd877c5cd4a9c9361d2a791fda98
Josh Meek <josh@prefect.io>
2019-10-23 12:48:25 -0400

    Update docker storage test output

--- tests/environments/storage/test_docker_storage.py
+++ tests/environments/storage/test_docker_storage.py
@@ -317,7 +317,7 @@ def test_create_dockerfile_from_base_image():
             (
                 "FROM python:3.6-slim",
                 "apt update && apt install -y gcc git && rm -rf /var/lib/apt/lists/*",
-                "pip install git+https://github.com/PrefectHQ/prefect.git@424be6b5ed8d3be85064de4b95b5c3d7cb665510#egg=prefect[kubernetes]",
+                "pip install git+https://github.com/PrefectHQ/prefect.git@424be6b5ed8d3be85064de4b95b5c3d7cb665510#egg=prefect",
             ),
         ),
     ],
