cde1e7cee9a7fbcbc6cfc9229c772844b96c98b1
Chris White <chris@prefect.io>
2019-11-04 18:37:23 -0800

    Include agent labels in the environment for flows run using local agent as well

--- src/prefect/agent/local/agent.py
+++ src/prefect/agent/local/agent.py
@@ -124,6 +124,7 @@ class LocalAgent(Agent):
         return {
             "PREFECT__CLOUD__API": config.cloud.api,
             "PREFECT__CLOUD__AUTH_TOKEN": config.cloud.agent.auth_token,
+            "PREFECT__CLOUD__AGENT__LABELS": str(self.labels),
             "PREFECT__CONTEXT__FLOW_RUN_ID": flow_run.id,  # type: ignore
             "PREFECT__CLOUD__USE_LOCAL_SECRETS": "false",
             "PREFECT__LOGGING__LOG_TO_CLOUD": "true",
