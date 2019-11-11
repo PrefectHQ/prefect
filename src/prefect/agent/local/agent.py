87b1b3e5b0c08ef3e7d352efc63a5fced0340d1b
Chris White <chris@prefect.io>
2019-11-11 10:48:16 -0800

    Move local agent logic

--- src/prefect/agent/local/agent.py
+++ src/prefect/agent/local/agent.py
@@ -23,9 +23,9 @@ class LocalAgent(Agent):
 
     def __init__(self, name: str = None, labels: Iterable[str] = None,) -> None:
         self.processes = []
+        super().__init__(name=name, labels=labels)
         if "local" not in self.labels:
             self.labels.append("local")
-        super().__init__(name=name, labels=labels)
 
     def heartbeat(self) -> None:
         for idx, process in enumerate(self.processes):
