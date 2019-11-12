bc1cad8a7455e1f045e3bf4d07066c68f44d0f29
nicholas <znicholasbrown@gmail.com>
2019-11-11 20:16:56 -0500

    Fix black issue

--- src/prefect/client/client.py
+++ src/prefect/client/client.py
@@ -656,7 +656,7 @@ class Client:
                 scheduledStartTime=scheduled_start_time.isoformat()
             )  # type: ignore
         if run_name is not None:
-                inputs.update(
+            inputs.update(
                 runName=run_name
             )  # type: ignore
         res = self.graphql(create_mutation, variables=dict(input=inputs))
