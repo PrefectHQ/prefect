7e5745af6e100b71408d26dd33ea6565293ce3e7
Josh Meek <josh@prefect.io>
2019-12-11 10:01:13 -0500

    Add tests for Azure Blob storage

--- tests/environments/storage/test_azure_storage.py
+++ tests/environments/storage/test_azure_storage.py
@@ -32,258 +32,188 @@ def test_serialize_azure_storage():
     assert serialized_storage["type"] == "Azure"
 
 
-# def test_boto3_client_property(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(client=MagicMock(return_value=client))
-#     monkeypatch.setattr("boto3.client", boto3)
+def test_blob_service_client_property(monkeypatch):
+    connection = MagicMock()
+    azure = MagicMock(from_connection_string=connection)
+    monkeypatch.setattr("azure.storage.blob.BlobServiceClient", azure)
 
-#     storage = S3(
-#         bucket="bucket",
-#         aws_access_key_id="id",
-#         aws_secret_access_key="secret",
-#         aws_session_token="session",
-#     )
+    storage = Azure(container="test", connection_string="conn",)
 
-#     boto3_client = storage._boto3_client
-#     assert boto3_client
-#     boto3.assert_called_with(
-#         "s3",
-#         aws_access_key_id="id",
-#         aws_secret_access_key="secret",
-#         aws_session_token="session",
-#     )
+    azure_client = storage._azure_block_blob_service
+    assert azure_client
+    connection.assert_called_with(conn_str="conn")
 
 
-# def test_add_flow_to_S3(bucket="bucket"):
-#     storage = S3(bucket="bucket")
+def test_add_flow_to_azure():
+    storage = Azure(container="test")
 
-#     f = Flow("test")
-#     assert f.name not in storage
-#     assert storage.add_flow(f)
-#     assert f.name in storage
+    f = Flow("test")
+    assert f.name not in storage
+    assert storage.add_flow(f)
+    assert f.name in storage
 
 
-# def test_add_multiple_flows_to_S3(bucket="bucket"):
-#     storage = S3(bucket="bucket")
+def test_add_multiple_flows_to_Azure():
+    storage = Azure(container="container")
 
-#     f = Flow("test")
-#     g = Flow("testg")
-#     assert f.name not in storage
-#     assert storage.add_flow(f)
-#     assert storage.add_flow(g)
-#     assert f.name in storage
-#     assert g.name in storage
+    f = Flow("test")
+    g = Flow("testg")
+    assert f.name not in storage
+    assert storage.add_flow(f)
+    assert storage.add_flow(g)
+    assert f.name in storage
+    assert g.name in storage
 
 
-# def test_upload_flow_to_s3(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+def test_upload_flow_to_azure(monkeypatch):
+    client = MagicMock(upload_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-#     storage = S3(bucket="bucket")
+    storage = Azure(container="container")
 
-#     f = Flow("test")
-#     assert f.name not in storage
-#     assert storage.add_flow(f)
-#     assert storage.build()
-#     assert boto3.upload_fileobj.called
-#     assert f.name in storage
+    f = Flow("test")
+    assert f.name not in storage
+    assert storage.add_flow(f)
+    assert storage.build()
+    assert client.upload_blob.called
+    assert f.name in storage
 
 
-# def test_upload_flow_to_s3_client_error(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     boto3.upload_fileobj.side_effect = ClientError({}, None)
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+def test_upload_flow_to_azure_blob_name(monkeypatch):
+    client = MagicMock(upload_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-#     storage = S3(bucket="bucket")
+    storage = Azure(container="container", blob_name="name")
 
-#     f = Flow("test")
-#     assert f.name not in storage
-#     assert storage.add_flow(f)
+    f = Flow("test")
+    assert storage.add_flow(f)
+    assert storage.build()
 
-#     with pytest.raises(ClientError):
-#         storage.build()
-#     assert boto3.upload_fileobj.called
+    assert service.get_blob_client.call_args[1]["container"] == "container"
+    assert service.get_blob_client.call_args[1]["blob"] == "name"
 
 
-# def test_upload_flow_to_s3_bucket_key(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+def test_upload_multiple_flows_to_azure_blob_name(monkeypatch):
+    client = MagicMock(upload_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-#     storage = S3(bucket="bucket", key="key")
+    storage = Azure(container="container", blob_name="name")
 
-#     f = Flow("test")
-#     assert storage.add_flow(f)
-#     assert storage.build()
+    f1 = Flow("test1")
+    f2 = Flow("test2")
+    assert storage.add_flow(f1)
+    assert storage.add_flow(f2)
+    assert storage.build()
 
-#     assert boto3.upload_fileobj.call_args[1]["Bucket"] == "bucket"
-#     assert boto3.upload_fileobj.call_args[1]["Key"] == "key"
+    assert service.get_blob_client.call_args[1]["container"] == "container"
 
 
-# def test_upload_multiple_flows_to_s3_bucket_key(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+def test_upload_flow_to_azure_blob_name_format(monkeypatch):
+    client = MagicMock(upload_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-#     storage = S3(bucket="bucket")
+    storage = Azure(container="container")
 
-#     f1 = Flow("test1")
-#     f2 = Flow("test2")
-#     assert storage.add_flow(f1)
-#     assert storage.add_flow(f2)
-#     assert storage.build()
+    f = Flow("test")
+    assert storage.add_flow(f)
+    assert storage.build()
 
-#     assert boto3.upload_fileobj.call_args[1]["Bucket"] == "bucket"
+    assert service.get_blob_client.call_args[1]["container"] == "container"
+    key = service.get_blob_client.call_args[1]["blob"].split("/")
 
+    assert key[0] == "test"
+    assert key[1]
 
-# def test_upload_flow_to_s3_flow_byte_stream(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
 
-#     storage = S3(bucket="bucket")
+def test_add_flow_to_azure_already_added(monkeypatch):
+    storage = Azure(container="container")
 
-#     f = Flow("test")
-#     assert storage.add_flow(f)
-#     assert storage.build()
+    f = Flow("test")
+    assert f.name not in storage
+    assert storage.add_flow(f)
+    assert f.name in storage
 
-#     flow_as_bytes = boto3.upload_fileobj.call_args[0][0]
-#     assert isinstance(flow_as_bytes, io.BytesIO)
+    with pytest.raises(ValueError):
+        storage.add_flow(f)
 
-#     new_flow = cloudpickle.loads(flow_as_bytes.read())
-#     assert new_flow.name == "test"
 
-#     state = new_flow.run()
-#     assert state.is_successful()
+def test_get_flow_azure(monkeypatch):
+    client = MagicMock(download_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
+    f = Flow("test")
 
-# def test_upload_flow_to_s3_key_format(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(upload_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+    monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
 
-#     storage = S3(bucket="bucket")
+    storage = Azure(container="container")
 
-#     f = Flow("test")
-#     assert storage.add_flow(f)
-#     assert storage.build()
+    assert f.name not in storage
+    flow_location = storage.add_flow(f)
 
-#     assert boto3.upload_fileobj.call_args[1]["Bucket"] == "bucket"
-#     key = boto3.upload_fileobj.call_args[1]["Key"].split("/")
+    assert storage.get_flow(flow_location)
+    assert client.download_blob.called
+    assert f.name in storage
 
-#     assert key[0] == "test"
-#     assert key[1]
 
+def test_get_flow_azure_bucket_key(monkeypatch):
+    client = MagicMock(download_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-# def test_add_flow_to_s3_already_added(monkeypatch):
-#     storage = S3(bucket="bucket")
+    f = Flow("test")
 
-#     f = Flow("test")
-#     assert f.name not in storage
-#     assert storage.add_flow(f)
-#     assert f.name in storage
+    monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
 
-#     with pytest.raises(ValueError):
-#         storage.add_flow(f)
+    storage = Azure(container="container", blob_name="name")
 
+    assert f.name not in storage
+    flow_location = storage.add_flow(f)
 
-# def test_get_flow_s3(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(download_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+    assert storage.get_flow(flow_location)
+    assert service.get_blob_client.call_args[1]["container"] == "container"
+    assert service.get_blob_client.call_args[1]["blob"] == flow_location
 
-#     f = Flow("test")
 
-#     monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
+def test_get_flow_azure_runs(monkeypatch):
+    client = MagicMock(download_blob=MagicMock())
+    service = MagicMock(get_blob_client=MagicMock(return_value=client))
+    monkeypatch.setattr(
+        "prefect.environments.storage.Azure._azure_block_blob_service", service
+    )
 
-#     storage = S3(bucket="bucket")
+    f = Flow("test")
 
-#     assert f.name not in storage
-#     flow_location = storage.add_flow(f)
+    monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
 
-#     assert storage.get_flow(flow_location)
-#     assert boto3.download_fileobj.called
-#     assert f.name in storage
+    storage = Azure(container="container")
 
+    assert f.name not in storage
+    flow_location = storage.add_flow(f)
 
-# def test_get_flow_s3_client_error(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(download_fileobj=MagicMock(return_value=client))
-#     boto3.download_fileobj.side_effect = ClientError({}, None)
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
+    new_flow = storage.get_flow(flow_location)
+    assert client.download_blob.called
+    assert f.name in storage
 
-#     f = Flow("test")
+    assert isinstance(new_flow, Flow)
+    assert new_flow.name == "test"
+    assert len(new_flow.tasks) == 0
 
-#     storage = S3(bucket="bucket")
-
-#     assert f.name not in storage
-#     flow_location = storage.add_flow(f)
-
-#     with pytest.raises(ClientError):
-#         storage.get_flow(flow_location)
-
-#     assert boto3.download_fileobj.called
-
-
-# def test_get_flow_s3_bucket_key(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(download_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
-
-#     f = Flow("test")
-
-#     monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
-
-#     storage = S3(bucket="bucket", key="key")
-
-#     assert f.name not in storage
-#     flow_location = storage.add_flow(f)
-
-#     assert storage.get_flow(flow_location)
-#     assert boto3.download_fileobj.call_args[1]["Bucket"] == "bucket"
-#     assert boto3.download_fileobj.call_args[1]["Key"] == "key"
-
-
-# def test_get_flow_s3_not_in_storage(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(download_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
-
-#     f = Flow("test")
-
-#     monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
-
-#     storage = S3(bucket="bucket", key="test")
-
-#     assert f.name not in storage
-
-#     with pytest.raises(ValueError):
-#         storage.get_flow("test/test")
-
-
-# def test_get_flow_s3_runs(monkeypatch):
-#     client = MagicMock()
-#     boto3 = MagicMock(download_fileobj=MagicMock(return_value=client))
-#     monkeypatch.setattr("prefect.environments.storage.S3._boto3_client", boto3)
-
-#     f = Flow("test")
-
-#     monkeypatch.setattr("cloudpickle.loads", MagicMock(return_value=f))
-
-#     storage = S3(bucket="bucket")
-
-#     assert f.name not in storage
-#     flow_location = storage.add_flow(f)
-
-#     new_flow = storage.get_flow(flow_location)
-#     assert boto3.download_fileobj.called
-#     assert f.name in storage
-
-#     assert isinstance(new_flow, Flow)
-#     assert new_flow.name == "test"
-#     assert len(new_flow.tasks) == 0
-
-#     state = new_flow.run()
-#     assert state.is_successful()
+    state = new_flow.run()
+    assert state.is_successful()
