a257afb5c3c456a2313362f0ead7f91ca177a751
Chris White <chris@prefect.io>
2019-12-27 16:18:40 -0600

    Add docstrings and a few tests

--- src/prefect/utilities/google.py
+++ src/prefect/utilities/google.py
@@ -3,7 +3,20 @@ from google.cloud import storage
 from google.oauth2.service_account import Credentials
 
 
-def _get_google_client(submodule, credentials: dict = None, project: str = None):
+def get_google_client(submodule, credentials: dict = None, project: str = None):
+    """
+    Utility function for loading Google Client objects from a given set of credentials.
+
+    Args:
+        - submodule: a Python submodule with a Client attribute
+        - credentials (dict, optional): a dictionary of Google credentials used to initialize the Client; if
+            not provided, will attempt to load the Client using ambient environment settings
+        - project (str, optional): the Google project to point the Client to; if not provided, Client defaults
+            will be used
+
+    Returns:
+        - Client: an initialized and authenticated Google Client
+    """
     Client = getattr(submodule, "Client")
     if credentials is not None:
         credentials = Credentials.from_service_account_info(credentials)
@@ -15,8 +28,32 @@ def _get_google_client(submodule, credentials: dict = None, project: str = None)
 
 
 def get_storage_client(credentials: dict = None, project: str = None):
-    return _get_google_client(storage, credentials=credentials, project=project)
+    """
+    Utility function for instantiating a Google Storage Client from a given set of credentials.
+
+    Args:
+        - credentials (dict, optional): a dictionary of Google credentials used to initialize the Client; if
+            not provided, will attempt to load the Client using ambient environment settings
+        - project (str, optional): the Google project to point the Client to; if not provided, Client defaults
+            will be used
+
+    Returns:
+        - Client: an initialized and authenticated Google Client
+    """
+    return get_google_client(storage, credentials=credentials, project=project)
 
 
 def get_bigquery_client(credentials: dict = None, project: str = None):
-    return _get_google_client(bigquery, credentials=credentials, project=project)
+    """
+    Utility function for instantiating a Google BigQuery Client from a given set of credentials.
+
+    Args:
+        - credentials (dict, optional): a dictionary of Google credentials used to initialize the Client; if
+            not provided, will attempt to load the Client using ambient environment settings
+        - project (str, optional): the Google project to point the Client to; if not provided, Client defaults
+            will be used
+
+    Returns:
+        - Client: an initialized and authenticated Google Client
+    """
+    return get_google_client(bigquery, credentials=credentials, project=project)
