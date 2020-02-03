ab00487fb9c8b0d157069799689a90affb61504e
Josh Meek <josh@prefect.io>
2020-02-03 14:23:35 -0500

    Pass cloud.auth_token to task run by FargateTaskEnvironment execute step

--- src/prefect/environments/execution/fargate/fargate_task.py
+++ src/prefect/environments/execution/fargate/fargate_task.py
@@ -254,7 +254,7 @@ class FargateTaskEnvironment(Environment):
                 "environment": [
                     {
                         "name": "PREFECT__CLOUD__AUTH_TOKEN",
-                        "value": config.cloud.agent.auth_token,
+                        "value": config.cloud.auth_token,
                     },
                     {"name": "PREFECT__CONTEXT__FLOW_RUN_ID", "value": flow_run_id},
                     {"name": "PREFECT__CONTEXT__IMAGE", "value": storage.name},
