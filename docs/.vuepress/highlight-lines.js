38bf0ac1d479ba31fc32ddee0b295dbe97556f69
Alex Goodman <alex@prefect.io>
2020-02-07 14:30:47 -0500

    provide flat() alternative implementation

--- docs/.vuepress/highlight-lines.js
+++ docs/.vuepress/highlight-lines.js
@@ -1,6 +1,15 @@
 // currently the build-in plugin for highlight-lines is broken
 const wrapperRE = /^<pre .*?><code>/
 
+// flat is not available in all backends (netlify)
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function (depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+            return flat.concat((Array.isArray(toFlatten) && (depth > 1)) ? toFlatten.flat(depth - 1) : toFlatten);
+        }, []);
+    }
+});
+
 module.exports = md => {
 
     const fence = md.renderer.rules.fence
