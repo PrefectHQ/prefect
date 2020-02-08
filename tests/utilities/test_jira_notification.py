151b3ff5078dfad0ad511032b41bf9e02c6db1c0
Jenny <jenny@prefect.io>
2020-02-07 22:03:06 -0500

    reformat

--- tests/utilities/test_jira_notification.py
+++ tests/utilities/test_jira_notification.py
@@ -33,7 +33,9 @@ def test_jira_notifier_returns_new_state_and_old_state_is_ignored(monkeypatch):
     monkeypatch.setattr("prefect.utilities.jira_notification.JIRA", jira)
     new_state = Failed(message="1", result=0)
     with set_temporary_config({"cloud.use_local_secrets": True}):
-        with prefect.context(secrets=dict(JIRAUSER="", JIRATOKEN="", JIRASERVER="", JIRAPROJECT="")):
+        with prefect.context(
+            secrets=dict(JIRAUSER="", JIRATOKEN="", JIRASERVER="", JIRAPROJECT="")
+        ):
             assert jira_notifier(Task(), "", new_state) is new_state
 
 
@@ -43,17 +45,25 @@ def test_jira_notifier_pulls_creds_from_secret(monkeypatch):
     monkeypatch.setattr("prefect.utilities.jira_notification.JIRA", jira)
     state = Failed(message="1", result=0)
     with set_temporary_config({"cloud.use_local_secrets": True}):
-        with prefect.context(secrets=dict(JIRAUSER="Bob", JIRATOKEN="", JIRASERVER="https://foo/bar", JIRAPROJECT="")):
+        with prefect.context(
+            secrets=dict(
+                JIRAUSER="Bob",
+                JIRATOKEN="",
+                JIRASERVER="https://foo/bar",
+                JIRAPROJECT="",
+            )
+        ):
             jira_notifier(Task(), "", state)
-            
+
         with pytest.raises(ValueError, match="JIRAUSER"):
             jira_notifier(Task(), "", state)
-            
 
         kwargs = jira.call_args[1]
-        assert kwargs == {'basic_auth': ('Bob', ''), 'options': {'server': 'https://foo/bar'}}
+        assert kwargs == {
+            "basic_auth": ("Bob", ""),
+            "options": {"server": "https://foo/bar"},
+        }
 
-        
 
 def test_jira_notifier_ignores_ignore_states(monkeypatch):
     all_states = [
@@ -74,7 +84,14 @@ def test_jira_notifier_ignores_ignore_states(monkeypatch):
     for state in all_states:
         s = state()
         with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(secrets=dict(JIRAUSER="Bob", JIRATOKEN="", JIRASERVER="https://foo/bar", JIRAPROJECT="")):
+            with prefect.context(
+                secrets=dict(
+                    JIRAUSER="Bob",
+                    JIRATOKEN="",
+                    JIRASERVER="https://foo/bar",
+                    JIRAPROJECT="",
+                )
+            ):
                 returned = jira_notifier(Task(), "", s, ignore_states=[State])
         assert returned is s
         assert jira.called is False
@@ -95,7 +112,6 @@ def test_jira_notifier_ignores_ignore_states(monkeypatch):
         Skipped,
     ],
 )
-
 def test_jira_notifier_is_curried_and_ignores_ignore_states(monkeypatch, state):
     state = state()
     client = MagicMock()
@@ -103,8 +119,15 @@ def test_jira_notifier_is_curried_and_ignores_ignore_states(monkeypatch, state):
     monkeypatch.setattr("prefect.utilities.jira_notification.JIRA", jira)
     handler = jira_notifier(ignore_states=[Finished])
     with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(secrets=dict(JIRAUSER="Bob", JIRATOKEN="", JIRASERVER="https://foo/bar", JIRAPROJECT="")):
-                returned = handler(Task(), "", state)
+        with prefect.context(
+            secrets=dict(
+                JIRAUSER="Bob",
+                JIRATOKEN="",
+                JIRASERVER="https://foo/bar",
+                JIRAPROJECT="",
+            )
+        ):
+            returned = handler(Task(), "", state)
     assert returned is state
     assert jira.called is not state.is_finished()
 
@@ -131,8 +154,14 @@ def test_jira_notifier_is_curried_and_uses_only_states(monkeypatch, state):
     monkeypatch.setattr("prefect.utilities.jira_notification.JIRA", jira)
     handler = jira_notifier(only_states=[TriggerFailed])
     with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(secrets=dict(JIRAUSER="Bob", JIRATOKEN="", JIRASERVER="https://foo/bar", JIRAPROJECT="")):
-                returned = handler(Task(), "", state)
+        with prefect.context(
+            secrets=dict(
+                JIRAUSER="Bob",
+                JIRATOKEN="",
+                JIRASERVER="https://foo/bar",
+                JIRAPROJECT="",
+            )
+        ):
+            returned = handler(Task(), "", state)
     assert returned is state
     assert jira.called is isinstance(state, TriggerFailed)
-
