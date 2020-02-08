758faf0d477ff8d43057bf3c2065dfe1088e1bb5
Alex Goodman <alex@prefect.io>
2020-02-08 09:53:24 -0500

    remove notes

--- examples/tutorial/01_etl.py
+++ examples/tutorial/01_etl.py
@@ -1,27 +1,8 @@
 import aircraftlib as aclib
 
-# pull data, store to DB
-# pull data periodically (hourly), append to DB
-# sqlalchemy for ORM to sqlite local DB (build engine, session, and query objects)
-# store raw vectors? or normallize in another way?
-# pandas read to dataframe: df = pd.read_sql(query.statement, query.session.bind)
-
-# ETL:
-#   - progress to parameters:
-#       - Fetch within an area (position, radius, disable)
-#       - Enable/Disable update reference data
-
-# analysis:
-#   - how many AC from each airline are in the sky right now?
-#   - Frequency management via clustering
-#   - Plot current flights (just dots)
-#   - Plot current routes (great circle distance)
-#   - Correlate current flights with known routes
-#   - Plot pariclar AC path over time (requires historical data)
-
 
 def main():
-    # Get the live AC vector data around Dulles airport
+    # Get the live aircraft vector data around Dulles airport
     dulles_airport_position = aclib.Position(lat=38.9519444444, long=-77.4480555556)
     area_surrounding_dulles = aclib.bounding_box(
         position=dulles_airport_position, radius_km=200
@@ -53,17 +34,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# what is a vector
-# goto logs ???
-# small snippits of data
-
-# DE portion:
-# schedules
-# cache & resulthandlers --> this is not useful without a good remote store
-# parallelization (different executor)
-# triggers
-
-
-# DS portion:
-# mapping
