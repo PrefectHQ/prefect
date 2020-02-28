6a4a4148b93b7257f49b442c67ecf4709d5ed951
Jenny <jenny@prefect.io>
2020-02-28 17:56:23 -0500

    remove unused import

--- src/prefect/utilities/jira_notification.py
+++ src/prefect/utilities/jira_notification.py
@@ -19,7 +19,6 @@ except ImportError:
 if TYPE_CHECKING:
     import prefect.engine.state
     import prefect.client
-    from prefect import Flow, Task
 
 TrackedObjectType = Union["Flow", "Task"]
 
