89556de2be8c222771add25541a87b672466181f
Jenny <jenny@prefect.io>
2020-03-02 15:52:14 -0500

    import smtplib

--- src/prefect/utilities/notifications/__init__.py
+++ src/prefect/utilities/notifications/__init__.py
@@ -2,6 +2,7 @@ from prefect.utilities.notifications.notifications import callback_factory
 from prefect.utilities.notifications.notifications import slack_notifier
 from prefect.utilities.notifications.notifications import gmail_notifier
 from prefect.utilities.notifications.notifications import slack_message_formatter
+from prefect.utilities.notifications.notifications import smtplib
 
 try:
     from prefect.utilities.notifications.jira_notification import jira_notifier
