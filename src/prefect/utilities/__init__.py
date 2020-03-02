de142e8e16ad30c41222c65af92db2bcbd241829
Jenny <jenny@prefect.io>
2020-03-02 17:37:29 -0500

    update util init.py

--- src/prefect/utilities/__init__.py
+++ src/prefect/utilities/__init__.py
@@ -4,6 +4,6 @@ import prefect.utilities.configuration
 import prefect.utilities.datetimes
 import prefect.utilities.exceptions
 import prefect.utilities.graphql
-import prefect.utilities.notifications
+import prefect.utilities.notifiers
 import prefect.utilities.serialization
 import prefect.utilities.tasks
