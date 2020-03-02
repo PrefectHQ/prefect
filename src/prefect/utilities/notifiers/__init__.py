71160e6113638649e7ff01762bce7cbd46d6a30a
Jenny <jenny@prefect.io>
2020-03-02 17:56:16 -0500

    stop breaking imports

--- src/prefect/utilities/notifiers/__init__.py
+++
@@ -1,9 +0,0 @@
-from prefect.utilities.notifiers.notifications import callback_factory
-from prefect.utilities.notifiers.notifications import slack_notifier
-from prefect.utilities.notifiers.notifications import gmail_notifier
-from prefect.utilities.notifiers.notifications import slack_message_formatter
-
-try:
-    from prefect.utilities.notifications.jira_notification import jira_notifier
-except ImportError:
-    pass
