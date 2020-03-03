53d0b71179920782efd242f1dd806149652617ae
Jenny <jenny@prefect.io>
2020-03-02 19:05:37 -0500

    no try catch

--- src/prefect/utilities/notifications/__init__.py
+++ src/prefect/utilities/notifications/__init__.py
@@ -3,7 +3,5 @@ from prefect.utilities.notifications.notifications import slack_notifier
 from prefect.utilities.notifications.notifications import gmail_notifier
 from prefect.utilities.notifications.notifications import slack_message_formatter
 
-try:
-    from prefect.utilities.notifications.jira_notification import jira_notifier
-except ImportError:
-    pass
+
+from prefect.utilities.notifications.jira_notification import jira_notifier
