6a70c50479fb3cf61badd938621e2408f50435d8
Alex Goodman <alex@prefect.io>
2020-03-17 15:06:34 -0400

    with review comments

--- tests/engine/result/test_gcs_result.py
+++ tests/engine/result/test_gcs_result.py
@@ -25,16 +25,21 @@ class TestGCSResult:
 
     def test_gcs_init(self, google_client):
         result = GCSResult(bucket="bob")
+        assert result.value == None
         assert result.bucket == "bob"
         assert result.credentials_secret == None
         assert google_client.called is False
         result.gcs_bucket()
         assert google_client.return_value.bucket.call_args[0][0] == "bob"
 
+    def test_gcs_init_with_value(self):
+        result = GCSResult(3, bucket="bob")
+        assert result.value == 3
+
     def test_gcs_writes_to_blob_using_rendered_template_name(self, google_client):
         bucket = MagicMock()
         google_client.return_value.bucket = MagicMock(return_value=bucket)
-        result = GCSResult(bucket="foo", filename_template="{thing}/here.txt")
+        result = GCSResult(bucket="foo", filepath_template="{thing}/here.txt")
         result.value = "so-much-data"
         with prefect.context(thing="42"):
             result.write()
@@ -55,7 +60,7 @@ class TestGCSResult:
         google_client.return_value.bucket = MagicMock(
             return_value=MagicMock(blob=MagicMock(return_value=blob))
         )
-        result = GCSResult(bucket="foo", filename_template="nothing/here.txt")
+        result = GCSResult(bucket="foo", filepath_template="nothing/here.txt")
         result.value = None
         result.write()
         assert blob.upload_from_string.called
