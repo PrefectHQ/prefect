3b2798083b1fdbe80040b867f78f08406d912506
lauralorenz <laura@prefect.io>
2020-03-27 18:43:39 -0400

    Make redirects clearer without double-redirecting.

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -10,17 +10,16 @@ export default ({ router }) => {
       path: '/guide/*',
       redirect: '/core/*'
     },
-
-    // redirect from `cloud/cloud_concepts` through `/cloud/concepts` to `orchestration/concepts`
+    // redirect from `cloud/cloud_concepts` to to `orchestration/concepts`
     {
       path: '/cloud/cloud_concepts/*',
-      redirect: '/cloud/concepts/*'
+      redirect: '/orchestration/concepts/*'
     },
+    // redirect from  `/cloud/concepts` to `orchestration/concepts`
     {
       path: '/cloud/concepts/*',
       redirect: '/orchestration/concepts/*'
     },
-
     // redirect from `api/unreleased` to `api/latest`
     {
       path: '/api/unreleased/*',
@@ -31,19 +30,26 @@ export default ({ router }) => {
       path: '/core/tutorials/*',
       redirect: '/core/advanced_tutorials/*'
     },
-
-    // redirect from `cloud/agent` through `cloud/agents` to `orchestration/agents`
+    // redirect from `cloud/agent` to `orchestration/agents`
     {
       path: '/cloud/agent/*',
-      redirect: '/cloud/agents/*'
+      redirect: '/orchestration/agents/*'
     },
+    // redirect from  `cloud/agents` to `orchestration/agents`
     {
       path: '/cloud/agents/*',
       redirect: '/orchestration/agents/*'
     },
+    // redirect from straight `cloud/` to `orchestration/`
+    {
+      path: '/cloud/',
+      redirect: '/orchestration/'
+    },
+    // redirect from subpages in `cloud` to subpages in `orchestration`
+    {
+      path: '/cloud/*',
+      redirect: '/orchestration/*'
+    },
 
-      // redirect from `cloud` to `orchestration
-    { path: '/cloud/*',
-    redirect: '/orchestration/*'}
   ])
 }
