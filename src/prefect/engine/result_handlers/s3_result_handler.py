f426999100ee156181cf79cf57380c1d19dd4084
Josh Meek <josh@prefect.io>
2020-03-31 14:23:20 -0400

    Use getattr for safe value retrieval of S3ResultHandler _client

--- src/prefect/engine/result_handlers/s3_result_handler.py
+++ src/prefect/engine/result_handlers/s3_result_handler.py
@@ -72,11 +72,7 @@ class S3ResultHandler(ResultHandler):
         Initializes a client if we believe we are in a new thread.
         We consider ourselves in a new thread if we haven't stored a client yet in the current context.
         """
-        if (
-            not prefect.context.get("boto3client")
-            or not hasattr(self, "_client")
-            or not self._client
-        ):
+        if not prefect.context.get("boto3client") or not getattr(self, "_client", None):
             self.initialize_client()
             prefect.context["boto3client"] = self._client
 
