cd60bc69e32b71061015dfbbf7b52f1b4f0b0b24
Zachary Manesiotis <zack.manesiotis@gmail.com>
2020-04-10 19:32:19 -0400

    fix: use == instead of is in comparisons
    Co-Authored-By: lauralorenz <laura@prefect.io>

--- tests/tasks/mysql/test_mysql.py
+++ tests/tasks/mysql/test_mysql.py
@@ -6,7 +6,7 @@ from prefect.tasks.mysql.mysql import MySQLExecute, MySQLFetch
 class TestMySQLExecute:
     def test_construction(self):
         task = MySQLExecute(db_name="test", user="test", password="test", host="test")
-        assert (task.commit is False) and (task.charset is "utf8mb4")
+        assert (task.commit is False) and (task.charset == "utf8mb4")
 
     def test_query_string_must_be_provided(self):
         task = MySQLExecute(db_name="test", user="test", password="test", host="test")
