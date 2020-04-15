d11dcb3def78de7e050d794621536612db3f4dc3
nicholas <znicholasbrown@gmail.com>
2020-04-14 21:33:20 -0400

    Scaffolding changes

--- server/services/ui/shell_scripts/start_server.sh
+++ server/services/ui/shell_scripts/start_server.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+echo "First echo: $PREFECT_SERVER__GRAPHQL_URL"
+
+if [ $PREFECT_SERVER__GRAPHQL_URL != 'http://localhost:4200/graphql' ]
+then
+    echo "Second echo: $PREFECT_SERVER__GRAPHQL_URL"
+    echo "Replacing graphql references with: $PREFECT_SERVER__GRAPHQL_URL"
+    for i in "/var/www/js/*.js"; do
+        echo $i
+        echo "s,http://localhost:4200/graphql,$PREFECT_SERVER__GRAPHQL_URL,g"
+        sed -i -e "s,http://localhost:4200/graphql,$PREFECT_SERVER__GRAPHQL_URL,g" $i
+    done
+fi
+
 echo "👾👾👾 UI running at localhost:8080 👾👾👾"
 
 nginx -g "daemon off;"
