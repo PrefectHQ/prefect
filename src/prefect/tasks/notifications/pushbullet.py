f3cea8557156b1e6d88cd3f23af199e38f999bf0
Jenny <jenny@prefect.io>
2020-04-20 17:07:34 -0400

    add super

--- src/prefect/tasks/notifications/pushbullet.py
+++ src/prefect/tasks/notifications/pushbullet.py
@@ -1,29 +1,25 @@
-
-
 from prefect import Task
 from prefect.client import Secret
 from prefect.utilities.tasks import defaults_from_attrs
+from typing import Any
 
-class SendPushBulletNotification(Task):
+
+class PushBulletTask(Task):
     """
     Task for sending a notification to a mobile phone (or other device) using pushbullet. For this task to function properly,
-    you must have the `"PUSHBULLET_TOKEN"` Prefect Secret set. You can set up a pushbullet account token here: https://www.pushbullet.com/#settings/account
+    you must have the `"PUSHBULLET_TOKEN"` Prefect Secret set. You can set up a pushbullet account and/or get a token here: https://www.pushbullet.com/#settings/account
 
     Args:
         - msg(str, optional):  The message you want to send to your phone; can also be provided at runtime.
+        - **kwargs (Any, optional): additional keyword arguments to pass to the standard Task init method
     """
 
-    def __init__(
-        self,
-        msg: str = None
-    ):
+    def __init__(self, msg: str = None, **kwargs: Any):
         self.msg = msg
+        super().__init__(**kwargs)
 
     @defaults_from_attrs("msg")
-    def run(
-        self,
-        msg: str = None
-    ) -> None:
+    def run(self, msg: str = None) -> None:
         """
         Run method for this Task. Invoked by calling this Task after initialization within a Flow context,
         or by using `Task.bind`.
@@ -33,20 +29,19 @@ class SendPushBulletNotification(Task):
                 at initialization
 
         Raises:
-            - HTTPError: if the POST request returns a non-200 status code
+            - None
 
         Returns:
             - None
         """
 
-        # 'import pushbullet is expensive time-wise, we should do this just-in-time to keep
+        # 'import  is expensive time-wise, we should do this just-in-time to keep
         # the 'import prefect' time low
         from pushbullet import Pushbullet
 
-        pbtoken = Secret("PUSHBULLET_TOKEN")
+        pbtoken = Secret("PUSHBULLET_TOKEN").get()
+
         pb = Pushbullet(pbtoken)
 
-
         ## send the request
-        resp = pb.push_note('Flow Notification', msg)
-        
+        pb.push_note("Flow Notification", msg)
