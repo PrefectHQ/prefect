f0d5b3263b53e2013ee6e9ad8bdaaf5ec8c33154
Jenny <jenny@prefect.io>
2020-04-21 12:46:38 -0400

    add secrets test

--- tests/tasks/notifications/test_pushbullet.py
+++ tests/tasks/notifications/test_pushbullet.py
@@ -11,36 +11,35 @@ pytest.importorskip("pushbullet")
 
 class TestInitialization:
     def test_inits_with_no_args(self):
-        t = PushBulletTask()
+        t = PushbulletTask()
         assert t
 
     def test_kwargs_get_passed_to_task_init(self):
-        t = PushBulletTask(msg="Test", tags=['foo'])
+        t = PushbulletTask(msg="Test", tags=['foo'])
         assert t.msg == "Test"
         assert t.tags == {'foo'}
 
     def test_token_pulled_from_secrets(self, monkeypatch):
-        task = PushBulletTask(msg="test")
+        task = PushbulletTask(msg="test")
         client = MagicMock()
         pushbullet = MagicMock(client=client)
-        # Pushbullet = MagicMock(client=client)
         monkeypatch.setattr("prefect.tasks.notifications.pushbullet_task.Pushbullet", pushbullet)
         with set_temporary_config({"cloud.use_local_secrets": True}):
             with prefect.context(
                 secrets=dict(
                     
-                    PUSHBULLET_TOKEN={"key":42}
+                    PUSHBULLET_TOKEN = 42
                 )
             ):
                 task.run()
-        kwargs = client.call_args[1]
-        assert kwargs == {"PUSHBULLET_TOKEN": "42"}
+        kwargs = pushbullet.call_args[0]
+        assert kwargs == (42,)
 
 
     # def test_kwarg_for_notification_from_get_passed_to_task_init(self, monkeypatch):
     #     smtp = MagicMock()
     #     monkeypatch.setattr("prefect.tasks.notifications.pushbullet.smtplib", smtp)
-    #     t = PushBulletTask(msg="Test")
+    #     t = PushbulletTask(msg="Test")
     #     with set_temporary_config({"cloud.use_local_secrets": True}):
     #         with context({"secrets": dict(PUSHBULLET_TOKEN="foo")}):
     #             res = t.run()
