3907d5302fbe18afd276924c87e250a9b4dc364b
Alex Cano <alexjcano1994@gmail.com>
2020-04-21 08:29:06 -0700

    utility for finding available resource capacity

--- server/src/prefect_server/api/resource_pools.py
+++ server/src/prefect_server/api/resource_pools.py
@@ -7,9 +7,16 @@ from typing import Optional
 
 import pendulum
 import prefect
+from prefect.engine.state import Finished, Running
 from prefect.utilities.graphql import with_args
 
 from prefect_server.database import models
+
+RUNNING_STATES = [
+    state.__name__
+    for state in prefect.engine.state.__dict__.values()
+    if isinstance(state, type) and issubclass(state, (Running))
+]
 
 
 async def create_resource_pool(
@@ -69,3 +76,63 @@ async def delete_resource_pool(resource_pool_id: str) -> bool:
 
     result = await models.ResourcePool.where(id=resource_pool_id).delete()
     return bool(result.affected_rows)
+
+
+async def get_available_resources(
+    resource_pool_id: Optional[str] = None, pool_name: Optional[str] = None
+) -> int:
+    """
+    Determines whether a Resource is available or not and returns
+    the number of available resources.
+
+    Both a `Flow` and a `Task` can take up a Resource, so we
+    count the number of non-completed `FlowRuns` and `TaskRuns`.
+    However, due to needing to decide how to integrate a `Resource` allocation
+    limit with those of `Task.tags`, this won't be checking
+    `TaskRuns` for resource limits.
+
+    Args:
+        - resource_pool_id (Optional[str]): ID of the resource pool
+        - pool_name (Optional[str]): Name of the resource pool
+
+    Raises:
+        - TypeError: If neither `resource_pool_id` or `pool_name` are
+            specified.
+        - ValueError: If a resource pool can't be found with the
+            provided `resource_pool_id` or `pool_name`.
+
+    Returns:
+        - int: Number of available resources in the pool
+    """
+    if resource_pool_id is None and pool_name is None:
+        raise TypeError(
+            "Must specify either resource_pool_id or pool_name. Got None for both."
+        )
+
+    if resource_pool_id is not None:
+        where_clause = {"id": {"_eq": resource_pool_id}}
+        error_message = f"Unable to find resource pool with id: {resource_pool_id}"
+    else:
+        where_clause = {"name": {"_eq": pool_name}}
+        error_message = f"Unable to find resource pool with name: {pool_name}"
+
+    pool = await models.ResourcePool.where(where=where_clause).first(
+        {"id", "name", "slots"}
+    )
+    if pool is None:
+        raise ValueError(error_message)
+
+    # Only count the resource as taken if the flow run is currently
+    # in a running state and also is tagged with the resource.
+    utilized_resources = await models.FlowRun.where(
+        where={
+            "flow": {"settings": {"_contains": {"resources": [pool.name]}}},
+            "state": {"_in": RUNNING_STATES},
+        }
+    ).count()
+
+    available_resources = pool.slots - utilized_resources
+    if available_resources > 0:
+        return available_resources
+    else:
+        return 0
--- server/tests/api/test_resource_pools.py
+++ server/tests/api/test_resource_pools.py
@@ -3,10 +3,12 @@
 
 import asyncio
 import uuid
+from typing import List
 
 import pendulum
 import pytest
 from asynctest import CoroutineMock
+from prefect.engine.state import Running
 
 from prefect_server import api
 from prefect_server.database import models, orm
@@ -31,7 +33,7 @@ async def resource_pool() -> models.ResourcePool:
     pool_id = await api.resource_pools.create_resource_pool(
         "test pool",
         description="A resource pool created from Prefect Server's test suite.",
-        slots=3,
+        slots=1,
     )
 
     populated_pool = await models.ResourcePool.where(id=pool_id).first(
@@ -108,7 +110,7 @@ class TestCreateResourcePool:
 
 
 class TestDeleteResourcePool:
-    async def test_delete_existing(self, resource_pool):
+    async def test_delete_existing(self, resource_pool: models.ResourcePool):
 
         pool_count = await orm.ModelQuery(model=models.ResourcePool).count()
         deleted = await api.resource_pools.delete_resource_pool(resource_pool.id)
@@ -118,7 +120,7 @@ class TestDeleteResourcePool:
 
         assert pool_count == (new_pool_count + 1)
 
-    async def test_delete_bad_id(self, resource_pool):
+    async def test_delete_bad_id(self, resource_pool: models.ResourcePool):
 
         pool_count = await orm.ModelQuery(model=models.ResourcePool).count()
         deleted = await api.resource_pools.delete_resource_pool(uuid.uuid4().hex)
@@ -127,3 +129,135 @@ class TestDeleteResourcePool:
         new_pool_count = await orm.ModelQuery(model=models.ResourcePool).count()
 
         assert pool_count == new_pool_count
+
+
+class TestGetAvailableResources:
+    async def test_raises_error_without_params(
+        self, resource_pool: models.ResourcePool
+    ):
+
+        with pytest.raises(TypeError):
+            available_resources = await api.resource_pools.get_available_resources()
+
+    @pytest.mark.parametrize(
+        "resources", [["bigquery"], ["spark"], ["bigquery", "spark"]]
+    )
+    async def test_contains_in(self, labeled_flow_id: str, resources: List[str]):
+        """
+        This test doesn't necessarily test our code, but it does test
+        that our understanding of how Hasura handles filtering a JSONB
+        field with the structure:
+        {
+            key: [value, value2, value,3]
+        }
+        """
+        result = await api.flows._update_flow_setting(
+            labeled_flow_id, "resources", resources
+        )
+        assert result is True
+        res = await models.Flow.where(
+            {"settings": {"_contains": {"resources": resources}}}
+        ).count()
+        assert res == 1
+
+    async def test_bottoms_out_at_0(
+        self,
+        flow_id: str,
+        flow_run_id: str,
+        flow_run_id_2: str,
+        resource_pool: models.ResourcePool,
+    ):
+        """
+        This test makes sure that in the case of over-allocating
+        resources, we return 0 slots available so we can't
+        over-over-allocate the resource.
+        """
+
+        result = await api.flows._update_flow_setting(
+            flow_id, "resources", [resource_pool.name]
+        )
+        assert result is True
+
+        available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+
+        assert available_resources == 0
+
+        await asyncio.gather(
+            api.states.set_flow_run_state(flow_run_id, Running()),
+            api.states.set_flow_run_state(flow_run_id_2, Running()),
+        )
+
+        available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+
+        assert available_resources == 0
+
+    async def test_only_includes_running_states(
+        self,
+        flow_id: str,
+        flow_run_id: str,
+        flow_run_id_2: str,
+        resource_pool: models.ResourcePool,
+    ):
+        """
+        Tests to make sure that only running states count towards
+        the pool's resource usage.
+        """
+
+        result = await api.flows._update_flow_setting(
+            flow_id, "resources", [resource_pool.name]
+        )
+
+        # Setting the limit higher so we can actually observe the changes
+        await models.ResourcePool.where(id=resource_pool.id).update(set={"slots": 10})
+
+        available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+
+        assert available_resources == 9
+
+        # Now that the flow is running, it should take up a spot in the pool
+        await asyncio.gather(api.states.set_flow_run_state(flow_run_id, Running()))
+
+        new_available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+        # We should have 1 less spot due to the new flow run
+        assert available_resources == (new_available_resources + 1)
+
+    async def test_only_includes_resource_runs(
+        self,
+        flow_id: str,
+        labeled_flow_id: str,
+        labeled_flow_run_id: str,
+        resource_pool: models.ResourcePool,
+    ):
+        """
+        Tests to make sure that only flows that are tagged as using
+        the resource count towards the pool's capacity.
+        """
+        await api.flows._update_flow_setting(flow_id, "resources", [resource_pool.name])
+
+        # Setting the limit higher so we can actually observe the changes
+        await models.ResourcePool.where(id=resource_pool.id).update(set={"slots": 10})
+
+        available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+
+        assert available_resources == 10
+
+        # Marking the flow that _doesn't_ use the pool as running
+        await asyncio.gather(
+            api.states.set_flow_run_state(labeled_flow_run_id, Running())
+        )
+
+        new_available_resources = await api.resource_pools.get_available_resources(
+            resource_pool.id
+        )
+        # No resource pool slots should be taken because it isn't tagged w/ the resource
+        assert available_resources == new_available_resources
