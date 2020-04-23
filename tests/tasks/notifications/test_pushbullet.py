ebb233601e948cb9a1b533f92715d8ba84dc53be
Jenny <jenny@prefect.io>
2020-04-22 21:50:32 -0400

    black....

--- tests/tasks/notifications/test_pushbullet.py
+++ tests/tasks/notifications/test_pushbullet.py
@@ -7,6 +7,7 @@ import pytest
 
 pytest.importorskip("pushbullet")
 
+
 class TestInitialization:
     def test_inits_with_no_args(self):
         t = PushbulletTask()
@@ -29,7 +30,7 @@ class TestInitialization:
                 task.run()
         kwargs = pushbullet.call_args[0]
         assert kwargs == (42,)
-    
+
     def test_raises_if_secret_not_provided(self):
         task = PushbulletTask()
         with pytest.raises(ValueError, match="Secret"):
@@ -46,5 +47,3 @@ class TestInitialization:
             with prefect.context(secrets=dict(PUSHBULLET_TOKEN=42)):
                 with pytest.raises(ValueError, match="message"):
                     task.run()
-
-    
