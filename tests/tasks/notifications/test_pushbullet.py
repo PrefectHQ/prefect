10f3b047c39064bbed319814d33e32f5c152ee6c
Jenny <jenny@prefect.io>
2020-04-22 21:46:25 -0400

    finish tests

--- tests/tasks/notifications/test_pushbullet.py
+++ tests/tasks/notifications/test_pushbullet.py
@@ -3,7 +3,9 @@ import prefect
 from prefect import context
 from prefect.tasks.notifications import PushbulletTask
 from prefect.utilities.configuration import set_temporary_config
+import pytest
 
+pytest.importorskip("pushbullet")
 
 class TestInitialization:
     def test_inits_with_no_args(self):
@@ -27,18 +29,22 @@ class TestInitialization:
                 task.run()
         kwargs = pushbullet.call_args[0]
         assert kwargs == (42,)
+    
+    def test_raises_if_secret_not_provided(self):
+        task = PushbulletTask()
+        with pytest.raises(ValueError, match="Secret"):
+            task.run()
 
-    # def test_kwarg_for_notification_from_get_passed_to_task_init(self, monkeypatch):
-    #     smtp = MagicMock()
-    #     monkeypatch.setattr("prefect.tasks.notifications.pushbullet.smtplib", smtp)
-    #     t = PushbulletTask(msg="Test")
-    #     with set_temporary_config({"cloud.use_local_secrets": True}):
-    #         with context({"secrets": dict(PUSHBULLET_TOKEN="foo")}):
-    #             res = t.run()
-    #     assert smtp.SMTP_SSL.return_value.sendmail.call_args[0][0] == ("test@lvh.me")
+    def test_raises_if_message_not_provided(self, monkeypatch):
+        task = PushbulletTask()
+        client = MagicMock()
+        pushbullet = MagicMock(client=client)
+        monkeypatch.setattr(
+            "prefect.tasks.notifications.pushbullet_task.Pushbullet", pushbullet
+        )
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            with prefect.context(secrets=dict(PUSHBULLET_TOKEN=42)):
+                with pytest.raises(ValueError, match="Secret"):
+                    task.run()
 
-    # def test_kwargs_for_smtp_server_get_passed_to_task_init(self):
-    #     t = EmailTask(smtp_server="mail.lvh.me", smtp_port=587, smtp_type="STARTTLS")
-    #     assert t.smtp_server == "mail.lvh.me"
-    #     assert t.smtp_port == 587
-    #     assert t.smtp_type == "STARTTLS"
+    
