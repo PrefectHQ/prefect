f004dd39226a09a70fc741dd46104d7b0e96d9b8
Chris White <chris@prefect.io>
2020-04-28 11:30:51 -0700

    Add bare bones serializers for new results classes

--- src/prefect/serialization/result.py
+++ src/prefect/serialization/result.py
@@ -1,6 +1,6 @@
 from marshmallow import fields
 
-from prefect.engine import result
+from prefect.engine import result, results
 from prefect.serialization.result_handlers import ResultHandlerSchema
 from prefect.utilities.serialization import JSONCompatible, ObjectSchema, OneOfSchema
 
@@ -28,6 +28,62 @@ class NoResultSchema(ObjectSchema):
 class NORESULTSchema(ObjectSchema):
     class Meta:
         object_class = result._NORESULT
+
+
+class AzureResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.AzureResult
+
+    location = fields.Str(allow_none=True)
+
+
+class ConstantResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.ConstantResult
+
+    location = fields.Str(allow_none=True)
+
+
+class GCSResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.GCSResult
+
+    location = fields.Str(allow_none=True)
+
+
+class LocalResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.LocalResult
+
+    location = fields.Str(allow_none=True)
+
+
+class PrefectResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.PrefectResult
+
+    location = fields.Str(allow_none=True)
+
+
+class S3ResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.S3Result
+
+    location = fields.Str(allow_none=True)
+
+
+class SecretResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.SecretResult
+
+    location = fields.Str(allow_none=True)
+
+
+class ResultHandlerResultSchema(ObjectSchema):
+    class Meta:
+        object_class = results.ResultHandlerResult
+
+    location = fields.Str(allow_none=True)
 
 
 class StateResultSchema(OneOfSchema):
