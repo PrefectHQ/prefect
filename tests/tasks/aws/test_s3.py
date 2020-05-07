600861b7d3f69cdde0759bf3c1e95af0b189f7dd
lauralorenz <laura@prefect.io>
2020-05-06 23:14:19 -0400

    change client tests to be for utility function, and test run methods against AWS api.

--- tests/tasks/aws/test_s3.py
+++ tests/tasks/aws/test_s3.py
@@ -21,38 +21,6 @@ class TestS3Download:
         with pytest.raises(ValueError, match="bucket"):
             task.run(key="")
 
-    def test_creds_are_pulled_from_secret(self, monkeypatch):
-        task = S3Download(bucket="bob")
-        client = MagicMock()
-        boto3 = MagicMock(client=client)
-        monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(
-                secrets=dict(
-                    AWS_CREDENTIALS={"ACCESS_KEY": "42", "SECRET_ACCESS_KEY": "99"}
-                )
-            ):
-                task.run(key="")
-        kwargs = client.call_args[1]
-        assert kwargs == {
-            "aws_access_key_id": "42",
-            "aws_secret_access_key": "99",
-            "aws_session_token": None,
-        }
-
-    def test_creds_default_to_environment(self, monkeypatch):
-        task = S3Download(bucket="bob")
-        client = MagicMock()
-        boto3 = MagicMock(client=client)
-        monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        task.run(key="")
-        kwargs = client.call_args[1]
-        assert kwargs == {
-            "aws_access_key_id": None,
-            "aws_secret_access_key": None,
-            "aws_session_token": None,
-        }
-
 
 class TestS3Upload:
     def test_initialization(self):
@@ -81,35 +49,3 @@ class TestS3Upload:
             ):
                 task.run(data="")
         assert type(client.upload_fileobj.call_args[1]["Key"]) == str
-
-    def test_creds_are_pulled_from_secret(self, monkeypatch):
-        task = S3Upload(bucket="bob")
-        client = MagicMock()
-        boto3 = MagicMock(client=client)
-        monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(
-                secrets=dict(
-                    AWS_CREDENTIALS={"ACCESS_KEY": "42", "SECRET_ACCESS_KEY": "99"}
-                )
-            ):
-                task.run(data="")
-        kwargs = client.call_args[1]
-        assert kwargs == {
-            "aws_access_key_id": "42",
-            "aws_secret_access_key": "99",
-            "aws_session_token": None,
-        }
-
-    def test_creds_default_to_environment(self, monkeypatch):
-        task = S3Upload(bucket="bob")
-        client = MagicMock()
-        boto3 = MagicMock(client=client)
-        monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        task.run(data="")
-        kwargs = client.call_args[1]
-        assert kwargs == {
-            "aws_access_key_id": None,
-            "aws_secret_access_key": None,
-            "aws_session_token": None,
-        }
--- tests/tasks/aws/test_step_function.py
+++ tests/tasks/aws/test_step_function.py
@@ -18,25 +18,15 @@ class TestStepActivate:
         assert task.name == "test"
         assert task.tags == {"AWS"}
 
-    def test_creds_are_pulled_from_secret(self, monkeypatch):
+    def test_exposes_boto3_start_execution_api(self, monkeypatch):
         task = StepActivate(state_machine_arn="arn", execution_name="name")
         client = MagicMock()
         boto3 = MagicMock(client=client)
         monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        with set_temporary_config({"cloud.use_local_secrets": True}):
-            with prefect.context(
-                secrets=dict(
-                    AWS_CREDENTIALS={
-                        "ACCESS_KEY": "42",
-                        "SECRET_ACCESS_KEY": "99",
-                        "SESSION_TOKEN": "1",
-                    }
-                )
-            ):
-                task.run()
-        kwargs = client.call_args[1]
-        assert kwargs == {
-            "aws_access_key_id": "42",
-            "aws_secret_access_key": "99",
-            "aws_session_token": "1",
-        }
+        task.run()
+
+        called_method = client.mock_calls[1]
+        assert called_method[0] == "().start_execution"
+        called_method.assert_called_once_with(
+            {"stateMachineArn": "arn", "name": "name", "input": {}}
+        )
