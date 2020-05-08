06219bc5e62b9ba53e2fb8fe85583ffd2fcf132b
Josh Meek <josh@prefect.io>
2020-05-08 12:06:05 -0400

    Improve tests for S3Result

--- tests/engine/results/test_s3_result.py
+++ tests/engine/results/test_s3_result.py
@@ -54,6 +54,67 @@ class TestS3Result:
         assert used_uri == new_result.location
         assert new_result.location.startswith("yes!/here.txt")
 
+    def test_s3_upload_handles_client_error(self, session):
+        result = S3Result(bucket="foo", location="{thing}/here.txt")
+
+        from botocore.exceptions import ClientError
+
+        used_uri = (
+            session.Session().client.return_value.upload_fileobj.side_effect
+        ) = ClientError({}, {})
+
+        with pytest.raises(ClientError):
+            with prefect.context(
+                secrets=dict(AWS_CREDENTIALS=dict(ACCESS_KEY=1, SECRET_ACCESS_KEY=42)),
+                thing="yes!",
+            ) as ctx:
+                with set_temporary_config({"cloud.use_local_secrets": True}):
+                    new_result = result.write("so-much-data", **ctx)
+
+    def test_s3_result_reads_blob_none_on_eof(self, session):
+        result = S3Result(bucket="foo", location="here.txt")
+
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            new_result = result.read("here.txt")
+
+            download = session.Session().client.return_value.download_fileobj
+
+            assert download.call_args[1]["Bucket"] == "foo"
+            assert download.call_args[1]["Fileobj"]
+            assert download.call_args[1]["Key"] == "here.txt"
+
+            assert new_result.value == None
+
+    def test_s3_result_reads_blob(self, session, monkeypatch):
+        result = S3Result(bucket="foo", location="here.txt")
+
+        monkeypatch.setattr(
+            "prefect.engine.results.s3_result.S3Result.deserialize_from_bytes",
+            MagicMock(return_value="data"),
+        )
+
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            new_result = result.read("here.txt")
+
+            download = session.Session().client.return_value.download_fileobj
+
+            assert download.call_args[1]["Bucket"] == "foo"
+            assert download.call_args[1]["Fileobj"]
+            assert download.call_args[1]["Key"] == "here.txt"
+
+            assert new_result.value == "data"
+
+    def test_s3_result_reads_blob_none(self, session):
+        result = S3Result(bucket="foo", location="here.txt")
+
+        with set_temporary_config({"cloud.use_local_secrets": True}):
+            session.Session().client.return_value.download_fileobj.side_effect = (
+                Exception()
+            )
+
+            with pytest.raises(Exception):
+                new_result = result.read("here.txt")
+
     def test_s3_result_is_pickleable(self, monkeypatch):
         class client:
             def __init__(self, *args, **kwargs):
@@ -112,3 +173,10 @@ class TestS3Result:
         result = S3Result(bucket="bob", location="stuff")
         result = result.format()
         assert result.exists("stuff") == True
+
+    def test_s3_result_exists_handles_unknown_exception(self, session):
+        session.Session().client.return_value.get_object.side_effect = Exception()
+        result = S3Result(bucket="bob", location="stuff")
+        result = result.format()
+        with pytest.raises(Exception):
+            result.exists("stuff")
