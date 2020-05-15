19f8f9db250c5c4c1fb6625594bd841d427b33bf
lauralorenz <laura@prefect.io>
2020-05-15 14:03:17 -0400

    don't anger ast by checking this way

--- src/prefect/agent/agent.py
+++ src/prefect/agent/agent.py
@@ -100,10 +100,9 @@ class Agent:
         self.name = name or config.cloud.agent.get("name", "agent")
 
         self.labels = labels or config.cloud.agent.get("labels", [])
-        try:
+        # quick hack in case config has not been evaluated to a list yet
+        if isinstance(self.labels, str):
             self.labels = ast.literal_eval(self.labels)
-        except ValueError:
-            pass
         self.env_vars = env_vars or config.cloud.agent.get("env_vars", dict())
         self.max_polls = max_polls
         self.log_to_cloud = False if no_cloud_logs else True
