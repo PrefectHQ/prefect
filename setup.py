85d148e7dce4ccb67c21838d33fcb1d7b3a9a03f
Andrew Schechtman-Rook <rook166@gmail.com>
2020-05-19 20:47:29 -0400

    added pandas extra to setup

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ extras = {
     ],
     "jira": ["jira >= 2.0.0"],
     "kubernetes": ["kubernetes >= 9.0.0a1, <= 11.0.0b2", "dask-kubernetes >= 0.8.0"],
+    "pandas": ["pandas >= 0.25.0"],
     "postgres": ["psycopg2-binary >= 2.8.2"],
     "pushbullet": ["pushbullet.py >= 0.11.0"],
     "redis": ["redis >= 3.2.1"],
