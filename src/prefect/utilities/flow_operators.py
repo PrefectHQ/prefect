d365399916bc6158aad6886ee6cf660ddcb1aa0b
gryBox <livni.itay@gmail.com>
2020-06-03 22:34:35 -0500

    spacing

--- src/prefect/utilities/flow_operators.py
+++ src/prefect/utilities/flow_operators.py
@@ -1,7 +1,8 @@
 import prefect
 
+
 def disjoint_union(G: "Flow", H: "Flow", validate: bool = True) -> None:
-        """
+    """
         Take all tasks and edges in another flow and makes a disjointed flow.
         Duplicate parameters and tasks are given a new slug and name.  Using the 
         template `{flow.name}-{task.slug}`.
@@ -19,33 +20,33 @@ def disjoint_union(G: "Flow", H: "Flow", validate: bool = True) -> None:
             - None
         """
 
-        for task in flow.tasks:
+    for task in flow.tasks:
 
-            fl1_tsk = self.get_tasks(task.name)
+        fl1_tsk = self.get_tasks(task.name)
 
-            # Check for duplicate names
-            if len(fl1_tsk) > 0:
+        # Check for duplicate names
+        if len(fl1_tsk) > 0:
 
-                duped_task = fl1_tsk[0]
+            duped_task = fl1_tsk[0]
 
-                print(f"Duplicate task {duped_task}")
+            print(f"Duplicate task {duped_task}")
 
-                task.name = f"{flow.name}-{task.name}"
-                task.slug = f"{flow.name}-{task.slug}"
+            task.name = f"{flow.name}-{task.name}"
+            task.slug = f"{flow.name}-{task.slug}"
 
-                duped_task.name = f"{self.name}-{duped_task.name}"
-                duped_task.slug = f"{self.name}-{duped_task.slug}"
+            duped_task.name = f"{self.name}-{duped_task.name}"
+            duped_task.slug = f"{self.name}-{duped_task.slug}"
 
-            self.add_task(task)
+        self.add_task(task)
 
-        for edge in flow.edges:
-            if edge not in self.edges:
-                self.add_edge(
-                    upstream_task=edge.upstream_task,
-                    downstream_task=edge.downstream_task,
-                    key=edge.key,
-                    mapped=edge.mapped,
-                    validate=validate,
-                )
+    for edge in flow.edges:
+        if edge not in self.edges:
+            self.add_edge(
+                upstream_task=edge.upstream_task,
+                downstream_task=edge.downstream_task,
+                key=edge.key,
+                mapped=edge.mapped,
+                validate=validate,
+            )
 
-        self.constants.update(flow.constants or {})
+    self.constants.update(flow.constants or {})
