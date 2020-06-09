afe66979bdf4f7175a6265e92ac8651528bd843d
Chris White <chris@prefect.io>
2020-06-08 20:12:53 -0700

    Remove old line

--- src/prefect/engine/flow_runner.py
+++ src/prefect/engine/flow_runner.py
@@ -405,7 +405,6 @@ class FlowRunner(Runner):
         with executor.start():
 
             for task in self.flow.sorted_tasks():
-                prefect.context['task_tags'] = task.tags
                 task_state = task_states.get(task)
 
                 # if a task is a constant task, we already know its return value
