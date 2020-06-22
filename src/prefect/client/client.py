3a1b7c9c46c0871ad16912ca51db3eb3e9a44f5c
James Lamb <jaylamb20@gmail.com>
2020-06-22 11:33:59 -0500

    linting

--- src/prefect/client/client.py
+++ src/prefect/client/client.py
@@ -662,7 +662,7 @@ class Client:
         # Set Docker storage image in environment metadata if provided
         if isinstance(flow.storage, prefect.environments.storage.Docker):
             flow.environment.metadata["image"] = flow.storage.name
-            serialized_flow = flow.serialize(build=False)  # type: Any
+            serialized_flow = flow.serialize(build=False)
 
         # If no image ever set, default metadata to all_extras image on current version
         if not flow.environment.metadata.get("image"):
@@ -670,7 +670,7 @@ class Client:
             flow.environment.metadata[
                 "image"
             ] = f"prefecthq/prefect:all_extras-{version}"
-            serialized_flow = flow.serialize(build=False)  # type: Any
+            serialized_flow = flow.serialize(build=False)
 
         # verify that the serialized flow can be deserialized
         try:
