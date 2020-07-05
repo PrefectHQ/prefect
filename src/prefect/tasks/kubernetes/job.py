1bbb69253235aa2a01fd8be97ff47a36e56dc894
Paweł Cieśliński <pcieslinski721@gmail.com>
2020-07-05 11:36:43 +0200

    Implement RunNamespacedJob Task
    This is a task to run Kubernetes jobs. It works as follows: creates
    a job on the cluster based on the specification given in the body,
    then regulary checks in 5-second intervals whether the job has been
    completed and finally removes all its associated resources form
    the cluster.

--- src/prefect/tasks/kubernetes/job.py
+++ src/prefect/tasks/kubernetes/job.py
@@ -1,8 +1,10 @@
+import time
 from typing import Any, cast
 
 from kubernetes import client
 
 from prefect import Task
+from prefect.engine import signals
 from prefect.utilities.tasks import defaults_from_attrs
 from prefect.utilities.kubernetes import get_kubernetes_client
 
@@ -49,7 +51,7 @@ class CreateNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.body = body or {}
         self.namespace = namespace
@@ -139,7 +141,7 @@ class DeleteNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.job_name = job_name
         self.namespace = namespace
@@ -192,7 +194,7 @@ class DeleteNamespacedJob(Task):
             name=job_name,
             namespace=namespace,
             body=client.V1DeleteOptions(**delete_option_kwargs),
-            **kube_kwargs
+            **kube_kwargs,
         )
 
 
@@ -235,7 +237,7 @@ class ListNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.namespace = namespace
         self.kube_kwargs = kube_kwargs or {}
@@ -318,7 +320,7 @@ class PatchNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.job_name = job_name
         self.body = body or {}
@@ -419,7 +421,7 @@ class ReadNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.job_name = job_name
         self.namespace = namespace
@@ -515,7 +517,7 @@ class ReplaceNamespacedJob(Task):
         namespace: str = "default",
         kube_kwargs: dict = None,
         kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
-        **kwargs: Any
+        **kwargs: Any,
     ):
         self.job_name = job_name
         self.body = body or {}
@@ -571,3 +573,134 @@ class ReplaceNamespacedJob(Task):
         api_client.replace_namespaced_job(
             name=job_name, namespace=namespace, body=body, **kube_kwargs
         )
+
+
+class RunNamespacedJob(Task):
+    """
+    Task for running a namespaced job on Kubernetes.
+    This task first creates a job on a Kubernetes cluster according to the specification
+    given in the body, and then regularly checks its status at 5-second intervals.
+    After the job is successfully completed, all resources are deleted: job and the
+    corresponding pods. If job is in the failed status, resources will not be removed
+    from the cluster so that user can check the logs on the cluster.
+
+    Note that all initialization arguments can optionally be provided or overwritten at runtime.
+
+    This task will attempt to connect to a Kubernetes cluster in three steps with
+    the first successful connection attempt becoming the mode of communication with a
+    cluster.
+
+    1. Attempt to use a Prefect Secret that contains a Kubernetes API Key. If
+    `kubernetes_api_key_secret` = `None` then it will attempt the next two connection
+    methods. By default the value is `KUBERNETES_API_KEY` so providing `None` acts as
+    an override for the remote connection.
+    2. Attempt in-cluster connection (will only work when running on a Pod in a cluster)
+    3. Attempt out-of-cluster connection using the default location for a kube config file
+
+    The arguments `body` and `kube_kwargs` will perform an in-place update when the task
+    is run. This means that it is possible to provide `body = {"info": "here"}` at
+    instantiation and then provide `body = {"more": "info"}` at run time which will make
+    `body = {"info": "here", "more": "info"}`. *Note*: Keys present in both instantiation
+    and runtime will be replaced with the runtime value.
+
+    Args:
+        - body (dict, optional): A dictionary representation of a Kubernetes V1Job
+            specification
+        - namespace (str, optional): The Kubernetes namespace to create this job in,
+            defaults to the `default` namespace
+        - kube_kwargs (dict, optional): Optional extra keyword arguments to pass to the
+            Kubernetes API (e.g. `{"pretty": "...", "dry_run": "..."}`)
+        - kubernetes_api_key_secret (str, optional): the name of the Prefect Secret
+            which stored your Kubernetes API Key; this Secret must be a string and in
+            BearerToken format
+        - **kwargs (dict, optional): additional keyword arguments to pass to the Task
+            constructor
+    """
+
+    JOB_STATUS_POOL_INTERVAL = 5
+
+    def __init__(
+        self,
+        body: dict = None,
+        namespace: str = "default",
+        kube_kwargs: dict = None,
+        kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
+        **kwargs: Any,
+    ):
+        self.body = body or {}
+        self.namespace = namespace
+        self.kube_kwargs = kube_kwargs or {}
+        self.kubernetes_api_key_secret = kubernetes_api_key_secret
+
+        super().__init__(**kwargs)
+
+    @defaults_from_attrs(
+        "body", "namespace", "kube_kwargs", "kubernetes_api_key_secret"
+    )
+    def run(
+        self,
+        body: dict = None,
+        namespace: str = "default",
+        kube_kwargs: dict = None,
+        kubernetes_api_key_secret: str = "KUBERNETES_API_KEY",
+    ) -> None:
+        """
+        Task run method.
+
+        Args:
+            - body (dict, optional): A dictionary representation of a Kubernetes V1Job
+                specification
+            - namespace (str, optional): The Kubernetes namespace to create this job in,
+                defaults to the `default` namespace
+            - kube_kwargs (dict, optional): Optional extra keyword arguments to pass to the
+                Kubernetes API (e.g. `{"pretty": "...", "dry_run": "..."}`)
+            - kubernetes_api_key_secret (str, optional): the name of the Prefect Secret
+                which stored your Kubernetes API Key; this Secret must be a string and in
+                BearerToken format
+
+        Raises:
+            - ValueError: if `body` is `None`
+            - ValueError: if `body["metadata"]["name"] is `None`
+        """
+        if not body:
+            raise ValueError("A dictionary representing a V1Job must be provided.")
+
+        body = {**self.body, **(body or {})}
+        kube_kwargs = {**self.kube_kwargs, **(kube_kwargs or {})}
+
+        job_name = body.get("metadata", {}).get("name")
+        if not job_name:
+            raise ValueError(
+                "The job name must be defined in the body under the metadata key."
+            )
+
+        api_client = cast(
+            client.BatchV1Api, get_kubernetes_client("job", kubernetes_api_key_secret)
+        )
+
+        api_client.create_namespaced_job(namespace=namespace, body=body, **kube_kwargs)
+        self.logger.info(f"Job {job_name} has been created.")
+
+        completed = False
+        while not completed:
+            res = api_client.read_namespaced_job_status(
+                name=job_name, namespace=namespace
+            )
+
+            if res.status.active:
+                time.sleep(self.JOB_STATUS_POOL_INTERVAL)
+            else:
+                if res.status.failed:
+                    raise signals.FAIL(
+                        f"Job {job_name} failed, check Kubernetes pod logs for more information."
+                    )
+                elif res.status.succeeded:
+                    self.logger.info(f"Job {job_name} has been completed.")
+                    completed = True
+
+        api_client.delete_namespaced_job(
+            name=job_name,
+            namespace=namespace,
+            body=client.V1DeleteOptions(propagation_policy="Background"),
+        )
+        self.logger.info(f"Job {job_name} has been deleted.")
