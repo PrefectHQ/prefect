b065a5b86d7f40a609371b8622fd3f607a2be9fd
JacksonMaxfield <jmaxfieldbrown@gmail.com>
2020-07-14 11:44:54 -0700

    Add DataFrame serializer and required deps

--- src/prefect/engine/serializers.py
+++ src/prefect/engine/serializers.py
@@ -1,9 +1,10 @@
 import base64
 import json
-from typing import Any
+from io import BytesIO, StringIO
+from typing import Any, Dict
 
 import cloudpickle
-
+import pandas as pd
 
 __all__ = ("Serializer", "PickleSerializer", "JSONSerializer")
 
@@ -107,3 +108,88 @@ class JSONSerializer(Serializer):
             - Any: the deserialized value
         """
         return json.loads(value)
+
+
+class DataFrameSerializer(Serializer):
+    """
+    A Serializer that uses the Pandas DataFrame API to serialize objects.
+
+    Args:
+        - format (str): the serialization format (i.e. CSV, Parquet)
+        - serialize_kwargs: (Dict[str, Any]): Extra kwargs to pass to the Pandas API
+        - deserialize_kwargs: (Dict[str, Any]): Extra kwargs to pass to the Pandas API
+    """
+
+    def _to_csv(df: pd.DataFrame, **kwargs) -> bytes:
+        s = StringIO()
+        df.to_csv(s, **kwargs)
+        return bytes(s.getvalue().encode())
+
+    def _read_csv(value: bytes, **kwargs) -> pd.DataFrame:
+        b = BytesIO(value)
+        return pd.read_csv(b, **kwargs)
+
+    def _to_parquet(df: pd.DataFrame, **kwargs) -> bytes:
+        b = BytesIO()
+        df.to_parquet(b, **kwargs)
+        return b.getvalue()
+
+    def _read_parquet(value: bytes, **kwargs) -> pd.DataFrame:
+        b = BytesIO(value)
+        return pd.read_parquet(b, **kwargs)
+
+    FORMAT_SERDES_LUT = {
+        "csv": {
+            "serialize": _to_csv,
+            "deserialize": _read_csv,
+        },
+        "parquet": {
+            "serialize": _to_parquet,
+            "deserialize": _read_parquet,
+        },
+    }
+
+    def __init__(
+        self,
+        format: str = "csv",
+        serialize_kwargs: Dict[str, Any] = {},
+        deserialize_kwargs: Dict[str, Any] = {},
+    ):
+        super().__init__()
+
+        # Check that the format is in the lookup table
+        if format not in self.FORMAT_SERDES_LUT:
+            raise TypeError(
+                f"Unsupported file format for DataFrameSerializer."
+                f"Provided: {format}. Support formats: {FORMAT_SERDES_LUT.keys()}"
+            )
+
+        # Store format and kwargs
+        self.format_io = self.FORMAT_SERDES_LUT[format]
+        self.serialize_kwargs = serialize_kwargs
+        self.deserialize_kwargs = deserialize_kwargs
+
+    def serialize(self, value: pd.DataFrame) -> bytes:
+        """
+        Serialize the DataFrame to the bytes for the specified format.
+
+        Args:
+            - value (pd.DataFrame): the dataframe to serialize
+
+        Returns:
+            - bytes: the serialized value
+        """
+        return self.format_io["serialize"](value, **self.serialize_kwargs)
+
+
+    def deserialize(self, value: bytes) -> pd.DataFrame:
+        """
+        Deserialize bytes to a DataFrame for the specified format.
+
+        Args:
+            - value (bytes): the value to deserialize
+
+        Returns:
+            - Any: the deserialized DataFrame
+        """
+        return self.format_io["deserialize"](value, **self.deserialize_kwargs)
