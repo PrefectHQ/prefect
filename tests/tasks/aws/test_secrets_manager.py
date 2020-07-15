9f26de21060b14f33a8c489e6969b246c9c5197e
Robin Beer <r.beer@outlook.de>
2020-07-15 22:33:48 +0200

    minor renaming and formatting

--- tests/tasks/aws/test_secrets_manager.py
+++ tests/tasks/aws/test_secrets_manager.py
@@ -3,21 +3,21 @@ from unittest.mock import MagicMock
 import pytest
 
 import prefect
-from prefect.tasks.aws import AWSSecretsManagerTask
+from prefect.tasks.aws import AWSSecretsManager
 from prefect.utilities.configuration import set_temporary_config
 
 
 class TestAWSSecretsManager:
     def test_initialization(self):
-        task = AWSSecretsManagerTask("test")
+        task = AWSSecretsManager("test")
 
     def test_initialization_passes_to_task_constructor(self):
-        task = AWSSecretsManagerTask(secret_name="test", tags=["AWS"])
+        task = AWSSecretsManager(secret_name="test", tags=["AWS"])
         assert task.name == "test"
         assert task.tags == {"AWS"}
 
     def test_raises_if_secret_not_eventually_provided(self):
-        task = AWSSecretsManagerTask(secret_name="test", tags=["AWS"])
+        task = AWSSecretsManager(secret_name="test", tags=["AWS"])
 
         # TODO: I did not understand what this test tests (copied from S3Download)
         with pytest.raises(ValueError, match="secret_name"):
