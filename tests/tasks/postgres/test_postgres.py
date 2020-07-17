a2aad2b8c8f45234a8979101f516cc45e2320ba5
Chris White <chris@prefect.io>
2020-07-16 20:06:29 -0700

    Remove password from init method of postgres tasks

--- tests/tasks/postgres/test_postgres.py
+++ tests/tasks/postgres/test_postgres.py
@@ -6,13 +6,13 @@ from prefect.tasks.postgres import PostgresExecute, PostgresFetch
 class TestPostgresExecute:
     def test_construction(self):
         task = PostgresExecute(
-            db_name="test", user="test", password="test", host="test"
+            db_name="test", user="test", host="test"
         )
         assert task.commit is False
 
     def test_query_string_must_be_provided(self):
         task = PostgresExecute(
-            db_name="test", user="test", password="test", host="test"
+            db_name="test", user="test", host="test"
         )
         with pytest.raises(ValueError, match="A query string must be provided"):
             task.run()
@@ -20,16 +20,16 @@ class TestPostgresExecute:
 
 class TestPostgresFetch:
     def test_construction(self):
-        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", host="test")
         assert task.fetch == "one"
 
     def test_query_string_must_be_provided(self):
-        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", host="test")
         with pytest.raises(ValueError, match="A query string must be provided"):
             task.run()
 
     def test_bad_fetch_param_raises(self):
-        task = PostgresFetch(db_name="test", user="test", password="test", host="test")
+        task = PostgresFetch(db_name="test", user="test", host="test")
         with pytest.raises(
             ValueError,
             match=r"The 'fetch' parameter must be one of the following - \('one', 'many', 'all'\)",
