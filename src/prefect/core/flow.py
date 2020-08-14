5ff0897c044919dfe772c39f6be902fbdc65a029
mastermjr <mastermjr@protonmail.com>
2020-08-14 19:22:28 -0400

    pushing changes to flow, adding in ability to modify flow_run_id locally inside call to `flow.run()`

--- src/prefect/core/flow.py
+++ src/prefect/core/flow.py
@@ -994,6 +994,13 @@ class Flow:
             "context", {}
         ).copy()  # copy to avoid modification
 
+        #set flow_run_id from args or uuid if flow_run_id is not an argument
+        flow_run_id = kwargs.pop("flow_run_id", None)
+        if flow_run_id is not None:
+            flow_run_context.setdefault("flow_run_id", flow_run_id)
+        else:
+            flow_run_context.setdefault("flow_run_id", str(uuid.uuid4()))
+
         # run this flow indefinitely, so long as its schedule has future dates
         while True:
 
@@ -1002,7 +1009,7 @@ class Flow:
             flow_run_context.update(
                 scheduled_start_time=next_run_time,
                 flow_id=self.name,
-                flow_run_id=str(uuid.uuid4()),
+                flow_run_id=flow_run_context['flow_run_id'],
                 flow_run_name=str(uuid.uuid4()),
             )
 
