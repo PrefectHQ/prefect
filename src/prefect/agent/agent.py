e75611a9a4978ba68602b82dfb951c26e9582d39
Josh Meek <josh@prefect.io>
2020-08-27 16:43:08 -0400

    Stop heartbeat thread in agent cleanup

--- src/prefect/agent/agent.py
+++ src/prefect/agent/agent.py
@@ -299,6 +299,10 @@ class Agent:
             # will terminate on exit anyway since it's a daemon thread.
             self._api_server_thread.join(timeout=1)
 
+        if self._heartbeat_thread is not None:
+            self.logger.debug("Stopping heartbeat thread")
+            self._heartbeat_thread.stop()
+
     def run_heartbeat_thread(self) -> None:
         def run() -> None:
             while True:
