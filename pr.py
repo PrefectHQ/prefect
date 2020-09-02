e99fc8c2e31df26f3a9110ef493fb9063d82ee42
Rowan Molony <rowan.molony@codema.ie>
2020-09-02 09:44:27 +0100

    Delete Accidentally Uploaded pr.py

--- pr.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from prefect import task, Flow
-
-task_func = task(lambda: pd.DataFrame())
-undecorated_func = lambda x: x.rename("blah")
-
-with Flow(name="Broken Flow") as flow:
-    task_arg = task_func()
-    undecorated_func(task_arg)
