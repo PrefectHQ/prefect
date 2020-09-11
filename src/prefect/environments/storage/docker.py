72afb38e30cfac5bb3d269f2d6f4f157f1274182
Severin Ryberg <s.ryberg@fz-juelich.de>
2020-09-11 13:07:36 +0200

    Updated docker init docstring

--- src/prefect/environments/storage/docker.py
+++ src/prefect/environments/storage/docker.py
@@ -74,8 +74,8 @@ class Docker(Storage):
             populated with a UUID after build
         - env_vars (dict, optional): a dictionary of environment variables to
             use when building
-        - files (dict, optional): a dictionary of files to copy into the image
-            when building. Takes the format of `{'src': 'dest'}`
+        - files (dict, optional): a dictionary of files or directories to copy into 
+            the image when building. Takes the format of `{'src': 'dest'}`
         - prefect_version (str, optional): an optional branch, tag, or commit
             specifying the version of prefect you want installed into the container;
             defaults to the version you are currently using or `"master"` if your
