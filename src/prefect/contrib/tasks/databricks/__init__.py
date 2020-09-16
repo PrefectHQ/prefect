f5228cc7469d95e1da3e1a96fdc9987252005403
Josh Meek <josh@prefect.io>
2020-09-16 14:33:20 -0400

    Deprecate use of contrib directory

--- src/prefect/contrib/tasks/databricks/__init__.py
+++ src/prefect/contrib/tasks/databricks/__init__.py
@@ -4,3 +4,9 @@ This module contains a collection of tasks for interacting with Databricks resou
 
 from prefect.contrib.tasks.databricks.databricks_submitjob import DatabricksSubmitRun
 from prefect.contrib.tasks.databricks.databricks_submitjob import DatabricksRunNow
+
+import warnings
+
+warnings.warn(
+    "Importing from `prefct.contrib.tasks` has been deprecated and instead should be `prefect.tasks`"
+)
--- src/prefect/tasks/mysql.py/__init__.py
+++ src/prefect/tasks/mysql.py/__init__.py
@@ -2,14 +2,8 @@
 This module contains a collection of tasks for interacting with MySQL databases via
 the pymysql library.
 """
-import warnings
-
 try:
-    from prefect.contrib.tasks.mysql.mysql import MySQLExecute, MySQLFetch
-
-    warnings.warn(
-        "Importing from `prefct.contrib.tasks` has been deprecated and instead should be `prefect.tasks`"
-    )
+    from prefect.tasks.mysql.mysql import MySQLExecute, MySQLFetch
 except ImportError:
     raise ImportError(
         'Using `prefect.contrib.tasks.mysql` requires Prefect to be installed with the "mysql" extra.'
