c643811a3c8efa2a0e531bcecaada5be6026a863
Jim Crist-Harif <jcristharif@gmail.com>
2020-09-30 11:02:18 -0500

    Change run_config serialization to use OneOfSchema
    My petty gripes with marshmallow don't outweigh the value of consistency
    in the codebase.

--- src/prefect/serialization/run_config.py
+++ src/prefect/serialization/run_config.py
@@ -1,58 +1,17 @@
-from typing import Callable
+from marshmallow import fields
 
-from marshmallow import fields, Schema, pre_dump, post_load, ValidationError
-
-from prefect.utilities.serialization import JSONCompatible
-from prefect.run_configs import RunConfig, KubernetesJob
+from prefect.utilities.serialization import JSONCompatible, OneOfSchema, ObjectSchema
+from prefect.run_configs import KubernetesJob
 
 
-class RunConfigSchema(Schema):
-    cls_to_spec_schema = {}
-    kind_to_cls = {}
-
+class RunConfigSchemaBase(ObjectSchema):
     labels = fields.List(fields.String())
-    kind = fields.String()
-    spec = fields.Dict(keys=fields.String())
-
-    @pre_dump
-    def pre_dump(self, obj: RunConfig, **kwargs) -> dict:
-        cls = type(obj)
-        if cls in self.cls_to_spec_schema:
-            schema = self.cls_to_spec_schema[cls]()
-            spec = schema.dump(obj)
-            return {
-                "labels": getattr(obj, "labels", None),
-                "kind": cls.__name__,
-                "spec": spec,
-            }
-        raise ValidationError(f"Don't know how to serialize {cls.__name__} objects")
-
-    @post_load
-    def post_load(self, obj: dict, **kwargs) -> RunConfig:
-        kind = obj.get("kind")
-        if kind is None:
-            raise ValidationError("No `RunConfig` kind found")
-        elif kind not in self.kind_to_cls:
-            raise ValidationError(f"Unknown `RunConfig` kind '{kind}'")
-        cls = self.kind_to_cls[kind]
-        labels = obj.get("labels")
-        spec = obj.get("spec", {})
-        return cls(labels=labels, **spec)
 
 
-def register(run_config_cls: Callable) -> Callable:
-    """Decorator to register a RunConfig schema with its corresponding class."""
+class KubernetesJobSchema(RunConfigSchemaBase):
+    class Meta:
+        object_class = KubernetesJob
 
-    def inner(spec: Callable) -> Callable:
-        RunConfigSchema.cls_to_spec_schema[run_config_cls] = spec
-        RunConfigSchema.kind_to_cls[run_config_cls.__name__] = run_config_cls
-        return spec
-
-    return inner
-
-
-@register(KubernetesJob)
-class KubernetesJobSpecSchema(Schema):
     job_template_path = fields.String(allow_none=True)
     job_template = JSONCompatible(allow_none=True)
     image = fields.String(allow_none=True)
@@ -61,3 +20,9 @@ class KubernetesJobSpecSchema(Schema):
     cpu_request = fields.String(allow_none=True)
     memory_limit = fields.String(allow_none=True)
     memory_request = fields.String(allow_none=True)
+
+
+class RunConfigSchema(OneOfSchema):
+    type_schemas = {
+        "KubernetesJob": KubernetesJobSchema,
+    }
--- tests/serialization/test_run_configs.py
+++ tests/serialization/test_run_configs.py
@@ -2,51 +2,15 @@ import pytest
 
 from marshmallow import ValidationError
 
-from prefect.run_configs import KubernetesJob, RunConfig
+from prefect.run_configs import KubernetesJob
 from prefect.serialization.run_config import RunConfigSchema
 
 
-class Custom(RunConfig):
-    pass
-
-
-def test_run_config_dump_errors_unknown_kind():
-    with pytest.raises(ValidationError, match="Don't know how to serialize"):
-        RunConfigSchema().dump(Custom())
-
-
-def test_run_config_load_errors_missing_kind():
-    with pytest.raises(ValidationError, match="No `RunConfig` kind found"):
-        RunConfigSchema().load({"labels": []})
-
-
-def test_run_config_load_errors_unknown_kind():
-    msg = {"kind": "Custom", "labels": [], "spec": {}}
-    with pytest.raises(ValidationError, match="Unknown `RunConfig` kind 'Custom'"):
-        RunConfigSchema().load(msg)
-
-
-class TestKubernetesJob:
-    def test_no_args(self):
-        config = KubernetesJob()
-        msg = RunConfigSchema().dump(config)
-        assert msg == {
-            "kind": "KubernetesJob",
-            "labels": [],
-            "spec": {
-                "job_template": None,
-                "job_template_path": None,
-                "image": None,
-                "env": None,
-                "cpu_limit": None,
-                "cpu_request": None,
-                "memory_limit": None,
-                "memory_request": None,
-            },
-        }
-
-    def test_full_args(self):
-        config = KubernetesJob(
+@pytest.mark.parametrize(
+    "config",
+    [
+        KubernetesJob(),
+        KubernetesJob(
             job_template_path="s3://bucket/test.yaml",
             image="myimage",
             env={"test": "foo"},
@@ -55,33 +19,29 @@ class TestKubernetesJob:
             memory_limit="4G",
             memory_request="2G",
             labels=["a", "b"],
-        )
-        msg = RunConfigSchema().dump(config)
-        spec = {
-            "job_template_path": "s3://bucket/test.yaml",
-            "job_template": None,
-            "image": "myimage",
-            "env": {"test": "foo"},
-            "cpu_limit": "2",
-            "cpu_request": "500m",
-            "memory_limit": "4G",
-            "memory_request": "2G",
-        }
-        assert msg["kind"] == "KubernetesJob"
-        assert sorted(msg["labels"]) == ["a", "b"]
-        assert msg["spec"] == spec
-        config2 = RunConfigSchema().load(msg)
-        for field in spec:
-            assert getattr(config, field) == getattr(config2, field)
-
-    def test_job_template(self):
-        job_template = {
-            "apiVersion": "batch/v1",
-            "kind": "Job",
-            "metadata": {"labels": {"example": "foo"}},
-        }
-        config = KubernetesJob(job_template=job_template)
-        msg = RunConfigSchema().dump(config)
-        assert msg["spec"]["job_template"] == job_template
-        config2 = RunConfigSchema().load(msg)
-        assert config2.job_template == job_template
+        ),
+        KubernetesJob(
+            job_template={
+                "apiVersion": "batch/v1",
+                "kind": "Job",
+                "metadata": {"labels": {"example": "foo"}},
+            }
+        ),
+    ],
+)
+def test_serialize_kubernetes_job(config):
+    msg = RunConfigSchema().dump(config)
+    config2 = RunConfigSchema().load(msg)
+    assert sorted(config.labels) == sorted(config2.labels)
+    fields = [
+        "job_template",
+        "job_template_path",
+        "image",
+        "env",
+        "cpu_limit",
+        "cpu_request",
+        "memory_limit",
+        "memory_request",
+    ]
+    for field in fields:
+        assert getattr(config, field) == getattr(config2, field)
