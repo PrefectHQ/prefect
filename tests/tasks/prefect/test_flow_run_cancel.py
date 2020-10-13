332a6b910f1869cbd069457a46f0097924f6c595
Mariia Kerimova <mariia@prefect.io>
2020-10-13 19:32:25 -0400

    Remove the test for missing flow id

--- tests/tasks/prefect/test_flow_run_cancel.py
+++ tests/tasks/prefect/test_flow_run_cancel.py
@@ -9,7 +9,7 @@ def test_flow_run_rename_task(monkeypatch):
     client = MagicMock()
     client.cancel_flow_run = MagicMock(return_value=True)
     monkeypatch.setattr(
-        "prefect.tasks.prefect.flow_run_rename.Client", MagicMock(return_value=client)
+        "prefect.tasks.prefect.flow_run_cancel.Client", MagicMock(return_value=client)
     )
     task = CancelFlowRunTask(flow_run_id="id123")
 
@@ -19,12 +19,3 @@ def test_flow_run_rename_task(monkeypatch):
     task.run()
     assert client.cancel_flow_run.called
     assert client.cancel_flow_run.call_args[0][0] == "id123"
-
-
-def test_missing_flow_run_id():
-    task = CancelFlowRunTask()
-    with prefect.context(flow_run_id=None):
-        with pytest.raises(
-            ValueError, match="Can't delete a flow without flow ID. Provide a flow ID."
-        ):
-            task.run()
