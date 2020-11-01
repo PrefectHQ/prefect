620525cfbfd6ce73c9ac119e0f51b82d152d90b8
Juan Calderon-Perez <gabrielcalderon@localhost>
2020-11-01 00:45:31 -0400

    Remove trailing space...

--- src/prefect/tasks/twitter/__init__.py
+++ src/prefect/tasks/twitter/__init__.py
@@ -6,4 +6,4 @@ try:
 except ImportError as exc:
     raise ImportError(
         'Using `prefect.tasks.twitter` requires Prefect to be installed with the "twitter" extra.'
-    ) from exc 
+    ) from exc
