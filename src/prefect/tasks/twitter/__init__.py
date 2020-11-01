8ac65c8c12ec9eb1ef249011843a1710619a2070
Juan Calderon-Perez <835733+gabrielcalderon@users.noreply.github.com>
2020-11-01 00:40:25 -0400

    Update __init__.py

--- src/prefect/tasks/twitter/__init__.py
+++ src/prefect/tasks/twitter/__init__.py
@@ -4,6 +4,6 @@ Tasks for interacting with Twitter.
 try:
     from prefect.tasks.twitter.twitter import LoadTweetReplies
 except ImportError as exc:
-    raise ImportError (
+    raise ImportError(
         'Using `prefect.tasks.twitter` requires Prefect to be installed with the "twitter" extra.'
     ) from exc 
