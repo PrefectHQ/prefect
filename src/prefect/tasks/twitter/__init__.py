80de9595d69cbb55007ab59bb4cfeab645d1cbee
Juan Calderon-Perez <gabrielcalderon@localhost>
2020-11-01 00:29:39 -0400

    Add FROM to explicitly chain exceptions in src/prefect/tasks/twitter

--- src/prefect/tasks/twitter/__init__.py
+++ src/prefect/tasks/twitter/__init__.py
@@ -4,6 +4,6 @@ Tasks for interacting with Twitter.
 try:
     from prefect.tasks.twitter.twitter import LoadTweetReplies
 except ImportError:
-    raise ImportError(
+    raise ImportError as exc (
         'Using `prefect.tasks.twitter` requires Prefect to be installed with the "twitter" extra.'
-    )
+    ) from exc 
