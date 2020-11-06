d2358840e33067c8ae71f4eaf820c5cd49748f03
Mariia Kerimova <mashun4ek@gmail.com>
2020-11-06 17:00:22 -0500

    Add test for task_run_name and modify docstring

--- src/prefect/core/task.py
+++ src/prefect/core/task.py
@@ -206,9 +206,9 @@ class Task(metaclass=SignatureValidator):
             If a callable function is provided, it should have signature `callable(**kwargs) -> str`
             and at write time all formatting kwargs will be passed and a fully formatted location is
             expected as the return value. The callable can be used for string formatting logic that
-            `.format(**kwargs)` doesn't support. The conflict occurred when prefect context 
-            variables(such as `config` or `parameters`, etc.) shadowed task input variable under 
-            the same name. The issue is solved by adding prefect context values if task doesn't 
+            `.format(**kwargs)` doesn't support. The conflict occurred when prefect context
+            variables(such as `config` or `parameters`, etc.) shadowed task input variable under
+            the same name. The issue is solved by adding prefect context values if task doesn't
             have argument of the same name. **Note**: this only works for tasks running against a
             backend API.
 
