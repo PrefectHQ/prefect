b7a5104eb996f2e53ee7ac73242253f5aaf463b5
Choi <phillip.choi@transamerica.com>
2020-12-02 10:31:24 -0600

    Removed List import from bitbucket.py

--- src/prefect/environments/storage/bitbucket.py
+++ src/prefect/environments/storage/bitbucket.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Dict, List
+from typing import TYPE_CHECKING, Any, Dict
 from urllib.error import HTTPError
 
 from prefect.environments.storage import Storage
