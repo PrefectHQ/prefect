fbedf759e44c557f6459d2971e49bcce94243026
Josh Meek <40716964+joshmeek@users.noreply.github.com>
2020-12-11 11:29:53 -0500

    Update src/prefect/agent/kubernetes/agent.py
    Co-authored-by: Jim Crist-Harif <jcrist@users.noreply.github.com>

--- src/prefect/agent/kubernetes/agent.py
+++ src/prefect/agent/kubernetes/agent.py
@@ -232,7 +232,7 @@ class KubernetesAgent(Agent):
                                             dict(
                                                 flow_run_id=flow_run_id,
                                                 name=self.name,
-                                                message="\n".join(pod_event_logs),
+                                                message=log_msg,
                                                 level="DEBUG",
                                                 timestamp=event.last_timestamp.isoformat(),
                                             )
