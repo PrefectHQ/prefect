13fc6603e5b04753807b84d3477eb0cb3fd0afda
Loic Macherel <loic.macherel@gmail.com>
2021-01-20 13:04:34 +0100

    Correct typo and black formatting

--- tests/tasks/gcp/test_gcs_upload_download.py
+++ tests/tasks/gcp/test_gcs_upload_download.py
@@ -139,5 +139,7 @@ class TestRuntimeValidation:
         )
         monkeypatch.setattr("prefect.tasks.gcp.storage.get_storage_client", client)
 
-        with pytest.raises(ValueError, match="data must be str or bytes: got .* instead"):
+        with pytest.raises(
+            ValueError, match="data must be str or bytes: got .* instead"
+        ):
             task.run([1, 2, 3])
