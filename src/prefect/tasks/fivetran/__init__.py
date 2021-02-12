93fa78d7503c7195c407c9ebfcd00e966eac3ffb
Craig Wright <craig@fivetran.com>
2021-02-12 15:46:26 -0800

    Removed unnecessary package check from initialization.

--- src/prefect/tasks/fivetran/__init__.py
+++ src/prefect/tasks/fivetran/__init__.py
@@ -1,9 +1,3 @@
 """
 This module contains a task for starting and monitoring Fivetran connector sync jobs
 """
-try:
-    from prefect.tasks.fivetran.fivetran import FivetranSyncTask
-except ImportError as import_error:
-    raise ImportError(
-        'Using `prefect.tasks.fivetran` requires Prefect to be installed with the "fivetran" extra.'
-    ) from import_error
