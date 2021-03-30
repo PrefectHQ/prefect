3643c5938e6f11a076be44b4975b31863d1c20bd
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2021-03-30 09:15:24 +0200

    Better docstrings and renaming of prefect.tasks.soda to prefect.tasks.sodasql

--- src/prefect/tasks/sodasql/__init__.py
+++ src/prefect/tasks/sodasql/__init__.py
@@ -3,8 +3,8 @@ This module contains a collection of tasks to run Data Quality tests using soda-
 """
 
 try:
-    from prefect.tasks.soda.soda import SodaSQLScan
+    from prefect.tasks.sodasql.sodasql_tasks import SodaSQLScan
 except ImportError as err:
     raise ImportError(
-        'Using `prefect.tasks.soda.soda` requires Prefect to be installed with the "sodasql" extra.'
+        'Using `prefect.tasks.sodasql` requires Prefect to be installed with the "sodasql" extra.'
     ) from err
--- tests/tasks/soda/test_soda.py
+++ tests/tasks/soda/test_soda.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from prefect.tasks.soda import SodaSQLScan
+from prefect.tasks.sodasql import SodaSQLScan
 
 
 class TestSodaSQLScan:
