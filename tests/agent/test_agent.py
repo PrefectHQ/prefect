cf2ef66cb87ad41ca77c4a5d4a9ef443d16308b7
Michael Adkins <michael@prefect.io>
2021-04-05 09:46:39 -0500

    Parametrize max polls test

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -434,82 +434,17 @@ def test_submit_deploy_flow_run_jobs_raises_exception_and_logs(monkeypatch, clou
         assert client.write_run_log.called
 
 
-def test_agent_start_max_polls(monkeypatch, runner_token, cloud_api):
-    on_shutdown = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.on_shutdown", on_shutdown)
+@pytest.mark.parametrize("max_polls", [0, 1, 3])
+def test_agent_start_max_polls(cloud_api, max_polls):
+    agent = Agent(max_polls=max_polls)
+    # Mock the backend API to avoid immediate failure
+    agent._setup_api_connection = MagicMock(return_value="id")
+    # Mock the deployment func to count calls
+    agent._submit_deploy_flow_run_jobs = MagicMock()
 
-    submit_deploy_flow_run_jobs = MagicMock()
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._submit_deploy_flow_run_jobs",
-        submit_deploy_flow_run_jobs,
-    )
-
-    setup_api_connection = MagicMock(return_value="id")
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._setup_api_connection", setup_api_connection
-    )
-
-    heartbeat = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.heartbeat", heartbeat)
-
-    agent = Agent(max_polls=1)
     agent.start()
 
-    assert on_shutdown.called
-    assert submit_deploy_flow_run_jobs.called
-    assert heartbeat.called
-
-
-def test_agent_start_max_polls_count(monkeypatch, runner_token, cloud_api):
-    on_shutdown = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.on_shutdown", on_shutdown)
-
-    submit_deploy_flow_run_jobs = MagicMock()
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._submit_deploy_flow_run_jobs",
-        submit_deploy_flow_run_jobs,
-    )
-
-    setup_api_connection = MagicMock(return_value="id")
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._setup_api_connection", setup_api_connection
-    )
-
-    heartbeat = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.heartbeat", heartbeat)
-
-    agent = Agent(max_polls=2)
-    agent.start()
-
-    assert on_shutdown.call_count == 1
-    assert submit_deploy_flow_run_jobs.call_count == 2
-    assert heartbeat.call_count == 1
-
-
-def test_agent_start_max_polls_zero(monkeypatch, runner_token, cloud_api):
-    on_shutdown = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.on_shutdown", on_shutdown)
-
-    submit_deploy_flow_run_jobs = MagicMock()
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._submit_deploy_flow_run_jobs",
-        submit_deploy_flow_run_jobs,
-    )
-
-    setup_api_connection = MagicMock(return_value="id")
-    monkeypatch.setattr(
-        "prefect.agent.agent.Agent._setup_api_connection", setup_api_connection
-    )
-
-    heartbeat = MagicMock()
-    monkeypatch.setattr("prefect.agent.agent.Agent.heartbeat", heartbeat)
-
-    agent = Agent(max_polls=0)
-    agent.start()
-
-    assert on_shutdown.call_count == 1
-    assert submit_deploy_flow_run_jobs.call_count == 0
-    assert heartbeat.call_count == 1
+    agent._submit_deploy_flow_run_jobs.call_count == max_polls
 
 
 def test_setup_api_connection_attaches_agent_id(cloud_api):
