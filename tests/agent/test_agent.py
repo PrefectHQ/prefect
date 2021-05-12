bda07ccfc40930bbeca8c2b5c6729d9bdc98cefb
Michael Adkins <michael@prefect.io>
2021-05-12 13:45:32 -0500

    Update platform error test message to new log text

--- tests/agent/test_agent.py
+++ tests/agent/test_agent.py
@@ -446,7 +446,7 @@ def test_deploy_flow_run_logs_flow_run_exceptions(monkeypatch, caplog, cloud_api
     client.write_run_logs.assert_called_with(
         [dict(flow_run_id="id", level="ERROR", message="Error Here", name="agent")]
     )
-    assert "Logging platform error for flow run" in caplog.text
+    assert "Encountered exception while deploying flow run id" in caplog.text
 
 
 def test_submit_deploy_flow_run_jobs_raises_exception_and_logs(monkeypatch, cloud_api):
