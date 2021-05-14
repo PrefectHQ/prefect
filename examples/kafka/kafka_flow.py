a496d6970d766c30840a7246dbd18f61940e83d8
Tenzin Choedak <tchoedak@gmail.com>
2021-05-14 14:12:44 -0700

    fix indentation and improve example by consuming all messages with a second consume task

--- examples/kafka/kafka_flow.py
+++ examples/kafka/kafka_flow.py
@@ -1,3 +1,4 @@
+import time
 from prefect.tasks.kafka.kafka import KafkaBatchConsume, KafkaBatchProduce
 from prefect import task, Flow
 
@@ -8,9 +9,9 @@ GROUP_ID = '1'
 
 @task
 def print_results(x):
-	print(messages[:2])
-	print(messages[-2:])
-	print(len(messages))
+    print(f'First two messages: {x[:2]}')
+    print(f'Last two messages: {x[-2:]}')
+    print(f'Total messages: {len(x)}')
 
 
 kafka_consume = KafkaBatchConsume(BOOTSTRAP_SERVER, GROUP_ID)
@@ -19,16 +20,20 @@ kafka_produce = KafkaBatchProduce(BOOTSTRAP_SERVER)
 
 with Flow('Kafka Example') as flow:
 
-	messages = [{'key': str(i), 'value': str(i)} for i in range(30000)]
+    messages = [{'key': str(i), 'value': str(i)} for i in range(30000)]
 
-	kafka_produce.run(
-		topic=TOPIC,
-		messages=messages,
-		threshold=1000,
-	)
+    kafka_produce.run(
+        topic=TOPIC,
+        messages=messages,
+        threshold=1000,
+    )
 
-	messages = kafka_consume.run(topic=[TOPIC], timeout=1.0, message_consume_limit=10000)
-	print_results(messages)
+    time.sleep(5)
 
+    messages = kafka_consume.run(topic=[TOPIC], timeout=1.0, message_consume_limit=10000)
+    print_results(messages)
+
+    remaining_messages = kafka_consume.run(topic=[TOPIC], timeout=1.0)
+    print_results(remaining_messages)
 
 flow.run()
