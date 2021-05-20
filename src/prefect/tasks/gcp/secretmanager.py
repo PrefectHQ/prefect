b0c26ae91570c0cfc27ae66fda11c79fe133f859
Stéphan Taljaard <steptaljaard@gmail.com>
2021-05-20 14:50:53 +0200

    flake8

--- src/prefect/tasks/gcp/secretmanager.py
+++ src/prefect/tasks/gcp/secretmanager.py
@@ -13,13 +13,16 @@ class GCPSecret(SecretBase):
     Note that all initialization arguments can optionally be provided or overwritten at runtime.
 
     For authentication, there are three options: you can set the `GCP_CREDENTIALS` Prefect Secret
-    containing your GCP access keys, or [explicitly provide a credentials dictionary](https://googleapis.dev/python/google-api-core/latest/auth.html#explicit-credentials)
-    , or otherwise it will use [default Google client logic](https://googleapis.dev/python/google-api-core/latest/auth.html).
+    containing your GCP access keys, or [explicitly provide a credentials dictionary]
+    (https://googleapis.dev/python/google-api-core/latest/auth.html#explicit-credentials),
+    or otherwise it will use [default Google client logic]
+    (https://googleapis.dev/python/google-api-core/latest/auth.html).
 
     Args:
         - project_id (Union[str, int], optional): the name of the project where the Secret is saved
         - secret_id (str, optional): the name of the secret to retrieve
-        - version_id (Union[str, int], optional): the version number of the secret to use; defaults to 'latest'
+        - version_id (Union[str, int], optional): the version number of the secret to use;
+            defaults to 'latest'
         - credentials (dict, optional): dictionary containing GCP credentials
         - **kwargs (dict, optional): additional keyword arguments to pass to the
             Task constructor
@@ -53,10 +56,11 @@ class GCPSecret(SecretBase):
         Args:
             - project_id (Union[str, int], optional): the name of the project where the Secret is saved
             - secret_id (str, optional): the name of the secret to retrieve
-            - version_id (Union[str, int], optional): the version number of the secret to use; defaults to "latest"
+            - version_id (Union[str, int], optional): the version number of the secret to use;
+                defaults to "latest"
             - credentials (dict, optional): your GCP credentials passed from an upstream Secret task.
-                If not provided here [default Google client logic](https://googleapis.dev/python/google-api-core/latest/auth.html)
-                will be used.
+                If not provided here [default Google client logic]
+                (https://googleapis.dev/python/google-api-core/latest/auth.html) will be used.
 
         Returns:
             - dict: the contents of this secret, as a dictionary
