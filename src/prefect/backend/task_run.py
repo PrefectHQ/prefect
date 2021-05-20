3dd410c02b736c926b8f9075cfb0d578c1acd12f
Michael Adkins <michael@prefect.io>
2021-05-20 16:31:58 -0500

    Improve edge-case handling during mapped task run result retrieval

--- src/prefect/backend/task_run.py
+++ src/prefect/backend/task_run.py
@@ -83,8 +83,26 @@ class TaskRunView:
 
     def _load_result(self) -> Any:
         if self.state.is_mapped():
-            # Mapped tasks require the state.map_states field to be manually filled
-            # to load results of all mapped subtasks
+            self._load_child_results()
+
+        # Fire the state result hydration
+        self.state.load_result()
+        return self.state.result
+
+    def _load_child_results(self) -> None:
+        """
+        Mapped tasks require `state.map_states` to be manually filled with the states
+        of child tasks for the result to be hydrated correctly
+
+        If the parent flow run is not `Finished`, we'll run into some interesting
+        problems during this retrieval as the mapped children can be uncreated, have
+        null states, or not be finished yet. We attempt to raise helpful errors in this
+        case, but it's not really a supported paradigm.
+        """
+        if not self.state.is_mapped():
+            raise ValueError("Child results cannot be loaded for an unmapped task.")
+
+            # Load all the child task runs
             child_task_runs = [
                 self._from_task_run_data(task_run)
                 for task_run in self._query_for_task_runs(
@@ -96,14 +114,30 @@ class TaskRunView:
                     },
                     # Ensure the returned tasks are ordered matching map indices
                     order_by={"map_index": EnumValue("asc")},
+                    # Handle errors cleanly below
+                    error_on_empty=False,
                 )
             ]
 
-            self.state.map_states = [task_run.state for task_run in child_task_runs]
+        # Raise an informative error if none were found
+        if not child_task_runs:
+            raise ValueError(
+                f"No child task runs were found for task {self.task_slug!r} in "
+                f"flow run {self.flow_run_id}. Is the flow run finished?"
+            )
 
-        # Fire the state result hydration
-        self.state.load_result()
-        return self.state.result
+        # Ensure that all the children are finished
+        for task_run in child_task_runs:
+            if not task_run.state.is_finished():
+                raise ValueError(
+                    f"Child task run {task_run.task_slug}[{task_run.map_index}] is "
+                    f"in state {task_run.state}. The result cannot be retrieved. "
+                    "Loading results for mapped tasks in running flows is not "
+                    "supported."
+                )
+
+            # Update state
+            self.state.map_states = [task_run.state for task_run in child_task_runs]
 
     def iter_mapped(self) -> Iterator["TaskRunView"]:
         """
