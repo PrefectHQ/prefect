c8f8ba2ec22386c1479de0a204576c67c74e7bd4
Zach Angell <zachary.james.angell@gmail.com>
2021-05-21 13:40:55 -0400

    Format

--- src/prefect/backend/__init__.py
+++ src/prefect/backend/__init__.py
@@ -1,6 +1,1 @@
-from prefect.backend.kv_store import (
-    set_key_value,
-    get_key_value,
-    delete_key,
-    list_keys
-)
+from prefect.backend.kv_store import set_key_value, get_key_value, delete_key, list_keys
