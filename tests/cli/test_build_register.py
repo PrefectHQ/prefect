7b8fc1843f0e83b1b79c723d58835c71dd171150
Michael Adkins <michael@prefect.io>
2021-05-24 12:33:21 -0500

    Use `ensure` to try to reduce test brittleness

--- tests/cli/test_build_register.py
+++ tests/cli/test_build_register.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pathlib
 import textwrap
 import threading
 from unittest.mock import MagicMock
@@ -141,10 +142,8 @@ class TestWatchForChanges:
 
         # Later iterations only yield paths that are new or changed
         def update():
-            with open(path3, "wb"):
-                pass
-            with open(path4, "wb"):
-                pass
+            pathlib.Path(path3).ensure(file=True)
+            pathlib.Path(path4).ensure(file=True)
             os.utime(path1)
 
         timer = threading.Timer(0.15, update)
