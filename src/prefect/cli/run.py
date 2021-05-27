634409687b3f94fe228e7676dc1f287c09c8164f
Michael Adkins <michael@prefect.io>
2021-05-27 15:06:08 -0500

    Move `execute` up out of display args

--- src/prefect/cli/run.py
+++ src/prefect/cli/run.py
@@ -424,6 +424,15 @@ See `prefect run --help` for more details on the options.
     ),
     default=None,
 )
+@click.option(
+    "--execute",
+    "-e",
+    help=(
+        "Execute the flow run in this process without an agent. If this process exits, "
+        "the flow run will be marked as 'Failed'."
+    ),
+    is_flag=True,
+)
 # Display settings ---------------------------------------------------------------------
 @click.option(
     "--quiet",
@@ -447,12 +456,6 @@ See `prefect run --help` for more details on the options.
     help="Wait for the flow run to finish executing and display status information.",
     is_flag=True,
 )
-@click.option(
-    "--execute",
-    "-e",
-    help="Execute the flow run in this process without an agent.",
-    is_flag=True,
-)
 @handle_terminal_error
 def run(
     ctx,
@@ -464,12 +467,12 @@ def run(
     labels,
     context_vars,
     params,
+    execute,
     log_level,
     param_file,
     run_name,
     quiet,
     no_logs,
-    execute,
     watch,
 ):
     """Run a flow"""
