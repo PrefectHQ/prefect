f742d55b98b5b724cd293b7f563ab201aa8b1438
Michael Adkins <michael@prefect.io>
2021-06-01 14:42:00 -0500

    Add new tasks to tasks.prefect init

--- src/prefect/tasks/prefect/__init__.py
+++ src/prefect/tasks/prefect/__init__.py
@@ -2,6 +2,11 @@
 Tasks for interacting with the Prefect API
 """
 
+from prefect.tasks.prefect.flow_run import (
+    create_flow_run,
+    wait_for_flow_run,
+    get_task_run_result,
+)
 from prefect.tasks.prefect.flow_run import FlowRunTask, StartFlowRun
 from prefect.tasks.prefect.flow_run_rename import RenameFlowRunTask, RenameFlowRun
 from prefect.tasks.prefect.flow_run_cancel import CancelFlowRunTask, CancelFlowRun
