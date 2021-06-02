94c84a67c4122c2987386a8c935e5da6e34ab514
Michael Adkins <michael@prefect.io>
2021-06-02 10:13:26 -0500

    Remove deprecated `prefect.environments` module

--- src/prefect/environments/execution/fargate/__init__.py
+++
@@ -1,1 +0,0 @@
-from prefect.environments.execution.fargate.fargate_task import FargateTaskEnvironment
--- tests/environments/execution/__init__.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-pytest.importorskip("boto3")
-pytest.importorskip("botocore")
-pytest.importorskip("dask_kubernetes")
-pytest.importorskip("kubernetes")
-pytest.importorskip("yaml")
