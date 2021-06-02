2a8320224399a35688e554048c82a5fdfbea75c2
Michael Adkins <michael@prefect.io>
2021-06-02 11:23:20 -0500

    Oh, there are two `get_flow_image` functions -- deprecate the old looking one
    Also, remove environment handling from the newer one

--- tests/utilities/test_agent.py
+++ tests/utilities/test_agent.py
@@ -1,65 +1,9 @@
 import pytest
 
-from prefect.environments import LocalEnvironment
 from prefect.storage import Docker, Local
 from prefect.run_configs import KubernetesRun, LocalRun
 from prefect.utilities.agent import get_flow_image, get_flow_run_command
 from prefect.utilities.graphql import GraphQLResult
-
-
-def test_get_flow_image_docker_storage():
-    flow_run = GraphQLResult(
-        {
-            "flow": GraphQLResult(
-                {
-                    "storage": Docker(
-                        registry_url="test", image_name="name", image_tag="tag"
-                    ).serialize(),
-                    "environment": LocalEnvironment().serialize(),
-                    "id": "id",
-                }
-            ),
-            "id": "id",
-        }
-    )
-    image = get_flow_image(flow_run=flow_run)
-    assert image == "test/name:tag"
-
-
-def test_get_flow_image_env_metadata():
-    flow_run = GraphQLResult(
-        {
-            "flow": GraphQLResult(
-                {
-                    "storage": Local().serialize(),
-                    "environment": LocalEnvironment(
-                        metadata={"image": "repo/name:tag"}
-                    ).serialize(),
-                    "id": "id",
-                }
-            ),
-            "id": "id",
-        }
-    )
-    image = get_flow_image(flow_run=flow_run)
-    assert image == "repo/name:tag"
-
-
-def test_get_flow_image_raises_on_missing_info():
-    flow_run = GraphQLResult(
-        {
-            "flow": GraphQLResult(
-                {
-                    "storage": Local().serialize(),
-                    "environment": LocalEnvironment().serialize(),
-                    "id": "id",
-                }
-            ),
-            "id": "id",
-        }
-    )
-    with pytest.raises(ValueError):
-        get_flow_image(flow_run=flow_run)
 
 
 @pytest.mark.parametrize("run_config", [KubernetesRun(), LocalRun(), None])
