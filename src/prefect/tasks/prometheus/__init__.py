31b047f5a4c7e8bd2c934dc2882f56f5bc4c690c
David Zucker <davzucky@hotmail.com>
2021-06-10 08:19:08 +0800

    Remove print error statement

--- src/prefect/tasks/prometheus/__init__.py
+++ src/prefect/tasks/prometheus/__init__.py
@@ -4,7 +4,6 @@ Tasks for interacting with Prometheus. The main task are using pushgateway
 try:
     from prefect.tasks.prometheus.pushgateway import PushGaugeToGateway, PushMode
 except ImportError as err:
-    print(err)
     raise ImportError(
         'Using `prefect.tasks.prometheus` requires Prefect to be installed with the "prometheus" extra.'
     ) from err
