a4d3006787c9bb875b582a029ad09536029c7097
Michael Adkins <michael@prefect.io>
2021-06-11 15:02:23 -0500

    Revert "Add backwards compatibility for `Flow.environment` to flow diagnostics"
    This reverts commit 490b908c4705d9067f112eefe8009dc011dbec6a.

--- src/prefect/utilities/diagnostics.py
+++ src/prefect/utilities/diagnostics.py
@@ -121,9 +121,7 @@ def flow_information(flow: "prefect.Flow") -> dict:
         return True
 
     # Check presence of environment attributes
-    # These removed in 0.15.0 but we will include them in a diagnostics check until
-    # 0.16.0 for debugging purposes
-    if hasattr(flow, "environment"):
+    if flow.environment:
         environment = {
             "type": type(flow.environment).__name__,  # type: ignore
             **_replace_values(flow.environment.__dict__),  # type: ignore
