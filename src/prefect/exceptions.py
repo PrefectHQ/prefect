67f95450e62e472f0db79d571e4ce1d45e522b77
Michael Adkins <michael@prefect.io>
2021-06-16 12:48:09 -0500

    Reverse order of inheritance so users can capture new exceptions with old name

--- src/prefect/exceptions.py
+++ src/prefect/exceptions.py
@@ -1,3 +1,12 @@
+# Import old exceptions for compatibility
+from prefect.utilities.exceptions import (
+    PrefectError,
+    ClientError as ClientError_,
+    AuthorizationError as AuthorizationError_,
+    StorageError,
+)
+
+
 class PrefectSignal(BaseException):
     """
     Signals inherit from `BaseException` and will not be caught by normal error
@@ -14,6 +23,8 @@ class VersionLockMismatchSignal(PrefectSignal):
     """
     Raised when version locking is enabled and a task run state version sent to Cloud
     does not match the version expected by the server.
+
+    This is not backwards compatible with `prefect.utilities.exceptions.VersionLockError`
     """
 
     pass
@@ -22,20 +33,24 @@ class VersionLockMismatchSignal(PrefectSignal):
 class TaskTimeoutSignal(PrefectSignal):
     """
     Raised when a task reaches a timeout limit
+
+    This is not backwards compatible with `prefect.utilities.exceptions.TaskTimeoutError`
     """
 
     pass
 
 
-class PrefectException(Exception):
+class PrefectException(PrefectError):
     """
     The base exception type for all Prefect related exceptions
     """
 
+    # NOTE: Should be updated to inherit from `Exception` when `PrefectError` is removed
+
     pass
 
 
-class ClientError(PrefectException):
+class ClientError(PrefectException, ClientError_):
     """
     Raised when there is error in Prefect Client <-> Server communication
     """
@@ -43,7 +58,7 @@ class ClientError(PrefectException):
     pass
 
 
-class AuthorizationError(ClientError):
+class AuthorizationError(ClientError, AuthorizationError_):
     """
     Raised when there is an issue authorizing with Prefect Cloud
     """
@@ -51,7 +66,7 @@ class AuthorizationError(ClientError):
     pass
 
 
-class FlowStorageError(PrefectException):
+class FlowStorageError(PrefectException, StorageError):
     """
     Raised when there is an error loading a flow from storage
     """
--- src/prefect/utilities/exceptions.py
+++ src/prefect/utilities/exceptions.py
@@ -1,41 +1,30 @@
 """
 The exception types in this module have been deprecated in favor of `prefect.exceptions`
 
-They all inherit from the new exception types for backwards compatibility for now
-
 Users should not be using these directly but we leave these for a version in case they
 are being used in try/except clauses
 """
 
 import warnings
 
-# Import new exceptions for compat inheritance
-from prefect.exceptions import (
-    PrefectException,
-    TaskTimeoutSignal,
-    ClientError as ClientError_,
-    VersionLockMismatchSignal,
-    AuthorizationError as AuthorizationError_,
-    FlowStorageError,
-)
 
-
-class PrefectError(PrefectException):
+class PrefectError(Exception):
     def __init__(self, *args: object) -> None:
-        warnings.warn(
-            "`prefect.utilities.exceptions.PrefectError` has been moved to "
-            "`prefect.exceptions.PrefectException` and will be removed in a future "
-            "release. Please update your imports.",
-            stacklevel=2,
-        )
+        if type(self) == PrefectError:
+            warnings.warn(
+                "`prefect.utilities.exceptions.PrefectError` has been moved to "
+                "`prefect.exceptions.PrefectException` and will be removed in a future "
+                "release. Please update your imports.",
+                stacklevel=2,
+            )
         super().__init__(*args)
 
 
-class TaskTimeoutError(PrefectError, TaskTimeoutSignal):
+class TaskTimeoutError(PrefectError):
     def __init__(self, *args: object) -> None:
         warnings.warn(
-            "`prefect.utilities.exceptions.PrefectError` has been moved to "
-            "`prefect.exceptions.PrefectException` and will be removed in a future "
+            "`prefect.utilities.exceptions.TaskTimeoutError` has been moved to "
+            "`prefect.exceptions.TaskTimeoutSignal` and will be removed in a future "
             "release. Please update your imports.",
             stacklevel=2,
         )
@@ -64,26 +53,28 @@ class SerializationError(PrefectError):
 
 class PrefectWarning(UserWarning):
     def __init__(self, *args: object) -> None:
-        warnings.warn(
-            "`prefect.utilities.exceptions.PrefectWarning` has been deprecated "
-            "and will be removed in a future release.",
-            stacklevel=2,
-        )
+        if type(self) == PrefectWarning:
+            warnings.warn(
+                "`prefect.utilities.exceptions.PrefectWarning` has been deprecated "
+                "and will be removed in a future release.",
+                stacklevel=2,
+            )
         super().__init__(*args)
 
 
-class ClientError(PrefectError, ClientError_):
+class ClientError(PrefectError):
     def __init__(self, *args: object) -> None:
-        warnings.warn(
-            "`prefect.utilities.exceptions.ClientError` has been moved to "
-            "`prefect.exceptions.ClientError` and will be removed in a future "
-            "release. Please update your imports.",
-            stacklevel=2,
-        )
+        if type(self) == ClientError:
+            warnings.warn(
+                "`prefect.utilities.exceptions.ClientError` has been moved to "
+                "`prefect.exceptions.ClientError` and will be removed in a future "
+                "release. Please update your imports.",
+                stacklevel=2,
+            )
         super().__init__(*args)
 
 
-class VersionLockError(PrefectError, VersionLockMismatchSignal):
+class VersionLockError(PrefectError):
     def __init__(self, *args: object) -> None:
         warnings.warn(
             "`prefect.utilities.exceptions.VersionLockError` has been moved to "
@@ -94,23 +85,25 @@ class VersionLockError(PrefectError, VersionLockMismatchSignal):
         super().__init__(*args)
 
 
-class AuthorizationError(ClientError, AuthorizationError_):
+class AuthorizationError(ClientError):
     def __init__(self, *args: object) -> None:
-        warnings.warn(
-            "`prefect.utilities.exceptions.AuthorizationError` has been moved to "
-            "`prefect.exceptions.AuthorizationError` and will be removed in a future "
-            "release. Please update your imports.",
-            stacklevel=2,
-        )
+        if type(self) == AuthorizationError:
+            warnings.warn(
+                "`prefect.utilities.exceptions.AuthorizationError` has been moved to "
+                "`prefect.exceptions.AuthorizationError` and will be removed in a "
+                "future release. Please update your imports.",
+                stacklevel=2,
+            )
         super().__init__(*args)
 
 
-class StorageError(PrefectError, FlowStorageError):
+class StorageError(PrefectError):
     def __init__(self, *args: object) -> None:
-        warnings.warn(
-            "`prefect.utilities.exceptions.StorageError` has been moved to "
-            "`prefect.exceptions.FlowStorageError` and will be removed in a future "
-            "release. Please update your imports.",
-            stacklevel=2,
-        )
+        if type(self) == StorageError:
+            warnings.warn(
+                "`prefect.utilities.exceptions.StorageError` has been moved to "
+                "`prefect.exceptions.FlowStorageError` and will be removed in a future "
+                "release. Please update your imports.",
+                stacklevel=2,
+            )
         super().__init__(*args)
