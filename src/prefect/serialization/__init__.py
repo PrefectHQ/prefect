cddb6dce7bee52a57f03d3507f7dcfaad6afecac
Michael Adkins <michael@prefect.io>
2021-06-16 09:44:08 -0500

    Restore serialization.environment import

--- src/prefect/serialization/__init__.py
+++ src/prefect/serialization/__init__.py
@@ -1,7 +1,7 @@
 import prefect.serialization.schedule
 import prefect.serialization.task
 import prefect.serialization.edge
-
+import prefect.serialization.environment
 import prefect.serialization.flow
 import prefect.serialization.state
 import prefect.serialization.storage
