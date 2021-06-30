a143f0b037dcb95fdf43e2f8bb3cca4a03db46aa
Michael Adkins <madkinszane@gmail.com>
2021-06-30 20:32:54 +0000

    Fix link to 0.14.22 docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,7 +81,7 @@ module.exports = {
         text: 'API Reference',
         items: [
           { text: 'Latest (0.15.0)', link: '/api/latest/' },
-          { text: '0.14.21', link: '/api/0.14.21/' },
+          { text: '0.14.22', link: '/api/0.14.22/' },
           { text: '0.13.19', link: '/api/0.13.19/' },
           { text: '0.12.6', link: '/api/0.12.6/' },
           { text: '0.11.5', link: '/api/0.11.5/' },
