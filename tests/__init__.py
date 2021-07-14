532af398167fe9d341bc9d1f3d72e4c3fd21cbec
Michael Adkins <michael@prefect.io>
2021-07-14 13:46:50 -0500

    Add placeholder test

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+def test_testing_environment():
+    # Added since pytest will error when no tests are collected; can be used to confirm
+    # that our test environment flags are set and such
+    pass
