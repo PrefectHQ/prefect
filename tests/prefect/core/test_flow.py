c4cac86e10fcc13f0aa7be144fbe0bc1035efa0d
Chris White <chris@prefect.io>
2021-07-14 19:25:24 -0700

    Add another init test

--- tests/prefect/core/test_flow.py
+++ tests/prefect/core/test_flow.py
@@ -1,20 +1,28 @@
+import pytest
+
 from prefect import flow
 from prefect.core import Flow
 
 
-def test_flow_class_initializes():
-    f = Flow(name="test", fn=lambda **kwargs: 42, version="A")
-    assert f.name == "test"
-    assert f.fn() == 42
-    assert f.version == "A"
+class TestFlow:
+    def test_initializes(self):
+        f = Flow(name="test", fn=lambda **kwargs: 42, version="A")
+        assert f.name == "test"
+        assert f.fn() == 42
+        assert f.version == "A"
+
+    def test_raises_on_bad_funcs(self):
+        with pytest.raises(TypeError):
+            Flow(name="test", fn={})
 
 
-def test_flow_decorator_initializes():
-    @flow(name="foo", version="B")
-    def my_flow():
-        return "bar"
+class TestDecorator:
+    def test_flow_decorator_initializes(self):
+        @flow(name="foo", version="B")
+        def my_flow():
+            return "bar"
 
-    assert isinstance(my_flow, Flow)
-    assert my_flow.name == "foo"
-    assert my_flow.version == "B"
-    assert my_flow.fn() == "bar"
+        assert isinstance(my_flow, Flow)
+        assert my_flow.name == "foo"
+        assert my_flow.version == "B"
+        assert my_flow.fn() == "bar"
