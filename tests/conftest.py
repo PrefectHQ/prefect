460ac8dd365355c475669d3ee4f02265a587ab47
Zach Angell <zachary.james.angell@gmail.com>
2021-07-15 13:53:01 -0400

    Add support for async tests

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+import inspect
+
+
+def pytest_collection_modifyitems(session, config, items):
+    """
+    Modify tests prior to execution
+    """
+    for item in items:
+        # automatically add @pytest.mark.asyncio to async tests
+        if isinstance(item, pytest.Function) and inspect.iscoroutinefunction(
+            item.function
+        ):
+            item.add_marker(pytest.mark.asyncio)
+
+        # if the item is in a deprecated subdirectory, mark it as deprecated
+        if "deprecated/" in str(item.fspath):
+            item.add_marker(pytest.mark.deprecated)
--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -2,3 +2,9 @@ def test_testing_environment():
     # Added since pytest will error when no tests are collected; can be safely removed
     # in the near future
     pass
+
+
+async def test_testing_environment_async():
+    # Added since pytest will error when no tests are collected; can be safely removed
+    # in the near future
+    pass
