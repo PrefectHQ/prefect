38913c15ed226fad6efa4d5cd7c370e8f1469c8c
Chris White <chris@prefect.io>
2021-07-16 14:00:09 -0700

    Add test for nonexistent path

--- tests/prefect/core/test_utilities.py
+++ tests/prefect/core/test_utilities.py
@@ -1,5 +1,14 @@
-from prefect.core.utilites import file_hash
+import pytest
+
+from prefect.core.utilities import file_hash
 
 
 def test_file_hash_returns_hash():
     assert isinstance(file_hash(__file__), str)
+
+
+def test_file_hash_raises_if_path_doesnt_exist():
+    fake_path = "/root/foo/bar.txt"
+
+    with pytest.raises(FileNotFoundError, match="/root/foo/bar.txt"):
+        file_hash(path=fake_path)
