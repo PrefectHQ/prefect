f0cc044da81a5af1bc268bfd8c022fab37ceae6b
Zach Angell <zachary.james.angell@gmail.com>
2021-07-16 13:13:22 -0400

    Cleanup fixtures

--- tests/prefect/orion/api/test_flows.py
+++ tests/prefect/orion/api/test_flows.py
@@ -1,11 +1,6 @@
 class TestCreateFlow:
-    async def test_create_flow(self, test_client):
-        flow_data = {
-            "name": "my-flow"
-        }
-        response = await test_client.post(
-            "/flows/",
-            json=flow_data
-        )
+    async def test_create_flow(self, client):
+        flow_data = {"name": "my-flow"}
+        response = await client.post("/flows/", json=flow_data)
         assert response.status_code == 200
         assert response.json()["name"] == "my-flow"
--- tests/prefect/orion/fixtures/database_fixtures.py
+++ tests/prefect/orion/fixtures/database_fixtures.py
@@ -7,7 +7,7 @@ from prefect.orion.utilities.database import reset_db
 
 @pytest.fixture(autouse=True, scope="function")
 async def database_engine():
-    """ Creates an in memory sqlite database for use in testing """
+    """Creates an in memory sqlite database for use in testing"""
     try:
         # create an in memory db engine
         engine = create_async_engine("sqlite+aiosqlite://", echo=True)
@@ -18,10 +18,13 @@ async def database_engine():
         # TODO - do we need to delete anything or clean stuff up?
         pass
 
+
 @pytest.fixture(autouse=True, scope="function")
 async def database_session(database_engine):
     try:
-        async_session = sessionmaker(database_engine, future=True, expire_on_commit=False, class_=AsyncSession)
+        async_session = sessionmaker(
+            database_engine, future=True, expire_on_commit=False, class_=AsyncSession
+        )
         async with async_session.begin() as session:
             yield session
     finally:
