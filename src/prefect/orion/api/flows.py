98ba8e90a01eebd0040a27453990b5bc60a57ec0
Zach Angell <zachary.james.angell@gmail.com>
2021-07-17 09:58:00 -0400

    Delete flow route

--- src/prefect/orion/api/flows.py
+++ src/prefect/orion/api/flows.py
@@ -6,7 +6,7 @@ from prefect.orion import models
 import sqlalchemy as sa
 
 
-@app.post("/flows/", response_model=schemas.Flow)
+@app.post("/flows/", response_model=schemas.Flow, status_code=201)
 async def create_flow(
     flow: schemas.Flow, session: sa.orm.Session = Depends(get_session)
 ):
@@ -19,3 +19,11 @@ async def read_flow(flow_id: str, session: sa.orm.Session = Depends(get_session)
     if not flow:
         raise HTTPException(status_code=404, detail="Flow not found")
     return flow
+
+
+@app.delete("/flows/{flow_id}", status_code=204)
+async def delete_flow(flow_id: str, session: sa.orm.Session = Depends(get_session)):
+    result = await models.flows.delete_flow(session=session, id=flow_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="Flow not found")
+    return result
--- tests/prefect/orion/api/test_flows.py
+++ tests/prefect/orion/api/test_flows.py
@@ -5,7 +5,7 @@ class TestCreateFlow:
     async def test_create_flow(self, client):
         flow_data = {"name": "my-flow"}
         response = await client.post("/flows/", json=flow_data)
-        assert response.status_code == 200
+        assert response.status_code == 201
         assert response.json()["name"] == "my-flow"
 
 
@@ -14,10 +14,10 @@ class TestReadFlow:
         # first create a flow to read
         flow_data = {"name": "my-flow"}
         response = await client.post("/flows/", json=flow_data)
-        assert response.status_code == 200
+        assert response.status_code == 201
         flow_id = response.json()["id"]
 
-        # make surwe we can read the flow correctly
+        # make sure we we can read the flow correctly
         response = await client.get(f"/flows/{flow_id}")
         assert response.status_code == 200
         assert response.json()["id"] == flow_id
@@ -25,4 +25,25 @@ class TestReadFlow:
 
     async def test_read_flow_returns_404_if_does_not_exist(self, client):
         response = await client.get(f"/flows/{uuid4()}")
+        assert response.status_code == 404
+
+
+class TestDeleteFlow:
+    async def test_delete_flow(self, client):
+        # first create a flow to delete
+        flow_data = {"name": "my-flow"}
+        response = await client.post("/flows/", json=flow_data)
+        assert response.status_code == 201
+        flow_id = response.json()["id"]
+
+        # delete the flow
+        response = await client.delete(f"/flows/{flow_id}")
+        assert response.status_code == 204
+
+        # make sure it's deleted
+        response = await client.get(f"/flows/{flow_id}")
+        assert response.status_code == 404
+
+    async def test_delete_flow_returns_404_if_does_not_exist(self, client):
+        response = await client.delete(f"/flows/{uuid4()}")
         assert response.status_code == 404
