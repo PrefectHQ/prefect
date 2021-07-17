77e5ec0983aba4f9f7109aa5dbbb30a887889db7
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-17 11:46:01 -0400

    Use OrionRouter for modularity

--- src/prefect/orion/utilities/server.py
+++ src/prefect/orion/utilities/server.py
@@ -1,3 +1,8 @@
+from enum import Enum
+from typing import TYPE_CHECKING, Any, Callable, get_type_hints
+
+import fastapi
+
 from prefect.orion.utilities.database import OrionAsyncSession
 
 
@@ -10,3 +15,17 @@ async def get_session():
     """
     async with OrionAsyncSession.begin() as session:
         yield session
+
+
+class OrionRouter(fastapi.APIRouter):
+    """
+    Allows API routers to use type annotations for `response_model` if not
+    provided explicitly. Inspired by https://github.com/dmontagu/fastapi-utils.
+    """
+
+    def add_api_route(
+        self, path: str, endpoint: Callable[..., Any], **kwargs: Any
+    ) -> None:
+        if kwargs.get("response_model") is None:
+            kwargs["response_model"] = get_type_hints(endpoint).get("return")
+        return super().add_api_route(path, endpoint, **kwargs)
