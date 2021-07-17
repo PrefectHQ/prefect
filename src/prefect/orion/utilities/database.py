1a6f0f5b1df6b230d1a19cac64cb8dd1f724bdb4
Zach Angell <zachary.james.angell@gmail.com>
2021-07-17 11:37:58 -0400

    Use database settings

--- src/prefect/orion/utilities/database.py
+++ src/prefect/orion/utilities/database.py
@@ -11,9 +11,13 @@ from sqlalchemy.sql.functions import FunctionElement
 from sqlalchemy.types import CHAR, TypeDecorator
 from prefect.orion.utilities.settings import Settings
 
+SETTINGS = Settings()
+
 camel_to_snake = re.compile(r"(?<!^)(?=[A-Z])")
 
-engine = create_async_engine("sqlite+aiosqlite:////tmp/orion.db", echo=True)
+engine = create_async_engine(
+    SETTINGS.database.connection_url, echo=SETTINGS.database.echo
+)
 OrionAsyncSession = sessionmaker(
     engine, future=True, expire_on_commit=False, class_=AsyncSession
 )
