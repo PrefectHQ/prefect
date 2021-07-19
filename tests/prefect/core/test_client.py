0d6980c5d704fede5fcedd843a29452923026bd4
Michael Adkins <michael@prefect.io>
2021-07-19 13:19:40 -0500

    Sketch `Client` flow run methods

--- tests/prefect/core/test_client.py
+++ tests/prefect/core/test_client.py
@@ -24,3 +24,17 @@ async def test_create_then_read_flow(user_client):
     assert lookup.name == foo.name
     assert lookup.tags == list(foo.tags)
     assert lookup.parameters == foo.parameters
+
+
+async def test_create_then_read_flow_run(user_client):
+    @flow
+    def foo():
+        pass
+
+    flow_run_id = await user_client.create_flow_run(foo)
+    assert isinstance(flow_run_id, str)
+
+    lookup = await user_client.read_flow_run(flow_run_id)
+    assert isinstance(lookup, schemas.FlowRun)
+    assert lookup.tags == list(foo.tags)
+    assert lookup.parameters == {}
