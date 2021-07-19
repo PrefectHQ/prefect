212e0643dfc42e934e57cfb6a0aec6d3285321a0
Michael Adkins <michael@prefect.io>
2021-07-19 12:59:35 -0500

    Use existing orion fixtures; restore to async; nest the clients

--- src/prefect/core/client.py
+++ src/prefect/core/client.py
@@ -1,3 +1,5 @@
+import httpx
+
 from typing import TYPE_CHECKING
 
 from prefect.orion import api
@@ -7,32 +9,33 @@ if TYPE_CHECKING:
 
 
 class Client:
-    def __init__(self, host_url: str) -> None:
-        self.host_url = host_url
+    def __init__(
+        self, base_url: str = None, http_client: httpx.AsyncClient = None
+    ) -> None:
+        self.base_url = base_url
+        self._client = http_client or httpx.AsyncClient(base_url=base_url)
 
-    async def post(route: str, json: dict):
-        pass
+    async def post(self, route: str, json: dict) -> httpx.Response:
+        return await self._client.post(route, json=json)
 
-    async def get(route: str):
-        pass
+    async def get(self, route: str) -> httpx.Response:
+        return await self._client.get(route)
 
+    async def create_flow(self, flow: "Flow") -> str:
+        flow_data = api.schemas.Flow(
+            name=flow.name,
+            tags=flow.tags,
+            parameters=flow.parameters,
+        )
+        response = await self.post("/flows/", json=flow_data.dict())
 
-async def create_flow(client: Client, flow: "Flow") -> str:
-    flow_data = api.schemas.Flow(
-        name=flow.name,
-        tags=flow.tags,
-        parameters=flow.parameters,
-    )
-    response = await client.post("/flows/", json=flow_data.dict())
+        flow_id = response.json().get("id")
+        if not flow_id:
+            raise Exception(f"Malformed response: {response}")
 
-    flow_id = response.json().get("id")
-    if not flow_id:
-        raise Exception(f"Malformed response: {response}")
+        # Return the id of the created flow
+        return flow_id
 
-    # Return the id of the created flow
-    return flow_id
-
-
-async def read_flow(client: Client, flow_id: str) -> api.schemas.Flow:
-    response = await client.get(f"/flows/{flow_id}")
-    return api.schemas.Flow(**response.json())
+    async def read_flow(self, flow_id: str) -> api.schemas.Flow:
+        response = await self.get(f"/flows/{flow_id}")
+        return api.schemas.Flow(**response.json())
