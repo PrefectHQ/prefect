6b0b7aee29c134431df8e3b4f5f07b230e5ce698
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-19 09:29:23 -0400

    Add pagination dependency

---
+++ src/prefect/orion/api/dependencies.py
@@ -0,0 +1,19 @@
+from prefect.orion.utilities.database import OrionAsyncSession
+from pydantic import BaseModel, conint, Field
+
+
+async def get_session():
+    """
+    Dependency-injected database session.
+
+    The context manager will automatically handle commits,
+    rollbacks, and closing the connection.
+    """
+    async with OrionAsyncSession.begin() as session:
+        yield session
+
+
+class Pagination(BaseModel):
+    # max limit is 200
+    limit: conint(ge=0, le=200) = 200
+    offset: conint(ge=0) = 0
