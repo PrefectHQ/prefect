c1eab36939a477a695e5ed9c0b038253e19acdf9
Zach Angell <zachary.james.angell@gmail.com>
2021-07-19 14:14:39 -0400

    Update route function names

--- src/prefect/orion/api/flow_runs.py
+++ src/prefect/orion/api/flow_runs.py
@@ -26,7 +26,7 @@ async def create_flow_run(
 
 
 @router.get("/{flow_run_id}")
-async def read_flow(
+async def read_flow_run(
     flow_run_id: str, session: sa.orm.Session = Depends(get_session)
 ) -> schemas.FlowRun:
     flow_run = await models.flow_runs.read_flow_run(session=session, id=flow_run_id)
@@ -36,7 +36,7 @@ async def read_flow(
 
 
 @router.get("/")
-async def read_flows(
+async def read_flow_runs(
     offset: int = 0,
     limit: int = 10,
     session: sa.orm.Session = Depends(get_session),
@@ -47,7 +47,9 @@ async def read_flows(
 
 
 @router.delete("/{flow_run_id}", status_code=204)
-async def delete_flow(flow_run_id: str, session: sa.orm.Session = Depends(get_session)):
+async def delete_flow_run(
+    flow_run_id: str, session: sa.orm.Session = Depends(get_session)
+):
     result = await models.flow_runs.delete_flow_run(session=session, id=flow_run_id)
     if not result:
         raise HTTPException(status_code=404, detail="Flow not found")
