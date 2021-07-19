10bb81da71dc7ba3d2ee2f9e8fe28c180681f503
Michael Adkins <michael@prefect.io>
2021-07-19 10:20:36 -0500

    Add `read_flow` and switch to async

--- src/prefect/core/client.py
+++ src/prefect/core/client.py
@@ -10,16 +10,25 @@ class Client:
     def __init__(self) -> None:
         pass
 
-    def create_flow(flow: "Flow") -> str:
+    async def create_flow(self, flow: "Flow") -> str:
         payload = api.schemas.Flow(
             name=flow.name,
             tags=flow.tags,
             parameters=flow.parameters,
         )
-        response = api.flows.create_flow(payload)
+        response = await api.flows.create_flow(payload)
         if not response:
             # TODO: Create prefect exception types
             raise Exception("Failed to create flow")
 
         # Return the id of the created flow
         return response.id
+
+    async def read_flow(self, flow_id: str) -> api.schemas.Flow:
+        response = await api.flows.read_flow(flow_id)
+        if not response:
+            # TODO: Create prefect exception types
+            raise Exception("Failed to read flow")
+
+        # Return the found flow schema
+        return response
