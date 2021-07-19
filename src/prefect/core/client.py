60627cb3fc83200d822b2528795369ad73d3f619
Michael Adkins <michael@prefect.io>
2021-07-19 13:19:40 -0500

    Sketch `Client` flow run methods

--- src/prefect/core/client.py
+++ src/prefect/core/client.py
@@ -1,6 +1,6 @@
 import httpx
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Iterable, Dict, Any
 
 from prefect.orion import api
 
@@ -39,3 +39,34 @@ class Client:
     async def read_flow(self, flow_id: str) -> api.schemas.Flow:
         response = await self.get(f"/flows/{flow_id}")
         return api.schemas.Flow(**response.json())
+
+    async def create_flow_run(
+        self,
+        flow: "Flow",
+        parameters: Dict[str, Any] = None,
+        context: dict = None,
+        extra_tags: Iterable[str] = None,
+        parent_task_run_id: str = None,
+    ) -> str:
+        tags = set(flow.tags).union(extra_tags or [])
+        parameters = parameters or {}
+        context = context or {}
+
+        # Retrieve the flow id
+        flow_id = await self.create_flow(flow)
+
+        flow_run_data = api.schemas.FlowRun(
+            flow_id=flow_id,
+            flow_version=flow.version,
+            parameters=parameters,
+            context=context,
+            tags=list(tags),
+            parent_task_run_id=parent_task_run_id,
+        )
+
+        flow_run_id = await self.post("/flow_runs/", json=flow_run_data.dict())
+        return flow_run_id
+
+    async def read_flow_run(self, flow_run_id: str) -> api.schemas.FlowRun:
+        response = await self.get(f"/flow_runs/{flow_run_id}")
+        return api.schemas.FlowRun(**response.json())
