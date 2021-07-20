d93d115faeb5dfde3dc258f74cfc068632155440
Michael Adkins <michael@prefect.io>
2021-07-20 11:02:57 -0500

    Move API methods out of the `Client`

--- src/prefect/core/client.py
+++ src/prefect/core/client.py
@@ -38,64 +38,6 @@ class Client:
     async def get(self, route: str) -> httpx.Response:
         return await self._client.get(route)
 
-    async def create_flow(self, flow: "Flow") -> str:
-        flow_data = api.schemas.Flow(
-            name=flow.name,
-            tags=flow.tags,
-            parameters=flow.parameters,
-        )
-        response = await self.post("/flows/", json=flow_data.dict())
-
-        flow_id = response.json().get("id")
-        if not flow_id:
-            raise Exception(f"Malformed response: {response}")
-
-        # Return the id of the created flow
-        return flow_id
-
-    async def read_flow(self, flow_id: str) -> api.schemas.Flow:
-        response = await self.get(f"/flows/{flow_id}")
-        return api.schemas.Flow(**response.json())
-
-    async def read_flow(self, flow_id: str) -> api.schemas.Flow:
-        response = await self.get(f"/flows/{flow_id}")
-        return api.schemas.Flow(**response.json())
-
-    async def create_flow_run(
-        self,
-        flow: "Flow",
-        parameters: Dict[str, Any] = None,
-        context: dict = None,
-        extra_tags: Iterable[str] = None,
-        parent_task_run_id: str = None,
-    ) -> str:
-        tags = set(flow.tags).union(extra_tags or [])
-        parameters = parameters or {}
-        context = context or {}
-
-        # Retrieve the flow id
-        flow_id = await self.create_flow(flow)
-
-        flow_run_data = api.schemas.FlowRun(
-            flow_id=flow_id,
-            flow_version=flow.version,
-            parameters=parameters,
-            context=context,
-            tags=list(tags),
-            parent_task_run_id=parent_task_run_id,
-        )
-
-        response = await self.post("/flow_runs/", data=flow_run_data.json())
-        flow_run_id = response.json().get("id")
-        if not flow_run_id:
-            raise Exception(f"Malformed response: {response}")
-
-        return flow_run_id
-
-    async def read_flow_run(self, flow_run_id: str) -> api.schemas.FlowRun:
-        response = await self.get(f"/flow_runs/{flow_run_id}")
-        return api.schemas.FlowRun(**response.json())
-
     def __enter__(self):
         self._previous_client = _current_client
         global _current_client
@@ -105,3 +47,70 @@ class Client:
         global _current_client
         _current_client = self._previous_client
         self._previous_client = None
+
+
+async def create_flow(self, flow: "Flow") -> str:
+    flow_data = api.schemas.Flow(
+        name=flow.name,
+        tags=flow.tags,
+        parameters=flow.parameters,
+    )
+    response = await client.post("/flows/", json=flow_data.dict())
+
+    flow_id = response.json().get("id")
+    if not flow_id:
+        raise Exception(f"Malformed response: {response}")
+
+    # Return the id of the created flow
+    return flow_id
+
+
+async def read_flow(flow_id: str, client: Client = None) -> api.schemas.Flow:
+    client = client or get_current_client()
+    response = await client.get(f"/flows/{flow_id}")
+    return api.schemas.Flow(**response.json())
+
+
+async def read_flow(flow_id: str, client: Client = None) -> api.schemas.Flow:
+    client = client or get_current_client()
+    response = await client.get(f"/flows/{flow_id}")
+    return api.schemas.Flow(**response.json())
+
+
+async def create_flow_run(
+    flow: "Flow",
+    parameters: Dict[str, Any] = None,
+    context: dict = None,
+    extra_tags: Iterable[str] = None,
+    parent_task_run_id: str = None,
+    client: Client = None,
+) -> str:
+    client = client or get_current_client()
+    tags = set(flow.tags).union(extra_tags or [])
+    parameters = parameters or {}
+    context = context or {}
+
+    # Retrieve the flow id
+    flow_id = await client.create_flow(flow)
+
+    flow_run_data = api.schemas.FlowRun(
+        flow_id=flow_id,
+        flow_version=flow.version,
+        parameters=parameters,
+        context=context,
+        tags=list(tags),
+        parent_task_run_id=parent_task_run_id,
+    )
+
+    response = await client.post("/flow_runs/", data=flow_run_data.json())
+    flow_run_id = response.json().get("id")
+    if not flow_run_id:
+        raise Exception(f"Malformed response: {response}")
+
+    return flow_run_id
+
+
+async def read_flow_run(flow_run_id: str, client: Client = None) -> api.schemas.FlowRun:
+    client = client or get_current_client()
+    response = await client.get(f"/flow_runs/{flow_run_id}")
+    return api.schemas.FlowRun(**response.json())
