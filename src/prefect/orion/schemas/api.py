aa68344b3b6dce9045decd5e62d8017319ef73ca
Zach Angell <zachary.james.angell@gmail.com>
2021-07-20 19:06:30 -0400

    Add examples

--- src/prefect/orion/schemas/api.py
+++ src/prefect/orion/schemas/api.py
@@ -36,15 +36,15 @@ class PrefectBaseModel(BaseModel):
 
 
 class Flow(PrefectBaseModel):
-    name: str
-    tags: List[str] = Field(default_factory=list)
+    name: str = Field(..., example="my-flow")
+    tags: List[str] = Field(default_factory=list, example=["tag-1", "tag-2"])
     parameters: ParameterSchema = Field(default_factory=ParameterSchema)
 
 
 class FlowRun(PrefectBaseModel):
     flow_id: UUID
-    flow_version: str
+    flow_version: str = Field(..., example="v1.0")
     parameters: dict = Field(default_factory=dict)
     parent_task_run_id: UUID = None
-    context: dict = Field(default_factory=dict)
-    tags: List[str] = Field(default_factory=list)
+    context: dict = Field(default_factory=dict, example={"my_var": "my_val"})
+    tags: List[str] = Field(default_factory=list, example=["tag-1", "tag-2"])
