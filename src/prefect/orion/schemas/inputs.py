0cb39b5a5b9e6a2d9f07d090ed73e5c5c7b5fddf
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-21 00:10:20 -0400

    Create `PrefectBaseModel` class

--- src/prefect/orion/schemas/inputs.py
+++ src/prefect/orion/schemas/inputs.py
@@ -5,18 +5,19 @@ Reduced schemas for accepting API inputs
 from typing import List
 from uuid import UUID
 
-from pydantic import BaseModel, Field
+from pydantic import Field
 
 from prefect.orion.utilities.functions import ParameterSchema
+from prefect.orion.utilities.schemas import PrefectBaseModel
 
 
-class FlowCreate(BaseModel):
+class FlowCreate(PrefectBaseModel):
     name: str = Field(..., example="my-flow")
     tags: List[str] = Field(default_factory=list, example=["tag-1", "tag-2"])
     parameters: ParameterSchema = Field(default_factory=ParameterSchema)
 
 
-class FlowRunCreate(BaseModel):
+class FlowRunCreate(PrefectBaseModel):
     flow_id: UUID
     flow_version: str = Field(..., example="v1.0")
     parameters: dict = Field(default_factory=dict)
--- src/prefect/orion/utilities/__init__.py
+++ src/prefect/orion/utilities/__init__.py
@@ -1,4 +1,5 @@
 import prefect.orion.utilities.settings
 import prefect.orion.utilities.database
-import prefect.orion.utilities.server
 import prefect.orion.utilities.functions
+import prefect.orion.utilities.server
+import prefect.orion.utilities.schemas
