3dee4a430c89fc852fbbb137ec6c5b618545a05a
Michael Adkins <michael@prefect.io>
2021-07-21 12:35:44 -0500

    Implement async engine for `Flow.__call__`

--- src/prefect/core/flow.py
+++ src/prefect/core/flow.py
@@ -5,8 +5,8 @@ from pydantic import validate_arguments
 from typing import Any, Callable, Iterable
 
 
-from prefect.core.utilities import file_hash
-from prefect.core.orion.flow_runs import create_flow_run_sync
+from prefect.core.utilities import file_hash, sync
+from prefect.core.orion.flow_runs import create_flow_run
 from prefect.core.futures import PrefectFuture
 
 from prefect.orion.utilities.functions import parameter_schema, ParameterSchema
@@ -40,8 +40,10 @@ class Flow:
         # TODO: Note that pydantic will now coerce parameter types into the correct type
         #       even if the user wants failure on inexact type matches. We may want to
         #       implement a strict runtime typecheck with a configuration flag
-        self.fn = validate_arguments(fn)
+        # TODO: `validate_arguments` is not an async safe wrapper so it breaks our func
+        # self.fn = validate_arguments(fn)
         update_wrapper(self, fn)
+        self.fn = fn
 
         # Version defaults to a hash of the function's file
         flow_file = fn.__globals__.get("__file__")
@@ -53,16 +55,24 @@ class Flow:
         # Generate a parameter schema from the function
         self.parameters = parameter_schema(self.fn)
 
-    def _run(self, *args, **kwargs):
+    async def _run(self, *args, **kwargs):
         # placeholder method that will eventually manage state
-        result = self.fn(*args, **kwargs)
-        return result
+        call_result = self.fn(*args, **kwargs)
+        if inspect.isawaitable(call_result):
+            return await call_result
+        return call_result
+
+    async def _call_async(self, args, kwargs) -> PrefectFuture:
+        parameters = inspect.signature(self.fn).bind_partial(*args, **kwargs).arguments
+        flow_run_id = await create_flow_run(self, parameters=parameters)
+        result = await self._run(*args, **kwargs)
+        return PrefectFuture(run_id=flow_run_id, result=result)
 
     def __call__(self, *args, **kwargs) -> PrefectFuture:
-        parameters = inspect.signature(self.fn).bind_partial(*args, **kwargs).arguments
-        flow_run_id = create_flow_run_sync(self, parameters=parameters)
-        result = self._run(*args, **kwargs)
-        return PrefectFuture(run_id=flow_run_id, result=result)
+        if inspect.iscoroutinefunction(self.fn):
+            return self._call_async(args, kwargs)
+        else:
+            return sync(self._call_async)(args, kwargs)
 
 
 def flow(_fn: Callable = None, *, name: str = None, **flow_init_kwargs: Any):
