a2bc2b5bced44175a1d28b3ad43f8e7112f038be
Michael Adkins <michael@prefect.io>
2021-07-21 11:02:07 -0500

    Split orion api functions into modules

---
+++ src/prefect/core/orion/client.py
@@ -0,0 +1,30 @@
+import httpx
+
+import contextvars
+
+
+# Async safe singleton for storing the default client for the current frame
+_CLIENT: contextvars.ContextVar["Client"] = contextvars.ContextVar("client")
+
+
+def get_client() -> "Client":
+    client = _CLIENT.get(None)
+    if not client:
+        client = Client()
+        set_client(client)
+    return client
+
+
+def set_client(client: "Client") -> contextvars.Token:
+    return _CLIENT.set(client)
+
+
+class Client:
+    def __init__(self, http_client: httpx.AsyncClient = None) -> None:
+        self._client = http_client
+
+    async def post(self, route: str, **kwargs) -> httpx.Response:
+        return await self._client.post(route, **kwargs)
+
+    async def get(self, route: str) -> httpx.Response:
+        return await self._client.get(route)
