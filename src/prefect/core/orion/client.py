7de51c5435ca93329187ca5de74c395a334788dc
Michael Adkins <michael@prefect.io>
2021-07-21 16:32:05 -0500

    Move `Client` definition above global variable

--- src/prefect/core/orion/client.py
+++ src/prefect/core/orion/client.py
@@ -1,6 +1,17 @@
+import contextvars
+
 import httpx
 
-import contextvars
+
+class Client:
+    def __init__(self, http_client: httpx.AsyncClient = None) -> None:
+        self._client = http_client or httpx.AsyncClient(base_url="http://localhost/")
+
+    async def post(self, route: str, **kwargs) -> httpx.Response:
+        return await self._client.post(route, **kwargs)
+
+    async def get(self, route: str) -> httpx.Response:
+        return await self._client.get(route)
 
 
 # Async safe singleton for storing the default client for the current frame
@@ -17,14 +28,3 @@ def get_client() -> "Client":
 
 def set_client(client: "Client") -> contextvars.Token:
     return _CLIENT.set(client)
-
-
-class Client:
-    def __init__(self, http_client: httpx.AsyncClient = None) -> None:
-        self._client = http_client or httpx.AsyncClient(base_url="http://localhost/")
-
-    async def post(self, route: str, **kwargs) -> httpx.Response:
-        return await self._client.post(route, **kwargs)
-
-    async def get(self, route: str) -> httpx.Response:
-        return await self._client.get(route)
