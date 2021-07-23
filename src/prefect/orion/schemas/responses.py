a1f0c17da5cfcecdaad54d345aac8de3b96150ce
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-22 23:27:26 -0400

    Add set flow run state route

---
+++ src/prefect/orion/schemas/responses.py
@@ -0,0 +1,16 @@
+from enum import auto
+
+from prefect.orion.schemas.core import State
+from prefect.orion.utilities.enum import AutoEnum
+from prefect.orion.utilities.schemas import PrefectBaseModel
+
+
+class SetStateStatus(AutoEnum):
+    ACCEPT = auto()
+    REJECT = auto()
+    ABORT = auto()
+
+
+class SetStateResponse(PrefectBaseModel):
+    status: SetStateStatus
+    state: State = None
