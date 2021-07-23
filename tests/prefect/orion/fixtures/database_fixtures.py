4cdf50005bb0b219c9f3941d0da5352d32a31990
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-22 20:54:27 -0400

    Update variable names

--- tests/prefect/orion/fixtures/database_fixtures.py
+++ tests/prefect/orion/fixtures/database_fixtures.py
@@ -53,15 +53,15 @@ async def flow_run_states(database_session, flow_run):
     )
     scheduled_flow_run_state = await models.flow_run_states.create_flow_run_state(
         session=database_session,
-        flow_run_state=scheduled_state,
         flow_run_id=flow_run.id,
+        state=scheduled_state,
     )
     running_state = schemas.actions.StateCreate(
         name="Running", type="Running", timestamp=pendulum.now()
     )
     running_flow_run_state = await models.flow_run_states.create_flow_run_state(
         session=database_session,
-        flow_run_state=running_state,
         flow_run_id=flow_run.id,
+        state=running_state,
     )
     return [scheduled_flow_run_state, running_flow_run_state]
