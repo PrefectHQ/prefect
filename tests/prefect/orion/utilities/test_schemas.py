9492ccad71dfd10ae047df4f5c0b2cca74c767af
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-23 20:25:31 -0400

    Update field arguments

--- tests/prefect/orion/utilities/test_schemas.py
+++ tests/prefect/orion/utilities/test_schemas.py
@@ -1,6 +1,6 @@
 import pytest
 import pydantic
-from prefect.orion.utilities.schemas import subclass_model
+from prefect.orion.utilities.schemas import pydantic_subclass
 
 
 class TestSubclassModel:
@@ -12,42 +12,50 @@ class TestSubclassModel:
         y: int = 2
 
     def test_subclass_is_a_subclass(self):
-        Child = subclass_model(self.Parent)
+        Child = pydantic_subclass(self.Parent)
         assert issubclass(Child, self.Parent)
 
     def test_parent_is_unchanged(self):
         original_fields = self.Parent.__fields__.copy()
-        Child = subclass_model(self.Parent)
+        Child = pydantic_subclass(self.Parent)
         assert self.Parent.__fields__ == original_fields
 
     def test_default_subclass_name(self):
-        Child = subclass_model(self.Parent)
+        Child = pydantic_subclass(self.Parent)
         assert Child.__name__ == "Parent"
 
     def test_subclass_name(self):
-        Child = subclass_model(self.Parent, name="Child")
+        Child = pydantic_subclass(self.Parent, name="Child")
         assert Child.__name__ == "Child"
 
     def test_subclass_fields(self):
-        Child = subclass_model(self.Parent, name="Child")
+        Child = pydantic_subclass(self.Parent, name="Child")
         c = Child(x=1)
         assert c.x == 1
         assert c.y == 2
 
-    def test_subclass_include(self):
-        Child = subclass_model(self.Parent, name="Child", include=["y"])
+    def test_subclass_include_fields(self):
+        Child = pydantic_subclass(self.Parent, name="Child", include_fields=["y"])
         c = Child(y=1)
         assert c.y == 1
         assert not hasattr(c, "x")
 
-    def test_subclass_exclude(self):
-        Child = subclass_model(self.Parent, name="Child", exclude=["x"])
+    def test_subclass_exclude_fields(self):
+        Child = pydantic_subclass(self.Parent, name="Child", exclude_fields=["x"])
         c = Child(y=1)
         assert c.y == 1
         assert not hasattr(c, "x")
+
+    def test_subclass_include_invalid_fields(self):
+        with pytest.raises(ValueError, match="(fields not found on base class)"):
+            pydantic_subclass(self.Parent, name="Child", include_fields=["z"])
+
+    def test_subclass_exclude_invalid_fields(self):
+        with pytest.raises(ValueError, match="(fields not found on base class)"):
+            pydantic_subclass(self.Parent, name="Child", exclude_fields=["z"])
 
     def test_extend_subclass(self):
-        class Child(subclass_model(self.Parent, include=["y"])):
+        class Child(pydantic_subclass(self.Parent, include_fields=["y"])):
             z: int
 
         c = Child(y=5, z=10)
@@ -55,7 +63,7 @@ class TestSubclassModel:
         assert c.z == 10
 
     def test_extend_subclass_respects_config(self):
-        class Child(subclass_model(self.Parent, include=["y"])):
+        class Child(pydantic_subclass(self.Parent, include_fields=["y"])):
             z: int
 
         with pytest.raises(
