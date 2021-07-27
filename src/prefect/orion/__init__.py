af28e109744f6acaf6bb636bed79c69806d284e2
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-07-26 22:59:32 -0400

    Create in-memory objects on connect

--- src/prefect/orion/__init__.py
+++ src/prefect/orion/__init__.py
@@ -1,18 +1,6 @@
-import asyncio
 import prefect.orion.utilities
 import prefect.orion.schemas
 import prefect.orion.models
 import prefect.orion.api
 
 from prefect.orion.utilities.settings import Settings
-from prefect.orion.utilities import database as _database
-
-# if the orion database engine is in-memory, populate it.
-# this will match any of the following connection strings:
-# - sqlite+aiosqlite:///:memory:
-# - sqlite+aiosqlite://
-if (
-    _database.engine.url.get_backend_name() == "sqlite"
-    and _database.engine.url.database in (":memory:", None)
-):
-    asyncio.run(_database.create_database_objects())
