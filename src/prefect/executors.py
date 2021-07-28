da804172b2690fce45ab9f86b7a8ebc2624bff58
Michael Adkins <michael@prefect.io>
2021-07-27 20:24:25 -0500

    Stash implementation using `Call` objects instead of `PrefectFuture`

--- src/prefect/executors.py
+++ src/prefect/executors.py
@@ -2,14 +2,17 @@ from typing import Callable, Tuple, Any, Dict
 from typing import NamedTuple
 from uuid import uuid4
 
+from concurrent.futures import ThreadPoolExecutor, Future
+
 
 class Call(NamedTuple):
+    id: str
     fn: Callable
     args: Tuple[Any, ...]
     kwargs: Dict[str, Any]
 
 
-class Executor:
+class BaseExecutor:
     _calls: Dict[str, Call]
 
     def __init__(self) -> None:
@@ -21,21 +24,58 @@ class Executor:
         *args: Any,
         **kwargs: Dict[str, Any],
     ) -> str:
-        call_id = str(uuid4())
-        self._calls[call_id] = Call(fn, args, kwargs)
+        call = Call(str(uuid4()), fn, args, kwargs)
+        self._calls[call.id] = call
 
-        # STUB: Execute immediately
-        self._execute(call_id)
+        # Call child hook
+        self._submit(call)
 
-        return call_id
+        return call.id
 
-    def _execute(self, call_id: str) -> None:
-        call = self._calls[call_id]
+    def result(self, call_id: str):
+        return self._result(call_id)
 
+    def _submit(self, call: Call) -> None:
+        raise NotImplementedError
+
+    def _result(self, call_id: str) -> Any:
+        raise NotImplementedError
+
+
+class SyncExecutor(BaseExecutor):
+    """
+    A simple synchronous executor that executes calls as they are submitted
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._results: Dict[str, Any] = {}
+
+    def _submit(self, call: Call) -> None:
         try:
-            call.fn(*call.args, **call.kwargs)
+            self._results[call.id] = call.fn(*call.args, **call.kwargs)
         except Exception as exc:
             # This is a Prefect exception at this point
             raise RuntimeError(
                 f"Encountered an exception while executing {call.fn}"
             ) from exc
+
+    def _result(self, call: Call) -> None:
+        return self._results[call.id]
+
+
+class ThreadedExecutor(BaseExecutor):
+    """
+    A simple synchronous executor that executes calls as they are submitted
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._pool = ThreadPoolExecutor()
+        self._futures: Dict[str, Future] = {}
+
+    def _submit(self, call: Call) -> None:
+        self._futures[call.id] = self._pool.submit(call.fn, *call.args, *call.kwargs)
+
+    def _result(self, call_id: str) -> None:
+        return self._futures[call_id].result()
