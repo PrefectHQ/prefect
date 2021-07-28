2e0e7adeb902cb4f6578de1ae892a4b477a16502
Michael Adkins <michael@prefect.io>
2021-07-28 15:42:28 -0500

    Update tests to reflect return types

--- tests/prefect/test_tasks.py
+++ tests/prefect/test_tasks.py
@@ -28,9 +28,9 @@ class TestTaskCall:
             # Returns a future so we can run assertions outside of the flow context
 
         flow_future = bar()
-        task_future = flow_future.result()
+        task_future = flow_future.result().data
         assert isinstance(task_future, PrefectFuture)
-        assert task_future.result() == 1
+        assert task_future.result().data == 1
 
     @pytest.mark.parametrize("error", [ValueError("Hello"), None])
     def test_state_reflects_result_of_run(self, error):
@@ -44,23 +44,9 @@ class TestTaskCall:
             return bar()
 
         flow_future = foo()
-        task_future = flow_future.result()
-
-        raised = None
-        try:
-            task_future.result()
-        except Exception as exc:
-            raised = exc
-
-        # Assert the exception was raised correctly
-        assert raised is error
+        task_future = flow_future.result().data
+        state = task_future.result()
 
         # Assert the final state is correct
-        # WORKS
-        states = OrionClient().read_task_run_states(task_future.run_id)
-        final_state = states[-1]
-        assert final_state.is_failed() if error else final_state.is_completed()
-
-        # ALSO WORKS
-        state = task_future.state()
         assert state.is_failed() if error else state.is_completed()
+        assert state.data is error
