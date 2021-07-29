5141bc0398f5f5fd0375015fb4932f190256a6de
Michael Adkins <michael@prefect.io>
2021-07-29 10:53:14 -0500

    Add flow/task run ids to futures

--- src/prefect/futures.py
+++ src/prefect/futures.py
@@ -1,67 +1,45 @@
-from enum import Enum
 from typing import Any, Optional, Callable
 from uuid import UUID
 
 from prefect.client import OrionClient
-from prefect.orion.schemas.core import State, StateType
-from prefect.orion.schemas.responses import SetStateResponse
-
-
-class RunType(Enum):
-    FlowRun = "FlowRun"
-    TaskRun = "TaskRun"
+from prefect.orion.schemas.core import State
 
 
 class PrefectFuture:
     def __init__(
         self,
-        run_id: UUID,
-        run_type: RunType,
+        flow_run_id: UUID,
         client: OrionClient,
         wait_callback: Callable[[float], Optional[State]],
+        task_run_id: UUID = None,
     ) -> None:
-        self.run_id = run_id
-        self.run_type = run_type
+        self.flow_run_id = flow_run_id
+        self.task_run_id = task_run_id
+        self.run_id = self.task_run_id or self.flow_run_id
         self._client = client
         self._result: Any = None
         self._exception: Optional[Exception] = None
         self._wait_callback = wait_callback
 
-        self.set_state(State(type=StateType.PENDING))
-
-    @property
-    def is_flow_run(self):
-        self.run_type == RunType.FlowRun
-
-    @property
-    def is_task_run(self):
-        self.run_type == RunType.TaskRun
-
     def result(self, timeout: float = None) -> State:
         """
         Return the state of the run the future represents
         """
-        state = self.get_state()
-        if state.is_done():
-            return state
+        # TODO: Since states in the backend don't have data attached yet this will
+        # return a state without data
+        # state = self.get_state()
+        # if state.is_done():
+        #     return state
 
         result = self._wait_callback(timeout)
 
         return result
 
-    def set_state(self, state: State) -> SetStateResponse:
-        method = (
-            self._client.set_flow_run_state
-            if self.is_flow_run
-            else self._client.set_task_run_state
-        )
-        return method(self.run_id, state)
-
     def get_state(self) -> State:
         method = (
-            self._client.read_flow_run_states
-            if self.is_flow_run
-            else self._client.read_task_run_states
+            self._client.read_task_run_states
+            if self.task_run_id
+            else self._client.read_flow_run_states
         )
         states = method(self.run_id)
         if not states:
