1406b7180b648a9af0120c348ae4a22e8af7fa32
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-08-01 21:04:01 -0400

    Autouse db fixture

--- tests/integration_tests/conftest.py
+++ tests/integration_tests/conftest.py
@@ -12,27 +12,6 @@ import inspect
 import pytest
 
 
-@pytest.fixture(scope="session")
-async def database_engine():
-    """Creates an in memory sqlite database for use in testing. For performance,
-    the database is created only once, at the beginning of testing. Subsequent
-    sessions roll themselves back at the end of each test to restore the
-    database.
-    """
-
-    engine = get_engine()
-
-    # populate database tables
-    try:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.create_all)
-        yield engine
-    finally:
-        async with engine.begin() as conn:
-            await conn.run_sync(Base.metadata.drop_all)
-        await engine.dispose()
-
-
 @pytest.fixture
 async def database_session(database_engine):
     OrionSession = get_session_factory(database_engine)
