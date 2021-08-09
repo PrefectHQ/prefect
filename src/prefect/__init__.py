e64b0c2a5edbde0b392dde5a4dd28d17e88f1b31
Michael Adkins <michael@prefect.io>
2021-08-09 12:33:32 -0500

    Renable logging at import

--- src/prefect/__init__.py
+++ src/prefect/__init__.py
@@ -1,7 +1,7 @@
 from prefect.utilities.settings import settings
 from prefect.utilities.logging import setup_logging
 
-# setup_logging(settings)
+setup_logging(settings)
 
 from prefect.flows import flow
 from prefect.tasks import task
