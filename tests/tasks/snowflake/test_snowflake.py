7b07c24de25805b741f3f4b1faeef49204e3fe6a
austen-bouza <73256032+austen-bouza@users.noreply.github.com>
2021-08-12 07:25:44 -0500

    Allow custom cursor types in SnowflakeQuery task (#4815)
    * Allow custom cursor types in Snowflake connection
    
    Adds a 'cursor_type' keyword argument to the run method of the
    SnowflakeQuery task. This change allows other cursor types such as
    DictCursor to be used, enabling queries to be returned using different
    types of data structures.
    
    * Add unit test and docstring for SnowflakeTask
    
    Adds unit testing and docstring for new cursor_type keyword argument in
    SnowflakeTask run method.

--- tests/tasks/snowflake/test_snowflake.py
+++ tests/tasks/snowflake/test_snowflake.py
@@ -1,4 +1,5 @@
 from unittest.mock import MagicMock
+from snowflake.connector.cursor import SnowflakeCursor, DictCursor
 
 import pytest
 
@@ -76,3 +77,52 @@ class TestSnowflakeQuery:
         ).run()
 
         assert output == ["TESTDB"]
+
+    def test_run_method_accepts_alternate_cursor(self, monkeypatch):
+        """
+        Tests that the default cursor used by the Snowflake connection
+        is of the type `SnowflakeCursor`, and that the cursor type can
+        be overriden by providing a different cursor type to the
+        `cursor_type` keyword argument.
+        """
+        snowflake_module_connect_method = MagicMock()
+        connection = MagicMock(spec=sf.SnowflakeConnection)
+        default_cursor = MagicMock(spec=sf.cursor.SnowflakeCursor)
+        dict_cursor = MagicMock(spec=sf.cursor.DictCursor)
+
+        # link all the mocks together appropriately
+        snowflake_module_connect_method.return_value = connection
+
+        # setting fetchall return
+        default_cursor.__enter__.return_value.execute.return_value.fetchall.return_value = [
+            "1",
+            "2",
+            "3",
+        ]
+        dict_cursor.__enter__.return_value.execute.return_value.fetchall.return_value = {
+            "one": "1",
+            "two": "2",
+            "three": "3",
+        }
+        snowflake_connector_module = MagicMock(connect=snowflake_module_connect_method)
+
+        def mock_cursor(cursor_class: SnowflakeCursor) -> SnowflakeCursor:
+            if cursor_class == DictCursor:
+                return dict_cursor
+            else:
+                return default_cursor
+
+        connection.cursor = mock_cursor
+
+        monkeypatch.setattr(
+            "prefect.tasks.snowflake.snowflake.sf", snowflake_connector_module
+        )
+
+        query = "select * from numbers"
+        task = SnowflakeQuery(account="test", user="test", password="test", query=query)
+
+        default_cursor_output = task.run(cursor_type=SnowflakeCursor)
+        dict_cursor_output = task.run(cursor_type=DictCursor)
+
+        assert default_cursor_output == ["1", "2", "3"]
+        assert dict_cursor_output == {"one": "1", "two": "2", "three": "3"}
