828efb62ca9df37d45ccaabec16e78265f5d05d5
Michael Adkins <michael@prefect.io>
2021-08-20 18:18:07 -0500

    Add compat module for asyncmock import

--- tests/unit_tests/orion/test_data.py
+++ tests/unit_tests/orion/test_data.py
@@ -1,7 +1,6 @@
 import pytest
 
-from unittest.mock import AsyncMock
-
+from prefect.utilities.compat import AsyncMock
 from prefect.orion.data import write_blob, read_blob, resolve_orion_datadoc
 from prefect.orion.schemas.data import DataDocument
 
