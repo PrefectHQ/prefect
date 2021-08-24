9648f64eee04e747127bedbbb1ed44f32db01a89
tchoedak <tchoedak@gmail.com>
2021-08-23 23:52:52 -0700

    remove schedule code in hello world example flow

--- src/prefect/hello_world.py
+++ src/prefect/hello_world.py
@@ -1,36 +1,24 @@
 """
 A very basic 'hello-world' flow for getting started quickly
-
 Examples:
-
     Run this flow locally
     --------------------
     >>> from prefect.hello_world import hello_flow
     >>> hello_flow.run()
-
     Run this flow with a different parameter than the default
     --------------------------------------------------------
     >>> from prefect.hello_world import hello_flow
     >>> hello_flow.run(name="marvin")
-
-
     Register this flow with the Prefect backend
     ------------------------------------------
     $ prefect create project 'default'
     $ prefect register --project default -m prefect.hello_world
-
-
     Run this flow with the Prefect backend and agent
     ------------------------------------------
     $ prefect run --name "hello-world" --watch
 """
 
 from prefect import task, Flow, Parameter
-from datetime import timedelta
-from prefect.schedules import IntervalSchedule
-
-
-schedule = IntervalSchedule(interval=timedelta(seconds=10))
 
 
 @task(log_stdout=True)
@@ -43,6 +31,6 @@ def capitalize(word: str) -> str:
     return word.capitalize()
 
 
-with Flow("hello-world", schedule) as hello_flow:
+with Flow("hello-world") as hello_flow:
     name = Parameter("name", default="world")
     say_hello(capitalize(name))
