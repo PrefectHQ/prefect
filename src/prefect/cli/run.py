eead6baab8c7374a564c20e1105a32bbea95b161
tchoedak <tchoedak@gmail.com>
2021-08-23 22:58:34 -0700

    throw an exception when schedule flag is on for non-local runs

--- src/prefect/cli/run.py
+++ src/prefect/cli/run.py
@@ -569,6 +569,12 @@ def run(
         )
     params_dict = {**file_params, **cli_params}
 
+    if schedule and flow_or_group_id:
+        raise ClickException(
+            "Received a local only flag for a non-local flow; "
+            "cannot specify both `--schedule` and `--id`"
+        )
+
     # Local flow run -------------------------------------------------------------------
 
     if path or module:
