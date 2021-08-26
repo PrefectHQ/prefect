9a45a74f74f961fa1555d27046e2b37b1c106684
Dustin Ngo <dustin.ngo@gmail.com>
2021-08-26 09:50:55 -0400

    Use up-to-date temporary path fixtures
    - as per https://github.com/PrefectHQ/orion/pull/146#pullrequestreview-739473690

--- tests/unit_tests/orion/test_serializers.py
+++ tests/unit_tests/orion/test_serializers.py
@@ -9,28 +9,28 @@ def serialize_deserialize(obj, serializer, **dump_kwargs):
 
 
 class TestFileSerializer:
-    def test_encode_writes_to_local_fs(self, tmpdir):
-        path = str(tmpdir.join("test"))
+    def test_encode_writes_to_local_fs(self, tmp_path):
+        path = str(tmp_path.joinpath("test"))
         FileSerializer.dumps(b"data", path=path)
 
-        with open(tmpdir.join("test"), "rb") as fp:
+        with open(tmp_path.joinpath("test"), "rb") as fp:
             assert fp.read() == b"data"
 
-    def test_reads_from_local_fs(self, tmpdir):
-        path = str(tmpdir.join("test"))
+    def test_reads_from_local_fs(self, tmp_path):
+        path = str(tmp_path.joinpath("test"))
         with open(path, "wb") as fp:
             fp.write(b"data")
 
         assert FileSerializer.loads(path.encode()) == b"data"
 
-    def test_roundtrip_data_is_unchanged(self, tmpdir):
-        tmp_path = str(tmpdir.join("test"))
+    def test_roundtrip_data_is_unchanged(self, tmp_path):
+        tmp_path = str(tmp_path.joinpath("test"))
         assert serialize_deserialize(b"test", FileSerializer, path=tmp_path) == b"test"
 
-    def test_requires_bytes(self, tmpdir):
+    def test_requires_bytes(self, tmp_path):
         with pytest.raises(TypeError):
             # Raises in fsspec -- might be worth an explicit type check earlier
-            tmp_path = str(tmpdir.join("test"))
+            tmp_path = str(tmp_path.joinpath("test"))
             FileSerializer.dumps("data", path=tmp_path)
 
     def test_requires_path(self):
