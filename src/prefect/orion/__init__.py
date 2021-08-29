bfb1f3025396c61ca3e3057ea2c8ba9c7d795741
Dustin Ngo <dustin.ngo@gmail.com>
2021-08-29 19:12:19 -0400

    Use direct imports for simplicity

--- src/prefect/orion/__init__.py
+++ src/prefect/orion/__init__.py
@@ -1,9 +1,7 @@
-__all__ = [
-    "api",
-    "models",
-    "orchestration",
-    "schemas",
-    "utilities",
-    "serializers",
-    "states",
-]
+import prefect.orion.api
+import prefect.orion.models
+import prefect.orion.orchestration
+import prefect.orion.schemas
+import prefect.orion.utilities
+import prefect.orion.serializers
+import prefect.orion.states
