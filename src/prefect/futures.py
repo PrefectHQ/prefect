121582e99523e0da48edad3e53b3e68b8b5d80cc
Michael Adkins <michael@prefect.io>
2021-08-30 00:17:30 -0500

    Cleanup following rebase

--- src/prefect/futures.py
+++ src/prefect/futures.py
@@ -23,12 +23,13 @@ class PrefectFuture:
         client: OrionClient,
         executor: "BaseExecutor",
         task_run_id: UUID = None,
+        _result: Any = None,  # Exposed for testing
     ) -> None:
         self.flow_run_id = flow_run_id
         self.task_run_id = task_run_id
         self.run_id = self.task_run_id or self.flow_run_id
         self._client = client
-        self._result: Any = None
+        self._result: Any = _result
         self._exception: Optional[Exception] = None
         self._executor = executor
 
