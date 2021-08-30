ca2bf819936bcde45ab89ed5bfb65c3448ba3ff8
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-08-30 10:05:49 -0400

    Update filter names

--- src/prefect/orion/schemas/filters.py
+++ src/prefect/orion/schemas/filters.py
@@ -13,86 +13,96 @@ from prefect.orion.models import orm
 
 
 class FlowFilter(PrefectBaseModel):
-    id_in: List[UUID] = None
-    name_in: List[str] = None
-    tags_eq: List[str] = None
+    ids: List[UUID] = None
+    names: List[str] = None
+    tags: List[str] = None
 
     def as_sql_filter(self) -> List:
         filters = []
 
-        if self.id_in is not None:
-            filters.append(orm.Flow.id.in_(self.id_in))
-        if self.name_in is not None:
-            filters.append(orm.Flow.name.in_(self.name_in))
-        if self.tags_eq is not None:
-            filters.append(json_has_all_keys(orm.Flow.tags, self.tags_eq))
+        if self.ids is not None:
+            filters.append(orm.Flow.id.in_(self.ids))
+        if self.names is not None:
+            filters.append(orm.Flow.name.in_(self.names))
+        if self.tags is not None:
+            filters.append(json_has_all_keys(orm.Flow.tags, self.tags))
 
         return sa.and_(*filters)
 
 
 class FlowRunFilter(PrefectBaseModel):
-    id_in: List[UUID] = None
-    tags_eq: List[str] = None
-    state_in: List[schemas.states.StateType] = None
-    flow_version_in: List[str] = None
-    start_time_gte: datetime.datetime = None
-    start_time_lte: datetime.datetime = None
-    parent_task_run_id_in: List[str] = None
+    ids: List[UUID] = None
+    tags: List[str] = None
+    states: List[schemas.states.StateType] = None
+    flow_versions: List[str] = None
+    start_time_before: datetime.datetime = None
+    start_time_after: datetime.datetime = None
+    parent_task_run_ids: List[str] = None
 
     def as_sql_filter(self) -> List:
         filters = []
 
-        if self.id_in is not None:
-            filters.append(orm.FlowRun.id.in_(self.id_in))
-        if self.tags_eq is not None:
-            filters.append(json_has_all_keys(orm.FlowRun.tags, self.tags_eq))
-        if self.flow_version_in is not None:
-            filters.append(orm.FlowRun.flow_version.in_(self.flow_version_in))
-        if self.state_in is not None:
+        if self.ids is not None:
+            filters.append(orm.FlowRun.id.in_(self.ids))
+        if self.tags is not None:
+            filters.append(json_has_all_keys(orm.FlowRun.tags, self.tags))
+        if self.flow_versions is not None:
+            filters.append(orm.FlowRun.flow_version.in_(self.flow_versions))
+        if self.states is not None:
             filters.append(
-                orm.FlowRun.state.has(orm.FlowRunState.type.in_(self.state_in))
+                orm.FlowRun.state.has(orm.FlowRunState.type.in_(self.states))
             )
+        if self.parent_task_run_ids is not None:
+            filters.append(orm.FlowRun.parent_task_run_id.in_(self.parent_task_run_ids))
 
         # TODO: use canonical start time instead of timestamp
-        if self.start_time_lte is not None:
+        if self.start_time_before is not None:
             filters.append(
-                orm.FlowRun.state.has(orm.FlowRunState.timestamp <= self.start_time_lte)
+                orm.FlowRun.state.has(
+                    orm.FlowRunState.timestamp <= self.start_time_before
+                )
             )
-        if self.start_time_gte is not None:
+        if self.start_time_after is not None:
             filters.append(
-                orm.FlowRun.state.has(orm.FlowRunState.timestamp >= self.start_time_gte)
+                orm.FlowRun.state.has(
+                    orm.FlowRunState.timestamp >= self.start_time_after
+                )
             )
 
         return sa.and_(*filters)
 
 
 class TaskRunFilter(PrefectBaseModel):
-    id_in: List[UUID] = None
-    tags_eq: List[str] = None
-    state_in: List[schemas.states.StateType] = None
-    start_time_gte: datetime.datetime = None
-    start_time_lte: datetime.datetime = None
+    ids: List[UUID] = None
+    tags: List[str] = None
+    states: List[schemas.states.StateType] = None
+    start_time_before: datetime.datetime = None
+    start_time_after: datetime.datetime = None
 
     def as_sql_filter(self) -> List:
         filters = []
 
-        if self.id_in is not None:
-            filters.append(orm.TaskRun.id.in_(self.id_in))
-        if self.tags_eq is not None:
-            filters.append(json_has_all_keys(orm.TaskRun.tags, self.tags_eq))
-        if self.state_in is not None:
+        if self.ids is not None:
+            filters.append(orm.TaskRun.id.in_(self.ids))
+        if self.tags is not None:
+            filters.append(json_has_all_keys(orm.TaskRun.tags, self.tags))
+        if self.states is not None:
             filters.append(
-                orm.TaskRun.state.has(orm.TaskRunState.type.in_(self.state_in))
+                orm.TaskRun.state.has(orm.TaskRunState.type.in_(self.states))
             )
 
         # TODO: use canonical start time instead of timestamp
-        if self.start_time_lte is not None:
+        if self.start_time_before is not None:
             filters.append(
-                orm.TaskRun.state.has(orm.TaskRunState.timestamp <= self.start_time_lte)
+                orm.TaskRun.state.has(
+                    orm.TaskRunState.timestamp <= self.start_time_before
+                )
             )
-        if self.start_time_gte is not None:
+        if self.start_time_after is not None:
             filters.append(
-                orm.TaskRun.state.has(orm.TaskRunState.timestamp >= self.start_time_gte)
+                orm.TaskRun.state.has(
+                    orm.TaskRunState.timestamp >= self.start_time_after
+                )
             )
 
         return sa.and_(*filters)
