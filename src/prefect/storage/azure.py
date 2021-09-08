2a6b29e3f61348168e54d7156c5de6a6f442e115
Mariia Kerimova <mashun4ek@gmail.com>
2021-09-08 08:37:43 -0400

    Pass connection string as a secret or env var for azure storage

--- src/prefect/storage/azure.py
+++ src/prefect/storage/azure.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING, Any
 import pendulum
 from slugify import slugify
 
+from prefect.client import Secret
 from prefect.engine.results import AzureResult
 from prefect.storage import Storage
 from prefect.utilities.storage import (
@@ -27,7 +28,8 @@ class Azure(Storage):
 
     Args:
         - container (str): the name of the Azure Blob Container to store the Flow
-        - connection_string (str, optional): an Azure connection string for communicating with
+        - connection_string_secret_name (str, optional): the name of a Prefect secret
+            that contains an Azure connection string for communicating with
             Blob storage. If not provided the value set in the environment as
             `AZURE_STORAGE_CONNECTION_STRING` will be used
         - blob_name (str, optional): a unique key to use for uploading this Flow to Azure. This
@@ -40,20 +42,18 @@ class Azure(Storage):
     def __init__(
         self,
         container: str,
-        connection_string: str = None,
+        connection_string_secret_name: str = None,
         blob_name: str = None,
         stored_as_script: bool = False,
         **kwargs: Any
     ) -> None:
-        self.connection_string = connection_string or os.getenv(
-            "AZURE_STORAGE_CONNECTION_STRING"
-        )
-
         self.container = container
+        self.connection_string_secret_name = connection_string_secret_name
         self.blob_name = blob_name
 
         result = AzureResult(
-            connection_string=self.connection_string, container=container
+            connection_string_secret=self.connection_string_secret_name,
+            container=container,
         )
         super().__init__(result=result, stored_as_script=stored_as_script, **kwargs)
 
@@ -70,15 +70,19 @@ class Azure(Storage):
         if flow_name not in self.flows:
             raise ValueError("Flow is not contained in this Storage")
         flow_location = self.flows[flow_name]
+        try:
+            client = self._azure_block_blob_service.get_blob_client(
+                container=self.container, blob=flow_location
+            )
 
-        client = self._azure_block_blob_service.get_blob_client(
-            container=self.container, blob=flow_location
-        )
+            self.logger.info(
+                "Downloading {} from {}".format(flow_location, self.container)
+            )
 
-        self.logger.info("Downloading {} from {}".format(flow_location, self.container))
-
-        content = client.download_blob().content_as_bytes()
-
+            content = client.download_blob().content_as_bytes()
+        except Exception as err:
+            self.logger.error("Error downloading Flow from Azure: {}".format(err))
+            raise
         if self.stored_as_script:
             return extract_flow_from_file(file_contents=content, flow_name=flow_name)  # type: ignore
 
@@ -146,6 +150,18 @@ class Azure(Storage):
             client.upload_blob(data)
 
         return self
+
+    @property
+    def connection_string(self) -> str:
+        if self.connection_string_secret_name is not None:
+            return Secret(self.connection_string_secret_name).get()
+        conn_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+        if conn_string is None:
+            raise Exception(
+                "Azure connection string not provided. Set `AZURE_STORAGE_CONNECTION_STRING` environment"
+                " variable or save connection string as Prefect secret."
+            )
+        return conn_string
 
     @property
     def _azure_block_blob_service(self):  # type: ignore
