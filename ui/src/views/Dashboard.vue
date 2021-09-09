8d92e8801e4447d1df0ffcc3799cdd7c1419a014
nicholas <znicholasbrown@gmail.com>
2021-09-09 12:24:01 -0400

    Rename home view to dashboard, remove schematics view, add flow run view, update routes

---
+++ ui/src/views/Dashboard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="px-8">
+    <h1>Dashboard</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+  components: {}
+})
+export default class Dashboard extends Vue {
+  sayHello(): void {
+    console.log('hello!')
+  }
+}
+</script>
