267745521ffead854982152e3e8ed0b6302c5e08
Michael Adkins <michael@prefect.io>
2021-09-09 12:29:13 -0500

    Drop `file_contents` from `exec_script` and fix tracebacks

--- src/prefect/utilities/evaluation.py
+++ src/prefect/utilities/evaluation.py
@@ -1,25 +1,39 @@
+import os
+from contextlib import nullcontext
 from typing import Any, Dict
+
+from prefect.utilities.filesystem import tmpchdir
 
 
 def exec_script(
-    file_path: str = None,
-    file_contents: str = None,
+    file_path: str,
 ) -> Dict[str, Any]:
     """
     Execute a python script with __file__ populated if feasible and return the global
     variables
     """
-    if file_contents is None:
-        if file_path is None:
-            raise ValueError("Either `file_path` of `file_contents` must be provided")
-        with open(file_path, "r") as f:
-            file_contents = f.read()
+    with open(file_path, "r") as f:
+        file_contents = f.read()
 
     # If a file_path has been provided, provide __file__ as a global variable
     # so it resolves correctly during extraction
-    exec_vals: Dict[str, Any] = {"__file__": file_path} if file_path else {}
+    exec_vals: Dict[str, Any] = {
+        "__file__": file_path,
+        "__name__": os.path.dirname(file_path),
+    }
 
-    # Load objects from file into the dict
-    exec(file_contents, exec_vals)
+    # Execute the script as if we are in its directory
+    with tmpchdir(file_path) if file_path else nullcontext():
 
+        # Compile the code so the file is attached to any traceback frames that arise
+        # This allows tracebacks to reference failing lines
+        code = compile(
+            file_contents,
+            filename=os.path.abspath(file_path),
+            mode="exec",
+        )
+
+        exec(code, exec_vals)
+
+    # Globals from the script will be populated in this dict now
     return exec_vals
