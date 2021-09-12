fd7ff688ed78b6aeca9e36cbee3c222700938dec
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-09-11 20:57:58 -0400

    Fix nondeterministic schedule

--- tests/orion/services/test_scheduler.py
+++ tests/orion/services/test_scheduler.py
@@ -35,7 +35,7 @@ async def test_create_schedule_respects_max_future_time(flow, session):
             name="test",
             flow_id=flow.id,
             schedule=schemas.schedules.IntervalSchedule(
-                interval=datetime.timedelta(days=30)
+                interval=datetime.timedelta(days=30), anchor_date=pendulum.now()
             ),
         ),
     )
@@ -43,10 +43,10 @@ async def test_create_schedule_respects_max_future_time(flow, session):
 
     n_runs = await models.flow_runs.count_flow_runs(session)
     assert n_runs == 0
-
     await Scheduler().run_once()
     runs = await models.flow_runs.read_flow_runs(session)
-    assert len(runs) == 3 < Scheduler.max_runs
+
+    assert len(runs) == 3
     expected_dates = await deployment.schedule.get_dates(
         Scheduler.max_runs, end=pendulum.now().add(seconds=Scheduler.max_future_seconds)
     )
