7f5313e3b67bec03d276be88f416b3af2283e3f1
nicholas <znicholasbrown@gmail.com>
2021-09-13 11:48:27 -0400

    Add list and list item classes

--- ui/src/views/Dashboard.vue
+++ ui/src/views/Dashboard.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <h1>Dashboard</h1>
+    <List />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
+import List from '@/components/List/List.vue'
 
 @Options({
-  components: {}
+  components: { List }
 })
 export default class Dashboard extends Vue {
   sayHello(): void {
