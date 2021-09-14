f7cd338f83f64d86b5af846fb994cb7c181f44cb
nicholas <znicholasbrown@gmail.com>
2021-09-14 05:33:28 -0400

    Move color mode selector to its own component, add local storage setting

--- orion-ui/src/views/Settings.vue
+++ orion-ui/src/views/Settings.vue
@@ -4,113 +4,19 @@
 
     <section class="mt-5">
       State color mode:
-      <Select
-        v-model="colorMode"
-        class="my-2 mr-2"
-        title="Color mode selector"
-        placeholder="Color mode"
-        search
-      >
-        <template v-slot:active>
-          <div class="theme-option d-flex align-center">
-            <i class="pi pi-palette-line mr-1" />
-            <span>{{ colorMode }}</span>
-            <div class="theme-palette ml-auto">
-              <div
-                v-for="color in colorMap[colorMode]"
-                :key="color"
-                class="palette-option"
-                :style="{ 'background-color': color }"
-              />
-            </div>
-          </div>
-        </template>
-
-        <Option v-for="mode in colorModes" :key="mode" :value="mode">
-          <span class="theme-option d-flex align-center">
-            <span>{{ mode }}</span>
-            <div class="theme-palette ml-auto">
-              <div
-                v-for="color in colorMap[mode]"
-                :key="color"
-                class="palette-option"
-                :style="{ 'background-color': color }"
-              />
-            </div>
-          </span>
-        </Option>
-      </Select>
+      <StateColorModeSelector />
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-
-const states = [
-  'cancelled',
-  'completed',
-  'failed',
-  'pending',
-  'running',
-  'scheduled'
-]
+import StateColorModeSelector from '@/components/Settings/StateColorModeSelector.vue'
 
 @Options({
-  components: {}
+  components: { StateColorModeSelector }
 })
-export default class Settings extends Vue {
-  colorMode: string = 'Default'
-
-  colorModes: string[] = [
-    'Default',
-    'Achromatomaly',
-    'Achromatopsia',
-    'Protanomaly',
-    'Protaponia',
-    'Deuteranomaly',
-    'Deuteranopia',
-    'Tritanomaly',
-    'Tritanopia'
-  ]
-
-  colorMap: { [key: string]: string[] } = {}
-
-  mounted(): void {
-    // There are some plugins for rollup that make this a little easier but this seems the most straightforward
-    // way if we're creating global css variables from our theme colors
-    const computedStyle = window.getComputedStyle(document.documentElement)
-
-    this.colorModes.forEach((m) => {
-      const mode = m.toLowerCase()
-      this.colorMap[m] = []
-
-      states.forEach((state) => {
-        const ref = `--${state}-${mode}`
-        const color = computedStyle.getPropertyValue(ref)
-        if (color) this.colorMap[m].push(color)
-      })
-    })
-  }
-}
+export default class Settings extends Vue {}
 </script>
 
-<style lang="scss" scoped>
-.theme-option {
-  width: 100%;
-
-  .theme-palette {
-    display: inline-block;
-    align-self: flex-end;
-
-    .palette-option {
-      border-radius: 50%;
-      border: 1px solid #eee;
-      display: inline-block;
-      margin-right: 4px;
-      height: 15px;
-      width: 15px;
-    }
-  }
-}
-</style>
+<style lang="scss" scoped></style>
