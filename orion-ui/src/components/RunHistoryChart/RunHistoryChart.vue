fab4235de906cf386b7055b7b1b6d56d32109a24
nicholas <znicholasbrown@gmail.com>
2021-09-14 08:47:02 -0400

    Add dummy data, scaffold run history chart

---
+++ orion-ui/src/components/RunHistoryChart/RunHistoryChart.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg ref="chart" />
+</template>
+
+<script lang="ts">
+import { Options, Vue, prop } from 'vue-class-component'
+import * as d3 from 'd3'
+
+export interface Bucket {
+  interval_start: Date
+  interval_end: Date
+  states: { [key: string]: number }
+}
+
+class Props {
+  buckets = prop<Bucket[]>({ required: true })
+}
+
+@Options({})
+export default class Schematic extends Vue.with(Props) {}
+</script>
+
+<style lang="scss"></style>
