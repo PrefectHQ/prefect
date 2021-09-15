9f3c8df69dd29c599e4f1b68022f866ce153f2ef
nicholas <znicholasbrown@gmail.com>
2021-09-15 16:12:48 -0400

    ok THERE WE GO

--- orion-ui/src/components/RunHistoryChart/RunHistoryChart.vue
+++ orion-ui/src/components/RunHistoryChart/RunHistoryChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="container">
+  <div ref="container" class="chart-container">
     <svg :id="id" ref="chart" class="run-history-chart" />
   </div>
 </template>
@@ -8,6 +8,7 @@
 import { Options, Vue, prop } from 'vue-class-component'
 import { ref } from 'vue'
 import * as d3 from 'd3'
+import { Series } from 'd3'
 
 const positiveStates: string[] = [
   'COMPLETED',
@@ -16,13 +17,22 @@ const positiveStates: string[] = [
   'PENDING'
 ]
 const mappedPositiveStates: [string, number][] = positiveStates.map(
-  (d: string) => [d, +1]
+  (d: string) => [d, -1]
 )
 
 const negativeStates: string[] = ['FAILED', 'CANCELLED']
 const mappedNegativeStates: [string, number][] = negativeStates.map(
-  (d: string) => [d, -1]
+  (d: string) => [d, +1]
 )
+
+const capR = 2
+const padding = {
+  top: 12,
+  bottom: 12,
+  middle: 12,
+  left: 16,
+  right: 16
+}
 
 const directions: Map<string, number> = new Map([
   ...mappedPositiveStates,
@@ -44,6 +54,7 @@ type TransitionType = d3.TransitionLike<SVGRectElement, unknown>
 type BarType = d3.Selection<SVGRectElement, unknown, HTMLElement, null>
 
 class Props {
+  backgroundColor = prop<String>({ required: false, default: null })
   data = prop<Bucket[]>({ required: true })
 }
 
@@ -118,6 +129,7 @@ export default class RunHistoryChart extends Vue.with(Props) {
   }
 
   handleWindowResize(): void {
+    console.log(this.container)
     this.height = this.container.offsetHeight
     this.width = this.container.offsetWidth
 
@@ -130,77 +142,47 @@ export default class RunHistoryChart extends Vue.with(Props) {
     const end = this.data[this.data.length - 1].interval_end
     this.xScale.domain([new Date(start), new Date(end)]).range([0, this.width])
 
-    console.log(this.series.flat(2), this.series)
+    const flattened = this.series.flat(2)
+    const min = Math.min(...flattened)
+    const max = Math.max(...flattened)
+    const startMin = Math.abs(min) > Math.abs(max)
+
     this.yScale
-      .domain(
-        // TODO: Figure out what the heck the overloads for D3 are supposed to be...
-        /* @ts-ignore */
-        // d3.extent(
-        //   this.buckets
-        //     .map((d) => d.states)
-        //     .map((d) => Object.values(d))
-        //     .flat()
-        // )
-        d3.extent(this.series.flat(2))
-      )
-      .rangeRound([0, this.height / 2])
+      .domain([startMin ? min : 0, startMin ? 0 : max])
+      .rangeRound([
+        padding.top,
+        this.height / 2 - padding.bottom - padding.middle
+      ])
   }
 
   createChart(): void {
     this.svg = d3.select('.run-history-chart')
+    const paddingY = padding.top + padding.middle + padding.bottom
+    const paddingX = padding.left + padding.right
 
-    this.svg?.attr('viewbox', `0, 0, ${this.width}, ${this.height}`)
+    this.svg?.attr(
+      'viewbox',
+      `0, 0, ${this.width - paddingX}, ${this.height - paddingY}`
+    )
+
+    this.svg
+      .append('rect')
+      .attr('fill', this.backgroundColor && `var(--${this.backgroundColor})`)
+      .attr('rx', 4)
+      .attr('width', '100%')
+      .attr('height', '100%')
 
     this.barSelection = this.svg.append('g')
 
     // TODO: Remove this guidelines (for tesitng purposes only)
-    this.svg
-      .append('line')
-      .attr('x1', 0)
-      .attr('x2', this.width)
-      .attr('y1', this.height / 2)
-      .attr('y2', this.height / 2)
-      .attr('stroke-width', 1)
-      .attr('stroke', 'var(--inactive)')
-  }
-
-  updateBars(
-    anchor: SelectionType,
-    ...args: any
-  ): BarType | TransitionType | undefined {
-    if (!this.barSelection) return
-    const maxWidth = this.width / this.data.length / 2
-
-    return (
-      anchor
-        .append('rect')
-        .attr('width', Math.min(6, maxWidth))
-        // TODO: Figure out what the heck the overloads for D3 are supposed to be...
-        /* @ts-ignore */
-        .attr('height', ([state, count]: [string, number], i: number) => {
-          console.log(i)
-          const seriesSlot = this.seriesMap.get(state)?.[i]
-          if (!seriesSlot) return 0
-
-          return this.yScale(seriesSlot[1] - seriesSlot[0])
-          // return this.height / 2 * arr?.reduce((curr, acc) => acc + curr[0], 0)
-        })
-        // TODO: Figure out what the heck the overloads for D3 are supposed to be...
-        /* @ts-ignore */
-        .attr('y', ([state, count]: [string, number], i: number) => {
-          const seriesSlot = this.seriesMap.get(state)?.[i]
-          if (!seriesSlot) return 0
-          // if (state == 'FAILED') return this.height / 2
-          // return 0
-          return this.yScale(seriesSlot[0])
-        })
-        .attr(
-          'class',
-          // TODO: Figure out what the heck the overloads for D3 are supposed to be...
-          /* @ts-ignore */
-          ([state, count]: [string, number]) => state.toLowerCase() + '-fill'
-        )
-    )
+    // this.svg
+    //   .append('line')
+    //   .attr('x1', 0)
+    //   .attr('x2', this.width)
+    //   .attr('y1', this.height / 2 - padding.top / 2)
+    //   .attr('y2', this.height / 2 - padding.top / 2)
+    //   .attr('stroke-width', padding.middle / 4)
+    //   .attr('stroke', 'rgba(0, 0, 0, 0.05')
   }
 
   updateBuckets(): void {
@@ -214,53 +196,8 @@ export default class RunHistoryChart extends Vue.with(Props) {
       .data(this.data)
       .join('g')
       .attr('id', (d: Bucket | any) => d.interval_start.toString())
-      .style(
-        'transform',
-        (d: Bucket) =>
-          `translate(${
-            this.xScale(new Date(d.interval_start)) +
-            this.width / this.data.length / 4
-          }px)`
-      )
-      // .join(
-      //   // (selection: any) =>
-      //   //   selection
-      //   //     .append('g')
-      //   //     // .attr('id', (d: Bucket | any) => d.interval_start.toString())
-      //   //     .style(
-      //   //       'transform',
-      //   //       (d: Bucket) =>
-      //   //         `translate(${
-      //   //           this.xScale(new Date(d.interval_start)) +
-      //   //           this.width / this.data.length / 4
-      //   //         }px)`
-      //   //     ),
-      //   (selection: any) =>
-      //     selection
-      //       .attr('id', (d: Bucket | any) => d.interval_start.toString())
-      //       .style(
-      //         'transform',
-      //         (d: Bucket) =>
-      //           `translate(${
-      //             this.xScale(new Date(d.interval_start)) +
-      //             this.width / this.data.length / 4
-      //           }px)`
-      //       ),
-      //   // (selection: d3.Selection): SelectionType => {
-      //   //   console.log(selection, typeof selection)
-      //   //   const g = selection.append('g')
-
-      //   //   // g.attr('id', (d: Bucket | any) => d.interval_start.toString())
-
-      //   //   // g.selectAll('rect')
-      //   //   //   .data((d: Bucket) => d.states)
-      //   //   //   .join(this.updateBars)
-      //   //   return g
-      //   // },
-      //   () => {},
-      //   () => {}
-      // )
-      .selectAll('rect')
+      .style('transform', `translate(${padding.left}px, ${padding.top}px)`)
+      .selectAll('path')
       /* @ts-ignore */
       .data((d: Bucket, i: number) => {
         const arr = Object.entries(d.states).map(([state, count]) => {
@@ -268,98 +205,101 @@ export default class RunHistoryChart extends Vue.with(Props) {
         })
         return arr
       })
-      .join('rect')
-      .attr('width', Math.min(6, maxWidth))
-      // TODO: Figure out what the heck the overloads for D3 are supposed to be...
+      .join('path')
       /* @ts-ignore */
-      .attr('height', (d: any) => {
-        const seriesSlot = this.seriesMap.get(d.state)?.[d.bucket_key]
-        console.log(seriesSlot, d)
-        if (!seriesSlot) return 0
+      .attr('d', (d: any, i: number) => {
+        const seriesSlot = this.seriesMap.get(d.state)![d.bucket_key]
+        const biasIndex = directions.get(d.state)
 
-        return seriesSlot[1] - seriesSlot[0]
-      })
-      // TODO: Figure out what the heck the overloads for D3 are supposed to be...
-      /* @ts-ignore */
-      .attr('y', (d: any) => {
-        const seriesSlot = this.seriesMap.get(d.state)?.[d.bucket_key]
-        if (!seriesSlot) return 0
-        return seriesSlot[1]
+        if (!seriesSlot || !biasIndex) return
+        const data = this.data.find(
+          /* @ts-ignore */
+          (_d) => _d.interval_start == seriesSlot.data.interval_start
+        )
+        const states = data?.states || []
+        const stateEntries = Object.entries(states)
+
+        const arr = biasIndex > 0 ? negativeStates : positiveStates
+        const stateIndex = arr.findIndex((s) => s == d.state)
+
+        const otherStates = stateEntries.filter(
+          ([state, count]) => state !== d.state && arr.includes(state)
+        )
+        const adjustedArr = arr.filter(
+          (s) => stateEntries.find((_s) => _s[0] == s)?.[1] || 0 > 0
+        )
+        const sumCountOther = otherStates.reduce(
+          (acc, curr) => acc + curr[1],
+          0
+        )
+
+        /*
+          Round both top and bottom corners if:
+            - Is the only bar in the series
+
+          Round top corners if:
+            - Is the first bar in the positive series
+            - Is the last bar in the negative series
+
+          Round bottom corners if:
+            - Is the last bar in the positive series
+            - Is the first bar in the negative series
+        */
+
+        let showCapTop = false
+        let showCapBottom = false
+
+        if (sumCountOther == 0) {
+          showCapTop = true
+          showCapBottom = true
+        } else if (biasIndex < 0) {
+          if (stateIndex === adjustedArr.length - 1) showCapBottom = true
+          if (stateIndex === 0) showCapTop = true
+        } else if (biasIndex > 0) {
+          if (stateIndex === adjustedArr.length - 1) showCapBottom = true
+          if (stateIndex === 0) showCapTop = true
+        }
+
+        const width = Math.min(10, maxWidth) / 2
+
+        const xStart =
+          /* @ts-ignore */
+          this.xScale(new Date(seriesSlot.data.interval_start)) + maxWidth / 2
+        const yStart =
+          this.yScale(seriesSlot[0]) +
+          padding.top +
+          (biasIndex > 0 ? padding.middle : 0) +
+          (biasIndex < 0 ? capR / 2 : 0)
+        const height = this.yScale(seriesSlot[1]) - this.yScale(seriesSlot[0])
+
+        if (height == 0) return ''
+
+        const capTop = showCapTop
+          ? `a ${capR} ${capR} 0 1 0 -${width} 0`
+          : `a 0 0 0 1 0 -${width} 0`
+        const capBottom = showCapBottom
+          ? `a ${capR} ${capR} 0 1 0 ${width} 0`
+          : `a 0 0 0 1 0 ${width} 0`
+
+        return `M${xStart},${yStart} v${height} ${capBottom ? capBottom : ''} ${
+          capBottom ? '' : `h${width}`
+        } v-${height} ${capTop ? capTop : ''}`
       })
       .attr(
         'class',
         // TODO: Figure out what the heck the overloads for D3 are supposed to be...
         /* @ts-ignore */
-        (d: any) => d.state.toLowerCase() + '-fill'
+        (d: any) => d.state.toLowerCase() + '-fill radii'
       )
-    // /* @ts-ignore */
-    // .join(
-    //   this.updateBars,
-    //   () => {},
-    //   () => {}
-    // )
-    //   .data(this.visibleLinks)
-    //   .join(
-    //     // enter
-    //     (selection: any) =>
-    //       selection
-    //         .append('path')
-    //         .attr('id', (d: Link) => d.source.id + '-' + d.target.id)
-    //         .attr('class', (d: Link) =>
-    //           this.useLinearGradient
-    //             ? d.source.data.state == 'pending'
-    //               ? d.source.data.state
-    //               : null
-    //             : d.source.data.state
-    //         )
-    //         .style('stroke', (d: Link, i: number) =>
-    //           this.useLinearGradient
-    //             ? `url("#${d.source.data.name + i}")`
-    //             : null
-    //         )
-    //         .style('stroke-width', (d: Link) =>
-    //           d.source.data.state == 'pending' ? 5 : 20
-    //         )
-    //         .attr('d', (d: Link) =>
-    //           this.line([
-    //             [d.source.cx, d.source.cy],
-    //             [d.target.cx, d.target.cy]
-    //           ])
-    //         )
-    //         .style('opacity', 1),
-    //     // update
-    //     (selection: any) =>
-    //       selection
-    //         .attr('id', (d: Link) => d.source.id + '-' + d.target.id)
-    //         .attr('class', (d: Link) =>
-    //           this.useLinearGradient
-    //             ? d.source.data.state == 'pending'
-    //               ? d.source.data.state
-    //               : null
-    //             : d.source.data.state
-    //         )
-    //         .style('stroke', (d: Link, i: number) =>
-    //           this.useLinearGradient
-    //             ? `url("#${d.source.data.name + i}")`
-    //             : null
-    //         )
-    //         .style('stroke-width', (d: Link) =>
-    //           d.source.data.state == 'pending' ? 5 : 20
-    //         )
-    //         .attr('d', (d: Link) =>
-    //           this.line([
-    //             [d.source.cx, d.source.cy],
-    //             [d.target.cx, d.target.cy]
-    //           ])
-    //         )
-    //         .style('opacity', 1),
-    //     // exit
-    //     (selection: any) => selection.remove()
-    //   )
   }
 }
 </script>
 
 <style lang="scss">
 @use '@/styles/components/run-history--chart.scss';
+
+.chart-container {
+  height: 100%;
+  width: 100%;
+}
 </style>
