562a07426a598066637e19b106b2b526fc250598
Michael Adkins <michael@prefect.io>
2021-09-15 15:03:38 -0500

    Retain the task group for the entire agent run and submit execution to a subprocess

--- src/prefect/agent.py
+++ src/prefect/agent.py
@@ -1,5 +1,8 @@
+from typing import Callable, List
+
 import pendulum
-from anyio import create_task_group, sleep
+import anyio
+import anyio.to_process
 
 import prefect.engine
 from prefect.client import OrionClient, inject_client
@@ -8,40 +11,56 @@ from prefect.orion.schemas.core import FlowRun
 from prefect.orion.schemas.filters import FlowRunFilter
 from prefect.orion.schemas.states import StateType
 from prefect.orion.utilities.database import UUID
+from prefect.utilities.asyncio import sync_compatible
 
 
-async def submit_local_flow_run(flow_run: FlowRun):
-    print(f"Submitting flow run '{flow_run.id}' for local execution")
-    await execute_flow_run(flow_run.id)
+async def submit_local_flow_run(flow_run: FlowRun, task_group) -> None:
+    task_group.start_soon(anyio.to_process.run_sync, execute_flow_run, flow_run.id)
+    print(f"Submitted {flow_run.id}")
 
 
 @inject_client
 async def run_agent(
     client: OrionClient,
-    prefetch_seconds=10,
-    query_interval_seconds=2,
-    submit_fn=submit_local_flow_run,
-):
-    submitted_flow_runs = set()
-    while True:
-        flow_runs = await client.read_flow_runs(
-            flow_runs=FlowRunFilter(
-                states=[StateType.SCHEDULED],
-                start_time_before=pendulum.now("utc").add(seconds=prefetch_seconds),
-            )
+    prefetch_seconds: int = 10,
+    query_interval_seconds: int = 2,
+    submit_fn: Callable[[FlowRun], None] = submit_local_flow_run,
+) -> None:
+    submitted_ids = set()
+
+    async with anyio.create_task_group() as submission_tg:
+        while True:
+            ready_runs = await query_for_ready_flow_runs(client, prefetch_seconds)
+
+            # Filter out runs that are already being submitted but maintain ordering
+            submittable_runs = [
+                run for run in ready_runs if run.id not in submitted_ids
+            ]
+
+            for flow_run in submittable_runs:
+                print(f"Submitting {flow_run.id}")
+                submission_tg.start_soon(submit_fn, flow_run, submission_tg)
+                submitted_ids.add(flow_run.id)
+
+            # Wait until the next query
+            await anyio.sleep(query_interval_seconds)
+
+
+async def query_for_ready_flow_runs(
+    client: OrionClient, prefetch_seconds: int
+) -> List[FlowRun]:
+    return await client.read_flow_runs(
+        flow_runs=FlowRunFilter(
+            states=[StateType.SCHEDULED],
+            start_time_before=pendulum.now("utc").add(seconds=prefetch_seconds),
         )
-        async with create_task_group() as submit_tasks:
-            for flow_run in flow_runs:
-                if flow_run.id in submitted_flow_runs:
-                    continue
-                submitted_flow_runs.add(flow_run.id)
-                submit_tasks.start_soon(submit_fn, flow_run)
-
-        await sleep(query_interval_seconds)
+    )
 
 
+@sync_compatible
 @inject_client
 async def execute_flow_run(flow_run_id: UUID, client: OrionClient):
+    print(f"Preparing to execute {flow_run_id}")
     flow_run = await client.read_flow_run(flow_run_id)
     if not flow_run.deployment_id:
         raise ValueError(
@@ -62,7 +81,7 @@ async def execute_flow_run(flow_run_id: UUID, client: OrionClient):
 
     await prefect.engine.begin_flow_run(
         flow,
-        parameters={},
+        parameters={},  # TODO: Include parameters from deployment
         client=client,
         flow_run_id=flow_run_id,
     )
