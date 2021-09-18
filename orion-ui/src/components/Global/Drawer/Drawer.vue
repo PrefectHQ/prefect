46de298713b2df276dc83f3fdb716b4d57fa7ea2
nicholas <znicholasbrown@gmail.com>
2021-09-18 16:43:42 -0400

    Scaffold drawer component

---
+++ orion-ui/src/components/Global/Drawer/Drawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <teleport :to="target">
+    <aside class="drawer">
+      <slot />
+    </aside>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { Vue, Options, prop } from 'vue-class-component'
+
+class Props {
+  target = prop<string>({ default: '#app', required: false })
+}
+
+@Options({})
+export default class Drawer extends Vue.with(Props) {}
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/components/drawer.scss';
+</style>
