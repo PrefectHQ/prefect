79c28f71290a8991cf8d3a3941b0855584fb3130
nicholas <znicholasbrown@gmail.com>
2021-09-19 16:25:39 -0400

    Scaffold timeline component, use Select components for schematic search

---
+++ orion-ui/src/views/Timeline.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <div class="search-input" tabindex="-1">
+        <Select v-model="selected" :search="true" class="ml-2">
+          <Option v-for="(value, key) in datasets" :key="key" :value="key">
+            {{ key }}
+          </Option>
+        </Select>
+      </div>
+
+      <div>
+        <Timeline :items="dataset" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+
+import Timeline from '../components/Timeline/Timeline.vue'
+import { default as dataset1 } from '../util/schematics/62_nodes.json'
+import { default as dataset2 } from '../util/schematics/50_linear_nodes.json'
+import { default as dataset3 } from '../util/schematics/etl.json'
+import { default as dataset4 } from '../util/schematics/1000_nodes.json'
+import { default as dataset5 } from '../util/schematics/15_nodes.json'
+import { default as dataset6 } from '../util/schematics/3_nodes.json'
+import { default as dataset7 } from '../util/schematics/61_cluster_nodes.json'
+
+@Options({
+  components: { Timeline }
+})
+export default class Schematics extends Vue {
+  selected: string = '3 Nodes: ETL'
+
+  search: string = ''
+  showOptions: boolean = false
+
+  datasets: { [key: string]: Items } = {
+    '3 Nodes: ETL': dataset3,
+    '50 Linear Nodes': dataset2,
+    '62 Random Nodes': dataset1,
+    '1000 Random Nodes': dataset4,
+    '15 Random Nodes': dataset5,
+    '3 Nodes': dataset6,
+    '61 Cluster Nodes': dataset7
+  }
+
+  get dataset(): Items {
+    return this.datasets[this.selected]
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
