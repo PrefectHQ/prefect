401ed07e9460064eaa93a315df749e7e3bde59e9
nicholas <znicholasbrown@gmail.com>
2021-09-20 11:49:08 -0400

    Make sure x scale grid lines actually match the time scale properly lol

--- orion-ui/src/views/Timeline.vue
+++ orion-ui/src/views/Timeline.vue
@@ -39,12 +39,13 @@ import { default as dataset4 } from '../util/schematics/1000_nodes.json'
 import { default as dataset5 } from '../util/schematics/15_nodes.json'
 import { default as dataset6 } from '../util/schematics/3_nodes.json'
 import { default as dataset7 } from '../util/schematics/61_cluster_nodes.json'
+import { default as dataset8 } from '../util/schematics/25_nodes_time_corrected.json'
 
 @Options({
   components: { Timeline }
 })
 export default class TimelineView extends Vue {
-  selected: string = '15 Random Nodes'
+  selected: string = '3 Nodes: ETL'
 
   search: string = ''
   showOptions: boolean = false
@@ -59,11 +60,16 @@ export default class TimelineView extends Vue {
     '1000 Random Nodes': dataset4,
     '15 Random Nodes': dataset5,
     '3 Nodes': dataset6,
-    '61 Cluster Nodes': dataset7
+    '61 Cluster Nodes': dataset7,
+    '25 Nodes': dataset8
   }
 
   get dataset(): Items {
     return this.datasets[this.selected]
+  }
+
+  mounted() {
+    console.log(this.datasets)
   }
 }
 </script>
