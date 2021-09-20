26dbbec4b920121ec6813865379ee95c70df4797
Zach Angell <zachary.james.angell@gmail.com>
2021-09-20 14:12:52 -0400

    Cleanup filter description and messages

--- tests/orion/schemas/test_filters.py
+++ tests/orion/schemas/test_filters.py
@@ -5,24 +5,26 @@ from prefect.orion.schemas import filters
 
 
 @pytest.mark.parametrize(
-    "Filter",
+    "TagsFilter",
     [filters.FlowFilterTags, filters.FlowRunFilterTags, filters.TaskRunFilterTags],
 )
-def test_all_and_is_null_filter_validation_does_not_allow_all_and_is_null(Filter):
-    with pytest.raises(ValueError, match="all_ with is_null_ = True"):
-        Filter(all_=["foo"], is_null_=True)
+def test_tags_filter_validation_does_not_allow_all_and_is_null(TagsFilter):
+    with pytest.raises(
+        ValueError, match="Cannot provide tags all_ with is_null_ = True"
+    ):
+        TagsFilter(all_=["foo"], is_null_=True)
 
 
 def test_deployment_ids_filter_validation_does_not_allow_any_and_is_null():
     with pytest.raises(
-        ValueError, match="Cannot provide deployment ids any_ with is_null_ = True"
+        ValueError, match="Cannot provide deployment_ids any_ with is_null_ = True"
     ):
         filters.FlowRunFilterDeploymentIds(any_=[uuid4()], is_null_=True)
 
 
 def test_parent_task_run_ids_filter_validation_does_not_allow_any_and_is_null():
     with pytest.raises(
-        ValueError, match="Cannot provide parent task run ids any_ with is_null_ = True"
+        ValueError, match="Cannot provide parent_task_run_ids any_ with is_null_ = True"
     ):
         filters.FlowRunFilterParentTaskRunIds(any_=[uuid4()], is_null_=True)
 
@@ -37,5 +39,5 @@ def test_parent_task_run_ids_filter_validation_does_not_allow_any_and_is_null():
     ],
 )
 def test_time_filters_before_must_be_greater_than_after(TimeFilter):
-    with pytest.raises(ValueError, match="before_ must be greater than after_"):
+    with pytest.raises(ValueError, match="time before_ must be greater than after_"):
         TimeFilter(before_=pendulum.now("UTC"), after_=pendulum.now("UTC").add(days=1))
