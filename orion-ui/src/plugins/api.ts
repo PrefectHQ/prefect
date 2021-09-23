e135a049178b2f713a419248b1d917ae8be7b780
nicholas <znicholasbrown@gmail.com>
2021-09-23 19:52:40 -0400

    Clear timeout instead of interval

--- orion-ui/src/plugins/api.ts
+++ orion-ui/src/plugins/api.ts
@@ -100,7 +100,7 @@ export class Query {
   async startPolling(): Promise<void> {
     // We clear the interval to make sure we're not polling
     // more than expected
-    if (this.interval) clearInterval(this.interval)
+    if (this.interval) clearTimeout(this.interval)
     if (!this.pollInterval) return
     await this.refetch()
     this.interval = setTimeout(() => this.startPolling(), this.pollInterval)
