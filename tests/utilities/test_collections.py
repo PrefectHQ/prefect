1383180420ad7b7e85d10a0b98169e51d4257a71
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2021-09-24 14:23:09 -0400

    Abstract collection visiting into a function

--- tests/utilities/test_collections.py
+++ tests/utilities/test_collections.py
@@ -1,6 +1,12 @@
+import pydantic
 import pytest
+from dataclasses import dataclass
 
-from prefect.utilities.collections import dict_to_flatdict, flatdict_to_dict
+from prefect.utilities.collections import (
+    dict_to_flatdict,
+    flatdict_to_dict,
+    visit_collection,
+)
 
 
 class TestFlatDict:
@@ -20,3 +26,41 @@ class TestFlatDict:
 
     def test_flatdict_to_dict(self, nested_dict):
         assert flatdict_to_dict(dict_to_flatdict(nested_dict)) == nested_dict
+
+
+async def negative_even_numbers(x):
+    if isinstance(x, int) and x % 2 == 0:
+        return -x
+    return x
+
+
+@dataclass
+class SimpleDataclass:
+    x: int
+    y: int
+
+
+class SimplePydantic(pydantic.BaseModel):
+    x: int
+    y: int
+
+
+class TestVisitCollection:
+    @pytest.mark.parametrize(
+        "inp,expected",
+        [
+            (3, 3),
+            (4, -4),
+            ([3, 4], [3, -4]),
+            ((3, 4), (3, -4)),
+            ([3, 4, [5, [6]]], [3, -4, [5, [-6]]]),
+            ({3: 4, 6: 7}, {3: -4, -6: 7}),
+            ({3: [4, {6: 7}]}, {3: [-4, {-6: 7}]}),
+            ({3, 4, 5}, {3, -4, 5}),
+            (SimpleDataclass(x=1, y=2), SimpleDataclass(x=1, y=-2)),
+            (SimplePydantic(x=1, y=2), SimplePydantic(x=1, y=-2)),
+        ],
+    )
+    async def test_visit_collection(self, inp, expected):
+        result = await visit_collection(inp, visit_fn=negative_even_numbers)
+        assert result == expected
