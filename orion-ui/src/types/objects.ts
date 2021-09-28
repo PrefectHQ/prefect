999c79fa2fe7fcb80ce61dd2ddfe242f00b83355
nicholas <znicholasbrown@gmail.com>
2021-09-28 13:16:40 -0400

    Update task run object definition

--- orion-ui/src/types/objects.ts
+++ orion-ui/src/types/objects.ts
@@ -29,7 +29,7 @@ export declare interface FlowRun {
   flow_id: string
   flow_version: string
   idempotency_key: string
-  next_scheduled_start_time: string
+  next_scheduled_start_time: string | null
   parameters: { [key: string]: any }
   auto_scheduled: boolean
   context: { [key: string]: any }
@@ -53,9 +53,25 @@ export declare interface FlowRun {
 export declare interface TaskRun {
   id: string
   flow_run_id: string
+  cache_expiration: string
+  cache_key: string
+  created: string
+  dynamic_key: string
+  emperical_policy: { [key: string]: any }
+  estimated_run_time: number
+  estimated_start_time_delta: number
+  total_run_time: number
+  expected_start_time: string
+  next_scheduled_start_time: string | null
+  run_count: number
   name: string
+  task_unputs: { [key: string]: any }
+  task_key: string
+  task_version: string
+  updated: string
   start_time: string
   end_time: string
+  state_id: string
   state_type: string
   state: State
   duration: number
