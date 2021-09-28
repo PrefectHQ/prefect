34508c0428c1cbec4ed08958c697825c1bc2d06a
nicholas <znicholasbrown@gmail.com>
2021-09-28 12:52:17 -0400

    Update flow run and state object definitions

--- orion-ui/src/types/objects.ts
+++ orion-ui/src/types/objects.ts
@@ -18,23 +18,36 @@ export declare interface State {
   type: string
   message: string
   state_details: { [key: string]: any }
+  data: { [key: string]: any }
   timestamp: string
   name: string
 }
 
 export declare interface FlowRun {
   id: string
-  flow_id: string
-  auto_scheduled: boolean
   deployment_id: string
-  duration: number
+  flow_id: string
+  flow_version: string
+  idempotency_key: string
+  next_scheduled_start_time: string
+  parameters: { [key: string]: any }
+  auto_scheduled: boolean
+  context: { [key: string]: any }
+  emperical_config: { [key: string]: any }
+  emperical_policy: { [key: string]: any }
+  estimated_run_time: number
+  estimated_start_time_delta: number
+  total_run_time: number
   start_time: string
   end_time: string
   name: string
+  parent_task_run_id: string
+  state_id: string
   state_type: string
   state: State
   tags: string[]
   task_run_count: number
+  updated: string
 }
 
 export declare interface TaskRun {
