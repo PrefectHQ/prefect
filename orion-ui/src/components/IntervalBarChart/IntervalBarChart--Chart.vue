e9b91dd2d2361c1e6a61d5c785ab1fe0217b24e4
nicholas <znicholasbrown@gmail.com>
2021-09-30 00:22:27 -0400

    Update barchart -> intervalchart

---
+++ orion-ui/src/components/IntervalBarChart/IntervalBarChart--Chart.vue
@@ -0,0 +1,146 @@
+<template>
+  <div ref="container" class="chart-container">
+    <svg :id="id" ref="chart"></svg>
+
+    <div class="bucket-container">
+      <div
+        v-for="item in items"
+        :key="item.interval_start"
+        class="interval-bucket"
+        :style="calculateBucketPosition(item)"
+        tabindex="0"
+      >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, prop, mixins } from 'vue-class-component'
+import * as d3 from 'd3'
+import { D3Base } from '@/components/Visualizations/D3Base'
+
+export interface Item {
+  interval_start: string
+  interval_end: string
+  value: number
+}
+export type ItemCollection = Item[]
+
+class Props {
+  intervalSeconds = prop<number>({ required: true })
+  intervalStart = prop<Date>({ required: true })
+  intervalEnd = prop<Date>({ required: true })
+  backgroundColor = prop<string>({ required: false, default: null })
+  items = prop<ItemCollection>({ required: true })
+  showAxis = prop<boolean>({ required: false, default: false, type: Boolean })
+}
+
+const capR = 2
+
+type SelectionType = d3.Selection<SVGGElement, unknown, HTMLElement, null>
+
+@Options({})
+export default class BarChart extends mixins(D3Base).with(Props) {
+  xScale = d3.scaleTime()
+  yScale = d3.scaleLinear()
+
+  barSelection: SelectionType = null as unknown as d3.Selection<
+    SVGGElement,
+    unknown,
+    HTMLElement,
+    null
+  >
+
+  padding = {
+    top: 16,
+    bottom: 16,
+    middle: 0,
+    left: 0,
+    right: 0
+  }
+
+  get maxValue(): number {
+    return Math.max.apply(
+      null,
+      this.items.map((item: Item) => item.value)
+    )
+  }
+
+  get r(): number {
+    return Math.min(capR, Math.min(10, this.width / this.items.length / 2) / 2)
+  }
+
+  createChart(): void {
+    this.svg = d3.select(`#${this.id}`)
+
+    this.svg.attr(
+      'viewbox',
+      `0, 0, ${this.width - this.paddingX}, ${this.height - this.paddingY}`
+    )
+
+    this.svg
+      .append('rect')
+      .attr(
+        'fill',
+        this.backgroundColor ? `var(--${this.backgroundColor})` : 'transparent'
+      )
+      .attr('rx', 4)
+      .attr('width', '100%')
+      .attr('height', '100%')
+
+    this.barSelection = this.svg.append('g')
+  }
+
+  updateScales(): void {
+    // Generate x scale
+    const start = this.intervalStart
+    const end = this.intervalEnd
+
+    this.xScale.domain([start, end]).range([0, this.width - this.paddingX])
+
+    // Generate y scale
+    this.yScale
+      .domain([0, this.maxValue])
+      .range([this.padding.top, this.height - this.paddingY])
+  }
+
+  get barWidth(): number {
+    return Math.floor(
+      Math.min(10, (this.width - this.paddingX) / this.items.length / 2)
+    )
+  }
+
+  calculateBucketPosition(item: Item): { [key: string]: string } {
+    const height = this.yScale(item.value)
+    const top = this.height - this.padding.bottom - height
+    const left = this.xScale(new Date(item.interval_start)) + this.padding.left
+
+    return {
+      height: height + 'px',
+      left: left + 'px',
+      top: top + 'px',
+      width: this.barWidth + 'px'
+    }
+  }
+
+  resize(): void {
+    this.svg.attr('viewbox', `0, 0, ${this.width}, ${this.height}`)
+    this.updateScales()
+  }
+
+  mounted(): void {
+    this.createChart()
+    this.updateScales()
+  }
+
+  beforeUpdate(): void {
+    if (!this.svg || !this.barSelection) this.createChart()
+    this.updateScales()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/components/interval-bar-chart--chart.scss';
+</style>
