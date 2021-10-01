dce2d97edc429d21d9cba12a7d6fc5441c30bf5b
nicholas <znicholasbrown@gmail.com>
2021-10-01 03:14:53 -0400

    Add details card

--- orion-ui/src/views/FlowRun.vue
+++ orion-ui/src/views/FlowRun.vue
@@ -16,7 +16,53 @@
     </div>
 
     <div class="main-grid">
-      <Card class="details" shadow="sm">details</Card>
+      <Card class="details" shadow="sm">
+        <div class="d-flex justify-space-between align-center py-1 px-2">
+          <div>
+            <div class="d-flex align-center">
+              <span
+                class="run-state correct-text caption mr-1"
+                :class="state.type?.toLowerCase() + '-bg'"
+              >
+                {{ state.name }}
+              </span>
+
+              <Tag
+                v-for="tag in tags"
+                :key="tag"
+                color="secondary-pressed"
+                class="font--primary caption font-weight-semibold mr-1"
+                icon="pi-label"
+                flat
+              >
+                {{ tag }}
+              </Tag>
+            </div>
+
+            <div
+              class="
+                caption
+                font--primary font-weight-semibold
+                text--grey-40
+                mt-1
+              "
+            >
+              <span>
+                Deployment:
+                <span class="text--grey-80">{{ deployment.name }}</span>
+              </span>
+              <span class="ml-1">
+                Results:
+                <span class="text--grey-80">{{ location }}</span></span
+              >
+            </div>
+          </div>
+
+          <div class="font--secondary">
+            {{ duration }}
+          </div>
+        </div>
+      </Card>
       <Card class="timeline" shadow="sm">timeline</Card>
       <Card class="schematic" shadow="sm">schematic</Card>
     </div>
@@ -90,8 +136,10 @@
 
 <script lang="ts" setup>
 import { Api, Query, Endpoints, BaseFilter } from '@/plugins/api'
+import { State, FlowRun, Deployment } from '@/typings/objects'
 import { computed, onBeforeUnmount, onBeforeMount, ref, Ref } from 'vue'
 import { useRoute } from 'vue-router'
+import { secondsToApproximateString } from '@/util/util'
 
 const route = useRoute()
 
@@ -108,9 +156,14 @@ const flowRunBase: Query = await Api.query({
 }).fetch()
 
 const flowId = flowRunBase.response.value.flow_id
+const deploymentId = flowRunBase.response.value.deployment_id
 
 const flowFilter = {
   id: flowId
+}
+
+const deploymentFilter = {
+  id: deploymentId
 }
 
 const taskRunsFilter = computed<BaseFilter>(() => {
@@ -138,6 +191,10 @@ const queries: { [key: string]: Query } = {
     endpoint: Endpoints.flow,
     body: flowFilter
   }),
+  deployment: Api.query({
+    endpoint: Endpoints.deployment,
+    body: deploymentFilter
+  }),
   task_runs_count: Api.query({
     endpoint: Endpoints.task_runs_count,
     body: taskRunsFilter,
@@ -159,8 +216,24 @@ const flow = computed(() => {
   return queries.flow.response?.value || {}
 })
 
-const flowRun = computed(() => {
+const deployment = computed<Deployment>(() => {
+  return queries.deployment.response?.value || {}
+})
+
+const location = computed(() => {
+  return deployment.value?.flow_data?.blob
+})
+
+const flowRun = computed<FlowRun>(() => {
   return flowRunBase.response?.value || {}
+})
+
+const state = computed<State>(() => {
+  return flowRun.value?.state
+})
+
+const tags = computed(() => {
+  return flowRun.value?.tags || []
 })
 
 const crumbs = computed(() => {
@@ -169,6 +242,14 @@ const crumbs = computed(() => {
 
 const taskRunsCount = computed(() => {
   return queries.task_runs_count.response?.value || 0
+})
+
+const duration = computed(() => {
+  return state.value.type == 'PENDING' || state.value.type == 'SCHEDULED'
+    ? '--'
+    : flowRun.value.total_run_time
+    ? secondsToApproximateString(flowRun.value.total_run_time)
+    : secondsToApproximateString(flowRun.value.estimated_run_time)
 })
 
 // This cleanup is necessary since the initial flow run query isn't
@@ -181,29 +262,6 @@ onBeforeUnmount(() => {
 onBeforeMount(() => {
   resultsTab.value = route.hash?.substr(1) || 'flows'
 })
-// const id = computed(() => {
-//   return route.params.id
-// })
-
-// const baseFilter = computed(() => {
-//   return {
-//     flows: {}
-//   }
-// })
-
-// const flowFilter = computed(() => {
-//   return {
-//     id: flow.value.id
-//   }
-// })
-
-// const flow = computed(() => {
-//   return queries.flow.response?.value.flow_id
-// })
-
-// const flowRun = computed(() => {
-//   return queries.flow_run.response?.value
-// })
 </script>
 
 <style lang="scss" scoped>
