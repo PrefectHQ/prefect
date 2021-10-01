341f81bac3199e1093ee0470213c6f309e9e21a5
nicholas <znicholasbrown@gmail.com>
2021-10-01 13:14:53 -0400

    Add min height to breadcrumbs to prevent cutoff text

--- orion-ui/src/components/Global/ResultsLists/ResultsList.vue
+++ orion-ui/src/components/Global/ResultsLists/ResultsList.vue
@@ -39,7 +39,12 @@ const items = ref<any[]>([])
 const itemRefs = shallowRef<{ [key: string]: Element }>({})
 
 const filter_ = computed(() => {
-  return { ...props.filter, limit: limit.value, offset: offset.value }
+  return {
+    ...props.filter,
+    limit: limit.value,
+    offset: offset.value
+    // sort: 'EXPECTED_START_TIME_ASC'
+  }
 })
 
 const getData = async () => {
