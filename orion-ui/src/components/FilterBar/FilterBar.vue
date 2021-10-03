6dc5c67c1cf6f9a1c0345a0a13e3ebc12bc6131f
nicholas <znicholasbrown@gmail.com>
2021-10-03 01:19:27 -0400

    Make sure filter menus don't move other elements

--- orion-ui/src/components/FilterBar/FilterBar.vue
+++ orion-ui/src/components/FilterBar/FilterBar.vue
@@ -1,21 +1,42 @@
 <template>
   <div class="bar d-flex font--secondary" :class="{ detached: detached }">
-    <button class="filter-button objects">
-      <span v-breakpoints="'sm'" class="mr-1">Flow Runs</span>
-      <i class="pi pi-arrow-drop-down-fill" />
-    </button>
+    <div class="object-container">
+      <button
+        class="filter-button objects text--grey-80"
+        @click="showObjectMenu = !showObjectMenu"
+      >
+        <span v-breakpoints="'sm'" class="mr-1">Flow Runs</span>
+        <i class="pi pi-arrow-drop-down-fill" />
+      </button>
+
+      <ObjectMenu
+        v-if="showObjectMenu"
+        v-model="object"
+        class="filter-menu"
+        @close="showObjectMenu = false"
+      />
+    </div>
 
     <div
-      class="search-input px-2 flex-grow-1 d-flex align-center font--primary"
+      class="
+        search-input
+        px-2
+        flex-grow-1 flex-shrink-0
+        d-flex
+        align-center
+        font--primary
+      "
     >
       <i class="pi pi-search-line mr-1" />
       <input v-model="search" class="flex-grow-1" placeholder="Search..." />
     </div>
 
-    <button class="filter-button filters">
-      <i class="pi pi-filter-3-line" />
-      <span v-breakpoints="'sm'" class="ml-1">Filters</span>
-    </button>
+    <div class="filter-container">
+      <button class="filter-button filters text--grey-80">
+        <i class="pi pi-filter-3-line" />
+        <span v-breakpoints="'sm'" class="ml-1">Filters</span>
+      </button>
+    </div>
 
     <teleport to="#app">
       <div class="observe" ref="observe" />
@@ -25,8 +46,13 @@
 
 <script lang="ts" setup>
 import { ref, Ref, onBeforeUnmount, onMounted } from 'vue'
+import ObjectMenu from './ObjectMenu.vue'
 
 const search = ref<string>('')
+
+const showObjectMenu = ref<boolean>(false)
+
+const object = ref<string>('flows')
 
 /**
  * This section is for performantly handling intersection of the filter bar
@@ -62,6 +88,6 @@ onBeforeUnmount(() => {
 })
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @use '@/styles/components/filter-bar.scss';
 </style>
