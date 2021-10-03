a64e71c26e3724e43828d82cc4c3ca85f49d7ffc
nicholas <znicholasbrown@gmail.com>
2021-10-03 03:59:13 -0400

    Add placeholder tags and timeframe menus

--- orion-ui/src/components/FilterBar/FilterMenu.vue
+++ orion-ui/src/components/FilterBar/FilterMenu.vue
@@ -1,31 +1,62 @@
 <template>
   <Card class="menu font--primary" tabindex="0">
-    <div class="menu-content"> HELLO IM A FILTER MENU </div>
+    <div class="menu-content d-flex align-center justify-start pa-2">
+      <Button class="mr-2" height="36px" @click="toggleMenu(0)">
+        <i class="pi pi-filter-3-line" />
+        Run States
+      </Button>
+
+      <Button class="mr-2" height="36px" @click="toggleMenu(1)">
+        <i class="pi pi-filter-3-line" />
+        Timeframe
+      </Button>
+
+      <Button class="mr-2" height="36px" @click="toggleMenu(2)">
+        <i class="pi pi-filter-3-line" />
+        Tags
+      </Button>
+    </div>
+
+    <!-- TODO: Put these in actual overflow menus -->
+    <div>
+      <component
+        v-for="(menu, i) in menus.filter((m) => m.show)"
+        :key="i"
+        :is="menu.component"
+        @close="toggleMenu(i)"
+      />
+    </div>
   </Card>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineEmits, defineProps } from 'vue'
+import { ref } from 'vue'
+import RunStatesMenu from './RunStatesMenu.vue'
+import TimeframeMenu from './TimeframeMenu.vue'
+import TagsMenu from './TagsMenu.vue'
 
-const emit = defineEmits(['update:modelValue', 'close'])
-// const props = defineProps<{
-//   modelValue: string
-//   options: { label: string; value: string }[]
-// }>()
+const menus = ref([
+  {
+    component: RunStatesMenu,
+    show: false
+  },
+  {
+    component: TimeframeMenu,
+    show: false
+  },
+  {
+    component: TagsMenu,
+    show: false
+  }
+])
 
-// const value = computed(() => {
-//   return props.modelValue
-// })
-
-// const selectOption = (val: string) => {
-//   emit('update:modelValue', val)
-//   emit('close')
-// }
+const toggleMenu = (i: number) => {
+  menus.value[i].show = !menus.value[i].show
+}
 </script>
 
 <style lang="scss" scoped>
 .menu {
-  height: auto;
   width: 100%;
   border-radius: 0;
 
@@ -33,8 +64,19 @@ const emit = defineEmits(['update:modelValue', 'close'])
     border-top: 2px solid $grey-10;
   }
 
-  ::v-deep(*) {
+  > ::v-deep(div) {
     border-radius: 0 0 3px 3px !important;
+  }
+
+  .menu-container {
+    position: relative;
+
+    .sub-menu {
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 1;
+    }
   }
 }
 </style>
