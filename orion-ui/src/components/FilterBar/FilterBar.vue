72e96307b107fc0e2e0cdf42ffc5d641ec038206
nicholas <znicholasbrown@gmail.com>
2021-10-02 20:58:04 -0400

    Scaffold filter bar

---
+++ orion-ui/src/components/FilterBar/FilterBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="filter-bar">I GUESS I FILTER THE PAGE</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+@use '@/styles/components/filter-bar.scss';
+</style>
