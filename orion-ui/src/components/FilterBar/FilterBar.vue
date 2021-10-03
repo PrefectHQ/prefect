e7a97a0c9da14a19b3083a828d0fa7b9c0b29ed6
nicholas <znicholasbrown@gmail.com>
2021-10-03 00:19:06 -0400

    Basic styling on the search input

--- orion-ui/src/components/FilterBar/FilterBar.vue
+++ orion-ui/src/components/FilterBar/FilterBar.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="bar" :class="{ detached: detached }">
-    I GUESS I FILTER THE PAGE
+  <div class="bar d-flex font--secondary" :class="{ detached: detached }">
+    <button class="filter-button objects">
+      <span v-breakpoints="'sm'" class="mr-1">Flow Runs</span>
+      <i class="pi pi-arrow-drop-down-fill" />
+    </button>
+
+    <div class="search-input px-2 flex-grow-1 d-flex align-center">
+      <i class="pi pi-search-line mr-1" />
+      <input v-model="search" class="flex-grow-1" placeholder="Search..." />
+    </div>
+
+    <button class="filter-button filters">
+      <i class="pi pi-filter-3-line" />
+      <span v-breakpoints="'sm'" class="ml-1">Filters</span>
+    </button>
 
     <teleport to="#app">
       <div class="observe" ref="observe" />
@@ -10,6 +23,12 @@
 
 <script lang="ts" setup>
 import { ref, Ref, onBeforeUnmount, onMounted } from 'vue'
+
+const search = ref<string>('')
+
+/**
+ * This section is for performantly handling intersection of the filter bar
+ */
 
 const detached: Ref<boolean> = ref(false)
 
