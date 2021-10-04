b9d896916063f563ef2b788a2d266a1467a84d72
nicholas <znicholasbrown@gmail.com>
2021-10-04 02:12:33 -0400

    Make menu opening/closing more intuitive

--- orion-ui/src/components/FilterBar/FilterSearch.vue
+++ orion-ui/src/components/FilterBar/FilterSearch.vue
@@ -9,15 +9,59 @@
       font--primary
       justify-space-between
     "
+    @click="focusSearchInput"
   >
     <i class="pi pi-search-line mr-1" />
-    <!-- <input v-model="search" class="flex-grow-1" placeholder="Search..." />
-    <div class="flex-grow-1">
-     
-      
-    </div> -->
-    <div class="d-flex"> <slot /> </div>
+    <input
+      v-model="search"
+      ref="searchInput"
+      class="flex-grow-1"
+      placeholder="Search..."
+      @focus="emitFocused"
+      @input="emitInput"
+      @keyup.enter="emitEnter"
+    />
+    <div class="d-flex align-center slot-content"> <slot /> </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, defineEmits } from 'vue'
+
+const emit = defineEmits(['input', 'keyup.enter', 'focused'])
+const searchInput = ref()
+const search = ref('')
+
+const emitInput = () => {
+  emit('input', search.value)
+}
+
+const emitEnter = () => {
+  emit('keyup.enter', search.value)
+}
+
+const emitFocused = () => {
+  console.log('hello')
+  emit('focused')
+}
+
+const focusSearchInput = () => {
+  searchInput.value.focus()
+}
+</script>
+
+<style lang="scss" scoped>
+.search-input {
+  position: relative;
+
+  input {
+    border: none;
+    outline: none;
+    height: 100%;
+  }
+
+  .slot-content {
+    z-index: 1;
+  }
+}
+</style>
