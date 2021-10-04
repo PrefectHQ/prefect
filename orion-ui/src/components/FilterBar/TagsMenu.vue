7af64b2778c7cd8db1ed1107a74c38b727336dbd
nicholas <znicholasbrown@gmail.com>
2021-10-04 04:12:11 -0400

    Filter out input text field and select events

--- orion-ui/src/components/FilterBar/TagsMenu.vue
+++ orion-ui/src/components/FilterBar/TagsMenu.vue
@@ -1,50 +1,50 @@
 <template>
   <Card class="run-states-menu" tabindex="0">
     <div class="menu-content pa-2">
-      <h4>Select Run States</h4>
+      <h4>Apply tag filter</h4>
 
-      <a @click="toggleAll">Select All</a>
+      <Input
+        v-model="input"
+        @keyup.enter="addTag"
+        placeholder="Press enter to add a tag"
+      />
 
-      <Checkbox
-        v-for="state in states"
-        :key="state.value"
-        v-model="state.checked"
-        class="d-flex my-1 font--secondary checkbox text-left"
-      >
-        <span>{{ state.name }}</span>
-      </Checkbox>
+      <div v-for="tag in tags" :key="tag"> {{ tag }}</div>
     </div>
+
+    <template v-slot:actions>
+      <hr class="hr" />
+
+      <div class="pa-2">
+        <Button class="mr-1" outlined @click="emit('close')">Cancel</Button>
+        <Button color="primary" @click="apply">Apply</Button>
+      </div>
+    </template>
   </Card>
 </template>
 
 <script lang="ts" setup>
-import { computed, defineEmits, defineProps, ref } from 'vue'
+import { defineProps, defineEmits, reactive, ref } from 'vue'
+import { useStore } from 'vuex'
 
+const store = useStore()
+const props = defineProps<{ object: string }>()
 const emit = defineEmits(['update:modelValue', 'close'])
-// const props = defineProps<{
-//   modelValue: string
-//   options: { label: string; value: string }[]
-// }>()
 
-// const value = computed(() => {
-//   return props.modelValue
-// })
+const input = ref('')
 
-const states = ref<{ name: string; value: string; checked: boolean }[]>([
-  { name: 'Scheduled', value: 'SCHEDULED', checked: false },
-  { name: 'Pending', value: 'PENDING', checked: false },
-  { name: 'Running', value: 'RUNNING', checked: false },
-  { name: 'Completed', value: 'COMPLETED', checked: false },
-  { name: 'Failed', value: 'FAILED', checked: false },
-  { name: 'Cancelled', value: 'CANCELLED', checked: false }
-  //   { name: 'Awaiting Retry', value: '' },
-  //   { name: 'Retrieved Cache', value: 'SCHEDULED' },
-  //   { name: 'Crashed', value: 'CRASHED' },
+const tags = reactive([
+  ...(store.getters.globalFilter[props.object]?.tags ?? [])
 ])
 
-const toggleAll = () => {
-  const checked = states.value.every((s) => s.checked)
-  states.value.forEach((s) => (s.checked = !checked))
+const addTag = () => {
+  tags.push(input.value)
+  input.value = ''
+}
+
+const apply = () => {
+  console.log('apply')
+  store.commit('tags', { object: props.object, tags: tags })
 }
 </script>
 
