3361f9fcb730dc32d151b6ff2473dd08278f5c5a
nicholas <znicholasbrown@gmail.com>
2021-10-05 04:57:54 -0400

    Add standard time inputs

--- orion-ui/src/components/FilterBar/Form--DateTime.vue
+++ orion-ui/src/components/FilterBar/Form--DateTime.vue
@@ -1,11 +1,13 @@
 <template>
-  <Card class="timeframe-menu font--secondary" miter shadow="sm" tabindex="0">
-    <div class="menu-content pa-2">
+  <div class="container">
+    <div class="font-weight-semibold">Timeframe</div>
+
+    <div class="my-2">
       <Radio
         v-model="timeframeSelector"
         :value="'standard'"
         :checked="timeframeSelector == 'standard'"
-        class="mb-1 radio"
+        class="radio"
       >
         Standard
       </Radio>
@@ -14,112 +16,108 @@
         v-model="timeframeSelector"
         :value="'custom'"
         :checked="timeframeSelector == 'custom'"
-        class="mb-1 radio"
-        disabled
+        class="radio"
       >
         Custom
       </Radio>
-
-      <div v-if="timeframeSelector == 'standard'">
-        From:
-        <select v-model="pastAmount">
-          <option v-for="(option, i) in amountOptions" :value="option" :key="i">
-            {{ option }}
-          </option>
-        </select>
-        <select v-model="pastUnit">
-          <option v-for="(option, i) in unitOptions" :value="option" :key="i">
-            {{ option }}
-          </option>
-        </select>
-        To:
-        <select v-model="futureAmount">
-          <option v-for="(option, i) in amountOptions" :value="option" :key="i">
-            {{ option }}
-          </option>
-        </select>
-        <select v-model="futureUnit">
-          <option v-for="(option, i) in unitOptions" :value="option" :key="i">
-            {{ option }}
-          </option>
-        </select>
-      </div>
     </div>
 
-    <template v-slot:actions>
-      <hr class="hr" />
-
-      <div class="pa-2">
-        <Button class="mr-1" outlined @click="emit('close')">Cancel</Button>
-        <Button color="primary" @click="apply">Apply</Button>
+    <div v-if="timeframeSelector == 'standard'">
+      <div class="caption-small text-uppercase font-weight-semibold my-1">
+        Past
       </div>
-    </template>
-  </Card>
+      <div class="d-flex">
+        <NumberInput
+          v-model="fromValue"
+          step="1"
+          class="d-inline-block selector"
+        />
+
+        <Select v-model="fromUnit" class="ml-2 d-inline-block selector">
+          <Option v-for="u in unitOptions" :key="u" :value="u">{{ u }}</Option>
+        </Select>
+      </div>
+
+      <div class="caption-small text-uppercase font-weight-semibold my-1">
+        Upcoming
+      </div>
+      <div class="d-flex">
+        <NumberInput
+          v-model="toValue"
+          step="1"
+          class="d-inline-block selector"
+        />
+
+        <Select v-model="toUnit" class="ml-2 d-inline-block selector">
+          <Option v-for="u in unitOptions" :key="u" :value="u">{{ u }}</Option>
+        </Select>
+      </div>
+    </div>
+    <div v-else> CUSTOM </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { defineEmits, ref, defineProps } from 'vue'
-import { useStore } from 'vuex'
+import { timeframe } from '@/store/mutations'
+import { defineEmits, ref, defineProps, watch, computed } from 'vue'
 
 const props = defineProps<{
-  object: 'flow_runs' | 'task_runs'
+  modelValue: {
+    dynamic: boolean
+    from: { timestamp: Date; unit: string; value: number }
+    to: { timestamp: Date; unit: string; value: number }
+  }
 }>()
-const emit = defineEmits(['close'])
-const store = useStore()
 
-const from = store.getters.globalFilter[props.object].timeframe?.from || {
-  value: 60,
-  unit: 'minutes'
-}
-const to = store.getters.globalFilter[props.object].timeframe?.to || {
-  value: 60,
-  unit: 'minutes'
-}
-const pastAmount = ref(from.value)
-const pastUnit = ref(from.unit)
-const futureAmount = ref(to.value)
-const futureUnit = ref(to.unit)
+const emit = defineEmits(['update:modelValue'])
+
+const fromUnit = ref(props.modelValue.from.unit)
+const toUnit = ref(props.modelValue.to.unit)
+const fromValue = ref(props.modelValue.from.value)
+const toValue = ref(props.modelValue.to.value)
+const fromTimestamp = ref(props.modelValue.from.timestamp)
+const toTimestamp = ref(props.modelValue.to.timestamp)
+
+const value = computed(() => {
+  return {
+    dynamic: timeframeSelector.value == 'standard',
+    from: {
+      timestamp:
+        timeframeSelector.value == 'standard' ? null : fromTimestamp.value,
+      unit: fromUnit.value,
+      value: fromValue.value
+    },
+    to: {
+      timestamp:
+        timeframeSelector.value == 'standard' ? null : toTimestamp.value,
+      unit: toUnit.value,
+      value: toValue.value
+    }
+  }
+})
 
 const timeframeSelector = ref('standard')
 
-const amountOptions = [1, 5, 15, 24, 30, 60]
-
 const unitOptions = ['minutes', 'hours', 'days']
 
-const apply = () => {
-  const from = {
-    unit: pastUnit.value,
-    value: pastAmount.value
-  }
-
-  const to = {
-    unit: futureUnit.value,
-    value: futureAmount.value
-  }
-
-  store.commit('timeframe', {
-    object: props.object,
-    dynamic: timeframeSelector.value == 'custom',
-    from,
-    to
-  })
-  emit('close')
-}
+watch([fromUnit, toUnit, fromValue, toValue], () => {
+  emit('update:modelValue', value.value)
+})
 </script>
 
 <style lang="scss" scoped>
-.timeframe-menu {
-  height: auto;
-  position: relative;
-  width: 100%;
+.container {
+  min-height: 400px !important;
 
-  .menu-content {
-    .radio {
-      > ::v-deep(input) {
-        display: none;
-      }
-    }
+  @media (max-width: 1024px) {
+    min-height: 300px;
   }
+}
+
+.selector {
+  height: 40px !important;
+  width: auto !important;
+  max-width: 200px !important;
 }
 
 .timeframe-selector {
