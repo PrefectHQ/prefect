843f7a2bb9b82de21c1be9824d708fcec0e3ed39
Zach Angell <zachary.james.angell@gmail.com>
2021-10-07 22:53:02 -0400

    Black format

--- src/prefect/executors/dask.py
+++ src/prefect/executors/dask.py
@@ -6,7 +6,7 @@ import weakref
 from contextlib import contextmanager
 from typing import Any, Callable, Iterator, TYPE_CHECKING, Union, Optional, Dict
 
-if sys.version_info < (3,7):
+if sys.version_info < (3, 7):
     from prefect.utilities.context import nullcontext
 else:
     from contextlib import nullcontext
