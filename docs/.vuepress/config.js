4eef9f87170ed26436f2ba74f2bca787526649ec
Zach Angell <zachary.james.angell@gmail.com>
2021-10-12 19:06:16 -0400

    speed and power

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -66,6 +66,7 @@ module.exports = {
     repo: 'PrefectHQ/prefect',
     docsDir: 'docs',
     editLinks: true,
+    lastUpdated: false,
     // repoLabel: 'GitHub',
     logo: '/assets/logomark-color.png',
     nav: [
