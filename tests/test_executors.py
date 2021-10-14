c65ca443a47b9de3c2f65dca02e7463641851352
Michael Adkins <michael@prefect.io>
2021-10-14 15:59:06 -0500

    Update subflow runs to use their own executor

--- tests/test_executors.py
+++ tests/test_executors.py
@@ -2,30 +2,7 @@ import pytest
 
 from prefect import flow, task
 from prefect.executors import DaskExecutor, SequentialExecutor
-
-
-def get_test_flow():
-    @task
-    def task_a():
-        print("Inside task_a.fn")
-        return "a"
-
-    @task
-    def task_b():
-        return "b"
-
-    @task
-    def task_c(b):
-        return b + "c"
-
-    @flow(version="test")
-    def test_flow():
-        a = task_a()
-        b = task_b()
-        c = task_c(b)
-        return a, b, c
-
-    return test_flow
+from prefect.context import get_run_context
 
 
 @pytest.mark.parametrize(
@@ -36,8 +13,24 @@ def get_test_flow():
     ],
 )
 def test_flow_run_by_executor(executor):
-    test_flow = get_test_flow()
-    test_flow.executor = executor
+    @task
+    def task_a():
+        return "a"
+
+    @task
+    def task_b():
+        return "b"
+
+    @task
+    def task_c(b):
+        return b + "c"
+
+    @flow(version="test", executor=executor)
+    def test_flow():
+        a = task_a()
+        b = task_b()
+        c = task_c(b)
+        return a, b, c
 
     a, b, c = test_flow().result()
     assert (a.result(), b.result(), c.result()) == (
@@ -47,7 +40,14 @@ def test_flow_run_by_executor(executor):
     )
 
 
-def get_failing_test_flow():
+@pytest.mark.parametrize(
+    "executor",
+    [
+        SequentialExecutor(),
+        DaskExecutor(),
+    ],
+)
+def test_failing_flow_run_by_executor(executor):
     @task
     def task_a():
         raise RuntimeError("This task fails!")
@@ -61,27 +61,13 @@ def get_failing_test_flow():
         # This task attempts to use the upstream data and should fail too
         return b + "c"
 
-    @flow(version="test")
+    @flow(version="test", executor=executor)
     def test_flow():
         a = task_a()
         b = task_b()
         c = task_c(b)
         d = task_c(c)
         return a, b, c, d
-
-    return test_flow
-
-
-@pytest.mark.parametrize(
-    "executor",
-    [
-        SequentialExecutor(),
-        DaskExecutor(),
-    ],
-)
-def test_failing_flow_run_by_executor(executor):
-    test_flow = get_failing_test_flow()
-    test_flow.executor = executor
 
     state = test_flow()
     assert state.is_failed()
@@ -104,3 +90,53 @@ def test_failing_flow_run_by_executor(executor):
         f"Upstream task run '{c.state_details.task_run_id}' did not reach a 'COMPLETED' state"
         in d.message
     )
+
+
+@pytest.mark.parametrize(
+    "parent_executor,child_executor",
+    [
+        (SequentialExecutor(), DaskExecutor()),
+        (DaskExecutor(), SequentialExecutor()),
+    ],
+)
+def test_subflow_run_by_executor(parent_executor, child_executor):
+    @task
+    def task_a():
+        return "a"
+
+    @task
+    def task_b():
+        return "b"
+
+    @task
+    def task_c(b):
+        return b + "c"
+
+    @flow(version="test", executor=parent_executor)
+    def parent_flow():
+        assert get_run_context().executor is parent_executor
+        a = task_a()
+        b = task_b()
+        c = task_c(b)
+        d = child_flow(c)
+        return a, b, c, d
+
+    @flow(version="test", executor=child_executor)
+    def child_flow(c):
+        assert get_run_context().executor is child_executor
+        a = task_a()
+        b = task_b()
+        c = task_c(b)
+        d = task_c(c)
+        return a, b, c, d
+
+    a, b, c, d = parent_flow().result()
+    # parent
+    assert (a.result(), b.result(), c.result()) == (
+        "a",
+        "b",
+        "bc",
+    )
+    # child
+    a, b, c, d = d.result()
+    assert (a.result(), b.result(), c.result(), d.result()) == ("a", "b", "bc", "bcc")
