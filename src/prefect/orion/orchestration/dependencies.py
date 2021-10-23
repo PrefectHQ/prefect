ec6d5efc170d6e61e53052a7b7206ea6f7ff219e
Dustin Ngo <dustin.ngo@gmail.com>
2021-10-23 18:44:49 -0400

    Inject flow policy dependency

--- src/prefect/orion/orchestration/dependencies.py
+++ src/prefect/orion/orchestration/dependencies.py
@@ -3,7 +3,8 @@ Injected orchestration dependencies
 """
 
 ORCHESTRATION_DEPENDENCIES = {
-    "task_policy": lambda: None
+    "task_policy": lambda: None,
+    "flow_policy": lambda: None,
 }
 
 
@@ -13,4 +14,13 @@ async def get_task_policy():
     if provided_policy is None:
         from prefect.orion.orchestration.core_policy import CoreTaskPolicy
         provided_policy = CoreTaskPolicy
+    return provided_policy
+
+
+async def get_flow_policy():
+    provided_policy = ORCHESTRATION_DEPENDENCIES.get("flow_policy", None)()
+
+    if provided_policy is None:
+        from prefect.orion.orchestration.core_policy import CoreFlowPolicy
+        provided_policy = CoreFlowPolicy
     return provided_policy
