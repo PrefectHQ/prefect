022c6c5f139b139023ea80c0e0bbf30a72c12950
Michael Adkins <michael@prefect.io>
2021-11-03 11:07:46 -0500

    Instead of dropping data, convert them to valid schemas

--- src/prefect/serialization/edge.py
+++ src/prefect/serialization/edge.py
@@ -10,14 +10,18 @@ class EdgeSchema(ObjectSchema):
         object_class = lambda: prefect.core.Edge
 
     @pre_load(pass_many=True)
-    def drop_edges_with_bad_task_schemas(self, data, **kwargs):
-        new_data = []
+    def cast_task_slug_strings_to_task_schema_dicts(self, data, **kwargs):
         for seredge in data:
-            if isinstance(seredge["upstream_task"], dict) and isinstance(
-                seredge["downstream_task"], dict
-            ):
-                new_data.append(seredge)
-        return new_data
+            if not isinstance(seredge, dict):
+                continue
+            upstream_task = seredge.get("upstream_task")
+            downstream_task = seredge.get("downstream_task")
+            if isinstance(upstream_task, str):
+                seredge["upstream_task"] = {"slug": upstream_task}
+            if isinstance(upstream_task, str):
+                seredge["downstream_task"] = {"slug": downstream_task}
+
+        return data
 
     upstream_task = fields.Nested(TaskSchema, only=["slug"])
     downstream_task = fields.Nested(TaskSchema, only=["slug"])
