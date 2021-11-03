fcec70f0bdd9a86c1a4a896071dae975a4f852ff
Michael Adkins <michael@prefect.io>
2021-11-03 11:00:13 -0500

    Fix load of serialized flows with bad edge task schemas

--- src/prefect/serialization/edge.py
+++ src/prefect/serialization/edge.py
@@ -1,4 +1,4 @@
-from marshmallow import fields
+from marshmallow import fields, pre_load
 
 import prefect
 from prefect.serialization.task import TaskSchema
@@ -8,6 +8,16 @@ from prefect.utilities.serialization import ObjectSchema
 class EdgeSchema(ObjectSchema):
     class Meta:
         object_class = lambda: prefect.core.Edge
+
+    @pre_load(pass_many=True)
+    def drop_edges_with_bad_task_schemas(self, data, **kwargs):
+        new_data = []
+        for seredge in data:
+            if isinstance(seredge["upstream_task"], dict) and isinstance(
+                seredge["downstream_task"], dict
+            ):
+                new_data.append(seredge)
+        return new_data
 
     upstream_task = fields.Nested(TaskSchema, only=["slug"])
     downstream_task = fields.Nested(TaskSchema, only=["slug"])
