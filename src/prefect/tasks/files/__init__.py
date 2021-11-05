45c5f7367414ea5ddad1406587b278395a27a4e7
Alexander Streed <desertaxle@users.noreply.github.com>
2021-11-05 10:20:34 -0500

    Glob Task (#5077)
    * Adds glob test with requested recursion changes
    
    * Fixes typo in function name
    
    Co-authored-by: Michael Adkins <madkinszane@gmail.com>
    
    * Fixes typo in doc string
    
    Co-authored-by: Michael Adkins <madkinszane@gmail.com>
    
    * Adds more details to doc string for path arg
    
    Co-authored-by: Michael Adkins <madkinszane@gmail.com>
    
    * Adds more detail to the doc string for pattern arg
    
    Co-authored-by: Michael Adkins <madkinszane@gmail.com>
    
    * Swaps out tmpdir for tmp_path
    
    * Fixes formatting issues
    
    * Fixes flake8 issues
    
    Co-authored-by: Michael Adkins <madkinszane@gmail.com>

--- src/prefect/tasks/files/__init__.py
+++ src/prefect/tasks/files/__init__.py
@@ -3,4 +3,4 @@ Tasks for working with files and filesystems.
 """
 
 from .compression import Unzip, Zip
-from .operations import Copy, Move, Remove
+from .operations import Copy, Glob, Move, Remove
