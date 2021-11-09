b5dbf16978b98725ee1bd0002b218a1d5c432f80
Anna Geller <anna.m.geller@gmail.com>
2021-11-09 19:43:24 +0100

    adding a full flow example with S3 and ECR image since it's a common pattern

--- src/prefect/run_configs/ecs.py
+++ src/prefect/run_configs/ecs.py
@@ -169,7 +169,7 @@ class ECSRun(RunConfig):
     )
     ```
 
-    6) Use an explicit task definition stored in S3 as YAML file, 
+    6) Use an explicit task definition stored in S3 as a YAML file, 
     but override the image and CPU:
 
     ```python
@@ -178,6 +178,56 @@ class ECSRun(RunConfig):
         image="XXXX.dkr.ecr.us-east-1.amazonaws.com/image_name:latest",
         cpu="2 vcpu",
     )
+    ```
+
+    7) An example flow showing ECSRun with a `task_definition` defined as dictionary, 
+    and S3 storage:
+    ```python
+    import prefect
+    from prefect.storage import S3
+    from prefect.run_configs import ECSRun
+    from prefect import task, Flow
+    from prefect.client.secrets import Secret
+
+
+    FLOW_NAME = "ecs_demo_ecr"
+    ACCOUNT_ID = Secret("AWS_ACCOUNT_ID").get()
+    STORAGE = S3(
+        bucket="prefect-datasets",
+        key=f"flows/{FLOW_NAME}.py",
+        stored_as_script=True,
+        local_script_path=f"{FLOW_NAME}.py",
+    )
+    RUN_CONFIG = ECSRun(
+        labels=["prod"],
+        task_definition=dict(
+            family=FLOW_NAME,
+            requiresCompatibilities=["FARGATE"],
+            networkMode="awsvpc",
+            cpu=1024,
+            memory=2048,
+            taskRoleArn=f"arn:aws:iam::{ACCOUNT_ID}:role/prefectTaskRole",
+            executionRoleArn=f"arn:aws:iam::{ACCOUNT_ID}:role/prefectECSAgentTaskExecutionRole",
+            containerDefinitions=[
+                dict(
+                    name="flow",
+                    image=f"{ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/your_image_name:latest",
+                )
+            ],
+        ),
+        run_task_kwargs=dict(cluster="prefectEcsCluster"),
+    )
+
+
+    @task
+    def say_hi():
+        logger = prefect.context.get("logger")
+        logger.info("Hi from Prefect %s from flow %s", prefect.__version__, FLOW_NAME)
+
+
+    with Flow(FLOW_NAME, storage=STORAGE, run_config=RUN_CONFIG,) as flow:
+        say_hi()
+
     ```
 
     [1]: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/\
