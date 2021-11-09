c03d378640d927aa91d6c27bcde25a1aa0b7b3fa
Craig Harshbarger <pleek91@gmail.com>
2021-11-09 15:53:08 -0600

    Add date-fns as a package for date formatting

--- orion-ui/src/utilities/date.ts
+++ orion-ui/src/utilities/date.ts
@@ -1,17 +1,7 @@
 import { format } from 'date-fns'
 
-export function timeZoneAbbreviation(date: Date | string): string {
-  const parsed = new Date(date)
-
-  return parsed
-    .toLocaleTimeString(undefined, { timeZoneName: 'short' })
-    .split(' ')[2]
-}
-
 export function formatDateTimeNumeric(date: Date | string): string {
   const parsed = new Date(date)
-  const dateTime = format(parsed, 'MM/dd/yyyy hh:mm:ss a')
-  const timezone = timeZoneAbbreviation(parsed)
 
-  return `${dateTime} ${timezone}`
+  return format(parsed, 'yyyy/MM/dd hh:mm:ss a')
 }
