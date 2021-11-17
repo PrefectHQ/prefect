6420c6c21180f44d7a40b617ea8ddcf9264ec119
Dustin Ngo <dustin.ngo@gmail.com>
2021-11-17 10:16:02 -0500

    Use consistent names

--- src/prefect/orion/orchestration/dependencies.py
+++ src/prefect/orion/orchestration/dependencies.py
@@ -3,26 +3,28 @@ Injected orchestration dependencies
 """
 
 ORCHESTRATION_DEPENDENCIES = {
-    "task_policy": lambda: None,
-    "flow_policy": lambda: None,
+    "task_policy": None,
+    "flow_policy": None,
 }
 
 
-async def get_task_policy():
-    provided_policy = ORCHESTRATION_DEPENDENCIES.get("task_policy")()
+async def provide_task_policy():
+    provided_policy = ORCHESTRATION_DEPENDENCIES.get("task_policy")
 
     if provided_policy is None:
         from prefect.orion.orchestration.core_policy import CoreTaskPolicy
 
         provided_policy = CoreTaskPolicy
+
     return provided_policy
 
 
-async def get_flow_policy():
-    provided_policy = ORCHESTRATION_DEPENDENCIES.get("flow_policy")()
+async def provide_flow_policy():
+    provided_policy = ORCHESTRATION_DEPENDENCIES.get("flow_policy")
 
     if provided_policy is None:
         from prefect.orion.orchestration.core_policy import CoreFlowPolicy
 
         provided_policy = CoreFlowPolicy
+
     return provided_policy
