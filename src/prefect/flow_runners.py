15512d9bc9ac592ac17dc42c218bab0d29f07ac5
Michael Adkins <michael@prefect.io>
2021-11-30 11:07:18 -0600

    Add client `FlowRunner` definition

---
+++ src/prefect/flow_runners.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+from typing import Dict
+from prefect.orion.schemas.core import FlowRunnerSettings
+
+
+_FLOW_RUNNERS: Dict[str, "FlowRunner"] = {}
+
+
+class FlowRunner(BaseModel):
+    env: Dict[str, str]
+
+    def to_settings(self):
+        return FlowRunnerSettings(name=type(self).__name__, settings=self.dict())
+
+    @classmethod
+    def from_settings(cls, settings: FlowRunnerSettings):
+        return cls(**settings.dict())
+
+
+def register_flow_runner(cls):
+    _FLOW_RUNNERS[cls.__name__] = cls
+
+
+def lookup_flow_runner(typename: str) -> FlowRunner:
+    """Return the serializer implementation for the given ``encoding``"""
+    try:
+        return _FLOW_RUNNERS[typename]
+    except KeyError:
+        raise ValueError(f"Unregistered flow runner {typename!r}")
+
+
+@register_flow_runner
+class SubprocessFlowRunner(FlowRunner):
+    pass
