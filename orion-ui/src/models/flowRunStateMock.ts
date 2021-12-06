def0275c908e9b761797bc9744e4d5a6270cc5df
Craig Harshbarger <pleek91@gmail.com>
2021-12-06 16:10:15 -0600

    New models directory to hold client models and their mocked versions

---
+++ orion-ui/src/models/flowRunStateMock.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import FlowRunState, { IFlowRunState } from './flowRunState'
+import { StateNames } from '@/types/states'
+import { fakerRandomState } from '@/utilities/faker'
+import faker from 'faker'
+
+export default class FlowRunStateMock extends FlowRunState {
+  constructor(state: Partial<IFlowRunState> = {}) {
+    const state_type = state.state_type ?? fakerRandomState()
+    const state_name = state.state_name ?? StateNames.get(state_type)!
+    const count_runs = state.count_runs ?? faker.datatype.number(100)
+    const sum_estimated_run_time =
+      state.sum_estimated_run_time ?? faker.datatype.number(100)
+    const sum_estimated_lateness =
+      state.sum_estimated_lateness ?? faker.datatype.number(100)
+
+    super({
+      state_type,
+      state_name,
+      count_runs,
+      sum_estimated_run_time,
+      sum_estimated_lateness
+    })
+  }
+}
