684e6bed7c720937ee26be66fb0661d91e9a6b44
Michael Adkins <michael@prefect.io>
2021-12-06 17:08:37 -0600

    Use `Depends` to provide the database interface

--- src/prefect/orion/api/dependencies.py
+++ src/prefect/orion/api/dependencies.py
@@ -1,23 +1,22 @@
 """
 Utilities for injecting FastAPI dependencies.
 """
-from fastapi import Request
+from fastapi import Request, Depends
 
 from prefect.orion.database.dependencies import provide_database_interface
+from prefect.orion.database.interface import OrionDBInterface
 from prefect.orion.utilities.server import response_scoped_dependency
 
 
 @response_scoped_dependency
-async def get_session(request: Request):
+async def get_session(
+    request: Request, db: OrionDBInterface = Depends(provide_database_interface)
+):
     """
     Dependency-injected database session.
     The context manager will automatically handle commits,
     rollbacks, and closing the connection.
     """
-    # we cant directly inject into FastAPI dependencies because
-    # they are converted to async_generator objects
-    db = provide_database_interface()
-
     # load engine with API timeout setting
     session_factory = await db.session_factory()
     async with session_factory() as session:
