2b2ff43e036a10753bede50f42fa440bb6ede8ca
Craig Harshbarger <pleek91@gmail.com>
2021-12-07 10:58:42 -0600

    Delete old setup file

--- orion-ui/tests/setupJest.ts
+++
@@ -1,7 +0,0 @@
-// import 'regenerator-runtime/runtime' // needed for async events
-import { config } from '@vue/test-utils'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-import MiterDesign from '@prefecthq/miter-design'
-
-config.global.plugins.push(MiterDesign)
