9eb443e6bc998cc177ea48c2a6bd1f6df9d1d3b1
Craig Harshbarger <pleek91@gmail.com>
2021-12-08 14:31:33 -0600

    Use a dynamic import rather than a vite plugin to start the mirage server

--- orion-ui/vite.config.ts
+++ orion-ui/vite.config.ts
@@ -2,24 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export function prefectMirage() {
-  return {
-    name: 'prefect-mirage-server',
-    transform(src, id) {
-      const useMirageJs = process.env.PREFECT_USE_MIRAGEJS ?? false
-
-      if (useMirageJs && id.endsWith('src/main.ts')) {
-        return {
-          code: `${src}\nimport { startServer } from './server'\nstartServer()`
-        }
-      }
-    }
-  }
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), prefectMirage()],
+  plugins: [vue()],
   base: process.env['ORION_UI_SERVE_BASE'] || '',
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, './src') }]
