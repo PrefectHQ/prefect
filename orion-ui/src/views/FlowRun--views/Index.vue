da191a69c1a1844aa36e4df715761555a40ec296
Marichka Offen <marichka@prefect.io>
2021-12-09 12:33:18 -0500

    Create separate component for label

--- orion-ui/src/views/FlowRun--views/Index.vue
+++ orion-ui/src/views/FlowRun--views/Index.vue
@@ -5,12 +5,7 @@
         <!-- TODO; This card is overflowing boundaries and text truncation doesn't seem to be working... fix that or whatever. -->
         <div class="d-inline-flex flex-column">
           <div class="flex-grow-0 flex-shrink-1">
-            <span
-              class="run-state correct-text caption mr-1"
-              :class="state.type?.toLowerCase() + '-bg'"
-            >
-              {{ state.name }}
-            </span>
+            <Label :state="state.name" :state-type="state.type" />
 
             <span class="d-inline-flex align-center text-truncate">
               <Tag
@@ -198,6 +193,7 @@ import { useRoute } from 'vue-router'
 import { secondsToApproximateString } from '@/util/util'
 import { formatDateTimeNumeric } from '@/utilities/dates'
 import Timeline from '@/components/Timeline/Timeline.vue'
+import Label from '@/components/Global/Label/Label.vue'
 
 const route = useRoute()
 
