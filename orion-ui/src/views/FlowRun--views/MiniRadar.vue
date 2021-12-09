1d95531abb458827c5e1264d43384402a2a44c90
nicholas <znicholasbrown@gmail.com>
2021-12-08 19:54:03 -0500

    Scale minimap with container

--- orion-ui/src/views/FlowRun--views/MiniRadar.vue
+++ orion-ui/src/views/FlowRun--views/MiniRadar.vue
@@ -1,21 +1,15 @@
 <template>
-  <div ref="container">
-    <MiniRadar :id="id" :radar="radar" hide-viewport disable-interactions />
-  </div>
+  <MiniRadar :id="id" :radar="radar" hide-viewport disable-interactions />
 </template>
 
 <script lang="ts" setup>
 import { Api, Endpoints, Query } from '@/plugins/api'
-import { computed, watch, ref, onMounted, onUnmounted } from 'vue'
+import { computed, watch, ref, onMounted } from 'vue'
 import { Radar } from '@/components/Radar/Radar'
 import MiniRadar from '@/components/Radar/MiniRadar.vue'
 
 const radar = ref<Radar>(new Radar())
 const props = defineProps<{ id: string }>()
-
-const container = ref<HTMLElement>()
-const height = ref<number>(0)
-const width = ref<number>(0)
 
 const radarFilter = computed(() => {
   return {
@@ -42,23 +36,7 @@ watch(
   }
 )
 
-const handleWindowResize = (): void => {
-  if (!container.value) return
-  height.value = container.value.offsetHeight
-  width.value = container.value.offsetWidth
-}
-
 onMounted(() => {
-  handleWindowResize()
-  window.addEventListener('resize', handleWindowResize)
-  radar.value
-    .id('id')
-    .dependencies('upstream_dependencies')
-    .center([width.value / 2, height.value / 2])
-    .items([])
-})
-
-onUnmounted(() => {
-  window.removeEventListener('resize', handleWindowResize)
+  radar.value.id('id').dependencies('upstream_dependencies').items([])
 })
 </script>
