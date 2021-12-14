c81fcb20def509be9fdafc5b6ec62c1e27dcc99f
Mathijs Miermans <m.miermans@gmail.com>
2021-12-13 22:21:19 -0800

    StepActivate task now accepts run args
    - Allow arguments to be passed to the  method. This is particularly necessary for the
       argument, which AWS requires to be unique across runs.
    - Fix the , which was always passing, even with bogus
      arguments. This assertion is also made in other tests. I plan to create a separate branch
      to confirm and fix this.

--- tests/tasks/aws/test_step_function.py
+++ tests/tasks/aws/test_step_function.py
@@ -4,31 +4,27 @@ import pytest
 
 pytest.importorskip("boto3")
 
-import prefect
 from prefect.tasks.aws import StepActivate
-from prefect.utilities.configuration import set_temporary_config
 
 
 class TestStepActivate:
     def test_initialization(self):
-        task = StepActivate(state_machine_arn="arn", execution_name="name")
+        task = StepActivate(state_machine_arn="arn")
 
     def test_initialization_passes_to_task_constructor(self):
         task = StepActivate(
-            state_machine_arn="arn", execution_name="name", name="test", tags=["AWS"]
+            state_machine_arn="arn", name="test", tags=["AWS"]
         )
         assert task.name == "test"
         assert task.tags == {"AWS"}
 
     def test_exposes_boto3_start_execution_api(self, monkeypatch):
-        task = StepActivate(state_machine_arn="arn", execution_name="name")
+        task = StepActivate(state_machine_arn="arn")
         client = MagicMock()
         boto3 = MagicMock(client=client)
         monkeypatch.setattr("prefect.utilities.aws.boto3", boto3)
-        task.run()
+        task.run(execution_name="name")
 
         called_method = client.mock_calls[1]
         assert called_method[0] == "().start_execution"
-        called_method.assert_called_once_with(
-            {"stateMachineArn": "arn", "name": "name", "input": {}}
-        )
+        assert called_method[2] == {"stateMachineArn": "arn", "name": "name", "input": "{}"}
