8a1fc7c3da8f61e4711856ef17e9028fa9247d6e
raghavsharma <raghavs@sigmoidanalytics.com>
2021-12-21 21:19:53 +0530

    Added 'firebolt' to setup.py file as an 'extra'

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ extras = {
     "dask_cloudprovider": ["dask_cloudprovider[aws] >= 0.2.0"],
     "dev": dev_requires + test_requires,
     "dropbox": ["dropbox ~= 9.0"],
+    "firebolt": ["firebolt-sdk >=0.2.1"],
     "ge": ["great_expectations >= 0.11.1"],
     "gcp": [
         "google-cloud-bigquery >= 1.6.0, < 2.0",
