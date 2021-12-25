9c573be0ced3d59b418883af3c1057068f5cfebe
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2021-12-25 10:42:01 +0100

    Refactored Mixpanel tasks __init__.py

--- src/prefect/tasks/mixpanel/__init__.py
+++ src/prefect/tasks/mixpanel/__init__.py
@@ -2,9 +2,4 @@
 This module contains a collection of tasks to interact with Mixpanel APIs
 """
 
-try:
-    from prefect.tasks.mixpanel.mixpanel_tasks import MixpanelExportTask
-except ImportError as err:
-    raise ImportError(
-        'Using `prefect.tasks.mixpanel` requires Prefect to be installed with the "mixpanel" extra.'
-    ) from err
+from .mixpanel_tasks import MixpanelExportTask
