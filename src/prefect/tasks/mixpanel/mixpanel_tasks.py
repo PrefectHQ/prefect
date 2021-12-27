bd2dba318781e9af544ea4e2a511ddf749a9321b
AlessandroLollo <alessandro.lollo@gmail.com>
2021-12-27 17:21:25 +0100

    Update src/prefect/tasks/mixpanel/mixpanel_tasks.py
    Co-authored-by: Terrence Dorsey <terrend@mishu.com>

--- src/prefect/tasks/mixpanel/mixpanel_tasks.py
+++ src/prefect/tasks/mixpanel/mixpanel_tasks.py
@@ -123,7 +123,7 @@ class MixpanelExportTask(Task):
             - use_eu_server (bool, optional): Whether to use the Mixpanel EU server to retrieve data.
                 More info at
                 https://help.mixpanel.com/hc/en-us/articles/360039135652-Data-Residency-in-EU.
-                Default to `False`
+                Default is `False`.
             - group_events: Whetner to group events with the same name.
                 This is taken into account only if `parse_response is True`.
 
