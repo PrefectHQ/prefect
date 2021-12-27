955bc266de2f3f72db5c3080e4eed9a5f2aa04d2
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2021-12-27 15:42:42 +0100

    Added extra cubejs

--- src/prefect/tasks/cubejs/__init__.py
+++ src/prefect/tasks/cubejs/__init__.py
@@ -2,4 +2,10 @@
 This is a collection of tasks to interact with a CubeJS/Cube Cloud environment.
 """
 
-from .cubejs_tasks import CubeJSQueryTask
+try:
+    from prefect.tasks.cubejs.cubejs_tasks import CubeJSQueryTask
+except ImportError as err:
+    raise ImportError(
+        'prefect.tasks.cubejs` requires Prefect to be installed with the "cubejs" extra.'
+    ) from err
+
