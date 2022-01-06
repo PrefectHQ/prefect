07e6b3255b948ef449cfa999ca3ec2148e850923
Craig Harshbarger <pleek91@gmail.com>
2022-01-06 10:37:33 -0600

    Update the import and for UrlQueryParams

--- orion-ui/packages/orion-design/src/models/DashboardFilters.ts
+++ orion-ui/packages/orion-design/src/models/DashboardFilters.ts
@@ -1,5 +1,5 @@
-import { QueryParams } from '../router/QueryParams'
+import { UrlQueryParams } from '../router/UrlQueryParams'
 
-export class DashboardFilters extends QueryParams {
+export class DashboardFilters extends UrlQueryParams {
   public flow_run_states = []
 }
