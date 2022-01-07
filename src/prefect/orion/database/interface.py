8c4346bba887632385ccf7164778c533897c9b6b
Dustin Ngo <dustin.ngo@gmail.com>
2022-01-07 15:10:38 -0500

    Sketch out ConcurrencyLimit ORM models

--- src/prefect/orion/database/interface.py
+++ src/prefect/orion/database/interface.py
@@ -142,6 +142,11 @@ class OrionDBInterface(metaclass=DBSingleton):
         return self.orm.Log
 
     @property
+    def ConcurrencyLimit(self):
+        """A concurrency model"""
+        return self.orm.ConcurrencyLimit
+
+    @property
     def deployment_unique_upsert_columns(self):
         """Unique columns for upserting a Deployment"""
         return self.orm.deployment_unique_upsert_columns
