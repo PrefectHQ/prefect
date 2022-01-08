08e580acf95963a2579971eb0ff4514233b5e7ea
Michael Adkins <michael@prefect.io>
2022-01-08 11:13:57 -0600

    Move logging into separate modules at 'prefect.logging'

---
+++ src/prefect/logging/__init__.py
@@ -0,0 +1,6 @@
+import prefect.logging.configuration
+import prefect.logging.formatters
+import prefect.logging.handlers
+import prefect.logging.loggers
+
+from prefect.logging.loggers import get_logger
