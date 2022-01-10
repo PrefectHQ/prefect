1a681f2d91ea12f4f423d68eacb688cdc2ec9a59
Craig Harshbarger <pleek91@gmail.com>
2022-01-10 15:19:32 -0600

    Move the logs mapper into the class

--- orion-ui/packages/orion-design/src/services/LogsApi.ts
+++ orion-ui/packages/orion-design/src/services/LogsApi.ts
@@ -15,28 +15,28 @@ export type ILogResponse = {
   task_run_id: string,
 }
 
-function logMapper(log: ILogResponse): Log {
-  return new Log({
-    id: log.id,
-    created: new Date(log.created),
-    updated: new Date(log.updated),
-    name: log.name,
-    level: log.level,
-    message: log.message,
-    timestamp: new Date(log.timestamp),
-    flowRunId: log.flow_run_id,
-    taskRunId: log.task_run_id,
-  })
-}
-
-function logsResponseMapper({ data }: AxiosResponse<ILogResponse[]>): Log[] {
-  return data.map(logMapper)
-}
-
 export class LogsApi extends Api {
   protected static route = '/api/logs'
 
   public static filter(filter: LogsRequestFilter): Promise<Log[]> {
-    return this.post('/filter', filter).then(logsResponseMapper)
+    return this.post('/filter', filter).then(this.logsResponseMapper)
+  }
+
+  protected static logMapper(log: ILogResponse): Log {
+    return new Log({
+      id: log.id,
+      created: new Date(log.created),
+      updated: new Date(log.updated),
+      name: log.name,
+      level: log.level,
+      message: log.message,
+      timestamp: new Date(log.timestamp),
+      flowRunId: log.flow_run_id,
+      taskRunId: log.task_run_id,
+    })
+  }
+
+  protected static logsResponseMapper({ data }: AxiosResponse<ILogResponse[]>): Log[] {
+    return data.map(this.logMapper)
   }
 }
