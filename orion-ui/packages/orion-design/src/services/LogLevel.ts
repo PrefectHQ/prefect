4ca9db8771fa5822dafab658e5a0e86c474453de
Craig Harshbarger <pleek91@gmail.com>
2022-01-12 11:13:31 -0600

    Add the log level filters

--- orion-ui/packages/orion-design/src/services/LogLevel.ts
+++ orion-ui/packages/orion-design/src/services/LogLevel.ts
@@ -1,5 +1,8 @@
 export class LogLevel {
 
+  public static LowestLogLevel = 0
+  public static HighestLogLevel = 0
+
   public static GetLabel(level: number): string {
     if (!LogLevel.Validate(level)) {
       throw 'Log level must be a number from 0 to 50'
@@ -13,8 +16,10 @@ export class LogLevel {
       case '4':
         return 'Error'
       case '3':
-        return 'Info'
+        return 'Warning'
       case '2':
+        return 'Info'
+      case '1':
         return 'Debug'
       default:
         return 'Not Set'
--- orion-ui/src/components/FlowRunLogsTabContent.vue
+++ orion-ui/src/components/FlowRunLogsTabContent.vue
@@ -1,9 +1,35 @@
 <template>
   <div class="flow-run-logs-tabs-content">
-    <FlowRunLogs class="flow-run-logs-tab-content" :logs="logs" />
+    <div class="flow-run-logs-tabs-content__header">
+      <p class="flow-run-logs-tabs-content__span">Showing: Start to Now</p>
+      <ButtonGroupInput
+        v-model:value="levelFilter"
+        :items="[10, 20, 30, 40, 50]"
+      >
+        <template #default="{ item }">
+          {{ LogLevel.GetLabel(item) }}
+        </template>
+      </ButtonGroupInput>
+    </div>
+    <div class="flow-run-logs-tab-content__table">
+      <FlowRunLogs :logs="logs">
+        <template #empty>
+          <p class="flow-run_logs-tab-content__empty">
+            No logs to show.
+            <Button
+              v-show="levelFilter.length"
+              class="ml-2"
+              @click="clearFilters"
+            >
+              Try clearing your filter
+            </Button>
+          </p>
+        </template>
+      </FlowRunLogs>
+    </div>
     <template v-if="loading">
       <div class="flow-run-logs-tabs-content__loading">
-        <m-loader :loading="true" class="flow-run-logs-tabs-content__loader" />
+        <m-loader loading class="flow-run-logs-tabs-content__loader" />
         <span>Run in progress...</span>
       </div>
     </template>
@@ -15,11 +41,13 @@ import {
   Logs,
   LogsRequestFilter,
   FlowRunLogs,
-  Log
+  Log,
+  LogLevel,
+  ButtonGroupInput
 } from '@prefecthq/orion-design'
 import { subscribe } from '@prefecthq/vue-compositions'
 import { SubscriptionOptions } from '@prefecthq/vue-compositions/src/subscribe/types'
-import { computed, defineProps, ref, watch } from 'vue'
+import { computed, defineProps, reactive, ref, watch } from 'vue'
 
 const props = defineProps({
   flowRunId: {
@@ -31,20 +59,43 @@ const props = defineProps({
   }
 })
 
+const levelFilter = ref<number[]>([])
+
 // todo: paginate this with limit/offset
-const filter = computed<LogsRequestFilter>(() => ({
-  logs: {
-    flow_run_id: {
-      any_: [props.flowRunId]
+const filter = computed<LogsRequestFilter>(() => {
+  const level: Required<LogsRequestFilter>['logs']['level'] = {}
+  const minLevel = Math.min(...levelFilter.value)
+  const maxLevel = Math.max(...levelFilter.value)
+
+  if (levelFilter.value.length) {
+    if (minLevel > 0) {
+      level.ge_ = minLevel
+    }
+
+    if (maxLevel < 50) {
+      level.le_ = maxLevel
     }
   }
-}))
+
+  return {
+    logs: {
+      flow_run_id: {
+        any_: [props.flowRunId]
+      },
+      level
+    }
+  }
+})
 
 const options: SubscriptionOptions = {
   interval: props.loading ? 5000 : undefined
 }
-const subscription = subscribe(Logs.filter.bind(Logs), [filter.value], options)
+const subscription = subscribe(Logs.filter.bind(Logs), [filter], options)
 const logs = computed<Log[]>(() => subscription.response.value ?? [])
+
+const clearFilters = () => {
+  levelFilter.value = []
+}
 
 watch(
   () => props.loading,
@@ -54,7 +105,30 @@ watch(
 
 <style>
 .flow-run-logs-tabs-content {
+}
+
+.flow-run-logs-tabs-content__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: var(--m-2) 0;
+  flex-wrap: wrap;
+  gap: var(--m-1);
+}
+
+.flow-run-logs-tabs-content__span {
+  font-size: 14px;
+  font-family: 'input-sans';
+  color: var(--grey-40);
+  margin: 0;
+}
+
+.flow-run-logs-tab-content__table {
   background-color: #fff;
+}
+
+.flow-run_logs-tab-content__empty {
+  text-align: center;
 }
 
 .flow-run-logs-tabs-content__loading {
