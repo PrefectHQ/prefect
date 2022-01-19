5184cc4319e0fe2edb7f7c740448f8be9322bdde
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-19 12:42:48 -0600

    moved state defaults into model in orion-design to be shared with nebula

--- orion-ui/src/store/filter/mutations.ts
+++ orion-ui/src/store/filter/mutations.ts
@@ -1,6 +1,6 @@
+import { GlobalFilterDefaults } from '@prefecthq/orion-design'
 import { MutationTree } from 'vuex'
 import { GlobalFilter, RunState, RunTimeFrame } from '@/typings/global'
-import { generateInitialGlobalFilterState } from './state'
 
 interface TimeFramePayload extends RunTimeFrame {
   object: 'flow_runs' | 'task_runs'
@@ -8,21 +8,13 @@ interface TimeFramePayload extends RunTimeFrame {
 
 export const mutations: MutationTree<GlobalFilter> = {
   setFilter(state: GlobalFilter, filter: GlobalFilter) {
-    if (filter.deployments) {
-      state.deployments = { ...filter.deployments }
-    }
-    if (filter.flow_runs) {
-      state.flow_runs = { ...filter.flow_runs }
-    }
-    if (filter.flows) {
-      state.flows = { ...filter.flows }
-    }
-    if (filter.task_runs) {
-      state.task_runs = { ...filter.task_runs }
-    }
+    state.deployments = { ...filter.deployments }
+    state.flow_runs = { ...filter.flow_runs }
+    state.flows = { ...filter.flows }
+    state.task_runs = { ...filter.task_runs }
   },
   resetFilter(state: GlobalFilter) {
-    const defaults = generateInitialGlobalFilterState()
+    const defaults = new GlobalFilterDefaults()
 
     state.deployments = defaults.deployments
     state.flow_runs = defaults.flow_runs
