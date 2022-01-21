ea44fe1f95fc661580e235533748e317b478739f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-21 14:35:43 -0600

    using css for styling needs of breadcrumbs

--- orion-ui/src/components/Global/BreadCrumbs/BreadCrumbs.vue
+++ orion-ui/src/components/Global/BreadCrumbs/BreadCrumbs.vue
@@ -2,22 +2,22 @@
   <component :is="props.tag" class="bread-crumbs">
     <i v-if="props.icon" class="pi text--grey-40 mr-2" :class="props.icon" />
     <div class="bread-crumbs__crumbs">
-      <div class="bread-crumbs__crumbs--first">
-        <template v-for="crumb in firstCrumbs" :key="crumb.text">
-          <BreadCrumb :crumb="crumb" />
-        </template>
-      </div>
-      <div class="bread-crumbs__crumbs--last">
-        <component :is="boldTag">
-          <BreadCrumb :crumb="lastCrumb" />
-        </component>
-      </div>
+      <span
+        v-for="(crumb, index) in crumbs"
+        :key="index"
+        class="bread-crumbs__crumb"
+        :class="{
+          'bread-crumbs__crumb--bold': bold && index === crumbs.length - 1
+        }"
+      >
+        <BreadCrumb :crumb="crumb" />
+      </span>
     </div>
   </component>
 </template>
 
 <script lang="ts" setup>
-import { withDefaults, computed } from 'vue'
+import { withDefaults } from 'vue'
 import { Crumb } from '../utils'
 import BreadCrumb from '@/components/Global/BreadCrumb/BreadCrumb.vue'
 
@@ -32,47 +32,38 @@ const props = withDefaults(defineProps<Props>(), {
   tag: 'h1',
   bold: false
 })
-
-const firstCrumbs = computed(() => {
-  return props.crumbs.slice(0, props.crumbs.length - 1)
-})
-
-const lastCrumb = computed(() => {
-  return props.crumbs.slice(-1)[0]
-})
-
-const boldTag = computed(() => {
-  return props.bold ? 'strong' : 'span'
-})
 </script>
 
 <style lang="scss" scoped>
-.bread-crumbs {
-  &__crumbs {
-    &--first {
-      font-size: 14px;
+.bread-crumbs__crumb {
+  font-size: 14px;
+
+  &:last-child {
+    display: block;
+    font-size: 20px;
+    margin: -6px 0 2px;
+  }
+
+  &:not(:nth-last-child(-n + 2))::after {
+    content: '\00a0/\00a0';
+  }
+}
+
+.bread-crumbs__crumb--bold {
+  font-weight: bold;
+}
+
+@media (min-width: 640px) {
+  .bread-crumbs__crumb {
+    font-size: 20px;
+
+    &:last-child {
+      display: inline;
+      margin: 0;
     }
 
-    &--last {
-      font-size: 20px;
-      margin: -6px 0 2px;
-    }
-
-    @media (min-width: 640px) {
-      display: flex;
-
-      &--first {
-        font-size: 20px;
-      }
-
-      &--last {
-        font-size: 20px;
-        margin: 0;
-      }
-
-      &--first::after {
-        content: '\00a0/\00a0';
-      }
+    &:not(:last-child)::after {
+      content: '\00a0/\00a0';
     }
   }
 }
