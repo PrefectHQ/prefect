c75e557d16dbad6ca13f7d057af3f4d54e099215
Dustin Ngo <dustin.ngo@gmail.com>
2022-01-21 15:54:34 -0500

    Concurrency limits of 0 will abort a transition to prevent deadlocks

--- src/prefect/orion/orchestration/core_policy.py
+++ src/prefect/orion/orchestration/core_policy.py
@@ -74,7 +74,13 @@ class SecureTaskConcurrencySlots(BaseOrchestrationRule):
         for tag in context.run.tags:
             cl = limits_by_tag.get(tag, None)
             if cl is not None:
-                if cl.active_slots >= cl.concurrency_limit:
+                limit = cl.concurrency_limit
+                if limit == 0:
+                    await self.reject_transition(
+                        reason=f'The concurrency limit on tag "{tag}" is 0 and will deadlock if the task tries to run again.',
+                        cleanup=True,
+                    )
+                if cl.active_slots >= limit:
                     await self.delay_transition(
                         30,
                         f"Concurrency limit for the {tag} tag has been reached",
