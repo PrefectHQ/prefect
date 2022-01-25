7cb2817a26cc9a12b9181261e30e1b7105cf35c6
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-24 19:24:13 -0600

    removed key from profile; key comes from collection record

--- orion-ui/packages/orion-design/src/profiles/BooleanProfile.ts
+++ orion-ui/packages/orion-design/src/profiles/BooleanProfile.ts
@@ -1,8 +1,6 @@
 import { Profile } from '../profiles'
 
 export class BooleanProfile implements Profile<boolean> {
-  public readonly key = 'boolean'
-
   public generate(): boolean {
     return Math.random() < 0.5
   }
