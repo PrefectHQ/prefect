7df2e63a06df5040ff90235e746677a97928e3e3
Craig Harshbarger <pleek91@gmail.com>
2022-01-25 15:50:44 -0600

    Fix bad auto merge

--- orion-ui/packages/orion-design/src/index.ts
+++ orion-ui/packages/orion-design/src/index.ts
@@ -1,11 +1,10 @@
 // from orion-ui. everything here should eventually get moved into orion-design
 // eslint-disable-next-line import/export
 export * from '@/components/'
-export * from './models'
-export * from './router'
 
 export * from './components'
-export * from './types'
 export * from './models'
+export * from './router'
 export * from './services'
+export * from './types'
 export * from './utilities'
