bf556eedc9d22dcdcd1df5cc06f7170e11305f1f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-24 19:25:34 -0600

    mocker profiles now optionally accept arguments

--- orion-ui/packages/orion-design/src/services/Mocker.ts
+++ orion-ui/packages/orion-design/src/services/Mocker.ts
@@ -1,30 +1,26 @@
 import { profiles, Profile } from '../profiles'
 
-export class Mocker<T extends Profile<any>> {
-  private readonly profiles: T[] = []
+type AnyProfile = Profile<any>
+type GeneratorParams<T extends AnyProfile> = Parameters<T['generate']>
+type GeneratorReturns<T extends AnyProfile> = ReturnType<T['generate']>
+type RequiredGeneratorParams<T extends AnyProfile> = OnlyRequired<GeneratorParams<T>>
+type OnlyRequired<T extends any[], U extends any[] = []> = Partial<T> extends T ? U : T extends [infer F, ...infer R] ? OnlyRequired<R, [...U, F]> : U
 
-  public constructor(profiles: T[]) {
+export class Mocker<T extends Record<string, AnyProfile>> {
+  private readonly profiles: T
+
+  public constructor(profiles: T) {
     this.profiles = profiles
   }
 
-  public create<K extends T['key']>(key: K): ReturnType<Extract<T, { key: K }>['generate']> {
-    const generator = this.profiles.find(x => x.key === key)
-
-    if (!generator) {
-      throw `No profile found for type ${key}`
-    }
-
-    return generator.generate()
+  public create<K extends keyof T>(...[key, args]: RequiredGeneratorParams<T[K]> extends never[] ? [key:K, args?: GeneratorParams<T[K]>] : [key: K, args: GeneratorParams<T[K]>]): GeneratorReturns<T[K]> {
+    return this.profiles[key].generate(...args ?? [])
   }
 
-  public createMany<K extends T['key']>(key: K, count: number = 3): ReturnType<Extract<T, { key: K }>['generate']>[] {
-    const results: ReturnType<Extract<T, { key: K }>['generate']>[] = []
-
-    for (let i=0; i<count; i++) {
-      results.push(this.create<K>(key))
-    }
-
-    return results
+  public createMany<K extends keyof T>(...[key, count = 3, args]: RequiredGeneratorParams<T[K]> extends never[] ? [key:K, count?: number, args?: GeneratorParams<T[K]>] : [key: K, count: number, args: GeneratorParams<T[K]>]): GeneratorReturns<T[K]>[] {
+    return new Array(count)
+      .fill(null)
+      .map(() => this.profiles[key].generate(...args ?? []))
   }
 }
 
