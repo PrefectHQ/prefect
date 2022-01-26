f87428e6452ae8ba1c9d33975ad792598341678f
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-26 15:21:22 -0600

    moved orion-ui components into index for components folder

--- orion-ui/packages/orion-design/src/index.ts
+++ orion-ui/packages/orion-design/src/index.ts
@@ -1,7 +1,3 @@
-// from orion-ui. everything here should eventually get moved into orion-design
-// eslint-disable-next-line import/export
-export * from '@/components/'
-
 export * from './components'
 export * from './types'
 export * from './models'
