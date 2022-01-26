72942d735c3b0eb666ef7bd410253c8ff928edb8
Chris White <chris@prefect.io>
2022-01-26 09:23:37 -0800

    Add CLI pizazz for agents

--- src/prefect/cli/agent.py
+++ src/prefect/cli/agent.py
@@ -9,15 +9,27 @@ from prefect.agent import OrionAgent
 from prefect.cli.base import app, console
 from prefect.utilities.asyncio import sync_compatible
 
-agent_app = typer.Typer(name="agent")
+agent_app = typer.Typer(
+    name="agent", help="Commands for starting and interacting with Agent processes."
+)
 app.add_typer(agent_app)
+
+
+ascii_name = r"""
+ ____            __           _        _                    _
+|  _ \ _ __ ___ / _| ___  ___| |_     / \   __ _  ___ _ __ | |_
+| |_) | '__/ _ \ |_ / _ \/ __| __|   / _ \ / _` |/ _ \ '_ \| __|
+|  __/| | |  __/  _|  __/ (__| |_   / ___ \ (_| |  __/ | | | |_
+|_|   |_|  \___|_|  \___|\___|\__| /_/   \_\__, |\___|_| |_|\__|
+                                           |___/
+"""
 
 
 @agent_app.command()
 @sync_compatible
 async def start():
     """
-    Start an agent service to query for and execute scheduled flow runs.
+    Start an agent process.
     """
     if settings.orion_host:
         console.print(f"Starting agent connected to {settings.orion_host}...")
@@ -26,6 +38,7 @@ async def start():
 
     running = True
     async with OrionAgent() as agent:
+        console.print(ascii_name)
         console.print("Agent started! Checking for flow runs...")
         while running:
             try:
