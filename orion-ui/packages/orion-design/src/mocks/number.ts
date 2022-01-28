e1c490ba6ca9eea7d173e33362d8bb5f90f1da88
Evan Sutherland <stackoverfloweth@gmail.com>
2022-01-28 11:07:35 -0600

    moved away from importing mocker and using this context instead

--- orion-ui/packages/orion-design/src/mocks/number.ts
+++ orion-ui/packages/orion-design/src/mocks/number.ts
@@ -1,3 +1,5 @@
-export function randomNumber(min: number = 0, max: number = 100): number {
+import { MockerFunction } from '../services'
+
+export const randomNumber: MockerFunction<number> = function(min: number = 0, max: number = 100) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
