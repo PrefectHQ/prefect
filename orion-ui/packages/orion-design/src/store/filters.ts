e274498926833601005fe3717ccff33b284b083d
Craig Harshbarger <pleek91@gmail.com>
2022-01-31 15:44:08 -0600

    Add a Pinia store for filters

---
+++ orion-ui/packages/orion-design/src/store/filters.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia'
+import { SimpleIdManager } from '../services/SimpleIdManager'
+import { Filter } from '../types/filters'
+
+type FilterState = {
+  id: number,
+} & Filter
+
+type FiltersState = {
+  filters: Record<number, FilterState>,
+}
+
+const filtersIdManager = new SimpleIdManager()
+
+export const useFiltersStore = defineStore('filters', {
+  state: (): FiltersState => ({
+    filters: {},
+  }),
+  actions: {
+    add(filter: Filter): FilterState {
+      const id = filtersIdManager.get()
+      const filterState = {
+        ...filter,
+        id,
+      }
+
+      this.filters[id] = filterState
+
+      return filterState
+    },
+    remove(filter: FilterState): void {
+      delete this.filters[filter.id]
+    },
+  },
+  getters: {
+    all: (state) => Object.values(state.filters),
+  },
+})
+
+const filters = useFiltersStore()
+
+
+filters.remove()
