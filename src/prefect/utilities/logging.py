f9429be6decc45559b2855bcc2d6f696d9cf9306
Michael Adkins <michael@prefect.io>
2022-01-31 09:52:23 -0600

    Fixup

--- src/prefect/utilities/logging.py
+++ src/prefect/utilities/logging.py
@@ -109,7 +109,7 @@ class LogManager:
             try:
                 while self.pending_length < max_batch_length:
                     log = self.queue.get_nowait()
-                    self.pending_length += sys.getsizeof(log, len(log["message"] + 150))
+                    self.pending_length += sys.getsizeof(log, len(log["message"]) + 150)
                     self.pending_logs.append(log)
 
             except Empty:
