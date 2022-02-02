c70caa9acf97f43aa85ed537fe39e1c6f7ceaae2
Craig Harshbarger <pleek91@gmail.com>
2022-02-02 10:50:19 -0600

    Add a service for converting filters to their string tag versions

---
+++ orion-ui/packages/orion-design/src/services/FilterTagService.ts
@@ -0,0 +1,86 @@
+/* eslint-disable default-case */
+import { Filter, ObjectDateFilter, ObjectStateFilter, ObjectStringFilter, ObjectTagFilter, ObjectTimeFilter } from '../types/filters'
+import { formatDateTimeNumeric } from '../utilities/dates'
+
+export class FilterTagService {
+  public convertFiltersToTags(filters: Required<Filter>[]): string[] {
+    return filters.map(filter => this.convertFilterToTag(filter))
+  }
+
+  public convertFilterToTag(filter: Required<Filter>): string {
+    const tagPrefix = this.createTagPrefix(filter)
+    const tagSuffix = this.createTagSuffix(filter)
+    const tagValue = this.createTagValue(filter)
+
+    return `${tagPrefix}${tagSuffix}:${tagValue}`
+  }
+
+  private createObjectStringFilterValue(filter: ObjectStringFilter): string {
+    switch (filter.operation) {
+      case 'contains':
+        return filter.value
+      case 'equals':
+        return `"${filter.value}"`
+    }
+  }
+
+  private createObjectDateFilterValue(filter: ObjectDateFilter): string {
+    return formatDateTimeNumeric(filter.value)
+  }
+
+  private createObjectTimeFilterValue(filter: ObjectTimeFilter): string {
+    return filter.value
+  }
+
+  private createObjectStateFilterValue(filter: ObjectStateFilter): string {
+    return filter.value.join('|')
+  }
+
+  private createObjectTagFilterValue(filter: ObjectTagFilter): string {
+    return filter.value.join(',')
+  }
+
+  private createTagPrefix(filter: Required<Filter>): string {
+    switch (filter.object) {
+      case 'flow':
+        return 'f'
+      case 'deployment':
+        return 'd'
+      case 'flow_run':
+        return 'fr'
+      case 'task_run':
+        return 'tr'
+      case 'tag':
+        return 't'
+    }
+  }
+
+  private createTagSuffix(filter: Required<Filter>): string {
+    switch (filter.type) {
+      case 'string':
+        return ''
+      case 'state':
+      case 'tag':
+        return filter.type[0]
+      case 'date':
+      case 'time':
+        return filter.operation[0]
+    }
+  }
+
+  private createTagValue(filter: Required<Filter>): string {
+    switch (filter.type) {
+      case 'string':
+        return this.createObjectStringFilterValue(filter)
+      case 'state':
+        return this.createObjectStateFilterValue(filter)
+      case 'tag':
+        return this.createObjectTagFilterValue(filter)
+      case 'date':
+        return this.createObjectDateFilterValue(filter)
+      case 'time':
+        return this.createObjectTimeFilterValue(filter)
+    }
+  }
+
+}
