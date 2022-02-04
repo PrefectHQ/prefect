2e42a9df0ba7248a0e20e2c230539046b3fb47ab
Dustin Ngo <dustin.ngo@gmail.com>
2022-02-04 11:30:47 -0500

    Add BlockData model methods and tests

--- src/prefect/orion/models/block_data.py
+++ src/prefect/orion/models/block_data.py
@@ -16,6 +16,7 @@ async def create_block_data(
     db: OrionDBInterface,
 ):
     insert_values = block_data.dict(shallow=True, exclude_unset=True)
+    blockname = insert_values["name"]
 
     # set `updated` manually
     # known limitation of `on_conflict_do_update`, will not use `Column.onupdate`
@@ -24,6 +25,26 @@ async def create_block_data(
     insert_stmt = (await db.insert(db.BlockData)).values(**insert_values)
 
     await session.execute(insert_stmt)
+    query = (
+        sa.select(db.BlockData)
+        .where(db.BlockData.name == blockname)
+        .execution_options(populate_existing=True)
+    )
+
+    result = await session.execute(query)
+    return result.scalar()
+
+
+@inject_db
+async def read_block_data_by_name(
+    session: sa.orm.Session,
+    name: str,
+    db: OrionDBInterface,
+):
+    query = sa.select(db.BlockData).where(db.BlockData.name == name).with_for_update()
+
+    result = await session.execute(query)
+    return result.scalar()
 
 
 @inject_db
