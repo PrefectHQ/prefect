0f6ddfbdf0b3b789d8749182abd7bfdca787dcbc
Jamie Dick <jamie@prefect.io>
2022-02-04 17:17:33 -0500

    importing task

--- src/prefect/tasks/airbyte/__init__.py
+++ src/prefect/tasks/airbyte/__init__.py
@@ -1,6 +1,6 @@
 """
 This module contains a task for triggering [Airbyte](https://airbyte.io/) connection sync jobs
 """
-from .airbyte import AirbyteConnectionTask
+from .airbyte import AirbyteConnectionTask, AirbyteConfigurationExport
 
 __all__ = ["AirbyteConnectionTask","AirbyteConfigurationExport"]
