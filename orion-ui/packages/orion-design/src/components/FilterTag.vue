b3751031a2e943fc3a78a02318dbf66da6bf6932
Craig Harshbarger <pleek91@gmail.com>
2022-02-04 09:26:37 -0600

    Start FilterTag component

---
+++ orion-ui/packages/orion-design/src/components/FilterTag.vue
@@ -0,0 +1,20 @@
+<template>
+  <span class="filter-tag">
+    {{ label }}
+  </span>
+</template>
+
+<script lang="ts" setup>
+  import { computed } from 'vue'
+  import { Filter } from '../types/filters'
+
+  type Props = {
+    filter: Required<Filter>,
+  }
+
+  const props = defineProps<Props>()
+
+  const label = computed<string>(() => props.filter.value)
+</script>
+
+<style lang="scss" scoped></style>
