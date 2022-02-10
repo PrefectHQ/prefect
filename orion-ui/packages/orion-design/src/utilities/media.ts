c5041249943e115aa6b3ee365feda950466fad28
Evan Sutherland <stackoverfloweth@gmail.com>
2022-02-09 21:47:00 -0600

    fixed export of media

--- orion-ui/packages/orion-design/src/utilities/media.ts
+++ orion-ui/packages/orion-design/src/utilities/media.ts
@@ -1,16 +1,16 @@
-import { media } from '@prefecthq/vue-compositions'
+import { media as matchMedia } from '@prefecthq/vue-compositions'
 import { reactive } from 'vue'
 
-const xs = media('(min-width: 450px)')
-const sm = media('(min-width: 640px)')
-const md = media('(min-width: 1024px)')
-const lg = media('(min-width: 1280px)')
-const xl = media('(min-width: 1440px)')
+const xs = matchMedia('(min-width: 450px)')
+const sm = matchMedia('(min-width: 640px)')
+const md = matchMedia('(min-width: 1024px)')
+const lg = matchMedia('(min-width: 1280px)')
+const xl = matchMedia('(min-width: 1440px)')
 
-export default reactive({
-    xs,
-    sm,
-    md,
-    lg,
-    xl
-  })
+export const media = reactive({
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+})
