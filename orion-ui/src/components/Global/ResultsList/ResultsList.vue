978632432e8e50444f7fcb4bb641659506cd0417
nicholas <znicholasbrown@gmail.com>
2022-02-11 15:53:15 -0500

    Use new filter service for dashboard queries

--- orion-ui/src/components/Global/ResultsList/ResultsList.vue
+++ orion-ui/src/components/Global/ResultsList/ResultsList.vue
@@ -60,7 +60,11 @@ const filter_ = computed(() => {
 
 const queries: { query: Query; watcher: WatchStopHandle }[] = []
 
+let watcher: WatchStopHandle
+
 const getData = async () => {
+  if (watcher) watcher()
+
   loading.value = true
 
   const query = Api.query({
@@ -71,7 +75,7 @@ const getData = async () => {
     }
   })
 
-  const watcher = watch(query.response, (val) => {
+  watcher = watch(query.response, (val) => {
     if (val) {
       val.forEach((r: any) => {
         items.value.set(r.id, r)
