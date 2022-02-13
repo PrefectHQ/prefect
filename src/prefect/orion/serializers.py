e0c1468b8f39d5264353a1df62caf7ebb8c12aea
Dustin Ngo <dustin.ngo@gmail.com>
2022-02-13 10:07:11 -0500

    Remove all server-side serializers

--- src/prefect/orion/serializers.py
+++ src/prefect/orion/serializers.py
@@ -2,12 +2,6 @@ from typing import TYPE_CHECKING, Any, Dict, Tuple, TypeVar, Union
 
 from typing_extensions import Protocol
 
-from prefect.orion.utilities.filesystem import (
-    FILE_SYSTEM_SCHEMES,
-    read_blob,
-    write_blob,
-)
-
 _SERIALIZERS: Dict[str, "Serializer"] = {}
 D = TypeVar("D")
 
@@ -48,26 +42,3 @@ def lookup_serializer(encoding: str) -> Serializer:
         return _SERIALIZERS[encoding]
     except KeyError:
         raise ValueError(f"Unregistered encoding {encoding!r}")
-
-
-# Server-side serializers --------------------------------------------------------------
-
-
-@register_serializer(encoding=FILE_SYSTEM_SCHEMES)
-class FileSerializer(Serializer[bytes]):
-    """
-    Persists bytes to a file system and creates a blob with a path to the data for
-    future retrieval
-    """
-
-    @staticmethod
-    def dumps(data: bytes, path: str) -> bytes:
-        write_blob(data, path)
-
-        # Return the path as bytes to conform to the spec
-        return path.encode()
-
-    @staticmethod
-    def loads(blob: bytes) -> bytes:
-        path = blob.decode()
-        return read_blob(path)
