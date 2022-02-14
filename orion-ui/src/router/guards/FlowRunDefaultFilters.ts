a6addc63bb894731884313f2ee6692286a238ef4
Craig Harshbarger <pleek91@gmail.com>
2022-02-14 10:48:04 -0600

    Add a super bare bones FlowRunsApi service. Update the flow run filters guard to use the service to get the name

--- orion-ui/src/router/guards/FlowRunDefaultFilters.ts
+++ orion-ui/src/router/guards/FlowRunDefaultFilters.ts
@@ -1,19 +1,20 @@
-import { Filter } from "@/../packages/orion-design/src/types/filters/index";
 import { useFiltersStore } from "@/../packages/orion-design/src/stores/filters";
+import { flowRunsApi } from "@/../packages/orion-design/src/services/FlowRunsApi";
 import { RouteLocationNormalized } from "vue-router";
 import { RouteGuard } from "./RouteGuard";
 
 export class FlowRunDefaultFilters implements RouteGuard {
   public async before(to: RouteLocationNormalized): Promise<void> {
-    const filtersInRoute = to.query.filter ?? []
     const filtersStore = useFiltersStore()
 
-    filtersStore.add({
-      object: 'flow_run',
-      property: 'name',
-      type: 'string',
-      operation: 'equals',
-      value: 'hello world'
+    flowRunsApi.getFlowRun(to.params.id as string).then(({ name }) => {
+      filtersStore.add({
+        object: 'flow_run',
+        property: 'name',
+        type: 'string',
+        operation: 'equals',
+        value: name
+      })
     })
   }
 }
