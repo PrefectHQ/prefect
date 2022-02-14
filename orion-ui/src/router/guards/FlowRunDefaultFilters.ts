0c03f20174528345711b964886c456664a04a103
Craig Harshbarger <pleek91@gmail.com>
2022-02-13 23:25:43 -0600

    Tweak the route guards a bit

--- orion-ui/src/router/guards/FlowRunDefaultFilters.ts
+++ orion-ui/src/router/guards/FlowRunDefaultFilters.ts
@@ -4,20 +4,16 @@ import { RouteLocationNormalized } from "vue-router";
 import { RouteGuard } from "./RouteGuard";
 
 export class FlowRunDefaultFilters implements RouteGuard {
-  private readonly filter: Required<Filter> = {
-    object: 'flow_run',
-    property: 'name',
-    type: 'string',
-    operation: 'equals',
-    value: ''
-  }
-
   public async before(to: RouteLocationNormalized): Promise<void> {
     const filtersInUrl = to.query.filter ?? []
     const filtersStore = useFiltersStore()
 
-    this.filter.value = 'hello world'
-
-    filtersStore.add(this.filter)
+    filtersStore.add({
+      object: 'flow_run',
+      property: 'name',
+      type: 'string',
+      operation: 'equals',
+      value: 'hello world'
+    })
   }
 }
