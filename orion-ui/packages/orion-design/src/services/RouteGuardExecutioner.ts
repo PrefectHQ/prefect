e89f41d3644212c9109a4889b5f891848d3491e0
Craig Harshbarger <pleek91@gmail.com>
2022-02-15 10:50:06 -0600

    Fix RouteGuardExecutioner

--- orion-ui/packages/orion-design/src/services/RouteGuardExecutioner.ts
+++ orion-ui/packages/orion-design/src/services/RouteGuardExecutioner.ts
@@ -1,16 +1,16 @@
-import { RouteLocationNormalized, RouteLocationRaw } from "vue-router";
-import { RouteGuard, RouteGuardReturn } from "./RouteGuard";
+import { RouteLocationNormalized, RouteLocationRaw } from 'vue-router'
+import { RouteGuard, RouteGuardReturn } from '../types/RouteGuard'
 
 export class RouteGuardExecutioner {
-  private static global: RouteGuard[] = []
+  private static readonly global: RouteGuard[] = []
 
   public static async before(to: RouteLocationNormalized, from: RouteLocationNormalized): Promise<Awaited<RouteGuardReturn>> {
-    const guards = this.getRouteGuards(to)    
-    
-    for(let guard of guards) {
+    const guards = this.getRouteGuards(to)
+
+    for (const guard of guards) {
       const result = await guard.before?.(to, from)
 
-      if(this.isRouteLocation(result)) {
+      if (this.isRouteLocation(result)) {
         return result
       }
     }
@@ -19,7 +19,7 @@ export class RouteGuardExecutioner {
   public static after(to: RouteLocationNormalized, from: RouteLocationNormalized): void {
     const guards = this.getRouteGuards(to)
 
-    for(let guard of guards) {
+    for (const guard of guards) {
       guard.after?.(to, from)
     }
   }
@@ -29,7 +29,7 @@ export class RouteGuardExecutioner {
   }
 
   private static getRouteGuards(route: RouteLocationNormalized): RouteGuard[] {
-    const routeGuards = route.meta.guards ?? []
+    const routeGuards = (route.meta.guards ?? []) as RouteGuard[]
 
     return [...this.global, ...routeGuards]
   }
