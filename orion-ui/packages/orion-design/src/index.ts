1c645249c22b691ce40518df993abb773e1adc2e
nicholas <znicholasbrown@gmail.com>
2022-02-16 11:02:42 -0500

    Add install method

--- orion-ui/packages/orion-design/src/index.ts
+++ orion-ui/packages/orion-design/src/index.ts
@@ -6,3 +6,24 @@ export * from './services'
 export * from './stores'
 export * from './types'
 export * from './utilities'
+
+
+import { App } from 'vue'
+
+import * as components from './components'
+
+
+function install(app: App) {
+    for (const component in components) {
+        // @ts-expect-error
+        if (components[component].install) {
+            // @ts-expect-error
+            components[component].install(app)
+        } else {
+            // @ts-expect-error
+            app.component(`O${component}`, components[component])
+        }
+    }
+}
+
+export default { install }
