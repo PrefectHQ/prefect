4003b1be66c5ac7d844d9df704588dfdddf2a8c4
Evan Sutherland <stackoverfloweth@gmail.com>
2022-02-17 11:14:41 -0600

    removed unused router folder and contents

--- orion-ui/packages/orion-design/src/index.ts
+++ orion-ui/packages/orion-design/src/index.ts
@@ -1,7 +1,6 @@
 export * from './components'
 export * from './mocks'
 export * from './models'
-export * from './router'
 export * from './services'
 export * from './stores'
 export * from './types'
