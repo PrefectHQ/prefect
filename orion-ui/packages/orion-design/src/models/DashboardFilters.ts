bacaed31a6c145cdefd35dc49981b4b1854a072c
Evan Sutherland <stackoverfloweth@gmail.com>
2022-02-17 11:14:41 -0600

    removed unused router folder and contents

--- orion-ui/packages/orion-design/src/models/DashboardFilters.ts
+++
@@ -1,6 +0,0 @@
-import { UrlQueryParams } from '@/router/UrlQueryParams'
-
-export class DashboardFilters extends UrlQueryParams {
-  // eslint-disable-next-line camelcase
-  public flow_run_states = []
-}
