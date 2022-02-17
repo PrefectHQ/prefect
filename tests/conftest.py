b0b2aa6115f2ae862127312af12f3434ad7b2930
Michael Adkins <michael@prefect.io>
2022-02-17 12:29:39 -0600

    Add `--only-service` support to easily select a single service for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import asyncio
-import inspect
 import logging
 import pathlib
+import warnings
 
 import pytest
 
@@ -26,6 +26,13 @@ def pytest_addoption(parser):
         help="include service integration tests for SERVICE.",
     )
     parser.addoption(
+        "--only-service",
+        action="append",
+        metavar="SERVICE",
+        default=[],
+        help="exclude all tests except service integration tests for SERVICE.",
+    )
+    parser.addoption(
         "--all-services",
         action="store_true",
         default=False,
@@ -41,6 +48,21 @@ def pytest_collection_modifyitems(session, config, items):
         # Do not skip any service tests
         return
 
+    only_services = set(config.getoption("--only-service"))
+    if only_services:
+        only_running_blurb = f"Only running tests for service(s): {', '.join(repr(s) for s in only_services)}."
+        for item in items:
+            item_services = {mark.args[0] for mark in item.iter_markers(name="service")}
+            not_in_only_services = only_services.difference(item_services)
+            if not_in_only_services:
+                item.add_marker(pytest.mark.skip(only_running_blurb))
+
+        if config.getoption("--service"):
+            warnings.warn(
+                "`--only-service` cannot be used with `--service`. `--service` will be ignored."
+            )
+        return
+
     run_services = set(config.getoption("--service"))
     for item in items:
         item_services = {mark.args[0] for mark in item.iter_markers(name="service")}
@@ -48,7 +70,7 @@ def pytest_collection_modifyitems(session, config, items):
         if missing_services:
             item.add_marker(
                 pytest.mark.skip(
-                    f"Requires service {', '.join(repr(s) for s in missing_services)}. "
+                    f"Requires service(s): {', '.join(repr(s) for s in missing_services)}. "
                     "Use '--service NAME' to include."
                 )
             )
