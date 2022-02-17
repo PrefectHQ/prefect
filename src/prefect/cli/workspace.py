ceb629745b7c0035b5ee60e5dce0f00bf879d638
Jake Kaplan <jake@prefect.io>
2022-02-17 09:32:28 -0500

    re-add workspace list command

--- src/prefect/cli/workspace.py
+++ src/prefect/cli/workspace.py
@@ -14,6 +14,7 @@ import inquirer
 from prefect.cli.base import (
     PrefectTyper,
     app,
+    console,
     exit_with_error,
     exit_with_success,
 )
@@ -40,8 +41,7 @@ class NebulaClient:
 
         httpx_settings = httpx_settings or {}
         httpx_settings.setdefault("headers", {})
-        if api_key:
-            httpx_settings["headers"].setdefault("Authorization", f"Bearer {api_key}")
+        httpx_settings["headers"].setdefault("Authorization", f"Bearer {api_key}")
 
         httpx_settings.setdefault("base_url", host)
         self._client = httpx.AsyncClient(**httpx_settings)
@@ -53,18 +53,53 @@ class NebulaClient:
 
 
 @workspace_app.command()
-async def login(
-    api_key: str = typer.Option(
-        ..., help="API Key to authenticate with Prefect", prompt=True
-    ),
-    workspace_handle: str = typer.Option(
-        None,
-        help="Full handle of workspace to login to (e.x. '<account_handle>`/<workspace_handle>').",
+async def list(
+    key: str = typer.Option(
+        None, "--key", "-k", help="API Key to authenticate with Prefect"
     ),
 ):
-    """Login. Sets PREFECT_API_URL and PREFECT_API_KEY."""
+    """Lists workspaces."""
+
+    api_key = key or PREFECT_API_KEY.value()
+    if api_key is None:
+        exit_with_error(
+            "Currently not logged in. Please provide `--key` to list workspaces."
+        )
 
     client = NebulaClient(api_key=api_key)
+    try:
+        workspaces = await client.read_workspaces()
+    except httpx.HTTPStatusError:
+        exit_with_error(
+            "Unable to authenticate. Please ensure your credentials are correct."
+        )
+
+    workspace_handles = [
+        format_handle(workspace["account_handle"], workspace["workspace_handle"])
+        for workspace in workspaces
+    ]
+    for handle in sorted(workspace_handles):
+        console.print(handle)
+
+
+@workspace_app.command()
+async def login(
+    key: str = typer.Option(
+        ..., "--key", "-k", help="API Key to authenticate with Prefect", prompt=True
+    ),
+    name: str = typer.Option(
+        None,
+        "--name",
+        "-n",
+        help="Full handle of workspace to login in the format '<account_handle>/<workspace_handle>'",
+    ),
+):
+    """
+    Login. Sets PREFECT_API_URL and PREFECT_API_KEY for profile.
+    If those values are already set they will be overwritten.
+    """
+
+    client = NebulaClient(api_key=key)
     try:
         workspaces = await client.read_workspaces()
     except httpx.HTTPStatusError:
@@ -77,13 +112,13 @@ async def login(
         for workspace in workspaces
     }
 
-    if workspace_handle:
-        if workspace_handle not in workspaces:
+    if name:
+        if name not in workspaces:
             exit_with_error(
-                f"Workspace {workspace_handle!r} not found. "
+                f"Workspace {name!r} not found. "
                 f"Leave `--workspace-handle` blank to select a workspace."
             )
-        workspace = workspaces[workspace_handle]
+        workspace = workspaces[name]
     else:
         questions = [
             inquirer.List(
@@ -107,23 +142,26 @@ async def login(
     )
 
     env["PREFECT_API_URL"] = host_url
-    env["PREFECT_API_KEY"] = api_key
+    env["PREFECT_API_KEY"] = key
     prefect.settings.write_profiles(profiles)
 
     exit_with_success(
-        f"Successfully logged into workspace {workspace_handle!r} with profile {profile.name!r}."
+        f"Successfully logged into workspace {name!r} with profile {profile.name!r}."
     )
 
 
 @workspace_app.command()
 async def logout():
-    """Logout. Unsets PREFECT_API_URL and PREFECT_API_KEY."""
+    """Logout. Removes PREFECT_API_URL and PREFECT_API_KEY from profile."""
+
     profiles = prefect.settings.load_profiles()
     profile = prefect.context.get_profile_context()
     env = profiles[profile.name]
 
-    env.pop("PREFECT_API_URL")
-    env.pop("PREFECT_API_KEY")
+    if "PREFECT_API_URL" in env:
+        env.pop("PREFECT_API_URL")
+    if "PREFECT_API_KEY" in env:
+        env.pop("PREFECT_API_KEY")
     prefect.settings.write_profiles(profiles)
 
     exit_with_success(f"Successfully logged out with profile {profile.name!r}")
