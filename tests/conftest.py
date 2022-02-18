63e624b659ea04698d5665eb7e32dd3619421cf1
Michael Adkins <michael@prefect.io>
2022-02-17 22:14:55 -0600

    Add option to exclude services in preparation for dropping ray from CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,6 +38,13 @@ def pytest_addoption(parser):
         default=False,
         help="include all service integration tests",
     )
+    parser.addoption(
+        "--exclude-service",
+        action="append",
+        metavar="SERVICE",
+        default=[],
+        help="exclude service integration tests for SERVICE.",
+    )
 
 
 def pytest_collection_modifyitems(session, config, items):
@@ -45,7 +52,22 @@ def pytest_collection_modifyitems(session, config, items):
     Modify all tests to automatically and transparently support asyncio
     """
     if config.getoption("--all-services"):
-        # Do not skip any service tests
+        exclude_services = set(config.getoption("--exclude-service"))
+        for item in items:
+            item_services = {mark.args[0] for mark in item.iter_markers(name="service")}
+            excluded_services = item_services.intersection(exclude_services)
+            if excluded_services:
+                item.add_marker(
+                    pytest.mark.skip(
+                        f"Excluding tests for service(s): {', '.join(repr(s) for s in excluded_services)}."
+                    )
+                )
+
+        if config.getoption("--only-service"):
+            warnings.warn(
+                "`--only-service` cannot be used with `--all-services`. "
+                "`--only-service` will be ignored."
+            )
         return
 
     only_services = set(config.getoption("--only-service"))
@@ -59,7 +81,8 @@ def pytest_collection_modifyitems(session, config, items):
 
         if config.getoption("--service"):
             warnings.warn(
-                "`--only-service` cannot be used with `--service`. `--service` will be ignored."
+                "`--service` cannot be used with `--only-service`. "
+                "`--service` will be ignored."
             )
         return
 
