fd4eb8ff87201df2cfe3c726593dd6ddd17e1237
Zach Angell <zachary.james.angell@gmail.com>
2022-02-21 11:13:14 -0500

    Index FlowRun.flow_runner_type

---
+++ src/prefect/orion/database/migrations/versions/sqlite/f327e877e423_index_flowrun_flow_runner_type.py
@@ -0,0 +1,28 @@
+"""Index FlowRun.flow_runner_type
+
+Revision ID: f327e877e423
+Revises: 7c91cb86dc4e
+Create Date: 2022-02-21 11:12:38.518778
+
+"""
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = "f327e877e423"
+down_revision = "7c91cb86dc4e"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index(
+        op.f("ix_flow_run__flow_runner_type"),
+        "flow_run",
+        ["flow_runner_type"],
+        unique=False,
+    )
+
+
+def downgrade():
+    op.drop_index(op.f("ix_flow_run__flow_runner_type"), table_name="flow_run")
