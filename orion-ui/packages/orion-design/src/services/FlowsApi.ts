1ed155373e65c93ff884b9df4f7f390983c28265
Evan Sutherland <stackoverfloweth@gmail.com>
2022-02-21 13:22:40 -0600

    added new getFlows method to flowsApi

--- orion-ui/packages/orion-design/src/services/FlowsApi.ts
+++ orion-ui/packages/orion-design/src/services/FlowsApi.ts
@@ -1,12 +1,42 @@
+import { AxiosResponse } from 'axios'
+import { Flow } from '@/models/Flow'
 import { Api, Route } from '@/services/Api'
 import { UnionFilters } from '@/services/Filter'
+
+export type IFlowResponse = {
+  created: string,
+  id: string,
+  name: string,
+  tags: string[],
+  updated?: string,
+}
 
 export class FlowsApi extends Api {
 
   protected route: Route = '/flows'
 
+  public getFlows(filter: UnionFilters): Promise<Flow[]> {
+    return this.post<IFlowResponse[]>('/filter', filter).then(this.mapFlowsResponse)
+  }
+
   public getFlowsCount(filter: UnionFilters): Promise<number> {
     return this.post<number>('/count', filter).then(({ data }) => data)
+  }
+
+  protected mapFlow(data: IFlowResponse): Flow {
+    return new Flow({
+      ...data,
+      updated: data.updated ? new Date(data.updated) : null,
+      created: new Date(data.created),
+    })
+  }
+
+  protected mapFlowResponse({ data }: AxiosResponse<IFlowResponse>): Flow {
+    return this.mapFlow(data)
+  }
+
+  protected mapFlowsResponse({ data }: AxiosResponse<IFlowResponse[]>): Flow[] {
+    return data.map(this.mapFlow)
   }
 
 }
