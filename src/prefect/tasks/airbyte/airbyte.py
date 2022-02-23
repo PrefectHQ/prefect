d7a67bf722381f56c2c922e826a72b132d698edf
Jamie Dick <jamie.m.dick@gmail.com>
2022-02-23 14:14:34 -0500

    Update src/prefect/tasks/airbyte/airbyte.py
    Co-authored-by: Terrence Dorsey <terrend@mishu.com>

--- src/prefect/tasks/airbyte/airbyte.py
+++ src/prefect/tasks/airbyte/airbyte.py
@@ -371,7 +371,7 @@ class AirbyteConfigurationExport(Task):
     Airbyte Open-Source you don't need the API Token for
     Authentication! All endpoints are possible to access using the
     API without it."
-    For more information refer to the [Airbyte docs](https://docs.airbyte.io/api-documentation)
+    For more information refer to the [Airbyte docs](https://docs.airbyte.io/api-documentation).
 
     Args:
         - airbyte_server_host (str, optional): Hostname of Airbyte server where connection is configured.
