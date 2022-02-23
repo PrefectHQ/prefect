3fd9449273ccb89a6e7cb203e37c20776acc00bf
葛兵 <gebing@foxmail.com>
2022-02-24 01:53:48 +0800

    Add support for flatten() when processing results that contain no flattenable value (#5337)
    * Fixed for Issue #4084, #3631, #5299, #4455, #4566, #4277.
    
    Add support for flatten() when processing results that contain no flattenable value.
    
    * Add change file in change directory.
    
    * Run `black`
    
    * Add test with exception
    
    * Add test for noniterable behavior
    
    * Add warning if flatten is used with non-iterable inputs
    
    * Log the warning instead for visibility
    
    * Actually use parameterized executors in tests
    
    Co-authored-by: Michael Adkins <michael@prefect.io>

--- src/prefect/utilities/executors.py
+++ src/prefect/utilities/executors.py
@@ -45,6 +45,8 @@ if TYPE_CHECKING:
 
 StateList = Union["State", List["State"]]
 
+FLOWRUNNER_LOGGER = get_logger("FlowRunner")
+
 
 def run_with_heartbeat(
     runner_method: Callable[..., "prefect.engine.state.State"]
@@ -696,10 +698,30 @@ def prepare_upstream_states_for_mapping(
     return map_upstream_states
 
 
+def _should_flatten(state: "State") -> bool:
+    return state.is_successful() and not state.is_skipped()
+
+
+def _can_flatten(state: "State") -> bool:
+    return hasattr(state.result, "__len__") and hasattr(state.result, "__getitem__")
+
+
 def _build_flattened_state(state: "State", index: int) -> "State":
     """Helper function for `flatten_upstream_state`"""
+    if not _should_flatten(state):
+        return state
+
     new_state = copy.copy(state)
-    new_state.result = state._result.from_value(state.result[index])  # type: ignore
+    if not _can_flatten(state):
+        message = (
+            "`flatten` was used on upstream task that did not return an iterable. "
+            "The value will be passed downstream unmodified."
+        )
+        FLOWRUNNER_LOGGER.warning(message)
+        new_state.result = state._result.from_value(state.result)  # type: ignore
+    else:
+        new_state.result = state._result.from_value(state.result[index])  # type: ignore
+
     return new_state
 
 
@@ -724,7 +746,10 @@ def flatten_mapped_children(
     executor: "prefect.executors.Executor",
 ) -> List["State"]:
     counts = executor.wait(
-        [executor.submit(lambda c: len(c._result.value), c) for c in mapped_children]
+        [
+            executor.submit(lambda c: len(c._result.value) if _can_flatten(c) else 1, c)
+            for c in mapped_children
+        ]
     )
     new_states = []
 
