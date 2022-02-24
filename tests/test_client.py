8094edb5add0f1b46b4d4f837387c806540f6d8a
Michael Adkins <michael@prefect.io>
2022-02-24 09:53:57 -0600

    Update client tests; preserve good error on restart

--- tests/test_client.py
+++ tests/test_client.py
@@ -30,8 +30,8 @@ class TestGetClient:
     def test_get_client_returns_client(self):
         assert isinstance(get_client(), OrionClient)
 
-    def test_get_client_caches_client(self):
-        assert get_client() is get_client()
+    def test_get_client_does_not_cache_client(self):
+        assert get_client() is not get_client()
 
     def test_get_client_cache_uses_profile_settings(self):
         client = get_client()
@@ -40,27 +40,16 @@ class TestGetClient:
             assert isinstance(new_client, OrionClient)
             assert new_client is not client
 
-    async def test_get_client_can_occur_in_client_context(self):
-        async with get_client() as client:
-            assert get_client() is client
-
-    async def test_get_client_returns_fresh_client_after_close(self):
-        async with get_client() as client:
-            pass
-
-        new_client = get_client()
-        assert isinstance(new_client, OrionClient)
-        assert new_client is not client
-
 
 class TestClientContextManager:
-    async def test_client_context_can_be_nested_with_external_api(self):
+    async def test_client_context_cannot_be_reentered(self):
         client = OrionClient("http://foo.test")
         async with client:
-            async with client:
-                pass
+            with pytest.raises(RuntimeError, match="cannot be started more than once"):
+                async with client:
+                    pass
 
-    async def test_client_context_cannot_be_used_after_closing(self):
+    async def test_client_context_cannot_be_reused(self):
         client = OrionClient("http://foo.test")
         async with client:
             pass
@@ -68,18 +57,6 @@ class TestClientContextManager:
         with pytest.raises(RuntimeError, match="cannot be started again after closing"):
             async with client:
                 pass
-
-    async def test_client_context_can_be_nested_with_app(self, app):
-        app = FastAPI()
-
-        @app.get("/api/admin/hello")
-        def hello() -> bool:
-            return True
-
-        client = OrionClient(app)
-        async with client:
-            async with client:
-                (await client.hello()).raise_for_status()
 
     async def test_client_context_manages_app_lifespan(self):
         startup, shutdown = MagicMock(), MagicMock()
@@ -96,29 +73,16 @@ class TestClientContextManager:
         startup.assert_called_once()
         shutdown.assert_called_once()
 
-    async def test_client_can_opt_out_of_lifespan_management(self):
-        startup, shutdown = MagicMock(), MagicMock()
-        app = FastAPI(on_startup=[startup], on_shutdown=[shutdown])
-
-        client = OrionClient(app, manage_ephemeral_lifespan=False)
-
-        async with client:
-            pass
-
-        startup.assert_not_called()
-        shutdown.assert_not_called()
-
     async def test_client_context_calls_app_lifespan_once_despite_nesting(self):
         startup, shutdown = MagicMock(), MagicMock()
         app = FastAPI(on_startup=[startup], on_shutdown=[shutdown])
 
-        client = OrionClient(app)
         startup.assert_not_called()
         shutdown.assert_not_called()
 
-        async with client:
-            async with client:
-                async with client:
+        async with OrionClient(app):
+            async with OrionClient(app):
+                async with OrionClient(app):
                     startup.assert_called_once()
             shutdown.assert_not_called()
 
@@ -137,34 +101,6 @@ class TestClientContextManager:
 
         startup.assert_called_once()
         shutdown.assert_called_once()
-
-    async def test_client_started_closed_counts(self):
-        client = OrionClient("http://foo.test")
-        assert client._started == 0
-        assert not client.is_closed
-
-        async with client:
-            assert client._started == 1
-            async with client:
-                assert client._started == 2
-            assert not client.is_closed
-            assert client._started == 1
-
-        assert client._started == 0
-        assert client.is_closed
-
-    async def test_client_context_counts_are_robust_to_concurrent_access(self):
-        client = OrionClient("http://foo.test")
-
-        async def enter_context():
-            async with client:
-                await asyncio.sleep(random.random())
-                return client._started
-
-        coroutines = [enter_context() for _ in range(20)]
-        results = await asyncio.gather(*coroutines)
-        assert all(result > 0 for result in results), "Never dipped below zero"
-        assert client.is_closed, "Client closes in the end"
 
     async def test_with_without_async_raises_helpful_error(self):
         with pytest.raises(RuntimeError, match="must be entered with an async context"):
@@ -200,11 +136,6 @@ async def test_client_does_not_run_migrations_for_hosted_app(
             pass
 
     mock.assert_not_awaited()
-
-
-async def test_client_is_ephemeral():
-    assert not OrionClient("http://foo.test").is_ephemeral
-    assert OrionClient(FastAPI()).is_ephemeral
 
 
 async def test_client_api_url():
