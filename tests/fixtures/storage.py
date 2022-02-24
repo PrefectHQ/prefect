f6f92bb801f0a14ddbda1323b74a5423e39bdb2f
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2022-02-24 07:56:59 -0500

    Refactor fixtures

--- tests/fixtures/storage.py
+++ tests/fixtures/storage.py
@@ -1,6 +1,8 @@
 """
-A small distributed storage API
+Fixtures that create a small distributed storage API, including a storage block
 """
+import httpx
+import anyio
 import subprocess
 import sys
 import time
@@ -10,7 +12,9 @@ import pytest
 from fastapi import Body, FastAPI
 from fastapi.exceptions import RequestValidationError
 
+from prefect.orion import models
 from prefect.orion.api.server import validation_exception_handler
+from prefect.orion.schemas.actions import BlockCreate, BlockSpecCreate
 
 app = FastAPI(exception_handlers={RequestValidationError: validation_exception_handler})
 
@@ -34,7 +38,10 @@ async def read_all():
 
 
 @pytest.fixture(scope="session")
-def run_storage_server():
+async def run_storage_server():
+    """
+    Run the simple storage API in a subprocess
+    """
     process = subprocess.Popen(
         [
             "uvicorn",
@@ -47,10 +54,61 @@ def run_storage_server():
         stdout=sys.stdout,
         stderr=subprocess.STDOUT,
     )
-    # ensure the server has time to start
-    time.sleep(0.5)
     try:
+        # Wait for the server to be ready
+        async with httpx.AsyncClient() as client:
+            response = None
+            with anyio.move_on_after(10):
+                while True:
+                    try:
+                        response = await client.get("http://127.0.0.1:1234/debug")
+                    except httpx.ConnectError:
+                        pass
+                    else:
+                        if response.status_code == 200:
+                            break
+                    await anyio.sleep(0.1)
+            if response:
+                response.raise_for_status()
+            if not response:
+                raise RuntimeError(
+                    "Timed out while attempting to connect to hosted test Orion."
+                )
+
+        # Yield to the consuming tests
         yield
+
     finally:
-        process.terminate()
-        process.kill()
+        # Cleanup the process
+        try:
+            process.terminate()
+            await process.aclose()
+            process.kill()
+        except Exception:
+            pass  # May already be terminated
+
+
+@pytest.fixture
+async def set_up_kv_storage(session, run_storage_server):
+    """
+    Use this fixture to make the distributed storage server the default
+    (useful for distributed flow or task runner tests)
+    """
+    block_spec = await models.block_specs.create_block_spec(
+        session=session,
+        block_spec=BlockSpecCreate(
+            name="simple-kv-storage-block", version="1", type="STORAGE", fields=dict()
+        ),
+    )
+
+    block = await models.blocks.create_block(
+        session=session,
+        block=BlockCreate(
+            name="test-storage-block",
+            data=dict(api_address="http://127.0.0.1:1234/storage"),
+            block_spec_id=block_spec.id,
+        ),
+    )
+
+    await models.blocks.set_default_storage_block(session=session, block_id=block.id)
+    await session.commit()
