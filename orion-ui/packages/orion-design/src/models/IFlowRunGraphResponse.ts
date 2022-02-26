8da35bf0241e4574a8b064cc725a69feff2fe935
nicholas <znicholasbrown@gmail.com>
2022-02-25 20:06:27 -0500

    Create TaskRunInputType

--- orion-ui/packages/orion-design/src/models/IFlowRunGraphResponse.ts
+++ orion-ui/packages/orion-design/src/models/IFlowRunGraphResponse.ts
@@ -1,10 +1,11 @@
 import { IStateResponse } from '@/services/StatesApi'
+import { TaskRunInputType } from '@/models'
 
 export type IFlowRunGraphResponse = {
   id: string,
   upstream_dependencies: {
     id: string,
-    input_type: 'constant' | 'parameter' | 'task_run',
+    input_type: TaskRunInputType,
   }[],
   state: IStateResponse,
 }
