162b10625194180430524e6e05f11985aca560db
Dylan Hughes <dylanbhughes@gmail.com>
2022-02-27 17:18:26 -0500

    Set or fetch session information in configuration table

--- src/prefect/orion/services/telemetry.py
+++ src/prefect/orion/services/telemetry.py
@@ -10,6 +10,10 @@ import httpx
 import pendulum
 
 import prefect
+from prefect.orion.database.dependencies import inject_db
+from prefect.orion.database.interface import OrionDBInterface
+from prefect.orion.models import configuration
+from prefect.orion.schemas.core import Configuration
 from prefect.orion.services.loop_service import LoopService
 
 
@@ -20,17 +24,55 @@ class Telemetry(LoopService):
 
     def __init__(self):
         super().__init__(loop_seconds=600)
-        self.session_id = str(uuid4())
-        self.session_start_timestamp = pendulum.now().to_iso8601_string()
         self.telemetry_environment = os.environ.get(
             "PREFECT_ORION_TELEMETRY_ENVIRONMENT", "production"
         )
+
+    @inject_db
+    async def _fetch_or_set_telemetry_session(self, db: OrionDBInterface):
+        session = await db.session()
+        async with session:
+            async with session.begin():
+                telemetry_session = await configuration.read_configuration(
+                    session, "TELEMETRY_SESSION"
+                )
+
+                if telemetry_session is None:
+                    self.logger.debug("No telemetry session found, setting")
+                    session_id = str(uuid4())
+                    session_start_timestamp = pendulum.now().to_iso8601_string()
+
+                    telemetry_session = Configuration(
+                        key="TELEMETRY_SESSION",
+                        value={
+                            "session_id": session_id,
+                            "session_start_timestamp": session_start_timestamp,
+                        },
+                    )
+
+                    await configuration.write_configuration(session, telemetry_session)
+
+                    self.session_id = session_id
+                    self.session_start_timestamp = session_start_timestamp
+                else:
+                    self.logger.debug("Session information retrieved from database")
+                    self.session_id = telemetry_session.value["session_id"]
+                    self.session_start_timestamp = telemetry_session.value[
+                        "session_start_timestamp"
+                    ]
+        self.logger.debug(
+            f"""Telemetry Session: {self.session_id}, {self.session_start_timestamp}"""
+        )
+        return (self.session_start_timestamp, self.session_id)
 
     async def run_once(self):
         """
         Sends a heartbeat to the sens-o-matic
         """
         from prefect.orion.api.server import ORION_API_VERSION
+
+        if not hasattr(self, "session_id"):
+            await self._fetch_or_set_telemetry_session()
 
         async with httpx.AsyncClient() as client:
             result = await client.post(
