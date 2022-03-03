80a7d47bf304447097691fe40c00ed402ab2ae4b
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-03-03 23:05:12 +0100

    Implement init

--- src/prefect/tasks/transform/transform_tasks.py
+++ src/prefect/tasks/transform/transform_tasks.py
@@ -9,14 +9,60 @@ class TrasformCreateMaterialization(Task):
     TODO
     """
 
-    def __init__(self):
+    def __init__(
+        self,
+        api_key: str = None,
+        api_key_env_var: str = None,
+        mql_server_url: str = None,
+        mql_server_url_env_var: str = None,
+        model_key_id: int = None,
+        materialization_name: str = None,
+        start_date: str = None,
+        end_date: str = None,
+        output_table: str = None,
+        force: bool = False,
+        **kwargs
+    ):
         """
         TODO
         """
-        pass
+        self.api_key = api_key
+        self.api_key_env_var = api_key_env_var
+        self.mql_server_url = mql_server_url
+        self.mql_server_url_env_var = mql_server_url_env_var
+        self.model_key_id = model_key_id
+        self.materialization_name = materialization_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.output_table = output_table
+        self.force = force
+        super().__init__(**kwargs)
 
-    @defaults_from_attrs()
-    def run():
+    @defaults_from_attrs(
+        "api_key",
+        "api_key_env_var",
+        "mql_server_url",
+        "mql_server_url_env_var",
+        "model_key_id",
+        "materialization_name",
+        "start_date",
+        "end_date",
+        "output_table",
+        "force"
+    )
+    def run(
+        self,
+        api_key: str = None,
+        api_key_env_var: str = None,
+        mql_server_url: str = None,
+        mql_server_url_env_var: str = None,
+        model_key_id: int = None,
+        materialization_name: str = None,
+        start_date: str = None,
+        end_date: str = None,
+        output_table: str = None,
+        force: bool = False,
+    ):
         """
         TODO
         """
