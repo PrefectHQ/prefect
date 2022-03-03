57607413168ff7d600a730f972f82208912b213f
Craig Harshbarger <pleek91@gmail.com>
2022-03-03 17:23:21 -0600

    Flow run page with loading and empty states.

--- orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
+++ orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
@@ -5,34 +5,16 @@
         <FlowsPageFlowListItem :flow="flow" class="flows-list__item" />
       </template>
     </m-card>
-    <div v-if="flows.length === 0" class="text-center my-8">
-      <h2>No results found</h2>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed, inject } from 'vue'
   import FlowsPageFlowListItem from '@/components/FlowsPageFlowListItem.vue'
-  import { UnionFilters } from '@/services/Filter'
-  import { flowsApi, getFlowsKey } from '@/services/FlowsApi'
+  import { Flow } from '@/models/Flow'
 
-  const props = defineProps<{
-    filter: UnionFilters,
-    interval?: number,
+  defineProps<{
+    flows: Flow[],
   }>()
-
-  const filter = computed(() => props.filter)
-  const getFlows = inject(getFlowsKey, flowsApi.getFlows)
-
-  const flowsSubscription = useSubscription(getFlows, [filter], {
-    interval: props.interval,
-  })
-
-  console.log({ flowsSubscription })
-
-  const flows = computed(() => flowsSubscription.response.value ?? [])
 </script>
 
 <style lang="scss">
