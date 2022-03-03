242738c299b368b16078a9f88d0c24b3b626f5c2
Michael Milton <michael.r.milton@gmail.com>
2022-03-03 20:06:35 +1100

    Add mypy type annotations

--- src/prefect/core/task.py
+++ src/prefect/core/task.py
@@ -907,7 +907,7 @@ class Task(metaclass=TaskMetaclass):
             return_annotation = Any
         return return_annotation
 
-    def pipe(self, task: "Task", *args, **kwargs) -> "Task":
+    def pipe(self, task: "Task", *args: Any, **kwargs: Any) -> "Task":
         """
         "Pipes" the result of this task through another task. `some_task().pipe(other_task)` is equivalent to
         `other_task(some_task())`, but can result in more readable code when used in a long chain of task calls.
