cc692e804c7cd802d50a36a79a3cc678aeed4155
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2022-03-04 00:03:20 +0100

    First implementation

--- src/prefect/tasks/transform/transform_tasks.py
+++ src/prefect/tasks/transform/transform_tasks.py
@@ -1,13 +1,19 @@
-from transform import MQLClient
 import os
+from transform import MQLClient
 
 from prefect import Task
 from prefect.utilities.tasks import defaults_from_attrs
+from prefect.engine.signals import FAIL
 
 
 class TrasformCreateMaterialization(Task):
     """
-    TODO
+    Task to create a materialization against a Transform metrics layer
+    deployment.
+    Please refer to [Transform official documentation](https://docs.transform.co/)
+    for more information.
+    This task uses [Transform official MQL Client](https://pypi.org/project/transform/)
+    under the hood.
 
     Args:
         - api_key (str, optional): Transform API Key to be used to
@@ -23,8 +29,8 @@ class TrasformCreateMaterialization(Task):
             against which the transformation will be created.
         - materialization_name (str, optional): The name of the Transform materialization
             to create.
-        - start_date (str, optional): The ISO start date of the materialization.
-        - end_date (str, optional): The ISO end date of the materialization.
+        - start_time (str, optional): The UTC start time of the materialization.
+        - end_time (str, optional): The UTC end time of the materialization.
         - output_table (str, optional): The name of the database table, in the form of
             `schema_name.table_name` where the materialization will be created.
         - force (bool, optional): Whether to force the materialization creation
@@ -41,8 +47,8 @@ class TrasformCreateMaterialization(Task):
         mql_server_url_env_var: str = None,
         model_key_id: int = None,
         materialization_name: str = None,
-        start_date: str = None,
-        end_date: str = None,
+        start_time: str = None,
+        end_time: str = None,
         output_table: str = None,
         force: bool = False,
         run_async: bool = True,
@@ -54,8 +60,8 @@ class TrasformCreateMaterialization(Task):
         self.mql_server_url_env_var = mql_server_url_env_var
         self.model_key_id = model_key_id
         self.materialization_name = materialization_name
-        self.start_date = start_date
-        self.end_date = end_date
+        self.start_time = start_time
+        self.end_time = end_time
         self.output_table = output_table
         self.force = force
         self.run_async = run_async
@@ -68,8 +74,8 @@ class TrasformCreateMaterialization(Task):
         "mql_server_url_env_var",
         "model_key_id",
         "materialization_name",
-        "start_date",
-        "end_date",
+        "start_time",
+        "end_time",
         "output_table",
         "force",
         "run_async",
@@ -82,14 +88,17 @@ class TrasformCreateMaterialization(Task):
         mql_server_url_env_var: str = None,
         model_key_id: int = None,
         materialization_name: str = None,
-        start_date: str = None,
-        end_date: str = None,
+        start_time: str = None,
+        end_time: str = None,
         output_table: str = None,
         force: bool = False,
         run_async: bool = True,
     ):
         """
-        TODO
+        Task run method to create a materialization against a Transform metrics
+        layer deployment.
+        All parameters can be provided either during task initialization or directly
+        in this `run` method.
 
         Args:
             - api_key (str, optional): Transform API Key to be used to
@@ -105,8 +114,8 @@ class TrasformCreateMaterialization(Task):
                 against which the transformation will be created.
             - materialization_name (str, optional): The name of the Transform materialization
                 to create.
-            - start_date (str, optional): The ISO start date of the materialization.
-            - end_date (str, optional): The ISO end date of the materialization.
+            - start_time (str, optional): The UTC start time of the materialization.
+            - end_time (str, optional): The ISO end time of the materialization.
             - output_table (str, optional): The name of the database table, in the form of
                 `schema_name.table_name` where the materialization will be created.
             - force (bool, optional): Whether to force the materialization creation
@@ -115,7 +124,9 @@ class TrasformCreateMaterialization(Task):
                 in async mode or not.
 
         Raises:
-            - TODO
+            - `ValueError` if both `api_key` and `api_key_env_var` are missing.
+            - `ValueError` if both `mql_server_url` and `mql_server_url_env_var` ar missing.
+            - `ValueError` if `materialization_name` is missing.
 
         Returns:
             - TODO
@@ -139,3 +150,28 @@ class TrasformCreateMaterialization(Task):
             raise ValueError()
         
         mql_url = mql_server_url or os.environ[mql_server_url_env_var]
+
+        if not materialization_name:
+            raise ValueError()
+        
+        mql = MQLClient(api_key=mql_api_key, mql_server_url=mql_url)
+
+        if run_async:
+            response = mql.create_materialization(
+                materialization_name=materialization_name,
+                start_time=start_time,
+                end_time=end_time,
+                model_key_id=model_key_id,
+                output_table=output_table,
+                force=force    
+            )
+            return response
+        else:
+            return mql.materialize(
+                materialization_name=materialization_name,
+                start_time=start_time,
+                end_time=end_time,
+                model_key_id=model_key_id,
+                output_table=output_table,
+                force=force
+        )
