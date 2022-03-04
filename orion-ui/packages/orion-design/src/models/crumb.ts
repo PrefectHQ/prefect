dae4af6b9346ed602dc1f15aba3eadc593567327
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-04 14:00:52 -0600

    delete lowercase crumb file

--- orion-ui/packages/orion-design/src/models/crumb.ts
+++
@@ -1,6 +0,0 @@
-export type Crumb = {
-  text: string,
-  // to?: used to expect string | undefined, but nebula routes return AppRouteLocation
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  to?: any,
-}
