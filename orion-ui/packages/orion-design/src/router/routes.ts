6af94e24e01aed01c97c15f0abe978d250232b17
Craig Harshbarger <pleek91@gmail.com>
2022-03-04 12:12:25 -0600

    Don't accept strings for the route. Fix relative imports. Remove unnecessary styles

--- orion-ui/packages/orion-design/src/router/routes.ts
+++ orion-ui/packages/orion-design/src/router/routes.ts
@@ -1,4 +1,4 @@
 import { InjectionKey } from 'vue'
 import { RouteLocationRaw } from 'vue-router'
 
-export const workspaceDashboardKey: InjectionKey<RouteLocationRaw> = Symbol()
+export const workspaceDashboardKey: InjectionKey<Exclude<RouteLocationRaw, string>> = Symbol()
