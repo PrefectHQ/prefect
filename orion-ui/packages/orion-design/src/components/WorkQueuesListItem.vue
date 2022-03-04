d851095d3159576eb2c74c0aa69cf5e3242b37fc
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-04 11:33:16 -0600

    moved work-queues-list and item from nebula

---
+++ orion-ui/packages/orion-design/src/components/WorkQueuesListItem.vue
@@ -0,0 +1,73 @@
+<template>
+  <section class="work-queues-list-item">
+    <div class="work-queues-list-item__title">
+      <BreadCrumbs :crumbs="crumbs" />
+    </div>
+
+    <div class="work-queues-list-item__status">
+      <m-tag class="work-queues-list-item__paused" :color="workQueue.isPaused ? 'grey-40' : 'primary'">
+        <span>{{ workQueue.isPaused ? 'paused' : 'active' }}</span>
+      </m-tag>
+
+      <m-tags :tags="workQueue.filter?.tags ?? []" />
+    </div>
+
+    <div class="work-queues-list-item__concurrency">
+      <DetailsKeyValue
+        label="Concurrency"
+        :value="workQueue.concurrencyLimit ? workQueue.concurrencyLimit.toLocaleString() : 'No Limit'"
+      />
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+  import BreadCrumbs from '@/components/BreadCrumbs.vue'
+  import DetailsKeyValue from '@/components/DetailsKeyValue.vue'
+  import { WorkQueue } from '@/models/WorkQueue'
+
+  const props = defineProps<{ workQueue: WorkQueue }>()
+
+  const crumbs = [{ text: props.workQueue.name, to: '#' }]
+</script>
+
+<style lang="scss">
+.work-queues-list-item {
+  display: grid;
+  grid-template-areas:
+    'title filters concurrency'
+    'status filters concurrency';
+  grid-template-columns: 1fr min-content min-content;
+  column-gap: var(--m-1);
+  row-gap: 2px;
+  padding: var(--p-1) var(--p-2);
+}
+
+.work-queues-list-item__title{
+  grid-area: title;
+  margin-top: calc(var(--m-1) * -1);
+}
+
+.work-queues-list-item__filters{
+  grid-area: filters;
+  display: flex;
+  align-items: center;
+}
+
+.work-queues-list-item__concurrency{
+  grid-area: concurrency;
+  display: flex;
+  align-items: center;
+}
+
+.work-queues-list-item__status{
+  grid-area: status;
+  display: flex;
+  gap: 2px;
+}
+
+.work-queues-list-item__paused > span {
+  padding: 0 var(--p-1) !important;
+  font-size: 13px;
+}
+</style>
