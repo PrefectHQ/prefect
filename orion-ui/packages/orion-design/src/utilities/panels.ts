83612015fa65bab7e946c0284ad6383695163692
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-07 16:40:51 -0600

    InjectionKeys for everyone

--- orion-ui/packages/orion-design/src/utilities/panels.ts
+++ orion-ui/packages/orion-design/src/utilities/panels.ts
@@ -2,7 +2,13 @@ import { Component, InjectionKey } from 'vue'
 
 // any because its a generic constructor
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type InstanceOfComponent = Component & { new (...args: any): any }
-type showPanel = <T extends InstanceOfComponent>(component: T, props: InstanceType<T>['$props']) => void
+export type InstanceOfComponent = Component & { new (...args: any): any }
+export type ShowPanel = <T extends InstanceOfComponent>(component: T, props: InstanceType<T>['$props']) => void
+export type ClosePanel = () => void
+export type ExitPanel = () => void
 
-export const showPanelKey: InjectionKey<showPanel> = Symbol()
+export const showPanelKey: InjectionKey<ShowPanel> = Symbol()
+export const closePanelKey: InjectionKey<ClosePanel> = Symbol()
+export const exitPanelKey: InjectionKey<ExitPanel> = Symbol()
+
+export const refreshWorkQueuesListKey: InjectionKey<() => Promise<void>> = Symbol()
