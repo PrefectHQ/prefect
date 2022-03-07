b58f9e9e4fce4636eb8195af285fa3be88fac7f3
Craig Harshbarger <pleek91@gmail.com>
2022-03-07 16:44:55 -0600

    Deployment panel

--- orion-ui/packages/orion-design/src/utilities/panels.ts
+++ orion-ui/packages/orion-design/src/utilities/panels.ts
@@ -3,6 +3,6 @@ import { Component, InjectionKey } from 'vue'
 // any because its a generic constructor
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type InstanceOfComponent = Component & { new (...args: any): any }
-type showPanel = <T extends InstanceOfComponent>(component: T, props: InstanceType<T>['$props']) => void
+export type ShowPanel = <T extends InstanceOfComponent>(component: T, props: InstanceType<T>['$props']) => void
 
-export const showPanelKey: InjectionKey<showPanel> = Symbol()
+export const showPanelKey: InjectionKey<ShowPanel> = Symbol()
