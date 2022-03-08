2e06fec4f9e6262367b3b3e97c0c8b699700a422
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-08 14:41:06 -0600

    going back to CreatePanel removed api import from formValues class

--- orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
+++ orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
@@ -5,18 +5,14 @@
 </template>
 
 <script lang="ts" setup>
-  import Subscription from '@prefecthq/vue-compositions/src/subscribe/subscription'
-  import WorkQueuePanel from '@/components/WorkQueuePanel.vue'
+  import WorkQueueCreatePanel from '@/components/WorkQueueCreatePanel.vue'
   import { useInjectedServices } from '@/compositions/useInjectedServices'
   import { showPanel } from '@/utilities/panels'
 
   const injectedServices = useInjectedServices()
 
   function openCreateWorkQueuePanel(): void {
-    showPanel(WorkQueuePanel, {
-      workQueueId: '',
-      workQueueSubscription: {} as Subscription<any>,
-      openWorkQueueEditPanel: () => null,
+    showPanel(WorkQueueCreatePanel, {
       ...injectedServices,
     })
   }
