2fd913f145446bd84369d47361dc6f3a86b5a613
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-08 14:24:14 -0600

    importing WorkQueueCreatePanel

--- orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
+++ orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-  // import WorkQueueCreatePanel from '@/components/WorkQueueCreatePanel.vue'
+  import WorkQueueCreatePanel from '@/components/WorkQueueCreatePanel.vue'
   import { useInjectedServices } from '@/compositions/useInjectedServices'
   import { showPanel } from '@/utilities/panels'
 
