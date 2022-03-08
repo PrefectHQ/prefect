70f6352f62068d43285b33758c3523b205f29602
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-08 14:35:43 -0600

    trying WorkQueuePanel

--- orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
+++ orion-ui/packages/orion-design/src/components/WorkQueueCreateButton.vue
@@ -6,17 +6,17 @@
 
 <script lang="ts" setup>
   import Subscription from '@prefecthq/vue-compositions/src/subscribe/subscription'
-  import WorkQueueEditPanel from '@/components/WorkQueueEditPanel.vue'
+  import WorkQueuePanel from '@/components/WorkQueuePanel.vue'
   import { useInjectedServices } from '@/compositions/useInjectedServices'
-  import { WorkQueue } from '@/models/WorkQueue'
   import { showPanel } from '@/utilities/panels'
 
   const injectedServices = useInjectedServices()
 
   function openCreateWorkQueuePanel(): void {
-    showPanel(WorkQueueEditPanel, {
-      workQueue: {} as WorkQueue,
+    showPanel(WorkQueuePanel, {
+      workQueueId: '',
       workQueueSubscription: {} as Subscription<any>,
+      openWorkQueueEditPanel: () => null,
       ...injectedServices,
     })
   }
