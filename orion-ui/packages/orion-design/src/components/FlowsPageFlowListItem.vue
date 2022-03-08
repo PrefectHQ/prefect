7df92eaf1651d2318d2672f1260ca4a9410b77d2
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-08 10:06:25 -0600

    updated flows page injection to use composition to manage injected services

--- orion-ui/packages/orion-design/src/components/FlowsPageFlowListItem.vue
+++ orion-ui/packages/orion-design/src/components/FlowsPageFlowListItem.vue
@@ -25,23 +25,20 @@
   import FilterCountButton from '@/components/FilterCountButton.vue'
   import FlowPanel from '@/components/FlowPanel.vue'
   import ListItem from '@/components/ListItem.vue'
+  import { useInjectedServices } from '@/compositions/useInjectedServices'
   import { Deployment } from '@/models/Deployment'
   import { Flow } from '@/models/Flow'
   import { workspaceDashboardKey } from '@/router/routes'
-  import { createDeploymentFlowRunKey, deploymentsApi, getDeploymentsCountKey, getDeploymentsKey } from '@/services/DeploymentsApi'
   import { UnionFilters } from '@/services/Filter'
-  import { flowRunsApi, getFlowRunsCountKey } from '@/services/FlowRunsApi'
   import { Filter } from '@/types/filters'
-  import { showPanelKey } from '@/utilities/panels'
   import { toPluralString } from '@/utilities/strings'
 
   const props = defineProps<{ flow: Flow }>()
 
-  const crumbs = [{ text: props.flow.name, to: '#' }]
-
   const route = inject(workspaceDashboardKey)!
-  const showPanel = inject(showPanelKey)!
+  const injectedServices = useInjectedServices()
 
+  const crumbs = [{ text: props.flow.name, to: '#' }]
   const recentFlowRunsFilters = computed<Required<Filter>[]>(() => [
     {
       object: 'flow',
@@ -76,34 +73,26 @@
     },
   }))
 
-  const getFlowRunsCount = inject(getFlowRunsCountKey, flowRunsApi.getFlowRunsCount)
-  const recentFlowRunsCountSubscription = useSubscription(getFlowRunsCount, [recentFlowRunsCountFilter])
+  const recentFlowRunsCountSubscription = useSubscription(injectedServices.getFlowRunsCount, [recentFlowRunsCountFilter])
   const recentFlowRunsCount = computed(() => recentFlowRunsCountSubscription.response.value ?? 0)
 
-  const getDeploymentsCount = inject(getDeploymentsCountKey, deploymentsApi.getDeploymentsCount)
-  const deploymentsCountSubscription = useSubscription(getDeploymentsCount, [countFilter])
+  const deploymentsCountSubscription = useSubscription(injectedServices.getDeploymentsCount, [countFilter])
   const deploymentsCount = computed(() => deploymentsCountSubscription.response.value ?? 0)
 
-  const getDeployments = inject(getDeploymentsKey, deploymentsApi.getDeployments)
-  const createDeploymentFlowRun = inject(createDeploymentFlowRunKey, deploymentsApi.createDeploymentFlowRun)
-
   function openFlowPanel(): void {
-    showPanel(FlowPanel, {
+    injectedServices.useShowPanel(FlowPanel, {
       flow: props.flow,
       dashboardRoute: route,
-      getDeployments,
-      getDeploymentsCount,
-      createDeploymentFlowRun,
-      getFlowRunsCount,
       openDeploymentPanel,
+      ...injectedServices,
     })
   }
 
   function openDeploymentPanel(deployment: Deployment): void {
-    showPanel(DeploymentPanel, {
+    injectedServices.useShowPanel(DeploymentPanel, {
       deployment,
-      getFlowRunsCount,
       dashboardRoute: route,
+      ...injectedServices,
     })
   }
 </script>
