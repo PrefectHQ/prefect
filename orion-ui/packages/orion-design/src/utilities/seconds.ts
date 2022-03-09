9565f9263625de7cc7cb75e13c176df0a091efdd
Craig Harshbarger <pleek91@gmail.com>
2022-03-09 16:51:00 -0600

    Fix secondsToApproximateString to use single char units like it did before

--- orion-ui/packages/orion-design/src/utilities/seconds.ts
+++ orion-ui/packages/orion-design/src/utilities/seconds.ts
@@ -17,32 +17,33 @@ export const intervals: Record<string, number> = {
   second: _s,
 }
 
-const aggregateSeconds = (s: number): Record<string, number> => {
-  const years = Math.floor(s / _y)
-  const days = Math.floor(s % _y / _d)
-  const hours = Math.floor(s % _y % _d / _h)
-  const minutes = Math.floor(s % _y % _d % _h / _m)
-  const seconds = Math.ceil(s % _y % _d % _h % _m)
+function aggregateSeconds(input: number): Record<string, number> {
+  const years = Math.floor(input / _y)
+  const days = Math.floor(input % _y / _d)
+  const hours = Math.floor(input % _y % _d / _h)
+  const minutes = Math.floor(input % _y % _d % _h / _m)
+  const seconds = Math.ceil(input % _y % _d % _h % _m)
 
   return { years, days, hours, minutes, seconds }
 }
 
-const intervalString = (
+// eslint-disable-next-line max-params
+function intervalString(
   type: string,
   seconds: number,
   showOnes: boolean = true,
   showSpaces: boolean = true,
-): string => {
+): string {
   return (
     `${seconds === 1 && !showOnes ? '' : seconds
     }${showSpaces ? ' ' : ''}${type}${seconds !== 1 && type.length > 1 ? 's' : ''}`
   )
 }
 
-export const secondsToString = (
+export function secondsToString(
   input: number,
   showOnes: boolean = true,
-): string => {
+): string {
   const { years, days, hours, minutes, seconds } = aggregateSeconds(input)
   const _y = intervalString('year', years, showOnes)
   const _d = intervalString('day', days, showOnes)
@@ -59,16 +60,16 @@ export const secondsToString = (
   )
 }
 
-export const secondsToApproximateString = (
+export function secondsToApproximateString(
   input: number,
   showOnes: boolean = true,
-): string => {
+): string {
   const { years, days, hours, minutes, seconds } = aggregateSeconds(input)
-  const _y = intervalString('year', years, showOnes)
-  const _d = intervalString('day', days, showOnes)
-  const _h = intervalString('hour', hours, showOnes)
-  const _m = intervalString('minute', minutes, showOnes)
-  const _s = intervalString('second', seconds, showOnes)
+  const _y = intervalString('y', years, showOnes)
+  const _d = intervalString('d', days, showOnes)
+  const _h = intervalString('h', hours, showOnes)
+  const _m = intervalString('m', minutes, showOnes)
+  const _s = intervalString('s', seconds, showOnes)
 
   if (years > 0 && days == 0) {
     return _y
