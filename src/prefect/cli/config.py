c4e738abd8b40b3cb5ee333a8ed1ec38c46a13f1
Chris White <chris@prefect.io>
2022-03-09 13:22:07 -0800

    Additional polish

--- src/prefect/cli/config.py
+++ src/prefect/cli/config.py
@@ -24,36 +24,6 @@ app.add_typer(config_app)
 
 
 @config_app.command()
-def get_profile(names: List[str] = typer.Argument(None)):
-    """
-    Show settings in one or many profiles. Defaults to the current profile.
-    """
-    profiles = prefect.settings.load_profiles()
-    if not names:
-        profile = prefect.context.get_profile_context()
-        names = [profile.name]
-
-    display_profiles = {
-        name: values for name, values in profiles.items() if name in names
-    }
-    console.out(toml.dumps(display_profiles).strip())
-
-
-@config_app.command()
-def list_profiles():
-    """
-    List profile names.
-    """
-    profiles = prefect.settings.load_profiles()
-    current = prefect.context.get_profile_context().name
-    for name in profiles:
-        if name == current:
-            console.print(f"* {name}")
-        else:
-            console.print(name)
-
-
-@config_app.command()
 def set(variables: List[str]):
     """
     Change the value for a setting.
@@ -111,94 +81,6 @@ def unset(variables: List[str]):
 
     prefect.settings.write_profiles(profiles)
     exit_with_success(f"Updated profile {profile.name!r}")
-
-
-@config_app.command()
-def create_profile(
-    name: str,
-    from_name: str = typer.Option(None, "--from", help="Copy an existing profile."),
-):
-    """
-    Create a new profile.
-    """
-
-    profiles = prefect.settings.load_profiles()
-    if name in profiles:
-        console.print(
-            textwrap.dedent(
-                f"""
-                [red]Profile {name!r} already exists.[/red]
-                To create a new profile, remove the existing profile first:
-
-                    prefect config rm-profile {name!r}
-                """
-            ).strip()
-        )
-        raise typer.Exit(1)
-
-    if from_name:
-        if from_name not in profiles:
-            exit_with_error("Profile {from_name!r} not found.")
-
-        profiles[name] = profiles[from_name]
-        from_blurb = f" matching {from_name!r}"
-    else:
-        from_blurb = ""
-        profiles[name] = {}
-
-    prefect.settings.write_profiles(profiles)
-    console.print(
-        textwrap.dedent(
-            f"""
-            [green]Created profile {name!r}{from_blurb}.[/green] 
-            To use your profile, set an environment variable:
-
-                export PREFECT_PROFILE={name!r}
-
-            or include the profile in your CLI commands:
-
-                prefect -p {name!r} config view
-            """
-        ).strip()
-    )
-
-
-@config_app.command()
-def rm_profile(name: str):
-    """
-    Remove the given profile.
-    """
-    profiles = prefect.settings.load_profiles()
-    if name not in profiles:
-        exit_with_error(f"Profle {name!r} not found.")
-
-    profiles.pop(name)
-
-    verb = "Removed"
-    if name == "default":
-        verb = "Reset"
-        profiles["default"] = {}
-
-    prefect.settings.write_profiles(profiles)
-    exit_with_success(f"{verb} profile {name!r}.")
-
-
-@config_app.command()
-def rename_profile(name: str, new_name: str):
-    """
-    Change the name of a profile.
-    """
-    profiles = prefect.settings.load_profiles()
-    if name not in profiles:
-        exit_with_error(f"Profle {name!r} not found.")
-
-    if new_name in profiles:
-        exit_with_error(f"Profile {new_name!r} already exists.")
-
-    profiles[new_name] = profiles.pop(name)
-
-    prefect.settings.write_profiles(profiles)
-    exit_with_success(f"Renamed profile {name!r} to {new_name!r}.")
 
 
 @config_app.command()
