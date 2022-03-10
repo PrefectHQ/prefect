5adeba14d673ebd722d390ed2f9fb0049f629398
Michael Milton <michael.r.milton@gmail.com>
2022-03-10 11:50:28 +1100

    Flaking

--- src/prefect/core/task.py
+++ src/prefect/core/task.py
@@ -910,8 +910,8 @@ class Task(metaclass=TaskMetaclass):
     def pipe(_prefect_self, _prefect_task: "Task", **kwargs: Any) -> "Task":
         """
         "Pipes" the result of this task through another task. ``some_task().pipe(other_task)`` is
-        equivalent to ``other_task(some_task())``, but can result in more readable code when used in a long
-        chain of task calls.
+        equivalent to ``other_task(some_task())``, but can result in more readable code when used in a
+        long chain of task calls.
 
         Args:
             - _prefect_task: The task to execute after this task.
