a137dd610ad8e4ce2ce1de0f8d84b01e1b962305
Zach Angell <zachary.james.angell@gmail.com>
2022-03-10 11:17:15 -0500

    Fix index name

--- src/prefect/orion/database/orm_models.py
+++ src/prefect/orion/database/orm_models.py
@@ -99,7 +99,7 @@ class ORMFlow:
 
     @declared_attr
     def __table_args__(cls):
-        return (sa.UniqueConstraint("name"), sa.Index("created"))
+        return (sa.UniqueConstraint("name"), sa.Index("ix_flow__created", "created"))
 
 
 @declarative_mixin
