799b151a6cca6371a68e59018790d65173b351b9
Dustin Ngo <dustin.ngo@gmail.com>
2022-03-11 18:20:59 -0500

    Add retry maximum

--- src/prefect/utilities/httpx.py
+++ src/prefect/utilities/httpx.py
@@ -1,13 +1,16 @@
 import anyio
 import httpx
-import typing
 
 
 class PrefectHttpxClient(httpx.AsyncClient):
+    RETRY_MAX = 5
+
     async def send(self, *args, **kwargs):
         response = await super().send(*args, **kwargs)
 
-        if response.status_code == 429:
+        retry_count = 0
+        while response.status_code == 429 and retry_count < self.RETRY_MAX:
+            retry_count += 1
             # respect CloudFlare conventions for handling rate-limit response headers
             # see https://support.cloudflare.com/hc/en-us/articles/115001635128-Configuring-Rate-Limiting-from-UI
             retry_seconds = int(response.headers["Retry-After"])
