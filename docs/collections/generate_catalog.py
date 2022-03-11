751ac466df674d27845718d2968506e6d20f2179
Alexander Streed <alex.s@prefect.io>
2022-03-11 09:03:26 -0600

    Cleans up generation script

--- docs/collections/generate_catalog.py
+++ docs/collections/generate_catalog.py
@@ -6,28 +6,25 @@ import mkdocs_gen_files
 import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-ITEMS_PER_ROW = 5
-
 with mkdocs_gen_files.open("collections/catalog.md", "a") as markdown_file:
+    # Get file paths for all collections files
     collection_files = [
         file
         for file in glob.glob("./docs/collections/catalog/*.yaml")
+        # Ignore the template file
         if Path(file).name != "TEMPLATE.yaml"
     ]
 
+    # Load collection information from YAML files into a list
     collection_configs = []
     for collection_file in collection_files:
         with open(collection_file, "r") as file:
             collection_configs.append(yaml.safe_load(file))
 
+    # Sort collections alphabetically by name
     sorted_collection_configs = sorted(
         collection_configs, key=lambda x: x["collectionName"]
     )
-
-    chunked_collection_configs = [
-        sorted_collection_configs[i : i + ITEMS_PER_ROW]
-        for i in range(0, len(collection_configs), ITEMS_PER_ROW)
-    ]
 
     env = Environment(
         loader=FileSystemLoader("./docs/collections/templates/"),
@@ -35,4 +32,5 @@ with mkdocs_gen_files.open("collections/catalog.md", "a") as markdown_file:
     )
     template = env.get_template("table.html")
 
+    # Render jinja2 template and append to catalog.md
     print(template.render(collections=sorted_collection_configs), file=markdown_file)
