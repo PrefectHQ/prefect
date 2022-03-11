11cb2b03bc0c63298ec422d66becc47d58c484a6
Jennifer Grange <jenny@prefect.io>
2022-03-11 10:10:35 -0500

    add delete section needs refresh

--- orion-ui/packages/orion-design/src/utilities/subscriptions.ts
+++ orion-ui/packages/orion-design/src/utilities/subscriptions.ts
@@ -1,9 +1,12 @@
 import Subscription from '@prefecthq/vue-compositions/src/subscribe/subscription'
 import { InjectionKey } from 'vue'
 import { WorkQueuesApi } from '@/services/WorkQueuesApi'
+import { FlowsApi } from '..'
 
 export type WorkQueueSubscription = Subscription<WorkQueuesApi['getWorkQueue']>
 export type WorkQueuesListSubscription = Subscription<WorkQueuesApi['getWorkQueues']>
+export type FlowsListSubscription = Subscription<FlowsApi['getFlows']>
 
 export const workQueueSubscriptionKey: InjectionKey<WorkQueueSubscription> = Symbol()
 export const workQueuesListSubscriptionKey: InjectionKey<WorkQueuesListSubscription> = Symbol()
+export const flowsListSubscriptionKey: InjectionKey<FlowsListSubscription>=Symbol()
