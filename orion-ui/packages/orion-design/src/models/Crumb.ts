0a7c49eaf30199420e083e3ffabee721b02cd485
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-15 13:20:05 -0500

    rebuilt BreadCrumbs components

--- orion-ui/packages/orion-design/src/models/Crumb.ts
+++ orion-ui/packages/orion-design/src/models/Crumb.ts
@@ -1,6 +1,8 @@
+import { RouteLocationRaw } from 'vue-router'
+
 export type Crumb = {
   text: string,
-  // to?: used to expect string | undefined, but nebula routes return AppRouteLocation
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  to?: any,
+  loading?: boolean,
+  clickable?: boolean,
+  to?: RouteLocationRaw,
 }
