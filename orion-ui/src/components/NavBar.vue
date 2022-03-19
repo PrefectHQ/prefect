582525e8d30b5b6a60a05d6b2d1a4324148924a3
Natalie Smith <thatgalnatalie@gmail.com>
2022-03-18 17:32:16 -0700

    add nav bar styling

--- orion-ui/src/components/NavBar.vue
+++ orion-ui/src/components/NavBar.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="navbar" tabindex="-1">
-    <router-link to="/" class="nav-item">
-      <img class="logo" src="@/assets/logos/prefect-logo-mark-gradient.svg">
+  <div class="nav-bar" tabindex="-1">
+    <router-link to="/" class="nav-bar__item">
+      <img class="nav-bar__logo" src="@/assets/logos/prefect-logo-mark-gradient.svg" />
     </router-link>
 
-    <router-link to="/flows" class="nav-item">
-      <i class="pi pi-flow pi-2x" />
+    <router-link to="/flows" class="nav-bar__item" :class="{ 'nav-bar__item--active': true }">
+      <i class="nav-bar__icon pi pi-flow" />
     </router-link>
 
-    <router-link to="/work-queues" class="nav-item">
-      <i class="pi pi-robot-line pi-2x" />
+    <router-link
+      to="/work-queues"
+      class="nav-bar__item"
+      :class="{ 'nav-bar__item--active': false }"
+    >
+      <i class="nav-bar__icon pi pi-robot-line" />
     </router-link>
 
-    <router-link to="/settings" class="nav-item mt-auto ml-auto">
-      <i class="pi pi-settings-3-line pi-2x" />
+    <router-link to="/settings" class="nav-bar__item mt-auto ml-auto">
+      <i class="nav-bar__icon pi pi-settings-3-line" />
     </router-link>
   </div>
 </template>
 
 <script lang="ts">
-  import { Vue } from 'vue-class-component'
+import { Vue } from 'vue-class-component'
 
-  export default class NavBar extends Vue {}
+export default class NavBar extends Vue { }
 </script>
 
 <style lang="scss" scoped>
-.logo {
-  width: 24px;
-}
+.nav-bar {
+  --width: 62px;
 
-.navbar {
-  background-color: #fff;
+  background-color: var(--white);
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
   display: flex;
   flex-direction: column;
@@ -38,33 +40,77 @@
   padding: 8px 0;
   position: fixed;
   top: 0;
-  width: 62px;
+  width: var(--width);
   z-index: var(--layer-nav);
+}
 
-  .nav-item {
-    // color: $red !important;
-    text-decoration: none;
+.nav-bar__logo {
+  width: 24px;
+}
 
-    height: 62px;
-    width: 62px;
+.nav-bar__item {
+  text-decoration: none;
+  height: var(--width);
+  width: var(--width);
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  transition: width 150ms ease-in-out 150ms;
+}
 
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
+.nav-bar__item:not(:last-child) {
+  border-bottom: 1px solid var(--secondary-hover);
+}
 
-    transition: width 150ms ease-in-out 150ms;
+.nav-bar__item--active {
+  background: var(--primary);
+
+  .nav-bar__icon {
+    color: var(--white);
   }
 }
 
+// .logo {
+//   width: 24px;
+// }
+
+// .navbar {
+//   background-color: #fff;
+//   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
+//   display: flex;
+//   flex-direction: column;
+//   height: 100vh;
+//   padding: 8px 0;
+//   position: fixed;
+//   top: 0;
+//   width: 62px;
+//   z-index: var(--layer-nav);
+
+//   .nav-item {
+//     // color: $red !important;
+//     text-decoration: none;
+
+//     height: 62px;
+//     width: 62px;
+
+//     display: inline-flex;
+//     align-items: center;
+//     justify-content: center;
+
+//     transition: width 150ms ease-in-out 150ms;
+//   }
+// }
+
 @media (max-width: 640px) {
-  .navbar {
+  .nav-bar {
     padding: 0 8px;
     height: 62px;
     flex-direction: row;
     max-width: unset;
     width: 100vw;
 
-    .nav-item {
+    .nav-bar__item {
       transition: width 150ms ease-in-out 150ms;
     }
   }
