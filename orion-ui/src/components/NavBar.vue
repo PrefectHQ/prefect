d384b636a2a779b7ae0d528b019d9cd5fbedca27
Natalie Smith <thatgalnatalie@gmail.com>
2022-03-21 12:00:46 -0700

    rm comments

--- orion-ui/src/components/NavBar.vue
+++ orion-ui/src/components/NavBar.vue
@@ -74,37 +74,6 @@ const route = useRoute()
   }
 }
 
-// .logo {
-//   width: 24px;
-// }
-
-// .navbar {
-//   background-color: #fff;
-//   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.06), 0px 1px 3px rgba(0, 0, 0, 0.1);
-//   display: flex;
-//   flex-direction: column;
-//   height: 100vh;
-//   padding: 8px 0;
-//   position: fixed;
-//   top: 0;
-//   width: 62px;
-//   z-index: var(--layer-nav);
-
-//   .nav-item {
-//     // color: $red !important;
-//     text-decoration: none;
-
-//     height: 62px;
-//     width: 62px;
-
-//     display: inline-flex;
-//     align-items: center;
-//     justify-content: center;
-
-//     transition: width 150ms ease-in-out 150ms;
-//   }
-// }
-
 @media (max-width: 640px) {
   .nav-bar {
     padding: 0 8px;
