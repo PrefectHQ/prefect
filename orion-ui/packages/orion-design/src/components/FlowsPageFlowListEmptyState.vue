24f2b64cb41fce09428de55f487acca6298b5e41
Marichka Offen <marichka@prefect.io>
2022-03-21 17:02:48 -0400

    create conditional to display docs button in FlowsPageFlowListEmptyState

--- orion-ui/packages/orion-design/src/components/FlowsPageFlowListEmptyState.vue
+++ orion-ui/packages/orion-design/src/components/FlowsPageFlowListEmptyState.vue
@@ -3,11 +3,13 @@
     header="Create a flow to get started"
     description="Flows are the most basic Prefect object. They are containers for workflow logic and allow users to interact with and reason about the state of their workflows."
   >
-    <a href="https://orion-docs.prefect.io/concepts/flows/" target="_blank">
-      <m-button color="primary" miter icon="pi-add-line">
-        View Docs
-      </m-button>
-    </a>
+    <template v-show="displayDocsButton">
+      <a href="https://orion-docs.prefect.io/concepts/flows/" target="_blank">
+        <m-button color="primary" miter icon="pi-add-line">
+          View Docs
+        </m-button>
+      </a>
+    </template>
     <template #example>
       <div class="flows-page-flow-list-empty-state__example">
         <template v-for="flow in flows" :key="flow.id">
@@ -25,6 +27,13 @@
   import { getDeploymentsCountKey } from '@/services/DeploymentsApi'
   import { getFlowRunsCountKey } from '@/services/FlowRunsApi'
   import { mocker } from '@/services/Mocker'
+
+  defineProps({
+      displayDocsButton: {
+        type: Boolean,
+        default: true
+    },
+  })
 
   provide(getFlowRunsCountKey, () => Promise.resolve(mocker.create('number', [0, 15])))
   provide(getDeploymentsCountKey, () => Promise.resolve(mocker.create('number', [0, 3])))
