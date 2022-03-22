0ed6a189b87f3c82ce79a4a42bbc747717e3932b
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-22 14:35:14 -0500

    merged types for Tab and RouterTab

--- orion-ui/packages/orion-design/src/types/tabs.ts
+++ orion-ui/packages/orion-design/src/types/tabs.ts
@@ -5,17 +5,11 @@ export type Tab = {
   class?: string,
 }
 
-type Route = {
-  name: string,
-  hash?: string,
-}
-
-export type RouterTab = {
-  title: string,
-  key: string,
-  route: Route,
-  icon?: string,
-  class?: string,
+export type RouterTab = Tab & {
+  route: {
+    name: string,
+    hash?: string,
+  },
 }
 
 export function isRouterTab(input: Tab | RouterTab): input is RouterTab {
