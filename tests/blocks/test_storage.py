fd20921eeeb5363f12599e2ba37706daab1042c3
ahuang11 <andrew.h@prefect.io>
2022-03-23 11:52:46 -0600

    Cast to string

--- tests/blocks/test_storage.py
+++ tests/blocks/test_storage.py
@@ -190,4 +190,4 @@ class TestFileStorageBlock:
 def test_local_storage_block_home_basepath():
     local_storage_block = storage.LocalStorageBlock(storage_path="~/.prefect")
     basepath = local_storage_block.basepath()
-    assert basepath == os.path.expanduser("~/.prefect")
+    assert str(basepath) == os.path.expanduser("~/.prefect")
