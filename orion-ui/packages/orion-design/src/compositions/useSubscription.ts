37e1b9470c64ae6dfa779de5a1798d8f9c790ab8
Marichka Offen <marichka@prefect.io>
2022-03-24 10:34:41 -0400

    Remove useSubscription.ts file

--- orion-ui/packages/orion-design/src/compositions/useSubscription.ts
+++
@@ -1,9 +0,0 @@
-import { useSubscription as useSubscribeComposition } from '@prefecthq/vue-compositions'
-import Subscription from '@prefecthq/vue-compositions/src/subscribe/subscription'
-import { Action, SubscriptionOptions, SubscribeArguments } from '@prefecthq/vue-compositions/src/subscribe/types'
-
-export const defaultOptions: SubscriptionOptions = { interval: 30000 }
-
-export function useSubscription<T extends Action>(...[action, args, options, manager]: SubscribeArguments<T>): Subscription<T> {
-  return useSubscribeComposition(action, args, { ...defaultOptions, ...options }, manager)
-}
