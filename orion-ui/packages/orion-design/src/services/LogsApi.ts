867275dcc894a236804c42ea635e142ac488c7a4
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-24 08:01:27 -0500

    updated service layer to use translate service

--- orion-ui/packages/orion-design/src/services/LogsApi.ts
+++ orion-ui/packages/orion-design/src/services/LogsApi.ts
@@ -1,9 +1,8 @@
 import { createActions } from '@prefecthq/vue-compositions'
-import { AxiosResponse } from 'axios'
-import { Route } from '.'
 import { ILogResponse } from '@/models/ILogResponse'
 import { Log } from '@/models/Log'
-import { Api } from '@/services/Api'
+import { Api, Route } from '@/services/Api'
+import { translate } from '@/services/Translate'
 import { LogsRequestFilter } from '@/types/LogsRequestFilter'
 
 export class LogsApi extends Api {
@@ -11,25 +10,8 @@ export class LogsApi extends Api {
   protected route: Route = '/logs'
 
   public getLogs(filter?: LogsRequestFilter): Promise<Log[]> {
-    return this.post('/filter', filter).then(response => this.logsResponseMapper(response))
-  }
-
-  protected logMapper(log: ILogResponse): Log {
-    return new Log({
-      id: log.id,
-      created: new Date(log.created),
-      updated: new Date(log.updated),
-      name: log.name,
-      level: log.level,
-      message: log.message,
-      timestamp: new Date(log.timestamp),
-      flowRunId: log.flow_run_id,
-      taskRunId: log.task_run_id,
-    })
-  }
-
-  protected logsResponseMapper({ data }: AxiosResponse<ILogResponse[]>): Log[] {
-    return data.map(log => this.logMapper(log))
+    return this.post<ILogResponse[]>('/filter', filter)
+      .then(({ data }) => data.map(x => translate.toDestination('ILogResponse:Log', x)))
   }
 
 }
