6e5ff72bb4df28291c81ea9754f6928f1897f323
Evan Sutherland <stackoverfloweth@gmail.com>
2022-03-24 08:01:27 -0500

    updated service layer to use translate service

--- orion-ui/packages/orion-design/src/services/WorkQueuesApi.ts
+++ orion-ui/packages/orion-design/src/services/WorkQueuesApi.ts
@@ -1,28 +1,29 @@
 import { createActions } from '@prefecthq/vue-compositions'
-import { AxiosResponse } from 'axios'
 import { InjectionKey } from 'vue'
-import { PaginatedFilter } from '.'
-import { IWorkQueueFilterResponse } from '@/models/IWorkQueueFilterResponse'
 import { IWorkQueueRequest } from '@/models/IWorkQueueRequest'
 import { IWorkQueueResponse } from '@/models/IWorkQueueResponse'
 import { WorkQueue } from '@/models/WorkQueue'
-import { WorkQueueFilter } from '@/models/WorkQueueFilter'
 import { Api, Route } from '@/services/Api'
+import { PaginatedFilter } from '@/services/Filter'
+import { translate } from '@/services/Translate'
 
 export class WorkQueuesApi extends Api {
 
   protected route: Route = '/work_queues'
 
   public getWorkQueue(id: string): Promise<WorkQueue> {
-    return this.get<IWorkQueueResponse>(`/${id}`).then(response => this.mapWorkQueueResponse(response))
+    return this.get<IWorkQueueResponse>(`/${id}`)
+      .then(({ data }) => translate.toDestination('IWorkQueueResponse:WorkQueue', data))
   }
 
   public getWorkQueues(filter: PaginatedFilter): Promise<WorkQueue[]> {
-    return this.post<IWorkQueueResponse[]>('/filter', filter).then(response => this.mapWorkQueuesResponse(response))
+    return this.post<IWorkQueueResponse[]>('/filter', filter)
+      .then(({ data }) => data.map(x => translate.toDestination('IWorkQueueResponse:WorkQueue', x)))
   }
 
   public createWorkQueue(request: IWorkQueueRequest): Promise<WorkQueue> {
-    return this.post<IWorkQueueResponse>('/', request).then(response => this.mapWorkQueueResponse(response))
+    return this.post<IWorkQueueResponse>('/', request)
+      .then(({ data }) => translate.toDestination('IWorkQueueResponse:WorkQueue', data))
   }
 
   public pauseWorkQueue(id: string): Promise<void> {
@@ -39,35 +40,6 @@ export class WorkQueuesApi extends Api {
 
   public deleteWorkQueue(id: string): Promise<void> {
     return this.delete(`/${id}`)
-  }
-
-  protected mapWorkQueue(data: IWorkQueueResponse): WorkQueue {
-    return new WorkQueue({
-      id: data.id,
-      created: new Date(data.created),
-      updated: new Date(data.updated),
-      name: data.name,
-      filter: this.mapWorkQueueFilter(data.filter),
-      description: data.description,
-      isPaused: data.is_paused ?? false,
-      concurrencyLimit: data.concurrency_limit,
-    })
-  }
-
-  protected mapWorkQueueResponse({ data }: AxiosResponse<IWorkQueueResponse>): WorkQueue {
-    return this.mapWorkQueue(data)
-  }
-
-  protected mapWorkQueuesResponse({ data }: AxiosResponse<IWorkQueueResponse[]>): WorkQueue[] {
-    return data.map(x => this.mapWorkQueue(x))
-  }
-
-  protected mapWorkQueueFilter(data: IWorkQueueFilterResponse): WorkQueueFilter {
-    return new WorkQueueFilter({
-      tags: data.tags ?? [],
-      deploymentIds: data.deployment_ids ?? [],
-      flowRunnerTypes: data.flow_runner_types ?? [],
-    })
   }
 
 }
