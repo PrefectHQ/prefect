3ac7d12812b58e332547c96f103b2ca112d2532a
Craig Harshbarger <pleek91@gmail.com>
2022-03-30 17:33:54 -0400

    Delete orion-ui services (committed to another branch)

--- orion-ui/src/services/flowsApi.ts
+++
@@ -1,7 +0,0 @@
-import { FlowsApi as OrionDesignFlowsApi } from '@prefecthq/orion-design'
-import { createActions } from '@prefecthq/vue-compositions'
-import { ApiRoute } from '@/mixins/ApiRoute'
-
-export class OrionFlowsApi extends ApiRoute(OrionDesignFlowsApi) {}
-
-export const flowsApi = createActions(new OrionFlowsApi())
--- orion-ui/src/services/taskRunsApi.ts
+++
@@ -1,7 +0,0 @@
-import { TaskRunsApi as OrionDesignTaskRunsApi } from '@prefecthq/orion-design'
-import { createActions } from '@prefecthq/vue-compositions'
-import { ApiRoute } from '@/mixins/ApiRoute'
-
-export class OrionTaskRunsApi extends ApiRoute(OrionDesignTaskRunsApi) {}
-
-export const taskRunsApi = createActions(new OrionTaskRunsApi())
