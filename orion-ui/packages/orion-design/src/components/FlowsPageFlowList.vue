b4886b44741adc524b2e037de442f64763743b44
Natalie Smith <thatgalnatalie@gmail.com>
2022-04-04 11:33:45 -0700

    bind flow

--- orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
+++ orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
@@ -4,8 +4,8 @@
       <VirtualScroller :items="flows" :item-estimate-height="70" @bottom="emit('bottom')">
         <template #default="{ item }">
           <FlowsPageFlowListItem :flow="item">
-            <template #flow-filters="{ flowId }">
-              <slot name="flow-filters" :flow-id="flowId" />
+            <template #flow-filters="{ flow }">
+              <slot name="flow-filters" :flow="flow" />
             </template>
           </FlowsPageFlowListItem>
         </template>
