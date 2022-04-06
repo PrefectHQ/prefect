fb10adca4a4c3e3705cdf1e6a6326cae815956c0
Natalie Smith <thatgalnatalie@gmail.com>
2022-04-06 15:04:35 -0700

    add taskRun and flowRun filter button components

--- orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
+++ orion-ui/packages/orion-design/src/components/FlowsPageFlowList.vue
@@ -1,24 +1,16 @@
 <template>
   <div class="flows-list">
-    <template v-if="loading">
-      <m-loader
-        loading
-        class="text-center my-3 mx-auto"
-      />
-    </template>
-    <template v-else>
-      <m-card shadow="sm">
-        <VirtualScroller :items="flows" :item-estimate-height="70" @bottom="emit('bottom')">
-          <template #default="{ item }">
-            <FlowsPageFlowListItem :flow="item">
-              <template #flow-filters="{ flow }">
-                <slot name="flow-filters" :flow="flow" />
-              </template>
-            </FlowsPageFlowListItem>
-          </template>
-        </VirtualScroller>
-      </m-card>
-    </template>
+    <m-card shadow="sm">
+      <VirtualScroller :items="flows" :item-estimate-height="70" @bottom="emit('bottom')">
+        <template #default="{ item }">
+          <FlowsPageFlowListItem :flow="item">
+            <template #flow-filters="{ flow }">
+              <slot name="flow-filters" :flow="flow" />
+            </template>
+          </FlowsPageFlowListItem>
+        </template>
+      </VirtualScroller>
+    </m-card>
     <template v-if="empty">
       <div class="text-center my-8">
         <h2>No results found</h2>
@@ -32,15 +24,11 @@
   import VirtualScroller from './VirtualScroller.vue'
   import FlowsPageFlowListItem from '@/components/FlowsPageFlowListItem.vue'
   import { Flow } from '@/models/Flow'
-
   const props = defineProps<{
     flows: Flow[],
-    loading: boolean,
   }>()
-
   const emit = defineEmits<{
     (event: 'bottom'): void,
   }>()
-
   const empty = computed(() => props.flows.length === 0)
 </script>
