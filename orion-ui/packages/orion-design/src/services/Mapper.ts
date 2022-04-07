c1c1a3b1e61b7a130437950c8f97ef057f87fff4
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-07 14:22:08 -0500

    fixed issue with having more than 1 return type for a given source type

--- orion-ui/packages/orion-design/src/services/Mapper.ts
+++ orion-ui/packages/orion-design/src/services/Mapper.ts
@@ -3,10 +3,10 @@ import { maps } from '@/maps'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Map = (...args: any) => any
 type Maps = Record<string, Record<string, Map>>
-type MapsMember<T extends Maps, K extends keyof T> = T[K][keyof T[K]]
-type Mappers<T extends Maps, K extends keyof T> = Extract<MapsMember<T, K>, Map>
-type MapperSourceType<T extends Maps, K extends keyof T> = Parameters<Mappers<T, K>>[0]
-type MapperDestinationType<T extends Maps, K extends keyof T> = ReturnType<Mappers<T, K>>
+type MapsMember<T extends Maps, SOURCE extends keyof T> = T[SOURCE][keyof T[SOURCE]]
+type Mappers<T extends Maps, SOURCE extends keyof T> = Extract<MapsMember<T, SOURCE>, Map>
+type MapperSourceType<T extends Maps, SOURCE extends keyof T> = Parameters<Mappers<T, SOURCE>>[0]
+type MapperDestinationType<T extends Maps, SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]> = ReturnType<T[SOURCE][DESTINATION]>
 
 export class Mapper<T extends Maps> {
   private readonly mapperFunctions: T
@@ -15,31 +15,31 @@ export class Mapper<T extends Maps> {
     this.mapperFunctions = mapperFunctions
   }
 
-  public map<K extends keyof T>(source: K, value: MapperSourceType<T, K>, destination: keyof T[K]): MapperDestinationType<T, K>
-  public map<K extends keyof T>(source: K, value: MapperSourceType<T, K>[], destination: keyof T[K]): MapperDestinationType<T, K>[]
-  public map<K extends keyof T>(source: K, value: MapperSourceType<T, K> | MapperSourceType<T, K>[], destination: keyof T[K]): MapperDestinationType<T, K> | MapperDestinationType<T, K>[] {
+  public map<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: MapperSourceType<T, SOURCE>, destination: DESTINATION): MapperDestinationType<T, SOURCE, DESTINATION>
+  public map<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: MapperSourceType<T, SOURCE>[], destination: DESTINATION): MapperDestinationType<T, SOURCE, DESTINATION>[]
+  public map<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: MapperSourceType<T, SOURCE> | MapperSourceType<T, SOURCE>[], destination: DESTINATION): MapperDestinationType<T, SOURCE, DESTINATION> | MapperDestinationType<T, SOURCE, DESTINATION>[] {
     const mapper = this.mapperFunctions[source][destination]
 
     if (Array.isArray(value)) {
-      return value.map(x => this.bindMapper<K>(mapper, x))
+      return value.map(x => this.bindMapper<SOURCE, DESTINATION>(mapper, x))
     }
 
-    return this.bindMapper<K>(mapper, value)
+    return this.bindMapper<SOURCE, DESTINATION>(mapper, value)
   }
 
-  public mapEntries<K extends keyof T>(source: K, value: Record<string, MapperSourceType<T, K>>, destination: keyof T[K]): Record<string, MapperDestinationType<T, K>>
-  public mapEntries<K extends keyof T>(source: K, value: Record<string, MapperSourceType<T, K>[]>, destination: keyof T[K]): Record<string, MapperDestinationType<T, K>[]>
-  public mapEntries<K extends keyof T>(source: K, value: Record<string, MapperSourceType<T, K>> | Record<string, MapperSourceType<T, K>[]>, destination: keyof T[K]): Record<string, MapperDestinationType<T, K>> | Record<string, MapperDestinationType<T, K>[]> {
-    const response = {} as Record<string, MapperDestinationType<T, K>>
+  public mapEntries<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: Record<string, MapperSourceType<T, SOURCE>>, destination: DESTINATION): Record<string, MapperDestinationType<T, SOURCE, DESTINATION>>
+  public mapEntries<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: Record<string, MapperSourceType<T, SOURCE>[]>, destination: DESTINATION): Record<string, MapperDestinationType<T, SOURCE, DESTINATION>[]>
+  public mapEntries<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(source: SOURCE, value: Record<string, MapperSourceType<T, SOURCE>> | Record<string, MapperSourceType<T, SOURCE>[]>, destination: DESTINATION): Record<string, MapperDestinationType<T, SOURCE, DESTINATION>> | Record<string, MapperDestinationType<T, SOURCE, DESTINATION>[]> {
+    const response = {} as Record<string, MapperDestinationType<T, SOURCE, DESTINATION>>
 
-    return Object.entries(value).reduce<Record<string, MapperDestinationType<T, K>>>((mapped, [key, value]) => {
+    return Object.entries(value).reduce<Record<string, MapperDestinationType<T, SOURCE, DESTINATION>>>((mapped, [key, value]) => {
       mapped[key] = this.map(source, value, destination)
 
       return mapped
     }, response)
   }
 
-  private bindMapper<K extends keyof T>(mapper: MapsMember<T, K>, value: Parameters<MapsMember<T, K>>[0]): MapperDestinationType<T, K> {
+  private bindMapper<SOURCE extends keyof T, DESTINATION extends keyof T[SOURCE]>(mapper: MapsMember<T, SOURCE>, value: Parameters<MapsMember<T, SOURCE>>[0]): MapperDestinationType<T, SOURCE, DESTINATION> {
     const map = mapper.bind(this, value)
 
     return map()
@@ -48,4 +48,4 @@ export class Mapper<T extends Maps> {
 
 export const mapper = new Mapper(maps)
 
-export type MapFunction<Source, Destination> = (this: typeof mapper, source: Source) => Destination
+export type MapFunction<SOURCE, DESTINATION> = (this: typeof mapper, source: SOURCE) => DESTINATION
