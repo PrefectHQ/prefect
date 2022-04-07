b189649a42a688de0ee0e27e30c7882e020a6f8c
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-07 17:57:18 -0500

    no longer need ternarys with nullable map

--- orion-ui/packages/orion-design/src/maps/FlowRunGraph.ts
+++ orion-ui/packages/orion-design/src/maps/FlowRunGraph.ts
@@ -5,9 +5,9 @@ import { MapFunction } from '@/services/Mapper'
 export const mapIFlowRunGraphResponseToFlowRunGraph: MapFunction<IFlowRunGraphResponse, FlowRunGraph> = function(source: IFlowRunGraphResponse): FlowRunGraph {
   return new FlowRunGraph({
     id: source.id,
-    expectedStartTime: source.expected_start_time ? this.map('string', source.expected_start_time, 'Date') : null,
-    startTime: source.start_time ? this.map('string', source.start_time, 'Date') : null,
-    endTime: source.end_time ? this.map('string', source.end_time, 'Date') : null,
+    expectedStartTime: this.map('string', source.expected_start_time, 'Date'),
+    startTime: this.map('string', source.start_time, 'Date'),
+    endTime: this.map('string', source.end_time, 'Date'),
     totalRunTime: source.total_run_time,
     estimatedRunTime: source.estimated_run_time,
     upstreamDependencies: source.upstream_dependencies.map(x => {
@@ -23,9 +23,9 @@ export const mapIFlowRunGraphResponseToFlowRunGraph: MapFunction<IFlowRunGraphRe
 export const mapFlowRunGraphToIFlowRunGraphResponse: MapFunction<FlowRunGraph, IFlowRunGraphResponse> = function(source: FlowRunGraph): IFlowRunGraphResponse {
   return {
     'id': source.id,
-    'expected_start_time': source.expectedStartTime ? this.map('Date', source.expectedStartTime, 'string') : null,
-    'start_time': source.startTime ? this.map('Date', source.startTime, 'string') : null,
-    'end_time': source.endTime ? this.map('Date', source.endTime, 'string') : null,
+    'expected_start_time': this.map('Date', source.expectedStartTime, 'string'),
+    'start_time': this.map('Date', source.startTime, 'string'),
+    'end_time': this.map('Date', source.endTime, 'string'),
     'total_run_time': source.totalRunTime,
     'estimated_run_time': source.estimatedRunTime,
     'upstream_dependencies': source.upstreamDependencies.map(x => {
