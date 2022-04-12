9a82590badd3ec57e4ccfdaba6716665e81513c7
Michael Adkins <madkinszane@gmail.com>
2022-04-12 11:33:00 -0500

    Update src/prefect/futures.py

--- src/prefect/futures.py
+++ src/prefect/futures.py
@@ -264,8 +264,8 @@ class PrefectFuture(Generic[R, A]):
     def __bool__(self) -> bool:
         warnings.warn(
             "A 'PrefectFuture' from a task call was cast to a boolean; "
-            "did you mean to check the return value of the task?"
-            "e.g. `if my_task().result()`",
+            "did you mean to check the result of the task instead? "
+            "e.g. `if my_task().result(): ...`",
             stacklevel=2,
         )
         return True
