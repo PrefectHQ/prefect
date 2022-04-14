4a3837f390ae9cfd2b373d420b1f3d1cf06d3205
Malthe Karbo <38499319+mkarbo@users.noreply.github.com>
2022-04-14 16:47:04 +0200

    no_args_is_help as default (PrefectHQ/orion#5678)
    * no_args_is_help as default
    
    * Docstring

--- src/prefect/cli/base.py
+++ src/prefect/cli/base.py
@@ -73,6 +73,20 @@ class PrefectTyper(typer.Typer):
     Wraps commands created by `Typer` to support async functions and handle errors.
     """
 
+    def add_typer(
+        self,
+        typer_instance: "PrefectTyper",
+        *args,
+        no_args_is_help: bool = True,
+        **kwargs,
+    ) -> None:
+        """
+        This will cause help to be default command for all sub apps unless specifically stated otherwise, opposite of before.
+        """
+        return super().add_typer(
+            typer_instance, *args, no_args_is_help=no_args_is_help, **kwargs
+        )
+
     def command(self, *args, **kwargs):
         command_decorator = super().command(*args, **kwargs)
 
