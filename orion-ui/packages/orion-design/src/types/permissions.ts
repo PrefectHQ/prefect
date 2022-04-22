fe0ff0a5f14403ef2cd7a9683d233f784464a66b
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-22 15:33:48 -0500

    code review suggestions

--- orion-ui/packages/orion-design/src/types/permissions.ts
+++ orion-ui/packages/orion-design/src/types/permissions.ts
@@ -1,5 +1,14 @@
 import { InjectionKey } from 'vue'
 
+export const actions = [
+  'create',
+  'read',
+  'update',
+  'delete',
+] as const
+export type Action = typeof actions[number]
+export type Access = 'access'
+
 export const workspaceKeys = [
   'block',
   'concurrency_limit',
@@ -13,6 +22,13 @@ export const workspaceKeys = [
 ] as const
 export type WorkspaceKey = typeof workspaceKeys[number]
 export type WorkspacePermissions = Record<WorkspaceKey, boolean>
+
+export function getWorkspacePermissions(check: (key: WorkspaceKey) => boolean): WorkspacePermissions {
+  return workspaceKeys.reduce<WorkspacePermissions>((reduced, key) => ({
+    ...reduced,
+    [key]: check(key),
+  }), {} as WorkspacePermissions)
+}
 
 export const accountKeys = [
   'account',
@@ -30,28 +46,33 @@ export const accountKeys = [
 export type AccountKey = typeof accountKeys[number]
 export type AccountPermissions = Record<AccountKey, boolean>
 
+export function getAccountPermissions(check: (key: AccountKey) => boolean): AccountPermissions {
+  return accountKeys.reduce<AccountPermissions>((reduced, key) => ({
+    ...reduced,
+    [key]: check(key),
+  }), {} as AccountPermissions)
+}
+
 export const featureFlags = [
   'billing',
   'collaboration',
 ] as const
-export type Access = 'access'
 export type FeatureFlag = typeof featureFlags[number]
+export type FeatureFlagPermissions = Record<FeatureFlag, boolean>
 
-export const actions = [
-  'create',
-  'read',
-  'update',
-  'delete',
-] as const
-export type Action = typeof actions[number]
-
-
-export type ActionPermissions = Record<Action, Record<WorkspaceKey | AccountKey, boolean>>
-export type FeatureFlagPermissions = Record<Access, Record<FeatureFlag, boolean>>
+export function getFeatureFlagPermissions(check: (key: FeatureFlag) => boolean): FeatureFlagPermissions {
+  return featureFlags.reduce<FeatureFlagPermissions>((reduced, key) => ({
+    ...reduced,
+    [key]: check(key),
+  }), {} as FeatureFlagPermissions)
+}
 
 export type AccountPermissionString = `${Action}:${AccountKey}`
 export type WorkspacePermissionString = `${Action}:${WorkspaceKey}`
 
-export type Can = ActionPermissions & FeatureFlagPermissions
+export type ActionPermissionRecords = Record<Action, Record<WorkspaceKey | AccountKey, boolean>>
+export type AccessPermissionRecords = Record<Access, FeatureFlagPermissions>
+
+export type Can = ActionPermissionRecords & AccessPermissionRecords
 
 export const canKey: InjectionKey<Can> = Symbol()
