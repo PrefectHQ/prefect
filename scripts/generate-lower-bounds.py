f28ebf85ee4104b360c7780acae6184a5ee6a765
Michael Adkins <michael@prefect.io>
2022-04-22 11:33:02 -0500

    Add testing for lower bounds script

--- scripts/generate-lower-bounds.py
+++ scripts/generate-lower-bounds.py
@@ -31,34 +31,37 @@ import re
 import sys
 
 
-def generate_lower_bounds(input_path):
-    with open(input_path, "r") as f:
-        for line in f:
-            # Split the package from the conditional section
-            pkg, cond = line.split(";", 1) if ";" in line else (line, "")
-            # Parse the package name and the
-            pkg_parsed = re.match(
-                r"^([\w\d_\-\[\]]*)\s*.*\s*(?:>=|==|~=)\s*([\d*\.?]+)", pkg
+def generate_lower_bounds(input_file):
+    for line in input_file:
+        output_line = ""
+
+        # Split the package from the conditional section
+        pkg, cond = line.split(";", 1) if ";" in line else (line, "")
+        # Parse the package name and the
+        pkg_parsed = re.match(
+            r"^([\w\d_\-\[\]]*)\s*.*\s*(?:>=|==|~=)\s*([\d*\.?]+)", pkg
+        )
+
+        if not pkg_parsed:
+            # There is no versioning for this requirement
+            output_line += pkg.strip()
+        else:
+            # Pin to the lowest version
+            pkg_name, pkg_min_version = (
+                pkg_parsed.groups()[0].strip(),
+                pkg_parsed.groups()[1].strip(),
             )
+            output_line += f"{pkg_name}=={pkg_min_version}"
 
-            if not pkg_parsed:
-                # There is no versioning for this requirement
-                print(pkg.strip(), end="")
-            else:
-                # Pin to the lowest version
-                pkg_name, pkg_min_version = (
-                    pkg_parsed.groups()[0].strip(),
-                    pkg_parsed.groups()[1].strip(),
-                )
-                print(f"{pkg_name}=={pkg_min_version}", end="")
+        # Include the condition if it exists
+        if cond:
+            output_line += f"; {cond.strip()}"
 
-            # Include the condition if it exists, otherwise a newline
-            if cond:
-                print(f"; {cond.strip()}")
-            else:
-                print()
+        yield output_line
 
 
 if __name__ == "__main__":
     input_path = sys.argv[1] if len(sys.argv) > 1 else "requirements.txt"
-    generate_lower_bounds(input_path)
+    with open(input_path, "r") as input_file:
+        for line in generate_lower_bounds(input_file):
+            print(line)
