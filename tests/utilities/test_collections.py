5bd0267f4cf50dd503a50c81285fd174fc0b93e5
Michael Adkins <michael@prefect.io>
2022-04-22 14:16:03 -0500

    Add `PartialModel` utility and use for `FlowRunContext` passing

--- tests/utilities/test_collections.py
+++ tests/utilities/test_collections.py
@@ -4,6 +4,7 @@ import pydantic
 import pytest
 
 from prefect.utilities.collections import (
+    PartialModel,
     dict_to_flatdict,
     flatdict_to_dict,
     visit_collection,
@@ -102,3 +103,58 @@ class TestVisitCollection:
         )
         assert result is None
         assert EVEN == expected
+
+
+class TestPartialModel:
+    def test_init(self):
+        p = PartialModel(SimplePydantic)
+        assert p.model_cls == SimplePydantic
+        assert p.fields == {}
+
+    def test_init_with_fields(self):
+        p = PartialModel(SimplePydantic, x=1, y=2)
+        assert p.fields == {"x": 1, "y": 2}
+        m = p.finalize()
+        assert isinstance(m, SimplePydantic)
+        assert m == SimplePydantic(x=1, y=2)
+
+    def test_init_with_invalid_field(self):
+        with pytest.raises(ValueError, match="Field 'z' is not present in the model"):
+            PartialModel(SimplePydantic, x=1, z=2)
+
+    def test_set_attribute(self):
+        p = PartialModel(SimplePydantic)
+        p.x = 1
+        p.y = 2
+        assert p.finalize() == SimplePydantic(x=1, y=2)
+
+    def test_set_invalid_attribute(self):
+        p = PartialModel(SimplePydantic)
+        with pytest.raises(ValueError, match="Field 'z' is not present in the model"):
+            p.z = 1
+
+    def test_set_already_set_attribute(self):
+        p = PartialModel(SimplePydantic, x=1)
+        with pytest.raises(ValueError, match="Field 'x' has already been set"):
+            p.x = 2
+
+    def test_finalize_with_fields(self):
+        p = PartialModel(SimplePydantic)
+        m = p.finalize(x=1, y=2)
+        assert isinstance(m, SimplePydantic)
+        assert m == SimplePydantic(x=1, y=2)
+
+    def test_finalize_with_invalid_field(self):
+        p = PartialModel(SimplePydantic)
+        with pytest.raises(ValueError, match="Field 'z' is not present in the model"):
+            p.finalize(z=1)
+
+    def test_finalize_with_already_set_field(self):
+        p = PartialModel(SimplePydantic, x=1)
+        with pytest.raises(ValueError, match="Field 'x' has already been set"):
+            p.finalize(x=1)
+
+    def test_finalize_with_missing_field(self):
+        p = PartialModel(SimplePydantic, x=1)
+        with pytest.raises(ValueError, match="validation error"):
+            p.finalize()
