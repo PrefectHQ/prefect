6b3940102b0c8a1ef5dc5766b46f0f11cf5b7a21
Evan Sutherland <stackoverfloweth@gmail.com>
2022-04-23 15:36:55 -0500

    using byPassPermissions method to bypass permissions for orion-ui

--- orion-ui/src/services/permissions.ts
+++
@@ -1,7 +0,0 @@
-import { Can, getAppAccountPermissions, getAppWorkspacePermissions, getAppFeatureFlags } from '@prefecthq/orion-design'
-
-export const can: Can = {
-  ...getAppAccountPermissions(() => true),
-  ...getAppWorkspacePermissions(() => true),
-  ...getAppFeatureFlags(() => true),
-}
