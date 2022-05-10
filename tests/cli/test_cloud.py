9f7e578da5cca5e094f08686deeaa910260ac439
Jake Kaplan <jake@prefect.io>
2022-05-10 10:12:35 -0400

    pr changes

--- tests/cli/test_cloud.py
+++ tests/cli/test_cloud.py
@@ -1,17 +1,19 @@
 import uuid
 
+import pytest
+
 import prefect.cli.cloud
 from prefect.context import use_profile
 from prefect.settings import (
     PREFECT_API_KEY,
     PREFECT_API_URL,
+    PREFECT_DEBUG_MODE,
     Profile,
     ProfilesCollection,
     load_profiles,
     save_profiles,
 )
 from prefect.testing.cli import invoke_and_assert
-
 
 ACCOUNT_ID = uuid.uuid4()
 ACCOUNT_HANDLE = "account-1"
@@ -25,6 +27,7 @@ WORKSPACE = {
     "workspace_id": WORKSPACE_ID,
     "workspace_handle": WORKSPACE_HANDLE,
 }
+FULL_URL = prefect.cli.cloud.build_url_from_workspace(WORKSPACE)
 
 
 class MockCloudClient:
@@ -43,19 +46,37 @@ def get_mock_cloud_client(*args, **kwargs):
     return MockCloudClient()
 
 
+class MockUnauthorizedCloudClient(MockCloudClient):
+    async def read_workspaces(self):
+        raise prefect.cli.cloud.CloudUnauthorizedError
+
+
+def get_unauthorized_mock_cloud_client(*args, **kwargs):
+    """Returns a MockUnauthorizedCloudClient that will throw an error"""
+    return MockUnauthorizedCloudClient()
+
+
 def mock_select_workspace(workspaces):
     """Mocks a user selecting a workspace via keyboard input. Will return the first workspace."""
     return list(workspaces)[0]
 
 
-def test_invalid_login():
+@pytest.mark.usefixtures("disable_terminal_wrapping")
+def test_invalid_login(monkeypatch):
+    monkeypatch.setattr(
+        "prefect.cli.cloud.get_cloud_client", get_unauthorized_mock_cloud_client
+    )
+
     invoke_and_assert(
         ["cloud", "login", "--key", "invalid_API_key"],
         expected_code=1,
-        expected_output="Unable to authenticate. Please ensure your credentials are correct.",
+        expected_output=(
+            "Unable to authenticate. Please ensure your credentials are correct."
+        ),
     )
 
 
+@pytest.mark.usefixtures("disable_terminal_wrapping")
 def test_login_updates_profile(monkeypatch):
     monkeypatch.setattr("prefect.cli.cloud.get_cloud_client", get_mock_cloud_client)
     monkeypatch.setattr("prefect.cli.cloud.select_workspace", mock_select_workspace)
@@ -69,16 +90,51 @@ def test_login_updates_profile(monkeypatch):
         invoke_and_assert(
             ["cloud", "login", "--key", API_KEY],
             expected_code=0,
-            expected_output=f"Successfully logged in and set workspace to {FULL_HANDLE!r} in profile \n{cloud_profile!r}.",
+            expected_output=(
+                f"Successfully logged in and set workspace to {FULL_HANDLE!r} "
+                f"in profile {cloud_profile!r}."
+            ),
         )
 
     profiles = load_profiles()
     assert profiles[cloud_profile].settings == {
-        PREFECT_API_URL: prefect.cli.cloud.build_url_from_workspace(WORKSPACE),
+        PREFECT_API_URL: FULL_URL,
         PREFECT_API_KEY: API_KEY,
     }
 
 
+@pytest.mark.usefixtures("disable_terminal_wrapping")
+def test_login_wont_unset_other_settings(monkeypatch):
+    monkeypatch.setattr("prefect.cli.cloud.get_cloud_client", get_mock_cloud_client)
+    monkeypatch.setattr("prefect.cli.cloud.select_workspace", mock_select_workspace)
+
+    cloud_profile = "cloud-foo"
+    save_profiles(
+        ProfilesCollection(
+            [Profile(name=cloud_profile, settings={PREFECT_DEBUG_MODE: True})],
+            active=None,
+        )
+    )
+
+    with use_profile(cloud_profile):
+        invoke_and_assert(
+            ["cloud", "login", "--key", API_KEY],
+            expected_code=0,
+            expected_output=(
+                f"Successfully logged in and set workspace to {FULL_HANDLE!r} "
+                f"in profile {cloud_profile!r}."
+            ),
+        )
+
+    profiles = load_profiles()
+    assert profiles[cloud_profile].settings == {
+        PREFECT_API_URL: FULL_URL,
+        PREFECT_API_KEY: API_KEY,
+        PREFECT_DEBUG_MODE: True,
+    }
+
+
+@pytest.mark.usefixtures("disable_terminal_wrapping")
 def test_logout_updates_profile():
     cloud_profile = "cloud-foo"
     save_profiles(
@@ -87,9 +143,7 @@ def test_logout_updates_profile():
                 Profile(
                     name=cloud_profile,
                     settings={
-                        PREFECT_API_URL: prefect.cli.cloud.build_url_from_workspace(
-                            WORKSPACE
-                        ),
+                        PREFECT_API_URL: FULL_URL,
                         PREFECT_API_KEY: API_KEY,
                     },
                 )
@@ -109,6 +163,7 @@ def test_logout_updates_profile():
     assert profiles[cloud_profile].settings == {}
 
 
+@pytest.mark.usefixtures("disable_terminal_wrapping")
 def test_cannot_logout_if_you_are_not_logged_in():
     cloud_profile = "cloud-foo"
     save_profiles(
@@ -119,11 +174,14 @@ def test_cannot_logout_if_you_are_not_logged_in():
         invoke_and_assert(
             ["cloud", "logout"],
             expected_code=1,
-            expected_output=f"Currently not authenticated in profile {cloud_profile!r}. "
-            "Please login with `prefect \ncloud login --key <API_KEY>`.",
+            expected_output=(
+                f"Currently not authenticated in profile {cloud_profile!r}. "
+                "Please login with `prefect cloud login --key <API_KEY>`."
+            ),
         )
 
 
+@pytest.mark.usefixtures("disable_terminal_wrapping")
 def test_cannot_set_workspace_if_you_are_not_logged_in():
     cloud_profile = "cloud-foo"
     save_profiles(
@@ -134,11 +192,14 @@ def test_cannot_set_workspace_if_you_are_not_logged_in():
         invoke_and_assert(
             ["cloud", "workspace", "set", "--workspace", f"{FULL_HANDLE}"],
             expected_code=1,
-            expected_output=f"Currently not authenticated in profile {cloud_profile!r}. "
-            "Please login with `prefect \ncloud login --key <API_KEY>`.",
+            expected_output=(
+                f"Currently not authenticated in profile {cloud_profile!r}. "
+                "Please login with `prefect cloud login --key <API_KEY>`."
+            ),
         )
 
 
+@pytest.mark.usefixtures("disable_terminal_wrapping")
 def test_set_workspace_updates_profile(monkeypatch):
     monkeypatch.setattr("prefect.cli.cloud.get_cloud_client", get_mock_cloud_client)
     monkeypatch.setattr("prefect.cli.cloud.select_workspace", mock_select_workspace)
@@ -163,5 +224,14 @@ def test_set_workspace_updates_profile(monkeypatch):
         invoke_and_assert(
             ["cloud", "workspace", "set"],
             expected_code=0,
-            expected_output=f"Successfully set workspace to {FULL_HANDLE!r} in profile {cloud_profile!r}.",
+            expected_output=(
+                f"Successfully set workspace to {FULL_HANDLE!r} "
+                f"in profile {cloud_profile!r}."
+            ),
         )
+
+    profiles = load_profiles()
+    assert profiles[cloud_profile].settings == {
+        PREFECT_API_URL: FULL_URL,
+        PREFECT_API_KEY: API_KEY,
+    }
