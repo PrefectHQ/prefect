7a02ce183c478b1d0557cd2d1651e3819f0dc6f7
peytonrunyan <peytonrunyan@gmail.com>
2022-05-12 10:30:42 -0400

    add checks for strings in and not in CLI output

--- src/prefect/testing/cli.py
+++ src/prefect/testing/cli.py
@@ -17,7 +17,7 @@ def invoke_and_assert(
     user_input=None,
 ) -> Result:
     """
-    Test utility for the Prefect CLI application.
+    Test utility for the Prefect CLI application, asserts exact match with CLI output.
     """
     runner = CliRunner()
     result = runner.invoke(app, command, catch_exceptions=False, input=user_input)
@@ -39,6 +39,76 @@ def invoke_and_assert(
         print()
 
         assert output == expected_output
+
+    return result
+
+
+def invoke_and_assert_in(
+    command: List[str],
+    desired_contents: str = None,
+    expected_code: int = 0,
+    echo: bool = True,
+    user_input=None,
+) -> Result:
+    """
+    Test utility for the Prefect CLI application, asserts string in CLI output.
+    """
+    runner = CliRunner()
+    result = runner.invoke(app, command, catch_exceptions=False, input=user_input)
+
+    if echo:
+        print("------ actual ------")
+        print(result.stdout)
+
+    if expected_code is not None:
+        assert (
+            result.exit_code == expected_code
+        ), f"Actual exit code: {result.exit_code!r}"
+
+    if desired_contents is not None:
+        output = result.stdout.strip()
+        desired_contents = textwrap.dedent(desired_contents).strip()
+
+        print("------ expected ------")
+        print(desired_contents)
+        print()
+
+        assert desired_contents in output, "Desired contents not found in CLI ouput"
+
+    return result
+
+
+def invoke_and_assert_not_in(
+    command: List[str],
+    undesired_contents: str = None,
+    expected_code: int = 0,
+    echo: bool = True,
+    user_input=None,
+) -> Result:
+    """
+    Test utility for the Prefect CLI application, asserts string not in CLI output.
+    """
+    runner = CliRunner()
+    result = runner.invoke(app, command, catch_exceptions=False, input=user_input)
+
+    if echo:
+        print("------ actual ------")
+        print(result.stdout)
+
+    if expected_code is not None:
+        assert (
+            result.exit_code == expected_code
+        ), f"Actual exit code: {result.exit_code!r}"
+
+    if undesired_contents is not None:
+        output = result.stdout.strip()
+        undesired_contents = textwrap.dedent(undesired_contents).strip()
+
+        print("------ expected ------")
+        print(undesired_contents)
+        print()
+
+        assert undesired_contents not in output, "Undesired contents found in CLI ouput"
 
     return result
 
