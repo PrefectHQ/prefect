f75e8533498c06762fcbe248ef9d4798fd862ce1
Anna Geller <anna@prefect.io>
2022-05-16 20:41:46 +0200

    review feedback

--- src/prefect/tasks/prefect/flow_run.py
+++ src/prefect/tasks/prefect/flow_run.py
@@ -313,7 +313,7 @@ class StartFlowRun(Task):
             for; if not provided, defaults to now
         - poll_interval (timedelta): the time to wait between each check if the flow is finished.
                 Has to be >= 3 seconds. Used only if `wait=True`. Defaults to 10 seconds.
-        - create_artifact_link (bool, optional): whether to create a link artifact
+        - create_link_artifact (bool, optional): whether to create a link artifact
             to the child flow run page.  Defaults to `True`.
         - **kwargs (dict, optional): additional keyword arguments to pass to the Task constructor
     """
@@ -329,7 +329,7 @@ class StartFlowRun(Task):
         run_name: str = None,
         scheduled_start_time: datetime.datetime = None,
         poll_interval: timedelta = timedelta(seconds=10),
-        create_artifact_link: bool = True,
+        create_link_artifact: bool = True,
         **kwargs: Any,
     ):
         self.flow_name = flow_name
@@ -358,6 +358,7 @@ class StartFlowRun(Task):
                 f"(poll_interval == {poll_interval.total_seconds()} seconds)!"
             )
         self.poll_interval = poll_interval
+        self.create_link_artifact = create_link_artifact
         if flow_name:
             kwargs.setdefault("name", f"Flow {flow_name}")
         super().__init__(**kwargs)
@@ -475,7 +476,7 @@ class StartFlowRun(Task):
 
         self.logger.debug(f"Creating link artifact for Flow Run {flow_run_id}.")
         run_link = client.get_cloud_url("flow-run", flow_run_id)
-        if self.create_artifact_link:
+        if self.create_link_artifact:
             create_link_artifact(urlparse(run_link).path)
         self.logger.info(f"Flow Run: {run_link}")
 
