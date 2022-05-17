476cf647575b3397d0372d8312f680ad9623cd11
nicholas <znicholasbrown@gmail.com>
2022-05-17 02:19:05 -0400

    Add 404 page, app level tailwind styles, fonts

--- orion-ui/tailwind.config.js
+++ orion-ui/tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      mono: ['Inconsolata', ...defaultTheme.fontFamily.mono],
+    },
+  },
 }
