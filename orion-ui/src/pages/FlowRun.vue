7455af34dedd6e4d794884623ddb7dc20fa022f5
nicholas <znicholasbrown@gmail.com>
2022-05-17 02:22:32 -0400

    Add flow run and flow runs page scaffolding

---
+++ orion-ui/src/pages/FlowRun.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flow-run">
+    Flow run {{ id }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useRouteParam } from '@prefecthq/orion-design'
+
+  const id = useRouteParam('id')
+</script>
+
+<style>
+.flow-run {}
+</style>
