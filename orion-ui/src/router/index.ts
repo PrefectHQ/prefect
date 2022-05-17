f33187c3bf6b5ea0412527be085a82b7e517c8e7
nicholas <znicholasbrown@gmail.com>
2022-05-17 02:12:08 -0400

    Fix router

--- orion-ui/src/router/index.ts
+++ orion-ui/src/router/index.ts
@@ -1,78 +1,11 @@
-import { RouteGuardExecutioner, GlobalClosePanels } from '@prefecthq/orion-design'
+import { RouteGuardExecutioner } from '@prefecthq/orion-design'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Dashboard from '../views/Dashboard.vue'
-import { DashboardDefaultFilters } from './guards/DashboardDefaultFilters'
-import { FlowRunDefaultFilters } from './guards/FlowRunDefaultFilters'
-import { GlobalLoadFiltersFromRoute } from './guards/GlobalLoadFiltersFromRoute'
-
-const routes: RouteRecordRaw[] = [
-  {
-    path: '/',
-    name: 'Dashboard',
-    // We don't implement route level code splitting for the Dashboard route because we don't want this to load asynchronously
-    component: Dashboard,
-    meta: {
-      guards: [new DashboardDefaultFilters()],
-      filters: {
-        visible: true,
-      },
-    },
-  },
-  {
-    path: '/flows',
-    name: 'Flows',
-    component: () => import('../views/Flows.vue'),
-  },
-  {
-    path: '/work-queues',
-    name: 'Work Queues',
-    component: () => import('../views/WorkQueues.vue'),
-  },
-  {
-    path: '/flow-run/:id',
-    name: 'FlowRun',
-    component: () => import('../views/FlowRun.vue'),
-    meta: {
-      guards: [new FlowRunDefaultFilters()],
-      filters: {
-        visible: true,
-        disabled: true,
-      },
-    },
-    children: [
-      {
-        path: '',
-        component: () => import('../views/FlowRun--views/Index.vue'),
-      },
-      {
-        path: 'timeline',
-        component: () => import('../views/FlowRun--views/Timeline.vue'),
-      },
-      {
-        path: 'radar',
-        component: () => import('../views/FlowRun--views/Radar.vue'),
-      },
-    ],
-  },
-  {
-    path: '/settings',
-    name: 'Settings',
-    component: () => import('../views/Settings.vue'),
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: () => import('../views/NotFound.vue'),
-  },
-]
+const routes: RouteRecordRaw[] = []
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
-
-RouteGuardExecutioner.register(new GlobalLoadFiltersFromRoute())
-RouteGuardExecutioner.register(new GlobalClosePanels())
 
 router.beforeEach(async (to, from) => {
   return await RouteGuardExecutioner.before(to, from)
