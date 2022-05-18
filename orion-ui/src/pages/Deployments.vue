cdac5e7a634832ed4f11abb78797f89c633a645a
Jennifer Grange <jenny@prefect.io>
2022-05-18 12:14:19 -0400

    add deployments subscription

--- orion-ui/src/pages/Deployments.vue
+++ orion-ui/src/pages/Deployments.vue
@@ -2,11 +2,26 @@
   <div class="deployments">
     Deployments
   </div>
+
+  <div v-for="deployment in deployments" :key="deployment.id">
+    {{ deployment }}
+  </div>
 </template>
 
-<script lang="ts" setup>
 
+<script lang="ts" setup>
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { deploymentsApi } from '@/services/deploymentsApi'
+
+  const filter = {}
+  const subscriptionOptions = {
+    interval: 30000,
+  }
+  const deploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [filter], subscriptionOptions)
+  const deployments = computed(() => deploymentsSubscription.response ?? [])
 </script>
+
 
 <style>
 .deployments {}
