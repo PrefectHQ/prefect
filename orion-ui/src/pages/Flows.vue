b4e74bba02e22ed9ee6cf305b9252795346cfc4d
Jennifer Grange <jenny@prefect.io>
2022-05-18 11:27:28 -0400

    add subscription for flow data to the flows page

--- orion-ui/src/pages/Flows.vue
+++ orion-ui/src/pages/Flows.vue
@@ -2,10 +2,23 @@
   <div class="flows">
     Flows
   </div>
+  <div v-for="flow in flows" :key="flow.id">
+    {{ flow }}
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { flowsApi } from '@/services/flowsApi'
 
+  const filter = {}
+  const subscriptionOptions = {
+    interval: 30000,
+  }
+
+  const flowsSubscription = useSubscription(flowsApi.getFlows, [filter], subscriptionOptions)
+  const flows = computed(() => flowsSubscription.response ?? [])
 </script>
 
 <style>
