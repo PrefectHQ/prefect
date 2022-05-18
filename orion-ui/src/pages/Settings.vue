165c625687953adb1faeefdf642d1eb75ab1ab8c
Jennifer Grange <jenny@prefect.io>
2022-05-18 15:51:08 -0400

    add admin api and add settings page data

--- orion-ui/src/pages/Settings.vue
+++ orion-ui/src/pages/Settings.vue
@@ -2,10 +2,24 @@
   <div class="settings">
     Settings
   </div>
+
+  <div>
+    {{ engineSettings }}
+  </div>
+
+  <div>
+    Version
+  </div>
+  <div>
+    {{ version }}
+  </div>
 </template>
 
 <script lang="ts" setup>
+  import { adminApi } from '@/services/adminApi'
 
+  const engineSettings = await adminApi.getSettings()
+  const  version  = await adminApi.getVersion()
 </script>
 
 <style>
