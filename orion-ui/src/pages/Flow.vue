1708231fc9ae7cd752d958bfc1b81ea3570b1b42
Jennifer Grange <jenny@prefect.io>
2022-05-18 18:14:58 -0400

    add flow page data

--- orion-ui/src/pages/Flow.vue
+++ orion-ui/src/pages/Flow.vue
@@ -1,13 +1,46 @@
 <template>
   <div class="flow">
-    Flow {{ id }}
+    Flow {{ flowId }}
+  </div>
+
+  <div>
+    Flow Details
+  </div>
+  <div>
+    {{ flowDetails }}
+  </div>
+
+  <div>
+    Flow Deployments
+  </div>
+  <div v-for="deployment in flowDeployments" :key="deployment.id">
+    {{ deployment }}
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useRouteParam } from '@prefecthq/orion-design'
+  import { useRouteParam, UnionFilters } from '@prefecthq/orion-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { deploymentsApi } from '@/services/deploymentsApi'
+  import { flowsApi } from '@/services/flowsApi'
 
-  const id = useRouteParam('id')
+  const flowId = useRouteParam('id')
+  const subscriptionOptions = {
+    interval: 300000,
+  }
+  const flowSubscription = useSubscription(flowsApi.getFlow, [flowId.value], subscriptionOptions)
+  const flowDetails = computed(() => flowSubscription.response ?? [])
+
+  const flowDeploymentFilter = computed<UnionFilters>(() => ({
+    flows: {
+      id: {
+        any_: [flowId.value],
+      },
+    },
+  }))
+  const flowDeploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [flowDeploymentFilter], subscriptionOptions)
+  const flowDeployments = computed(() => flowDeploymentsSubscription.response ?? [])
 </script>
 
 <style>
