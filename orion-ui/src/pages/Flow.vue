5ce025505b5b546d03c66472cb335ef138e7053e
Jennifer Grange <jenny@prefect.io>
2022-05-18 22:05:42 -0400

    remove flow runs from flow page

--- orion-ui/src/pages/Flow.vue
+++ orion-ui/src/pages/Flow.vue
@@ -16,11 +16,6 @@
   <div v-for="deployment in flowDeployments" :key="deployment.id">
     {{ deployment }}
   </div>
-
-  <div>Flow Runs</div>
-  <div v-for="flowRun in flowFlowRuns" :key="flowRun.id">
-    {{ flowRun }}
-  </div>
 </template>
 
 <script lang="ts" setup>
@@ -28,7 +23,6 @@
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { deploymentsApi } from '@/services/deploymentsApi'
-  import { flowRunsApi } from '@/services/flowRunsApi'
   import { flowsApi } from '@/services/flowsApi'
 
   const flowId = useRouteParam('id')
@@ -44,19 +38,9 @@
         any_: [flowId.value],
       },
     },
-    flow_runs:{
-      state: {
-        type: {
-          any_: ['FAILED'],
-        },
-      },
-    },
-
   }))
   const flowDeploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [flowDeploymentFilter], subscriptionOptions)
   const flowDeployments = computed(() => flowDeploymentsSubscription.response ?? [])
-
-  const flowFlowRuns = await flowRunsApi.getFlowRuns(flowDeploymentFilter.value)
 </script>
 
 <style>
