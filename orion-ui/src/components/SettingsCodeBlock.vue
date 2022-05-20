03b6aa1a6c59b3451e6447bd3b829369fb34d62d
Jennifer Grange <jenny@prefect.io>
2022-05-20 12:28:24 -0400

    add codeblock

---
+++ orion-ui/src/components/SettingsCodeBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <PCode>
+    {{ engineSettings }}
+  </PCode>
+</template>
+
+<script lang="ts" setup>
+  import { Settings } from '@prefecthq/orion-design'
+  import { PCode } from '@prefecthq/prefect-design'
+  import { defineProps, computed } from 'vue'
+
+  const props = defineProps<{
+    engineSettings: Settings,
+  }>()
+</script>
+
+<style>
+.settings {}
+</style>
