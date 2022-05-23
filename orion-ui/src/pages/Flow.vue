91fb21d810e3b45107012f90f3b1a95475f86ec3
Craig Harshbarger <pleek91@gmail.com>
2022-05-23 14:04:10 -0500

    Add flow run lists

--- orion-ui/src/pages/Flow.vue
+++ orion-ui/src/pages/Flow.vue
@@ -17,14 +17,18 @@
     <div v-for="deployment in flowDeployments" :key="deployment.id">
       {{ deployment }}
     </div>
+
+    <p>Flow Sub Runs</p>
+    <FlowRunList :flow-runs="flowRuns" :selected="selectedFlowRuns" disabled />
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { useRouteParam, UnionFilters } from '@prefecthq/orion-design'
+  import { useRouteParam, UnionFilters, FlowRunList } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
   import { deploymentsApi } from '@/services/deploymentsApi'
+  import { flowRunsApi } from '@/services/flowRunsApi'
   import { flowsApi } from '@/services/flowsApi'
 
   const flowId = useRouteParam('id')
@@ -34,13 +38,17 @@
   const flowSubscription = useSubscription(flowsApi.getFlow, [flowId.value], subscriptionOptions)
   const flowDetails = computed(() => flowSubscription.response ?? [])
 
-  const flowDeploymentFilter = computed<UnionFilters>(() => ({
+  const flowFilter = computed<UnionFilters>(() => ({
     flows: {
       id: {
         any_: [flowId.value],
       },
     },
   }))
-  const flowDeploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [flowDeploymentFilter], subscriptionOptions)
+  const flowDeploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [flowFilter], subscriptionOptions)
   const flowDeployments = computed(() => flowDeploymentsSubscription.response ?? [])
+
+  const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [flowFilter], subscriptionOptions)
+  const flowRuns = computed(()=> flowRunsSubscription.response ?? [])
+  const selectedFlowRuns = ref([])
 </script>
