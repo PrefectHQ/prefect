f54566380cda1c68349165318353bf4bc599110e
Craig Harshbarger <pleek91@gmail.com>
2022-05-23 12:02:26 -0500

    Use PLayoutDefault for all pages

--- orion-ui/src/pages/FlowRuns.vue
+++ orion-ui/src/pages/FlowRuns.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="flow-runs">
-    Flow runs
-  </div>
+  <p-layout-default class="flow-runs">
+    <template #header>
+      <div class="flow-runs">
+        Flow runs
+      </div>
+    </template>
 
-  <div>
-    Flow Run History
-  </div>
-  <div v-for="run in flowRunHistory" :key="run.id">
-    {{ run }}
-  </div>
+    <div>
+      Flow Run History
+    </div>
+    <div v-for="run in flowRunHistory" :key="run.id">
+      {{ run }}
+    </div>
 
-  <div>
-    Flow Run List
-  </div>
-  <div v-for="flowRun in flowRuns" :key="flowRun.id">
-    {{ flowRun }}
-  </div>
+    <div>
+      Flow Run List
+    </div>
+    <div v-for="flowRun in flowRuns" :key="flowRun.id">
+      {{ flowRun }}
+    </div>
+  </p-layout-default>
 </template>
 
 <script lang="ts" setup>
@@ -34,7 +38,3 @@
   const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [filter], subscriptionOptions)
   const flowRuns = computed(()=> flowRunsSubscription.response ?? [])
 </script>
-
-<style>
-.flow-runs {}
-</style>
