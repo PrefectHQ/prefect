0bfa7fc161980678d27797d44ca472b9f3149084
Alexander Streed <desertaxle@users.noreply.github.com>
2022-05-24 11:15:28 -0500

    Update src/prefect/tasks/sftp/sftp.py
    Co-authored-by: Terrence Dorsey <terrend@mishu.com>

--- src/prefect/tasks/sftp/sftp.py
+++ src/prefect/tasks/sftp/sftp.py
@@ -17,7 +17,7 @@ class SftpDownload(Task):
         - username (str): Username used to authenticate.
         - password (str): Password used to authenticate.
         - port_number (int): The port to connect to the server.
-        - remote_path (str): The remote sftp file path.
+        - remote_path (str): The remote SFTP file path.
         - local_path (str): The local file path to download file to.
         - **kwargs (dict, optional): Additional keyword arguments to pass to the Task constructor.
 
