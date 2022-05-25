7aabc89c0524fcf408511a6c5efa65cd8b593455
Craig Harshbarger <pleek91@gmail.com>
2022-05-25 08:42:17 -0500

    Fix subscription options and duplicate subscription. Add route keys

--- orion-ui/src/App.vue
+++ orion-ui/src/App.vue
@@ -22,7 +22,9 @@
     flowsRouteKey,
     deploymentsRouteKey,
     queuesRouteKey,
-    settingsRouteKey
+    settingsRouteKey,
+    flowRunRouteKey,
+    flowRouteKey
   } from '@prefecthq/orion-design'
   import { provide } from 'vue'
   import { routes } from '@/router/routes'
@@ -48,6 +50,8 @@
   provide(deploymentsRouteKey, routes.deployments)
   provide(queuesRouteKey, routes.queues)
   provide(settingsRouteKey, routes.settings)
+  provide(flowRunRouteKey, routes.flowRun)
+  provide(flowRouteKey, routes.flow)
 </script>
 
 <style>
