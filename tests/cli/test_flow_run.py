adc4339ee7cfb64bdf03d450d956862d4ffb5dcb
peytonrunyan <peytonrunyan@gmail.com>
2022-05-25 10:39:26 -0400

    update test to use fixtures

--- tests/cli/test_flow_run.py
+++ tests/cli/test_flow_run.py
@@ -1,25 +1,15 @@
+import asyncio
+import pdb
+import time
+
+from prefect import flow
+from prefect.flow_runners import UniversalFlowRunner
 from prefect.testing.cli import invoke_and_assert
-
-
-class MockClient:
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, *args, **kwargs):
-        pass
-
-    async def delete_flow_run_by_id(self, *args):
-        pass
-
-
-def get_mock_client(*args, **kwargs):
-    """Used to patch `get_client` calls"""
-    return MockClient()
 
 
 def test_delete_flow_run_fails_correctly():
     UUID_404_input = "ccb86ed0-e824-4d8b-b825-880401320e41"
-    UUID_404_output = "Flow run UUID('ccb86ed0-e824-4d8b-b825-880401320e41') not found!"
+    UUID_404_output = "Flow run ccb86ed0-e824-4d8b-b825-880401320e41 not found!"
     invoke_and_assert(
         command=["flow-run", "delete", UUID_404_input],
         expected_output=UUID_404_output,
@@ -27,15 +17,13 @@ def test_delete_flow_run_fails_correctly():
     )
 
 
-def test_delete_flow_run_succeeds(monkeypatch):
-    monkeypatch.setattr("prefect.cli.flow_run.get_client", get_mock_client)
-    good_input = "a9ea6c01-d2ee-401d-8716-3f0500caa1b3"
-    good_output = (
-        "Successfully deleted flow run UUID('a9ea6c01-d2ee-401d-8716-3f0500caa1b3')."
-    )
+def test_delete_flow_run_succeeds(orion_client, flow_run):
 
+    flow_id_string = str(flow_run.id)
+    print(f"flow run: {flow_run!r}")
+    good_output = f"Successfully deleted flow run {flow_id_string}."
     invoke_and_assert(
-        command=["flow-run", "delete", good_input],
+        command=["flow-run", "delete", flow_id_string],
         expected_output=good_output,
         expected_code=0,
     )
