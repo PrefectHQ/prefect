7e51f5d7be8b90c115bbf6406bc400e6adf12de2
Jennifer Grange <jenny@prefect.io>
2022-05-26 16:43:52 -0400

    move filter to orion

--- orion-ui/src/pages/Flows.vue
+++ orion-ui/src/pages/Flows.vue
@@ -1,33 +1,33 @@
 <template>
   <p-layout-default class="flows">
-    <template #header>
-      Flows
-    </template>
-    <FlowSearch v-model="flows" />
-    <div v-for="flow in flows" :key="flow.id">
+    <template #header> Flows </template>
+    <SearchInput v-model="flowSearchInput" />
+    <div v-for="flow in filteredFlowList" :key="flow.id">
       {{ flow }}
     </div>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { FlowSearch, Flow } from '@prefecthq/orion-design'
-  import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed, ref } from 'vue'
-  import { flowsApi } from '@/services/flowsApi'
+import { FlowSearch, Flow, SearchInput } from "@prefecthq/orion-design";
+import { useSubscription } from "@prefecthq/vue-compositions";
+import { computed, ref } from "vue";
+import { flowsApi } from "@/services/flowsApi";
 
-  const filter = {}
-  const subscriptionOptions = {
-    interval: 30000,
-  }
-  const flowsSubscription = useSubscription(flowsApi.getFlows, [filter], subscriptionOptions)
-  const filteredFlowList=ref(null)
-  const flows = computed<Flow[]>({
-    get() {
-      return filteredFlowList.value ?? flowsSubscription.response ?? []
-    },
-    set(value: Flow[] | null) {
-      filteredFlowList.value = value
-    },
-  })
+const filter = {}
+const subscriptionOptions = {interval: 30000}
+const flowsSubscription = useSubscription(flowsApi.getFlows,[filter],subscriptionOptions)
+const flows = computed<Flow[]>(()=> flowsSubscription.response ?? [])
+const flowSearchInput = ref('')
+const filteredFlowList = computed(()=> flowFilterFunction(flows.value, flowSearchInput.value))
+
+const flowFilterFunction=(array, text) => array?.reduce<Flow[]>(
+    (previous: Flow, current: Flow): Flow[] => {
+    if (current.name.toLowerCase().includes(text.toLowerCase())) {
+      previous.push(current)
+    }
+    return previous
+  },
+  []
+)
 </script>
