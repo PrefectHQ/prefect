b662acab378176204e73ac1c304d9a4f037e2ebd
Craig Harshbarger <pleek91@gmail.com>
2022-05-26 15:00:39 -0500

    Review tweaks

--- orion-ui/src/pages/FlowRuns.vue
+++ orion-ui/src/pages/FlowRuns.vue
@@ -15,38 +15,41 @@
           </p-label>
         </div>
         <div class="grid gap-1 grid-cols-4">
+          <StateSelect v-model:selected="states" empty-message="All run states" />
           <FlowCombobox v-model:selected="flows" empty-message="All flows" />
           <DeploymentCombobox v-model:selected="deployments" empty-message="All deployments" />
           <PTagsInput v-model:tags="tags" empty-message="All Tags" inline />
-          <StateSelect v-model:selected="states" empty-message="All run states" />
         </div>
       </div>
 
       <FlowRunsScatterPlot :history="flowRunHistory" style="height: 275px" />
 
-      <div class="flow-runs--sort-search">
-        <SearchInput v-model="flowRunSearchInput" placeholder="Search by run name" label="Search by run name" />
-        <FlowRunsSort v-model="sort" />
-      </div>
+      <div class="flow-runs__list grid gap-1">
+        <div class="flow-runs--sort-search">
+          <SearchInput v-model="flowRunSearchInput" placeholder="Search by run name" label="Search by run name" />
+          <FlowRunsSort v-model="sort" />
+        </div>
 
-      <FlowRunList :flow-runs="flowRuns" :selected="selectedFlowRuns" disabled />
+        <FlowRunList :flow-runs="flowRuns" :selected="selectedFlowRuns" disabled />
+      </div>
     </div>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { PageHeadingFlowRuns, FlowRunsSort, FlowRunSortValues, FlowRunList, FlowRunsScatterPlot, StateSelect, StateType, DeploymentCombobox, FlowCombobox, useFlowRunFilter } from '@prefecthq/orion-design'
+  import { PageHeadingFlowRuns, FlowRunsSort, FlowRunSortValues, FlowRunList, FlowRunsScatterPlot, StateSelect, StateType, DeploymentCombobox, FlowCombobox, useFlowRunFilter, SearchInput } from '@prefecthq/orion-design'
   import { PTagsInput, PDateInput } from '@prefecthq/prefect-design'
   import { useRouteQueryParam, useSubscription } from '@prefecthq/vue-compositions'
-  import { endOfWeek, startOfWeek } from 'date-fns'
-  import { debounce } from 'lodash'
+  import { addDays, endOfToday, startOfToday, subDays } from 'date-fns'
+  import debounce from 'lodash.debounce'
   import { computed, Ref, ref } from 'vue'
   import { flowRunsApi } from '@/services/flowRunsApi'
   import { UiApi } from '@/services/uiApi'
 
   const sort = ref<FlowRunSortValues>('EXPECTED_START_TIME_DESC')
 
-  const startDateParam = useRouteQueryParam('start-date', startOfWeek(new Date()).toISOString())
+  const defaultStartDate = subDays(startOfToday(), 7)
+  const startDateParam = useRouteQueryParam('start-date', defaultStartDate.toISOString())
 
   const startDate = computed({
     get() {
@@ -57,7 +60,8 @@
     },
   })
 
-  const endDateParam = useRouteQueryParam('end-date', endOfWeek(new Date()).toISOString())
+  const defaultEndDate = addDays(endOfToday(), 1)
+  const endDateParam = useRouteQueryParam('end-date', defaultEndDate.toISOString())
 
   const endDate = computed({
     get() {
@@ -68,26 +72,25 @@
     },
   })
 
-  const updatedInput = ref(null)
+  const flowRunSearchTerm = ref<string>('')
   const flowRunSearchInput = computed({
     get() {
-      return updatedInput.value ?? null
+      return flowRunSearchTerm.value ?? null
     },
-    set(value) {
-      updateInput(value)
+    set(value: string) {
+      updateFlowRunSearchTerm(value)
     },
   })
 
-  const updateInput = debounce((value)=> {
-    updatedInput.value = value
-  }, 1200,
-  )
+  const updateFlowRunSearchTerm = debounce((value: string)=> {
+    flowRunSearchTerm.value = value
+  }, 1200)
 
   const states = useRouteQueryParam('state', []) as Ref<StateType[]>
   const deployments = useRouteQueryParam('deployment', [])
   const flows = useRouteQueryParam('flow', [])
   const tags = useRouteQueryParam('tag', [])
-  const filter = useFlowRunFilter({ states, deployments, flows, tags, startDate, endDate, sort })
+  const filter = useFlowRunFilter({ states, deployments, flows, tags, startDate, endDate, sort, name: flowRunSearchTerm })
 
   const subscriptionOptions = {
     interval: 30000,
@@ -103,6 +106,6 @@
 
 <style>
 .flow-runs--sort-search {
-  @apply flex justify-end gap-x-2
+  @apply flex justify-end gap-1
 }
 </style>
