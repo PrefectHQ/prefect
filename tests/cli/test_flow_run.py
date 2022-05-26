748367cf312703deb50d8df4f28731dc3155220f
peytonrunyan <peytonrunyan@gmail.com>
2022-05-26 17:40:20 -0400

    update tests part 1

--- tests/cli/test_flow_run.py
+++ tests/cli/test_flow_run.py
@@ -1,29 +1,19 @@
-import asyncio
-import pdb
-import time
-
-from prefect import flow
-from prefect.flow_runners import UniversalFlowRunner
 from prefect.testing.cli import invoke_and_assert
 
 
 def test_delete_flow_run_fails_correctly():
-    UUID_404_input = "ccb86ed0-e824-4d8b-b825-880401320e41"
-    UUID_404_output = "Flow run ccb86ed0-e824-4d8b-b825-880401320e41 not found!"
+    missing_flow_run_id = "ccb86ed0-e824-4d8b-b825-880401320e41"
     invoke_and_assert(
-        command=["flow-run", "delete", UUID_404_input],
-        expected_output=UUID_404_output,
+        command=["flow-run", "delete", missing_flow_run_id],
+        expected_output=f"Flow run '{missing_flow_run_id}' not found!",
         expected_code=1,
     )
 
 
 def test_delete_flow_run_succeeds(flow_run):
 
-    flow_id_string = str(flow_run.id)
-    print(f"flow run: {flow_run!r}")
-    good_output = f"Successfully deleted flow run {flow_id_string}."
     invoke_and_assert(
-        command=["flow-run", "delete", flow_id_string],
-        expected_output=good_output,
+        command=["flow-run", "delete", str(flow_run.id)],
+        expected_output=f"Successfully deleted flow run '{str(flow_run.id)}'.",
         expected_code=0,
     )
