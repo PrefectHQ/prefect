7bc07dc88e978a9c5881b70d8bcb58eb21c3bf72
Craig Harshbarger <pleek91@gmail.com>
2022-05-26 16:26:18 -0500

    lint

--- orion-ui/src/pages/Flows.vue
+++ orion-ui/src/pages/Flows.vue
@@ -1,8 +1,8 @@
 <template>
   <p-layout-default class="flows">
-    <template #header> 
-      Flows 
-      </template>
+    <template #header>
+      Flows
+    </template>
     <SearchInput v-model="flowSearchInput" />
     <div v-for="flow in filteredFlowList" :key="flow.id">
       {{ flow }}
@@ -11,23 +11,23 @@
 </template>
 
 <script lang="ts" setup>
-  import { FlowSearch, Flow, SearchInput } from "@prefecthq/orion-design";
-  import { useSubscription } from "@prefecthq/vue-compositions";
-  import { computed, ref } from "vue";
-  import { flowsApi } from "@/services/flowsApi";
+  import { FlowSearch, Flow, SearchInput } from '@prefecthq/orion-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed, ref } from 'vue'
+  import { flowsApi } from '@/services/flowsApi'
 
   const filter = {}
-  const subscriptionOptions = {interval: 30000}
-  const flowsSubscription = useSubscription(flowsApi.getFlows,[filter],subscriptionOptions)
+  const subscriptionOptions = { interval: 30000 }
+  const flowsSubscription = useSubscription(flowsApi.getFlows, [filter], subscriptionOptions)
   const flows = computed<Flow[]>(()=> flowsSubscription.response ?? [])
   const flowSearchInput = ref('')
   const filteredFlowList = computed(()=> fuzzyFilterFunction(flows.value, flowSearchInput.value))
 
-  const fuzzyFilterFunction=(array: Flow[], text: string) => array.reduce<Flow[]>(
+  const fuzzyFilterFunction = (array: Flow[], text: string): Flow[] => array.reduce<Flow[]>(
     (previous, current) => {
-    if (current.name.toLowerCase().includes(text.toLowerCase())) {
-      previous.push(current)
-    }
-    return previous
-    },[])
+      if (current.name.toLowerCase().includes(text.toLowerCase())) {
+        previous.push(current)
+      }
+      return previous
+    }, [])
 </script>
