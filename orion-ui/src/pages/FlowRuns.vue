657e71f2f1bfaf2704a71e1b99b0e98b9ef7164a
Craig Harshbarger <pleek91@gmail.com>
2022-05-26 13:41:30 -0500

    Add filters

--- orion-ui/src/pages/FlowRuns.vue
+++ orion-ui/src/pages/FlowRuns.vue
@@ -1,54 +1,77 @@
 <template>
   <p-layout-default class="flow-runs">
     <template #header>
-      <div>
-        Flow runs
-      </div>
+      <PageHeadingFlowRuns />
     </template>
 
-    <div class="grid gap-1 grid-cols-3">
-      <FlowCombobox v-model:selected="flows" empty-message="All flows" />
-      <DeploymentCombobox v-model:selected="deployments" empty-message="All deployments" />
-      <StateSelect v-model:selected="states" empty-message="All run states" />
-    </div>
+    <div class="grid gap-3">
+      <div class="grid gap-1">
+        <div class="grid gap-1 grid-cols-2">
+          <p-label label="Start Date">
+            <PDateInput v-model="startDate" />
+          </p-label>
+          <p-label label="End Date">
+            <PDateInput v-model="endDate" />
+          </p-label>
+        </div>
+        <div class="grid gap-1 grid-cols-4">
+          <FlowCombobox v-model:selected="flows" empty-message="All flows" />
+          <DeploymentCombobox v-model:selected="deployments" empty-message="All deployments" />
+          <PTagsInput v-model:tags="tags" empty-message="All Tags" inline />
+          <StateSelect v-model:selected="states" empty-message="All run states" />
+        </div>
+      </div>
 
-    <div>
-      Flow Run History
-    </div>
-    <FlowRunsScatterPlot :history="flowRunHistory" style="height: 275px" />
+      <FlowRunsScatterPlot :history="flowRunHistory" style="height: 275px" />
 
-    <div>
-      Flow Run List
-    </div>
-    <div class="flow-runs--sort-search">
-      <FlowRunsSort v-model="selectedSortOption" />
-    </div>
+      <div class="flow-runs--sort-search">
+        <FlowRunsSort v-model="sort" />
+      </div>
 
-    <FlowRunList :flow-runs="flowRuns" :selected="selectedFlowRuns" disabled />
+      <FlowRunList :flow-runs="flowRuns" :selected="selectedFlowRuns" disabled />
+    </div>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { UnionFilters, FlowRunsSort, FlowRunSortValues, FlowRunList, FlowRunsScatterPlot, StateSelect, StateType, DeploymentCombobox, FlowCombobox } from '@prefecthq/orion-design'
-  import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed, ref } from 'vue'
+  import { PageHeadingFlowRuns, FlowRunsSort, FlowRunSortValues, FlowRunList, FlowRunsScatterPlot, StateSelect, StateType, DeploymentCombobox, FlowCombobox, useFlowRunFilter } from '@prefecthq/orion-design'
+  import { PTagsInput, PDateInput } from '@prefecthq/prefect-design'
+  import { useRouteQueryParam, useSubscription } from '@prefecthq/vue-compositions'
+  import { endOfWeek, startOfWeek } from 'date-fns'
+  import { computed, Ref, ref } from 'vue'
   import { flowRunsApi } from '@/services/flowRunsApi'
   import { UiApi } from '@/services/uiApi'
 
+  const sort = ref<FlowRunSortValues>('EXPECTED_START_TIME_DESC')
 
-  const flowRunsOffset = ref<number>(0)
-  const flowRunsLimit = ref<number>(100)
-  const selectedSortOption = ref<FlowRunSortValues>('EXPECTED_START_TIME_DESC')
+  const startDateParam = useRouteQueryParam('start-date', startOfWeek(new Date()).toISOString())
 
-  const flowRunsFilter = computed<UnionFilters>(() => {
-    return {
-      offset: flowRunsOffset.value,
-      limit: flowRunsLimit.value,
-      sort: selectedSortOption.value,
-    }
+  const startDate = computed({
+    get() {
+      return new Date(startDateParam.value)
+    },
+    set(value: Date) {
+      startDateParam.value = value.toISOString()
+    },
   })
 
-  const filter = {}
+  const endDateParam = useRouteQueryParam('end-date', endOfWeek(new Date()).toISOString())
+
+  const endDate = computed({
+    get() {
+      return new Date(endDateParam.value)
+    },
+    set(value: Date) {
+      endDateParam.value = value.toISOString()
+    },
+  })
+
+  const states = useRouteQueryParam('state', []) as Ref<StateType[]>
+  const deployments = useRouteQueryParam('deployment', [])
+  const flows = useRouteQueryParam('flow', [])
+  const tags = useRouteQueryParam('tag', [])
+  const filter = useFlowRunFilter({ states, deployments, flows, tags, startDate, endDate, sort })
+
   const subscriptionOptions = {
     interval: 30000,
   }
@@ -56,13 +79,9 @@
   const flowRunHistorySubscription = useSubscription(UiApi.getFlowRunHistory, [filter], subscriptionOptions)
   const flowRunHistory = computed(() => flowRunHistorySubscription.response ?? [])
 
-  const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [flowRunsFilter], subscriptionOptions)
+  const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [filter], subscriptionOptions)
   const flowRuns = computed(()=> flowRunsSubscription.response ?? [])
   const selectedFlowRuns = ref([])
-
-  const states = ref<StateType[]>([])
-  const deployments = ref<string[]>([])
-  const flows = ref<string[]>([])
 </script>
 
 <style>
