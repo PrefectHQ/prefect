042a8ab710240f03604ad01fa2be530e148b211c
Natalie Smith <thatgalnatalie@gmail.com>
2022-05-26 20:43:53 -0700

    Update Queue.vue

--- orion-ui/src/pages/Queue.vue
+++ orion-ui/src/pages/Queue.vue
@@ -4,67 +4,97 @@
       Queue {{ workQueueId }}
     </template>
 
-    <div>
-      Queue Details
-    </div>
-    <div>
-      {{ workQueueDetails }}
+    <div class="mb-2">
+      <p-key-value label="Description" :value="workQueueDescription" />
     </div>
 
-    <p-key-value label="Description" :value="workQueueDescription" />
-
-    <p-key-value label="Work Queue ID" :value="workQueueID" />
-
-    <p-key-value label="Flow Run Concurrency" :value="workQueueFlowRunConcurrency" />
-
-    <p-key-value label="Created" :value="workQueueCreated" />
-
-    <div>
-      Filters
+    <div class="mb-2">
+      <p-key-value label="Work Queue ID" :value="workQueueID" />
     </div>
 
-    <p-key-value label="Tags">
-      <template #value>
-        <p-tag v-for="tag in workQueueDetails?.filter.tags" :key="tag">
-          {{ tag }}
-        </p-tag>
-      </template>
-    </p-key-value>
+    <div class="mb-2">
+      <p-key-value label="Flow Run Concurrency" :value="workQueueFlowRunConcurrency" />
+    </div>
 
-    <p-key-value label="Deployments">
-      <template #value>
-        <DeploymentsTable :deployments="workQueueDeployments" />
-      </template>
-    </p-key-value>
+    <div class="mb-2">
+      <p-key-value label="Created" :value="workQueueCreated" />
+    </div>
 
-    <p-key-value label="Flow Runners">
-      <template #value>
-        <p-checkbox v-for="runners in workQueueDetails?.filter.flowRunnerTypes" :key="runners" v-model="flowRunners" :label="runners" />
-      </template>
-    </p-key-value>
+    <div class="mb-2">
+      <p-key-value label="Tags">
+        <template #value>
+          <p-tag v-for="tag in workQueueTags" :key="tag">
+            {{ tag }}
+          </p-tag>
+        </template>
+      </p-key-value>
+    </div>
+
+    <div class="mb-2">
+      <p-key-value label="Deployments">
+        <template #value>
+          <div v-for="deployment in workQueueDeployments" :key="deployment.id">
+            <router-link :to="`/deployment/${deployment.id}`">
+              {{ deployment.name }}
+            </router-link>
+          </div>
+        </template>
+      </p-key-value>
+    </div>
+
+    <div class="mb-2">
+      <p-key-value label="Flow Runners">
+        <template #value>
+          <p-checkbox
+            v-for="runner in flowRunnerTypes"
+            :key="runner.value"
+            v-model="flowRunners"
+            :label="runner.label"
+            :value="runner.value"
+            editor="checkbox"
+          />
+        </template>
+      </p-key-value>
+    </div>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { useRouteParam,  UnionFilters, DeploymentsTable } from '@prefecthq/orion-design'
+  import { useRouteParam, UnionFilters, FlowRunnerType, formatDate } from '@prefecthq/orion-design'
   import { PKeyValue } from '@prefecthq/prefect-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, ref } from 'vue'
   import { deploymentsApi } from '@/services/deploymentsApi'
   import { workQueuesApi } from '@/services/workQueuesApi'
-  const flowRunners = ref([])
+
   const workQueueId = useRouteParam('id')
   const subscriptionOptions = {
     interval: 300000,
   }
+
+  const flowRunnerTypes: { label: string, value: FlowRunnerType }[] = [
+    { label: 'Universal', value: 'universal' },
+    { label: 'Kubernetes', value: 'kubernetes' },
+    { label: 'Docker', value: 'docker' },
+    { label: 'Subprocess', value: 'subprocess' },
+  ]
+
   const workQueueSubscription = useSubscription(workQueuesApi.getWorkQueue, [workQueueId.value], subscriptionOptions)
   const workQueueDetails = computed(() => workQueueSubscription.response)
   const workQueueDeploymentIds = computed(() => workQueueDetails?.value?.filter?.deploymentIds)
-
   const workQueueDescription = computed(() => workQueueDetails.value?.description ?? '')
   const workQueueID = computed(() => workQueueDetails.value?.id ?? '')
-  const workQueueFlowRunConcurrency = computed(() => workQueueDetails.value?.concurrencyLimit ?? 0)
-  const workQueueCreated = computed(() => String(workQueueDetails.value?.created) ?? '')
+  const workQueueFlowRunConcurrency = computed(() => workQueueDetails.value?.concurrencyLimit ?? 'Unlimited')
+  const workQueueCreated = computed(() => {
+    if (workQueueDetails.value?.created) {
+      return formatDate(workQueueDetails.value?.created)
+    }
+    return ''
+  })
+  const workQueueFlowRunners = computed(() => workQueueDetails.value?.filter?.flowRunnerTypes ?? [])
+  const workQueueTags = computed(() => workQueueDetails.value?.filter.tags ?? [])
+
+  const flowRunners = ref(workQueueFlowRunners.value)
 
   const workQueueDeploymentFilter = computed<UnionFilters>(() => ({
     deployments: {
