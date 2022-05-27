a199b5dfe0cb5830f8a8175409880ebddc89d380
Marichka Offen <marichka@prefect.io>
2022-05-27 17:16:05 -0400

    Add tables

--- orion-ui/src/pages/Flows.vue
+++ orion-ui/src/pages/Flows.vue
@@ -4,14 +4,12 @@
       Flows
     </template>
     <SearchInput v-model="flowSearchInput" />
-    <div v-for="flow in filteredFlowList" :key="flow.id">
-      {{ flow }}
-    </div>
+    <FlowsTable :flows="filteredFlowList" @delete="flowsSubscription.refresh()" />
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { FlowSearch, Flow, SearchInput } from '@prefecthq/orion-design'
+  import { FlowSearch, Flow, SearchInput, FlowsTable } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, ref } from 'vue'
   import { flowsApi } from '@/services/flowsApi'
@@ -20,6 +18,7 @@
   const subscriptionOptions = { interval: 30000 }
   const flowsSubscription = useSubscription(flowsApi.getFlows, [filter], subscriptionOptions)
   const flows = computed<Flow[]>(()=> flowsSubscription.response ?? [])
+  console.log(flows)
   const flowSearchInput = ref('')
   const filteredFlowList = computed(()=> fuzzyFilterFunction(flows.value, flowSearchInput.value))
 
