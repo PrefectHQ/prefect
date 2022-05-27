e0792fc3eade1766a2b561b4ccd00c2970382904
Jennifer Grange <jenny@prefect.io>
2022-05-27 08:57:10 -0400

    add search

--- orion-ui/src/pages/Deployments.vue
+++ orion-ui/src/pages/Deployments.vue
@@ -4,15 +4,17 @@
       Deployments
     </template>
 
-    <div v-for="deployment in deployments" :key="deployment.id">
+    <SearchInput v-model="deploymentSearchInput" placeholder="Search..." label="Search by flow or deployment name" />
+    <div v-for="deployment in filteredDeployments" :key="deployment.id" class="mb-4">
       {{ deployment }}
     </div>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
+  import { SearchInput, Deployment } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { computed, ref } from 'vue'
   import { deploymentsApi } from '@/services/deploymentsApi'
 
   const filter = {}
@@ -21,4 +23,14 @@
   }
   const deploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [filter], subscriptionOptions)
   const deployments = computed(() => deploymentsSubscription.response ?? [])
+  const deploymentSearchInput = ref('')
+  const filteredDeployments = computed(()=> fuzzyFilterFunction(deployments.value, deploymentSearchInput.value))
+
+  const fuzzyFilterFunction = (array: Deployment[], text: string): Deployment[] => array.reduce<Deployment[]>(
+    (previous, current) => {
+      if (current.name.toLowerCase().includes(text.toLowerCase())) {
+        previous.push(current)
+      }
+      return previous
+    }, [])
 </script>
