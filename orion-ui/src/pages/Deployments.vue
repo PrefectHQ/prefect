36725a0409e4d559ab4c6986c2f56920c12bf66e
Craig Harshbarger <pleek91@gmail.com>
2022-05-31 14:01:04 -0500

    Update the deployments page with empty state and page heading

--- orion-ui/src/pages/Deployments.vue
+++ orion-ui/src/pages/Deployments.vue
@@ -1,16 +1,21 @@
 <template>
   <p-layout-default class="deployments">
     <template #header>
-      Deployments
+      <PageHeadingDeployments />
     </template>
 
-    <SearchInput v-model="deploymentSearchInput" placeholder="Search deployments" label="Search by flow or deployment name" />
-    <DeploymentsTable :deployments="filteredDeployments" @delete="deploymentsSubscription.refresh()" />
+    <template v-if="empty">
+      <DeploymentsPageEmptyState />
+    </template>
+    <template v-else>
+      <SearchInput v-model="deploymentSearchInput" placeholder="Search deployments" label="Search by flow or deployment name" />
+      <DeploymentsTable :deployments="filteredDeployments" @delete="deploymentsSubscription.refresh()" />
+    </template>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { SearchInput, Deployment, DeploymentsTable } from '@prefecthq/orion-design'
+  import { SearchInput, Deployment, DeploymentsTable, PageHeadingDeployments, DeploymentsPageEmptyState } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, ref } from 'vue'
   import { deploymentsApi } from '@/services/deploymentsApi'
@@ -22,13 +27,14 @@
   const deploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [filter], subscriptionOptions)
   const deployments = computed(() => deploymentsSubscription.response ?? [])
   const deploymentSearchInput = ref('')
-  const filteredDeployments = computed(()=> fuzzyFilterFunction(deployments.value, deploymentSearchInput.value))
+  const filteredDeployments = computed(()=> search(deployments.value, deploymentSearchInput.value))
+  const empty = computed(() => deploymentsSubscription.executed && deployments.value.length === 0)
 
-  const fuzzyFilterFunction = (array: Deployment[], text: string): Deployment[] => array.reduce<Deployment[]>(
-    (previous, current) => {
-      if (current.name.toLowerCase().includes(text.toLowerCase())) {
-        previous.push(current)
-      }
-      return previous
-    }, [])
+  const search = (array: Deployment[], text: string): Deployment[] => array.reduce<Deployment[]>((previous, current) => {
+    if (current.name.toLowerCase().includes(text.toLowerCase())) {
+      previous.push(current)
+    }
+
+    return previous
+  }, [])
 </script>
