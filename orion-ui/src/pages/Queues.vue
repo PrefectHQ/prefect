5138cfac84d9276b4788079e11ca201362a487de
Craig Harshbarger <pleek91@gmail.com>
2022-05-31 15:32:17 -0500

    Make sure we don't flash the table and then show the empty state

--- orion-ui/src/pages/Queues.vue
+++ orion-ui/src/pages/Queues.vue
@@ -3,12 +3,15 @@
     <template #header>
       <PageHeadingQueues />
     </template>
-    <template v-if="empty">
-      <WorkQueuesPageEmptyState />
-    </template>
-    <template v-else>
-      <SearchInput v-model="searchInput" />
-      <QueuesTable :queues="filteredQueues" @delete="queuesSubscription.refresh()" @clear="clear" />
+
+    <template v-if="loaded">
+      <template v-if="empty">
+        <WorkQueuesPageEmptyState />
+      </template>
+      <template v-else>
+        <SearchInput v-model="searchInput" />
+        <QueuesTable :queues="filteredQueues" @delete="queuesSubscription.refresh()" @clear="clear" />
+      </template>
     </template>
   </p-layout-default>
 </template>
@@ -26,6 +29,7 @@
   const queuesSubscription = useSubscription(workQueuesApi.getWorkQueues, [filter], subscriptionOptions)
   const queues = computed(() => queuesSubscription.response ?? [])
   const empty = computed(() => queuesSubscription.executed && queues.value.length == 0)
+  const loaded = computed(() => queuesSubscription.executed)
   const searchInput = ref('')
   const filteredQueues = computed(() => search(queues.value, searchInput.value))
 
