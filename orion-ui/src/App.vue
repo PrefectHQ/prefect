3f532bdae1026a5c3f26ea955f0cdc1762a4fbe1
Craig Harshbarger <pleek91@gmail.com>
2022-05-31 13:42:25 -0500

    Get the mobile menu working

--- orion-ui/src/App.vue
+++ orion-ui/src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="app" data-teleport-target="app">
-    <ContextSidebar />
+    <template v-if="!media.lg">
+      <PGlobalSidebar class="app__mobile-menu">
+        <template #upper-links>
+          <p-icon icon="PrefectGradient" class="app__prefect-icon" />
+        </template>
+        <template #bottom-links>
+          <PIcon icon="MenuIcon" class="app__menu-icon" @click="toggle" />
+        </template>
+      </PGlobalSidebar>
+    </template>
+    <ContextSidebar v-if="showMenu" class="app__sidebar" @click="close" />
     <suspense>
-      <router-view class="application__router-view" />
+      <router-view class="app__router-view" />
     </suspense>
   </div>
 </template>
@@ -26,9 +36,11 @@
     flowRunRouteKey,
     flowRouteKey,
     deploymentRouteKey,
-    workQueueRouteKey
+    workQueueRouteKey,
+    media
   } from '@prefecthq/orion-design'
-  import { provide } from 'vue'
+  import { PGlobalSidebar, PIcon } from '@prefecthq/prefect-design'
+  import { computed, provide, ref, watchEffect } from 'vue'
   import { routes } from '@/router/routes'
   import { deploymentsApi } from '@/services/deploymentsApi'
   import { flowRunsApi } from '@/services/flowRunsApi'
@@ -56,12 +68,51 @@
   provide(flowRouteKey, routes.flow)
   provide(deploymentRouteKey, routes.deployment)
   provide(workQueueRouteKey, routes.queue)
+
+  const mobileMenuOpen = ref(false)
+  const showMenu = computed(() => media.lg || mobileMenuOpen.value)
+
+  function toggle(): void {
+    mobileMenuOpen.value = !mobileMenuOpen.value
+  }
+
+  function close(): void {
+    mobileMenuOpen.value = false
+  }
+
+  watchEffect(() => document.body.classList.toggle('body-scrolling-disabled', showMenu.value && !media.lg))
 </script>
 
 <style>
+.body-scrolling-disabled { @apply
+  overflow-hidden
+}
+
 .app { @apply
   text-slate-900;
-  display: grid;
-  grid-template-columns: max-content minmax(0, 1fr);
+}
+
+.app__prefect-icon { @apply
+  w-6
+  h-6
+}
+
+.app__menu-icon { @apply
+  text-white
+  w-6
+  h-6
+  cursor-pointer
+}
+
+.app__router-view { @apply
+  relative
+  z-0
+}
+
+@screen lg {
+  .app {
+    display: grid;
+    grid-template-columns: max-content minmax(0, 1fr);
+  }
 }
 </style>
