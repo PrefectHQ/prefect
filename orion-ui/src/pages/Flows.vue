035b8c308dba1de58f65d0d910d82fb9d86440b2
Craig Harshbarger <pleek91@gmail.com>
2022-05-31 13:59:08 -0500

    Update the flows page to have its header and empty state

--- orion-ui/src/pages/Flows.vue
+++ orion-ui/src/pages/Flows.vue
@@ -1,15 +1,20 @@
 <template>
   <p-layout-default class="flows">
     <template #header>
-      Flows
+      <PageHeadingFlows />
     </template>
-    <SearchInput v-model="flowSearchInput" placeholder="Search flows" label="Search by flow name" />
-    <FlowsTable :flows="filteredFlowList" @delete="flowsSubscription.refresh()" />
+    <template v-if="empty">
+      <FlowsPageEmptyState />
+    </template>
+    <template v-else>
+      <SearchInput v-model="flowSearchInput" placeholder="Search flows" label="Search by flow name" />
+      <FlowsTable :flows="filteredFlowList" @delete="flowsSubscription.refresh()" />
+    </template>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { Flow, SearchInput, FlowsTable } from '@prefecthq/orion-design'
+  import { Flow, SearchInput, FlowsTable, FlowsPageEmptyState, PageHeadingFlows } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed, ref } from 'vue'
   import { flowsApi } from '@/services/flowsApi'
@@ -17,15 +22,16 @@
   const filter = {}
   const subscriptionOptions = { interval: 30000 }
   const flowsSubscription = useSubscription(flowsApi.getFlows, [filter], subscriptionOptions)
-  const flows = computed<Flow[]>(()=> flowsSubscription.response ?? [])
+  const flows = computed<Flow[]>(() => flowsSubscription.response ?? [])
+  const empty = computed(() => flowsSubscription.executed && flows.value.length === 0)
   const flowSearchInput = ref('')
-  const filteredFlowList = computed(()=> fuzzyFilterFunction(flows.value, flowSearchInput.value))
+  const filteredFlowList = computed(()=> search(flows.value, flowSearchInput.value))
 
-  const fuzzyFilterFunction = (array: Flow[], text: string): Flow[] => array.reduce<Flow[]>(
-    (previous, current) => {
-      if (current.name.toLowerCase().includes(text.toLowerCase())) {
-        previous.push(current)
-      }
-      return previous
-    }, [])
+  const search = (array: Flow[], text: string): Flow[] => array.reduce<Flow[]>((previous, current) => {
+    if (current.name.toLowerCase().includes(text.toLowerCase())) {
+      previous.push(current)
+    }
+
+    return previous
+  }, [])
 </script>
