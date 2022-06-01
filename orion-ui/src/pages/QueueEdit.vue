a2c200df706bdcf2a0cecf71beedda0a0c2f4106
Marichka Offen <marichka@prefect.io>
2022-06-01 18:03:13 -0400

    Add updateQueue method

--- orion-ui/src/pages/QueueEdit.vue
+++ orion-ui/src/pages/QueueEdit.vue
@@ -7,25 +7,29 @@
       Fill out the details below.
     </p>
 
-    <WorkQueueForm :work-queue="workQueueDetails" @submit="submit" />
+    <WorkQueueForm :work-queue="workQueueDetails" @submit="updateQueue" />
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { WorkQueueForm, useRouteParam, WorkQueue } from '@prefecthq/orion-design'
+  import { WorkQueueForm, useRouteParam } from '@prefecthq/orion-design'
+  import { showToast } from '@prefecthq/prefect-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { workQueuesApi } from '@/services/workQueuesApi'
 
   const workQueueId = useRouteParam('id')
-  const subscriptionOptions = {
-    interval: 300000,
-  }
 
-  const workQueueSubscription = useSubscription(workQueuesApi.getWorkQueue, [workQueueId.value], subscriptionOptions)
+  const workQueueSubscription = useSubscription(workQueuesApi.getWorkQueue, [workQueueId.value])
   const workQueueDetails = computed(() => workQueueSubscription.response)
 
-  const submit = (workQueue: Partial<WorkQueue>): void => {
-    console.log('submitted', workQueue)
+  const updateQueue = async (workQueue: any): Promise<void> => {
+    try {
+      await workQueuesApi.updateWorkQueue(workQueueId.value, workQueue)
+      showToast(`Work queue ${workQueueDetails.value?.name} has been updated`, 'success', undefined, 3000)
+    } catch (error) {
+      showToast('Error occurred while updating your queue', 'error', undefined, 3000)
+      console.error(error)
+    }
   }
 </script>
