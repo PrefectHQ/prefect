dbd87ed8c6cf52e6018b9e40888fb2baa3bd6d19
nicholas <znicholasbrown@gmail.com>
2022-06-01 18:21:33 -0400

    Bump od v

--- orion-ui/src/router/index.ts
+++ orion-ui/src/router/index.ts
@@ -16,6 +16,11 @@ const routeRecords: AppRouteRecord[] = [
     component: FlowRunsPage,
   },
   {
+    name: 'radar',
+    path: '/flow-run/:id/radar',
+    component: (): RouteComponent => import('@/pages/FlowRunRadar.vue'),
+  },
+  {
     name: 'flow-run',
     path: '/flow-run/:id',
     component: (): RouteComponent => import('@/pages/FlowRun.vue'),
