fc2bc886ca248d7379c79e091b463ddcc964376a
Marichka Offen <marichka@prefect.io>
2022-06-02 11:42:58 -0400

    Setup CreateQueue page

--- orion-ui/src/pages/QueueCreate.vue
+++ orion-ui/src/pages/QueueCreate.vue
@@ -1,11 +1,33 @@
 <template>
-  <p-layout-default class="queue-create">
+  <p-layout-default>
     <template #header>
-      New Queue
+      <PageHeading :crumbs="header" />
     </template>
+
+    <WorkQueueForm @submit="createQueue" @cancel="goToQueues" />
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-//
+  import { WorkQueueForm, PageHeading } from '@prefecthq/orion-design'
+  import { showToast } from '@prefecthq/prefect-design'
+  import router, { routes } from '@/router'
+  import { workQueuesApi } from '@/services/workQueuesApi'
+
+  const header = [{ text: 'Create Work Queue' }]
+
+  const goToQueues = (): void => {
+    router.push(routes.queues())
+  }
+
+  const createQueue = async (workQueue: any): Promise<void> => {
+    try {
+      await workQueuesApi.createWorkQueue(workQueue)
+      showToast('Work queue has been created', 'success', undefined, 3000)
+      goToQueues()
+    } catch (error) {
+      showToast('Error occurred while creating new queue', 'error', undefined, 3000)
+      console.error(error)
+    }
+  }
 </script>
