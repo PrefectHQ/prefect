c15d6c6f26c9a783e1224cce883e014707b451bb
Craig Harshbarger <pleek91@gmail.com>
2022-06-02 22:16:44 -0500

    Updates from orion-design

--- orion-ui/src/pages/Queues.vue
+++ orion-ui/src/pages/Queues.vue
@@ -8,40 +8,26 @@
       <template v-if="empty">
         <WorkQueuesPageEmptyState />
       </template>
+
       <template v-else>
-        <SearchInput v-model="searchInput" />
-        <QueuesTable :queues="filteredQueues" @delete="queuesSubscription.refresh()" @clear="clear" />
+        <WorkQueuesTable :work-queues="workQueues" @delete="workQueuesSubscription.refresh()" />
       </template>
     </template>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { WorkQueue, QueuesTable, PageHeadingQueues, WorkQueuesPageEmptyState } from '@prefecthq/orion-design'
+  import { WorkQueuesTable, PageHeadingQueues, WorkQueuesPageEmptyState } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
-  import { computed, ref } from 'vue'
+  import { computed } from 'vue'
   import { workQueuesApi } from '@/services/workQueuesApi'
 
-  const filter = {}
   const subscriptionOptions = {
     interval: 30000,
   }
-  const queuesSubscription = useSubscription(workQueuesApi.getWorkQueues, [filter], subscriptionOptions)
-  const queues = computed(() => queuesSubscription.response ?? [])
-  const empty = computed(() => queuesSubscription.executed && queues.value.length == 0)
-  const loaded = computed(() => queuesSubscription.executed)
-  const searchInput = ref('')
-  const filteredQueues = computed(() => search(queues.value, searchInput.value))
 
-  const search = (array: WorkQueue[], text: string): WorkQueue[] => array.reduce<WorkQueue[]>((previous, current) => {
-    if (current.name.toLowerCase().includes(text.toLowerCase())) {
-      previous.push(current)
-    }
-
-    return previous
-  }, [])
-
-  const clear = (): void => {
-    searchInput.value = ''
-  }
+  const workQueuesSubscription = useSubscription(workQueuesApi.getWorkQueues, [{}], subscriptionOptions)
+  const workQueues = computed(() => workQueuesSubscription.response ?? [])
+  const empty = computed(() => workQueuesSubscription.executed && workQueues.value.length == 0)
+  const loaded = computed(() => workQueuesSubscription.executed)
 </script>
