3cf670ffa979d0906af28154afdabdaf9bbd3ad6
Craig Harshbarger <pleek91@gmail.com>
2022-06-15 14:40:57 -0500

    Block pages, routes, and services

---
+++ orion-ui/src/pages/BlocksCatalogCreate.vue
@@ -0,0 +1,16 @@
+<template>
+  <p-layout-default v-if="blockType" class="blocks-catalog-create">
+    <PageHeadingBlocksCatalogCreate :block-type="blockType" />
+  </p-layout-default>
+</template>
+
+<script lang="ts" setup>
+  import { PageHeadingBlocksCatalogCreate, useRouteParam } from '@prefecthq/orion-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { blockTypesApi } from '@/services/blockTypesApi'
+
+  const blockTypeName = useRouteParam('blockTypeName')
+  const blockTypeSubscription = useSubscription(blockTypesApi.getBlockTypeByName, [blockTypeName])
+  const blockType = computed(() => blockTypeSubscription.response)
+</script>
