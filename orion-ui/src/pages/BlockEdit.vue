0b163572f57274cac3e212845b23db8aee0f5e8d
Craig Harshbarger <pleek91@gmail.com>
2022-06-15 14:40:57 -0500

    Block pages, routes, and services

---
+++ orion-ui/src/pages/BlockEdit.vue
@@ -0,0 +1,16 @@
+<template>
+  <p-layout-default v-if="blockDocument" class="block-edit">
+    <PageHeadingBlockEdit :block-document="blockDocument" />
+  </p-layout-default>
+</template>
+
+<script lang="ts" setup>
+  import { PageHeadingBlockEdit, useRouteParam } from '@prefecthq/orion-design'
+  import { useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { blockDocumentsApi } from '@/services/blockDocumentsApi'
+
+  const blockDocumentId = useRouteParam('blockDocumentId')
+  const blockDocumentSubscription = useSubscription(blockDocumentsApi.getBlockDocument, [blockDocumentId])
+  const blockDocument = computed(() => blockDocumentSubscription.response)
+</script>
---
+++ orion-ui/src/pages/BlocksCatalog.vue
@@ -0,0 +1,9 @@
+<template>
+  <p-layout-default class="blocks-catalog">
+    <PageHeadingBlocksCatalog />
+  </p-layout-default>
+</template>
+
+<script lang="ts" setup>
+  import { PageHeadingBlocksCatalog } from '@prefecthq/orion-design'
+</script>
