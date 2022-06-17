060e325121e3ebcfa926a78e9c8f2f4e7229f6ae
Craig Harshbarger <pleek91@gmail.com>
2022-06-17 15:20:28 -0500

    Layout and hook up the blocks catalog and the create page

--- orion-ui/src/pages/BlocksCatalogCreate.vue
+++ orion-ui/src/pages/BlocksCatalogCreate.vue
@@ -1,16 +1,19 @@
 <template>
   <p-layout-default v-if="blockType" class="blocks-catalog-create">
-    <PageHeadingBlocksCatalogCreate :block-type="blockType" />
+    <template #header>
+      <PageHeadingBlocksCatalogCreate :block-type="blockType" />
+    </template>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
-  import { PageHeadingBlocksCatalogCreate, useRouteParam } from '@prefecthq/orion-design'
+  import { PageHeadingBlocksCatalogCreate, useRouteParam, titleCase } from '@prefecthq/orion-design'
   import { useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { blockTypesApi } from '@/services/blockTypesApi'
 
-  const blockTypeName = useRouteParam('blockTypeName')
+  const blockTypeNameParam = useRouteParam('blockTypeName')
+  const blockTypeName = computed(() => titleCase(blockTypeNameParam.value))
   const blockTypeSubscription = useSubscription(blockTypesApi.getBlockTypeByName, [blockTypeName])
   const blockType = computed(() => blockTypeSubscription.response)
 </script>
