9155d575fbef43cf6628c087f2c7181d9dbab409
Michael Adkins <michael@prefect.io>
2022-06-17 13:45:46 -0500

    Include conda in `PythonRequirements` class

--- src/prefect/software/base.py
+++ src/prefect/software/base.py
@@ -1,3 +1,6 @@
+from typing import Iterable, List
+
+
 class Requirement:
     # Implementations are expected to at least contain a name attribute
     name: str
@@ -24,3 +27,20 @@ class Requirement:
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({str(self)!r})"
+
+
+def remove_duplicate_requirements(
+    constant: Iterable[Requirement], to_deduplicate: Iterable[Requirement]
+) -> List[Requirement]:
+    """
+    Returns a list of requirements that excludes requirements already specified
+    in the first iterable.
+    """
+    constant_names = {req.name for req in constant}
+    return [req for req in to_deduplicate if req.name not in constant_names]
+
+
+def pop_requirement_by_name(
+    requirements: List[Requirement], name: str
+) -> Optional[Requirement]:
+    requirement = next()
