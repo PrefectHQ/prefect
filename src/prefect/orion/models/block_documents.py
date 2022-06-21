d79d8fa273182dcd182ce07f34e6c96ae711d1cd
Alexander Streed <alex.s@prefect.io>
2022-06-21 13:40:41 -0500

    Adds back default is anonymous filter

--- src/prefect/orion/models/block_documents.py
+++ src/prefect/orion/models/block_documents.py
@@ -15,6 +15,7 @@ from prefect.orion.database.interface import OrionDBInterface
 from prefect.orion.database.orm_models import ORMBlockDocument
 from prefect.orion.schemas.actions import BlockDocumentReferenceCreate
 from prefect.orion.schemas.core import BlockDocument, BlockDocumentReference
+from prefect.orion.schemas.filters import BlockDocumentFilterIsAnonymous
 from prefect.utilities.hashing import hash_objects
 
 
@@ -356,7 +357,9 @@ async def read_block_documents(
     """
     # if no filter is provided, one is created that excludes anonymous blocks
     if block_document_filter is None:
-        block_document_filter = schemas.filters.BlockDocumentFilter()
+        block_document_filter = schemas.filters.BlockDocumentFilter(
+            is_anonymous=BlockDocumentFilterIsAnonymous(eq_=False)
+        )
 
     filtered_block_documents_query = sa.select(db.BlockDocument.id).order_by(
         db.BlockDocument.name
