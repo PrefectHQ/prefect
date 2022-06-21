05186cc20e47e38072062c8ab4d73861624fd6cf
Craig Harshbarger <pleek91@gmail.com>
2022-06-21 09:20:14 -0500

    Fix the context sidebar missing

---
+++ orion-ui/src/components/ContextSidebar.vue
@@ -0,0 +1,17 @@
+<template>
+  <p-context-sidebar class="context-sidebar">
+    <p-context-nav-item title="Flow Runs" icon="FlowRun" :to="routes.flowRuns()" />
+    <p-context-nav-item title="Flows" icon="Flow" :to="routes.flows()" />
+    <p-context-nav-item title="Deployments" icon="LocationMarkerIcon" :to="routes.deployments()" />
+    <p-context-nav-item title="Work Queues" icon="DatabaseIcon" :to="routes.workQueues()" />
+
+    <template #footer>
+      <p-context-nav-item title="Settings" icon="CogIcon" :to="routes.settings()" />
+    </template>
+  </p-context-sidebar>
+</template>
+
+<script lang="ts" setup>
+  import { PContextSidebar, PContextNavItem } from '@prefecthq/prefect-design'
+  import { routes } from '@/router'
+</script>
