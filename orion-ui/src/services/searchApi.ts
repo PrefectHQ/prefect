22d61aa39052a58c62d30c46d294d7078b254a21
Craig Harshbarger <pleek91@gmail.com>
2022-06-21 09:14:44 -0500

    Delete searchapi

--- orion-ui/src/services/searchApi.ts
+++
@@ -1,7 +0,0 @@
-import { SearchApi as OrionDesignSearchApi } from '@prefecthq/orion-design'
-import { createActions } from '@prefecthq/vue-compositions'
-import { ApiRoute } from '@/mixins/ApiRoute'
-
-export class OrionSearchApi extends ApiRoute(OrionDesignSearchApi) {}
-
-export const searchApi = createActions(new OrionSearchApi())
