30299cdbc6dd3dd9f72e42f279cddb5491f936ac
Michael Adkins <michael@prefect.io>
2022-06-22 09:20:09 -0500

    Begin sketch of deployment/packager/manifest relationship

--- src/prefect/packaging/base.py
+++ src/prefect/packaging/base.py
@@ -1,26 +1,40 @@
 import abc
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Generic, TypeVar
 
 from pydantic import BaseModel
 
-from prefect.orion.schemas.actions import DeploymentCreate
-
 if TYPE_CHECKING:
-    from prefect.deployments import DeploymentSpec
+    from prefect.deployments import FlowSource
+
+D = TypeVar("D")
+
+
+class Serializer(BaseModel, Generic[D], abc.ABC):
+    """
+    A serializer that can encode objects of type 'D' into bytes.
+    """
+
+    def dumps(self, obj: D) -> bytes:
+        """Encode the object into a blob of bytes."""
+
+    def loads(self, blob: bytes) -> D:
+        """Decode the blob of bytes into an object."""
+
+
+class PackageManifest(BaseModel, abc.ABC):
+    """
+    Describes a package.
+    """
 
 
 class Packager(BaseModel, abc.ABC):
-    @abc.abstractmethod
-    async def check_compat(self, deployment: "DeploymentSpec") -> None:
-        """
-        Check compatbility with a deployment.
-        """
+    """
+    Creates a package for a flow.
+    """
 
     @abc.abstractmethod
-    async def package(self, deployment: "DeploymentSpec") -> DeploymentCreate:
+    async def package(self, flow: "FlowSource") -> PackageManifest:
         """
-        Package the flow referenced by the deployment.
-
-        A schema for creating an API deployment will be returned with populated
-        references to the packaged flow.
+        Package a flow and returns a manifest describing how the package can be used
+        in a deployment.
         """
---
+++ src/prefect/packaging/serializers.py
@@ -0,0 +1,150 @@
+import base64
+import importlib
+import inspect
+import json
+import warnings
+from typing import Any
+
+import pydantic
+
+from prefect.packaging.base import Serializer
+
+
+def to_qualified_name(obj: Any) -> str:
+    return obj.__module__ + "." + obj.__qualname__
+
+
+def from_qualified_name(name: str) -> Any:
+    # Try importing the path directly to support "module" or "module.sub_module"
+    try:
+        module = importlib.import_module(name)
+        return module
+    except ImportError:
+        # If no subitem was included raise the import error
+        if "." not in name:
+            raise
+
+    # Otherwise, we'll try to load it as an attribute of a module
+    mod_name, attr_name = name.rsplit(".", 1)
+    module = importlib.import_module(mod_name)
+    return getattr(module, attr_name)
+
+
+class PickleSerializer(Serializer):
+    """
+    Serializes objects using the pickle protocol.
+
+    Wraps pickles in base64 for safe transmission.
+    """
+
+    picklelib: str = "cloudpickle"
+    picklelib_version: str = None
+
+    @pydantic.validator("picklelib")
+    def check_picklelib(cls, value):
+        """
+        Check that the given pickle library is importable and has dumps/loads methods.
+        """
+        try:
+            pickler = from_qualified_name(value)
+        except (ImportError, AttributeError) as exc:
+            raise ValueError(
+                f"Failed to import requested pickle library: {value!r}."
+            ) from exc
+
+        if not hasattr(pickler, "dumps"):
+            raise ValueError(
+                f"Pickle library at {value!r} does not have a 'dumps' method."
+            )
+
+        if not hasattr(pickler, "loads"):
+            raise ValueError(
+                f"Pickle library at {value!r} does not have a 'loads' method."
+            )
+
+        return value
+
+    @pydantic.root_validator
+    def check_picklelib_version(cls, values):
+        """
+        Infers a default value for `picklelib_version` if null or ensures it matches
+        the version retrieved from the `pickelib`.
+        """
+        picklelib = values["picklelib"]
+        picklelib_version = values.get("picklelib_version")
+
+        pickler = from_qualified_name(picklelib)
+        pickler_version = getattr(pickler, "__version__")
+
+        if not picklelib_version:
+            values["picklelib_version"] = pickler_version
+        elif picklelib_version != pickler_version:
+            warnings.warn(
+                f"Mismatched {picklelib!r} versions. Found {pickler_version} in the "
+                f"environment but {picklelib_version} was requested. This may cause "
+                "the serializer to fail.",
+                RuntimeWarning,
+                stacklevel=3,
+            )
+
+        return values
+
+    def dumps(self, obj: Any) -> bytes:
+        pickler = from_qualified_name(self.picklelib)
+        blob = pickler.dumps(obj)
+
+        return base64.encodebytes(blob)
+
+    def loads(self, blob: bytes) -> Any:
+        pickler = from_qualified_name(self.picklelib)
+        return pickler.loads(base64.decodebytes(blob))
+
+
+class SourceSerializer(Serializer):
+    """
+    Serializes objects by retrieving their source code.
+
+    Creates a JSON blob with keys:
+        source: The source code
+        symbol_name: The name of the object to extract from the source code
+
+    Deserialization requires the code to run with `exec`.
+    """
+
+    def dumps(self, obj: Any) -> bytes:
+        return json.dumps(
+            {
+                "source": inspect.getsource(obj),
+                "symbol_name": obj.__name__,
+            }
+        )
+
+    def loads(self, blob: bytes) -> Any:
+        document = json.loads(blob)
+        if not isinstance(document, dict) or set(document.keys()) != {
+            "source",
+            "symbol_name",
+        }:
+            raise ValueError(
+                "Invalid serialized data. "
+                "Expected dictionary with keys 'source' and 'symbol_name'. "
+                f"Got: {document}"
+            )
+
+        exec_globals, exec_locals = {}, {}
+        exec(document["source"], exec_globals, exec_locals)
+        symbols = {**exec_globals, **exec_locals}
+
+        return symbols[document["symbol_name"]]
+
+
+class ImportSerializer(Serializer):
+    """
+    Serializes objects by storing their importable path.
+    """
+
+    def dumps(self, obj: Any) -> bytes:
+        return to_qualified_name(obj).encode()
+
+    def loads(self, blob: bytes) -> Any:
+        return from_qualified_name(blob.decode())
