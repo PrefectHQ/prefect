2f89bbf5a5d71b395eb6e87af068b6d654c24d73
ahuang11 <andrew.h@prefect.io>
2022-06-21 19:36:51 -0700

    Linting

--- tests/tasks/azure/test_datafactory.py
+++ tests/tasks/azure/test_datafactory.py
@@ -1,15 +1,15 @@
 import time
 from unittest.mock import MagicMock, Mock
-from pandas import DataFrame
 
 import pytest
+from pandas import DataFrame
 
 import prefect
 from prefect.tasks.azure import DatafactoryCreate, PipelineCreate, PipelineRun
 from prefect.utilities.configuration import set_temporary_config
 
 
-class MockFactory():
+class MockFactory:
     def __init__(self, **kwargs):
         self.provisioning_state = "Queued"
         self.status = "Queued"
@@ -17,8 +17,8 @@ class MockFactory():
         self.provisioning_state = "Succeeded"
         self.status = "Succeeded"
 
-class MockClient():
 
+class MockClient:
     def __init__(self, *args, **kwargs):
         pass
 
@@ -32,7 +32,10 @@ class MockClient():
 
     @property
     def pipeline_runs(self):
-        return MagicMock(create_or_update=lambda *args: MockFactory(), get=lambda *args: MagicMock(status="Succeeded"))
+        return MagicMock(
+            create_or_update=lambda *args: MockFactory(),
+            get=lambda *args: MagicMock(status="Succeeded"),
+        )
 
     @property
     def activity_runs(self):
@@ -46,13 +49,14 @@ class TestDatafactoryCreate:
 
     def test_initialization_passes_to_task_constructor(self):
         task = DatafactoryCreate(
-            datafactory_name="df_name",
-            resource_group_name="rg_name"
+            datafactory_name="df_name", resource_group_name="rg_name"
         )
         assert task.datafactory_name == "df_name"
         assert task.resource_group_name == "rg_name"
 
-    @pytest.mark.parametrize("required_arg", ["datafactory_name", "resource_group_name"])
+    @pytest.mark.parametrize(
+        "required_arg", ["datafactory_name", "resource_group_name"]
+    )
     def test_raises_if_required_args_not_eventually_provided(self, required_arg):
         task = DatafactoryCreate()
         required_args = {"datafactory_name": "name", "resource_group_name": "name"}
@@ -68,7 +72,9 @@ class TestDatafactoryCreate:
 
     def test_run(self, monkeypatch):
         datafactory = MagicMock(DataFactoryManagementClient=MockClient)
-        monkeypatch.setattr("prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory)
+        monkeypatch.setattr(
+            "prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory
+        )
         task = DatafactoryCreate()
         result = task.run(datafactory_name="df_name", resource_group_name="rg_name")
         assert result == "df_name"
@@ -83,31 +89,50 @@ class TestPipelineCreate:
         task = PipelineCreate(
             datafactory_name="df_name",
             resource_group_name="rg_name",
-            pipeline_name="p_name"
+            pipeline_name="p_name",
         )
         assert task.datafactory_name == "df_name"
         assert task.resource_group_name == "rg_name"
         assert task.pipeline_name == "p_name"
 
-    @pytest.mark.parametrize("required_arg", ["datafactory_name", "resource_group_name", "pipeline_name", "activities"])
+    @pytest.mark.parametrize(
+        "required_arg",
+        ["datafactory_name", "resource_group_name", "pipeline_name", "activities"],
+    )
     def test_raises_if_required_args_not_eventually_provided(self, required_arg):
         task = PipelineCreate()
-        required_args = {"datafactory_name": "name", "resource_group_name": "name", "pipeline_name": "name", "activities": []}
+        required_args = {
+            "datafactory_name": "name",
+            "resource_group_name": "name",
+            "pipeline_name": "name",
+            "activities": [],
+        }
         required_args.pop(required_arg)
         with pytest.raises(ValueError, match="must be specified"):
             task.run(**required_args)
 
     def test_raises_if_required_args_empty_string(self):
         task = PipelineCreate()
-        required_args = {"datafactory_name": "", "resource_group_name": "", "pipeline_name": ""}
+        required_args = {
+            "datafactory_name": "",
+            "resource_group_name": "",
+            "pipeline_name": "",
+        }
         with pytest.raises(ValueError, match="must be specified"):
             task.run(**required_args)
 
     def test_run(self, monkeypatch):
         datafactory = MagicMock(DataFactoryManagementClient=MockClient)
-        monkeypatch.setattr("prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory)
+        monkeypatch.setattr(
+            "prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory
+        )
         task = PipelineCreate()
-        result = task.run(datafactory_name="df_name", resource_group_name="rg_name", pipeline_name="p_name", activities=["activity"])
+        result = task.run(
+            datafactory_name="df_name",
+            resource_group_name="rg_name",
+            pipeline_name="p_name",
+            activities=["activity"],
+        )
         assert result == "p_name"
 
 
@@ -120,29 +145,45 @@ class TestPipelineRun:
         task = PipelineRun(
             datafactory_name="df_name",
             resource_group_name="rg_name",
-            pipeline_name="p_name"
+            pipeline_name="p_name",
         )
         assert task.datafactory_name == "df_name"
         assert task.resource_group_name == "rg_name"
         assert task.pipeline_name == "p_name"
 
-    @pytest.mark.parametrize("required_arg", ["datafactory_name", "resource_group_name", "pipeline_name"])
+    @pytest.mark.parametrize(
+        "required_arg", ["datafactory_name", "resource_group_name", "pipeline_name"]
+    )
     def test_raises_if_required_args_not_eventually_provided(self, required_arg):
         task = PipelineRun()
-        required_args = {"datafactory_name": "name", "resource_group_name": "name", "pipeline_name": "name"}
+        required_args = {
+            "datafactory_name": "name",
+            "resource_group_name": "name",
+            "pipeline_name": "name",
+        }
         required_args.pop(required_arg)
         with pytest.raises(ValueError, match="must be specified"):
             task.run(**required_args)
 
     def test_raises_if_required_args_empty_string(self):
         task = PipelineRun()
-        required_args = {"datafactory_name": "", "resource_group_name": "", "pipeline_name": ""}
+        required_args = {
+            "datafactory_name": "",
+            "resource_group_name": "",
+            "pipeline_name": "",
+        }
         with pytest.raises(ValueError, match="must be specified"):
             task.run(**required_args)
 
     def test_run(self, monkeypatch):
         datafactory = MagicMock(DataFactoryManagementClient=MockClient)
-        monkeypatch.setattr("prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory)
+        monkeypatch.setattr(
+            "prefect.tasks.azure.datafactory.azure.mgmt.datafactory", datafactory
+        )
         task = PipelineRun()
-        result = task.run(datafactory_name="df_name", resource_group_name="rg_name", pipeline_name="p_name")
+        result = task.run(
+            datafactory_name="df_name",
+            resource_group_name="rg_name",
+            pipeline_name="p_name",
+        )
         assert result == {"result": "success"}
