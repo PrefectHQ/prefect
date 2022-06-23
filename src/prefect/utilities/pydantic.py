120dac11dd25c065c945c72994e8e92a90dd1b9c
Michael Adkins <michael@prefect.io>
2022-06-23 12:23:19 -0500

    Move PartialModel to pydantic utilities

--- src/prefect/utilities/pydantic.py
+++ src/prefect/utilities/pydantic.py
@@ -1,4 +1,4 @@
-from typing import Any, Type, TypeVar
+from typing import Any, Generic, Type, TypeVar
 
 import pydantic
 from typing_extensions import Self
@@ -6,10 +6,10 @@ from typing_extensions import Self
 from prefect.utilities.importtools import from_qualified_name, to_qualified_name
 
 D = TypeVar("D", bound=Any)
-M = TypeVar("M", bound=Type[pydantic.BaseModel])
+M = TypeVar("M", bound=pydantic.BaseModel)
 
 
-def add_type_dispatch(model_cls: M) -> M:
+def add_type_dispatch(model_cls: Type[M]) -> Type[M]:
     """
     Extend a Pydantic model to add a field that includes the import path for the model
     on serialization and dynamically imports the model on deserialization. This allows
@@ -47,3 +47,62 @@ def add_type_dispatch(model_cls: M) -> M:
     model_cls.__new__ = __new__
 
     return model_cls
+
+
+class PartialModel(Generic[M]):
+    """
+    A utility for creating a Pydantic model in several steps.
+
+    Fields may be set at initialization, via attribute assignment, or at finalization
+    when the concrete model is returned.
+
+    Pydantic validation does not occur until finalization.
+
+    Each field can only be set once and a `ValueError` will be raised on assignment if
+    a field already has a value.
+
+    Example:
+        >>> class MyModel(pydantic.BaseModel):
+        >>>     x: int
+        >>>     y: str
+        >>>     z: float
+        >>>
+        >>> partial_model = PartialModel(MyModel, x=1)
+        >>> partial_model.y = "two"
+        >>> model = partial_model.finalize(z=3.0)
+    """
+
+    def __init__(self, __model_cls: Type[M], **kwargs: Any) -> None:
+        self.fields = kwargs
+        # Set fields first to avoid issues if `fields` is also set on the `model_cls`
+        # in our custom `setattr` implementation.
+        self.model_cls = __model_cls
+
+        for name in kwargs.keys():
+            self.raise_if_not_in_model(name)
+
+    def finalize(self, **kwargs: Any) -> M:
+        for name in kwargs.keys():
+            self.raise_if_already_set(name)
+            self.raise_if_not_in_model(name)
+        return self.model_cls(**self.fields, **kwargs)
+
+    def raise_if_already_set(self, name):
+        if name in self.fields:
+            raise ValueError(f"Field {name!r} has already been set.")
+
+    def raise_if_not_in_model(self, name):
+        if name not in self.model_cls.__fields__:
+            raise ValueError(f"Field {name!r} is not present in the model.")
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name in {"fields", "model_cls"}:
+            return super().__setattr__(__name, __value)
+
+        self.raise_if_already_set(__name)
+        self.raise_if_not_in_model(__name)
+        self.fields[__name] = __value
+
+    def __repr__(self) -> str:
+        dsp_fields = ", ".join(f"{key}={repr(value)}" for key, value in self.fields)
+        return f"PartialModel({self.model_cls.__name__}{dsp_fields})"
