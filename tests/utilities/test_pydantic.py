a34762ae3470e4a939b8a2d2d2e3a8f91ea100f8
Michael Adkins <michael@prefect.io>
2022-06-24 11:40:23 -0500

    Add support for literal "type" fields to pydantic utility

--- tests/utilities/test_pydantic.py
+++ tests/utilities/test_pydantic.py
@@ -2,9 +2,10 @@ import json
 
 import pydantic
 import pytest
+from typing_extensions import Literal
 
 from prefect.utilities.dispatch import register_type
-from prefect.utilities.pydantic import PartialModel, add_type_dispatch_field
+from prefect.utilities.pydantic import PartialModel, add_type_dispatch
 
 
 class SimplePydantic(pydantic.BaseModel):
@@ -69,7 +70,7 @@ class TestPartialModel:
 
 class TestTypeDispatchField:
     def test_base_can_be_resolved_to_self(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__ = "__base__"
 
@@ -78,7 +79,7 @@ class TestTypeDispatchField:
         assert type(post_instance) == Base
 
     def test_subtype_dict_contains_dispatch_key(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__ = "base"
 
@@ -89,7 +90,7 @@ class TestTypeDispatchField:
         assert Foo().dict().get("type") == "foo"
 
     def test_subtype_json_contains_dispatch_key(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__ = "base"
 
@@ -100,7 +101,7 @@ class TestTypeDispatchField:
         assert json.loads(Foo().json()).get("type") == "foo"
 
     def test_subtype_can_be_resolved_with_base_parse(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__ = "base"
 
@@ -115,7 +116,7 @@ class TestTypeDispatchField:
         assert isinstance(post_instance, Base)
 
     def test_subtype_can_be_resolved_with_subtype_parse(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__ = "base"
 
@@ -130,7 +131,7 @@ class TestTypeDispatchField:
         assert isinstance(post_instance, Base)
 
     def test_subtype_with_callable_dispatch_key_can_be_resolved_with_base_parse(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             @classmethod
             def __dispatch_key__(cls):
@@ -147,7 +148,7 @@ class TestTypeDispatchField:
         assert isinstance(post_instance, Base)
 
     def test_subtypes_are_unique(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             @classmethod
             def __dispatch_key__(cls):
@@ -168,7 +169,7 @@ class TestTypeDispatchField:
         assert type(post_foo) != type(post_bar)
 
     def test_registries_are_unique_per_base(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             @classmethod
             def __dispatch_key__(cls):
@@ -178,7 +179,7 @@ class TestTypeDispatchField:
         class Foo(Base):
             pass
 
-        @add_type_dispatch_field
+        @add_type_dispatch
         class SecondBase(pydantic.BaseModel):
             @classmethod
             def __dispatch_key__(cls):
@@ -197,7 +198,7 @@ class TestTypeDispatchField:
         assert isinstance(post_second_foo, SecondFoo)
 
     def test_unregistered_subtype_cannot_be_resolved_with_base_parse(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__: str
 
@@ -221,7 +222,7 @@ class TestTypeDispatchField:
                 pass
 
     def test_register_type_without_dispatch_key(self):
-        @add_type_dispatch_field
+        @add_type_dispatch
         class Base(pydantic.BaseModel):
             __dispatch_key__: str
 
@@ -233,3 +234,43 @@ class TestTypeDispatchField:
             @register_type
             class Foo(Base):
                 pass
+
+    def test_type_field_can_be_used_directly(self):
+        @add_type_dispatch
+        class Base(pydantic.BaseModel):
+            type: str
+
+        @register_type
+        class Foo(Base):
+            type: Literal["foo"] = "foo"
+
+        assert Foo.__dispatch_key__() == "foo"
+
+        instance = Foo()
+        post_instance = Base.parse_raw(instance.json())
+        assert type(post_instance) == Foo
+        assert isinstance(post_instance, Foo)
+        assert isinstance(post_instance, Base)
+
+    def test_both_type_field_and_dispatch_key_cannot_be_set(self):
+
+        with pytest.raises(
+            ValueError,
+            match="Model class 'Base' defines a `__dispatch_key__` and a type field. Only one of these may be defined for dispatch",
+        ):
+
+            @add_type_dispatch
+            class Base(pydantic.BaseModel):
+                type: str
+                __dispatch_key__ = "base"
+
+    def test_base_type_field_must_be_string_type(self):
+
+        with pytest.raises(
+            TypeError,
+            match="Model class 'Base' defines a 'type' field with type 'int' but it must be 'str'",
+        ):
+
+            @add_type_dispatch
+            class Base(pydantic.BaseModel):
+                type: int
