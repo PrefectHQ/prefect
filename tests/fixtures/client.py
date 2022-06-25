e5cec22071714e7dc7074a51cdd5493533b98d2d
Michael Adkins <michael@prefect.io>
2022-06-25 12:30:02 -0500

    :facepalm: I got bitten by the async fixture context issue. Fixed and added comments to help future developers.

--- tests/fixtures/client.py
+++ tests/fixtures/client.py
@@ -6,7 +6,7 @@ from prefect.client import get_client
 
 
 @pytest.fixture
-async def orion_client(test_database_url):
+async def orion_client(test_database_connection_url):
     async with get_client() as client:
         yield client
 
