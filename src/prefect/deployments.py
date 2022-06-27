1745f8c98055b39afa57c44569d2ce34ac647ba2
Michael Adkins <michael@prefect.io>
2022-06-27 10:53:50 -0500

    Update module docstring

--- src/prefect/deployments.py
+++ src/prefect/deployments.py
@@ -1,9 +1,10 @@
 """
 Objects for specifying deployments and utilities for loading flows from deployments.
 
-The primary object is the `DeploymentSpec` which can be used to define a deployment.
-Once a specification is written, it can be used with the Orion client or CLI to create
-a deployment in the backend.
+Deployments can be defined with the `Deployment` object.
+
+To use your deployment, it must be registered with the API. The `Deployment.create()`
+method can be used, or the `prefect deployment create` CLI.
 
 Examples:
     Define a flow
@@ -12,29 +13,31 @@ Examples:
     >>> def hello_world(name="world"):
     >>>     print(f"Hello, {name}!")
 
-    Write a deployment specification that sets a new parameter default
-    >>> from prefect.deployments import DeploymentSpec
-    >>> DeploymentSpec(
+    Write a deployment that sets a new parameter default
+    >>> from prefect.deployments import Deployment
+    >>> Deployment(
     >>>     flow=hello_world,
     >>>     name="my-first-deployment",
     >>>     parameters={"name": "Earth"},
     >>>     tags=["foo", "bar"],
     >>> )
 
-    Add a schedule to the deployment specification to run the flow hourly
+    Add a schedule to the deployment to run the flow hourly
     >>> from prefect.orion.schemas.schedules import IntervalSchedule
     >>> from datetime import timedelta
-    >>> DeploymentSpec(
+    >>> Deployment(
     >>>     ...
     >>>     schedule=IntervalSchedule(interval=timedelta(hours=1))
     >>> )
 
-    Deployment specifications can also be written in YAML and refer to the flow's
-    location instead of the `Flow` object
+    Deployments can also be written in YAML and refer to the flow's location instead 
+    of the `Flow` object. If there are multiple flows in the file, a name will needed
+    to load the correct flow.
     ```yaml
     name: my-first-deployment
-    flow_location: ./path-to-the-flow-script.py
-    flow_name: hello-world
+    flow:
+        path: ./path-to-the-flow-script.py
+        name: hello-world
     tags:
     - foo
     - bar
