4829560bc6d65e1944e892cbd812f280a11bf448
Nathan Nowack <thrast36@gmail.com>
2022-06-27 18:04:05 -0500

    add classmethod from_file to KubernetesClusterConfig and alter naming conventions

--- src/prefect/blocks/kubernetes.py
+++ src/prefect/blocks/kubernetes.py
@@ -1,32 +1,46 @@
 from typing import Dict
 
 import yaml
+from kubernetes.client import ApiClient
+from kubernetes.config import new_client_from_config_dict
 
 from prefect.blocks.core import Block, register_block
 
 
 @register_block
-class KubernetesCluster(Block):
+class KubernetesClusterConfig(Block):
     """
     A `Block` class for holding information about kubernetes clusters.
     """
 
     config: Dict = None
-    config_file: str
-    context: str
+    context: str = None
 
-    def block_initialization(self) -> None:
+    @classmethod
+    def from_file(cls, filepath: str, context: str):
         """
-        Overriding Block.block_initialization to set config_dict.
+        Class method to extract dict representing k8s config from config file.
         """
+        with open(filepath, "r") as f:
+            contents = yaml.safe_load(f)
+            for cluster_config in contents["clusters"]:
+                if cluster_config["name"] == context:
+                    return cls(config=cluster_config, context=context)
 
-        self.config = self.config_dict()
-        return super().block_initialization()
+        raise KeyError(f"No context found in config file with name: {context!r}")
 
-    def config_dict(self) -> Dict:
-        """
-        Method to extract dict representing k8s config from config file.
-        """
-        with open(self.config_file, "r") as f:
-            config_contents = yaml.safe_load(f)
-            return config_contents
+    # @classmethod
+    # def from_current_context(cls):
+    #     pass
+
+    def get_client_from_config_dict(self) -> ApiClient:
+
+        try:
+            assert self.config
+
+            return new_client_from_config_dict(
+                config_dict=self.config, context=self.context
+            )
+
+        except AssertionError:
+            raise ("config is not populated")
