d8d901c249ea7d21f73b02522377d938f1c5c64e
Nathan Nowack <thrast36@gmail.com>
2022-06-29 14:24:13 -0500

    fix path for k8s imports

--- src/prefect/blocks/kubernetes.py
+++ src/prefect/blocks/kubernetes.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 import yaml
 from kubernetes.client import ApiClient
-from kubernetes.config import (
+from kubernetes.config.kube_config import (
     ConfigException,
     list_kube_config_contexts,
     load_kube_config_from_dict,
