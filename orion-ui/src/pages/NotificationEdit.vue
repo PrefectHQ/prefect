7a775f4681806a270d5a7f2c1f18f5e72ce59a98
Jennifer Grange <jenny@prefect.io>
2022-06-30 11:13:36 -0400

    add edit notification

--- orion-ui/src/pages/NotificationEdit.vue
+++ orion-ui/src/pages/NotificationEdit.vue
@@ -5,15 +5,33 @@
 </template>
 
 <script lang="ts" setup>
-  import { NotificationForm } from '@prefecthq/orion-design'
-  import { useSubscription, useRouteParam } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { NotificationForm, Notification, INotificationUpdateRequest } from '@prefecthq/orion-design'
+  import { showToast } from '@prefecthq/prefect-design'
+  import { useRouteParam } from '@prefecthq/vue-compositions'
+  import { ref } from 'vue'
+  import router, { routes } from '@/router'
   import { notificationsApi } from '@/services/notificationsApi'
 
   const notificationId = useRouteParam('notificationId')
-  const notificationSubscription = useSubscription(notificationsApi.getNotification, [notificationId.value])
-  const notification = computed(() => notificationSubscription.response)
-  function submit(notification: Partial<Notification>): void {
-    console.log({ notification })
+  const notification = ref({ ...await notificationsApi.getNotification(notificationId.value) })
+
+  function mapNotificationUpdateToNotificationUpdateRequest(notificationUpdate: Partial<Notification>): INotificationUpdateRequest {
+    return {
+      name: notificationUpdate.name,
+      state_names: notificationUpdate.stateNames,
+      tags: notificationUpdate.tags,
+      is_active: notificationUpdate.isActive,
+    }
+  }
+
+  async function submit(notificationSource: Partial<Notification>): Promise<void> {
+    try {
+      const notificationUpdate: INotificationUpdateRequest = mapNotificationUpdateToNotificationUpdateRequest(notificationSource)
+      await notificationsApi.updateNotification(notificationId.value, notificationUpdate)
+      router.push(routes.notifications())
+    } catch (error) {
+      showToast('Error updating notification', 'error')
+      console.warn(error)
+    }
   }
 </script>
