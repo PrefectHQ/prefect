b18067d4056f4134695235c69e59f207e72c6c2a
Michael Adkins <michael@prefect.io>
2022-07-01 10:28:29 -0500

    Rename to context_name

--- tests/blocks/test_kubernetes.py
+++ tests/blocks/test_kubernetes.py
@@ -44,36 +44,32 @@ def config_file(tmp_path) -> Path:
 
 
 async def test_instantiation_from_file(config_file):
-
     cluster_config = KubernetesClusterConfig.from_file(path=config_file)
 
     assert isinstance(cluster_config, KubernetesClusterConfig)
     assert isinstance(cluster_config.config, Dict)
-    assert isinstance(cluster_config.context, str)
+    assert isinstance(cluster_config.context_name, str)
 
     assert cluster_config.config == yaml.safe_load(CONFIG_CONTENT)
-    assert cluster_config.context == "docker-desktop"
+    assert cluster_config.context_name == "docker-desktop"
 
 
-async def test_instantiation_from_file_with_bad_context(config_file):
+async def test_instantiation_from_file_with_unknown_context_name(config_file):
     with pytest.raises(ValueError):
-        KubernetesClusterConfig.from_file(path=config_file, context="random_not_real")
+        KubernetesClusterConfig.from_file(
+            path=config_file, context_name="random_not_real"
+        )
 
 
 async def test_get_api_client(config_file):
     cluster_config = KubernetesClusterConfig.from_file(path=config_file)
     api_client = cluster_config.get_api_client()
-
     assert isinstance(api_client, ApiClient)
 
 
 async def test_configure_client(config_file):
     cluster_config = KubernetesClusterConfig.from_file(path=config_file)
-
     cluster_config.configure_client()
-
     context_dict = list_kube_config_contexts(config_file=str(config_file))
-
     current_context = context_dict[1]["name"]
-
-    assert cluster_config.context == current_context
+    assert cluster_config.context_name == current_context
