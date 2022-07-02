ab938ad190b2ff8d25a8a23e515d92c09da7989d
Jennifer Grange <jenny@prefect.io>
2022-07-02 07:35:18 -0400

    remove filter and fix typo

--- orion-ui/src/router/index.ts
+++ orion-ui/src/router/index.ts
@@ -93,7 +93,7 @@ const routeRecords: AppRouteRecord[] = [
         component: (): RouteComponent => import('@/pages/Notifications.vue'),
       },
       {
-        name: 'notifications.create,
+        name: 'notifications.create',
         path: 'new',
         component: (): RouteComponent => import('@/pages/NotificationCreate.vue'),
       },
