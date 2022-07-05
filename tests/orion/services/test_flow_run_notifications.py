0b299b9d513b32e5e3476f6729d77e11320f9952
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2022-07-05 11:19:02 -0400

    Remove debug notification block from core library

--- tests/orion/services/test_flow_run_notifications.py
+++ tests/orion/services/test_flow_run_notifications.py
@@ -1,24 +1,8 @@
 import pytest
 import sqlalchemy as sa
 
-from prefect.blocks.notifications import DebugPrintNotification
 from prefect.orion import models, schemas
 from prefect.orion.services.flow_run_notifications import FlowRunNotifications
-
-
-@pytest.fixture(autouse=True)
-async def notifier_block(orion_client):
-
-    block = DebugPrintNotification()
-    schema = await orion_client.read_block_schema_by_checksum(
-        block._calculate_schema_checksum()
-    )
-
-    return await orion_client.create_block_document(
-        block._to_block_document(
-            name="Debug Print Notification", block_schema_id=schema.id
-        )
-    )
 
 
 @pytest.fixture
@@ -29,7 +13,7 @@ async def completed_policy(session, notifier_block):
             flow_run_notification_policy=schemas.core.FlowRunNotificationPolicy(
                 state_names=["Completed"],
                 tags=[],
-                block_document_id=notifier_block.id,
+                block_document_id=notifier_block._block_document_id,
             ),
         )
     )
@@ -45,7 +29,7 @@ async def completed_etl_policy(session, notifier_block):
             flow_run_notification_policy=schemas.core.FlowRunNotificationPolicy(
                 state_names=["Completed"],
                 tags=["ETL"],
-                block_document_id=notifier_block.id,
+                block_document_id=notifier_block._block_document_id,
             ),
         )
     )
@@ -61,7 +45,7 @@ async def failed_policy(session, notifier_block):
             flow_run_notification_policy=schemas.core.FlowRunNotificationPolicy(
                 state_names=["Failed"],
                 tags=[],
-                block_document_id=notifier_block.id,
+                block_document_id=notifier_block._block_document_id,
             ),
         )
     )
