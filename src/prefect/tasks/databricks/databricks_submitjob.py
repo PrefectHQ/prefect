86bb971dab4cc58a7178d7c268d296f4ba3829a7
Edmondo Porcu <edmondo.porcu@gmail.com>
2022-07-07 13:34:31 -0700

    Fix wrong formatting

--- src/prefect/tasks/databricks/databricks_submitjob.py
+++ src/prefect/tasks/databricks/databricks_submitjob.py
@@ -1000,7 +1000,7 @@ class DatabricksSubmitMultitaskRun(Task):
         "polling_period_seconds",
         "databricks_retry_limit",
         "databricks_retry_delay",
-        "git_source"
+        "git_source",
     )
     def run(
         self,
