08063f295dad2f34449a1d120f1fa301c6922a28
Michael Adkins <michael@prefect.io>
2022-07-08 09:22:02 -0500

    Cleanup implementation; move result linking to states

--- src/prefect/orion/schemas/states.py
+++ src/prefect/orion/schemas/states.py
@@ -131,6 +131,13 @@ class State(IDBaseModel, Generic[R]):
         if self.data:
             data = self.data.decode()
 
+        # Link the result to this state for dependency tracking
+        # Performing this here lets us capture relationships for futures resolved into
+        # data
+        from prefect.engine import link_state_to_result
+
+        link_state_to_result(self, data)
+
         if self.is_failed() and raise_on_failure:
             if isinstance(data, Exception):
                 raise data
