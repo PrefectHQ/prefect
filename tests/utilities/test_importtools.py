b8d4d21158c84a09bf717775c907eb994aa26160
Michael Adkins <michael@prefect.io>
2022-07-18 14:14:38 -0500

    Fix bug with `lazy_import` and add handling for missing modules at import-time

--- tests/utilities/test_importtools.py
+++ tests/utilities/test_importtools.py
@@ -1,4 +1,5 @@
 import importlib.util
+import sys
 from types import ModuleType
 
 import pytest
@@ -38,13 +39,54 @@ def test_to_and_from_qualified_name_roundtrip(obj):
     assert from_qualified_name(to_qualified_name(obj)) == obj
 
 
+@pytest.fixture
+def pop_docker_module():
+    # Allows testing of `lazy_import` on a clean sys
+    orginal = sys.modules.pop("docker")
+    try:
+        yield
+    finally:
+        sys.modules["docker"] = orginal
+
+
+@pytest.mark.usefixtures("pop_docker_module")
 def test_lazy_import():
-    requests: ModuleType("requests") = lazy_import("requests")
-    assert isinstance(requests, importlib.util._LazyModule)
-    assert isinstance(requests, ModuleType)
-    assert callable(requests.get)
+    docker: ModuleType("docker") = lazy_import("docker")
+    assert isinstance(docker, importlib.util._LazyModule)
+    assert isinstance(docker, ModuleType)
+    assert callable(docker.from_env)
 
 
-def test_lazy_import_still_fails_for_missing_modules():
+def test_lazy_import_fails_for_missing_modules():
     with pytest.raises(ModuleNotFoundError, match="flibbidy"):
-        lazy_import("flibbidy")
+        lazy_import("flibbidy", error_on_import=True)
+
+
+def test_lazy_import_allows_deferred_failure_for_missing_module():
+    module = lazy_import("flibbidy", error_on_import=False)
+    assert isinstance(module, ModuleType)
+    with pytest.raises(ModuleNotFoundError, match=f"No module named 'flibbidy'") as exc:
+        module.foo
+    assert (
+        "module = lazy_import" in exc.exconly()
+    ), "Exception should contain original line in message"
+
+
+def test_lazy_import_includes_help_message_for_missing_modules():
+    with pytest.raises(
+        ModuleNotFoundError, match="No module named 'flibbidy'.\nHello world"
+    ):
+        lazy_import("flibbidy", error_on_import=True, help_message="Hello world")
+
+
+def test_lazy_import_includes_help_message_in_deferred_failure():
+    module = lazy_import(
+        "flibbidy",
+        error_on_import=False,
+        help_message="No module named 'flibbidy'.*Hello world",
+    )
+    assert isinstance(module, ModuleType)
+    with pytest.raises(
+        ModuleNotFoundError, match="No module named 'flibbidy'.*Hello world"
+    ):
+        module.foo
