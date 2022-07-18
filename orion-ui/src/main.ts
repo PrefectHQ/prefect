38fa46e826f35750a2342413b752c6ef607518d0
Craig Harshbarger <pleek91@gmail.com>
2022-07-18 09:38:48 -0500

    Remove unused dependencies

--- orion-ui/src/main.ts
+++ orion-ui/src/main.ts
@@ -2,7 +2,6 @@ import { plugin as OrionDesign } from '@prefecthq/orion-design'
 import { plugin as PrefectDesign } from '@prefecthq/prefect-design'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-import './registerServiceWorker'
 import router from './router'
 import { applyActiveColorModeClass } from './utilities/colorMode'
 
