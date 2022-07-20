4d903754f92e0df0b23831d0c609015d278e3b38
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2022-07-20 14:31:07 -0400

    Fix bug with flatdict conversion

--- tests/utilities/test_collections.py
+++ tests/utilities/test_collections.py
@@ -32,23 +32,34 @@ class TestAutoEnum:
         json.dumps(Color.RED) == "RED"
 
 
-class TestFlatDict:
-    @pytest.fixture
-    def nested_dict(self):
-        return {1: 2, 2: {1: 2, 3: 4}, 3: {1: 2, 3: {4: 5, 6: {7: 8}}}}
-
-    def test_dict_to_flatdict(self, nested_dict):
-        assert dict_to_flatdict(nested_dict) == {
-            (1,): 2,
-            (2, 1): 2,
-            (2, 3): 4,
-            (3, 1): 2,
-            (3, 3, 4): 5,
-            (3, 3, 6, 7): 8,
-        }
-
-    def test_flatdict_to_dict(self, nested_dict):
-        assert flatdict_to_dict(dict_to_flatdict(nested_dict)) == nested_dict
+@pytest.mark.parametrize(
+    "d, expected",
+    [
+        (
+            {1: 2},
+            {(1,): 2},
+        ),
+        (
+            {1: 2, 2: {1: 2, 3: 4}, 3: {1: 2, 3: {4: 5, 6: {7: 8}}}},
+            {
+                (1,): 2,
+                (2, 1): 2,
+                (2, 3): 4,
+                (3, 1): 2,
+                (3, 3, 4): 5,
+                (3, 3, 6, 7): 8,
+            },
+        ),
+        (
+            {1: 2, 3: {}, 4: {5: {}}},
+            {(1,): 2, (3,): {}, (4, 5): {}},
+        ),
+    ],
+)
+def test_flatdict_conversion(d, expected):
+    flat = dict_to_flatdict(d)
+    assert flat == expected
+    assert flatdict_to_dict(flat) == d
 
 
 async def negative_even_numbers(x):
