b832b4823d95323aad8bbe09ae71dfafce009974
nicholas <znicholasbrown@gmail.com>
2022-07-22 14:08:59 -0400

    Add page heading

--- orion-ui/src/pages/DeploymentEdit.vue
+++ orion-ui/src/pages/DeploymentEdit.vue
@@ -1,10 +1,23 @@
 <template>
   <p-layout-default class="deployment-edit">
-    Hello!
+    <template #header>
+      <PageHeadingDeploymentEdit v-if="deployment" :deployment="deployment" />
+    </template>
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
+  import { PageHeadingDeploymentEdit } from '@prefecthq/orion-design'
+  import { useSubscription, useRouteParam } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+  import { deploymentsApi } from '@/services/deploymentsApi'
 
+  const deploymentId = useRouteParam('id')
+  const subscriptionOptions = {
+    interval: 300000,
+  }
+
+  const deploymentSubscription = useSubscription(deploymentsApi.getDeployment, [deploymentId.value], subscriptionOptions)
+  const deployment = computed(() => deploymentSubscription.response)
 </script>
 
