3fd168774da6ffa7e30d1b3cfcd936af168d7a9b
Chris Guidry <chris.g@prefect.io>
2022-07-28 09:27:58 -0400

    Head off possible `PendingRollbackError`s from the `scheduler` and `flow-run-notifications` `LoopService`s.
    The comments in the code tell the story in detail, but because this loop-service
    handles Exceptions internally, it may sometimes handle transient DB errors that
    require a rollback before proceeding with more work.
    
    Part of PrefectHQ/nebula#1220

--- src/prefect/orion/services/flow_run_notifications.py
+++ src/prefect/orion/services/flow_run_notifications.py
@@ -2,7 +2,6 @@
 A service that checks for flow run notifications and sends them.
 """
 import asyncio
-from typing import List
 
 import sqlalchemy as sa
 
@@ -21,8 +20,6 @@ class FlowRunNotifications(LoopService):
     actually sends the notification.
     """
 
-    batch_size: int = 10
-
     # check queue every 4 seconds
     # note: a tight loop is executed until the queue is exhausted
     loop_seconds: int = 4
@@ -34,9 +31,14 @@ class FlowRunNotifications(LoopService):
         async with session:
             async with session.begin():
                 while True:
+                    # Drain the queue one entry at a time, because if a database error
+                    # happens while sending a notification, we'll need to rollback the
+                    # transaction, which effectively re-queues any notifications that
+                    # we pulled here.  If we drain in batches larger than 1, we risk
+                    # double-sending notifications when a transient error occurs.
                     notifications = await db.get_flow_run_notifications_from_queue(
                         session=session,
-                        limit=self.batch_size,
+                        limit=1,
                     )
                     self.logger.debug(
                         f"Got {len(notifications)} notifications from queue."
@@ -46,59 +48,75 @@ class FlowRunNotifications(LoopService):
                     if not notifications:
                         break
 
-                    await self.send_flow_run_notifications(
-                        session=session, db=db, notifications=notifications
-                    )
+                    try:
+                        await self.send_flow_run_notification(
+                            session=session, db=db, notification=notifications[0]
+                        )
+                    finally:
+                        connection = await session.connection()
+                        if connection.invalidated:
+                            # If the connection was invalidated due to an error that
+                            # we handled in _send_flow_run_notification, we'll need to
+                            # rollback the session before we can proceed with more
+                            # iterations of the loop.  This may happen due to transient
+                            # database connection errors, but will _not_ happen due to
+                            # an calling a third-party service to send a notification.
+                            await session.rollback()
+                            assert not connection.invalidated
 
     @inject_db
-    async def send_flow_run_notifications(
-        self, session: sa.orm.session, db: OrionDBInterface, notifications: List
+    async def send_flow_run_notification(
+        self,
+        session: sa.orm.session,
+        db: OrionDBInterface,
+        # notification: OrionDBInterface.Notification,
+        notification,
     ):
-        for notification in notifications:
-            try:
-                orm_block_document = await session.get(
-                    db.BlockDocument, notification.block_document_id
-                )
-                if orm_block_document is None:
-                    self.logger.error(
-                        f"Missing block document {notification.block_document_id} "
-                        f"from policy {notification.flow_run_notification_policy_id}"
-                    )
-                    continue
-                block = Block._from_block_document(
-                    await schemas.core.BlockDocument.from_orm_model(
-                        session=session,
-                        orm_block_document=orm_block_document,
-                        include_secrets=True,
-                    )
-                )
-
-                message_template = (
-                    notification.flow_run_notification_policy_message_template
-                    or models.flow_run_notification_policies.DEFAULT_MESSAGE_TEMPLATE
-                )
-                message = message_template.format(
-                    **{
-                        k: notification[k]
-                        for k in schemas.core.FLOW_RUN_NOTIFICATION_TEMPLATE_KWARGS
-                    }
-                )
-                await block.notify(
-                    subject="Prefect flow run notification",
-                    body=message,
-                )
-
-                self.logger.debug(
-                    f"Successfully sent notification for flow run {notification.flow_run_id} "
+        try:
+            orm_block_document = await session.get(
+                db.BlockDocument, notification.block_document_id
+            )
+            if orm_block_document is None:
+                self.logger.error(
+                    f"Missing block document {notification.block_document_id} "
                     f"from policy {notification.flow_run_notification_policy_id}"
                 )
+                return
 
-            except Exception:
-                self.logger.error(
-                    f"Error sending notification for policy {notification.flow_run_notification_policy_id} "
-                    f"on flow run {notification.flow_run_id}",
-                    exc_info=True,
+            block = Block._from_block_document(
+                await schemas.core.BlockDocument.from_orm_model(
+                    session=session,
+                    orm_block_document=orm_block_document,
+                    include_secrets=True,
                 )
+            )
+
+            message_template = (
+                notification.flow_run_notification_policy_message_template
+                or models.flow_run_notification_policies.DEFAULT_MESSAGE_TEMPLATE
+            )
+            message = message_template.format(
+                **{
+                    k: notification[k]
+                    for k in schemas.core.FLOW_RUN_NOTIFICATION_TEMPLATE_KWARGS
+                }
+            )
+            await block.notify(
+                subject="Prefect flow run notification",
+                body=message,
+            )
+
+            self.logger.debug(
+                f"Successfully sent notification for flow run {notification.flow_run_id} "
+                f"from policy {notification.flow_run_notification_policy_id}"
+            )
+
+        except Exception:
+            self.logger.error(
+                f"Error sending notification for policy {notification.flow_run_notification_policy_id} "
+                f"on flow run {notification.flow_run_id}",
+                exc_info=True,
+            )
 
 
 if __name__ == "__main__":
