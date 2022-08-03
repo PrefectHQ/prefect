57c189b9ae9ae038990e97e1c459e74e5871148b
Craig Harshbarger <pleek91@gmail.com>
2022-08-03 09:52:10 -0500

    Progress

--- orion-ui/playwright.config.ts
+++ orion-ui/playwright.config.ts
@@ -41,6 +41,7 @@ const config: PlaywrightTestConfig = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     video: 'on',
+    baseURL: 'http://127.0.0.1:4200',
   },
 
   /* Configure projects for major browsers */
--- orion-ui/tests/notifications.spec.ts
+++ orion-ui/tests/notifications.spec.ts
@@ -1,45 +1,35 @@
-import { test, expect, Page } from '@playwright/test'
-
-async function countNotifications(page: Page): Promise<number> {
-  const rows = await page.locator('.notifications-table .p-table-body .p-table-row')
-
-  return await rows.count()
-}
+import { expect } from '@playwright/test'
+import { test, useButton, useCombobox, useLabel, useSelect, useTable } from './utilities'
 
 test('Can create a notification', async ({ page }) => {
-  await page.goto('http://127.0.0.1:4200/notifications')
-  const notifications = await countNotifications(page)
+  await page.goto('/notifications')
+
+  const { table, rows: notifications } = useTable('.notifications-table .p-table')
+  const existingNotifications = await notifications.count()
 
   // navigate to the Create Notification page
   await page.locator('[href="/notifications/new"]').first().click()
-  await page.locator('.notification-create').waitFor()
 
-  // select a state
-  const states = await page.locator('.state-select')
-  await states.click()
-  await page.locator('.p-select-option').first().click()
-  await states.click()
+  const { control: states } = useLabel('Run states')
+  const { selectOption } = useSelect(states)
+  await selectOption('Completed')
+  await selectOption('Running')
 
-  // add a tag
-  const tags = await page.locator('.p-tags-input')
-  await tags.click()
-  await page.locator('.p-combobox__text-input').fill('foo')
-  await page.keyboard.press('Enter')
-  await tags.click()
+  const { control: tags } = useLabel('Tags')
+  const { selectCustomOption } = useCombobox(tags)
+  await selectCustomOption('foo')
 
-  // pick "Slack Webhook"
-  await page.locator('.p-button-group').locator('text=Slack Webhook').click()
+  const { button: slackWebhookButton } = useButton('Slack Webhook')
+  await slackWebhookButton.click()
 
-  // fill Webhook URL input
-  await page.locator('text=Webhook URL').fill('https://slack.test')
+  const { control: webhookUrl } = useLabel('Webhook URL')
+  await webhookUrl.fill('https://slack.test')
 
-  // create the notification
-  await page.locator('.p-form__footer').locator('text=Create').click()
+  const { button: createButton } = useButton('Create')
+  await createButton.click()
 
-  // wait for navigation to the Notifications page
-  await page.locator('.notifications .p-table.notifications-table').waitFor()
+  await table.waitFor()
+  const newNotifications = await notifications.count()
 
-  const expected = notifications + 1
-
-  expect(await countNotifications(page)).toBe(expected)
+  expect(newNotifications).toBe(existingNotifications + 1)
 })
