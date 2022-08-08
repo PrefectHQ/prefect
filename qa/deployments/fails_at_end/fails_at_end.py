90d8e00da0814ed4424f724065ee7e0352a85f59
peytonrunyan <peytonrunyan@gmail.com>
2022-08-08 13:20:33 -0400

    Update qa flows

--- qa/deployments/fails_at_end/fails_at_end.py
+++ qa/deployments/fails_at_end/fails_at_end.py
@@ -1,7 +1,4 @@
 from prefect import flow, get_run_logger, task
-from prefect.cli._dev_utilities import assert_state_type_failed
-from prefect.client import get_client
-from prefect.context import get_run_context
 
 purpose = """
 The purpose of this flow is to see how Prefect handles a flow
@@ -32,7 +29,7 @@ def grand_child(from_child):
 
 
 @flow
-def fails_at_end_body():
+def fails_at_end():
     logger = get_run_logger()
     logger.info(purpose)
 
@@ -40,18 +37,6 @@ def fails_at_end_body():
     c1_res = first_child.submit(parent_res)
     c2_res = second_child.submit(parent_res)
     grand_child.submit(c1_res)
-
-    ctx = get_run_context()
-    return ctx.flow_run.id
-
-
-@flow
-async def fails_at_end():
-    run_id = fails_at_end_body()
-    async with get_client() as client:
-        flow_run = await client.read_flow_run(flow_run_id=run_id)
-
-    await assert_state_type_failed(flow_run.state_type)
 
 
 if __name__ == "__main__":
