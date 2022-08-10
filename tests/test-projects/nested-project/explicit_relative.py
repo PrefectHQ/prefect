cd2efd907d8c9e003cdaed2944c9fce8745a1b97
Chris White <chris@prefect.io>
2022-08-10 12:35:55 -0700

    Remove brittle tests for now, add others

--- tests/test-projects/nested-project/explicit_relative.py
+++
@@ -1,9 +0,0 @@
-from prefect import flow
-
-from .shared_libs.bar import bar
-from .shared_libs.foo import foo
-
-
-@flow
-def foobar():
-    return foo() + bar()
--- tests/test-projects/tree-project/imports/explicit_relative.py
+++
@@ -1,6 +0,0 @@
-from ..shared_libs.bar import bar
-from ..shared_libs.foo import foo
-
-
-def foobar():
-    return foo() + bar()
