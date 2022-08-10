932ad7d3bcb6ae85026a67ae51da64439486de03
Andrew Brookins <andrew.b@prefect.io>
2022-08-10 15:58:48 -0700

    Destroy flag after fixture is done with it

--- tests/cli/test_flags.py
+++ tests/cli/test_flags.py
@@ -11,13 +11,19 @@ ENABLE_FANCY_DEPLOYMENTS = "enable-fancy-deployments"
 @pytest.fixture
 def disabled_flag():
     with temporary_settings(updates={PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        yield feature_flags.create_if_missing(ENABLE_NEW_PROFILES, is_enabled=False)
+        flag = feature_flags.create_if_missing(ENABLE_NEW_PROFILES, is_enabled=False)
+        yield flag
+        flag.destroy()
 
 
 @pytest.fixture
 def enabled_flag():
     with temporary_settings(updates={PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        yield feature_flags.create_if_missing(ENABLE_FANCY_DEPLOYMENTS, is_enabled=True)
+        flag = feature_flags.create_if_missing(
+            ENABLE_FANCY_DEPLOYMENTS, is_enabled=True
+        )
+        yield flag
+        flag.destroy()
 
 
 def test_does_not_list_feature_flags_with_flags_disabled_globally(enabled_flag):
