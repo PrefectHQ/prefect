7888ebe7690e3925c20768c55d46b820551745a5
peytonrunyan <peytonrunyan@gmail.com>
2022-08-11 16:09:46 -0400

    change to exception

--- src/prefect/engine.py
+++ src/prefect/engine.py
@@ -213,7 +213,7 @@ async def create_then_begin_flow_run(
     engine_logger.info(f"Created flow run {flow_run.name!r} for flow {flow.name!r}")
 
     if state.is_failed():
-        flow_run_logger(flow_run).error(state.message)
+        flow_run_logger(flow_run).exception("Validation of flow parameters failed.")
         engine_logger.info(
             f"Flow run {flow_run.name!r} received invalid parameters and is marked as failed."
         )
