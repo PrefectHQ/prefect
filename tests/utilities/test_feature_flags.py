018ceb76739c94de363b9c4f97111f052713703e
Andrew Brookins <andrew.b@prefect.io>
2022-08-12 12:05:29 -0700

    Rewrite as a FeatureFlagClient subclass

--- tests/utilities/test_feature_flags.py
+++ tests/utilities/test_feature_flags.py
@@ -1,100 +1,138 @@
+from typing import Generator
+
 import pytest
-from flipper import Condition, FeatureFlagClient
+from flipper import Condition
+from flipper.bucketing import Percentage, PercentageBucketer
+from flipper.contrib.memory import MemoryFeatureFlagStore
 
 from prefect.settings import PREFECT_FEATURE_FLAGGING_ENABLED, temporary_settings
-from prefect.utilities.feature_flags import (
-    create_if_missing,
-    flag_is_enabled,
-    get_feature_flag_client,
-    list_feature_flags,
-)
+from prefect.utilities.feature_flags.flagger import FeatureFlagger
 
 
-@pytest.fixture()
-def features() -> FeatureFlagClient:
-    """A feature flag client fixture for working with feature flags in the test suite"""
-    return get_feature_flag_client()
+@pytest.fixture
+def features() -> Generator[FeatureFlagger, None, None]:
+    """
+    A feature flagger whose flag store exists only for the duration of
+    a test run.
+
+    NOTE: Using this fixture in a text activates feature flagging globally
+          for the duration of the test.
+    """
+    yield FeatureFlagger(MemoryFeatureFlagStore())
 
 
-@pytest.fixture(autouse=True)
-def delete_test_flags(features: FeatureFlagClient):
-    for flag in features.list():
-        features.destroy(flag.name)
-
-
-def test_creates_missing_flag(
-    features: FeatureFlagClient,
-):
-    name = "test"
-
-    assert features.exists(name) is False
-
+@pytest.fixture
+def enable_flagging(features):
     with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        flag = create_if_missing(name)
+        yield features
+
+
+@pytest.fixture
+def disable_flagging(features):
+    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "false"}):
+        yield features
+
+
+@pytest.mark.usefixtures("disable_flagging")
+class TestFeatureFlaggingDisabled:
+    def test_create(self, features):
+        assert features.create("test", is_enabled=True) is None
+
+    def test_exists(self, features):
+        features.create("test", is_enabled=True)
+        assert features.exists("test") is False
+
+    def test_is_enabled(self, features):
+        features.create("test", is_enabled=True)
+        assert features.is_enabled("test") is False
+
+    def test_get(self, features):
+        features.create("test", is_enabled=True)
+        assert features.get("test") is None
+
+    def test_all(self, features):
+        flags = features.all()
+        assert flags == []
+
+
+@pytest.mark.usefixtures("enable_flagging")
+class TestFeatureFlaggingEnabled:
+    def test_create_missing_flag(self, features):
+        name = "test"
+        assert features.exists(name) is False
+
+        flag = features.create(name)
         assert flag
         assert flag.name == name
         assert features.exists(name) is True
 
-
-def test_create_ignores_existing_flag():
-    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
+    def test_create_does_not_overwrite_existing_flag_state(self, features):
         # Create a disabled flag.
-        flag = create_if_missing("test", is_enabled=False)
+        flag = features.create("test", is_enabled=False)
         assert flag
         assert flag.name == "test"
         assert flag.is_enabled() is False
 
-        # Try to create the same flag, but enabled -- this should return a
-        # FeatureFlag instance that reports it is disabled because the
-        # current state of the existing flag (disabled) is honored.
-        flag = create_if_missing("test", is_enabled=True)
+        # Try to create the same flag, but enabled. This should return a flag
+        # that reports it is disabled because the current state of the existing
+        # flag (disabled) is honored.
+        flag = features.create("test", is_enabled=True)
         assert flag
         assert flag.is_enabled() is False
 
-
-def test_create_disabled_flag():
-    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        flag = create_if_missing("test", is_enabled=False)
-        flag = create_if_missing("test", is_enabled=False)
-        assert flag.name == "test"
-        assert flag_is_enabled("test") is False
-
-
-def test_create_enabled_flag():
-    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        flag = create_if_missing("test", is_enabled=True)
+    def test_create_disabled_flag(self, features):
+        flag = features.create("test", is_enabled=False)
         assert flag
         assert flag.name == "test"
-        assert flag_is_enabled("test") is True
+        assert features.is_enabled("test", client=features) is False
 
+    def test_create_enabled_flag(self, features):
+        flag = features.create("test", is_enabled=True)
+        assert flag
+        assert flag.name == "test"
+        assert features.is_enabled("test") is True
 
-def test_flag_conditions():
-    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        flag = create_if_missing(
+    def test_flag_conditions(self, features):
+        """
+        NOTE: The upstream library tests this behavior, but verifying it
+              here gives useful information about how to use conditions.
+        """
+        flag = features.create(
             "test", is_enabled=True, conditions=[Condition(is_admin=True)]
         )
         assert flag
         assert flag.name == "test"
 
         # Flag is disabled without the necessary condition
-        assert flag_is_enabled("test", is_admin=False) is False
+        assert features.is_enabled("test", is_admin=False) is False
 
         # Flag is enabled with the necessary condition
-        assert flag_is_enabled("test", is_admin=True) is True
+        assert features.is_enabled("test", is_admin=True) is True
 
         # Flag is enabled if no conditions are passed in.
-        assert flag_is_enabled("test") is True
+        assert features.is_enabled("test") is True
 
+    def test_flag_bucketer(self, features):
+        """
+        Like conditions, bucket behavior is tested by the upstream library.
+        Unlike conditions, buckets work without any interaction with the
+        caller, so we have less of a need for usage examples. We'll only
+        make sure we assigned the bucket correctly.
+        """
+        flag = features.create(
+            "test", is_enabled=True, bucketer=PercentageBucketer(Percentage(0.5))
+        )
+        assert flag
 
-def test_list_feature_flags():
-    # returns an empty list if the setting is not enabled
-    flags = list_feature_flags()
-    assert flags == []
+        assert flag.get_meta()["bucketer"] == {
+            "percentage": {"type": "Percentage", "value": 0.5},
+            "type": "PercentageBucketer",
+        }
 
-    with temporary_settings({PREFECT_FEATURE_FLAGGING_ENABLED: "true"}):
-        flags = {create_if_missing(f"flag-{i}") for i in range(5)}
+    def test_all_features(self, features):
+        flags = {features.create(f"flag-{i}") for i in range(5)}
         flag_names = {flag.name for flag in flags if flag}
         assert len(flag_names) == 5
 
-        listed_flags = list_feature_flags()
-        assert {flag.name for flag in listed_flags} == flag_names
+        all_flags = features.all()
+        assert {flag.name for flag in all_flags} == flag_names
