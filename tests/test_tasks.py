95f4a0f71bd3b97ba3473ffc7a93c9307dd60502
peytonrunyan <peytonrunyan@gmail.com>
2022-08-12 16:41:56 -0400

    update test fxn name

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1536,7 +1536,7 @@ class TestTaskInputs:
             task_2_input=[],
         )
 
-    async def test_task_inputs_populated_with_result_upstream_from_state_with_unpacking_no_trackables(
+    async def test_task_inputs_populated_with_result_upstream_from_state_with_unpacking_no_trackable_types(
         self, orion_client
     ):
         @task
