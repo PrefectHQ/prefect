73d220439b4bc746026808dce3e216d4c2751577
MSSandroid <12539982+MSSandroid@users.noreply.github.com>
2022-08-19 14:26:48 +0200

    Fix testing of _get_image_and_tag

--- tests/docker/test_dockercontainer_image_parsing.py
+++ tests/docker/test_dockercontainer_image_parsing.py
@@ -4,7 +4,7 @@ def docker_image_parsing(image, tags):
     d = DockerContainer(image=':'.join([image]+tags))
     parsed_image, parsed_tag = d._get_image_and_tag()
     assert parsed_image == image
-    assert parsed_tag == tags[0]
+    assert parsed_tag == (tags[0] if len(tags)>0 else None)
 
 def test_dockercontainer_image_parsing():
     for image in ['some_url_to_a_docker_repo.com:5050/path/to/image','some_url_to_a_docker_repo.com/path/to/image']:
