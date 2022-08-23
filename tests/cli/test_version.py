9331c00f6b3d5512006ae55171660af4d799e877
Michael Adkins <michael@prefect.io>
2022-08-23 11:36:18 -0500

    Add version tests

---
+++ tests/cli/test_version.py
@@ -0,0 +1,44 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from prefect.settings import PREFECT_API_URL, PREFECT_CLOUD_URL, temporary_settings
+from prefect.testing.cli import invoke_and_assert
+
+# @pytest.fixture(autouse=True)
+# def temporary_profiles_path(tmp_path):
+#     path = tmp_path / "profiles.toml"
+#     with temporary_settings({PREFECT_PROFILES_PATH: path}):
+#         yield path
+
+
+def test_version_ephemeral_server_type():
+    invoke_and_assert(
+        ["version"], expected_output_contains="Server type:         ephemeral"
+    )
+
+
+@pytest.mark.usefixtures("use_hosted_orion")
+def test_version_hosted_server_type():
+    invoke_and_assert(
+        ["version"], expected_output_contains="Server type:         hosted"
+    )
+
+
+def test_version_cloud_server_type():
+    with temporary_settings(
+        {
+            PREFECT_API_URL: PREFECT_CLOUD_URL.value()
+            + "/accounts/<test>/workspaces/<test>"
+        }
+    ):
+        invoke_and_assert(
+            ["version"], expected_output_contains="Server type:         cloud"
+        )
+
+
+def test_version_client_error_server_type(monkeypatch):
+    monkeypatch.setattr("prefect.get_client", MagicMock(side_effect=ValueError))
+    invoke_and_assert(
+        ["version"], expected_output_contains="Server type:         <client error>"
+    )
