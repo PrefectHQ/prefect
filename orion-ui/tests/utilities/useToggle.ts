75caa5f2a8405acae07c4a0ce7506860274bf433
Evan Sutherland <stackoverfloweth@gmail.com>
2022-08-25 08:55:05 -0500

    code review suggestion

--- orion-ui/tests/utilities/useToggle.ts
+++ orion-ui/tests/utilities/useToggle.ts
@@ -5,34 +5,24 @@ export type UseToggle = {
   toggle: Locator,
   getState: () => Promise<boolean | undefined>,
   setState: (state: boolean) => Promise<void>,
+  waitFor: (state: boolean) => Promise<void>,
 }
 
 export function useToggle(page: Page | Locator = PAGE): UseToggle {
   return {
     toggle: getToggle(page),
-    getState: async (toggle: Locator = getToggle(page)) => {
-      const count = await toggle.count()
-      console.log({ count })
-      const promises: Promise<boolean>[] = []
-
-      for (let i = 0; i < count; ++i) {
-        promises.push(getToggleState(toggle.nth(i)))
-      }
-
-      const states = await Promise.all(promises)
-      if (new Set(states).size === 1) {
-        return states[0]
-      }
+    getState: (toggle: Locator = getToggle(page)) => {
+      return isChecked(toggle)
     },
     setState: async (state, toggle: Locator = getToggle(page)) => {
-      const count = await toggle.count()
-      const promises: Promise<void>[] = []
+      const current = await isChecked(toggle)
 
-      for (let i = 0; i < count; ++i) {
-        promises.push(setToggleState(toggle.nth(i), state))
+      if (current !== state) {
+        toggle.click()
       }
-
-      await Promise.all(promises)
+    },
+    waitFor: (state, toggle: Locator = getToggle(page)) => {
+      return toggle.locator(`[aria-checked="${state}"]`).waitFor()
     },
   }
 }
@@ -41,15 +31,6 @@ function getToggle(page: Page | Locator): Locator {
   return page.locator('.p-toggle')
 }
 
-async function setToggleState(toggle: Locator, state: boolean): Promise<void> {
-  const current = await getToggleState(toggle)
-
-  if (current !== state) {
-    await toggle.click()
-    await toggle.locator(`[aria-checked="${state}"]`).waitFor()
-  }
-}
-
-async function getToggleState(toggle: Locator): Promise<boolean> {
+async function isChecked(toggle: Locator): Promise<boolean> {
   return await toggle.locator('[aria-checked="true"]').count() === 1
 }
