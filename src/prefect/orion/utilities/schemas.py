86ead63ceeaddc81ab67e01df2c42e34b90dad6d
Michael Adkins <michael@prefect.io>
2022-08-31 11:42:16 -0500

    Fix parsing of `PREFECT_TEST_MODE` in base model

--- src/prefect/orion/utilities/schemas.py
+++ src/prefect/orion/utilities/schemas.py
@@ -138,7 +138,9 @@ class PrefectBaseModel(BaseModel):
     class Config:
         # extra attributes are forbidden in order to raise meaningful errors for
         # bad API payloads
-        if os.getenv("PREFECT_TEST_MODE"):
+        # We cannot load this setting through the normal pattern due to circular
+        # imports; instead just parse the value as a boolean directly
+        if pydantic.parse_obj_as(bool, os.getenv("PREFECT_TEST_MODE")):
             extra = "forbid"
         else:
             extra = "ignore"
