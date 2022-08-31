aa8c723e0a33f7bd529e3200c58cc83dc443f567
Michael Adkins <michael@prefect.io>
2022-08-31 11:40:05 -0500

    Update pin to include patch version

--- src/prefect/infrastructure/submission.py
+++ src/prefect/infrastructure/submission.py
@@ -6,7 +6,7 @@ import prefect
 from prefect.infrastructure.base import Infrastructure
 from prefect.orion.schemas.core import FlowRun
 
-MIN_COMPAT_PREFECT_VERSION = "2.0"
+MIN_COMPAT_PREFECT_VERSION = "2.0.0"
 
 
 def base_flow_run_labels(flow_run: FlowRun) -> Dict[str, str]:
