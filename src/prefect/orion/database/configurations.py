5d26c5641d5544af4040bf0a2277aee1d83f354e
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2022-09-03 13:27:43 -0400

    Update PR link

--- src/prefect/orion/database/configurations.py
+++ src/prefect/orion/database/configurations.py
@@ -210,7 +210,7 @@ class AioSqliteConfiguration(BaseDatabaseConfiguration):
             # use `named` paramstyle for sqlite instead of `qmark` in very rare
             # circumstances, we've seen aiosqlite pass parameters in the wrong
             # order; by using named parameters we avoid this issue
-            # see e.g. https://github.com/PrefectHQ/prefect/pull/6645
+            # see https://github.com/PrefectHQ/prefect/pull/6702
             kwargs["paramstyle"] = "named"
 
             # ensure a long-lasting pool is used with in-memory databases
