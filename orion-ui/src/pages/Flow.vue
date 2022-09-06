c4c978fcbebd43e184ee62f539b1a46b61ec422e
Jennifer Grange <jenny@prefect.io>
2022-09-06 14:25:42 -0400

    add flow runs views tab

--- orion-ui/src/pages/Flow.vue
+++ orion-ui/src/pages/Flow.vue
@@ -11,6 +11,14 @@
       <template #deployments>
         <DeploymentsTable :deployments="flowDeployments" @update="flowDeploymentsSubscription.refresh()" @delete="flowDeploymentsSubscription.refresh()" />
       </template>
+      <template #runs>
+        <FlowRunList v-if="flowRuns.length" :flow-runs="flowRuns" disabled :selected="selectedFlowRuns" />
+        <PEmptyResults v-else>
+          <template #message>
+            No runs from the last 7 days
+          </template>
+        </PEmptyResults>
+      </template>
     </p-tabs>
 
     <template #well>
@@ -20,19 +28,20 @@
 </template>
 
 <script lang="ts" setup>
-  import { UnionFilters, DeploymentsTable, PageHeadingFlow, FlowDetails } from '@prefecthq/orion-design'
+  import { UnionFilters, DeploymentsTable, PageHeadingFlow, FlowDetails, FlowRunList, useFlowRunFilterFromParam } from '@prefecthq/orion-design'
   import { media } from '@prefecthq/prefect-design'
   import { useSubscription, useRouteParam } from '@prefecthq/vue-compositions'
-  import { computed } from 'vue'
+  import { computed, ref, Ref } from 'vue'
   import { useRouter } from 'vue-router'
   import { routes } from '@/router/routes'
   import { deploymentsApi } from '@/services/deploymentsApi'
+  import { flowRunsApi } from '@/services/flowRunsApi'
   import { flowsApi } from '@/services/flowsApi'
 
   const flowId = useRouteParam('id')
   const router = useRouter()
   const tabs = computed(() => {
-    const values = ['Deployments']
+    const values = ['Deployments', 'Runs']
 
     if (!media.xl) {
       values.unshift('Details')
@@ -58,6 +67,13 @@
 
   const flowDeploymentsSubscription = useSubscription(deploymentsApi.getDeployments, [flowDeploymentFilter], subscriptionOptions)
   const flowDeployments = computed(() => flowDeploymentsSubscription.response ?? [])
+
+  const flows: Ref<string[]> = ref<string[]>([flowId.value])
+  const filter =  useFlowRunFilterFromParam({ flows: flows.value })
+  console.log('filter on flow page', filter.filter)
+  const flowRunsSubscription = useSubscription(flowRunsApi.getFlowRuns, [filter.filter.value], subscriptionOptions)
+  const flowRuns = computed(()=> flowRunsSubscription.response ?? [])
+  const selectedFlowRuns = ref([])
 
   function deleteFlow(): void {
     router.push(routes.flows())
