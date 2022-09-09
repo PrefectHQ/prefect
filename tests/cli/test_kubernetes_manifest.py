0e1b8313322ae33807df9aa72b0044efb6d5639d
Hallen <hallenmaia@me.com>
2022-09-09 10:48:43 -0300

    Agent manifest tests

--- tests/cli/test_kubernetes_manifest.py
+++ tests/cli/test_kubernetes_manifest.py
@@ -1,6 +1,8 @@
 import yaml
 
+from prefect.docker import get_prefect_image_name
 from prefect.infrastructure.kubernetes import KubernetesJob
+from prefect.settings import PREFECT_API_URL
 from prefect.testing.cli import invoke_and_assert
 
 
@@ -23,6 +25,82 @@ def test_printing_the_orion_manifest():
 
     orion_container = deployment["spec"]["template"]["spec"]["containers"][0]
     assert orion_container["command"][0:3] == ["prefect", "orion", "start"]
+
+
+def test_printing_the_agent_manifest_with_no_args():
+    """`prefect kubernetes manifest agent` should print a valid YAML file
+    representing a basic agent deployment to a cluster"""
+    result = invoke_and_assert(
+        ["kubernetes", "manifest", "agent"],
+        expected_output_contains="kind: Deployment",
+    )
+    manifests = yaml.load_all(result.stdout, yaml.SafeLoader)
+
+    # Spot-check a few things.  This test is mostly just confirming that the output
+    # looks roughly like a set of Kubernetes manifests in YAML.
+    assert manifests
+
+    for manifest in manifests:
+        assert manifest["metadata"]["namespace"] == "default"
+
+        if manifest["kind"] == "Deployment":
+            assert manifest["metadata"]["name"] == "prefect-agent"
+            assert len(manifest["spec"]["template"]["spec"]["containers"]) == 1
+
+            agent_container = manifest["spec"]["template"]["spec"]["containers"][0]
+            assert agent_container["image"] == get_prefect_image_name()
+            assert agent_container["command"] == [
+                "prefect",
+                "agent",
+                "start",
+                "-q",
+                "kubernetes",
+            ]
+            assert len(agent_container["env"]) == 1
+            assert agent_container["env"][0]["name"] == "PREFECT_API_URL"
+            assert agent_container["env"][0]["value"] == str(PREFECT_API_URL.value())
+
+
+def test_printing_the_agent_manifest_with_api_url_image_tag_and_work_queue():
+    result = invoke_and_assert(
+        [
+            "kubernetes",
+            "manifest",
+            "agent",
+            "--api-url",
+            "test_api_url",
+            "-i",
+            "test_image_tag",
+            "-q",
+            "test_work_queue",
+        ],
+        expected_output_contains="kind: Deployment",
+    )
+    manifests = yaml.load_all(result.stdout, yaml.SafeLoader)
+    assert manifests
+
+    deployment = next(m for m in manifests if m["kind"] == "Deployment")
+    assert deployment["metadata"]["name"] == "prefect-agent"
+    assert len(deployment["spec"]["template"]["spec"]["containers"]) == 1
+
+    agent_container = deployment["spec"]["template"]["spec"]["containers"][0]
+    assert agent_container["image"] == "test_image_tag"
+    assert agent_container["command"][3:5] == ["-q", "test_work_queue"]
+    assert len(agent_container["env"]) == 1
+    assert agent_container["env"][0]["name"] == "PREFECT_API_URL"
+    assert agent_container["env"][0]["value"] == "test_api_url"
+
+
+def test_printing_the_agent_manifest_with_namespace():
+    result = invoke_and_assert(
+        ["kubernetes", "manifest", "agent", "-n", "test_namespace"],
+        expected_output_contains="kind: Deployment",
+    )
+    manifests = yaml.load_all(result.stdout, yaml.SafeLoader)
+    assert manifests
+
+    for manifest in manifests:
+        assert manifest["metadata"]["namespace"] == "test_namespace"
 
 
 def test_printing_the_job_base_manifest():
