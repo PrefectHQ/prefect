23d57b3607c36cb7b0707a93de4aaaba8bfa37da
Alexander Streed <desertaxle@users.noreply.github.com>
2022-09-15 17:25:58 -0500

    Restricts blocks in suite tests to only blocks in prefect module (#6851)

--- tests/blocks/test_block_standards.py
+++ tests/blocks/test_block_standards.py
@@ -3,12 +3,19 @@ import pytest
 from prefect.blocks.core import Block
 from prefect.testing.standard_test_suites import BlockStandardTestSuite
 from prefect.utilities.dispatch import get_registry_for_type
+from prefect.utilities.importtools import to_qualified_name
 
 block_registry = get_registry_for_type(Block) or {}
 
+blocks_under_test = [
+    block
+    for block in block_registry.values()
+    if to_qualified_name(block).startswith("prefect.")
+]
+
 
 @pytest.mark.parametrize(
-    "block", sorted(block_registry.values(), key=lambda x: x.get_block_type_slug())
+    "block", sorted(blocks_under_test, key=lambda x: x.get_block_type_slug())
 )
 class TestAllBlocksAdhereToStandards(BlockStandardTestSuite):
     @pytest.fixture
