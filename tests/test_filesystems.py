b713da5397931cf49fbafa04c95237193ce2165e
Bada-S <jamessopkin@gmail.com>
2022-09-20 14:04:30 -0500

    resolving git conflict

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -62,13 +62,6 @@ class TestLocalFileSystem:
         assert fs._resolve_path(tmp_path / "subdirectory") == tmp_path / "subdirectory"
         assert fs._resolve_path("subdirectory") == tmp_path / "subdirectory"
 
-    def test_resolve_path_sync(self, tmp_path):
-        fs = LocalFileSystem(basepath=str(tmp_path))
-
-        assert fs._resolve_path(tmp_path) == tmp_path
-        assert fs._resolve_path(tmp_path / "subdirectory") == tmp_path / "subdirectory"
-        assert fs._resolve_path("subdirectory") == tmp_path / "subdirectory"
-
     async def test_get_directory_duplicate_directory(self, tmp_path):
         fs = LocalFileSystem(basepath=str(tmp_path))
         await fs.get_directory(".", ".")
