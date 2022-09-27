972135427ae9a49e721db345bc62e10c4d696a6e
Dustin Ngo <dustin.ngo@gmail.com>
2022-09-27 14:02:57 -0400

    ⚫

--- tests/client/test_orion_client.py
+++ tests/client/test_orion_client.py
@@ -1063,7 +1063,9 @@ class TestClientAPIVersionRequests:
             ):
                 await client.hello()
 
-    @pytest.mark.skip(reason="This test is no longer compatible with the current API version checking logic")
+    @pytest.mark.skip(
+        reason="This test is no longer compatible with the current API version checking logic"
+    )
     async def test_patch_version(
         self, app, major_version, minor_version, patch_version
     ):
