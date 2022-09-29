3a90e80a665abf8f57b9ce62860bd0793d735521
James Ashby <James.Ashby@Colorado.EDU>
2022-09-29 10:25:18 -0600

    add tests

--- tests/test_task_runners.py
+++ tests/test_task_runners.py
@@ -1,8 +1,11 @@
+import time
 import pytest
+import anyio
 
 # Import the local 'tests' module to pickle to ray workers
 from prefect.task_runners import ConcurrentTaskRunner, SequentialTaskRunner
 from prefect.testing.standard_test_suites import TaskRunnerStandardTestSuite
+from prefect import task, flow
 
 
 @pytest.fixture
@@ -21,7 +24,6 @@ async def test_task_runner_cannot_be_started_while_running():
             async with task_runner.start():
                 pass
 
-
 class TestSequentialTaskRunner(TaskRunnerStandardTestSuite):
     @pytest.fixture
     def task_runner(self):
@@ -32,3 +34,55 @@ class TestConcurrentTaskRunner(TaskRunnerStandardTestSuite):
     @pytest.fixture
     def task_runner(self):
         yield ConcurrentTaskRunner()
+
+class TestConcurrentTaskRunnerLimitedThreads(TaskRunnerStandardTestSuite):
+    @pytest.fixture
+    def task_runner(self):
+        yield ConcurrentTaskRunner(max_workers=3)
+
+class TestConcurrentTaskRunnerSingleThreaded(TaskRunnerStandardTestSuite):
+    @pytest.fixture
+    def task_runner(self):
+        yield ConcurrentTaskRunner(max_workers=1)
+
+    def test_sync_tasks_run_sequentially_with_single_thread(
+        self, task_runner, tmp_file
+    ):
+        @task
+        def foo():
+            time.sleep(self.get_sleep_time())
+            tmp_file.write_text("foo")
+
+        @task
+        def bar():
+            tmp_file.write_text("bar")
+
+        @flow(version="test", task_runner=task_runner)
+        def test_flow():
+            foo()
+            bar()
+
+        test_flow()
+
+        assert tmp_file.read_text() == "bar"
+
+    async def test_async_tasks_run_sequentially_with_single_thread(
+        self, task_runner, tmp_file
+    ):
+        @task
+        async def foo():
+            await anyio.sleep(self.get_sleep_time())
+            tmp_file.write_text("foo")
+
+        @task
+        async def bar():
+            tmp_file.write_text("bar")
+
+        @flow(version="test", task_runner=task_runner)
+        async def test_flow():
+            await foo.submit()
+            await bar.submit()
+
+        await test_flow()
+
+        assert tmp_file.read_text() == "bar"
