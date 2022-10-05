c05ceacd9d013f035ab400da2613e5e90e083737
Michael Adkins <michael@prefect.io>
2022-10-05 10:08:37 -0500

    Add null test

--- tests/results/test_result_literal.py
+++ tests/results/test_result_literal.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 from prefect.results import BaseResult, ResultLiteral
@@ -36,3 +38,17 @@ async def test_result_literal_json_roundtrip(value):
 async def test_result_literal_does_not_allow_unsupported_types():
     with pytest.raises(TypeError, match="Unsupported type 'dict' for result literal"):
         await ResultLiteral.create({"foo": "bar"})
+
+
+async def test_result_literal_null_is_distinguishable_from_none():
+    """
+    This is important for separating cases where _no result_ is stored in the database
+    because the user disabled persistence (for example) from cases where the result
+    is stored but is a null value.
+    """
+    result = await ResultLiteral.create(None)
+    assert result is not None
+    serialized = result.json()
+    assert serialized is not None
+    assert serialized != "null"
+    assert json.loads(serialized) is not None
