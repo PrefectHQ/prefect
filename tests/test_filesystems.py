6640ecae020404b0fc7930ff8d2e9dd1cbc68c3e
peytonrunyan <peytonrunyan@gmail.com>
2022-10-07 16:57:35 -0400

    Add private repos

--- tests/test_filesystems.py
+++ tests/test_filesystems.py
@@ -1,7 +1,10 @@
 import os
 from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import Tuple
 
 import pytest
+from pydantic import ValidationError
 
 import prefect
 from prefect.filesystems import GitHub, LocalFileSystem, RemoteFileSystem
@@ -165,6 +168,50 @@ class TestRemoteFileSystem:
 
 
 class TestGitHub:
+    def setup_test_directory(
+        self, tmp_src: str, sub_dir: str = "puppy"
+    ) -> Tuple[str, str]:
+        """Add files and directories to a temporary directory. Returns a tuple with the
+        expected parent-level contents and the expected child-level contents.
+        """
+        # add file to tmp_src
+        f1_name = "dog.text"
+        f1_path = Path(tmp_src) / f1_name
+        f1 = open(f1_path, "w")
+        f1.close()
+
+        # add sub-directory to tmp_src
+        sub_dir_path = Path(tmp_src) / sub_dir
+        os.mkdir(sub_dir_path)
+
+        # add file to sub-directory
+        f2_name = "cat.txt"
+        f2_path = sub_dir_path / f2_name
+        f2 = open(f2_path, "w")
+        f2.close()
+
+        parent_contents = {f1_name, sub_dir}
+        child_contents = {f2_name}
+
+        assert set(os.listdir(tmp_src)) == parent_contents
+        assert set(os.listdir(sub_dir_path)) == child_contents
+
+        return parent_contents, child_contents
+
+    class MockTmpDir:
+        """Utility for having `TemporaryDirectory` return a known location."""
+
+        dir = None
+
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __enter__(self):
+            return self.dir
+
+        def __exit__(self, *args, **kwargs):
+            pass
+
     async def test_subprocess_errors_are_surfaced(self):
         g = GitHub(repository="incorrect-url-scheme")
         with pytest.raises(
@@ -195,3 +242,120 @@ class TestGitHub:
 
         assert mock.await_count == 1
         assert f"git clone prefect -b 2.0.0 --depth 1" in mock.await_args[0][0]
+
+    async def test_token_added_correctly_from_credential(self, monkeypatch):
+        """Ensure that the repo url is in the format `https://<oauth-key>@github.com/<username>/<repo>.git`."""  # noqa: E501
+
+        class p:
+            returncode = 0
+
+        mock = AsyncMock(return_value=p())
+        monkeypatch.setattr(prefect.filesystems, "run_process", mock)
+        credential = "XYZ"
+        repo = "https://github.com/PrefectHQ/prefect.git"
+        g = GitHub(
+            repository_url=repo,
+            credentials=credential,
+        )
+        await g.get_directory()
+        assert mock.await_count == 1
+        assert (
+            f"git clone https://{credential}@{repo} --depth 1" in mock.await_args[0][0]
+        )
+
+    async def test_ssh_fails_with_credential(self, monkeypatch):
+        """Ensure that credentials cannot be passed in if the URL is not in the HTTPS
+        format.
+        """
+
+        class p:
+            returncode = 0
+
+        mock = AsyncMock(return_value=p())
+        monkeypatch.setattr(prefect.filesystems, "run_process", mock)
+        credential = "XYZ"
+        error_msg = (
+            "Crendentials can only be used with GitHub repositories using the 'HTTPS' format"  # noqa
+            ".*"
+            "(type=value_error.invalidrepositoryurl)"
+        )
+        with pytest.raises(ValidationError, match=error_msg):
+            GitHub(
+                repository_url="git@github.com:PrefectHQ/prefect.git",
+                credentials=credential,
+            )
+
+    async def test_dir_contents_copied_correctly_with_get_directory(
+        self, monkeypatch
+    ):  # noqa
+        """Check that `get_directory` is able to correctly copy contents from src->dst"""  # noqa
+
+        class p:
+            returncode = 0
+
+        mock = AsyncMock(return_value=p())
+        monkeypatch.setattr(prefect.filesystems, "run_process", mock)
+
+        sub_dir_name = "puppy"
+
+        with TemporaryDirectory() as tmp_src:
+            parent_contents, child_contents = self.setup_test_directory(
+                tmp_src, sub_dir_name
+            )
+            self.MockTmpDir.dir = tmp_src
+
+            # move file contents to tmp_dst
+            with TemporaryDirectory() as tmp_dst:
+                monkeypatch.setattr(
+                    prefect.filesystems,
+                    "TemporaryDirectory",
+                    self.MockTmpDir,
+                )
+
+                g = GitHub(
+                    repository_url="https://github.com/PrefectHQ/prefect.git",
+                )
+                await g.get_directory(local_path=tmp_dst)
+
+                assert set(os.listdir(tmp_dst)) == parent_contents
+                assert set(os.listdir(Path(tmp_dst) / sub_dir_name)) == child_contents
+
+    async def test_dir_contents_copied_correctly_with_get_directory_and_from_path(
+        self, monkeypatch
+    ):  # noqa
+        """Check that `get_directory` is able to correctly copy contents from src->dst
+        when `from_path` is included.
+
+        It is expected that the directory specified by `from_path` will be moved to the
+        specified destination, along with all of its contents.
+        """
+
+        class p:
+            returncode = 0
+
+        mock = AsyncMock(return_value=p())
+        monkeypatch.setattr(prefect.filesystems, "run_process", mock)
+
+        sub_dir_name = "puppy"
+
+        with TemporaryDirectory() as tmp_src:
+            parent_contents, child_contents = self.setup_test_directory(
+                tmp_src, sub_dir_name
+            )
+            self.MockTmpDir.dir = tmp_src
+
+            # move file contents to tmp_dst
+            with TemporaryDirectory() as tmp_dst:
+                monkeypatch.setattr(
+                    prefect.filesystems,
+                    "TemporaryDirectory",
+                    self.MockTmpDir,
+                )
+
+                g = GitHub(
+                    repository_url="https://github.com/PrefectHQ/prefect.git",
+                )
+                await g.get_directory(local_path=tmp_dst, from_path=sub_dir_name)
+
+                assert set(os.listdir(tmp_dst)) == set([sub_dir_name])
+                assert set(os.listdir(Path(tmp_dst) / sub_dir_name)) == child_contents
