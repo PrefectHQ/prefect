bbee097653559003fa0db61ab00f1ff8567eea9a
peytonrunyan <peytonrunyan@gmail.com>
2022-10-07 16:57:35 -0400

    Add private repos

--- src/prefect/filesystems.py
+++ src/prefect/filesystems.py
@@ -7,14 +7,16 @@ import shutil
 import sys
 import tempfile
 import urllib.parse
+from distutils.dir_util import copy_tree
 from pathlib import Path, PurePath
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple, Union
 
 import anyio
 import fsspec
 from pydantic import Field, SecretStr, validator
 
 from prefect.blocks.core import Block
+from prefect.exceptions import InvalidRepositoryURLError
 from prefect.utilities.asyncutils import run_sync_in_worker_thread, sync_compatible
 from prefect.utilities.filesystem import filter_files
 from prefect.utilities.processutils import run_process
@@ -791,47 +793,103 @@ class GitHub(ReadableDeploymentStorage):
         default=None,
         description="An optional reference to pin to; can be a branch name or tag.",
     )
+    credentials: Optional[SecretStr] = Field(
+        name="Personal Access Token",
+        default=None,
+        description="A github Personal Access Token (PAT).",
+    )
 
+    @validator("credentials")
+    def _ensure_credentials_go_with_https(cls, v: str, values: dict):
+        """Ensure that credentials are not provided with 'SSH' formatted GitHub URLs."""
+        if v is not None:
+            if urllib.parse.urlparse(values["repository_url"]).scheme != "https":
+                raise InvalidRepositoryURLError(
+                    (
+                        "Crendentials can only be used with GitHub repositories "
+                        "using the 'HTTPS' format. You must either remove the "
+                        "credential if you wish to use the 'SSH' format and are not "
+                        "using a private repository, or you must change the repository "
+                        "url to the 'HTTPS' format. "
+                    )
+                )
+
+        return v
+
+    def _create_repo_url(self) -> str:
+        """Format the URL provided to the `git clone` command.
+
+        For private repos: https://<oauth-key>@github.com/<username>/<repo>.git
+        All other repos should be the same as `self.repository`.
+        """
+        url_components = urllib.parse.urlparse(self.repository_url)
+        if url_components.scheme == "https" and self.credentials is not None:
+            repo_url = url_components.netloc + url_components.path
+            updated_components = url_components._replace(
+                netloc=f"{self.credentials.get_secret_value()}@{url_components.netloc}"
+            )
+            full_url = urllib.parse.urlunparse(updated_components)
+        else:
+            full_url = self.repository_url
+
+        return full_url
+
+    @staticmethod
+    def _get_paths(
+        dst_dir: Union[str, None], src_dir: str, sub_directory: str
+    ) -> Tuple[str, str]:
+        """Returns the fully formed paths for GitHubRepository contents in the form
+        (content_source, content_destination).
+        """
+        if dst_dir is None:
+            content_destination = Path(".").absolute()
+        else:
+            content_destination = Path(dst_dir)
+
+        content_source = Path(src_dir)
+
+        if sub_directory:
+            content_destination = content_destination.joinpath(sub_directory)
+            content_source = content_source.joinpath(sub_directory)
+
+        return str(content_source), str(content_destination)
+
+    @sync_compatible
     async def get_directory(
-        self, from_path: str = None, local_path: str = None
+        self, from_path: Optional[str] = None, local_path: Optional[str] = None
     ) -> None:
         """
-        Clones a GitHub project specified in `from_path` to the provided `local_path`; defaults to cloning
-        the repository reference configured on the Block to the present working directory.
+        Clones a GitHub project specified in `from_path` to the provided `local_path`;
+        defaults to cloning the repository reference configured on the Block to the
+        present working directory.
 
         Args:
-            from_path: If provided, interpreted as a subdirectory of the underlying repository that will
-                be copied to the provided local path.
+            from_path: If provided, interpreted as a subdirectory of the underlying
+                repository that will be copied to the provided local path.
             local_path: A local path to clone to; defaults to present working directory.
         """
-        cmd = "git clone"
-
-        cmd += f" {self.repository}"
+        # CONSTRUCT COMMAND
+        cmd = f"git clone {self._create_repo_url()}"
         if self.reference:
-            cmd += f" -b {self.reference} --depth 1"
+            cmd += f" -b {self.reference}"
 
-        if local_path is None:
-            local_path = Path(".").absolute()
+        # Limit git history
+        cmd += " --depth 1"
 
-        if not from_path:
-            from_path = ""
+        # Clone to a temporary directory and move the subdirectory over
+        with tempfile.TemporaryDirectory(suffix="prefect") as tmp_dir:
+            tmp_path_str = tmp_dir
+            cmd += f" {tmp_path_str}"
 
-        # in this case, we clone to a temporary directory and move the subdirectory over
-        tmp_dir = None
-        tmp_dir = tempfile.TemporaryDirectory(suffix="prefect")
-        path_to_move = str(Path(tmp_dir.name).joinpath(from_path))
-        cmd += f" {tmp_dir.name} && cp -R {path_to_move}/."
-
-        cmd += f" {local_path}"
-
-        try:
             err_stream = io.StringIO()
             out_stream = io.StringIO()
             process = await run_process(cmd, stream_output=(out_stream, err_stream))
-        finally:
-            if tmp_dir:
-                tmp_dir.cleanup()
+            if process.returncode != 0:
+                err_stream.seek(0)
+                raise OSError(f"Failed to pull from remote:\n {err_stream.read()}")
 
-        if process.returncode != 0:
-            err_stream.seek(0)
-            raise OSError(f"Failed to pull from remote:\n {err_stream.read()}")
+            content_source, content_destination = self._get_paths(
+                dst_dir=local_path, src_dir=tmp_path_str, sub_directory=from_path
+            )
+
+            copy_tree(src=content_source, dst=content_destination)
