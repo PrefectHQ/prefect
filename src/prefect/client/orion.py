acab2c87dfb9a17307a7ea16124e1d9752d763a6
José Duarte <duarte.gmj@gmail.com>
2022-10-22 11:46:03 +0100

    Revert "Replace queue with set for unique matching"
    This reverts commit 78f67aefef40c4ba2244cfff2c9d93337a083cc1.

--- src/prefect/client/orion.py
+++ src/prefect/client/orion.py
@@ -808,7 +808,7 @@ class OrionClient:
         """
         page_length = 100
         current_page = 0
-        work_queues = set()
+        work_queues = []
 
         while True:
             new_queues = await self.read_work_queues(
@@ -816,15 +816,15 @@ class OrionClient:
             )
             if not new_queues:
                 break
-            filtered_queues = set()
+            filtered_queues = []
             for q in new_queues:
                 for p in prefixes:
                     if q.name.startswith(p):
-                        filtered_queues.add(q)
-            work_queues.update(filtered_queues)
+                        filtered_queues.append(q)
+            work_queues += filtered_queues
             current_page += 1
 
-        return list(work_queues)
+        return work_queues
 
     async def delete_work_queue_by_id(
         self,
