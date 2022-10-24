d3c43ca432b3d31b24a3d28b1f2f1c78cdc0dbfd
Dustin Ngo <dustin.ngo@gmail.com>
2022-10-24 18:17:45 -0400

    Remove extra columns

--- src/prefect/orion/database/orm_models.py
+++ src/prefect/orion/database/orm_models.py
@@ -353,7 +353,6 @@ class ORMFlowRun(ORMRun):
     parameters = sa.Column(JSON, server_default="{}", default=dict, nullable=False)
     idempotency_key = sa.Column(sa.String)
     context = sa.Column(JSON, server_default="{}", default=dict, nullable=False)
-    restarts = sa.Column(sa.Integer, server_default="0", default=0, nullable=False)
     empirical_policy = sa.Column(
         Pydantic(schemas.core.FlowRunPolicy),
         server_default="{}",
@@ -526,9 +525,6 @@ class ORMTaskRun(ORMRun):
     cache_expiration = sa.Column(Timestamp())
     task_version = sa.Column(sa.String)
     flow_retry_attempt = sa.Column(
-        sa.Integer, server_default="0", default=0, nullable=False
-    )
-    flow_restart_attempt = sa.Column(
         sa.Integer, server_default="0", default=0, nullable=False
     )
     empirical_policy = sa.Column(
