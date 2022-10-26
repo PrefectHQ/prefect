478c0ad51221986bcc59c8eeef92d62c1a06bc02
Jennifer Grange <jenny@prefect.io>
2022-10-26 16:28:51 -0400

    update permissions

--- orion-ui/src/utilities/permissions.ts
+++ orion-ui/src/utilities/permissions.ts
@@ -1,5 +1,15 @@
-import { getAppPermissions } from '@prefecthq/orion-design'
+import { getAppPermissions, isFeatureFlagString } from '@prefecthq/orion-design'
+import { reactive } from 'vue'
+import { VITE_PREFECT_CANARY } from '@/utilities/meta'
 
-export const can = getAppPermissions(
-  () => true,
+export const can = reactive(
+  getAppPermissions(
+    permissionString => {
+      if (isFeatureFlagString(permissionString)) {
+        return VITE_PREFECT_CANARY()
+      }
+
+      return true
+    },
+  ),
 )
