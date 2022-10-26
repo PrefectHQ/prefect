4ac668bc50959cb0f57db3c2f594436606a1b6ea
Chris Guidry <chris.g@prefect.io>
2022-10-26 12:57:02 -0400

    Future-proofing with getattr/isinstance/callable checks.  Including a note in the docs about how to use Prefect with proxies.
    Fixes #7204

--- src/prefect/client/orion.py
+++ src/prefect/client/orion.py
@@ -161,19 +161,25 @@ class OrionClient:
             **httpx_settings,
         )
 
-        # See https://www.python-httpx.org/advanced/#custom-transports
-        # If we're using an HTTP/S client (not the ephemeral client), adjust the
-        # transport to add retries _after_ it is instantiated. If we alter the transport
-        # before instantiation, the transport will not be aware of proxies unless we
-        # reproduce all of the logic to make it so.
+        # See https://www.python-httpx.org/advanced/#custom-transports If we're using an
+        # HTTP/S client (not the ephemeral client), adjust the transport to add retries
+        # _after_ it is instantiated. If we alter the transport before instantiation,
+        # the transport will not be aware of proxies unless we reproduce all of the
+        # logic to make it so.
         #
         # Only alter the transport to set our default of 3 retries, don't modify any
-        # transport a user may have provided via httpx_settings
+        # transport a user may have provided via httpx_settings.
+        #
+        # Making liberal use of getattr and isinstance checks here to avoid any
+        # surprises if the internals of httpx or httpcore change on us
         if isinstance(api, str) and not httpx_settings.get("transport"):
-            orion_transport = self._client._transport_for_url(httpx.URL(api))
-            if isinstance(orion_transport, httpx.AsyncHTTPTransport):
-                if isinstance(orion_transport._pool, httpcore.AsyncConnectionPool):
-                    orion_transport._pool._retries = 3
+            transport_for_url = getattr(self._client, "_transport_for_url", None)
+            if callable(transport_for_url):
+                orion_transport = transport_for_url(httpx.URL(api))
+                if isinstance(orion_transport, httpx.AsyncHTTPTransport):
+                    pool = getattr(orion_transport, "_pool", None)
+                    if isinstance(pool, httpcore.AsyncConnectionPool):
+                        pool._retries = 3
 
         self.logger = get_logger("client")
 
