c30b3e14451232ea7be57f584038addac5f67b8c
peytonrunyan <peytonrunyan@gmail.com>
2022-10-28 15:09:32 -0400

    update tests

--- tests/orion/schemas/test_actions.py
+++ tests/orion/schemas/test_actions.py
@@ -6,19 +6,21 @@ from prefect.orion.schemas.actions import FlowRunCreate
 
 
 @pytest.mark.parametrize(
-    "test_params,expected_serialization",
+    "test_params,expected_dict",
     [
-        ({"param": 1}, '"parameters":{"param":1}'),
-        ({"param": "1"}, '"parameters":{"param":"1"}'),
-        ({"param": {1: 2}}, '"parameters":{"param":{"1":2}}'),
+        ({"param": 1}, {"param": 1}),
+        ({"param": "1"}, {"param": "1"}),
+        ({"param": {1: 2}}, {"param": {"1": 2}}),
         (
             {"df": {"col": {0: "1"}}},
-            '"parameters":{"df":{"col":{"0":"1"}}}',
+            {"df": {"col": {"0": "1"}}},
         ),  # Example of serialzied dataframe parameter with int key
     ],
 )
 class TestFlowRunCreate:
-    def test_dict_succeeds_with_parameters(self, test_params, expected_serialization):
+    def test_dict_json_compatible_succeeds_with_parameters(
+        self, test_params, expected_dict
+    ):
         frc = FlowRunCreate(flow_id=uuid4(), flow_version="0.1", parameters=test_params)
-        res = frc.dict()
-        assert expected_serialization in res
+        res = frc.dict(json_compatible=True)
+        assert res["parameters"] == expected_dict
