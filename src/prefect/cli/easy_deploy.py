3f345a203307b5b8008fe5116fca6f9a78215743
peytonrunyan <peytonrunyan@gmail.com>
2022-11-11 15:18:15 -0500

    improve tables

--- src/prefect/cli/easy_deploy.py
+++ src/prefect/cli/easy_deploy.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import time
 from pathlib import Path
@@ -5,16 +6,51 @@ from typing import Iterable
 
 import readchar
 import typer
+from rich.layout import Layout
 from rich.live import Live
 from rich.table import Table
 
 from prefect.cli._utilities import exit_with_error
 from prefect.cli.root import app
 
-AVAILABLE_PROVIDERS = ["AWS", "GCP", "Azure"]
+AVAILABLE_PROVIDERS = [
+    "AWS",
+    "GCP",
+    "Azure",
+    "cobra",
+    "stork",
+    "penguin",
+    "shark",
+    "elephant",
+    "buffalo",
+    "whale",
+    "wolf",
+    "seal",
+    "eagle",
+    "wren",
+    "horse",
+    "rattlesnake",
+    "ape",
+    "crow",
+    "tuna",
+]
 
 
-def build_table(selected_idx: int, providers: Iterable[str]) -> Table:
+class CliTable:
+    window = []
+    window_size = 0
+    window_top = 0
+    window_bottom = None
+    key_press = None
+    idx = 0
+
+
+def build_table(
+    rows: Iterable[str],
+    search_chars,
+    console,
+    cli_table: CliTable,
+) -> Table:
     """
     Generate a table of providers. The `select_idx` of workspaces will be highlighted.
 
@@ -25,7 +61,30 @@ def build_table(selected_idx: int, providers: Iterable[str]) -> Table:
     Returns:
         rich.table.Table
     """
+    layout = Layout()
+    n_rows = os.get_terminal_size()[1]
+    if n_rows != cli_table.window_size:
+        cli_table.window_size = n_rows - 5
 
+    if cli_table.window_bottom is None:
+        cli_table.window_bottom = cli_table.window_size
+
+    # page down
+    if (
+        cli_table.key_press == readchar.key.DOWN
+        and cli_table.idx >= cli_table.window_bottom
+    ):
+        cli_table.window_top = cli_table.window_bottom - 1
+        cli_table.window_bottom = cli_table.window_top + cli_table.window_size - 1
+        cli_table.idx = cli_table.idx - 1
+
+    # page up
+    if cli_table.key_press == readchar.key.UP and cli_table.idx < cli_table.window_top:
+        cli_table.window_top = cli_table.window_top - (cli_table.window_size - 1)
+        cli_table.window_bottom = cli_table.window_top + cli_table.window_size
+        cli_table.idx = cli_table.idx + 1
+
+    # while n_rows >= 0:
     table = Table()
     table.add_column(
         "[#024dfd]Select a Cloud Provider:",
@@ -33,12 +92,21 @@ def build_table(selected_idx: int, providers: Iterable[str]) -> Table:
         style="#8ea0ae",
         no_wrap=True,
     )
+    table.add_row(f"[green]Search: '{search_chars}'[/green]")
+    # table.add_row(f"idx: {cli_table.idx}, t: {cli_table.window_top}, b: {cli_table.window_bottom}, s: {cli_table.window_size}")
 
-    for i, provider in enumerate(sorted(providers)):
-        if i == selected_idx:
-            table.add_row("[#024dfd on #FFFFFF]> " + provider)
+    start = max(0, cli_table.window_top)
+    end = min(cli_table.window_bottom, len(rows))
+    # print(f"start: {start}, end: {end}")
+    for i, row in enumerate(rows[start:end]):
+        position = i + cli_table.window_top
+        # print(f"pos: {position} idx: {cli_table.idx}")
+        if position == cli_table.idx:
+            # print("HIT")
+            table.add_row("[#024dfd on #FFFFFF]> " + row)
         else:
-            table.add_row("  " + provider)
+            table.add_row("  " + row)
+    layout.update(table)
     return table
 
 
@@ -136,34 +204,63 @@ def select_provider() -> str:
     Returns:
         str: the selected workspace
     """
-
-    current_idx = 0
     providers = sorted(AVAILABLE_PROVIDERS)
+    selectable_providers = providers
     selected_provider = None
+    search_chars = ""
+
+    cli_table = CliTable()
 
     with Live(
-        build_table(current_idx, providers), auto_refresh=False, console=app.console
+        build_table(providers, search_chars, app.console, cli_table),
+        vertical_overflow="visble",
+        auto_refresh=False,
+        # console=app.console
     ) as live:
         while selected_provider is None:
             key = readchar.readkey()
-
-            if key == readchar.key.UP:
-                current_idx = current_idx - 1
+            typed = False
+            if key.isalnum() or key in ["/", "\\", ".", "-"]:
+                search_chars += key
+                cli_table.idx = 0
+                typed = True
+            elif key == readchar.key.BACKSPACE:
+                search_chars = search_chars[:-1]
+                cli_table.idx = 0
+            elif key == readchar.key.UP:
+                cli_table.idx = max(0, cli_table.idx - 1)
                 # wrap to bottom if at the top
-                if current_idx < 0:
-                    current_idx = len(providers) - 1
+                if cli_table.idx < 0:
+                    cli_table.idx = len(providers) - 1
             elif key == readchar.key.DOWN:
-                current_idx = current_idx + 1
+                cli_table.idx = cli_table.idx + 1
                 # wrap to top if at the bottom
-                if current_idx >= len(providers):
-                    current_idx = 0
+                if cli_table.idx >= len(providers):
+                    cli_table.idx = 0
             elif key == readchar.key.CTRL_C:
                 # gracefully exit with no message
                 exit_with_error("")
             elif key == readchar.key.ENTER:
-                selected_provider = providers[current_idx]
+                selected_provider = selectable_providers[cli_table.idx]
 
-            live.update(build_table(current_idx, providers), refresh=True)
+            if typed:
+                # cli_table.window_bottom = 0
+                # cli_table.window_top = cli_table.window_size
+                selectable_providers = [
+                    p for p in selectable_providers if search_chars.lower() in p.lower()
+                ]
+                cli_table.window_bottom = None
+                cli_table.window_top = 0
+            else:
+                selectable_providers = [
+                    p for p in providers if search_chars.lower() in p.lower()
+                ]
+
+            cli_table.key_press = key
+            live.update(
+                build_table(selectable_providers, search_chars, app.console, cli_table),
+                refresh=True,
+            )
 
         return selected_provider
 
