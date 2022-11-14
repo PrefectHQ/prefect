65ccd06e7d17a09083838c19da88a35065667586
Chris Pickett <chris.pickett@prefect.io>
2022-11-14 16:53:43 -0500

    Add tests that demonstrate deadlocks (#7532)

---
+++ tests/engine/reliability/test_deadlocks.py
@@ -0,0 +1,52 @@
+import time
+
+import pytest
+
+from prefect import flow, task
+
+
+@task(name=f"{__name__}-add_one")
+def add_one(x: int) -> int:
+    return x + 1
+
+
+@task(name=f"{__name__}-subtract_ten")
+def subtract_ten(x: int) -> int:
+    return x - 10
+
+
+@task(name=f"{__name__}-sleep")
+def sleep(x: float):
+    time.sleep(x)
+
+
+@pytest.mark.skip(reason="Causes a deadlock.")
+def test_map_wait_for_many_tasks():
+    @flow
+    def run(n):
+        added = add_one.map(range(n))
+        result = [subtract_ten(x, wait_for=added) for x in range(n)]
+
+        return result
+
+    run(500)
+
+
+@pytest.mark.skip(reason="Causes a deadlock.")
+def test_loop_wait_for_many_tasks():
+    @flow
+    def run(n):
+        added = add_one.map(range(n))
+        for x in range(n):
+            subtract_ten.submit(x, wait_for=added)
+
+    run(500)
+
+
+@pytest.mark.skip(reason="Causes a deadlock.")
+def test_sleep_wait_for():
+    @flow
+    def run(sleep_time: float, n: int):
+        add_one.map(range(n), wait_for=[sleep.submit(sleep_time)])
+
+    run(5, 50)
