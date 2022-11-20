b95b1f69aa84aa0a916cdbcbde11594d83c674e2
Michael Adkins <michael@prefect.io>
2022-11-19 19:32:52 -0600

    Explore a second, user-space loop

--- poc.py
+++ poc.py
@@ -26,7 +26,25 @@ mt_logger.debug("Logging initialized")
 threadlocals = threading.local()
 threadlocals.runtime = None
 
-current_runtime = contextvars.ContextVar("current_runtime")
+current_runtime: contextvars.ContextVar["Runtime"] = contextvars.ContextVar(
+    "current_runtime"
+)
+
+
+import asyncio
+
+
+class Event(asyncio.Event):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self._loop is None:
+            self._loop = asyncio.get_event_loop()
+
+    def set(self):
+        self._loop.call_soon_threadsafe(super().set)
+
+    def clear(self):
+        self._loop.call_soon_threadsafe(super().clear)
 
 
 @dataclasses.dataclass
@@ -53,14 +71,20 @@ class WorkItem:
 class Runtime:
     def __init__(self) -> None:
         self._exit_stack = contextlib.ExitStack()
-        self._portal: anyio.abc.BlockingPortal = None
+        self._runtime_loop: anyio.abc.BlockingPortal = None
+        self._userspace_loop: anyio.abc.BlockingPortal = None
         self._work_queue = queue.Queue()
         self._wakeup = threading.Event()
         self._thread = threading.current_thread()
 
     def __enter__(self):
         self._exit_stack.__enter__()
-        self._portal = self._exit_stack.enter_context(anyio.start_blocking_portal())
+        self._runtime_loop = self._exit_stack.enter_context(
+            anyio.start_blocking_portal()
+        )
+        self._userspace_loop = self._exit_stack.enter_context(
+            anyio.start_blocking_portal()
+        )
         threadlocals.runtime = self
         self._context_token = current_runtime.set(self)
         return self
@@ -74,11 +98,13 @@ class Runtime:
         self,
         fn: typing.Callable[..., typing.Awaitable[T]],
         *args: typing.Any,
-        **kwargs: typing.Any
+        **kwargs: typing.Any,
     ) -> T:
         mt_logger.debug("Scheduling async task %s", fn)
         # Schedule the async task on a portal thread
-        future = self._portal.start_task_soon(functools.partial(fn, *args, **kwargs))
+        future = self._runtime_loop.start_task_soon(
+            functools.partial(fn, *args, **kwargs)
+        )
 
         # When the future is done, fire wakeup. This ensures that if the future does
         # not schedule any work, the main thread will move on.
@@ -94,7 +120,7 @@ class Runtime:
         self, fn: typing.Callable[..., T], *args: typing.Any, **kwargs: typing.Any
     ) -> T:
         """
-        Schedule work on the main thread from an async thread.
+        Schedule work on the main thread from the runtime thread.
 
         The async function must be running with `run_async`.
         """
@@ -104,7 +130,7 @@ class Runtime:
         work_item = WorkItem(future=future, fn=fn, args=args, kwargs=kwargs)
 
         # Wake up this thread when the future is complete
-        future.add_done_callback(lambda _: self._portal.call(event.set))
+        future.add_done_callback(lambda _: self._runtime_loop.call(event.set))
         self._work_queue.put(work_item)
 
         # Wake up the main thread
@@ -116,11 +142,46 @@ class Runtime:
 
         return future.result()
 
+    async def run_async_from_loop(
+        self, fn: typing.Callable[..., T], *args: typing.Any, **kwargs: typing.Any
+    ):
+        """
+        Schedule work on a user-space event loop from the runtime event loop.
+
+        The user may make blocking synchronous calls and we must be careful to avoid
+        blocking the runtime event loop. Even if the user's call does not perform IO,
+        it may need to block to perform
+        """
+        future = self._userspace_loop.start_task_soon(
+            functools.partial(fn, *args, **kwargs)
+        )
+        event = anyio.Event()
+        future.add_done_callback(lambda _: self._runtime_loop.call(event.set))
+        await event.wait()
+        return future.result()
+
+    def run_async_from_userspace(
+        self, fn: typing.Callable[..., T], *args: typing.Any, **kwargs: typing.Any
+    ):
+        """ """
+        future = self._runtime_loop.start_task_soon(
+            functools.partial(fn, *args, **kwargs)
+        )
+        # event = anyio.Event()
+        # future.add_done_callback(lambda _: self._runtime_loop.call(event.set))
+        # await event.wait()
+        # THIS BLOCKS THIS LOOP!
+        # This is fine until the runtime (which we just called into) needs to schedule
+        # another user-space task here. Really, this needs to be an awaitable for things
+        # to work
+        return future.result()
+
     def _consume_work_queue(self):
         """
         Wait for wakeup then get work from the work queue until it is empty.
         """
         mt_logger.debug("Waiting for wakeup event...")
+        # TODO: LOOKS LIKE A RACE CONDITION HERE SOMETIMES
         self._wakeup.wait()
 
         while True:
@@ -140,6 +201,9 @@ class Runtime:
 
     def _get_debug_info():
         pass
+
+    def in_sync_thread(self):
+        return self._thread.ident == threading.get_ident()
 
     @classmethod
     def print_debug_info(cls):
@@ -170,11 +234,25 @@ class Workflow:
 
 
 def entrypoint(workflow, parameters):
+    print(f"Entrypoint for {workflow.name!r}")
     runtime = current_runtime.get(None)
-    if inspect.iscoroutinefunction(workflow.fn) and not runtime:
+    if runtime:
+        print("In sync thread?", runtime.in_sync_thread())
+    if inspect.iscoroutinefunction(workflow.fn) and (
+        not runtime or not runtime.in_sync_thread()
+    ):
+        print("Returning coroutine")
         return workflow.fn(**parameters)
-    elif runtime:
+    elif runtime and runtime.in_sync_thread():
+        print("Running with runtime")
         return runtime.run_async(run_workflow, runtime, workflow, parameters)
+
+    elif runtime and not runtime.in_sync_thread():
+        print("Oh.. we're already in the async thread.. now what?")
+        return runtime.run_async_from_userspace(
+            run_workflow, runtime, workflow, parameters
+        )
+
     else:
         mt_logger.debug("Creating new runtime")
         with Runtime() as runtime:
@@ -196,9 +274,13 @@ async def orchestrate_run(
     run: Run,
     parameters: dict,
 ):
-
+    as_logger.debug("Orchestrating workflow %r", workflow.name)
+    runtime = current_runtime.get()
     if inspect.iscoroutinefunction(workflow.fn):
-        result = await workflow.fn(**parameters)
+        # Send user coroutines to another event loop that we do not "trust" in so the
+        # engine event loop is never blocked
+        print("Sending task to userspace loop")
+        result = await runtime.run_async_from_loop(workflow.fn, **parameters)
     else:
         result = await runtime.run_in_main_thread(workflow.fn, **parameters)
 
@@ -218,6 +300,7 @@ async def bar():
 
 
 def foobar():
+    print("Runing foobar")
     return foo() + bar()
 
 
@@ -225,14 +308,26 @@ async def afoobar():
     print("Running afoobar!")
     Runtime.print_debug_info()
     result = foo()
+    result = foo()
     aresult = await bar()
+    aresult = await bar()
+    foobar()
     return result + aresult
 
 
 def foofoobar():
+    print("Running foofoobar")
+    Runtime.print_debug_info()
+    # result = foobar()
+    aresult = afoobar()
+    return result + aresult
+
+
+async def afoofoobar():
+    print("Running afoofoobar")
     Runtime.print_debug_info()
     result = foobar()
-    aresult = afoobar()
+    aresult = await afoobar()
     return result + aresult
 
 
@@ -241,6 +336,7 @@ bar = Workflow(name="bar", fn=bar)
 foobar = Workflow(name="foobar", fn=foobar)
 afoobar = Workflow(name="afoobar", fn=afoobar)
 foofoobar = Workflow(name="foofoobar", fn=foofoobar)
+afoofoobar = Workflow(name="afoofoobar", fn=afoofoobar)
 
 print("---- Sync ----")
 result = foo()
@@ -262,3 +358,8 @@ print(result)
 print("---- Sync (double nested) ----")
 result = foofoobar()
 print(result)
+
+
+# print("---- Async (double nested) ----")
+# result = anyio.run(afoofoobar)
+# print(result)
