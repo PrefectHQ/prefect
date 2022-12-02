38046e4f54d432b35fe4fb12e89f189aeca5fd6b
Chris Pickett <chris.pickett@prefect.io>
2022-12-02 12:02:57 -0500

    Revert "Keep data from being lost when assigning a generator to `State.data`"
    This reverts commit 5224c44afffeafdc57cf88ffc8979a18c4953921.

--- tests/client/test_client_state_schema.py
+++
@@ -1,7 +0,0 @@
-from prefect.client.schemas import State
-from prefect.orion.schemas.states import StateType
-
-
-def test_handles_generator_as_data():
-    state = State(type=StateType.COMPLETED, data=(i for i in range(5)))
-    assert list(state.data) == [0, 1, 2, 3, 4]
