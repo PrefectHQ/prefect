d3c7a98ae67b7d1dd3067765950d4e97d2564394
Peyton <44583861+peytonrunyan@users.noreply.github.com>
2022-12-08 12:36:25 -0500

    fix formatting

--- tests/orion/services/test_flow_run_notifications.py
+++ tests/orion/services/test_flow_run_notifications.py
@@ -3,7 +3,7 @@ import sqlalchemy as sa
 
 from prefect.orion import models, schemas
 from prefect.orion.services.flow_run_notifications import FlowRunNotifications
-from prefect.settings import PREFECT_API_URL, PREFECT_UI_URL, temporary_settings
+from prefect.settings import PREFECT_UI_URL, temporary_settings
 
 
 @pytest.fixture
@@ -107,7 +107,9 @@ async def test_service_sends_notifications(
         ("http://some-url", "http://some-url"),
     ],
 )
-def test_get_ui_url_for_flow_run_id_with_ui_url(flow_run, provided_ui_url, expected_ui_url):
+def test_get_ui_url_for_flow_run_id_with_ui_url(
+    flow_run, provided_ui_url, expected_ui_url
+):
     with temporary_settings({PREFECT_UI_URL: provided_ui_url}):
         url = FlowRunNotifications(handle_signals=False).get_ui_url_for_flow_run_id(
             flow_run_id=flow_run.id
