6c032e0aec541aa0c8ff45bc1efbe9607ea3aa90
ahuang11 <andrew.h@prefect.io>
2022-12-12 11:07:23 -0800

    Isolate job block

--- src/prefect/blocks/abstract.py
+++ src/prefect/blocks/abstract.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Any, List, Tuple
 
 from prefect import get_run_logger
 from prefect.blocks.core import Block
@@ -67,92 +66,4 @@ class JobBlock(Block, ABC):
         """
         Triggers a job run using the input job ID in the external
         service and returns a JobRun object.
-        """
-
-
-class DatabaseBlock(Block, ABC):
-    """
-    The DatabaseBlock class represents a resource for interacting with a database.
-    Heavily influenced by [PEP 249](https://peps.python.org/pep-0249/).
-    Skewed towards RDBMS systems and a separate interface may be necessary
-    for non relational databases.
-
-    These blocks have the option to compose a credentials block or accept
-    credentials directly. It depends on how tightly coupled the database
-    configuration is to the credentials configuration.
-    """
-
-    @property
-    def logger(self):
-        """
-        Returns a logger based on whether the JobBlock
-        is called from within a flow or task run context.
-        If a run context is present, the logger property returns a run logger.
-        Else, it returns a default logger labeled with the class's name.
-        """
-        try:
-            return get_run_logger()
-        except MissingContextError:
-            return get_logger(self.__class__.__name__)
-
-    @abstractmethod
-    async def fetch_one(self, operation, parameters, **execution_kwargs) -> Tuple[Any]:
-        """
-        Fetch a single result from the database.
-
-        Args:
-            operation: The SQL query or other operation to be executed.
-            parameters: The parameters for the operation.
-            **execution_kwargs: Additional keyword arguments to pass to execute.
-
-        Returns:
-            A list of tuples containing the data returned by the database,
-                where each row is a tuple and each column is a value in the tuple.
-        """
-
-    @abstractmethod
-    async def fetch_many(
-        self, operation, parameters, limit, **execution_kwargs
-    ) -> List[Tuple[Any]]:
-        """
-        Fetch a limited number of results from the database.
-
-        Args:
-            operation: The SQL query or other operation to be executed.
-            parameters: The parameters for the operation.
-            limit: The number of results to return.
-            **execution_kwargs: Additional keyword arguments to pass to execute.
-
-        Returns:
-            A list of tuples containing the data returned by the database,
-                where each row is a tuple and each column is a value in the tuple.
-        """
-
-    @abstractmethod
-    async def fetch_all(
-        self, operation, parameters, **execution_kwargs
-    ) -> List[Tuple[Any]]:
-        """
-        Fetch all results from the database.
-
-        Args:
-            operation: The SQL query or other operation to be executed.
-            parameters: The parameters for the operation.
-            **execution_kwargs: Additional keyword arguments to pass to execute.
-
-        Returns:
-            A list of tuples containing the data returned by the database,
-                where each row is a tuple and each column is a value in the tuple.
-        """
-
-    @abstractmethod
-    async def execute(self, operation, parameters, **execution_kwargs) -> None:
-        """
-        Executes an operation on the database. This method is intended to be used
-        for operations that do not return data, such as INSERT, UPDATE, or DELETE.
-
-        Args:
-            operation: The SQL query or other operation to be executed.
-            parameters: The parameters for the operation.
-            **execution_kwargs: Additional keyword arguments to pass to execute.
         """
