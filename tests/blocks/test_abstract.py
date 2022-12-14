3686445c8925e1180e8b6742ec6990bc51845f40
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-12-14 11:26:22 -0800

    Abstract database block (#7866)
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>
    Co-authored-by: Bill Palombi <bill@prefect.io>

--- tests/blocks/test_abstract.py
+++ tests/blocks/test_abstract.py
@@ -1,6 +1,6 @@
 import pytest
 
-from prefect.blocks.abstract import JobBlock, JobRun
+from prefect.blocks.abstract import DatabaseBlock, JobBlock, JobRun
 from prefect.exceptions import PrefectException
 
 
@@ -61,3 +61,98 @@ class TestJobBlock:
         record_2 = caplog.records[1]
         assert record_2.name == "prefect.AJobRun"
         assert record_2.msg == "Job run completed."
+
+
+class TestDatabaseBlock:
+    def test_database_block_is_abstract(self):
+        with pytest.raises(
+            TypeError, match="Can't instantiate abstract class DatabaseBlock"
+        ):
+            DatabaseBlock()
+
+    async def test_database_block_implementation(self, caplog):
+        class ADatabaseBlock(DatabaseBlock):
+            def __init__(self):
+                self._results = tuple(
+                    zip(["apple", "banana", "cherry"], [1, 2, 3], [True, False, True])
+                )
+                self._engine = None
+
+            def fetch_one(self, operation, parameters=None, **execution_kwargs):
+                self.logger.info(f"Fetching one result using {parameters}.")
+                return self._results[0]
+
+            def fetch_many(
+                self, operation, parameters=None, size=None, **execution_kwargs
+            ):
+                self.logger.info(f"Fetching {size} results using {parameters}.")
+                return self._results[:size]
+
+            def fetch_all(self, operation, parameters=None, **execution_kwargs):
+                self.logger.info(f"Fetching all results using {parameters}.")
+                return self._results
+
+            def execute(self, operation, parameters=None, **execution_kwargs) -> None:
+                self.logger.info(f"Executing operation using {parameters}.")
+
+            def execute_many(
+                self, operation, seq_of_parameters, **execution_kwargs
+            ) -> None:
+                self.logger.info(
+                    f"Executing many operations using {seq_of_parameters}."
+                )
+
+            def __enter__(self):
+                self._engine = True
+                return self
+
+            def __exit__(self, *args):
+                self._engine = None
+
+        a_database_block = ADatabaseBlock()
+        parameters = {"a": "b"}
+        assert a_database_block.fetch_one(
+            "SELECT * FROM table", parameters=parameters
+        ) == ("apple", 1, True)
+        assert a_database_block.fetch_many(
+            "SELECT * FROM table", size=2, parameters=parameters
+        ) == (("apple", 1, True), ("banana", 2, False))
+        assert a_database_block.fetch_all(
+            "SELECT * FROM table", parameters=parameters
+        ) == (("apple", 1, True), ("banana", 2, False), ("cherry", 3, True))
+        assert (
+            a_database_block.execute(
+                "INSERT INTO table VALUES (1, 2, 3)", parameters=parameters
+            )
+            is None
+        )
+        assert (
+            a_database_block.execute_many(
+                "INSERT INTO table VALUES (1, 2, 3)",
+                seq_of_parameters=[parameters, parameters],
+                parameters=parameters,
+            )
+            is None
+        )
+
+        records = caplog.records
+        for record in records:
+            assert record.name == "prefect.ADatabaseBlock"
+        assert records[0].message == "Fetching one result using {'a': 'b'}."
+        assert records[1].message == "Fetching 2 results using {'a': 'b'}."
+        assert records[2].message == "Fetching all results using {'a': 'b'}."
+        assert records[3].message == "Executing operation using {'a': 'b'}."
+        assert (
+            records[4].message
+            == "Executing many operations using [{'a': 'b'}, {'a': 'b'}]."
+        )
+
+        # test context manager
+        with a_database_block as db:
+            assert db._engine is True
+        assert a_database_block._engine is None
+
+        match = "ADatabaseBlock does not support async context management."
+        with pytest.raises(NotImplementedError, match=match):
+            async with a_database_block:
+                pass
