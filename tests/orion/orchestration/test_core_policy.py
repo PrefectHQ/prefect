880e2c75ef6e1fd7364cb6e69fc832da348b6dc7
Chris Pickett <chris.pickett@prefect.io>
2022-12-14 15:27:15 -0500

    Fix heisentest in test_core_policy

--- tests/orion/orchestration/test_core_policy.py
+++ tests/orion/orchestration/test_core_policy.py
@@ -1772,7 +1772,11 @@ class TestTaskConcurrencyLimits:
                 mutated_state.type = random.choice(
                     list(
                         set(states.StateType)
-                        - {initial_state.type, proposed_state.type}
+                        - {
+                            initial_state.type,
+                            proposed_state.type,
+                            states.StateType.CANCELLING,
+                        }
                     )
                 )
                 await self.reject_transition(
