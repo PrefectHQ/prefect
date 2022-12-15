bad2b6f9db020e66150c9547c58f0ac5d5185e9b
Peyton <44583861+peytonrunyan@users.noreply.github.com>
2022-12-15 13:08:01 -0500

    Handle creating and executing deployments that rely on scripts that have flow calls in them (#7817)

--- src/prefect/exceptions.py
+++ src/prefect/exceptions.py
@@ -120,7 +120,7 @@ class ScriptError(PrefectException):
         user_exc: Exception,
         path: str,
     ) -> None:
-        message = f"Script at {str(path)!r} encountered an exception"
+        message = f"Script at {str(path)!r} encountered an exception: {user_exc!r}"
         super().__init__(message)
         self.user_exc = user_exc
 
