ad232e9013886b0625c10bae4acc0759089b5ecc
Michael Adkins <michael@prefect.io>
2022-12-16 13:33:58 -0600

    Improve `Event` implementation consistency and fix deadlock (#7897)

--- src/prefect/_internal/concurrency/primitives.py
+++ src/prefect/_internal/concurrency/primitives.py
@@ -2,10 +2,14 @@
 Thread-safe async primitives.
 """
 import asyncio
+import collections
 import concurrent.futures
+import threading
 from typing import Generic, Optional, TypeVar
 
-from prefect._internal.concurrency.event_loop import call_in_loop, get_running_loop
+from typing_extensions import Literal
+
+from prefect._internal.concurrency.event_loop import call_soon_in_loop, get_running_loop
 
 T = TypeVar("T")
 
@@ -13,48 +17,66 @@ T = TypeVar("T")
 class Event:
     """
     A thread-safe async event.
+
+    Unlike `asyncio.Event` this implementation does not bind to a loop on creation. This
+    matches the behavior of `asyncio.Event` in Python 3.10+, but differs from earlier
+    versions.
+
+    This event also does not support a `clear()` operation. This matches the behavior of
+    `anyio.Event` types and prevents sneaky bugs; create a new event instead.
     """
 
     def __init__(self) -> None:
-        self._loop = get_running_loop()
-
-        # For compatibility with Python <3.10, do not create the event until a loop
-        # is present
-        if self._loop:
-            self._event = asyncio.Event()
-
-        self._is_set = False
+        self._waiters = collections.deque()
+        self._value = False
+        self._lock = threading.Lock()
 
     def set(self) -> None:
-        """Set the flag, notifying all listeners."""
-        self._is_set = True
-        if self._loop:
-            if self._loop != get_running_loop():
-                call_in_loop(self._loop, self._event.set)
-            else:
-                self._event.set()
-
-    def is_set(self) -> bool:
-        """Return ``True`` if the flag is set, ``False`` if not."""
-        return self._is_set
-
-    async def wait(self) -> None:
         """
-        Wait until the flag has been set.
+        Set the flag, notifying all waiters.
 
-        If the flag has already been set when this method is called, it returns immediately.
+        Unlike `asyncio.Event`, waiters may not be notified immediately when this is
+        called; instead, notification will be placed on the owning loop of each waiter
+        for thread safety.
         """
-        if self._is_set:
-            return
+        with self._lock:
+            if not self._value:
+                self._value = True
 
-        if not self._loop:
-            self._loop = get_running_loop()
-            self._event = asyncio.Event()
+                # We freeze the waiters queue during iteration so removal in `wait()`
+                # does not change the size during iteration. The lock ensures that no
+                # waiters are added until after we finish here.
+                for fut in tuple(self._waiters):
+                    if not fut.done():
+                        # The `asyncio.Future.set_result` method is not thread-safe
+                        # and must be run in the loop that owns the future
+                        call_soon_in_loop(fut._loop, fut.set_result, True)
 
-        await self._event.wait()
+    def is_set(self):
+        return self._value
 
-    def __repr__(self) -> str:
-        return f"Event<loop={id(self._loop)}, is_set={self._is_set}>"
+    async def wait(self) -> Literal[True]:
+        """
+        Block until the internal flag is true.
+
+        If the internal flag is true on entry, return True immediately.
+        Otherwise, block until another `set()` is called, then return True.
+        """
+        # Taking a sync lock in an async context is generally not recommended, but this
+        # lock should only ever be held very briefly and we need to prevent race
+        # conditions during between `set()` and `wait()`
+        with self._lock:
+            if self._value:
+                return True
+
+            fut = asyncio.get_running_loop().create_future()
+            self._waiters.append(fut)
+
+        try:
+            await fut
+            return True
+        finally:
+            self._waiters.remove(fut)
 
 
 class Future(concurrent.futures.Future, Generic[T]):
