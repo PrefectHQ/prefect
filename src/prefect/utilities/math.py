53e9f622fccb35998fc4e790f527b1708855db7f
Dustin Ngo <dustin.ngo@gmail.com>
2022-12-19 23:40:09 -0500

    Improve poisson process generation

--- src/prefect/utilities/math.py
+++ src/prefect/utilities/math.py
@@ -2,25 +2,50 @@ import math
 import random
 
 
-def poisson_interval(average_interval):
+def poisson_interval(average_interval, lower=0, upper=1):
+    """
+    Generates an "inter-arrival time" for a Poisson process.
+
+    Draws a random variable from an exponential distribution using the inverse-CDF
+    method. Can optionally be passed a lower and upper bound between (0, 1] to clamp
+    the potential output values.
+    """
+
     # note that we ensure the argument to the logarithm is stabilized to prevent
     # calling log(0), which results in a DomainError
-    return -math.log(max(1 - random.random(), 1e-10)) * average_interval
+    return -math.log(max(1 - random.uniform(lower, upper), 1e-10)) * average_interval
+
+
+def exponential_cdf(x, average_interval):
+    ld = 1 / average_interval
+    return 1 - math.exp(-ld * x)
+
+
+def lower_clamping_factor(k):
+    """
+    Computes a lower clamping factor that can be used to bound a random variate drawn
+    from an exponential distribution.
+
+    Given an upper clamping factor `k` (and corresponding upper bound k * average_interval),
+    this function computes a lower clamping factor `c` that corresponds to a lower bound
+    (c * average_interval) where the probability mass between the lower bound and the
+    median is equal to the probability mass between the median and the upper bound.
+    """
+
+    return math.log(max(2**k / (2**k - 1), 1e-10), 2)
 
 
 def clamped_poisson_interval(average_interval, clamping_factor=0.3):
     """
-    Clamps Poisson intervals to a range defined by the clamping_factor
+    Bounds Poisson "inter-arrival times" to a range defined by the clamping factor.
 
-    Note that because the distribution of Poisson intervals is not symmetric around the
-    average interval, a symmetric clamp with slightly skew the mean interval.
+    The upper bound for this random variate is average_interval * (1 + clamping_factor).
+    A lower bound is picked so that the average interval remains fixed.
     """
+    k = 1 + clamping_factor
+    upper_bound = average_interval * k
+    lower_bound = max(0, average_interval * lower_clamping_factor(k))
 
-    interval = poisson_interval(average_interval)
-    while not (
-        average_interval * (1 - clamping_factor)
-        < interval
-        < average_interval * (1 + clamping_factor)
-    ):
-        interval = poisson_interval(average_interval)
-    return interval
+    upper_rv = exponential_cdf(upper_bound, average_interval)
+    lower_rv = exponential_cdf(lower_bound, average_interval)
+    return poisson_interval(average_interval, lower_rv, upper_rv)
--- tests/utilities/test_math.py
+++ tests/utilities/test_math.py
@@ -1,3 +1,5 @@
+import pytest
+
 from prefect.utilities.math import clamped_poisson_interval, poisson_interval
 
 
@@ -8,20 +10,17 @@ def test_poisson_intervals():
     assert expected_average * 0.95 < observed_average < expected_average * 1.05
 
 
-def test_clamped_poisson_intervals():
+@pytest.mark.parametrize("clamping_factor", [0.1, 0.2, 0.5, 1, 10, 100])
+def test_clamped_poisson_intervals(clamping_factor):
     expected_average = 42
     bunch_of_intervals = [
-        clamped_poisson_interval(expected_average, clamping_factor=0.3)
+        clamped_poisson_interval(expected_average, clamping_factor=clamping_factor)
         for _ in range(100_000)
     ]
     observed_average = sum(bunch_of_intervals) / len(bunch_of_intervals)
 
-    assert expected_average * 0.9 < observed_average < expected_average * 1.1
+    assert expected_average * 0.95 < observed_average < expected_average * 1.05
 
     assert max(bunch_of_intervals) < expected_average * (
-        1 + 0.3
+        1 + clamping_factor
     ), "no intervals should exceed the upper clamp limit"
-
-    assert min(bunch_of_intervals) > expected_average * (
-        1 - 0.3
-    ), "no intervals should be smaller than the lower clamp limit"
