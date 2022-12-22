8ff6f553c88e79d29c254f309da70b1e6b889bd3
Marichka Offen <marichka@prefect.io>
2022-12-22 16:04:08 -0500

    Update WorkerPool.vue

--- orion-ui/src/pages/WorkerPool.vue
+++ orion-ui/src/pages/WorkerPool.vue
@@ -1,18 +1,56 @@
 <template>
-  <p-layout-default v-if="workerPool" class="worker-pool">
+  <p-layout-well v-if="workerPool" class="worker-pool">
     <template #header>
       <PageHeadingWorkerPool :worker-pool="workerPool" />
     </template>
-  </p-layout-default>
+
+    <p-tabs :tabs="tabs">
+      <template #details>
+        details
+        <!-- <WorkerPoolDetails :worker-pool="workerPool" /> -->
+      </template>
+
+      <template #runs>
+        Runs
+        <!-- <WorkerPoolRunFilteredList :worker-pool-filter="workerPoolFilter" /> -->
+      </template>
+
+      <template #queues>
+        Queues
+        <!-- <WorkerPoolQueueFilteredList :worker-pool-filter="workerPoolFilter" /> -->
+      </template>
+
+      <template #workers>
+        Workers
+        <!-- <WorkerPoolWorkerFilteredList :worker-pool-filter="workerPoolFilter" /> -->
+      </template>
+    </p-tabs>
+
+    <template #well>
+      Details
+      <!-- <WorkerPoolDetails :worker-pool="workerPool" /> -->
+    </template>
+  </p-layout-well>
 </template>
 
 <script lang="ts" setup>
   import { useWorkspaceApi, PageHeadingWorkerPool } from '@prefecthq/orion-design'
+  import { media } from '@prefecthq/prefect-design'
   import { useRouteParam, useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
 
   const api = useWorkspaceApi()
   const workerPoolName = useRouteParam('workerPoolName')
+
+  const tabs = computed(() => {
+    const values = ['Runs', 'Queues', 'Workers']
+
+    if (!media.xl) {
+      values.unshift('Details')
+    }
+
+    return values
+  })
 
   const subscriptionOptions = {
     interval: 300000,
