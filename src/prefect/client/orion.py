958b20fbe333c654020ade6e8496120a013c121a
Dustin Ngo <dustin.ngo@gmail.com>
2022-12-22 13:14:58 -0500

    Implement configurable retry delays and exponential backoff (#7954)
    Co-authored-by: Michael Adkins <michael@prefect.io>

--- src/prefect/client/orion.py
+++ src/prefect/client/orion.py
@@ -1642,6 +1642,7 @@ class OrionClient:
             empirical_policy=schemas.core.TaskRunPolicy(
                 retries=task.retries,
                 retry_delay=task.retry_delay_seconds,
+                retry_jitter_factor=task.retry_jitter_factor,
             ),
             state=state.to_state_create(),
             task_inputs=task_inputs or {},
