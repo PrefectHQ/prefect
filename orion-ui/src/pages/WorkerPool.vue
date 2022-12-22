03f07a2b55e689859431a3ae11b178934496e40a
Marichka Offen <marichka@prefect.io>
2022-12-22 15:50:30 -0500

    start work on Worker Pool page

--- orion-ui/src/pages/WorkerPool.vue
+++ orion-ui/src/pages/WorkerPool.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>This is Worker POOL details page</h1>
+  <p-layout-default v-if="workerPool" class="worker-pool">
+    <template #header>
+      <PageHeadingWorkerPool :worker-pool="workerPool" />
+    </template>
+  </p-layout-default>
 </template>
+
+<script lang="ts" setup>
+  import { useWorkspaceApi, PageHeadingWorkerPool } from '@prefecthq/orion-design'
+  import { useRouteParam, useSubscription } from '@prefecthq/vue-compositions'
+  import { computed } from 'vue'
+
+  const api = useWorkspaceApi()
+  const workerPoolName = useRouteParam('workerPoolName')
+
+  const subscriptionOptions = {
+    interval: 300000,
+  }
+  const workerPoolSubscription = useSubscription(api.workerPools.getWorkerPoolByName, [workerPoolName.value], subscriptionOptions)
+  const workerPool = computed(() => workerPoolSubscription.response)
+</script>
