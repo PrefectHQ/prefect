1a92672f42deba852a57b3ce348fe7a5668e5377
masonmenges <mason.menges2@gmail.com>
2022-12-23 10:52:33 -0700

    add tests for orion status command

---
+++ tests/cli/test_orion.py
@@ -0,0 +1,50 @@
+from prefect.testing.cli import invoke_and_assert
+from prefect.testing.utilities import AsyncMock
+
+
+def test_orion_status_healthy():
+    invoke_and_assert(["orion", "status"], expected_output_contains="Orion Healthy")
+
+
+def test_orion_status_unhealthy(monkeypatch):
+    async def mock_api_healthcheck(*args):
+        return Exception("All connection attempts failed")
+
+    monkeypatch.setattr(
+        "prefect.client.OrionClient.api_healthcheck", mock_api_healthcheck
+    )
+    invoke_and_assert(
+        ["orion", "status"], expected_output_contains="All connection attempts failed"
+    )
+
+
+def test_orion_status_wait_arg(monkeypatch):
+    retry_response = "All connection attempts failed"
+    mock_waiting_healthcheck = AsyncMock()
+
+    monkeypatch.setattr(
+        "prefect.client.OrionClient.api_healthcheck", mock_waiting_healthcheck
+    )
+    mock_waiting_healthcheck.side_effect = [
+        retry_response,
+        retry_response,
+        retry_response,
+        None,
+    ]
+
+    invoke_and_assert(
+        ["orion", "status", "--wait"], expected_output_contains="Orion Healthy"
+    )
+
+
+def test_orion_status_timeout(monkeypatch):
+    async def mock_api_healthcheck(*args):
+        return Exception("All connection attempts failed")
+
+    monkeypatch.setattr(
+        "prefect.client.OrionClient.api_healthcheck", mock_api_healthcheck
+    )
+    invoke_and_assert(
+        ["orion", "status", "--wait", "--timeout", 1],
+        expected_output_contains="Timeout exceeded, All connection attempts failed",
+    )
