8e3686a36e900eb9f19f92c734ceef0e1d002c0a
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-27 16:26:58 -0600

    replaced local guard with isNotNullish from orion-design (#7994)

--- orion-ui/src/maps/uiSettings.ts
+++ orion-ui/src/maps/uiSettings.ts
@@ -1,15 +1,11 @@
+import { isNotNullish } from '@prefecthq/orion-design'
 import { MapFunction } from '@/services/mapper'
 import { Settings } from '@/services/uiSettings'
 import { SettingsResponse } from '@/types/settingsResponse'
-import { FeatureFlag } from '@/utilities/permissions'
 
 export const mapSettingsResponseToSettings: MapFunction<SettingsResponse, Settings> = function(source) {
   return {
     apiUrl: source.api_url,
-    flags: this.map('FlagResponse', source.flags, 'Flag').filter(flagIsDefined),
+    flags: this.map('FlagResponse', source.flags, 'FeatureFlag').filter(isNotNullish),
   }
-}
-
-function flagIsDefined(flag: FeatureFlag | null): flag is FeatureFlag {
-  return flag !== null
 }
