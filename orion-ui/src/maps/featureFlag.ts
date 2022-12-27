b07695e3434e6ef3cb23f0b01e26733c59ec6195
Evan Sutherland <stackoverfloweth@gmail.com>
2022-12-27 13:52:52 -0600

    code review suggestion

--- orion-ui/src/maps/featureFlag.ts
+++ orion-ui/src/maps/featureFlag.ts
@@ -2,11 +2,11 @@ import { MapFunction } from '@/services/mapper'
 import { FlagResponse } from '@/types/flagResponse'
 import { FeatureFlag } from '@/utilities/permissions'
 
-export const mapFlagResponseToFeatureFlag: MapFunction<FlagResponse, FeatureFlag> = (source) => {
+export const mapFlagResponseToFeatureFlag: MapFunction<FlagResponse, FeatureFlag | null> = (source) => {
   switch (source) {
     // case 'magic':
     //   return 'access:magic'
     default:
-      throw new Error(`Invalid FlagResponse in mapper ${source}`)
+      return null
   }
 }
--- orion-ui/src/maps/uiSettings.ts
+++ orion-ui/src/maps/uiSettings.ts
@@ -1,10 +1,15 @@
 import { MapFunction } from '@/services/mapper'
 import { Settings } from '@/services/uiSettings'
 import { SettingsResponse } from '@/types/settingsResponse'
+import { FeatureFlag } from '@/utilities/permissions'
 
 export const mapSettingsResponseToSettings: MapFunction<SettingsResponse, Settings> = function(source) {
   return {
     apiUrl: source.api_url,
-    flags: this.map('FlagResponse', source.flags, 'Flag'),
+    flags: this.map('FlagResponse', source.flags, 'Flag').filter(flagIsDefined),
   }
+}
+
+function flagIsDefined(flag: FeatureFlag | null): flag is FeatureFlag {
+  return flag !== null
 }
