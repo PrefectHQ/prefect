ee363846122d6533493a798378d9cec3e841f103
Alexander Streed <alex.s@prefect.io>
2022-12-28 10:59:48 -0600

    Removes deployment changes

--- src/prefect/deployments.py
+++ src/prefect/deployments.py
@@ -5,8 +5,7 @@ Objects for specifying deployments and utilities for loading flows from deployme
 import importlib
 import json
 import sys
-from datetime import datetime, timezone
-from functools import partial
+from datetime import datetime
 from pathlib import Path
 from typing import Any, Dict, Iterable, List, Optional
 from uuid import UUID
@@ -276,7 +275,6 @@ class Deployment(BaseModel):
             "description",
             "version",
             "work_queue_name",
-            "worker_pool_queue_id",
             "tags",
             "parameters",
             "schedule",
@@ -377,7 +375,7 @@ class Deployment(BaseModel):
         description="The work queue for the deployment.",
         yaml_comment="The work queue that will handle this deployment's runs",
     )
-    worker_pool_queue_id: Optional[UUID] = Field(default=None)
+
     # flow data
     parameters: Dict[str, Any] = Field(default_factory=dict)
     manifest_path: Optional[str] = Field(
@@ -405,7 +403,6 @@ class Deployment(BaseModel):
         default_factory=ParameterSchema,
         description="The parameter schema of the flow, including defaults.",
     )
-    timestamp: datetime = Field(default_factory=partial(datetime.now, tz=timezone.utc))
 
     @validator("infrastructure", pre=True)
     def infrastructure_must_have_capabilities(cls, value):
@@ -503,8 +500,6 @@ class Deployment(BaseModel):
                     {"infrastructure", "storage"}
                 )
                 for field in set(self.__fields__.keys()) - excluded_fields:
-                    if field == "timestamp":
-                        continue
                     new_value = getattr(deployment, field)
                     setattr(self, field, new_value)
 
@@ -637,7 +632,6 @@ class Deployment(BaseModel):
                 flow_id=flow_id,
                 name=self.name,
                 work_queue_name=self.work_queue_name,
-                worker_pool_queue_id=self.worker_pool_queue_id,
                 version=self.version,
                 schedule=self.schedule,
                 parameters=self.parameters,
