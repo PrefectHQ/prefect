901ce3d36cdf872925cc9f8c5c834b39e55289c0
Alexander Streed <alex.s@prefect.io>
2023-01-06 09:03:07 -0600

    Adds tests for worker CLI

--- src/prefect/experimental/cli/worker.py
+++ src/prefect/experimental/cli/worker.py
@@ -3,12 +3,13 @@ from functools import partial
 import anyio
 import typer
 
-from prefect.cli._types import PrefectTyper
+from prefect.cli._types import PrefectTyper, SettingsOption
 from prefect.cli._utilities import exit_with_error
 from prefect.cli.root import app
 from prefect.experimental.workers.base import BaseWorker
 from prefect.settings import (
     PREFECT_WORKER_HEARTBEAT_SECONDS,
+    PREFECT_WORKER_PREFETCH_SECONDS,
     PREFECT_WORKER_QUERY_SECONDS,
     PREFECT_WORKER_WORKFLOW_STORAGE_SCAN_SECONDS,
 )
@@ -32,6 +33,8 @@ async def start(
     worker_type: str = typer.Option(
         "process", "-t", "--type", help="The type of worker to start."
     ),
+    prefetch_seconds: int = SettingsOption(PREFECT_WORKER_PREFETCH_SECONDS),
+    run_once: bool = typer.Option(False, help="Run worker loops only one time."),
     limit: int = typer.Option(
         None,
         "-l",
@@ -47,8 +50,12 @@ async def start(
             "Please ensure that you have installed this worker type on this machine."
         )
     async with Worker(
-        name=worker_name, worker_pool_name=worker_pool_name, limit=limit
+        name=worker_name,
+        worker_pool_name=worker_pool_name,
+        limit=limit,
+        prefetch_seconds=prefetch_seconds,
     ) as worker:
+        app.console.print(f"Worker {worker.name!r} started!")
         async with anyio.create_task_group() as tg:
             # wait for an initial heartbeat to configure the worker
             await worker.sync_with_backend()
@@ -60,7 +67,8 @@ async def start(
                     critical_service_loop,
                     workload=worker.get_and_submit_flow_runs,
                     interval=PREFECT_WORKER_QUERY_SECONDS.value(),
-                    printer=worker._logger.debug,
+                    run_once=run_once,
+                    printer=app.console.print,
                 )
             )
             # schedule the sync loop
@@ -69,7 +77,8 @@ async def start(
                     critical_service_loop,
                     workload=worker.sync_with_backend,
                     interval=PREFECT_WORKER_HEARTBEAT_SECONDS.value(),
-                    printer=worker._logger.debug,
+                    run_once=run_once,
+                    printer=app.console.print,
                 )
             )
             # schedule the storage scan loop
@@ -78,6 +87,9 @@ async def start(
                     critical_service_loop,
                     workload=worker.scan_storage_for_deployments,
                     interval=PREFECT_WORKER_WORKFLOW_STORAGE_SCAN_SECONDS.value(),
-                    printer=worker._logger.debug,
+                    run_once=run_once,
+                    printer=app.console.print,
                 )
             )
+
+    app.console.print(f"Worker {worker.name!r} stopped!")
