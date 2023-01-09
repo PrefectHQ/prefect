64cede6d6d9a89bcfc092b4a61be63200f1472c9
Alexander Streed <alex.s@prefect.io>
2023-01-09 12:22:41 -0600

    Fixes apply

--- flows/deployment.py
+++ flows/deployment.py
@@ -10,14 +10,13 @@ def hello(name: str = "world"):
 if __name__ == "__main__":
     # Create deployment
     if hasattr(Deployment, "build_from_flow"):
-        deployment = Deployment.build_from_flow(
-            flow=hello, name="test-deployment", apply=True
-        )
+        deployment = Deployment.build_from_flow(flow=hello, name="test-deployment")
     else:
         deployment = Deployment(
             name="test-deployment",
             flow_name=hello.name,
         )
+    deployment.apply()
 
     # Update deployment
     deployment.tags = ["test"]
