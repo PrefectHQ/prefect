6724e93b83b1f76c9f46e3a51d8b727da6a63cdf
Alexander Streed <alex.s@prefect.io>
2023-01-09 13:37:24 -0600

    Bifurcates script on version

--- flows/deployment.py
+++ flows/deployment.py
@@ -1,4 +1,9 @@
+import anyio
+from packaging.version import Version
+
+import prefect
 from prefect import flow, get_run_logger
+from prefect.client.orion import get_client
 from prefect.deployments import Deployment
 from prefect.utilities.callables import parameter_schema
 
@@ -8,18 +13,33 @@ def hello(name: str = "world"):
     get_run_logger().info(f"Hello {name}!")
 
 
+async def apply_deployment(deployment):
+    async with get_client() as client:
+        flow_id = await client.create_flow_from_name(deployment.flow_name)
+        await client.create_deployment(flow_id=flow_id, name=deployment.name)
+
+
+async def update_deployment(deployment):
+    async with get_client() as client:
+        await client.update_deployment(deployment=deployment)
+
+
 if __name__ == "__main__":
     # Create deployment
-    if hasattr(Deployment, "build_from_flow"):
-        deployment = Deployment.build_from_flow(flow=hello, name="test-deployment")
-    else:
+    if Version(prefect.__version__) < Version("2.1.0"):
         deployment = Deployment(
             name="test-deployment",
             flow_name=hello.name,
             parameter_openapi_schema=parameter_schema(hello),
         )
-    deployment.apply()
+        anyio.run(apply_deployment, deployment)
+    else:
+        deployment = Deployment.build_from_flow(flow=hello, name="test-deployment")
+        deployment.apply()
 
     # Update deployment
     deployment.tags = ["test"]
-    deployment.apply()
+    if Version(prefect.__version__) < Version("2.1.0"):
+        anyio.run(update_deployment, deployment)
+    else:
+        deployment.apply()
