bd6049c1585ddc8810c887c62195920425e2fac2
Dustin Ngo <dustin.ngo@gmail.com>
2023-01-18 02:45:26 -0500

    Break up artifact migration into components

---
+++ src/prefect/orion/database/migrations/versions/postgresql/2023_01_13_045501_2882cd2df464_migrate_artifact_data.py
@@ -0,0 +1,88 @@
+"""Migrates state data to the artifact table
+
+Revision ID: 2882cd2df464
+Revises: 2882cd2df463
+Create Date: 2023-01-13 04:55:01.358638
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+import prefect
+
+# revision identifiers, used by Alembic.
+revision = "2882cd2df464"
+down_revision = "2882cd2df463"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    ### START DATA MIGRATION
+
+    # insert nontrivial task run state results into the artifact table
+    def update_task_run_artifact_data_in_batches(batch_size, offset):
+        return f"""
+            INSERT INTO artifact (task_run_state_id, task_run_id, artifact_data)
+            SELECT id, task_run_id, _data
+            FROM task_run_state
+            WHERE _data != 'null' AND _data IS NOT NULL
+            LIMIT {batch_size} OFFSET {offset};
+        """
+
+    # backpopulate the result artifact id on the task run state table
+    def update_task_run_state_from_artifact_id_in_batches(batch_size, offset):
+        return f"""
+            UPDATE task_run_state
+            SET result_artifact_id = (SELECT id FROM artifact WHERE task_run_state.id = task_run_state_id)
+            WHERE task_run_state.id in (SELECT id FROM task_run_state WHERE (_data != 'null' AND _data IS NOT NULL) AND (result_artifact_id IS NULL) LIMIT {batch_size});
+        """
+
+    # insert nontrivial flow run state results into the artifact table
+    def update_flow_run_artifact_data_in_batches(batch_size, offset):
+        return f"""
+            INSERT INTO artifact (flow_run_state_id, flow_run_id, artifact_data)
+            SELECT id, flow_run_id, _data
+            FROM flow_run_state
+            WHERE _data != 'null' AND _data IS NOT NULL
+            LIMIT {batch_size} OFFSET {offset};
+        """
+
+    # backpopulate the result artifact id on the flow run state table
+    def update_flow_run_state_from_artifact_id_in_batches(batch_size, offset):
+        return f"""
+            UPDATE flow_run_state
+            SET result_artifact_id = (SELECT id FROM artifact WHERE flow_run_state.id = flow_run_state_id)
+            WHERE flow_run_state.id in (SELECT id FROM flow_run_state WHERE (_data != 'null' AND _data IS NOT NULL) AND (result_artifact_id IS NULL) LIMIT {batch_size});
+        """
+
+    data_migration_queries = [
+        update_task_run_artifact_data_in_batches,
+        update_task_run_state_from_artifact_id_in_batches,
+        update_flow_run_artifact_data_in_batches,
+        update_flow_run_state_from_artifact_id_in_batches,
+    ]
+
+    with op.get_context().autocommit_block():
+        conn = op.get_bind()
+        for query in data_migration_queries:
+
+            batch_size = 500
+            offset = 0
+
+            while True:
+                # execute until we've updated task_run_state_id and artifact_data
+                # autocommit mode will commit each time `execute` is called
+                sql_stmt = sa.text(query(batch_size, offset))
+                result = conn.execute(sql_stmt)
+
+                if result.rowcount <= 0:
+                    break
+
+                offset += batch_size
+
+    ### END DATA MIGRATION
+
+
+def downgrade():
+    pass
---
+++ src/prefect/orion/database/migrations/versions/postgresql/2023_01_13_045502_2882cd2df465_cleanup_artifact_migration.py
@@ -0,0 +1,54 @@
+"""State data migration cleanup
+
+Revision ID: 2882cd2df465
+Revises: 2882cd2df464
+Create Date: 2023-01-13 04:55:02.358638
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+import prefect
+
+# revision identifiers, used by Alembic.
+revision = "2882cd2df465"
+down_revision = "2882cd2df464"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # drop state id columns after data migration
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_index(batch_op.f("ix_artifact__task_run_state_id"))
+        batch_op.drop_column("task_run_state_id")
+        batch_op.drop_index(batch_op.f("ix_artifact__flow_run_state_id"))
+        batch_op.drop_column("flow_run_state_id")
+
+
+def downgrade():
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.add_column(
+            sa.Column(
+                "flow_run_state_id",
+                prefect.orion.utilities.database.UUID(),
+                nullable=True,
+            ),
+        )
+        batch_op.create_index(
+            batch_op.f("ix_artifact__flow_run_state_id"),
+            ["flow_run_state_id"],
+            unique=False,
+        )
+        batch_op.add_column(
+            sa.Column(
+                "task_run_state_id",
+                prefect.orion.utilities.database.UUID(),
+                nullable=True,
+            ),
+        )
+        batch_op.create_index(
+            batch_op.f("ix_artifact__task_run_state_id"),
+            ["task_run_state_id"],
+            unique=False,
+        )
