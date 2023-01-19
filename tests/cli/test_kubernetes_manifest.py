4741a06153e269becb5c1d9cde4040494126c987
Jamie Zieziula <jamie.m.dick@gmail.com>
2023-01-19 16:37:00 -0500

    update test to ignore certain manifest types

--- tests/cli/test_kubernetes_manifest.py
+++ tests/cli/test_kubernetes_manifest.py
@@ -95,7 +95,8 @@ def test_printing_the_agent_manifest_with_no_args():
     assert manifests
 
     for manifest in manifests:
-        assert manifest["metadata"]["namespace"] == "default"
+        if manifest["kind"] not in ["ClusterRole","ClusterRoleBinding"]:
+            assert manifest["metadata"]["namespace"] == "default"
 
         if manifest["kind"] == "Deployment":
             assert manifest["metadata"]["name"] == "prefect-agent"
