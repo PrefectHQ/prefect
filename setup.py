3993c556ec40456719dfc5f1474264f8a37a3bf7
Laerte Pereira <laertefbk@gmail.com>
2023-01-23 16:14:46 -0300

    chore: Add Changelog link

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     author_email="help@prefect.io",
     url="https://www.prefect.io",
     project_urls={
+        "Changelog": "https://github.com/PrefectHQ/prefect/blob/main/RELEASE-NOTES.md",
         "Documentation": "https://docs.prefect.io",
         "Source": "https://github.com/PrefectHQ/prefect",
         "Tracker": "https://github.com/PrefectHQ/prefect/issues",
