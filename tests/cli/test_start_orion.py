98dce31499d5f9d610bbe8f76d46f0b9c5ebe9a5
ddelange <14880945+ddelange@users.noreply.github.com>
2023-01-26 07:59:10 +0100

    Switch from subprocess to anyio

--- tests/cli/test_start_orion.py
+++ tests/cli/test_start_orion.py
@@ -1,23 +1,92 @@
+import os
 import re
 import signal
-import subprocess
 import sys
+import tempfile
 import time
 
+import anyio
+import httpx
 import pytest
 
-ORION_START_ARGS = [
-    "prefect",
-    "orion",
-    "start",
-    "--host",
-    "0.0.0.0",
-    "--port",
-    "4200",
-    "--log-level",
-    "INFO",
-]
+from prefect.settings import get_current_settings
+from prefect.testing.fixtures import is_port_in_use
+from prefect.utilities.processutils import open_process
+
 SHUTDOWN_TIMEOUT = 10
+
+
+@pytest.fixture(scope="function")
+async def orion_process():
+    """
+    Runs an instance of the Orion API at a dedicated URL instead of the ephemeral
+    application. Requires a port from 2222-2227 to be available.
+    Uses the same database as the rest of the tests.
+    Yields:
+        The connection string
+    """
+
+    ports = [2222 + i for i in range(5)]
+
+    while True:
+        try:
+            port = ports.pop()
+        except IndexError as exc:
+            raise RuntimeError("No ports available to run test API.") from exc
+
+        if not is_port_in_use(port):
+            break
+
+    out = tempfile.TemporaryFile()  # capture output for test assertions
+
+    # Will connect to the same database as normal test clients
+    async with open_process(
+        command=[
+            "prefect",
+            "orion",
+            "start",
+            "--host",
+            "127.0.0.1",
+            "--port",
+            str(port),
+            "--log-level",
+            "INFO",
+        ],
+        stdout=out,
+        stderr=out,
+        env={**os.environ, **get_current_settings().to_environment_variables()},
+    ) as process:
+        process.out = out
+        api_url = f"http://localhost:{port}/api"
+
+        # Wait for the server to be ready
+        async with httpx.AsyncClient() as client:
+            response = None
+            with anyio.move_on_after(20):
+                while True:
+                    try:
+                        response = await client.get(api_url + "/health")
+                    except httpx.ConnectError:
+                        pass
+                    else:
+                        if response.status_code == 200:
+                            break
+                    await anyio.sleep(0.1)
+            if response:
+                response.raise_for_status()
+            if not response:
+                raise RuntimeError(
+                    "Timed out while attempting to connect to hosted test Orion."
+                )
+
+        # Yield to the consuming tests
+        yield process
+
+        # Then shutdown the process
+        try:
+            process.terminate()
+        except ProcessLookupError:
+            pass
 
 
 class TestUvicornSignalForwarding:
@@ -25,27 +94,16 @@ class TestUvicornSignalForwarding:
         sys.platform == "win32",
         reason="SIGTERM is only used in non-Windows environments",
     )
-    def test_sigint_sends_sigterm_then_sigkill(self):
-        with subprocess.Popen(
-            ORION_START_ARGS,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        ) as proc:
-            out = []
-            for line in proc.stdout:
-                print(line)
-                out.append(line)
-                if b"Uvicorn running" in line:
-                    break
-            assert proc.returncode is None, "Orion should now be running"
-            proc.send_signal(signal.SIGINT)
-            time.sleep(0.1)
-            proc.send_signal(signal.SIGINT)
-            proc.wait(timeout=SHUTDOWN_TIMEOUT)
-            out += proc.stdout
-        out = "".join(line.decode() for line in out)
+    async def test_sigint_sends_sigterm_then_sigkill(self, orion_process):
+        orion_process.send_signal(signal.SIGINT)
+        time.sleep(0.1)
+        orion_process.send_signal(signal.SIGINT)
+        with anyio.move_on_after(SHUTDOWN_TIMEOUT):
+            await orion_process.wait()
+        orion_process.out.seek(0)
+        out = orion_process.out.read().decode()
 
-        assert proc.returncode == 0, "The main process should exit gracefully"
+        assert orion_process.returncode == 0, "The main process should exit gracefully"
         assert re.search(
             r"(Sending SIGTERM)(.|\s)*(Sending SIGKILL)", out
         ), "When sending two SIGINT shortly after each other, the main process should first send a SIGTERM and then a SIGKILL to the uvicorn subprocess"
@@ -54,27 +112,16 @@ class TestUvicornSignalForwarding:
         sys.platform == "win32",
         reason="SIGTERM is only used in non-Windows environments",
     )
-    def test_sigterm_sends_sigterm_then_sigkill(self):
-        with subprocess.Popen(
-            ORION_START_ARGS,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        ) as proc:
-            out = []
-            for line in proc.stdout:
-                print(line)
-                out.append(line)
-                if b"Uvicorn running" in line:
-                    break
-            assert proc.returncode is None, "Orion should now be running"
-            proc.send_signal(signal.SIGTERM)
-            time.sleep(0.1)
-            proc.send_signal(signal.SIGTERM)
-            proc.wait(timeout=SHUTDOWN_TIMEOUT)
-            out += proc.stdout
-        out = "".join(line.decode() for line in out)
+    async def test_sigterm_sends_sigterm_then_sigkill(self, orion_process):
+        orion_process.send_signal(signal.SIGTERM)
+        time.sleep(0.1)
+        orion_process.send_signal(signal.SIGTERM)
+        with anyio.move_on_after(SHUTDOWN_TIMEOUT):
+            await orion_process.wait()
+        orion_process.out.seek(0)
+        out = orion_process.out.read().decode()
 
-        assert proc.returncode == 0, "The main process should exit gracefully"
+        assert orion_process.returncode == 0, "The main process should exit gracefully"
         assert re.search(
             r"(Sending SIGTERM)(.|\s)*(Sending SIGKILL)", out
         ), "When sending two SIGTERM shortly after each other, the main process should first send a SIGTERM and then a SIGKILL to the uvicorn subprocess"
@@ -83,25 +130,14 @@ class TestUvicornSignalForwarding:
         sys.platform == "win32",
         reason="SIGTERM is only used in non-Windows environments",
     )
-    def test_sigterm_sends_sigterm_directly(self):
-        with subprocess.Popen(
-            ORION_START_ARGS,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        ) as proc:
-            out = []
-            for line in proc.stdout:
-                print(line)
-                out.append(line)
-                if b"Uvicorn running" in line:
-                    break
-            assert proc.returncode is None, "Orion should now be running"
-            proc.send_signal(signal.SIGTERM)
-            proc.wait(timeout=SHUTDOWN_TIMEOUT)
-            out += proc.stdout
-        out = "".join(line.decode() for line in out)
+    async def test_sigterm_sends_sigterm_directly(self, orion_process):
+        orion_process.send_signal(signal.SIGTERM)
+        with anyio.move_on_after(SHUTDOWN_TIMEOUT):
+            await orion_process.wait()
+        orion_process.out.seek(0)
+        out = orion_process.out.read().decode()
 
-        assert proc.returncode == 0, "The main process should exit gracefully"
+        assert orion_process.returncode == 0, "The main process should exit gracefully"
         assert re.search(
             r"(Sending SIGTERM)(.|\s)*(Application shutdown complete)", out
         ), "When sending a SIGTERM, the main process should send a SIGTERM to the uvicorn subprocess"
@@ -110,25 +146,14 @@ class TestUvicornSignalForwarding:
         sys.platform != "win32",
         reason="CTRL_BREAK_EVENT is only defined in Windows",
     )
-    def test_sends_ctrl_break_win32(self):
-        with subprocess.Popen(
-            ORION_START_ARGS,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-        ) as proc:
-            out = []
-            for line in proc.stdout:
-                print(line)
-                out.append(line)
-                if b"Uvicorn running" in line:
-                    break
-            assert proc.returncode is None, "Orion should now be running"
-            proc.send_signal(signal.SIGINT)
-            proc.wait(timeout=SHUTDOWN_TIMEOUT)
-            out += proc.stdout
-        out = "".join(line.decode() for line in out)
+    async def test_sends_ctrl_break_win32(self, orion_process):
+        orion_process.send_signal(signal.SIGINT)
+        with anyio.move_on_after(SHUTDOWN_TIMEOUT):
+            await orion_process.wait()
+        orion_process.out.seek(0)
+        out = orion_process.out.read().decode()
 
-        assert proc.returncode == 0, "The main process should exit gracefully"
+        assert orion_process.returncode == 0, "The main process should exit gracefully"
         assert re.search(
             r"Sending CTRL_BREAK_EVENT", out
         ), "When sending a SIGINT, the main process should send a CTRL_BREAK_EVENT to the uvicorn subprocess"
