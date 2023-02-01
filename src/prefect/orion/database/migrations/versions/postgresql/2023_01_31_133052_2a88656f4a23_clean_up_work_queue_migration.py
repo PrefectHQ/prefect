1bf351920f253459cb6c3115d78945dee57fe05b
Alexander Streed <alex.s@prefect.io>
2023-02-01 11:04:47 -0600

    Fixes postgres downgrade

--- src/prefect/orion/database/migrations/versions/postgresql/2023_01_31_133052_2a88656f4a23_clean_up_work_queue_migration.py
+++ src/prefect/orion/database/migrations/versions/postgresql/2023_01_31_133052_2a88656f4a23_clean_up_work_queue_migration.py
@@ -39,41 +39,41 @@ def downgrade():
         "work_pool_queue",
         sa.Column(
             "id",
-            sa.CHAR(length=36),
-            server_default=sa.text(
-                "(((\n        lower(hex(randomblob(4)))\n        || '-'\n        || lower(hex(randomblob(2)))\n        || '-4'\n        || substr(lower(hex(randomblob(2))),2)\n        || '-'\n        || substr('89ab',abs(random()) % 4 + 1, 1)\n        || substr(lower(hex(randomblob(2))),2)\n        || '-'\n        || lower(hex(randomblob(6)))\n    )))"
-            ),
+            prefect.orion.utilities.database.UUID(),
+            server_default=sa.text("(GEN_RANDOM_UUID())"),
             nullable=False,
         ),
         sa.Column(
             "created",
-            sa.DATETIME(),
-            server_default=sa.text("(strftime('%Y-%m-%d %H:%M:%f000', 'now'))"),
+            prefect.orion.utilities.database.Timestamp(timezone=True),
+            server_default=sa.text("CURRENT_TIMESTAMP"),
             nullable=False,
         ),
         sa.Column(
             "updated",
-            sa.DATETIME(),
-            server_default=sa.text("(strftime('%Y-%m-%d %H:%M:%f000', 'now'))"),
+            prefect.orion.utilities.database.Timestamp(timezone=True),
+            server_default=sa.text("CURRENT_TIMESTAMP"),
             nullable=False,
         ),
-        sa.Column("name", sa.VARCHAR(), nullable=False),
-        sa.Column("description", sa.VARCHAR(), nullable=True),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("is_paused", sa.Boolean(), server_default="0", nullable=False),
+        sa.Column("concurrency_limit", sa.Integer(), nullable=True),
+        sa.Column("priority", sa.Integer(), nullable=False),
         sa.Column(
-            "is_paused", sa.BOOLEAN(), server_default=sa.text("'0'"), nullable=False
+            "work_pool_id", prefect.orion.utilities.database.UUID(), nullable=False
         ),
-        sa.Column("concurrency_limit", sa.INTEGER(), nullable=True),
-        sa.Column("priority", sa.INTEGER(), nullable=False),
-        sa.Column("work_pool_id", sa.CHAR(length=36), nullable=False),
         sa.ForeignKeyConstraint(
             ["work_pool_id"],
             ["work_pool.id"],
-            name="fk_work_pool_queue__work_pool_id__work_pool",
-            ondelete="CASCADE",
+            name=op.f("fk_work_pool_queue__work_pool_id__work_pool"),
+            ondelete="cascade",
         ),
-        sa.PrimaryKeyConstraint("id", name="pk_work_pool_queue"),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_work_pool_queue")),
         sa.UniqueConstraint(
-            "work_pool_id", "name", name="uq_work_pool_queue__work_pool_id_name"
+            "work_pool_id",
+            "name",
+            name=op.f("uq_work_pool_queue__work_pool_id_name"),
         ),
     )
     with op.batch_alter_table("work_pool_queue", schema=None) as batch_op:
