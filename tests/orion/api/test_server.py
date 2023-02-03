a84a479a951236df2f6dd7d0118803e3a5f87a62
Dustin Ngo <dustin.ngo@gmail.com>
2023-02-03 10:36:50 -0500

    Add Orion version route

--- tests/orion/api/test_server.py
+++ tests/orion/api/test_server.py
@@ -10,6 +10,7 @@ from prefect.orion.api.server import (
     _memoize_block_auto_registration,
     create_orion_api,
     method_paths_from_routes,
+    ORION_API_VERSION,
 )
 from prefect.settings import (
     PREFECT_MEMO_STORE_PATH,
@@ -52,11 +53,18 @@ async def test_health_check_route(client):
     assert response.status_code == status.HTTP_200_OK
 
 
+async def test_version_route(client):
+    response = await client.get("/version")
+    assert response.json() == ORION_API_VERSION
+    assert response.status_code == status.HTTP_200_OK
+
+
 class TestCreateOrionAPI:
 
     BUILTIN_ROUTES = {
         "GET /redoc",
         "GET /health",
+        "GET /version",
         "HEAD /docs",
         "GET /openapi.json",
         "GET /docs/oauth2-redirect",
