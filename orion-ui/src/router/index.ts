21442980a4be63782707f82dce41908a9ed7058a
Marichka Offen <marichka@prefect.io>
2023-02-07 16:22:24 -0500

    Update index.ts

--- orion-ui/src/router/index.ts
+++ orion-ui/src/router/index.ts
@@ -14,7 +14,6 @@ const workspaceRoutes = createWorkspaceRouteRecords({
   deployment: () => import('@/pages/Deployment.vue'),
   deploymentEdit: () => import('@/pages/DeploymentEdit.vue'),
   deploymentFlowRunCreate: () => import('@/pages/FlowRunCreate.vue'),
-  customFlowRunCreate: () => import('@/pages/FlowRunCreate.vue'),
   workQueues: () => import('@/pages/WorkQueues.vue'),
   workQueue: () => import('@/pages/WorkQueue.vue'),
   workQueueCreate: () => import('@/pages/WorkQueueCreate.vue'),
