6e1da0bdf92c0cdcfa6f5b98701cb5987bbecb30
Alexander Streed <alex.s@prefect.io>
2023-02-08 12:13:21 -0600

    Update missed prefix removal

--- src/prefect/client/orion.py
+++ src/prefect/client/orion.py
@@ -744,7 +744,7 @@ class OrionClient:
         try:
             if work_pool_name is not None:
                 response = await self._client.post(
-                    f"experimental/work_pools/{work_pool_name}/queues", json=data
+                    f"/work_pools/{work_pool_name}/queues", json=data
                 )
             else:
                 response = await self._client.post("/work_queues/", json=data)
