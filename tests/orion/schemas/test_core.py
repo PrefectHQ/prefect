eac463b5609fa585c7a6041b4bf972bd7fe3e1d0
Dustin Ngo <dustin.ngo@gmail.com>
2023-02-13 02:55:20 -0500

    Add serverside schema tests

--- tests/orion/schemas/test_core.py
+++ tests/orion/schemas/test_core.py
@@ -363,3 +363,34 @@ class TestWorkPool:
             name="test", type="test", default_queue_id=qid, base_job_template=template
         )
         assert wp
+
+
+class TestArtifacts:
+    async def test_validates_metadata_sizes(self):
+        artifact = schemas.core.Artifact(
+            metadata_={"a very long key": "x" * 5000, "a very short key": "o" * 10}
+        )
+        assert len(artifact.metadata_["a very short key"]) == 10
+        assert len(artifact.metadata_["a very long key"]) < 5000
+        assert len(artifact.metadata_["a very long key"]) == 503  # max length + "..."
+
+    async def test_from_state_result_populates_type_and_metadata(self):
+        # TODO: results received from the API should conform to a schema
+        result = dict(
+            data="abcdefghijklmnopqrstuvwxyz",
+            artifact_type="a test result",
+            artifact_description="the most remarkable word",
+        )
+        artifact = schemas.core.Artifact.from_state_result(result)
+        assert artifact.data == "abcdefghijklmnopqrstuvwxyz"
+        assert artifact.type == "a test result"
+        assert artifact.metadata_["description"] == "the most remarkable word"
+
+    async def test_from_state_result_compatible_with_older_result_payloads(self):
+        result = dict(
+            data="abcdefghijklmnopqrstuvwxyz",
+        )
+        artifact = schemas.core.Artifact.from_state_result(result)
+        assert artifact.data == "abcdefghijklmnopqrstuvwxyz"
+        assert artifact.type is None
+        assert artifact.metadata_ is None
