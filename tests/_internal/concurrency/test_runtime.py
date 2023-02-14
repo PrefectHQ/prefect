ee4b1f271d3634c71637217e61d777d8caf9a6ec
Zanie Adkins <michael@prefect.io>
2023-02-14 16:20:02 -0600

    Fix runtime bug where async context vars were not propagated (#8523)

--- tests/_internal/concurrency/test_runtime.py
+++ tests/_internal/concurrency/test_runtime.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextvars
 
 import anyio
 import pytest
@@ -13,6 +14,9 @@ def identity(x):
 async def aidentity(x):
     await asyncio.sleep(0)
     return x
+
+
+TEST_CONTEXTVAR = contextvars.ContextVar("TEST_CONTEXTVAR")
 
 
 def test_runtime_context_manager():
@@ -108,3 +112,60 @@ def test_async_runtime_submit_from_worker_thread():
 
     with Runtime() as runtime:
         assert runtime.run_in_thread(work) == 1
+
+
+@pytest.mark.parametrize("sync", [True, False], ids=["sync", "async"])
+async def test_runtime_run_in_thread_captures_context_variables(sync):
+    def _get_value():
+        return TEST_CONTEXTVAR.get()
+
+    if sync:
+
+        def get_value():
+            return _get_value()
+
+    else:
+
+        async def get_value():
+            return _get_value()
+
+    with Runtime() as runtime:
+        try:
+            token = TEST_CONTEXTVAR.set("test")
+            assert await runtime.run_in_thread(get_value) == "test"
+        finally:
+            TEST_CONTEXTVAR.reset(token)
+
+
+@pytest.mark.parametrize("sync", [True, False], ids=["sync", "async"])
+async def test_runtime_run_in_loop_captures_context_variables(sync):
+    async def get_value():
+        return TEST_CONTEXTVAR.get()
+
+    with Runtime() as runtime:
+        try:
+            token = TEST_CONTEXTVAR.set("test")
+            result = await runtime.run_in_loop(get_value)
+            assert result == "test"
+        finally:
+            TEST_CONTEXTVAR.reset(token)
+
+
+async def test_async_runtime_submit_from_thread_captures_context_variables():
+    def _get_value():
+        return TEST_CONTEXTVAR.get()
+
+    async def get_value():
+        return _get_value()
+
+    async def work():
+        token = TEST_CONTEXTVAR.set("test")
+        try:
+            future = runtime.submit_from_thread(get_value)
+        finally:
+            TEST_CONTEXTVAR.reset(token)
+        return await asyncio.wrap_future(future)
+
+    with Runtime() as runtime:
+        result = await runtime.run_in_loop(work)
+        assert result == "test"
