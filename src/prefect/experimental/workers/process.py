c634fb3c7a6acc9574c7a5e9185eb9d8521607ec
Alexander Streed <desertaxle@users.noreply.github.com>
2023-02-15 10:48:44 -0600

    Adds metadata fields to `BaseWorker` (#8527)

--- src/prefect/experimental/workers/process.py
+++ src/prefect/experimental/workers/process.py
@@ -65,6 +65,9 @@ class ProcessWorker(BaseWorker):
     job_configuration = ProcessJobConfiguration
     job_configuration_variables = ProcessVariables
 
+    _description = "Worker that executes flow runs within processes."
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/39WQhVu4JK40rZWltGqhuC/d15be6189a0cb95949a6b43df00dcb9b/image5.png?h=250"
+
     async def verify_submitted_deployment(self, deployment: Deployment):
         # TODO: Implement deployment verification for `ProcessWorker`
         pass
