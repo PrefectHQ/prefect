d7678b3d4f711731fd072316f0da0e1848c6814c
Zanie Adkins <michael@prefect.io>
2023-02-17 10:32:45 -0600

    Replace runtime with supervisor concept and explicit API (#8559)

---
+++ src/prefect/_internal/concurrency/from_sync.py
@@ -0,0 +1,79 @@
+import concurrent.futures
+import threading
+from typing import Callable, TypeVar
+
+from typing_extensions import ParamSpec
+
+from prefect._internal.concurrency.runtime import get_runtime_thread
+from prefect._internal.concurrency.supervisors import (
+    SyncSupervisor,
+    get_supervisor,
+    set_supervisor,
+)
+
+P = ParamSpec("P")
+T = TypeVar("T")
+
+
+def call_soon_in_runtime_thread(
+    __fn: Callable[P, T], *args: P.args, **kwargs: P.kwargs
+) -> SyncSupervisor[T]:
+    """
+    Schedule a coroutine function in the runtime thread.
+
+    Returns a supervisor.
+    """
+    current_supervisor = get_supervisor()
+    runtime = get_runtime_thread()
+
+    supervisor = SyncSupervisor()
+
+    with set_supervisor(supervisor):
+        if (
+            current_supervisor is None
+            or current_supervisor.owner_thread_ident != runtime.ident
+        ):
+            future = runtime.submit_to_loop(__fn, *args, **kwargs)
+        else:
+            future = current_supervisor.send_call(__fn, *args, **kwargs)
+
+    supervisor.set_future(future)
+    return supervisor
+
+
+def call_soon_in_worker_thread(
+    __fn: Callable[P, T], *args: P.args, **kwargs: P.kwargs
+) -> SyncSupervisor[T]:
+    """
+    Schedule a function in a worker thread.
+
+    Returns a supervisor.
+    """
+    runtime = get_runtime_thread()
+    supervisor = SyncSupervisor()
+    with set_supervisor(supervisor):
+        future = runtime.submit_to_worker_thread(__fn, *args, **kwargs)
+    supervisor.set_future(future)
+    return supervisor
+
+
+def call_soon_in_main_thread(
+    __fn: Callable[P, T], *args: P.args, **kwargs: P.kwargs
+) -> concurrent.futures.Future:  # [T]
+    """
+    Call a function in the main thread.
+
+    Must be used from a call scheduled by `call_soon_in_worker_thread` or
+    `call_soon_in_runtime_thread` or the main thread will not be watching for work.
+
+    Returns a future.
+    """
+    current_supervisor = get_supervisor()
+    if current_supervisor is None:
+        raise RuntimeError("No supervisor found.")
+
+    if current_supervisor.owner_thread_ident != threading.main_thread().ident:
+        raise RuntimeError("Watching future is not owned by the main thread.")
+
+    future = current_supervisor.send_call(__fn, *args, **kwargs)
+    return future
