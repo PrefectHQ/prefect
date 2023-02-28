a05ae7d02fe5bda26ad126a3d91fba4a0d4095a9
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-02-27 19:05:47 -0600

    add sqlite migrations

---
+++ src/prefect/server/database/migrations/versions/sqlite/2023_02_27_184203_4c6903568ef0_remove_artifact_uq.py
@@ -0,0 +1,26 @@
+"""Removes unique constraint from key column in artifact table
+
+Revision ID: 4c6903568ef0
+Revises: 8d148e44e669
+Create Date: 2023-02-27 18:42:03.560057
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "4c6903568ef0"
+down_revision = "8d148e44e669"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_index("ix_artifact__key")
+        batch_op.create_index(batch_op.f("ix_artifact__key"), ["key"], unique=False)
+
+
+def downgrade():
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_index(batch_op.f("ix_artifact__key"))
+        batch_op.create_index("ix_artifact__key", ["key"], unique=False)
