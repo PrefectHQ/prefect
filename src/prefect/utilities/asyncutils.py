a47a4f7b9233a6463c055c217ee4ffd731c9978c
Michael Adkins <michael@prefect.io>
2023-03-02 13:46:30 -0600

    Add supervisor handling stub to `sync_compatible`
    Fixes cases where sync flows are run in the async main thread and sync compatible methods are used within the flow

--- src/prefect/utilities/asyncutils.py
+++ src/prefect/utilities/asyncutils.py
@@ -217,7 +217,16 @@ def sync_compatible(async_fn: T) -> T:
 
     @wraps(async_fn)
     def coroutine_wrapper(*args, **kwargs):
-        if in_async_main_thread():
+        from prefect._internal.concurrency.api import create_call, from_sync
+        from prefect._internal.concurrency.supervisors import get_supervisor
+
+        supervisor = get_supervisor()
+        if supervisor and not is_async_fn(supervisor._call.fn):
+            # In a supervised call that is not async; send the async call to the parent
+            return from_sync.send_call_to_supervising_thread(
+                create_call(async_fn, *args, **kwargs)
+            ).result()
+        elif in_async_main_thread():
             # In the main async context; return the coro for them to await
             return async_fn(*args, **kwargs)
         elif in_async_worker_thread():
