c3dcf87ab52392c43f460b9ce94d3ee5e8e2390e
Zanie Adkins <michael@prefect.io>
2023-03-06 16:06:55 -0600

    Add portal abstract base for worker threads and supervisors (#8717)

--- src/prefect/_internal/concurrency/portals.py
+++ src/prefect/_internal/concurrency/portals.py
@@ -1,3 +1,4 @@
+import abc
 import asyncio
 import atexit
 import concurrent.futures
@@ -72,6 +73,9 @@ class Call(Generic[T]):
 
         return None
 
+    def result(self):
+        return self.future.result()
+
     def _run_sync(self):
         try:
             result = self.context.run(self.fn, *self.args, **self.kwargs)
@@ -114,11 +118,11 @@ class Call(Generic[T]):
 
             async def run_and_return_result():
                 await coro
-                return self.future.result()
+                return self.result()
 
             return run_and_return_result()
         else:
-            return self.future.result()
+            return self.result()
 
     def __repr__(self) -> str:
         name = getattr(self.fn, "__name__", str(self.fn))
@@ -134,11 +138,50 @@ class Call(Generic[T]):
         return f"{name}({call_args})"
 
 
-class Worker:
+class Portal(abc.ABC):
     """
-    A worker running on a thread.
+    Allows submission of calls to execute elsewhere.
+    """
 
-    Runs an event loop and allows submission of calls.
+    @abc.abstractmethod
+    def start(self):
+        """
+        Start the portal.
+        """
+
+    @abc.abstractmethod
+    def submit(self, call: Call) -> Call:
+        """
+        Submit a call to execute elsewhere.
+
+        The call's result can be retrieved with `call.result()`.
+
+        Returns the call.
+        """
+
+    @abc.abstractmethod
+    def shutdown(self) -> None:
+        """
+        Shutdown the portal.
+        """
+
+    @abc.abstractproperty
+    def name(self) -> str:
+        """
+        Get the name of the portal.
+        """
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *_):
+        self.shutdown()
+
+
+class WorkerThreadPortal(Portal):
+    """
+    A portal to a worker running on a thread with an event loop.
     """
 
     def __init__(
@@ -149,7 +192,7 @@ class Worker:
         )
         self._ready_future = concurrent.futures.Future()
         self._loop: Optional[asyncio.AbstractEventLoop] = None
-        self._shutdown_event: Optional[Event] = None
+        self._shutdown_event: Event = Event()
         self._run_once: bool = run_once
         self._submitted_count: int = 0
 
@@ -158,19 +201,19 @@ class Worker:
 
     def start(self):
         """
-        Start the worker; raises any exceptions encountered during startup.
+        Start the worker thread; raises any exceptions encountered during startup.
         """
         self.thread.start()
         # Wait for the worker to be ready
         self._ready_future.result()
 
-    def submit(self, call: Call) -> None:
+    def submit(self, call: Call) -> Call:
         if self._submitted_count > 0 and self._run_once:
             raise RuntimeError(
                 "Worker configured to only run once. A call has already been submitted."
             )
 
-        if not self._shutdown_event:
+        if self._loop is None:
             self.start()
 
         if self._shutdown_event.is_set():
@@ -181,30 +224,38 @@ class Worker:
         if self._run_once:
             call.future.add_done_callback(lambda _: self.shutdown())
 
+        return call
+
     def shutdown(self) -> None:
+        """
+        Shutdown the worker thread. Does not wait for the thread to stop.
+        """
         if not self._shutdown_event:
             return
 
         self._shutdown_event.set()
         # TODO: Consider blocking on `thread.join` here?
 
+    @property
+    def name(self) -> str:
+        return self.thread.name
+
     def _entrypoint(self):
         """
-        Entrypoint for the worker.
+        Entrypoint for the thread.
 
         Immediately create a new event loop and pass control to `run_until_shutdown`.
         """
         try:
             asyncio.run(self._run_until_shutdown())
         except BaseException:
-            # Log exceptions that crash the worker
-            logger.exception("Worker encountered exception")
+            # Log exceptions that crash the thread
+            logger.exception("%s encountered exception", self.name)
             raise
 
     async def _run_until_shutdown(self):
         try:
             self._loop = asyncio.get_running_loop()
-            self._shutdown_event = Event()
             self._ready_future.set_result(True)
         except Exception as exc:
             self._ready_future.set_exception(exc)
@@ -213,15 +264,15 @@ class Worker:
         await self._shutdown_event.wait()
 
 
-GLOBAL_WORKER: Optional[Worker] = None
+GLOBAL_WORKER: Optional[WorkerThreadPortal] = None
 
 
-def get_global_worker() -> Worker:
+def get_global_thread_portal() -> WorkerThreadPortal:
     global GLOBAL_WORKER
 
     # Create a new worker on first call or if the existing worker is dead
     if GLOBAL_WORKER is None or not GLOBAL_WORKER.thread.is_alive():
-        GLOBAL_WORKER = Worker(daemon=True, name="GlobalWorkerThread")
+        GLOBAL_WORKER = WorkerThreadPortal(daemon=True, name="GlobalWorkerThread")
         GLOBAL_WORKER.start()
 
     return GLOBAL_WORKER
--- tests/_internal/concurrency/test_supervisors.py
+++ tests/_internal/concurrency/test_supervisors.py
@@ -3,12 +3,12 @@ import time
 
 import pytest
 
+from prefect._internal.concurrency.portals import WorkerThreadPortal
 from prefect._internal.concurrency.supervisors import (
     AsyncSupervisor,
     Call,
     SyncSupervisor,
 )
-from prefect._internal.concurrency.workers import Worker
 
 
 def fake_fn(*args, **kwargs):
@@ -40,13 +40,13 @@ def sleep_repeatedly(seconds: int):
 
 @pytest.mark.parametrize("cls", [AsyncSupervisor, SyncSupervisor])
 async def test_supervisor_repr(cls):
-    worker = Worker(run_once=True)
-    supervisor = cls(Call.new(fake_fn, 1, 2), worker=worker)
-    assert (
-        repr(supervisor)
-        == f"<{cls.__name__} call=fake_fn(1, 2), worker='WorkerThread',"
-        " owner='MainThread'>"
-    )
+    with WorkerThreadPortal(run_once=True) as portal:
+        supervisor = cls(Call.new(fake_fn, 1, 2), portal=portal)
+        assert (
+            repr(supervisor)
+            == f"<{cls.__name__} call=fake_fn(1, 2), portal='WorkerThread',"
+            " owner='MainThread'>"
+        )
 
 
 def test_sync_supervisor_timeout_in_worker_thread():
@@ -54,20 +54,20 @@ def test_sync_supervisor_timeout_in_worker_thread():
     In this test, a timeout is raised due to a slow call that is occuring on the worker
     thread.
     """
-    worker = Worker(run_once=True)
-    supervisor = SyncSupervisor(
-        Call.new(sleep_repeatedly, 1), worker=worker, timeout=0.1
-    )
-    supervisor.start()
+    with WorkerThreadPortal(run_once=True) as portal:
+        supervisor = SyncSupervisor(
+            Call.new(sleep_repeatedly, 1), portal=portal, timeout=0.1
+        )
+        supervisor.start()
 
     t0 = time.time()
     with pytest.raises(TimeoutError):
         supervisor.result()
     t1 = time.time()
 
-    # The future has a timeout error too
+    # The call has a timeout error too
     with pytest.raises(TimeoutError):
-        supervisor._call.future.result()
+        supervisor._call.result()
 
     assert t1 - t0 < 1
 
@@ -77,63 +77,67 @@ def test_sync_supervisor_timeout_in_main_thread():
     In this test, a timeout is raised due to a slow call that is sent back to the main
     thread by the worker thread.
     """
-    worker = Worker(run_once=True)
+    with WorkerThreadPortal(run_once=True) as portal:
 
-    def on_worker_thread():
-        # Send sleep to the main thread
-        future = supervisor.send_call_to_supervisor(Call.new(time.sleep, 2))
-        return future
+        def on_worker_thread():
+            # Send sleep to the main thread
+            call = supervisor.submit(Call.new(time.sleep, 2))
+            return call
 
-    supervisor = SyncSupervisor(Call.new(on_worker_thread), worker=worker, timeout=0.1)
-    supervisor.start()
+        supervisor = SyncSupervisor(
+            Call.new(on_worker_thread), portal=portal, timeout=0.1
+        )
+        supervisor.start()
 
-    t0 = time.time()
-    future = supervisor.result()
-    t1 = time.time()
+        t0 = time.time()
+        call = supervisor.result()
+        t1 = time.time()
 
     # The timeout error is not raised by `supervisor.result()` because the worker
-    # does not check the result of the future; however, the work that was sent
+    # does not check the result of the call; however, the work that was sent
     # to the main thread should have a timeout error
     with pytest.raises(TimeoutError):
-        future.result()
+        call.result()
 
     # main thread timeouts round up to the nearest second
     assert t1 - t0 < 2
 
 
 async def test_async_supervisor_timeout_in_worker_thread():
-    worker = Worker(run_once=True)
-    supervisor = AsyncSupervisor(
-        Call.new(sleep_repeatedly, 1), worker=worker, timeout=0.1
-    )
-    supervisor.start()
+    with WorkerThreadPortal(run_once=True) as portal:
+        supervisor = AsyncSupervisor(
+            Call.new(sleep_repeatedly, 1), portal=portal, timeout=0.1
+        )
+        supervisor.start()
 
-    t0 = time.time()
-    with pytest.raises(TimeoutError):
-        await supervisor.result()
-    t1 = time.time()
+        t0 = time.time()
+        with pytest.raises(TimeoutError):
+            await supervisor.result()
+        t1 = time.time()
 
     assert t1 - t0 < 1
 
-    # The future has a timeout error too
+    # The call has a timeout error too
     with pytest.raises(TimeoutError):
-        supervisor._call.future.result()
+        supervisor._call.result()
 
 
 async def test_async_supervisor_timeout_in_main_thread():
-    worker = Worker(run_once=True)
+    with WorkerThreadPortal(run_once=True) as portal:
 
-    def on_worker_thread():
-        # Send sleep to the main thread
-        future = supervisor.send_call_to_supervisor(Call.new(asyncio.sleep, 1))
-        return future
+        def on_worker_thread():
+            # Send sleep to the main thread
+            future = supervisor.submit(Call.new(asyncio.sleep, 1))
+            return future
 
-    supervisor = AsyncSupervisor(Call.new(on_worker_thread), worker=worker, timeout=0.1)
-    supervisor.start()
+        supervisor = AsyncSupervisor(
+            Call.new(on_worker_thread), portal=portal, timeout=0.1
+        )
+        supervisor.start()
 
-    t0 = time.time()
-    future = await supervisor.result()
-    t1 = time.time()
+        t0 = time.time()
+        future = await supervisor.result()
+        t1 = time.time()
 
     assert t1 - t0 < 1
 
