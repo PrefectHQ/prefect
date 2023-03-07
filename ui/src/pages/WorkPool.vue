bca64d6a0f4a5f633f95496b61d9218cf941cde9
Alexander Streed <alex.s@prefect.io>
2023-03-07 11:18:51 -0600

    Adds example commands and workers table

--- ui/src/pages/WorkPool.vue
+++ ui/src/pages/WorkPool.vue
@@ -1,41 +1,55 @@
 <template>
-  <p-layout-well v-if="workPool" class="work-pool">
+  <p-layout-default v-if="workPool">
     <template #header>
       <PageHeadingWorkPool :work-pool="workPool" @update="workPoolSubscription.refresh" />
     </template>
 
-    <p-tabs :tabs="tabs">
-      <template #details>
-        <WorkPoolDetails :work-pool="workPool" />
+    <p-layout-well v-if="workPool" class="work-pool">
+      <template #header>
+        <CodeBanner :command="codeBannerCliCommand" :title="codeBannerTitle" :subtitle="codeBannerSubtitle" />
       </template>
+      <p-tabs :tabs="tabs">
+        <template #details>
+          <WorkPoolDetails :work-pool="workPool" />
+        </template>
 
-      <template #runs>
-        <FlowRunFilteredList :flow-run-filter="flowRunFilter" />
+        <template #runs>
+          <FlowRunFilteredList :flow-run-filter="flowRunFilter" />
+        </template>
+
+        <template #work-queues>
+          <WorkPoolQueuesTable :work-pool-name="workPoolName" />
+        </template>
+
+        <template #workers>
+          <WorkersTable :work-pool-name="workPoolName" />
+        </template>
+      </p-tabs>
+
+      <template #well>
+        <WorkPoolDetails alternate :work-pool="workPool" />
       </template>
-
-      <template #work-queues>
-        <WorkPoolQueuesTable :work-pool-name="workPoolName" />
-      </template>
-    </p-tabs>
-
-    <template #well>
-      <WorkPoolDetails alternate :work-pool="workPool" />
-    </template>
-  </p-layout-well>
+    </p-layout-well>
+  </p-layout-default>
 </template>
 
 <script lang="ts" setup>
   import { media } from '@prefecthq/prefect-design'
-  import { useWorkspaceApi, PageHeadingWorkPool, WorkPoolDetails, FlowRunFilteredList, WorkPoolQueuesTable, useRecentFlowRunsFilter } from '@prefecthq/prefect-ui-library'
+  import { useWorkspaceApi, PageHeadingWorkPool, WorkPoolDetails, FlowRunFilteredList, WorkPoolQueuesTable, useRecentFlowRunsFilter, WorkersTable, useCan } from '@prefecthq/prefect-ui-library'
   import { useRouteParam, useSubscription } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { usePageTitle } from '@/compositions/usePageTitle'
 
   const api = useWorkspaceApi()
   const workPoolName = useRouteParam('workPoolName')
+  const can = useCan()
 
   const tabs = computed(() => {
     const values = ['Runs', 'Work Queues']
+
+    if (can.access.workers && !isAgentWorkPool.value) {
+      values.push('Workers')
+    }
 
     if (!media.xl) {
       values.unshift('Details')
@@ -49,6 +63,16 @@
   }
   const workPoolSubscription = useSubscription(api.workPools.getWorkPoolByName, [workPoolName.value], subscriptionOptions)
   const workPool = computed(() => workPoolSubscription.response)
+  const isAgentWorkPool = computed(() => workPool.value?.type === 'prefect-agent')
+
+  const codeBannerTitle = computed(() => {
+    if (!workPool.value) {
+      return 'Your work pool is ready to go!'
+    }
+    return `Your work pool ${workPool.value.name} is ready to go!`
+  })
+  const codeBannerCliCommand = computed(() => `prefect ${isAgentWorkPool.value ? 'agent' : 'worker'} start --pool ${workPool.value?.name}`)
+  const codeBannerSubtitle = computed(() => `Work pools organize work that ${isAgentWorkPool.value ? 'Prefect agents' : 'Prefect workers'} can pull from.`)
 
   const { filter: flowRunFilter } = useRecentFlowRunsFilter({
     workPools: {
