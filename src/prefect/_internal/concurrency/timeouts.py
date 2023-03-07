a98fa4dcdabb55589d3d8433ea8850590dfce8a7
Zanie Adkins <michael@prefect.io>
2023-03-07 13:29:25 -0600

    Add cancellation reporting to calls and waiters (#8731)

--- src/prefect/_internal/concurrency/timeouts.py
+++ src/prefect/_internal/concurrency/timeouts.py
@@ -23,26 +23,47 @@ class CancelContext:
     """
     Tracks if a cancel context manager was cancelled.
 
-    The `cancelled` property is threadsafe.
+    A context cannot be marked as cancelled after it is reported as completed.
     """
 
     def __init__(self, timeout: Optional[float]) -> None:
         self._timeout = timeout
+        self._deadline = get_deadline(timeout)
         self._cancelled: bool = False
         self._chained: List["CancelContext"] = []
+        self._lock = threading.Lock()
+        self._completed = False
 
     @property
     def timeout(self) -> Optional[float]:
         return self._timeout
 
     @property
+    def deadline(self) -> Optional[float]:
+        return self._deadline
+
     def cancelled(self):
-        return self._cancelled
+        with self._lock:
+            return self._cancelled
+
+    def completed(self):
+        with self._lock:
+            return self._completed
 
     def mark_cancelled(self):
-        self._cancelled = True
-        for ctx in self._chained:
-            ctx.mark_cancelled()
+        with self._lock:
+            if self._completed:
+                return  # Do not mark completed tasks as cancelled
+
+            logger.debug("Marked %r as cancelled", self)
+            self._cancelled = True
+            for ctx in self._chained:
+                ctx.mark_cancelled()
+
+    def mark_completed(self):
+        with self._lock:
+            logger.debug("Marked %r as completed", self)
+            self._completed = True
 
     def chain(self, ctx: "CancelContext") -> None:
         """
@@ -52,10 +73,15 @@ class CancelContext:
         If this context is already cancelled, the given context will be marked as
         cancelled immediately.
         """
-        if self._cancelled:
-            ctx.mark_cancelled()
-        else:
-            self._chained.append(ctx)
+        with self._lock:
+            if self._cancelled:
+                ctx.mark_cancelled()
+            else:
+                self._chained.append(ctx)
+
+    def __repr__(self) -> str:
+        timeout = f"{self._timeout:.2f}" if self._timeout else "None"
+        return f"<CancelContext at {hex(id(self))} timeout={timeout}>"
 
 
 @contextlib.contextmanager
@@ -75,9 +101,7 @@ def cancel_async_after(timeout: Optional[float]):
 
     try:
         with anyio.fail_after(timeout) as cancel_scope:
-            logger.debug(
-                f"Entered asynchronous cancel context with %.2f timeout", timeout
-            )
+            logger.debug(f"Entered asynchronous cancel context %r", ctx)
             yield ctx
     finally:
         if cancel_scope.cancel_called:
@@ -134,9 +158,7 @@ def cancel_sync_at(deadline: Optional[float]):
 
     timeout = max(0, deadline - time.monotonic())
 
-    ctx = CancelContext(timeout=timeout)
-    with cancel_sync_after(timeout) as inner_ctx:
-        inner_ctx.chain(ctx)
+    with cancel_sync_after(timeout) as ctx:
         yield ctx
 
 
@@ -173,9 +195,9 @@ def cancel_sync_after(timeout: Optional[float]):
 
     with method(timeout) as ctx:
         logger.debug(
-            f"Entered synchronous cancel context with %.2f %s based timeout",
-            timeout,
+            f"Entered synchronous %s based cancel context %r",
             method_name,
+            ctx,
         )
         yield ctx
 
