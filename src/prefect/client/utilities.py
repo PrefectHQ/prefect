52d94e4f222a93ace5bac183835970438c47d9da
Michael Adkins <michael@prefect.io>
2023-03-08 12:31:33 -0600

    Revert "Disable loading the client from the context"
    This reverts commit a7033b0a8903ace51413ed3a3f92942f65a81401.

--- src/prefect/client/utilities.py
+++ src/prefect/client/utilities.py
@@ -20,13 +20,20 @@ def inject_client(fn):
 
     @wraps(fn)
     async def with_injected_client(*args, **kwargs):
+        import prefect.context
         from prefect.client.orchestration import get_client
 
         client = None
+        flow_run_ctx = prefect.context.FlowRunContext.get()
+        task_run_ctx = prefect.context.TaskRunContext.get()
 
         if "client" in kwargs and kwargs["client"] is not None:
             # Client provided in kwargs
             client = kwargs["client"]
+            client_context = asyncnullcontext()
+        elif flow_run_ctx is not None or task_run_ctx is not None:
+            # Client available in context
+            client = (flow_run_ctx or task_run_ctx).client
             client_context = asyncnullcontext()
         else:
             # A new client is needed
