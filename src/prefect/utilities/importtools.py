a7c6604f93e209a6788cc7960b8c639bb725719e
Michael Adkins <michael@prefect.io>
2023-03-09 12:44:25 -0600

    DEBUG: Fix inequality for correct version range

--- src/prefect/utilities/importtools.py
+++ src/prefect/utilities/importtools.py
@@ -28,7 +28,7 @@ def to_qualified_name(obj: Any) -> str:
     Returns:
         str: the qualified name
     """
-    if sys.version_info >= (3, 10):
+    if sys.version_info < (3, 10):
         if isinstance(obj, classmethod):
             raise ValueError(str(dir(obj)))
     return obj.__module__ + "." + obj.__qualname__
