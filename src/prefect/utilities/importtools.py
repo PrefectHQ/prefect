e25c8d03063caa1ad8dae7269756385ed085e08a
Michael Adkins <michael@prefect.io>
2023-03-09 12:36:43 -0600

    DEBUG: Display it like we mean it

--- src/prefect/utilities/importtools.py
+++ src/prefect/utilities/importtools.py
@@ -30,7 +30,7 @@ def to_qualified_name(obj: Any) -> str:
     """
     if sys.version_info >= (3, 10):
         if isinstance(obj, classmethod):
-            print(dir(obj))
+            raise ValueError(str(dir(obj)))
     return obj.__module__ + "." + obj.__qualname__
 
 
