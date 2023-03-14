8db2ab9cb5a619801694d9af3ff2f4fb4e6bc49a
Chris White <chris@prefect.io>
2023-03-14 11:55:45 -0700

    Auto-populate a pull step based on git

--- src/prefect/cli/project.py
+++ src/prefect/cli/project.py
@@ -34,7 +34,9 @@ def set_default_deployment_yaml(path: str) -> bool:
     return True
 
 
-def set_default_project_yaml(path: str, name: str = None) -> bool:
+def set_default_project_yaml(
+    path: str, name: str = None, pull_step: dict = None
+) -> bool:
     """
     Creates default project.yaml file in the provided path if one does not already exist;
     returns boolean specifying whether a file was created.
@@ -51,33 +53,33 @@ def set_default_project_yaml(path: str, name: str = None) -> bool:
     with open(path / "project.yaml", "w") as f:
         # write header
         f.write(
-            "###\n### File for configuring project / deployment build, push and pull steps\n###\n\n"
+            "# File for configuring project / deployment build, push and pull steps\n\n"
         )
 
-        f.write("### Generic metadata about this project\n")
+        f.write("# Generic metadata about this project\n")
         yaml.dump({"name": contents["name"]}, f, sort_keys=False)
         yaml.dump({"prefect-version": contents["prefect-version"]}, f, sort_keys=False)
         f.write("\n")
 
         # build
         f.write(
-            "### build section allows you to manage docker images and produce infrastructure configuration\n"
+            "# build section allows you to manage docker images and produce infrastructure configuration\n"
         )
         yaml.dump({"build": contents["build"]}, f, sort_keys=False)
         f.write("\n")
 
         # push
         f.write(
-            "### push section allows you to manage how this project is uploaded to remote locations\n"
+            "# push section allows you to manage how this project is uploaded to remote locations\n"
         )
         yaml.dump({"push": contents["push"]}, f, sort_keys=False)
         f.write("\n")
 
         # pull
         f.write(
-            "### pull section allows you to provide instructions for cloning this project in new locations.\n"
+            "# pull section allows you to provide instructions for cloning this project in new locations.\n"
         )
-        yaml.dump({"pull": contents["pull"]}, f, sort_keys=False)
+        yaml.dump({"pull": pull_step or contents["pull"]}, f, sort_keys=False)
     return True
 
 
@@ -90,6 +92,7 @@ async def init(name: str = None):
     # determine if in git repo or use directory name as a default
     is_git_based = False
     repo_name = None
+    pull_step = None
     try:
         p = subprocess.check_output(
             ["git", "remote", "get-url", "origin"],
@@ -103,12 +106,28 @@ async def init(name: str = None):
         dir_name = os.path.basename(os.getcwd())
         name = name or dir_name
 
+    # hand craft a pull step
+    if is_git_based:
+        try:
+            p = subprocess.check_output(
+                ["git", "rev-parse", "--abbrev-ref", "HEAD"],
+                shell=sys.platform == "win32",
+                stderr=subprocess.DEVNULL,
+            )
+            branch = p.decode().strip()
+        except subprocess.CalledProcessError:
+            branch = "main"
+
+        pull_step = [
+            {"prefect_github.clone_project": {"repo": repo_name, "branch": branch}}
+        ]
+
     files = []
     if set_default_ignore_file("."):
         files.append("[green].prefectignore[/green]")
     if set_default_deployment_yaml("."):
         files.append("[green]deployment.yaml[/green]")
-    if set_default_project_yaml(".", name=name):
+    if set_default_project_yaml(".", name=name, pull_step=pull_step):
         files.append("[green]project.yaml[/green]")
 
     files = "\n".join(files)
