c20884bcaab3291eec8462e32e055018fd676138
Michael Adkins <michael@prefect.io>
2023-03-15 08:57:35 -0500

    Use `SLEEP_TIME` in `test_task_timeout_deadline_is_reset_on_retry`

--- tests/public/tasks/test_task_timeouts.py
+++ tests/public/tasks/test_task_timeouts.py
@@ -90,14 +90,14 @@ async def test_async_task_timeout_in_async_flow():
 async def test_task_timeout_deadline_is_reset_on_retry():
     run_count: int = 0
 
-    @prefect.task(timeout_seconds=1, retries=2)
+    @prefect.task(timeout_seconds=0.1, retries=2)
     async def sleep_task():
         nonlocal run_count
         run_count += 1
 
         # Task should timeout 2 times then succeed on the third run
         if run_count < 3:
-            await anyio.sleep(2)
+            await anyio.sleep(SLEEP_TIME)
 
         return run_count
 
@@ -109,5 +109,5 @@ async def test_task_timeout_deadline_is_reset_on_retry():
         return t1 - t0, state
 
     runtime, task_state = await parent_flow()
-    assert runtime < 3, f"Task should exit early; ran for {runtime}s"
+    assert runtime < SLEEP_TIME, f"Task should exit early; ran for {runtime}s"
     assert await task_state.result() == 3  # Task should run 3 times
