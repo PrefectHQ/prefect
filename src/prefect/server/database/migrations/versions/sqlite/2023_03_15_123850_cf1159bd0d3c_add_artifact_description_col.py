a2b8933be84fb6c87bc62a7096e11d80fdbcbd06
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-03-16 10:51:22 -0500

    Add artifact description column (#8805)

---
+++ src/prefect/server/database/migrations/versions/sqlite/2023_03_15_123850_cf1159bd0d3c_add_artifact_description_col.py
@@ -0,0 +1,33 @@
+"""Add description col to artifact table
+
+Revision ID: cf1159bd0d3c
+Revises: f3df94dca3cc
+Create Date: 2023-03-15 12:38:50.049225
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "cf1159bd0d3c"
+down_revision = "f3df94dca3cc"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("PRAGMA foreign_keys=OFF")
+
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.add_column(sa.Column("description", sa.String(), nullable=True))
+
+    op.execute("PRAGMA foreign_keys=ON")
+
+
+def downgrade():
+    op.execute("PRAGMA foreign_keys=OFF")
+
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_column("description")
+
+    op.execute("PRAGMA foreign_keys=ON")
