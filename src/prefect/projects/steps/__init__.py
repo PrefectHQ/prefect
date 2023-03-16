505091aa10472bf9e6686f3ff4c05a4482c21926
Chris White <chris@prefect.io>
2023-03-15 17:47:36 -0700

    Docker image builds and pushes are working against dockerhub

--- src/prefect/projects/steps/__init__.py
+++ src/prefect/projects/steps/__init__.py
@@ -1,2 +1,3 @@
+from .build import build_docker_image
 from .core import run_step
 from .pull import git_clone_project, set_working_directory
--- src/prefect/projects/steps/build.py
+++ src/prefect/projects/steps/build.py
@@ -1,9 +1,44 @@
 """
 Core set of steps for specifying a Prefect project build step.
 """
-import subprocess
 import sys
+from pathlib import Path
+
+import pendulum
+
+from prefect.docker import build_image, docker_client
+from prefect.utilities.slugify import slugify
 
 
-def build_docker_image(dockerfile: str = None) -> dict:
-    pass
+def build_docker_image(
+    image_name: str, dockerfile: str = None, tag: str = None, push: bool = True
+) -> dict:
+    image_id = build_image(
+        context=Path("."),
+        dockerfile=dockerfile,
+        pull=True,
+        stream_progress_to=sys.stdout,
+    )
+
+    if not tag:
+        tag = slugify(pendulum.now("utc").isoformat())
+
+    if push:
+        with docker_client() as client:
+            image: Image = client.images.get(image_id)
+            image.tag(image_name, tag=tag)
+            events = client.api.push(image_name, tag=tag, stream=True, decode=True)
+            try:
+                for event in events:
+                    if "status" in event:
+                        sys.stdout.write(event["status"])
+                        if "progress" in event:
+                            sys.stdout.write(" " + event["progress"])
+                        sys.stdout.write("\n")
+                        sys.stdout.flush()
+                    elif "error" in event:
+                        raise OSError(event["error"])
+            finally:
+                client.api.remove_image(f"{image_name}:{tag}", noprune=True)
+
+    return dict(image_name=f"{image_name}:{tag}")
