67c8460f108656d3742892adedb82d57736801b6
Jeff Hale <jeffmshale@gmail.com>
2023-03-17 23:45:41 -0400

    move install and cloud quickstart under getting started in nav

--- docs/integrations/generate_catalog.py
+++ docs/integrations/generate_catalog.py
@@ -1,4 +1,4 @@
-"""Script to generate the collections catalog table"""
+"""Script to generate the integartions(formerly collections) catalog table"""
 import glob
 from pathlib import Path
 
@@ -7,7 +7,7 @@ import yaml
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 with mkdocs_gen_files.open("integrations/catalog.md", "w") as markdown_file:
-    # Get file paths for all collections files
+    # Get file paths for all integrations files
     collection_files = [
         file
         for file in glob.glob("./docs/integrations/catalog/*.yaml")
@@ -15,13 +15,13 @@ with mkdocs_gen_files.open("integrations/catalog.md", "w") as markdown_file:
         if Path(file).name != "TEMPLATE.yaml"
     ]
 
-    # Load collection information from YAML files into a list
+    # Load integration information from YAML files into a list
     collection_configs = []
     for collection_file in collection_files:
         with open(collection_file, "r") as file:
             collection_configs.append(yaml.safe_load(file))
 
-    # Sort collections alphabetically by name
+    # Sort integrations alphabetically by name
     sorted_collection_configs = sorted(
         collection_configs, key=lambda x: x["tag"].lower()
     )
