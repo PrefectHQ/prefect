be8bd3f3387cdf9913ad631fa0d5362d78b023a9
Chris White <chris@prefect.io>
2023-03-21 09:15:47 -0700

    More runtime attributes (#8864)

--- src/prefect/runtime/deployment.py
+++ src/prefect/runtime/deployment.py
@@ -14,6 +14,8 @@ Example usage:
 
 Available attributes:
     - `id`: the deployment's unique ID
+    - `name`: the deployment's name
+    - `version`: the deployment's version
     - `flow_run_id`: the current flow run ID for this deployment
     - `parameters`: the parameters that were passed to this run; note that these do not necessarily
         include default values set on the flow function, only the parameter values set on the deployment
@@ -23,11 +25,14 @@ import os
 from typing import Any, List, Optional
 
 from prefect._internal.concurrency.api import create_call, from_sync
+from prefect.client.orchestration import get_client
 from prefect.context import FlowRunContext
 
 from .flow_run import _get_flow_run
 
-__all__ = ["id", "flow_run_id", "parameters"]
+__all__ = ["id", "flow_run_id", "name", "parameters", "version"]
+
+CACHED_DEPLOYMENT = {}
 
 
 def __getattr__(name: str) -> Any:
@@ -45,6 +50,15 @@ def __getattr__(name: str) -> Any:
 
 def __dir__() -> List[str]:
     return sorted(__all__)
+
+
+async def _get_deployment(deployment_id):
+    # deployments won't change between calls so let's avoid the lifecycle of a client
+    if CACHED_DEPLOYMENT.get(deployment_id):
+        return CACHED_DEPLOYMENT[deployment_id]
+    async with get_client() as client:
+        CACHED_DEPLOYMENT[deployment_id] = await client.read_deployment(deployment_id)
+        return CACHED_DEPLOYMENT[deployment_id]
 
 
 def get_id() -> Optional[str]:
@@ -76,8 +90,38 @@ def get_parameters() -> dict:
     return flow_run.parameters or {}
 
 
+def get_name() -> dict:
+    dep_id = get_id()
+
+    if dep_id is None:
+        return None
+
+    deployment = from_sync.call_soon_in_loop_thread(
+        create_call(_get_deployment, dep_id)
+    ).result()
+    return deployment.name
+
+
+def get_version() -> dict:
+    dep_id = get_id()
+
+    if dep_id is None:
+        return None
+
+    deployment = from_sync.call_soon_in_loop_thread(
+        create_call(_get_deployment, dep_id)
+    ).result()
+    return deployment.version
+
+
 def get_flow_run_id() -> Optional[str]:
     return os.getenv("PREFECT__FLOW_RUN_ID")
 
 
-FIELDS = {"id": get_id, "flow_run_id": get_flow_run_id, "parameters": get_parameters}
+FIELDS = {
+    "id": get_id,
+    "flow_run_id": get_flow_run_id,
+    "parameters": get_parameters,
+    "name": get_name,
+    "version": get_version,
+}
