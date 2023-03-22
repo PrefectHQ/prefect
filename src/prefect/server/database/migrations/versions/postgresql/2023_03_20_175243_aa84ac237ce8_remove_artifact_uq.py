4e923d63588077697fa7323d16ad607e0e4e707c
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-03-22 13:08:19 -0500

    Allow multiple artifacts to have the same key (#8855)

---
+++ src/prefect/server/database/migrations/versions/postgresql/2023_03_20_175243_aa84ac237ce8_remove_artifact_uq.py
@@ -0,0 +1,32 @@
+"""Removes unique constraint from artifact table key column
+Revision ID: aa84ac237ce8
+Revises: 4a1a0e4f89de
+Create Date: 2023-03-20 17:52:43.438841
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "aa84ac237ce8"
+down_revision = "4a1a0e4f89de"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_index("ix_artifact__key")
+        batch_op.create_index(
+            "ix_artifact__key",
+            ["key"],
+            unique=False,
+        )
+
+
+def downgrade():
+    with op.batch_alter_table("artifact", schema=None) as batch_op:
+        batch_op.drop_index("ix_artifact__key")
+        batch_op.create_index(
+            "ix_artifact__key",
+            ["key"],
+            unique=True,
+        )
