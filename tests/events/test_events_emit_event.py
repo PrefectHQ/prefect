ca0ed42e6b0b2acea72371dc13049c921b8e8970
Chris Pickett <chris.pickett@prefect.io>
2023-03-22 09:58:18 -0400

    Create `emit_event` helper that takes args for an `Event` and emits it via a worker (#8867)

---
+++ tests/events/test_events_emit_event.py
@@ -0,0 +1,56 @@
+import time
+from uuid import UUID
+
+from prefect.events import emit_event
+from prefect.events.clients import AssertingEventsClient
+from prefect.events.worker import EventsWorker
+from prefect.server.utilities.schemas import DateTimeTZ
+
+
+def test_emits_simple_event(asserting_events_worker: EventsWorker, reset_worker_events):
+    emit_event(
+        event="vogon.poetry.read",
+        resource={"prefect.resource.id": "vogon.poem.oh-freddled-gruntbuggly"},
+    )
+
+    time.sleep(0.1)
+
+    assert isinstance(asserting_events_worker._client, AssertingEventsClient)
+
+    assert len(asserting_events_worker._client.events) == 1
+    event = asserting_events_worker._client.events[0]
+    assert event.event == "vogon.poetry.read"
+    assert event.resource.id == "vogon.poem.oh-freddled-gruntbuggly"
+
+
+def test_emits_complex_event(
+    asserting_events_worker: EventsWorker, reset_worker_events
+):
+    emit_event(
+        event="vogon.poetry.read",
+        resource={"prefect.resource.id": "vogon.poem.oh-freddled-gruntbuggly"},
+        occurred=DateTimeTZ(2023, 3, 1, 12, 39, 28),
+        related=[
+            {
+                "prefect.resource.id": "vogon.ship.the-business-end",
+                "prefect.resource.role": "locale",
+            }
+        ],
+        payload={"text": "Oh freddled gruntbuggly..."},
+        id=UUID(int=1),
+    )
+
+    time.sleep(0.1)
+
+    assert isinstance(asserting_events_worker._client, AssertingEventsClient)
+
+    assert len(asserting_events_worker._client.events) == 1
+    event = asserting_events_worker._client.events[0]
+    assert event.event == "vogon.poetry.read"
+    assert event.resource.id == "vogon.poem.oh-freddled-gruntbuggly"
+    assert event.occurred == DateTimeTZ(2023, 3, 1, 12, 39, 28)
+    assert len(event.related) == 1
+    assert event.related[0].id == "vogon.ship.the-business-end"
+    assert event.related[0].role == "locale"
+    assert event.payload == {"text": "Oh freddled gruntbuggly..."}
+    assert event.id == UUID(int=1)
