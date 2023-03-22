568333f692d259ec28a89ad10bc76cf072af0595
Zanie Adkins <michael@prefect.io>
2023-03-22 18:41:29 -0500

    Fix sleep time assertion (#8882)

--- tests/public/tasks/test_task_timeouts.py
+++ tests/public/tasks/test_task_timeouts.py
@@ -81,7 +81,7 @@ async def test_async_task_timeout_in_async_flow():
         return t1 - t0, state
 
     runtime, task_state = await parent_flow()
-    assert runtime < 1, f"Task should exit early; ran for {runtime}s"
+    assert runtime < SLEEP_TIME, f"Task should exit early; ran for {runtime}s"
     assert task_state.is_failed()
     with pytest.raises(TimeoutError):
         await task_state.result()
