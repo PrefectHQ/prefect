54977780068f2618211eca56b577716cd431e8ef
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-03-24 16:41:18 -0500

    Add index for querying artifacts (#8861)

---
+++ src/prefect/server/database/migrations/versions/postgresql/2023_03_20_185610_46bd82c6279a_add_index_on_artifact.py
@@ -0,0 +1,36 @@
+"""Add index on artifact
+
+Revision ID: 46bd82c6279a
+Revises: d20618ce678e
+Create Date: 2023-03-20 18:56:10.725419
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "46bd82c6279a"
+down_revision = "d20618ce678e"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        CREATE INDEX 
+        IF NOT EXISTS
+        ix_artifact__key_created_desc
+        ON artifact (key, created DESC)
+        INCLUDE (id, updated, type, task_run_id, flow_run_id)
+    """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+        DROP INDEX 
+        IF EXISTS
+        ix_artifact__key_created_desc
+    """
+    )
