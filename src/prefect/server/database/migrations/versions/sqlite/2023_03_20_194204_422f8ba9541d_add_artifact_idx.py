56cee46f9f559fccd2984dcb8150371d8a2a2941
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-03-24 16:41:18 -0500

    Add index for querying artifacts (#8861)

---
+++ src/prefect/server/database/migrations/versions/sqlite/2023_03_20_194204_422f8ba9541d_add_artifact_idx.py
@@ -0,0 +1,40 @@
+"""Add index for querying artifacts
+
+Revision ID: 422f8ba9541d
+Revises: b9aafc3ab936
+Create Date: 2023-03-20 19:42:04.862363
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "422f8ba9541d"
+down_revision = "b9aafc3ab936"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute("PRAGMA foreign_keys=OFF")
+
+    op.execute(
+        """
+        CREATE INDEX IF NOT EXISTS
+        ix_artifact__key_created_desc
+        ON artifact_collection (key, created DESC)
+        """
+    )
+    op.execute("PRAGMA foreign_keys=ON")
+
+
+def downgrade():
+    op.execute("PRAGMA foreign_keys=OFF")
+
+    op.execute(
+        """
+        DROP INDEX IF EXISTS
+        ix_artifact__key_created_desc
+        """
+    )
+
+    op.execute("PRAGMA foreign_keys=ON")
