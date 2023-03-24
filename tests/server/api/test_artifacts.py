405a4775fde2618453627bd197cafc67b7641971
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-03-24 15:17:48 -0500

    Add ability to filter artifacts by type (#8927)

--- tests/server/api/test_artifacts.py
+++ tests/server/api/test_artifacts.py
@@ -36,6 +36,7 @@ async def artifacts(flow_run, task_run, client):
         description="# This is a markdown description title",
         flow_run_id=flow_run.id,
         task_run_id=task_run.id,
+        type="table",
     ).dict(json_compatible=True)
     artifact1 = await client.post("/experimental/artifacts/", json=artifact1_schema)
 
@@ -45,6 +46,7 @@ async def artifacts(flow_run, task_run, client):
         data=2,
         flow_run_id=flow_run.id,
         task_run_id=uuid4(),
+        type="markdown",
     ).dict(json_compatible=True)
     artifact2 = await client.post("/experimental/artifacts/", json=artifact2_schema)
 
@@ -53,6 +55,7 @@ async def artifacts(flow_run, task_run, client):
         description="# This is a markdown description title",
         flow_run_id=uuid4(),
         task_run_id=uuid4(),
+        type="result",
     ).dict(json_compatible=True)
     artifact3 = await client.post("/experimental/artifacts/", json=artifact3_schema)
 
@@ -255,6 +258,38 @@ class TestReadArtifacts:
         assert all(
             [item["task_run_id"] == str(task_run_id) for item in response.json()]
         )
+
+    async def test_read_artifacts_with_artifact_type_filter_any(
+        self, artifacts, client
+    ):
+        artifact_type = artifacts[1]["type"]
+        artifact_type_filter = dict(
+            artifacts=schemas.filters.ArtifactFilter(
+                type=schemas.filters.ArtifactFilterType(any_=[artifact_type])
+            ).dict(json_compatible=True)
+        )
+        response = await client.post(
+            "/experimental/artifacts/filter", json=artifact_type_filter
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()) == 1
+        assert response.json()[0]["type"] == artifact_type
+
+    async def test_read_artifacts_with_artifact_type_filter_not_any(
+        self, artifacts, client
+    ):
+        artifact_type = artifacts[2]["type"]
+        artifact_type_filter = dict(
+            artifacts=schemas.filters.ArtifactFilter(
+                type=schemas.filters.ArtifactFilterType(not_any_=[artifact_type])
+            ).dict(json_compatible=True)
+        )
+        response = await client.post(
+            "/experimental/artifacts/filter", json=artifact_type_filter
+        )
+        assert response.status_code == status.HTTP_200_OK
+        assert len(response.json()) == 2
+        assert all([item["type"] != artifact_type for item in response.json()])
 
     async def test_read_artifacts_with_multiple_filters(
         self, artifacts, flow_run, task_run, client
