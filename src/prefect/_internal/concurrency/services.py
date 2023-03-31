21e88294559258496fe8ddc63fa8ded508ba32ac
Michael Adkins <michael@prefect.io>
2023-03-31 11:23:41 -0500

    Update `send` to be an instance method and allow args to configure instances

--- src/prefect/_internal/concurrency/services.py
+++ src/prefect/_internal/concurrency/services.py
@@ -1,11 +1,10 @@
 import abc
 import asyncio
 import atexit
-import concurrent.futures
 import contextlib
 import threading
 from collections import deque
-from typing import Generic, Optional, Set, Type, TypeVar
+from typing import Awaitable, Dict, Generic, Optional, Type, TypeVar, Union
 
 from typing_extensions import Self
 
@@ -18,11 +17,10 @@ T = TypeVar("T")
 
 
 class QueueService(abc.ABC, Generic[T]):
-    _instance: Optional[Self] = None
-    _instances: Set[Self] = set()
+    _instances: Dict[int, Self] = {}
     _instance_lock = threading.Lock()
 
-    def __init__(self) -> None:
+    def __init__(self, *args) -> None:
         self._queue: Optional[asyncio.Queue] = None
         self._loop: Optional[asyncio.AbstractEventLoop] = None
         self._done_event: Optional[asyncio.Event] = None
@@ -31,6 +29,7 @@ class QueueService(abc.ABC, Generic[T]):
         self._lock = threading.Lock()
         self._stopped: bool = False
         self._started: bool = False
+        self._key = hash(args)
 
     def start(self):
         self._queue = asyncio.Queue()
@@ -45,7 +44,7 @@ class QueueService(abc.ABC, Generic[T]):
             self.send(self._early_items.pop())
 
         # Stop at interpreter exit by default
-        atexit.register(self.drain)
+        atexit.register(self.drain_all)
 
     def _stop(self):
         """
@@ -53,8 +52,11 @@ class QueueService(abc.ABC, Generic[T]):
 
         Does not wait for the instance to finish. See `drain`.
         """
+        if self._stopped:
+            return
+
         # Stop sending work to this instance
-        self._clear_instance(self)
+        self._remove_instance()
 
         if not self._started:
             # Wait for start to finish before stopping
@@ -63,9 +65,9 @@ class QueueService(abc.ABC, Generic[T]):
         self._stopped = True
         call_soon_in_loop(self._loop, self._queue.put_nowait, None)
 
-    def _send(self, item: T):
+    def send(self, item: T):
         """
-        Send an item to the instance.
+        Send an item to the instance of the service.
         """
         if self._stopped:
             raise RuntimeError("Cannot put items in a stopped service.")
@@ -92,70 +94,58 @@ class QueueService(abc.ABC, Generic[T]):
         Process an item sent to the service.
         """
 
-    @classmethod
-    def send(cls, item: T) -> None:
+    def drain_one(self) -> Union[Awaitable, None]:
         """
-        Send an item to an instance of the service.
+        Stop this instance of the service and wait for remaining work to be completed.
+
+        Returns an awaitable if called from an async context.
         """
-        cls.get_instance()._send(item)
+        self._stop()
+        future = asyncio.run_coroutine_threadsafe(self._done_event.wait(), self._loop)
+        if get_running_loop() is not None:
+            return asyncio.wrap_future(future)
+        else:
+            return future.result()
 
     @classmethod
-    def drain(cls, wait: bool = True):
+    def drain_all(cls) -> Union[Awaitable, None]:
         """
         Stop all instances of the service and wait for all remaining work to be
         completed.
 
-        This method is not safe to call from an asynchronous context unless `wait` is
-        set to `False`.
+        Returns an awaitable if called from an async context.
         """
-        if wait and get_running_loop() is not None:
-            raise RuntimeError(
-                "Queue services cannot be drained from an async context without "
-                "`wait=False`."
-            )
-
         futures = []
-
-        with cls._instance_lock:
-            instances = tuple(cls._instances)
+        instances = tuple(cls._instances.values())
 
         for instance in instances:
-            if not instance._stopped:
-                instance._stop()
+            futures.append(instance.drain_one())
 
-            futures.append(
-                asyncio.run_coroutine_threadsafe(
-                    instance._done_event.wait(), instance._loop
-                )
-            )
-
-            cls._instances.discard(instance)
-
-        if wait:
-            concurrent.futures.wait(futures)
-
-        return futures
+        if get_running_loop() is not None:
+            return asyncio.gather(*futures)
 
     @classmethod
-    def get_instance(cls: Type[Self]) -> Self:
-        with cls._instance_lock:
-            if cls._instance is None:
-                cls._instance = cls._new_instance()
+    def instance(cls: Type[Self], *args) -> Self:
+        """
+        Get an instance of the service.
 
-            return cls._instance
+        If an instance already exists with the given arguments, it will be returned.
+        """
+        key = hash(args)
+        if key not in cls._instances:
+            cls._instances[key] = cls._new_instance(*args)
+
+        return cls._instances[key]
+
+    def _remove_instance(self):
+        self._instances.pop(self._key, None)
 
     @classmethod
-    def _clear_instance(cls, instance: Optional[Self] = None):
-        with cls._instance_lock:
-            if instance is None or instance == cls._instance:
-                cls._instance = None
-
-    @classmethod
-    def _new_instance(cls):
+    def _new_instance(cls, *args):
         """
         Create and start a new instance of the service.
         """
-        instance = cls()
+        instance = cls(*args)
 
         # If already on the global loop, just start it here to avoid deadlock
         if threading.get_ident() == get_global_loop().thread.ident:
@@ -165,19 +155,16 @@ class QueueService(abc.ABC, Generic[T]):
         else:
             from_sync.call_soon_in_loop_thread(create_call(instance.start)).result()
 
-        cls._instances.add(instance)
-
         return instance
 
 
 @contextlib.contextmanager
 def drain_on_exit(service: QueueService):
     yield
-    service.drain()
+    service.drain_all()
 
 
 @contextlib.asynccontextmanager
 async def drain_on_exit_async(service: QueueService):
     yield
-    futures = [asyncio.wrap_future(fut) for fut in service.drain(wait=False)]
-    await asyncio.gather(*futures)
+    await service.drain_all()
