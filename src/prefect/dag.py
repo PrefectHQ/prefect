186f499b914053c0cb8e218b880b8e92aa64b823
Michael Adkins <michael@prefect.io>
2023-04-07 12:18:59 -0500

    Hack subset of magic methods; only run with bools toggled once

--- src/prefect/dag.py
+++ src/prefect/dag.py
@@ -1,5 +1,5 @@
-from typing import Optional, Set
-from unittest.mock import Mock
+from typing import Set
+from unittest.mock import MagicProxy, Mock
 
 import pydantic
 
@@ -20,19 +20,52 @@ class DAGBuildContext(ContextModel):
     def add_mock(self, mock):
         self.mocks.add(mock)
 
-    @classmethod
-    def toggle(cls, last_ctx: Optional["DAGBuildContext"]) -> "DAGBuildContext":
-        if last_ctx is None:
-            return cls()
 
-        return cls(
-            # Toggle booleans
-            bool_returns=not last_ctx.bool_returns,
-            mocks=last_ctx.mocks,
-        )
+magic_methods = [
+    "__lt__",
+    "__le__",
+    "__gt__",
+    "__ge__",
+    "__eq__",
+    "__ne__",
+    "__getitem__",
+    "__setitem__",
+    "__delitem__",
+    "__len__",
+    "__contains__",
+    "__str__",
+    "__sizeof__",
+    "__enter__",
+    "__exit__",
+    "__divmod__",
+    "__rdivmod__",
+    "__neg__",
+    "__pos__",
+    "__abs__",
+    "__invert__",
+    "__complex__",
+    "__int__",
+    "__float__",
+    "__index__",
+    "__round__",
+    "__trunc__",
+    "__floor__",
+    "__ceil__",
+    "__bool__",
+    "__next__",
+    "__fspath__",
+    "__aiter__",
+]
 
 
 class DAGMock(Mock):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        _type = type(self)
+        for entry in magic_methods:
+            setattr(_type, entry, MagicProxy(entry, self))
+
     def __hash__(self) -> int:
         return super().__hash__()
 
@@ -45,13 +78,12 @@ class FutureMock(DAGMock):
         return self._get_result_mock()
 
     def _get_result_mock(self):
-        mock = ResultMock()
-        mock.__iter__ = lambda _: iter([mock[0]])
-        return mock
+        return ResultMock()
 
 
 class ResultMock(DAGMock):
-    pass
+    def __iter__(self):
+        return iter([self[0]])
 
 
 def get_mock_for_future(future: "PrefectFuture"):
@@ -60,26 +92,13 @@ def get_mock_for_future(future: "PrefectFuture"):
 
 
 def build_dag(flow: Flow[P, R], *args: P.args, **kwargs: P.kwargs):
-    last_ctx = None
+    with DAGBuildContext(bool_returns=True) as ctx_true:
+        flow(*args, **kwargs)
 
-    while True:
-        logger.debug("Running iteration")
-        with DAGBuildContext.toggle(last_ctx) as ctx:
-            flow(*args, **kwargs)
+    with DAGBuildContext(bool_returns=False) as ctx_false:
+        flow(*args, **kwargs)
 
-        if stable_dag(last_ctx, ctx):
-            break
-
-        last_ctx = ctx
-
-    return ctx.mocks
-
-
-def stable_dag(previous, current) -> bool:
-    if previous is None or current is None:
-        return False
-
-    return set(previous.mocks) == set(current.mocks)
+    return ctx_true.mocks.union(ctx_false.mocks)
 
 
 def is_building_dag() -> bool:
