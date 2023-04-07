053376a50f51f990778fe847ccf568e056e53943
Chris White <chris@prefect.io>
2023-04-07 11:33:15 -0700

    Add docs to api-ref and add step mechanics

--- src/prefect/projects/steps/core.py
+++ src/prefect/projects/steps/core.py
@@ -5,7 +5,7 @@ Project steps are YAML representations of Python functions along with their inpu
 
 Whenever a step is run, the following actions are taken:
 
-- The step's inputs are resolved (see [the projects concepts documentation](/concepts/projects/#templating-options) for more details)
+- The step's inputs and block / variable references are resolved (see [the projects concepts documentation](/concepts/projects/#templating-options) for more details)
 - The step's function is imported; if it cannot be found, the `requires` keyword is used to install the necessary packages
 - The step's function is called with the resolved inputs
 - The step's output is returned and used to resolve inputs for subsequent steps
