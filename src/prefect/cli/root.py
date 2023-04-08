330a7c87dc50a42a60728833c2bb871a4ab3af57
Alexander Streed <alex.s@prefect.io>
2023-04-08 06:39:35 -0500

    Remove duplicate code

--- src/prefect/cli/root.py
+++ src/prefect/cli/root.py
@@ -283,12 +283,6 @@ async def deploy(
     if project.get("pull") is None:
         project["pull"] = []
 
-    # Pull from deployment config if not passed
-    if entrypoint is None:
-        entrypoint = base_deploy.get("entrypoint")
-    if flow_name is None:
-        flow_name = base_deploy.get("flow_name")
-
     if (
         not flow_name
         and not base_deploy.get("flow_name")
@@ -296,9 +290,9 @@ async def deploy(
         and not base_deploy.get("entrypoint")
     ):
         exit_with_error("An entrypoint or flow name must be provided.")
-    if not name and not base_deploy["name"]:
+    if not name and not base_deploy.get("name"):
         exit_with_error("A deployment name must be provided.")
-    if (flow_name or base_deploy["flow_name"]) and entrypoint:
+    if (flow_name or base_deploy.get("flow_name")) and entrypoint:
         exit_with_error("Can only pass an entrypoint or a flow name but not both.")
 
     # Pull from deployment config if not passed
