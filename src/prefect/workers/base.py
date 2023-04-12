238580d6fc2feab73b0908555d7b01339e02d71b
Alexander Streed <alex.s@prefect.io>
2023-04-12 10:41:47 -0500

    Adds support for cancellation to BaseWorker and ProcessWorker

--- src/prefect/workers/base.py
+++ src/prefect/workers/base.py
@@ -1,4 +1,5 @@
 import abc
+import inspect
 from typing import TYPE_CHECKING, Any, Dict, List, Optional, Set, Type, Union
 from uuid import uuid4
 
@@ -14,10 +15,27 @@ from prefect.engine import propose_state
 from prefect.events import emit_event
 from prefect.events.related import object_as_related_resource, tags_as_related_resources
 from prefect.events.schemas import RelatedResource
-from prefect.exceptions import Abort, ObjectNotFound
+from prefect.exceptions import (
+    Abort,
+    InfrastructureNotAvailable,
+    InfrastructureNotFound,
+    ObjectNotFound,
+)
 from prefect.logging.loggers import get_logger
 from prefect.server import schemas
 from prefect.server.schemas.actions import WorkPoolUpdate
+from prefect.server.schemas.filters import (
+    FlowRunFilter,
+    FlowRunFilterId,
+    FlowRunFilterState,
+    FlowRunFilterStateName,
+    FlowRunFilterStateType,
+    WorkPoolFilter,
+    WorkPoolFilterName,
+    WorkQueueFilter,
+    WorkQueueFilterName,
+)
+from prefect.server.schemas.states import StateType
 from prefect.settings import PREFECT_WORKER_PREFETCH_SECONDS, get_current_settings
 from prefect.states import Crashed, Pending, exception_to_failed_state
 from prefect.utilities.dispatch import register_base_type
@@ -316,6 +334,8 @@ class BaseWorker(abc.ABC):
         self._limit = limit
         self._limiter: Optional[anyio.CapacityLimiter] = None
         self._submitting_flow_run_ids = set()
+        self._cancelling_flow_run_ids = set()
+        self._scheduled_task_scopes = set()
 
     @classmethod
     def get_documentation_url(cls) -> str:
@@ -364,6 +384,17 @@ class BaseWorker(abc.ABC):
             "Workers must implement a method for running submitted flow runs"
         )
 
+    async def kill_infrastructure(
+        self, infrastructure_pid: str, grace_seconds: int = 30
+    ):
+        """
+        Method for killing infrastructure created by a worker. Should be implemented by
+        individual workers if they support killing infrastructure.
+        """
+        raise NotImplementedError(
+            "This worker does not support killing infrastructure."
+        )
+
     @classmethod
     def __dispatch_key__(cls):
         if cls.__name__ == "BaseWorker":
@@ -397,6 +428,104 @@ class BaseWorker(abc.ABC):
     async def get_and_submit_flow_runs(self):
         runs_response = await self._get_scheduled_flow_runs()
         return await self._submit_scheduled_flow_runs(flow_run_response=runs_response)
+
+    async def check_for_cancelled_flow_runs(self):
+        if not self.is_setup:
+            raise RuntimeError(
+                "Worker is not set up. Please make sure you are running this worker "
+                "as an async context manager."
+            )
+
+        self._logger.debug("Checking for cancelled flow runs...")
+
+        work_queue_filter = (
+            WorkQueueFilter(name=WorkQueueFilterName(any_=list(self._work_queues)))
+            if self._work_queues
+            else None
+        )
+
+        named_cancelling_flow_runs = await self._client.read_flow_runs(
+            flow_run_filter=FlowRunFilter(
+                state=FlowRunFilterState(
+                    type=FlowRunFilterStateType(any_=[StateType.CANCELLED]),
+                    name=FlowRunFilterStateName(any_=["Cancelling"]),
+                ),
+                # Avoid duplicate cancellation calls
+                id=FlowRunFilterId(not_any_=list(self._cancelling_flow_run_ids)),
+            ),
+            work_pool_filter=WorkPoolFilter(
+                name=WorkPoolFilterName(any_=[self._work_pool_name])
+            ),
+            work_queue_filter=work_queue_filter,
+        )
+
+        typed_cancelling_flow_runs = await self._client.read_flow_runs(
+            flow_run_filter=FlowRunFilter(
+                state=FlowRunFilterState(
+                    type=FlowRunFilterStateType(any_=[StateType.CANCELLING]),
+                ),
+                # Avoid duplicate cancellation calls
+                id=FlowRunFilterId(not_any_=list(self._cancelling_flow_run_ids)),
+            ),
+            work_pool_filter=WorkPoolFilter(
+                name=WorkPoolFilterName(any_=[self._work_pool_name])
+            ),
+            work_queue_filter=work_queue_filter,
+        )
+
+        cancelling_flow_runs = named_cancelling_flow_runs + typed_cancelling_flow_runs
+
+        if cancelling_flow_runs:
+            self._logger.info(
+                f"Found {len(cancelling_flow_runs)} flow runs awaiting cancellation."
+            )
+
+        for flow_run in cancelling_flow_runs:
+            self._cancelling_flow_run_ids.add(flow_run.id)
+            self._runs_task_group.start_soon(self.cancel_run, flow_run)
+
+        return cancelling_flow_runs
+
+    async def cancel_run(self, flow_run: "FlowRun"):
+        if not flow_run.infrastructure_pid:
+            self._logger.error(
+                f"Flow run '{flow_run.id}' does not have an infrastructure pid"
+                " attached. Cancellation cannot be guaranteed."
+            )
+            await self._mark_flow_run_as_cancelled(
+                flow_run,
+                state_updates={
+                    "message": (
+                        "This flow run is missing infrastructure tracking information"
+                        " and cancellation cannot be guaranteed."
+                    )
+                },
+            )
+            return
+
+        try:
+            await self.kill_infrastructure(flow_run.infrastructure_pid)
+        except NotImplementedError:
+            self._logger.error(
+                f"Worker type {self.type!r} does not support killing created "
+                "infrastructure. Cancellation cannot be guaranteed."
+            )
+        except InfrastructureNotFound as exc:
+            self._logger.warning(f"{exc} Marking flow run as cancelled.")
+            await self._mark_flow_run_as_cancelled(flow_run)
+        except InfrastructureNotAvailable as exc:
+            self._logger.warning(f"{exc} Flow run cannot be cancelled by this worker.")
+        except Exception:
+            self._logger.exception(
+                "Encountered exception while killing infrastructure for flow run "
+                f"'{flow_run.id}'. Flow run may not be cancelled."
+            )
+            # We will try again on generic exceptions
+            self._cancelling_flow_run_ids.remove(flow_run.id)
+            return
+        else:
+            await self._mark_flow_run_as_cancelled(flow_run)
+            self._logger.info(f"Cancelled flow run '{flow_run.id}'!")
 
     async def _update_local_work_pool_info(self):
         try:
@@ -730,6 +859,23 @@ class BaseWorker(abc.ABC):
                     f"Reported flow run '{flow_run.id}' as crashed: {message}"
                 )
 
+    async def _mark_flow_run_as_cancelled(
+        self, flow_run: "FlowRun", state_updates: Optional[dict] = None
+    ) -> None:
+        state_updates = state_updates or {}
+        state_updates.setdefault("name", "Cancelled")
+        state_updates.setdefault("type", StateType.CANCELLED)
+        state = flow_run.state.copy(update=state_updates)
+
+        await self._client.set_flow_run_state(flow_run.id, state, force=True)
+
+        # Do not remove the flow run from the cancelling set immediately because
+        # the API caches responses for the `read_flow_runs` and we do not want to
+        # duplicate cancellations.
+        await self._schedule_task(
+            60 * 10, self._cancelling_flow_run_ids.remove, flow_run.id
+        )
+
     async def _set_work_pool_template(self, work_pool, job_template):
         """Updates the `base_job_template` for the worker's workerpool server side."""
         await self._client.update_work_pool(
@@ -738,6 +884,34 @@ class BaseWorker(abc.ABC):
                 base_job_template=job_template,
             ),
         )
+
+    async def _schedule_task(self, __in_seconds: int, fn, *args, **kwargs):
+        """
+        Schedule a background task to start after some time.
+
+        These tasks will be run immediately when the worker exits instead of waiting.
+
+        The function may be async or sync. Async functions will be awaited.
+        """
+
+        async def wrapper(task_status):
+            # If we are shutting down, do not sleep; otherwise sleep until the scheduled
+            # time or shutdown
+            if self.setup:
+                with anyio.CancelScope() as scope:
+                    self._scheduled_task_scopes.add(scope)
+                    task_status.started()
+                    await anyio.sleep(__in_seconds)
+
+                self._scheduled_task_scopes.remove(scope)
+            else:
+                task_status.started()
+
+            result = fn(*args, **kwargs)
+            if inspect.iscoroutine(result):
+                await result
+
+        await self._runs_task_group.start(wrapper)
 
     async def __aenter__(self):
         self._logger.debug("Entering worker context...")
