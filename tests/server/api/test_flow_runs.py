dfbdcb3ff17ce16854a9d849e99f6b6f5e69baef
Michael Adkins <michael@prefect.io>
2023-04-25 11:09:18 -0500

    Remove manual retry test

--- tests/server/api/test_flow_runs.py
+++ tests/server/api/test_flow_runs.py
@@ -1031,24 +1031,6 @@ class TestManuallyRetryingFlowRuns:
         )
         assert restarted_run.state.type == StateType.SCHEDULED
 
-    async def test_only_proposing_scheduled_states_manually_retries(
-        self, failed_flow_run_with_deployment, client, session
-    ):
-        assert failed_flow_run_with_deployment.run_count == 1
-        assert failed_flow_run_with_deployment.deployment_id
-        flow_run_id = failed_flow_run_with_deployment.id
-
-        await client.post(
-            f"/flow_runs/{flow_run_id}/set_state",
-            json=dict(state=dict(type="RUNNING", name="AwaitingRetry")),
-        )
-
-        session.expire_all()
-        restarted_run = await models.flow_runs.read_flow_run(
-            session=session, flow_run_id=flow_run_id
-        )
-        assert restarted_run.state.type == "FAILED"
-
     async def test_cannot_restart_flow_run_without_deployment(
         self, failed_flow_run_without_deployment, client, session
     ):
