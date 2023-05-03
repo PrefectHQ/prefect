b43841caa0f5217fc9648c4f70d67cf5339d4057
Jeff Hale <jeffmshale@gmail.com>
2023-05-03 11:53:12 -0400

    fix for black

--- src/prefect/cli/deployment.py
+++ src/prefect/cli/deployment.py
@@ -900,9 +900,9 @@ async def build(
             "The slug of a remote storage block. Use the syntax:"
             " 'block_type/block_name', where block_type is one of 'github', 's3',"
             " 'gcs', 'azure', 'smb', or a registered block from a library that"
-            " implements the WritableDeploymentStorage interface, such as"
+            " implements the WritableDeploymentStorage interface such as"
             " 'gitlab-repository', 'bitbucket-repository', 's3-bucket',"
-            " 'gcs-bucket'"   
+            " 'gcs-bucket'"
         ),
     ),
     skip_upload: bool = typer.Option(
