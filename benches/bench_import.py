98f0a41aad207cc5a27ea6b337be26670bf0aba3
Zanie Adkins <zanie@prefect.io>
2023-05-12 15:51:36 -0500

    Add benchmarks for `import prefect` (#9558)

---
+++ benches/bench_import.py
@@ -0,0 +1,8 @@
+import subprocess
+from pytest_benchmark.fixture import BenchmarkFixture
+
+
+def bench_import_prefect(benchmark: BenchmarkFixture):
+    benchmark.pedantic(
+        subprocess.check_call, args=(["python", "-c", "import prefect"],), rounds=5
+    )
