7902bb56d088662eb82ff510fe303d7f0cefbd55
Alexander Streed <alex.s@prefect.io>
2023-05-17 16:14:23 -0500

    Include server version in version guard

--- flows/worker.py
+++ flows/worker.py
@@ -1,3 +1,4 @@
+import os
 import prefect
 import subprocess
 from packaging.version import Version
@@ -5,8 +6,11 @@ from packaging.version import Version
 
 # Checks to make sure that collections are loaded prior to attempting to start a worker
 def main():
-    # Worker CLI was introduced in 2.8.0
-    if Version(prefect.__version__) >= Version("2.8"):
+    TEST_SERVER_VERSION = os.environ.get("TEST_SERVER_VERSION")
+    # Work pool became GA in 2.8.0
+    if Version(prefect.__version__) >= Version("2.8") and Version(
+        TEST_SERVER_VERSION
+    ) >= Version("2.8"):
         subprocess.check_call(["python", "-m", "pip", "install", "prefect-kubernetes"])
         subprocess.check_call(
             [
