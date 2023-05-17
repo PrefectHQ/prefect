4dbbd9af44ef8c6167bdb2aa9fb57346a636f281
Alexander Streed <alex.s@prefect.io>
2023-05-17 16:01:56 -0500

    Adds version guard

--- flows/worker.py
+++ flows/worker.py
@@ -1,24 +1,28 @@
+import prefect
 import subprocess
+from packaging.version import Version
 
 
 # Checks to make sure that collections are loaded prior to attempting to start a worker
 def main():
-    subprocess.check_call(["python", "-m", "pip", "install", "prefect-kubernetes"])
-    subprocess.check_call(
-        [
-            "prefect",
-            "worker",
-            "start",
-            "-p",
-            "test-pool",
-            "-t",
-            "kubernetes",
-            "--run-once",
-        ]
-    )
-    subprocess.check_call(
-        ["python", "-m", "pip", "uninstall", "prefect-kubernetes", "-y"]
-    )
+    # Worker CLI was introduced in 2.8.0
+    if Version(prefect.__version__) > Version("2.7"):
+        subprocess.check_call(["python", "-m", "pip", "install", "prefect-kubernetes"])
+        subprocess.check_call(
+            [
+                "prefect",
+                "worker",
+                "start",
+                "-p",
+                "test-pool",
+                "-t",
+                "kubernetes",
+                "--run-once",
+            ]
+        )
+        subprocess.check_call(
+            ["python", "-m", "pip", "uninstall", "prefect-kubernetes", "-y"]
+        )
 
 
 if __name__ == "__main__":
