39759a835a3dcb74722172949e51c4caf78269fa
Zanie Adkins <zanie@prefect.io>
2023-06-01 16:23:17 -0500

    Fix service logging errors during drain at exit

--- src/prefect/_internal/concurrency/services.py
+++ src/prefect/_internal/concurrency/services.py
@@ -62,7 +62,7 @@ class QueueService(abc.ABC, Generic[T]):
 
         # Stop at interpreter exit by default
         if sys.version_info < (3, 9):
-            atexit.register(self.drain)
+            atexit.register(self._at_exit)
         else:
             # See related issue at https://bugs.python.org/issue42647
             # Handling items may require spawning a thread and in 3.9  new threads
@@ -72,9 +72,12 @@ class QueueService(abc.ABC, Generic[T]):
             # httpx client.
             from threading import _register_atexit
 
-            _register_atexit(self.drain)
+            _register_atexit(self._at_exit)
 
-    def _stop(self):
+    def _at_exit(self):
+        self.drain(at_exit=True)
+
+    def _stop(self, at_exit: bool = False):
         """
         Stop running this instance.
 
@@ -85,7 +88,8 @@ class QueueService(abc.ABC, Generic[T]):
             return
 
         with self._lock:
-            logger.debug("Stopping service %r", self)
+            if not at_exit:  # The logger may not be available during interpreter exit
+                logger.debug("Stopping service %r", self)
 
             # Stop sending work to this instance
             self._remove_instance()
@@ -167,13 +171,14 @@ class QueueService(abc.ABC, Generic[T]):
         """
         yield
 
-    def _drain(self) -> concurrent.futures.Future:
+    def _drain(self, at_exit: bool = False) -> concurrent.futures.Future:
         """
         Internal implementation for `drain`. Returns a future for sync/async interfaces.
         """
 
         logger.debug("Draining service %r", self)
-        self._stop()
+        self._stop(at_exit=at_exit)
+
         if self._done_event.is_set():
             future = concurrent.futures.Future()
             future.set_result(None)
@@ -182,13 +187,13 @@ class QueueService(abc.ABC, Generic[T]):
         future = asyncio.run_coroutine_threadsafe(self._done_event.wait(), self._loop)
         return future
 
-    def drain(self) -> None:
+    def drain(self, at_exit: bool = False) -> None:
         """
         Stop this instance of the service and wait for remaining work to be completed.
 
         Returns an awaitable if called from an async context.
         """
-        future = self._drain()
+        future = self._drain(at_exit=at_exit)
         if get_running_loop() is not None:
             return asyncio.wrap_future(future)
         else:
