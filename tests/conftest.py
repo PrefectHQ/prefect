8bdc8d167e739f288520d93df7103f59283de148
Zanie Adkins <zanie@prefect.io>
2023-06-02 13:40:15 -0500

    Disable asyncio debug mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -215,7 +215,7 @@ def event_loop(request):
     asyncio_logger = logging.getLogger("asyncio")
     asyncio_logger.setLevel("WARNING")
     asyncio_logger.addHandler(logging.StreamHandler())
-    loop.set_debug(True)
+    loop.set_debug(False)
     loop.slow_callback_duration = 0.25
 
     try:
