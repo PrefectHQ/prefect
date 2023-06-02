c7fc765c0d22b8e3b2a988f865a3edfaa7e3a7b8
Zanie Adkins <zanie@prefect.io>
2023-06-02 12:43:09 -0500

    DEBUG: Silence exception message

--- src/prefect/_internal/concurrency/calls.py
+++ src/prefect/_internal/concurrency/calls.py
@@ -201,7 +201,7 @@ class Call(Generic[T]):
         except BaseException as exc:
             self.cancel_context.mark_completed()
             self.future.set_exception(exc)
-            logger.debug("Encountered exception in call %r", self, exc_info=True)
+            # logger.debug("Encountered exception in call %r", self, exc_info=True)
             # Prevent reference cycle in `exc`
             del self
         else:
