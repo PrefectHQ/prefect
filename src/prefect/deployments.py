c07c6e19507f663935063f0cfab85501e0b8e75a
Chris Pickett <chris.pickett@prefect.io>
2023-06-09 16:04:28 -0400

    Exclude `triggers` field when loading a deployment via the API

--- src/prefect/deployments.py
+++ src/prefect/deployments.py
@@ -575,7 +575,7 @@ class Deployment(BaseModel):
                     )
 
                 excluded_fields = self.__fields_set__.union(
-                    {"infrastructure", "storage", "timestamp"}
+                    {"infrastructure", "storage", "timestamp", "triggers"}
                 )
                 for field in set(self.__fields__.keys()) - excluded_fields:
                     new_value = getattr(deployment, field)
