2f60750bdb50665d3967d5ba763bcc3c31c99596
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-06-14 15:18:08 -0500

    add project back in cli

--- src/prefect/cli/__init__.py
+++ src/prefect/cli/__init__.py
@@ -19,6 +19,7 @@ import prefect.cli.flow
 import prefect.cli.flow_run
 import prefect.cli.kubernetes
 import prefect.cli.profile
+import prefect.cli.project
 import prefect.cli.server
 import prefect.cli.variable
 import prefect.cli.work_pool
