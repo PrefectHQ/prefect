e65304b25a30d56bdc62fa455ca592756da5bf1c
jakekaplan <40362401+jakekaplan@users.noreply.github.com>
2023-06-14 11:07:09 -0400

    make more sqlite locking retryable (#9938)

--- src/prefect/server/api/server.py
+++ src/prefect/server/api/server.py
@@ -88,6 +88,8 @@ API_ROUTERS = (
     api.root.router,
 )
 
+SQLITE_LOCKED_MSG = "database is locked"
+
 
 class SPAStaticFiles(StaticFiles):
     """
@@ -158,7 +160,7 @@ def is_client_retryable_exception(exc: Exception):
         if getattr(exc.orig, "sqlite_errorname", None) in {
             "SQLITE_BUSY",
             "SQLITE_BUSY_SNAPSHOT",
-        }:
+        } or SQLITE_LOCKED_MSG in getattr(exc.orig, "args", []):
             return True
         else:
             # Avoid falling through to the generic `DBAPIError` case below
