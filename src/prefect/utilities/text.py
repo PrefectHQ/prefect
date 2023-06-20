d266801f5c54a15628092c77fd099f0ae104860b
Nathan Nowack <thrast36@gmail.com>
2023-06-20 19:33:04 +0100

    rm unneeded util

--- src/prefect/utilities/text.py
+++ src/prefect/utilities/text.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from uuid import UUID
 
 
 def truncated_to(length: int, value: Optional[str]) -> str:
@@ -18,11 +17,3 @@ def truncated_to(length: int, value: Optional[str]) -> str:
     proposed = f"{beginning}...{omitted} additional characters...{end}"
 
     return proposed if len(proposed) < len(value) else value
-
-
-def is_valid_uuid(uuid_str: str, version=4) -> bool:
-    try:
-        uuid_obj = UUID(uuid_str, version=version)
-    except ValueError:
-        return False
-    return str(uuid_obj) == uuid_str
