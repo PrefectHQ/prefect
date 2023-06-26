6f8992a80f05f3d977ff6cee46d548b4703d6c13
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-06-26 10:23:48 -0500

    Update src/prefect/cli/_prompts.py
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>

--- src/prefect/cli/_prompts.py
+++ src/prefect/cli/_prompts.py
@@ -412,7 +412,7 @@ async def prompt_push_custom_docker_image(
     push_step = {
         "requires": "prefect-docker",
         "image_name": "{{ build-image.image_name }}",
-        "tag": "{{ build-id.tag }}",
+        "tag": "{{ build-image.tag }}",
     }
 
     docker_credentials = {}
