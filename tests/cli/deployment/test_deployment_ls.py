b81f462d0b1d6eee633d4919eee5d8107e749ae8
rito-sixt <ritabrata.moitra@sixt.com>
2023-06-30 20:56:55 +0530

    Try to reproduce error via unit test, this test should fail but doesn't

--- tests/cli/deployment/test_deployment_ls.py
+++ tests/cli/deployment/test_deployment_ls.py
@@ -1,9 +1,15 @@
 import prefect
+from prefect.server import models
 from prefect.testing.cli import invoke_and_assert
 from prefect.utilities.asyncutils import run_sync_in_worker_thread
 
 
-async def test_ls_deployment_shows_deployments(
+# @pytest.fixture
+# async def delete_flow(prefect_client, session, deployment):
+#     await models.flows.delete_flow(session, deployment.flow_id)
+
+
+async def test_ls_deployment_shows_all_deployments(
     prefect_client: prefect.PrefectClient, deployment
 ):
     await run_sync_in_worker_thread(
@@ -16,5 +22,23 @@ async def test_ls_deployment_shows_deployments(
             "Deployments",
             str(deployment.name),
             str(deployment.id),
+        ],
+    )
+
+
+async def test_ls_deployment_crashes_if_flow_not_found(
+    prefect_client: prefect.PrefectClient, session, deployment
+):
+    await models.flows.delete_flow(session, deployment.flow_id)
+
+    await run_sync_in_worker_thread(
+        invoke_and_assert,
+        command=[
+            "deployment",
+            "ls",
+        ],
+        expected_code=0,
+        expected_output_contains=[
+            "Deployments",
         ],
     )
