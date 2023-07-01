fbd2b468ecf36b632b868df1fb99ebe4c898b903
teozul <teo.zulian@gmail.com>
2023-07-01 16:13:05 +0200

    consider custom run name at flow creation

--- src/prefect/engine.py
+++ src/prefect/engine.py
@@ -334,13 +334,16 @@ async def create_then_begin_flow_run(
             state = await exception_to_failed_state(
                 message="Validation of flow parameters failed with error:"
             )
-
+    flow_run_name = _resolve_custom_flow_run_name(
+        flow=flow, parameters=parameters
+    )
     flow_run = await client.create_flow_run(
         flow,
         # Send serialized parameters to the backend
         parameters=flow.serialize_parameters(parameters),
         state=state,
         tags=TagsContext.get().current_tags,
+        name=flow_run_name
     )
 
     engine_logger.info(f"Created flow run {flow_run.name!r} for flow {flow.name!r}")
