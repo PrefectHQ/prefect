6c88428cbe3ea578c7de0173171061f6e8c41f08
Jeff Hale <jeffmshale@gmail.com>
2023-07-05 16:33:50 -0400

    fix format

--- src/prefect/cli/_prompts.py
+++ src/prefect/cli/_prompts.py
@@ -379,9 +379,10 @@ async def prompt_build_custom_docker_image(
         else:
             if confirm(
                 "A Dockerfile exists. You chose not to use it. A temporary Dockerfile"
-                " will be automatically built during the deployment build step. If another file"
-                " named 'Dockerfile' already exists at that time, the build step will fail."
-                " Would you like to rename your existing Dockerfile?"
+                " will be automatically built during the deployment build step. If"
+                " another file named 'Dockerfile' already exists at that time, the"
+                " build step will fail. Would you like to rename your existing"
+                " Dockerfile?"
             ):
                 new_dockerfile_name = prompt(
                     "New Dockerfile name", default="Dockerfile.backup"
