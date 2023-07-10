1d2fe11463c13535014015b1dc065340bcc6955f
Ishankoradia <39583356+Ishankoradia@users.noreply.github.com>
2023-07-10 22:52:19 +0530

    Feature sendgrid notification (#10118)
    Co-authored-by: Chris Pickett <chris.pickett@prefect.io>

--- src/prefect/blocks/notifications.py
+++ src/prefect/blocks/notifications.py
@@ -617,3 +617,57 @@ class CustomWebhookNotificationBlock(NotificationBlock):
         client = httpx.AsyncClient(headers={"user-agent": "Prefect Notifications"})
         resp = await client.request(**self._build_request_args(body, subject))
         resp.raise_for_status()
+
+
+class SendgridEmail(AbstractAppriseNotificationBlock):
+    """
+    Enables sending notifications via any sendgrid account.
+    See [Apprise Notify_sendgrid docs](https://github.com/caronc/apprise/wiki/Notify_Sendgrid)
+
+    Examples:
+        Load a saved Sendgrid and send a email message:
+        ```python
+        from prefect.blocks.notifications import SendgridEmail
+
+        sendgrid_block = SendgridEmail.load("BLOCK_NAME")
+
+        sendgrid_block.notify("Hello from Prefect!")
+    """
+
+    _description = "Enables sending notifications via Sendgrid email service."
+    _block_type_name = "Sendgrid Email"
+    _block_type_slug = "sendgrid-email"
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/3PcxFuO9XUqs7wU9MiUBMg/af6affa646899cc1712d14b7fc4c0f1f/email__1_.png?h=250"
+    _documentation_url = "https://docs.prefect.io/api-ref/prefect/blocks/notifications/#prefect.blocks.notifications.SendgridEmail"
+
+    api_key: SecretStr = Field(
+        default=...,
+        title="API Key",
+        description="The API Key associated with your sendgrid account.",
+    )
+
+    sender_email: str = Field(
+        title="Sender email id",
+        description="The sender email id.",
+        example="test-support@gmail.com",
+    )
+
+    to_emails: List[str] = Field(
+        default=...,
+        title="Recipient emails",
+        description="Email ids of all recipients.",
+        example="recipient1@gmail.com",
+    )
+
+    def block_initialization(self) -> None:
+        from apprise.plugins.NotifySendGrid import NotifySendGrid
+
+        url = SecretStr(
+            NotifySendGrid(
+                apikey=self.api_key.get_secret_value(),
+                from_email=self.sender_email,
+                targets=self.to_emails,
+            ).url()
+        )
+
+        self._start_apprise_client(url)
--- tests/blocks/test_notifications.py
+++ tests/blocks/test_notifications.py
@@ -1,6 +1,7 @@
 from importlib import reload
 from typing import Type
 from unittest.mock import patch
+import urllib
 
 import cloudpickle
 import pytest
@@ -15,6 +16,7 @@ from prefect.blocks.notifications import (
     PREFECT_NOTIFY_TYPE_DEFAULT,
     TwilioSMS,
     CustomWebhookNotificationBlock,
+    SendgridEmail,
 )
 from prefect.testing.utilities import AsyncMock
 
@@ -562,3 +564,88 @@ class TestCustomWebhook:
                 json_data={"msg": "{{subject}}\n{{body}}", "token": "{{token}}"},
                 secrets={"token": "someSecretToken"},
             )
+
+
+class TestSendgridEmail:
+    URL_PARAMS = {
+        # default notify format
+        "format": "html",
+        # default overflow mode
+        "overflow": "upstream",
+        # socket read timeout
+        "rto": 4.0,
+        # socket connect timeout
+        "cto": 4.0,
+        # ssl certificate authority verification
+        "verify": "yes",
+    }
+
+    async def test_notify_async(self):
+        with patch("apprise.Apprise", autospec=True) as AppriseMock:
+            reload_modules()
+
+            apprise_instance_mock = AppriseMock.return_value
+            apprise_instance_mock.async_notify = AsyncMock()
+
+            sg_block = SendgridEmail(
+                api_key="test-api-key",
+                sender_email="test@gmail.com",
+                to_emails=["test1@gmail.com", "test2@gmail.com"],
+            )
+            await sg_block.notify("test")
+
+            # check if the apprise object is created
+            AppriseMock.assert_called_once()
+
+            # check if the Apprise().add function is called with correct url
+            url = f"sendgrid://{sg_block.api_key.get_secret_value()}:{sg_block.sender_email}/"
+            url += "/".join(
+                [urllib.parse.quote(email, safe="") for email in sg_block.to_emails]
+            )
+
+            url += "?"
+            url += urllib.parse.urlencode(TestSendgridEmail.URL_PARAMS)
+
+            apprise_instance_mock.add.assert_called_once_with(url)
+            apprise_instance_mock.async_notify.assert_awaited_once_with(
+                body="test", title=None, notify_type=PREFECT_NOTIFY_TYPE_DEFAULT
+            )
+
+    def test_notify_sync(self):
+        with patch("apprise.Apprise", autospec=True) as AppriseMock:
+            reload_modules()
+
+            apprise_instance_mock = AppriseMock.return_value
+            apprise_instance_mock.async_notify = AsyncMock()
+
+            sg_block = SendgridEmail(
+                api_key="test-api-key",
+                sender_email="test@gmail.com",
+                to_emails=["test1@gmail.com", "test2@gmail.com"],
+            )
+            sg_block.notify("test")
+
+            # check if the Apprise().add function is called with correct url
+            url = f"sendgrid://{sg_block.api_key.get_secret_value()}:{sg_block.sender_email}/"
+            url += "/".join(
+                [urllib.parse.quote(email, safe="") for email in sg_block.to_emails]
+            )
+            url += "?"
+            url += urllib.parse.urlencode(TestSendgridEmail.URL_PARAMS)
+
+            AppriseMock.assert_called_once()
+            apprise_instance_mock.add.assert_called_once_with(url)
+            apprise_instance_mock.async_notify.assert_called_once_with(
+                body="test", title=None, notify_type=PREFECT_NOTIFY_TYPE_DEFAULT
+            )
+
+    def test_is_picklable(self):
+        reload_modules()
+        block = SendgridEmail(
+            api_key="test-api-key",
+            sender_email="test@gmail.com",
+            to_emails=["test1@gmail.com", "test2@gmail.com"],
+        )
+        pickled = cloudpickle.dumps(block)
+        unpickled = cloudpickle.loads(pickled)
+        assert isinstance(unpickled, SendgridEmail)
