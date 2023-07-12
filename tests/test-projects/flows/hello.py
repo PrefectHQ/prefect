c845e9addb8d0ef47c28310b35b7bdb543c0faea
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-07-12 09:17:20 -0500

    Support deploying by name with multiple deployments with the same name during `prefect deploy` (#10189)
    Co-authored-by: Chris Pickett <chris.pickett@prefect.io>
    Co-authored-by: Bill Palombi <bill@prefect.io>

--- tests/test-projects/flows/hello.py
+++ tests/test-projects/flows/hello.py
@@ -4,3 +4,8 @@ from prefect import flow
 @flow(name="An important name")
 def my_flow(number: int, message: str = None):
     pass
+
+
+@flow(name="Second important name")
+def my_flow2(number: int, message: str = None):
+    pass
