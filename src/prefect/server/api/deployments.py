666fd023e02bb15b4ef054f91ecee73fae90dbcf
Chris White <chris@prefect.io>
2023-07-20 19:16:45 -0700

    Wire up work queue name to creating a flow run

--- src/prefect/server/api/deployments.py
+++ src/prefect/server/api/deployments.py
@@ -66,22 +66,22 @@ async def create_deployment(
         if deployment.work_pool_name and deployment.work_queue_name:
             # If a specific pool name/queue name combination was provided, get the
             # ID for that work pool queue.
-            deployment_dict[
-                "work_queue_id"
-            ] = await worker_lookups._get_work_queue_id_from_name(
-                session=session,
-                work_pool_name=deployment.work_pool_name,
-                work_queue_name=deployment.work_queue_name,
-                create_queue_if_not_found=True,
+            deployment_dict["work_queue_id"] = (
+                await worker_lookups._get_work_queue_id_from_name(
+                    session=session,
+                    work_pool_name=deployment.work_pool_name,
+                    work_queue_name=deployment.work_queue_name,
+                    create_queue_if_not_found=True,
+                )
             )
         elif deployment.work_pool_name:
             # If just a pool name was provided, get the ID for its default
             # work pool queue.
-            deployment_dict[
-                "work_queue_id"
-            ] = await worker_lookups._get_default_work_queue_id_from_work_pool_name(
-                session=session,
-                work_pool_name=deployment.work_pool_name,
+            deployment_dict["work_queue_id"] = (
+                await worker_lookups._get_default_work_queue_id_from_work_pool_name(
+                    session=session,
+                    work_pool_name=deployment.work_pool_name,
+                )
             )
         elif deployment.work_queue_name:
             # If just a queue name was provided, ensure that the queue exists and
@@ -412,7 +412,7 @@ async def create_flow_run_from_deployment(
             deployment.work_queue_id = (
                 await worker_lookups._get_work_queue_id_from_name(
                     session=session,
-                    work_pool_name=deployment.work_pool_name,
+                    work_pool_name=deployment.work_queue.work_pool.name,
                     work_queue_name=flow_run.work_queue_name,
                     create_queue_if_not_found=True,
                 )
@@ -426,6 +426,7 @@ async def create_flow_run_from_deployment(
                     "parameters",
                     "tags",
                     "infrastructure_document_id",
+                    "work_queue_name",
                 }
             ),
             flow_id=deployment.flow_id,
