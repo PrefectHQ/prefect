61d27d33283b7a57b1275871300f324e98d132e6
LennyArdiles <len@len-dev-null.net>
2023-07-24 13:15:31 -0400

    Fix line length formatting to align with standards

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -79,7 +79,8 @@ class TestParameterBindError:
         type_error = TypeError("Demo TypeError")
         expected_str = (
             "Error binding parameters for function 'fn': Demo TypeError.\nFunction 'fn'"
-            " has signature 'a: int, b: str' but received args: () and kwargs: ['c', 'd']."
+            " has signature 'a: int, b: str' but received args: () and kwargs: ['c',"
+            " 'd']."
         )
         pbe = ParameterBindError.from_bind_failure(
             fn, type_error, (), {"c": 3, "d": "test"}
