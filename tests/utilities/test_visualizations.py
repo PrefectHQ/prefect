bdea679e306aa716dda8764d5f3bdcb72061fe12
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-08-16 16:07:55 -0500

    add initial graph content test

--- tests/utilities/test_visualizations.py
+++ tests/utilities/test_visualizations.py
@@ -23,6 +23,11 @@ async def async_task_b(input_data):
     return f"Async Result B from {input_data}"
 
 
+@task(viz_return_value=5)
+def untrackable_task_result():
+    return "Untrackable Task Result"
+
+
 @flow
 def simple_sync_flow():
     a = sync_task_a()
@@ -61,9 +66,11 @@ def flow_with_task_interaction():
     sync_task_b(b)
 
 
-@task(viz_return_value=5)
-def untrackable_task_result():
-    return "Untrackable Task Result"
+@flow
+def flow_with_flow_params(x=1):
+    a = sync_task_a()
+    b = a + x
+    sync_task_b(b)
 
 
 @flow
@@ -82,6 +89,7 @@ def flow_with_untrackable_task_result():
         flow_with_task_interaction,
         flow_with_mixed_tasks,
         flow_with_untrackable_task_result,
+        flow_with_flow_params,
     ],
 )
 def test_visualize_does_not_raise(test_flow, monkeypatch):
@@ -90,3 +98,30 @@ def test_visualize_does_not_raise(test_flow, monkeypatch):
     )
 
     test_flow.visualize()
+
+
+@pytest.mark.parametrize(
+    "test_flow, expected_nodes",
+    [
+        (
+            simple_sync_flow,
+            {
+                '\t"sync_task_b-0"\n',
+                '\t"sync_task_a-0"\n',
+                '\t"sync_task_a-0" -> "sync_task_b-0"\n',
+            },
+        ),
+    ],
+)
+def test_visualize_graph_contents(test_flow, expected_nodes, monkeypatch):
+    mock_visualize = MagicMock(return_value=None)
+    monkeypatch.setattr("prefect.flows.visualize_task_dependencies", mock_visualize)
+
+    test_flow.visualize()
+    graph = mock_visualize.call_args[0][0]
+
+    actual_nodes = set(graph.body)
+
+    assert (
+        actual_nodes == expected_nodes
+    ), f"Expected nodes {expected_nodes} but found {actual_nodes}"
