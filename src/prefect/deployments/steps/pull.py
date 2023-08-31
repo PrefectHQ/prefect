aab8f6c5497272be221bc83037dcc8f8794570c0
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-08-31 08:48:32 -0500

    reformat for clarity

--- src/prefect/deployments/steps/pull.py
+++ src/prefect/deployments/steps/pull.py
@@ -29,7 +29,35 @@ def set_working_directory(directory: str) -> dict:
     return dict(directory=directory)
 
 
-def _format_token(netloc: str, access_token: str, credentials: dict) -> str:
+def _format_token_from_access_token(netloc: str, access_token: str) -> str:
+    if "bitbucketserver" in netloc:
+        if ":" not in access_token:
+            raise ValueError(
+                "Please prefix your BitBucket Server access_token with a username,"
+                " e.g. 'username:token'."
+            )
+        # If they pass a header themselves, we can use it as-is
+        return access_token
+
+    elif "bitbucket" in netloc:
+        return (
+            access_token
+            if (access_token.startswith("x-token-auth:") or ":" in access_token)
+            else f"x-token-auth:{access_token}"
+        )
+
+    elif "gitlab" in netloc:
+        return (
+            f"oauth2:{access_token}"
+            if not access_token.startswith("oauth2:")
+            else access_token
+        )
+
+    # all other cases (GitHub, etc.)
+    return access_token
+
+
+def _format_token_from_credentials(netloc: str, credentials: dict) -> str:
     """
     Formats the access token for the git provider.
 
@@ -41,61 +69,57 @@ def _format_token(netloc: str, access_token: str, credentials: dict) -> str:
     password = credentials.get("password") if credentials else None
     token = credentials.get("token") if credentials else None
 
-    user_provided_token = access_token or token or password
+    user_provided_token = token or password
+
+    if not user_provided_token:
+        raise ValueError(
+            "Please provide a `token` or `password` in your Credentials block to clone"
+            " a repo from a repo."
+        )
 
     if "bitbucketserver" in netloc:
-        # If they pass a header themselves, we can use it as-is
-        if access_token:
-            if ":" in access_token:
-                return access_token
-            else:
-                raise ValueError(
-                    "Please prefix your BitBucket Server access_token with a username,"
-                    " e.g. 'username:token'."
-                )
-
         # If they pass a BitBucketCredentials block and we don't have both a username and at
         # least one of a password or token and they don't provide a header themselves,
         # we can raise the appropriate error to avoid the wrong format for BitBucket Server.
-        if username and user_provided_token:
-            return (
-                f"{username}:{user_provided_token}"
-                if username not in user_provided_token
-                else user_provided_token
-            )
-        # If no username was provided but they did provide the username in the token or password
-        elif user_provided_token and ":" in user_provided_token:
-            return user_provided_token
-        else:
+        if not username and ":" not in user_provided_token:
             raise ValueError(
                 "Please provide a `username` and a `password` or `token` in your"
                 " BitBucketCredentials block to clone a repo from BitBucket Server."
             )
+        # if username or if no username but it's provided in the token
+        return (
+            f"{username}:{user_provided_token}"
+            if username and username not in user_provided_token
+            else user_provided_token
+        )
 
     elif "bitbucket" in netloc:
-        if user_provided_token:
-            contains_header = (
-                user_provided_token.startswith("x-token-auth:")
-                or ":" in user_provided_token
+        if not user_provided_token:
+            raise ValueError(
+                "Please provide a `token` or `password` in your BitBucketCredentials"
+                " block to clone a repo from BitBucket."
             )
-            if contains_header:
-                return user_provided_token
-            else:
-                return f"x-token-auth:{user_provided_token}"
+        return (
+            user_provided_token
+            if user_provided_token.startswith("x-token-auth:")
+            or ":" in user_provided_token
+            else f"x-token-auth:{user_provided_token}"
+        )
 
     elif "gitlab" in netloc:
-        if user_provided_token:
-            return (
-                f"oauth2:{user_provided_token}"
-                if not user_provided_token.startswith("oauth2:")
-                else user_provided_token
+        if not user_provided_token:
+            raise ValueError(
+                "Please provide a `token` in your GitLabCredentials block to clone a"
+                " repo from GitLab."
             )
+        return (
+            f"oauth2:{user_provided_token}"
+            if not user_provided_token.startswith("oauth2:")
+            else user_provided_token
+        )
 
     # all other cases (GitHub, etc.)
-    elif access_token:
-        return access_token
-    elif token:
-        return token
+    return user_provided_token
 
 
 def git_clone(
@@ -181,8 +205,14 @@ def git_clone(
 
     url_components = urllib.parse.urlparse(repository)
 
-    if access_token or credentials:
-        access_token = _format_token(url_components.netloc, access_token, credentials)
+    if access_token:
+        access_token = _format_token_from_access_token(
+            url_components.netloc, access_token
+        )
+    if credentials:
+        access_token = _format_token_from_credentials(
+            url_components.netloc, credentials
+        )
 
     if url_components.scheme == "https" and access_token is not None:
         updated_components = url_components._replace(
