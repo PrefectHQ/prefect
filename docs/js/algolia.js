68f58a5eaec4e35af7cbc67495c99b57d61ab42e
nicholas <znicholasbrown@gmail.com>
2023-10-05 15:59:09 -0400

    It's working sort of :)

--- docs/js/algolia.js
+++ docs/js/algolia.js
@@ -1,18 +1,57 @@
-const searchClient = algoliasearch('56FX6RAQWT', 'f0c51e95953d6290a9e6632e66244682');
+const { autocomplete, getAlgoliaResults } = window['@algolia/autocomplete-js'];
 
-const search = instantsearch({
-    indexName: 'demo_ecommerce',
-    searchClient,
+const searchClient = algoliasearch('56FX6RAQWT', 'a44396335bea82d8190edeb3f690b61b');
+
+function convertUrlToCurrentContextUrl(url) {
+    // Parse the input URL
+    const parsedUrl = new URL(url);
+
+    // Return the reconstructed URL with the current base
+    return `${parsedUrl.pathname.replace(/\/latest|latest/, '')}${parsedUrl.search}${parsedUrl.hash}`;
+}
+
+const itemTemplate = ({ item, components, html }) => {
+    const title = Object.values(item.hierarchy).filter((lvl) => lvl !== null).join(' > ')
+    console.log({ item })
+    return html`
+<a href="${convertUrlToCurrentContextUrl(item.url)}" class="md-nav__link">
+    ${title}
+</a>
+    `
+}
+
+const prefectioSource = ({ query }) => {
+    return {
+        sourceId: 'prefect_docs',
+        getItems() {
+            return getAlgoliaResults({
+                searchClient,
+                queries: [
+                    {
+                        indexName: 'prefectio',
+                        query,
+                        params: {
+                            hitsPerPage: 10,
+                        },
+                    },
+                ],
+            })
+        },
+        templates: {
+            item: itemTemplate
+        }
+    }
+}
+
+const algoliaSources = (res) => {
+    return [
+        prefectioSource(res)
+    ]
+}
+
+autocomplete({
+    placeholder: 'Search our docs',
+    container: '#autocomplete',
+    getSources: algoliaSources
 });
 
-search.addWidgets([
-    instantsearch.widgets.searchBox({
-        container: '#searchbox',
-    }),
-
-    instantsearch.widgets.hits({
-        container: '#hits',
-    })
-]);
-
-search.start();
