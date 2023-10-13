05661790b6ae77545f8af49095e3a2e68526f3ac
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-10-13 14:35:56 -0500

    check if wq exists first

--- src/prefect/server/schemas/responses.py
+++ src/prefect/server/schemas/responses.py
@@ -284,7 +284,8 @@ class DeploymentResponse(ORMBaseModel):
         if response.last_polled and response.last_polled > offline_horizon:
             response.status = schemas.statuses.DeploymentStatus.READY
         elif (
-            orm_deployment.work_queue.last_polled
+            orm_deployment.work_queue
+            and orm_deployment.work_queue.last_polled
             and orm_deployment.work_queue.last_polled > offline_horizon
         ):
             response.status = schemas.statuses.DeploymentStatus.NOT_READY
