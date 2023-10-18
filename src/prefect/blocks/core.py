896e5013237dd638613f13a3e95e9511684629d6
Alexander Streed <desertaxle@users.noreply.github.com>
2023-10-18 16:37:34 -0500

    Add ability to convert `GitRepository` into `git_clone` deployment step (#10957)

--- src/prefect/blocks/core.py
+++ src/prefect/blocks/core.py
@@ -182,6 +182,15 @@ def _should_update_block_type(
     return server_block_fields != local_block_fields
 
 
+class BlockNotSavedError(RuntimeError):
+    """
+    Raised when a given block is not saved and an operation that requires
+    the block to be saved is attempted.
+    """
+
+    pass
+
+
 @register_base_type
 @instrument_method_calls_on_class_instances
 class Block(BaseModel, ABC):
@@ -1050,3 +1059,25 @@ class Block(BaseModel, ABC):
             object.__setattr__(m, "__dict__", kwargs)
             object.__setattr__(m, "__fields_set__", set(kwargs.keys()))
             return m
+
+    def get_block_placeholder(self) -> str:
+        """
+        Returns the block placeholder for the current block which can be used for
+        templating.
+
+        Returns:
+            str: The block placeholder for the current block in the format
+                `prefect.blocks.{block_type_name}.{block_document_name}`
+
+        Raises:
+            BlockNotSavedError: Raised if the block has not been saved.
+
+        If a block has not been saved, the return value will be `None`.
+        """
+        block_document_name = self._block_document_name
+        if not block_document_name:
+            raise BlockNotSavedError(
+                "Could not generate block placeholder for unsaved block."
+            )
+
+        return f"prefect.blocks.{self.get_block_type_slug()}.{block_document_name}"
--- src/prefect/runner/storage.py
+++ src/prefect/runner/storage.py
@@ -1,10 +1,12 @@
 import subprocess
 from pathlib import Path
-from typing import Optional, Protocol, TypedDict, runtime_checkable
+from typing import Dict, Optional, Protocol, TypedDict, Union, runtime_checkable
 from urllib.parse import urlparse, urlunparse
 
 from anyio import run_process
 
+from prefect.blocks.core import Block
+from prefect.blocks.system import Secret
 from prefect.logging.loggers import get_logger
 
 
@@ -43,6 +45,13 @@ class RunnerStorage(Protocol):
         """
         ...
 
+    def to_pull_step(self) -> dict:
+        """
+        Returns a dictionary representation of the storage object that can be
+        used as a deployment pull step.
+        """
+        ...
+
     def __eq__(self, __value) -> bool:
         """
         Equality check for runner storage objects.
@@ -52,7 +61,7 @@ class RunnerStorage(Protocol):
 
 class GitCredentials(TypedDict, total=False):
     username: str
-    access_token: str
+    access_token: Union[str, Secret]
 
 
 class GitRepository:
@@ -89,24 +98,30 @@ class GitRepository:
     def __init__(
         self,
         url: str,
-        credentials: Optional[GitCredentials] = None,
+        credentials: Union[GitCredentials, Block, None] = None,
         name: Optional[str] = None,
-        branch: str = "main",
+        branch: Optional[str] = None,
+        include_submodules: bool = False,
         pull_interval: Optional[int] = 60,
     ):
         if credentials is None:
             credentials = {}
 
-        if credentials.get("username") and not credentials.get("access_token"):
+        if (
+            isinstance(credentials, dict)
+            and credentials.get("username")
+            and not credentials.get("access_token")
+        ):
             raise ValueError(
                 "If a username is provided, an access token must also be provided."
             )
         self._url = url
         self._branch = branch
-        self._username = credentials.get("username")
-        self._access_token = credentials.get("access_token")
+        self._credentials = credentials
+        self._include_submodules = include_submodules
         repo_name = urlparse(url).path.split("/")[-1].replace(".git", "")
-        self._name = name or f"{repo_name}-{branch}"
+        default_name = f"{repo_name}-{branch}" if branch else repo_name
+        self._name = name or default_name
         self._logger = get_logger(f"runner.storage.git-repository.{self._name}")
         self._storage_base_path = Path.cwd()
         self._pull_interval = pull_interval
@@ -122,12 +137,40 @@ class GitRepository:
     def pull_interval(self) -> Optional[int]:
         return self._pull_interval
 
+    @property
+    def _repository_url_with_credentials(self) -> str:
+        if not self._credentials:
+            return self._url
+
+        url_components = urlparse(self._url)
+
+        credentials = (
+            self._credentials.dict()
+            if isinstance(self._credentials, Block)
+            else self._credentials
+        )
+        if isinstance(credentials.get("access_token"), Secret):
+            credentials["access_token"] = credentials["access_token"].get()
+
+        formatted_credentials = _format_token_from_credentials(
+            urlparse(self._url).netloc, credentials
+        )
+        if url_components.scheme == "https" and formatted_credentials is not None:
+            updated_components = url_components._replace(
+                netloc=f"{formatted_credentials}@{url_components.netloc}"
+            )
+            repository_url = urlunparse(updated_components)
+        else:
+            repository_url = self._url
+
+        return repository_url
+
     async def pull_code(self):
         """
         Pulls the contents of the configured repository to the local filesystem.
         """
         self._logger.debug(
-            "Pull contents from repository '%s' to '%s'...",
+            "Pulling contents from repository '%s' to '%s'...",
             self._name,
             self.destination,
         )
@@ -142,21 +185,7 @@ class GitRepository:
             )
             existing_repo_url = None
             if result.stdout is not None:
-                existing_repo_url = result.stdout.decode().strip()
-                existing_repo_url_parts = urlparse(existing_repo_url)
-                if self._access_token and not self._username:
-                    existing_repo_url_parts = existing_repo_url_parts._replace(
-                        netloc=existing_repo_url_parts.netloc.replace(
-                            f"{self._access_token}@", ""
-                        )
-                    )
-                if self._username and self._access_token:
-                    existing_repo_url_parts = existing_repo_url_parts._replace(
-                        netloc=existing_repo_url_parts.netloc.replace(
-                            f"{self._username}:{self._access_token}@", ""
-                        )
-                    )
-                existing_repo_url = urlunparse(existing_repo_url_parts)
+                existing_repo_url = _strip_auth_from_url(result.stdout.decode().strip())
 
             if existing_repo_url != self._url:
                 raise ValueError(
@@ -166,44 +195,37 @@ class GitRepository:
 
             self._logger.debug("Pulling latest changes from origin/%s", self._branch)
             # Update the existing repository
-            await run_process(
-                ["git", "pull", "origin", self._branch],
-                cwd=self.destination,
-            )
+            cmd = ["git", "pull", "origin"]
+            if self._branch:
+                cmd += [self._branch]
+            if self._include_submodules:
+                cmd += ["--recurse-submodules"]
+            cmd += ["--depth", "1"]
+            await run_process(cmd, cwd=self.destination)
         else:
             self._logger.debug("Cloning repository %s", self._url)
             # Clone the repository if it doesn't exist at the destination
 
-            repo_url_parts = urlparse(self._url)
-            if self._access_token and not self._username:
-                updated_components = repo_url_parts._replace(
-                    netloc=f"{self._access_token}@{repo_url_parts.netloc}"
-                )
-                repository_url = urlunparse(updated_components)
-            elif self._username and self._access_token:
-                updated_components = repo_url_parts._replace(
-                    netloc=(
-                        f"{self._username}:{self._access_token}@{repo_url_parts.netloc}"
-                    )
-                )
-                repository_url = urlunparse(updated_components)
-            else:
-                repository_url = self._url
+            repository_url = self._repository_url_with_credentials
+
+            cmd = [
+                "git",
+                "clone",
+                repository_url,
+            ]
+            if self._branch:
+                cmd += ["--branch", self._branch]
+            if self._include_submodules:
+                cmd += ["--recurse-submodules"]
+
+            # Limit git history and set path to clone to
+            cmd += ["--depth", "1", str(self.destination)]
 
             try:
-                await run_process(
-                    [
-                        "git",
-                        "clone",
-                        "--branch",
-                        self._branch,
-                        repository_url,
-                        str(self.destination),
-                    ]
-                )
+                await run_process(cmd)
             except subprocess.CalledProcessError as exc:
                 # Hide the command used to avoid leaking the access token
-                exc_chain = None if self._access_token else exc
+                exc_chain = None if self._credentials else exc
                 raise RuntimeError(
                     f"Failed to clone repository {self._url!r} with exit code"
                     f" {exc.returncode}."
@@ -224,6 +246,34 @@ class GitRepository:
             f" branch={self._branch!r})"
         )
 
+    def to_pull_step(self) -> Dict:
+        pull_step = {
+            "prefect.deployments.steps.git_clone": {
+                "repository": self._url,
+                "branch": self._branch,
+            }
+        }
+        if isinstance(self._credentials, Block):
+            pull_step["prefect.deployments.steps.git_clone"][
+                "credentials"
+            ] = f"{{{{ {self._credentials.get_block_placeholder()} }}}}"
+        elif isinstance(self._credentials, dict):
+            if isinstance(self._credentials.get("access_token"), Secret):
+                pull_step["prefect.deployments.steps.git_clone"]["credentials"] = {
+                    **self._credentials,
+                    "access_token": (
+                        "{{"
+                        f" {self._credentials['access_token'].get_block_placeholder()} }}}}"
+                    ),
+                }
+            elif self._credentials.get("access_token") is not None:
+                raise ValueError(
+                    "Please save your access token as a Secret block before converting"
+                    " this storage object to a pull step."
+                )
+
+        return pull_step
+
 
 def create_storage_from_url(
     url: str, pull_interval: Optional[int] = 60
@@ -243,3 +293,83 @@ def create_storage_from_url(
     if parsed_url.scheme == "git" or parsed_url.path.endswith(".git"):
         return GitRepository(url=url, pull_interval=pull_interval)
     raise ValueError(f"Unsupported storage URL: {url}. Only git URLs are supported.")
+
+
+def _format_token_from_credentials(netloc: str, credentials: dict) -> str:
+    """
+    Formats the credentials block for the git provider.
+
+    BitBucket supports the following syntax:
+        git clone "https://x-token-auth:{token}@bitbucket.org/yourRepoOwnerHere/RepoNameHere"
+        git clone https://username:<token>@bitbucketserver.com/scm/projectname/teamsinspace.git
+    """
+    username = credentials.get("username") if credentials else None
+    password = credentials.get("password") if credentials else None
+    token = credentials.get("token") if credentials else None
+    access_token = credentials.get("access_token") if credentials else None
+
+    user_provided_token = access_token or token or password
+
+    if not user_provided_token:
+        raise ValueError(
+            "Please provide a `token` or `password` in your Credentials block to clone"
+            " a repo."
+        )
+
+    if username:
+        return f"{username}:{user_provided_token}"
+
+    if "bitbucketserver" in netloc:
+        # If they pass a BitBucketCredentials block and we don't have both a username and at
+        # least one of a password or token and they don't provide a header themselves,
+        # we can raise the appropriate error to avoid the wrong format for BitBucket Server.
+        if not username and ":" not in user_provided_token:
+            raise ValueError(
+                "Please provide a `username` and a `password` or `token` in your"
+                " BitBucketCredentials block to clone a repo from BitBucket Server."
+            )
+        # if username or if no username but it's provided in the token
+        return (
+            f"{username}:{user_provided_token}"
+            if username and username not in user_provided_token
+            else user_provided_token
+        )
+
+    elif "bitbucket" in netloc:
+        return (
+            user_provided_token
+            if user_provided_token.startswith("x-token-auth:")
+            or ":" in user_provided_token
+            else f"x-token-auth:{user_provided_token}"
+        )
+
+    elif "gitlab" in netloc:
+        return (
+            f"oauth2:{user_provided_token}"
+            if not user_provided_token.startswith("oauth2:")
+            else user_provided_token
+        )
+
+    # all other cases (GitHub, etc.)
+    return user_provided_token
+
+
+def _strip_auth_from_url(url: str) -> str:
+    parsed = urlparse(url)
+
+    # Construct a new netloc without the auth info
+    netloc = parsed.hostname
+    if parsed.port:
+        netloc += f":{parsed.port}"
+
+    # Build the sanitized URL
+    return urlunparse(
+        (
+            parsed.scheme,
+            netloc,
+            parsed.path,
+            parsed.params,
+            parsed.query,
+            parsed.fragment,
+        )
+    )
