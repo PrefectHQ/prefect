a7e869ecc21db7f06f5f8ca0cd43c19042e3d645
Jeff Hale <discdiver@users.noreply.github.com>
2023-10-19 09:14:06 -0400

    Clarify CLI prompt message for missing integration library for worker (#10990)

--- tests/cli/test_worker.py
+++ tests/cli/test_worker.py
@@ -503,10 +503,9 @@ class TestInstallPolicyOption:
             ],
             user_input=readchar.key.ENTER,
             expected_output_contains=[
-                (
-                    "Could not find a kubernetes worker in the current"
-                    " environment. Install it now?"
-                ),
+                "Could not find the Prefect integration library for the",
+                "kubernetes",
+                "Install the library now?",
                 "Installing prefect-kubernetes...",
                 "Worker 'test-worker' started!",
                 "Worker 'test-worker' stopped!",
