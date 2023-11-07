7ad2e2022758d709e6fb2742a456c2fdbf90dac7
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-11-07 16:43:05 -0600

    Allow `image` passed into `deploy` to be optional if loading flow from storage (#11117)
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>

--- tests/runner/test_runner.py
+++ tests/runner/test_runner.py
@@ -1273,6 +1273,61 @@ class TestDeploy:
             pull=True,
         )
 
+    async def test_deploy_without_image_with_flow_stored_remotely(
+        self,
+        work_pool_with_image_variable,
+    ):
+        deployment_id = await deploy(
+            await (
+                await flow.from_source(
+                    source=MockStorage(), entrypoint="flows.py:test_flow"
+                )
+            ).to_deployment(__file__),
+            work_pool_name=work_pool_with_image_variable.name,
+        )
+
+        assert len(deployment_id) == 1
+
+    async def test_deploy_without_image_or_flow_storage_raises(
+        self,
+        work_pool_with_image_variable,
+    ):
+        with pytest.raises(ValueError):
+            await deploy(
+                await dummy_flow_1.to_deployment(__file__),
+                work_pool_name=work_pool_with_image_variable.name,
+            )
+
+    async def test_deploy_with_image_and_flow_stored_remotely_raises(
+        self,
+        work_pool_with_image_variable,
+    ):
+        with pytest.raises(RuntimeError, match="Failed to generate Dockerfile"):
+            await deploy(
+                await (
+                    await flow.from_source(
+                        source=MockStorage(), entrypoint="flows.py:test_flow"
+                    )
+                ).to_deployment(__file__),
+                work_pool_name=work_pool_with_image_variable.name,
+                image="test-registry/test-image:test-tag",
+            )
+
+    async def test_deploy_multiple_flows_one_using_storage_one_without_raises_with_no_image(
+        self,
+        work_pool_with_image_variable,
+    ):
+        with pytest.raises(ValueError):
+            await deploy(
+                await dummy_flow_1.to_deployment(__file__),
+                await (
+                    await flow.from_source(
+                        source=MockStorage(), entrypoint="flows.py:test_flow"
+                    )
+                ).to_deployment(__file__),
+                work_pool_name=work_pool_with_image_variable.name,
+            )
+
     async def test_deploy_with_image_string_no_tag(
         self,
         mock_build_image: MagicMock,
