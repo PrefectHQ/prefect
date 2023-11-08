45d8871c708928ad3bb1af0c75fa07618cd5168b
Chris White <chris@prefect.io>
2023-11-08 10:42:22 -0800

    Add env var lookup for task runs

--- src/prefect/cli/task_run.py
+++ src/prefect/cli/task_run.py
@@ -14,6 +14,14 @@ app.add_typer(task_run_app, aliases=["task-runs"])
 
 @task_run_app.command()
 async def execute(
-    id: Optional[UUID] = typer.Argument(None, help="ID of the flow run to execute")
+    id: Optional[UUID] = typer.Argument(None, help="ID of the task run to execute")
 ):
-    app.console.print("One task execution coming right up!")
+    if id is None:
+        environ_task_id = os.environ.get("PREFECT__TASK_RUN_ID")
+        if environ_task_id:
+            id = UUID(environ_task_id)
+
+    if id is None:
+        exit_with_error("Cloud not determine the ID of the task run to execute.")
+
+    # await Runner().execute_flow_run(id)
