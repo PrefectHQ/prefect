bf9857389287d15988ac509ddb75dcf77fe29df6
Alexander Streed <alex.s@prefect.io>
2023-11-15 13:09:30 -0600

    Adds tests and additonal output to work pool create command

--- src/prefect/cli/deploy.py
+++ src/prefect/cli/deploy.py
@@ -31,6 +31,7 @@ from prefect.cli._prompts import (
     prompt_select_remote_flow_storage,
     prompt_select_work_pool,
 )
+from prefect.cli._types import SettingsOption
 from prefect.cli._utilities import (
     exit_with_error,
 )
@@ -118,8 +119,8 @@ async def deploy(
             " --work-queue flag."
         ),
     ),
-    work_pool_name: str = typer.Option(
-        None,
+    work_pool_name: str = SettingsOption(
+        PREFECT_DEFAULT_WORK_POOL_NAME,
         "-p",
         "--pool",
         help="The work pool that will handle this deployment's runs.",
@@ -439,10 +440,7 @@ async def _run_single_deploy(
     deploy_config["schedule"] = _construct_schedule(deploy_config, ci=ci)
 
     # determine work pool
-    work_pool_name = (
-        get_from_dict(deploy_config, "work_pool.name")
-        or PREFECT_DEFAULT_WORK_POOL_NAME.value()
-    )
+    work_pool_name = get_from_dict(deploy_config, "work_pool.name")
     if work_pool_name:
         try:
             work_pool = await client.read_work_pool(deploy_config["work_pool"]["name"])
