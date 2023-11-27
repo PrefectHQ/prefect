5d095badbca0b9cca3af675e7bac5543c53bc783
Alexander Streed <desertaxle@users.noreply.github.com>
2023-11-27 10:03:04 -0600

    Use new route to read work pool types when connected to Prefect Cloud (#11236)

--- tests/client/test_cloud_client.py
+++ tests/client/test_cloud_client.py
@@ -1,5 +1,48 @@
+import uuid
+
+import httpx
+import pytest
+import respx
+from respx.patterns import M
+
 from prefect.client.cloud import get_cloud_client
-from prefect.settings import PREFECT_UNIT_TEST_MODE, temporary_settings
+from prefect.settings import PREFECT_API_URL, PREFECT_UNIT_TEST_MODE, temporary_settings
+
+mock_work_pool_types_response = {
+    "prefect": {
+        "prefect-agent": {
+            "type": "prefect-agent",
+            "default_base_job_configuration": {},
+        }
+    },
+    "prefect-kubernetes": {
+        "kubernetes": {
+            "type": "kubernetes",
+            "default_base_job_configuration": {},
+        }
+    },
+}
+
+
+@pytest.fixture
+async def mock_work_pool_types():
+    with respx.mock(
+        assert_all_mocked=False, base_url=PREFECT_API_URL.value()
+    ) as respx_mock:
+        respx_mock.route(
+            M(
+                path__regex=(
+                    r"accounts/(.{36})/workspaces/(.{36})/collections/work_pool_types"
+                )
+            ),
+            method="GET",
+        ).mock(
+            return_value=httpx.Response(
+                200,
+                json=mock_work_pool_types_response,
+            )
+        )
+        yield
 
 
 async def test_cloud_client_follow_redirects():
@@ -19,3 +62,16 @@ async def test_cloud_client_follow_redirects():
     # do not follow redirects by default during unit tests
     async with get_cloud_client() as client:
         assert client._client.follow_redirects is False
+
+
+async def test_get_cloud_work_pool_types(mock_work_pool_types):
+    account_id = uuid.uuid4()
+    workspace_id = uuid.uuid4()
+    with temporary_settings(
+        updates={
+            PREFECT_API_URL: f"https://api.prefect.cloud/api/accounts/{account_id}/workspaces/{workspace_id}/"
+        }
+    ):
+        async with get_cloud_client() as client:
+            response = await client.read_worker_metadata()
+            assert response == mock_work_pool_types_response
