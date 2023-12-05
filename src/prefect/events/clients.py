d7510eacaa808717c39b08568298874696892602
Chris Guidry <chris.g@prefect.io>
2023-12-05 15:26:37 -0500

    A Prefect Cloud event stream subscriber
    This subscriber client is based on the [example gist][1] I'd shared with a few
    folks, but with the additional reliability affordances we have in the official
    inbound event client.
    
    ```
    from prefect.events.clients import PrefectCloudEventSubscriber
    from prefect.events.filters import EventFilter, EventNameFilter
    
    filter = EventFilter(event=EventNameFilter(prefix=["prefect.flow-run."]))
    
    async with PrefectCloudEventSubscriber(filter=filter) as subscriber:
        async for event in subscriber:
            print(event.occurred, event.resource.id, event.event)
    ```
    
    [1]: https://gist.github.com/chrisguidry/1ee49e66ee426033f8b232c637ec2588

--- src/prefect/events/clients.py
+++ src/prefect/events/clients.py
@@ -1,12 +1,42 @@
 import abc
 import asyncio
 from types import TracebackType
-from typing import Any, ClassVar, Dict, List, Optional, Tuple, Type
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    ClassVar,
+    Dict,
+    List,
+    Mapping,
+    Optional,
+    Tuple,
+    Type,
+)
+from uuid import UUID
 
+import orjson
+import pendulum
+
+try:
+    from cachetools import TTLCache
+except ImportError:
+    pass
+from starlette.status import WS_1008_POLICY_VIOLATION
 from websockets.client import WebSocketClientProtocol, connect
-from websockets.exceptions import ConnectionClosed
+from websockets.exceptions import (
+    ConnectionClosed,
+    ConnectionClosedError,
+    ConnectionClosedOK,
+)
 
 from prefect.events import Event
+from prefect.logging import get_logger
+from prefect.settings import PREFECT_API_KEY, PREFECT_API_URL
+
+if TYPE_CHECKING:
+    from prefect.events.filters import EventFilter
+
+logger = get_logger(__name__)
 
 
 class EventsClient(abc.ABC):
@@ -87,8 +117,8 @@ class PrefectCloudEventsClient(EventsClient):
 
     def __init__(
         self,
-        api_url: str,
-        api_key: str,
+        api_url: str = None,
+        api_key: str = None,
         reconnection_attempts: int = 10,
         checkpoint_every: int = 20,
     ):
@@ -101,6 +131,9 @@ class PrefectCloudEventsClient(EventsClient):
             checkpoint_every: How often the client should sync with the server to
                 confirm receipt of all previously sent events
         """
+        api_url = api_url or PREFECT_API_URL.value()
+        api_key = api_key or PREFECT_API_KEY.value()
+
         socket_url = (
             api_url.replace("https://", "wss://")
             .replace("http://", "ws://")
@@ -186,6 +219,177 @@ class PrefectCloudEventsClient(EventsClient):
 
                 return
             except ConnectionClosed:
+                if i == self._reconnection_attempts:
+                    # this was our final chance, raise the most recent error
+                    raise
+
+                if i > 2:
+                    # let the first two attempts happen quickly in case this is just
+                    # a standard load balancer timeout, but after that, just take a
+                    # beat to let things come back around.
+                    await asyncio.sleep(1)
+
+
+class PrefectCloudEventSubscriber:
+    """
+    Subscribes to a Prefect Cloud event stream, yielding events as they occur.
+
+    Example:
+
+        from prefect.events.clients import PrefectCloudEventSubscriber
+        from prefect.events.filters import EventFilter, EventNameFilter
+
+        filter = EventFilter(event=EventNameFilter(prefix=["prefect.flow-run."]))
+
+        async with PrefectCloudEventSubscriber(api_url, api_key, filter) as subscriber:
+            async for event in subscriber:
+                print(event.occurred, event.resource.id, event.event)
+
+    """
+
+    _websocket: Optional[WebSocketClientProtocol]
+    _filter: "EventFilter"
+    _seen_events: "Mapping[UUID, bool]"
+
+    def __init__(
+        self,
+        api_url: str = None,
+        api_key: str = None,
+        filter: "EventFilter" = None,
+        reconnection_attempts: int = 10,
+    ):
+        """
+        Args:
+            api_url: The base URL for a Prefect Cloud workspace
+            api_key: The API of an actor with the manage_events scope
+            reconnection_attempts: When the client is disconnected, how many times
+                the client should attempt to reconnect
+        """
+        api_url = api_url or PREFECT_API_URL.value()
+        api_key = api_key or PREFECT_API_KEY.value()
+
+        from prefect.events.filters import EventFilter
+
+        self._filter = filter or EventFilter()
+
+        try:
+            self._seen_events = TTLCache(maxsize=500_000, ttl=120)
+        except NameError:
+            logger.warn(
+                "cachetools is not installed, so event deduplication may "
+                "require significant memory.  For long-running subscribers, install "
+                "the cachetools package."
+            )
+            self._seen_events = {}
+
+        socket_url = (
+            api_url.replace("https://", "wss://")
+            .replace("http://", "ws://")
+            .rstrip("/")
+        ) + "/events/out"
+
+        logger.debug("Connecting to %s", socket_url)
+
+        self._api_key = api_key
+        self._connect = connect(
+            socket_url,
+            subprotocols=["prefect"],
+        )
+        self._websocket = None
+        self._reconnection_attempts = reconnection_attempts
+
+    async def __aenter__(self) -> "PrefectCloudEventSubscriber":
+        # Don't handle any errors in the initial connection, because these are most
+        # likely a permission or configuration issue that should propagate
+        await self._reconnect()
+        return self
+
+    async def _reconnect(self) -> None:
+        logger.debug("Reconnecting...")
+        if self._websocket:
+            self._websocket = None
+            await self._connect.__aexit__(None, None, None)
+
+        self._websocket = await self._connect.__aenter__()
+
+        # make sure we have actually connected
+        logger.debug("  pinging...")
+        pong = await self._websocket.ping()
+        await pong
+
+        logger.debug("  authenticating...")
+        await self._websocket.send(
+            orjson.dumps({"type": "auth", "token": self._api_key}).decode()
+        )
+
+        try:
+            message = orjson.loads(await self._websocket.recv())
+            logger.debug("  auth result %s", message)
+            assert message["type"] == "auth_success"
+        except (AssertionError, ConnectionClosedError) as e:
+            if isinstance(e, AssertionError) or e.code == WS_1008_POLICY_VIOLATION:
+                raise Exception(
+                    "Unable to authenticate to the event stream. Please ensure the "
+                    "provided api_key you are using is valid for this environment."
+                ) from e
+            raise
+
+        from prefect.events.filters import EventOccurredFilter
+
+        self._filter.occurred = EventOccurredFilter(
+            since=pendulum.now("UTC").subtract(minutes=1),
+            until=pendulum.now("UTC").add(years=1),
+        )
+
+        logger.debug("  filtering events since %s...", self._filter.occurred.since)
+        filter_message = {
+            "type": "filter",
+            "filter": self._filter.dict(json_compatible=True),
+        }
+        await self._websocket.send(orjson.dumps(filter_message).decode())
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[Exception]],
+        exc_val: Optional[Exception],
+        exc_tb: Optional[TracebackType],
+    ) -> None:
+        self._websocket = None
+        await self._connect.__aexit__(exc_type, exc_val, exc_tb)
+
+    def __aiter__(self) -> "PrefectCloudEventSubscriber":
+        return self
+
+    async def __anext__(self) -> Event:
+        for i in range(self._reconnection_attempts + 1):
+            try:
+                # If we're here and the websocket is None, then we've had a failure in a
+                # previous reconnection attempt.
+                #
+                # Otherwise, after the first time through this loop, we're recovering
+                # from a ConnectionClosed, so reconnect now.
+                if not self._websocket or i > 0:
+                    await self._reconnect()
+                    assert self._websocket
+
+                while True:
+                    message = orjson.loads(await self._websocket.recv())
+                    event = Event.parse_obj(message["event"])
+
+                    if event.id in self._seen_events:
+                        continue
+                    self._seen_events[event.id] = True
+
+                    return event
+            except ConnectionClosedOK:
+                logger.debug('Connection closed with "OK" status')
+                raise StopAsyncIteration
+            except ConnectionClosed:
+                logger.debug(
+                    "Connection closed with %s/%s attempts",
+                    i + 1,
+                    self._reconnection_attempts,
+                )
                 if i == self._reconnection_attempts:
                     # this was our final chance, raise the most recent error
                     raise
