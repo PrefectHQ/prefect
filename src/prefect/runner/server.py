ef0855076d382778f33c26dd5d31a32748d31707
Uriel Mandujano <uriel@prefect.io>
2023-12-05 15:07:43 -0600

    ft: Add deployments endpoints on runners
    Co-authored-by: nate nowack <nate@prefect.io>

--- src/prefect/runner/server.py
+++ src/prefect/runner/server.py
@@ -1,8 +1,15 @@
+import inspect
+import typing as t
+
 import pendulum
 import uvicorn
 from prefect._vendor.fastapi import APIRouter, FastAPI, status
 from prefect._vendor.fastapi.responses import JSONResponse
+from pydantic import BaseModel, create_model
 
+from prefect._internal.pydantic import HAS_PYDANTIC_V2
+from prefect.client.orchestration import get_client
+from prefect.flows import load_flow_from_entrypoint
 from prefect.settings import (
     PREFECT_RUNNER_POLL_FREQUENCY,
     PREFECT_RUNNER_SERVER_HOST,
@@ -10,6 +17,21 @@ from prefect.settings import (
     PREFECT_RUNNER_SERVER_MISSED_POLLS_TOLERANCE,
     PREFECT_RUNNER_SERVER_PORT,
 )
+from prefect.utilities.asyncutils import sync_compatible
+
+if t.TYPE_CHECKING:
+    from prefect.deployments import Deployment
+    from prefect.runner import Runner
+
+
+if HAS_PYDANTIC_V2:
+    from prefect._internal.pydantic.v2_schema import (
+        create_v2_schema,
+        has_v2_model_as_param,
+        process_v2_params,
+    )
+else:
+    from prefect.utilities.callables import create_v1_schema, process_v1_params
 
 
 def perform_health_check(runner, delay_threshold: int = None) -> JSONResponse:
@@ -49,8 +71,74 @@ def shutdown(runner) -> int:
     return _shutdown
 
 
+def _model_for_function(fn: t.Callable) -> t.Type[BaseModel]:
+    signature = inspect.signature(fn)
+    model_fields = {}
+    docstrings = fn.__doc__ or {}
+    aliases = {}
+
+    class ModelConfig:
+        arbitrary_types_allowed = True
+
+    if HAS_PYDANTIC_V2 and has_v2_model_as_param(signature):
+        create_schema = create_v2_schema
+        process_params = process_v2_params
+    else:
+        create_schema = create_v1_schema
+        process_params = process_v1_params
+
+    for position, param in enumerate(signature.parameters.values()):
+        name, type_, field = process_params(
+            param, position=position, docstrings=docstrings, aliases=aliases
+        )
+        # Generate a Pydantic model at each step so we can check if this parameter
+        # type supports schema generation
+        try:
+            create_schema(
+                "CheckParameter", model_cfg=ModelConfig, **{name: (type_, field)}
+            )
+        except ValueError:
+            # This field's type is not valid for schema creation, update it to `Any`
+            type_ = t.Any
+        model_fields[name] = (type_, field)
+    return create_model("FunctionModel", __config__=ModelConfig, **model_fields)
+
+
+async def __run_deployment(deployment: "Deployment"):
+    assert deployment.entrypoint is not None
+    _flow = load_flow_from_entrypoint(deployment.entrypoint)
+
+    Model: t.Type[BaseModel] = _model_for_function(_flow.fn)
+
+    async def _create_flow_run_for_deployment(m: Model):  # type: ignore
+        async with get_client() as client:
+            await client.create_flow_run_from_deployment(
+                deployment_id=deployment.id,
+                parameters=m.dict(),
+            )
+
+    return _create_flow_run_for_deployment
+
+
+@sync_compatible
+async def get_deployment_router(runner: "Runner") -> APIRouter:
+    from prefect import get_client
+
+    router = APIRouter()
+
+    async with get_client() as client:
+        for deployment_id in runner._deployment_ids:
+            deployment = await client.read_deployment(deployment_id)
+            router.add_api_route(
+                f"/deployments/{deployment.id}/runs",
+                await __run_deployment(deployment),
+                methods=["POST"],
+            )
+    return router
+
+
 def start_webserver(
-    runner,
+    runner: "Runner",
     log_level: str = None,
 ) -> None:
     """
@@ -68,8 +156,10 @@ def start_webserver(
     )
     router.add_api_route("/run_count", run_count(runner=runner), methods=["GET"])
     router.add_api_route("/shutdown", shutdown(runner=runner), methods=["POST"])
-
     webserver.include_router(router)
+
+    deployments_router = get_deployment_router(runner)
+    webserver.include_router(deployments_router)
 
     host = PREFECT_RUNNER_SERVER_HOST.value()
     port = PREFECT_RUNNER_SERVER_PORT.value()
