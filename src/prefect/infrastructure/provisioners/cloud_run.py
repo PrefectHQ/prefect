afc5cd1ad93d4f114b133b0142e7cf183d819241
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2023-12-13 09:01:07 -0600

    remove ?

--- src/prefect/infrastructure/provisioners/cloud_run.py
+++ src/prefect/infrastructure/provisioners/cloud_run.py
@@ -265,7 +265,7 @@ class CloudRunPushProvisioner:
                             " resource names"
                         )
                     },
-                    {"option": "Customize resource names?"},
+                    {"option": "Customize resource names"},
                     {"option": "Do not provision infrastructure"},
                 ],
             )
