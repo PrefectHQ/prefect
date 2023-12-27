5274c9cd825fcad194770bca79fe1208660b5c07
David Zucker <davzucky@hotmail.com>
2023-12-27 17:12:24 +0900

    Add error log when cannot instanciate the service

--- src/prefect/server/services/dynamic_service_loader.py
+++ src/prefect/server/services/dynamic_service_loader.py
@@ -3,12 +3,15 @@ The dynamic service loader.
 """
 
 import importlib
-from typing import List
+from typing import List, Optional
 
+from prefect.logging import get_logger
 from prefect.server.services.loop_service import LoopService
 
+logger = get_logger("server")
 
-def string_to_loop_service(service_path: str) -> LoopService:
+
+def string_to_loop_service(service_path: str) -> Optional[LoopService]:
     """
     Given a string representing a LoopService subclass, import the class and return an
     instance of it.
@@ -20,13 +23,19 @@ def string_to_loop_service(service_path: str) -> LoopService:
         - LoopService: an instance of the LoopService subclass
     """
     module_name, class_name = service_path.strip(" ").rsplit(".", 1)
-    module = importlib.import_module(module_name)
-    return getattr(module, class_name)()
+    try:
+        module = importlib.import_module(module_name)
+        instance = getattr(module, class_name)()
+        return instance
+    except:  # noqa: E722
+        logger.error(f"Failed to initialize the instance of the service {service_path}")
+        return None
 
 
 def string_to_loop_services(services_path: str) -> List[LoopService]:
     """
     Converts a string of service paths into a list of LoopService objects.
+    This only return the services that implement the LoopService interface. Service that don't implement it are excluded from the list
 
     Args:
         services_path (str): A string containing multiple service paths separated by a ',' delimiter.
@@ -35,7 +44,16 @@ def string_to_loop_services(services_path: str) -> List[LoopService]:
         List[LoopService]: A list of LoopService objects.
 
     """
-    return [
-        string_to_loop_service(service_path=service_path)
-        for service_path in services_path.split(",")
-    ]
+    services = []
+    for service_path in services_path.split(","):
+        service = string_to_loop_service(service_path=service_path)
+        if not service:
+            continue
+
+        if not isinstance(service, LoopService):
+            logger.error(f"Service {service_path} is not a LoopService")
+            continue
+
+        services.append(service)
+
+    return services
--- tests/server/services/test_dynamic_service_loader.py
+++ tests/server/services/test_dynamic_service_loader.py
@@ -17,8 +17,43 @@ from prefect.server.services.loop_service import LoopService
     ],
 )
 async def test_string_to_loop_services(services_path, session):
+    async with session:
+        services = string_to_loop_services(services_path)
+
+    assert len(services) == len(services_path.split(","))
+    for service in services:
+        assert isinstance(service, LoopService)
+
+
+@pytest.mark.parametrize(
+    ["services_path", "nb_result_services"],
+    [
+        ("prefect.server.services.telemetry.Telemetry", 1),
+        ("prefect.server.services.late_runs.MarkLateRuns", 1),
+        (
+            "prefect.server.services.telemetry.Telemetry,prefect.server.services.late_runs.MarkLateRuns",
+            2,
+        ),
+        (
+            (
+                " prefect.server.services.telemetry.Telemetry ,"
+                " prefect.server.services.late_runs.MarkLateRuns "
+            ),
+            2,
+        ),
+        (
+            "prefect.server.services.telemetry.Telemetry,prefect.server.services.late_runs.someService",
+            1,
+        ),
+        ("prefect.server.services.telemetry.Telemetry,prefect.tasks.Task", 1),
+    ],
+)
+async def test_return_only_valid_service_loop(
+    services_path: str, nb_result_services: int, session
+):
     async with session.begin():
         services = string_to_loop_services(services_path)
 
+    assert len(services) == nb_result_services
     for service in services:
         assert isinstance(service, LoopService)
