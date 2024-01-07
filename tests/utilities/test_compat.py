b3a6ec7572153e03c35b914d05b8489dcae95d36
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-06 19:28:38 -0800

    Update test_compat.py

--- tests/utilities/test_compat.py
+++ tests/utilities/test_compat.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
+import sys
+
+import pytest
+
 from prefect import flow
+
+pytestmark = pytest.mark.skipif(sys.version_info < (3, 9))
 
 def test_destringify_class():
     class Test:
