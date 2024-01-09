d9071138396c0a8c92fcc2daf36f9dac6fea8125
Chris Pickett <chris.pickett@prefect.io>
2024-01-09 09:43:14 -0500

    Update src/prefect/input/run_input.py
    Co-authored-by: Andrew Brookins <andrew.b@prefect.io>

--- src/prefect/input/run_input.py
+++ src/prefect/input/run_input.py
@@ -1,13 +1,13 @@
 """
-This module offers a framework for data exchange between flow runs enabling
+This module contains functions that allow sending type-checked `RunInput` data
+to flows at runtime. Flows can send back responses, establishing two-way
+channels with senders. These functions are particularly useful for systems that
+require ongoing data transfer or need to react to input quickly.
 real-time interaction and efficient data handling. It's designed to facilitate
 dynamic communication within distributed or microservices-oriented systems,
 making it ideal for scenarios requiring continuous data synchronization and
-processing.
-
-Use this module to create responsive, real-time applications where different
-flow runs need to communicate and exchange data dynamically. It's particularly
-useful for systems that require ongoing data input and output.
+processing. It's particularly useful for systems that require ongoing data
+input and output.
 
 The following is an example of two flows. One sends a random number to the
 other and waits for a response. The other receives the number, squares it, and
