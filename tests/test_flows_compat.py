45ebca7d1e8e067acd2478cda9481cd816a05a85
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-10 11:30:10 -0800

    Lint fix

--- tests/test_flows_compat.py
+++ tests/test_flows_compat.py
@@ -13,7 +13,6 @@ class Test:
 
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python 3.10 or higher")
 def test_class_destringify():
-
     @flow
     def foo(x: Test):
         return
