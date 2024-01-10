f3721ded4759b98675c0148a0658dcdb28e76fd5
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-10 11:28:05 -0800

    Update test_flows_compat.py

--- tests/test_flows_compat.py
+++ tests/test_flows_compat.py
@@ -7,10 +7,12 @@ import pytest
 from prefect import flow
 
 
+class Test:
+    pass
+
+
 @pytest.mark.skipif(sys.version_info < (3, 10), reason="requires python 3.10 or higher")
 def test_class_destringify():
-    class Test:
-        pass
 
     @flow
     def foo(x: Test):
