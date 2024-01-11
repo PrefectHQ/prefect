d7845622c61df2ec3b8b1668f650a460781deacf
Andrew-S-Rosen <asrosen93@gmail.com>
2024-01-10 17:12:26 -0800

    fix

--- src/prefect/utilities/callables.py
+++ src/prefect/utilities/callables.py
@@ -311,8 +311,10 @@ def parameter_schema(fn: Callable) -> ParameterSchema:
     Returns:
         ParameterSchema: the argument schema
     """
-
-    signature = inspect.signature(fn)
+    try:
+        signature = inspect.signature(fn, eval_str=True)
+    except (NameError, TypeError):
+        signature = inspect.signature(fn)
 
     model_fields = {}
     aliases = {}
