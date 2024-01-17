8fd1571b90e07745aee9f615f613094aa0e7e2e7
Nathan Nowack <thrast36@gmail.com>
2024-01-17 14:23:40 -0600

    correct bad name

--- src/prefect/flows.py
+++ src/prefect/flows.py
@@ -487,19 +487,19 @@ class Flow(Generic[P, R]):
         args, kwargs = parameters_to_args_kwargs(self.fn, parameters)
 
         if HAS_PYDANTIC_V2:
-            has_v1_types = any(isinstance(o, V1BaseModel) for o in args) or any(
+            has_v1_models = any(isinstance(o, V1BaseModel) for o in args) or any(
                 isinstance(o, V1BaseModel) for o in kwargs.values()
             )
             has_v2_types = any(is_v2_type(o) for o in args) or any(
                 is_v2_type(o) for o in kwargs.values()
             )
 
-            if has_v1_types and has_v2_types:
+            if has_v1_models and has_v2_types:
                 raise ParameterTypeError(
                     "Cannot mix Pydantic v1 and v2 types as arguments to a flow."
                 )
 
-            if has_v1_types:
+            if has_v1_models:
                 validated_fn = V1ValidatedFunction(
                     self.fn, config={"arbitrary_types_allowed": True}
                 )
