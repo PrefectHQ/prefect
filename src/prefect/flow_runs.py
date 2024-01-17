d27388f902203e60170c0d0d2f1f5e136e0ae32b
Peter Vlasveld <pvlasveld@gmail.com>
2024-01-16 19:43:43 -0500

    wait_for_flow_run PrefectClient function (#11305)

---
+++ src/prefect/flow_runs.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from uuid import UUID
+
+import anyio
+
+from prefect.client.orchestration import PrefectClient
+from prefect.client.schemas import FlowRun
+from prefect.exceptions import FlowRunWaitTimeout
+
+
+async def wait_for_flow_run(
+    client: PrefectClient,
+    flow_run_id: UUID,
+    timeout: Optional[int] = 10800,
+    poll_interval: int = 5,
+) -> FlowRun:
+    """Waits for the prefect flow run to finish and returns the FlowRun
+
+    Ars:
+        flow_run_id: The flow run ID for the flow run to wait for.
+        timeout: The timeout in seconds, by default 10800 (3 hours).
+        poll_interval: The poll interval in seconds, by default 5.
+    Returns:
+        FlowRun: The finished flow run.
+    Raises:
+        prefect.exceptions.FlowWaitTimeout: If flow run goes over the timeout.
+    """
+    with anyio.move_on_after(timeout):
+        while True:
+            flow_run = await client.read_flow_run(flow_run_id)
+            flow_state = flow_run.state
+            if flow_state and flow_state.is_final():
+                return flow_run
+            await anyio.sleep(poll_interval)
+
+    raise FlowRunWaitTimeout(
+        f"Flow run with ID {flow_run_id} exceeded watch timeout of {timeout} seconds"
+    )
