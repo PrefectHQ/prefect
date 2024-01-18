110d9f18b563e05ef98ec28e0b73adcc78248272
Collin Choy <collin@prefect.io>
2024-01-18 18:29:17 -0500

    Fix mistype - should be value not the alias.

--- src/prefect/cli/server.py
+++ src/prefect/cli/server.py
@@ -219,8 +219,8 @@ async def upgrade(
 async def downgrade(
     yes: bool = typer.Option(False, "--yes", "-y"),
     revision: str = typer.Option(
-        "base",
         "-1",
+        "-r",
         help=(
             "The revision to pass to `alembic downgrade`. If not provided, "
             "downgrades to the most recent revision. Use 'base' to run all "
