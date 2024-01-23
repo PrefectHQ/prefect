1210b5a7a201799d37d36651a114db99601f536f
gabcoyne <gabcoyne@gmail.com>
2024-01-23 09:21:08 -0600

    add test for tags

--- tests/cli/deployment/test_deployment_run.py
+++ tests/cli/deployment/test_deployment_run.py
@@ -484,7 +484,8 @@ async def test_run_deployment_watch(
     deployment_run_with_watch_command = partial(
         invoke_and_assert,
         command=["deployment", "run", deployment_name, "-w"]
-        + (["-wt", str(timeout)] if timeout else []),
+        + (["-wt", str(timeout)] if timeout else [])
+        + ["-t", "cool-tag"],
         expected_output_contains=expected_output,
         expected_code=expected_code,
     )
@@ -501,6 +502,7 @@ async def test_run_deployment_watch(
 
     assert flow_run.state.is_scheduled()
 
+    assert flow_run.tags == ["cool-tag", "test"]
     mock_wait_for_flow_run.assert_awaited_once_with(
         flow_run.id,
         timeout=timeout,
