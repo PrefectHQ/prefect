e573568f5705c0d4645ab2aa3076be03ac55f702
Chris Pickett <chris.pickett@prefect.io>
2024-01-30 13:58:38 -0500

    Give `test_generate_work_pool_base_job_template` up to 4 runs to pass (#11755)

--- tests/infrastructure/test_docker_container.py
+++ tests/infrastructure/test_docker_container.py
@@ -933,7 +933,7 @@ def base_job_template_with_defaults(docker_default_base_job_template):
     return base_job_template_with_defaults
 
 
-@pytest.mark.flaky
+@pytest.mark.flaky(max_runs=4)
 @pytest.mark.usefixtures("mock_collection_registry")
 @pytest.mark.parametrize(
     "container_config",
