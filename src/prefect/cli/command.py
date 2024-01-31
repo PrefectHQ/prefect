05d07da0c2d43cec96866d527db50666aa65feec
George Coyne <gabcoyne@gmail.com>
2024-01-31 10:59:22 -0600

    just use serve (#11788)

--- src/prefect/cli/command.py
+++ src/prefect/cli/command.py
@@ -4,7 +4,6 @@ import logging
 import os
 import sys
 import tempfile
-from enum import Enum
 from typing import List, Optional, Union
 
 import anyio
@@ -15,6 +14,7 @@ from pydantic import VERSION as PYDANTIC_VERSION
 from prefect import flow
 from prefect.cli._types import PrefectTyper
 from prefect.cli.root import app
+from prefect.client.schemas.schedules import CronSchedule
 from prefect.logging import get_run_logger
 
 if PYDANTIC_VERSION.startswith("2."):
@@ -24,13 +24,6 @@ else:
 
 command_app = PrefectTyper(name="command", help="Commands for working with commands.")
 app.add_typer(command_app)
-
-
-class ScheduleType(str, Enum):
-    INTERVAL = "interval"
-    CRON = "cron"
-    RRULE = "rrule"
-    NOSCHEDULE = "noschedule"
 
 
 @flow
@@ -144,103 +137,19 @@ async def command(
     await shell_run_command(command, cwd=cwd)
 
 
-# @command_app.command("serve")
-# async def serve(
-#     command: str,
-#     name: str = typer.Option(..., "--name", help="Name of the flow to serve."),
-#     cwd: Optional[str] = typer.Option(None, help="Working directory for the flow."),
-#     interval: Optional[str] = typer.Option(
-#         None,
-#         help="Interval at which to run the flow, as a string parseable into a timedelta.",
-#     ),
-#     cron: Optional[str] = typer.Option(
-#         None, help="CRON expression for scheduling the flow."
-#     ),
-#     rrule: Optional[str] = typer.Option(
-#         None, help="RRULE expression for complex scheduling."
-#     ),
-#     schedule: Optional[ScheduleType] = typer.Option(
-#         None, help="Custom scheduling type."
-#     ),
-#     is_schedule_active: Optional[bool] = typer.Option(
-#         None, help="Flag to activate or deactivate scheduling."
-#     ),
-#     # triggers: Optional[List[DeploymentTrigger]] = typer.Option(
-#     #     None, help="List of triggers for flow deployment."
-#     # ),
-#     parameters: Optional[dict] = typer.Option(
-#         None, help="Dictionary of parameters to pass to the flow."
-#     ),
-#     description: Optional[str] = typer.Option(None, help="Description of the flow."),
-#     tags: Optional[List[str]] = typer.Option(
-#         None, help="List of tags associated with the flow."
-#     ),
-#     version: Optional[str] = typer.Option(None, help="Version of the flow."),
-#     enforce_parameter_schema: bool = typer.Option(
-#         False, help="Flag to enforce parameter schema."
-#     ),
-#     pause_on_shutdown: bool = typer.Option(
-#         True, help="Flag to pause the flow on shutdown."
-#     ),
-#     print_starting_message: bool = typer.Option(
-#         True, help="Flag to print a starting message."
-#     ),
-#     limit: Optional[int] = typer.Option(
-#         None, help="Limit the number of concurrent runs."
-#     ),
-#     webserver: bool = typer.Option(
-#         False, help="Flag to start a webserver for the flow."
-#     ),
-# ):
-#     """
-#     Serve the execution of a command by executing it as a Prefect flow
-#     """
-#     from prefect.runner import Runner
-
-#     # Handling for my_flow.serve(__file__)
-#     # Will set name to name of file where my_flow.serve() without the extension
-#     # Non filepath strings will pass through unchanged
-#     name = Path(name).stem
-#     # Parse and convert the schedule string to the correct schedule type
-#     parsed_schedule = None
-#     if schedule:
-#         if schedule == ScheduleType.INTERVAL:
-#             parsed_schedule = IntervalSchedule()
-#         elif schedule == ScheduleType.CRON:
-#             parsed_schedule = CronSchedule()
-#         elif schedule == ScheduleType.RRULE:
-#             parsed_schedule = RRuleSchedule()
-#         elif schedule == ScheduleType.NOSCHEDULE:
-#             parsed_schedule = NoSchedule()
-
-#     runner = Runner(name=name, limit=limit)
-#     deployment_id = await runner.add_flow(
-#         flow=await shell_run_command(command=command, cwd=cwd),
-#         name=name,
-#         triggers=triggers,
-#         interval=interval,
-#         cron=cron,
-#         rrule=rrule,
-#         schedule=parsed_schedule,
-#         is_schedule_active=is_schedule_active,
-#         parameters=parameters,
-#         description=description,
-#         tags=tags,
-#         version=version,
-#         enforce_parameter_schema=enforce_parameter_schema,
-#     )
-#     if print_starting_message:
-#         help_message = (
-#             f"[green]Your flow {name!r} is being served and polling for"
-#             " scheduled runs!\n[/]\nTo trigger a run for this flow, use the"
-#             " following command:\n[blue]\n\t$ prefect deployment run"
-#             f" '{name}/{name}'\n[/]"
-#         )
-#         if PREFECT_UI_URL:
-#             help_message += (
-#                 "\nYou can also run your flow via the Prefect UI:"
-#                 f" [blue]{PREFECT_UI_URL.value()}/deployments/deployment/{deployment_id}[/]\n"
-#             )
-
-#         app.console.print(Panel(help_message))
-#     await runner.start(webserver=webserver)
+@command_app.command("serve")
+async def serve(
+    command: str,
+    name: str = typer.Option(help="Name of the flow to serve"),
+    cwd: str = typer.Option(None, help="Working directory for the flow"),
+    cron_schedule: str = typer.Option(None, help="Cron schedule for the flow"),
+):
+    """
+    Serve the execution of a command by executing it as a Prefect flow
+    """
+    CronSchedule(cron_schedule) if cron_schedule else None
+    # Call the shell_run_command flow with provided arguments
+    await shell_run_command.serve(
+        name=name,
+        parameters={"command": command, "cwd": cwd},
+    )
