e254fe0f8b3e9031d0f129fa81b4c9f342ca8cfe
Chris Guidry <chris.g@prefect.io>
2024-02-02 16:24:48 -0500

    Update test_connection_leak_warnings.py

--- tests/server/utilities/test_connection_leak_warnings.py
+++ tests/server/utilities/test_connection_leak_warnings.py
@@ -2,7 +2,7 @@
 This test suite exists to reproduce the issue in
 https://github.com/PrefectHQ/prefect/issues/11820
 
-They shoulnd't be run as part of the normal test suite, but can be run manually with:
+They shouldn't be run as part of the normal test suite, but can be run manually with:
 
 TEST_CONNECTION_LEAK=true pytest tests/server/utilities/test_connection_leak_warnings.py
 """
