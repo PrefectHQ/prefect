d45f07704acb94f87da83348dc1618191a89cde9
jakekaplan <40362401+jakekaplan@users.noreply.github.com>
2024-02-06 11:19:41 -0500

    fix context tests (#11881)

--- flows/client_context_lifespan.py
+++ flows/client_context_lifespan.py
@@ -1,3 +1,13 @@
+import sys
+
+from packaging.version import Version
+
+import prefect
+
+# Only run these tests if the version is at least 2.13.0
+if Version(prefect.__version__) < Version("2.13.0"):
+    sys.exit(0)
+
 import asyncio
 import random
 import threading
