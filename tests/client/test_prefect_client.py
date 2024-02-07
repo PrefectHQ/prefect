386eb164974072fc4d52edeceb2620c68c09e379
jakekaplan <40362401+jakekaplan@users.noreply.github.com>
2024-02-07 13:26:56 -0500

    fix deployment flake (#11919)

--- tests/client/test_prefect_client.py
+++ tests/client/test_prefect_client.py
@@ -945,6 +945,7 @@ async def test_read_flow_by_name(prefect_client):
 async def test_create_flow_run_from_deployment(
     prefect_client: PrefectClient, deployment
 ):
+    start_time = pendulum.now("utc")
     flow_run = await prefect_client.create_flow_run_from_deployment(deployment.id)
     # Deployment details attached
     assert flow_run.deployment_id == deployment.id
@@ -957,10 +958,7 @@ async def test_create_flow_run_from_deployment(
     # State is scheduled for now
     assert flow_run.state.type == StateType.SCHEDULED
     assert (
-        pendulum.now("utc")
-        .diff(flow_run.state.state_details.scheduled_time)
-        .in_seconds()
-        < 1
+        start_time <= flow_run.state.state_details.scheduled_time <= pendulum.now("utc")
     )
 
 
