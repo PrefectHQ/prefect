af22ab1ced25ef6921d4b600ed5b6a277ee58c6d
Chris Pickett <chris.pickett@prefect.io>
2024-02-21 16:41:21 -0500

    Enable enhanced scheduling UI by default.

--- src/prefect/settings.py
+++ src/prefect/settings.py
@@ -1467,7 +1467,7 @@ PREFECT_EXPERIMENTAL_ENABLE_WORK_QUEUE_STATUS = Setting(bool, default=True)
 Whether or not to enable experimental work queue status in-place of work queue health.
 """
 
-PREFECT_EXPERIMENTAL_ENABLE_ENHANCED_SCHEDULING_UI = Setting(bool, default=False)
+PREFECT_EXPERIMENTAL_ENABLE_ENHANCED_SCHEDULING_UI = Setting(bool, default=True)
 """
 Whether or not to enable the enhanced scheduling UI.
 """
