2a4f00fd3ea42973dbc34ff59fd72cb8b7c3ce69
nate nowack <nate@prefect.io>
2024-02-21 18:22:47 -0600

    update wording

--- src/prefect/utilities/dockerutils.py
+++ src/prefect/utilities/dockerutils.py
@@ -112,7 +112,7 @@ def docker_client() -> Generator["DockerClient", None, None]:
             yield client
     except docker.errors.DockerException as exc:
         raise RuntimeError(
-            "This error is likely caused because Docker is not running. Please start Docker and rerun your code."
+            "This error is often thrown because Docker is not running. Please ensure Docker is running."
         ) from exc
     finally:
         client is not None and client.close()
