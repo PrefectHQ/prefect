bfed5be87533dcee042cb989ecc2f2a3e2f9fa59
Andrew Brookins <andrew.b@prefect.io>
2024-03-06 10:00:56 -0800

    Add a couple of tests

--- src/prefect/_internal/concurrency/threads.py
+++ src/prefect/_internal/concurrency/threads.py
@@ -214,7 +214,7 @@ class EventLoopThread(Portal):
         for call in self._on_shutdown:
             await self._run_call(call)
 
-        # Remove calls from the list to prevent memory cycles.
+        # Empty the list to allow calls to be garbage collected. Issue #10338.
         self._on_shutdown = []
 
     async def _run_call(self, call: Call) -> None:
