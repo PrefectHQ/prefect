7c4c3024b48f4c1e772f81ff7f5efa9a7cfb9e9d
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-03-12 13:59:54 -0400

    Update utilities.py

--- src/prefect/client/utilities.py
+++ src/prefect/client/utilities.py
@@ -1,16 +1,44 @@
 """
 Utilities for working with clients.
 """
+
 # This module must not import from `prefect.client` when it is imported to avoid
 # circular imports for decorators such as `inject_client` which are widely used.
 
 from functools import wraps
+from typing import Any, Callable, Coroutine, Optional
 
 from prefect._internal.concurrency.event_loop import get_running_loop
+from prefect.client.orchestration import PrefectClient, get_client
+from prefect.context import FlowRunContext, TaskRunContext
 from prefect.utilities.asyncutils import asyncnullcontext
 
 
-def inject_client(fn):
+def get_or_infer_client(client: Optional[PrefectClient] = None) -> PrefectClient:
+    """
+    Infer a client from the provided client or the current context.
+    """
+    if client is not None:
+        return client
+
+    flow_run_context = FlowRunContext.get()
+    task_run_context = TaskRunContext.get()
+    if (
+        flow_run_context
+        and getattr(flow_run_context.client, "_loop") == get_running_loop()
+    ):
+        return flow_run_context.client
+    elif (
+        task_run_context
+        and getattr(task_run_context.client, "_loop") == get_running_loop()
+    ):
+        return task_run_context.client
+    else:
+
+        return get_client()
+
+
+def inject_client(fn: Callable[..., Any]) -> Callable[..., Coroutine[Any, Any, Any]]:
     """
     Simple helper to provide a context managed client to a asynchronous function.
 
@@ -20,32 +48,10 @@ def inject_client(fn):
     """
 
     @wraps(fn)
-    async def with_injected_client(*args, **kwargs):
-        from prefect.client.orchestration import get_client
-        from prefect.context import FlowRunContext, TaskRunContext
-
-        flow_run_context = FlowRunContext.get()
-        task_run_context = TaskRunContext.get()
+    async def with_injected_client(*args: Any, **kwargs: Any) -> Any:
         client = None
         client_context = asyncnullcontext()
-
-        if "client" in kwargs and kwargs["client"] is not None:
-            # Client provided in kwargs
-            client = kwargs["client"]
-        elif flow_run_context and flow_run_context.client._loop == get_running_loop():
-            # Client available from flow run context
-            client = flow_run_context.client
-        elif task_run_context and task_run_context.client._loop == get_running_loop():
-            # Client available from task run context
-            client = task_run_context.client
-
-        else:
-            # A new client is needed
-            client_context = get_client()
-
-        # Removes existing client to allow it to be set by setdefault below
-        kwargs.pop("client", None)
-
+        client = get_or_infer_client(kwargs.pop("client", None))
         async with client_context as new_client:
             kwargs.setdefault("client", new_client or client)
             return await fn(*args, **kwargs)
