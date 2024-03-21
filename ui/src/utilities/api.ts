881509c0053e4c3375826d028e08d1567617fff4
Chris Pickett <chris.pickett@prefect.io>
2024-03-21 09:57:48 -0400

    Implement CSRF support in the UI (#12354)
    Co-authored-by: Nicholas Brown <znicholasbrown@gmail.com>
    Co-authored-by: Craig Harshbarger <pleek91@gmail.com>

--- ui/src/utilities/api.ts
+++ ui/src/utilities/api.ts
@@ -2,14 +2,24 @@ import { createApi, PrefectConfig } from '@prefecthq/prefect-ui-library'
 import { createActions } from '@prefecthq/vue-compositions'
 import { InjectionKey } from 'vue'
 import { AdminApi } from '@/services/adminApi'
+import { CsrfTokenApi, setupCsrfInterceptor } from '@/services/csrfTokenApi'
+import { AxiosInstance } from 'axios'
+
+
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function createPrefectApi(config: PrefectConfig) {
-  const workspaceApi = createApi(config)
+  const csrfTokenApi = createActions(new CsrfTokenApi(config))
 
+  function axiosInstanceSetupHook(axiosInstance: AxiosInstance) {
+    setupCsrfInterceptor(csrfTokenApi, axiosInstance)
+  };
+
+  const workspaceApi = createApi(config, axiosInstanceSetupHook)
   return {
     ...workspaceApi,
-    admin: createActions(new AdminApi(config)),
+    csrf: csrfTokenApi,
+    admin: createActions(new AdminApi(config, axiosInstanceSetupHook)),
   }
 }
 
