72591d5cc458503b16e4a7f123f51328ebe74ab9
zzstoatzz <thrast36@gmail.com>
2024-03-25 18:59:22 -0500

    fix tests

--- tests/_internal/pydantic/test_type_adapter.py
+++ tests/_internal/pydantic/test_type_adapter.py
@@ -23,7 +23,7 @@ class Model(BaseModel):
 
 
 @pytest.mark.parametrize(
-    "type, value, expected",
+    "type_, value, expected",
     [
         (int, 42, 42),
         (str, "42", "42"),
@@ -32,9 +32,17 @@ class Model(BaseModel):
         (List[Model], [{"a": 42, "b": "42", "c": True}], [Model(a=42, b="42", c=True)]),
     ],
 )
-def test_validate_python(type, value, expected):
-    assert validate_python(type, value) == expected
+def test_validate_python(type_, value, expected):
+    assert validate_python(type_, value) == expected
 
     # check it works with the flag disabled
     with temporary_settings({PREFECT_EXPERIMENTAL_ENABLE_PYDANTIC_V2_INTERNALS: False}):
-        assert validate_python(type, value) == expected
+        # parse_obj_as doesn't handle the `Model` / `List[Model]` case out of the box
+        if isinstance(expected, BaseModel) or (
+            isinstance(expected, list)
+            and all(isinstance(i, BaseModel) for i in expected)
+        ):
+            with pytest.raises(RuntimeError, match="no validator found for"):
+                assert validate_python(type_, value) == expected
+        else:
+            assert validate_python(type_, value) == expected
