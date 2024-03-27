627a8dfc7b22509dcd7492e009778963ace977ce
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-03-27 12:51:53 -0400

    one venv

--- scripts/test-internal-pydantic.sh
+++ scripts/test-internal-pydantic.sh
@@ -2,15 +2,15 @@ versions=(3.12 3.8)
 pydantic=("pydantic<2" "pydantic<3")
 
 for version in "${versions[@]}"; do
+    uv venv $version
+    source $version/bin/activate
+    uv pip install -e "../.[dev]"
     for pydantic_version in "${pydantic[@]}"; do
         echo "Testing Python $version with $pydantic_version"
         pyenv local $version
-        uv venv $version
-        source $version/bin/activate
-        uv pip install -e "../.[dev]"
         uv pip install -U $pydantic_version
         python -m pytest ../tests/_internal/pydantic
-        deactivate
-        rm -rf $version
     done
+    deactivate
+    rm -rf $version
 done
