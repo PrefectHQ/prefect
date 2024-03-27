d96ac88232e38bae69ce9701beab4e28f4d332e3
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-03-27 13:17:06 -0500

    move one fn down

--- src/prefect/_internal/schemas/validators.py
+++ src/prefect/_internal/schemas/validators.py
@@ -179,19 +179,6 @@ def handle_openapi_schema(value: Optional["ParameterSchema"]) -> "ParameterSchem
     return value
 
 
-def validate_automation_names(
-    field_value: List["DeploymentTrigger"], values: dict
-) -> List["DeploymentTrigger"]:
-    """
-    Ensure that each trigger has a name for its automation if none is provided.
-    """
-    for i, trigger in enumerate(field_value, start=1):
-        if trigger.name is None:
-            trigger.name = f"{values['name']}__automation_{i}"
-
-    return field_value
-
-
 ### SCHEDULE SCHEMA VALIDATORS ###
 
 
@@ -369,3 +356,16 @@ def validate_trigger_within(
     if value.total_seconds() < minimum:
         raise ValueError("The minimum `within` is 0 seconds")
     return value
+
+
+def validate_automation_names(
+    field_value: List["DeploymentTrigger"], values: dict
+) -> List["DeploymentTrigger"]:
+    """
+    Ensure that each trigger has a name for its automation if none is provided.
+    """
+    for i, trigger in enumerate(field_value, start=1):
+        if trigger.name is None:
+            trigger.name = f"{values['name']}__automation_{i}"
+
+    return field_value
