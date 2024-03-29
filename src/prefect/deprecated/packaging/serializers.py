e694bf671d420f981ee66138300b63ebc365814c
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-03-29 13:47:01 -0500

    Move docker and serializers validation (#12491)

--- src/prefect/deprecated/packaging/serializers.py
+++ src/prefect/deprecated/packaging/serializers.py
@@ -7,13 +7,17 @@ import base64
 import inspect
 import json
 import os.path
-import warnings
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from typing import Any, List
 
 from prefect._internal.compatibility.deprecated import deprecated_class
 from prefect._internal.pydantic import HAS_PYDANTIC_V2
+from prefect._internal.schemas.validators import (
+    validate_picklelib,
+    validate_picklelib_and_modules,
+    validate_picklelib_version,
+)
 
 if HAS_PYDANTIC_V2:
     import pydantic.v1 as pydantic
@@ -55,69 +59,15 @@ class PickleSerializer(Serializer):
 
     @pydantic.validator("picklelib")
     def check_picklelib(cls, value):
-        """
-        Check that the given pickle library is importable and has dumps/loads methods.
-        """
-        try:
-            pickler = from_qualified_name(value)
-        except (ImportError, AttributeError) as exc:
-            raise ValueError(
-                f"Failed to import requested pickle library: {value!r}."
-            ) from exc
-
-        if not callable(getattr(pickler, "dumps", None)):
-            raise ValueError(
-                f"Pickle library at {value!r} does not have a 'dumps' method."
-            )
-
-        if not callable(getattr(pickler, "loads", None)):
-            raise ValueError(
-                f"Pickle library at {value!r} does not have a 'loads' method."
-            )
-
-        return value
+        return validate_picklelib(value)
 
     @pydantic.root_validator
     def check_picklelib_version(cls, values):
-        """
-        Infers a default value for `picklelib_version` if null or ensures it matches
-        the version retrieved from the `pickelib`.
-        """
-        picklelib = values.get("picklelib")
-        picklelib_version = values.get("picklelib_version")
-
-        if not picklelib:
-            raise ValueError("Unable to check version of unrecognized picklelib module")
-
-        pickler = from_qualified_name(picklelib)
-        pickler_version = getattr(pickler, "__version__", None)
-
-        if not picklelib_version:
-            values["picklelib_version"] = pickler_version
-        elif picklelib_version != pickler_version:
-            warnings.warn(
-                (
-                    f"Mismatched {picklelib!r} versions. Found {pickler_version} in the"
-                    f" environment but {picklelib_version} was requested. This may"
-                    " cause the serializer to fail."
-                ),
-                RuntimeWarning,
-                stacklevel=3,
-            )
-
-        return values
+        return validate_picklelib_version(values)
 
     @pydantic.root_validator
     def check_picklelib_and_modules(cls, values):
-        """
-        Prevents modules from being specified if picklelib is not cloudpickle
-        """
-        if values.get("picklelib") != "cloudpickle" and values.get("pickle_modules"):
-            raise ValueError(
-                "`pickle_modules` cannot be used without 'cloudpickle'. Got"
-                f" {values.get('picklelib')!r}."
-            )
-        return values
+        return validate_picklelib_and_modules(values)
 
     def dumps(self, obj: Any) -> bytes:
         pickler = from_qualified_name(self.picklelib)
