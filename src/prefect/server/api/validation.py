1cc9176c3a5b68bd0142b6037d496c318aa5929b
urimandujano <uriel@prefect.io>
2024-03-29 18:38:20 -0500

    Handle referenced blocks in base templates during `job_variable` validation (#12329)
    Co-authored-by: Andrew Brookins <andrew.b@prefect.io>

--- src/prefect/server/api/validation.py
+++ src/prefect/server/api/validation.py
@@ -1,9 +1,10 @@
 from typing import Union
 
 from prefect._vendor.fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
 
-import prefect.server.schemas as schemas
 from prefect._internal.schemas.validators import validate_values_conform_to_schema
+from prefect.server import models, schemas
 
 
 def _get_base_config_defaults(base_config: dict):
@@ -16,15 +17,59 @@ def _get_base_config_defaults(base_config: dict):
     return defaults
 
 
-def validate_job_variables_for_flow_run(
+async def _resolve_default_references(variables: dict, session: AsyncSession) -> dict:
+    """
+    Iterate through discovered job_variables and resolve references to blocks. The input
+    variables should have a format of:
+
+    {
+        "variable_name": {
+            "$ref": {
+                "block_document_id": "block_document_id"
+            },
+        "other_variable_name": "plain_value"
+    }
+
+    """
+    for name, default_value in variables.items():
+        if not isinstance(default_value, dict):
+            continue
+
+        if "$ref" not in default_value:
+            continue
+
+        reference_data = default_value.get("$ref")
+
+        if (block_doc_id := reference_data.get("block_document_id")) is None:
+            continue
+
+        block_document = await models.block_documents.read_block_document_by_id(
+            session, block_doc_id
+        )
+        if not block_document:
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Block not found.")
+
+        variables[name] = block_document.data
+
+    return variables
+
+
+async def validate_job_variables_for_flow_run(
     flow_run: Union[
         schemas.actions.DeploymentFlowRunCreate, schemas.actions.FlowRunUpdate
     ],
     deployment: schemas.core.Deployment,
+    session: AsyncSession,
 ) -> None:
+    """
+    This will raise HTTP 404 error if the referenced block document or
+    the actor does not have permissions to access that block. Therefore, this is only safe
+    to use within the context of an API request
+    """
     base_vars = _get_base_config_defaults(
         deployment.work_queue.work_pool.base_job_template
     )
+    base_vars = await _resolve_default_references(base_vars, session)
     flow_run_vars = flow_run.job_variables or {}
     job_vars = {**base_vars, **deployment.infra_overrides, **flow_run_vars}
 
