2edd0b8bbb2cf4e27cee10557d75556e4b5159ce
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-03-30 22:53:15 -0400

    Fix: Pydantic dynamic imports (#12503)

--- src/prefect/pydantic/__init__.py
+++ src/prefect/pydantic/__init__.py
@@ -4,14 +4,20 @@ from prefect._internal.pydantic._flags import HAS_PYDANTIC_V2, USE_PYDANTIC_V2
 if typing.TYPE_CHECKING:
     # import of virtually everything is supported via `__getattr__` below,
     # but we need them here for type checking and IDE support
-    from pydantic import Field, SecretField
+    from pydantic import Field, validator, root_validator
     from .main import BaseModel, PrefectBaseModel
 
-__all__ = ["BaseModel", "PrefectBaseModel", "Field", "SecretField"]
+__all__ = [
+    "BaseModel",
+    "PrefectBaseModel",
+    "Field",
+    "validator",
+    "root_validator",
+]
 
-_dynamic_imports: "typing.Dict[str, tuple[str, str]]" = {
-    "BaseModel": ("prefect.pydantic.main", "BaseModel"),
-    "PrefectBaseModel": ("prefect.pydantic.main", "PrefectBaseModel"),
+_dynamic_imports: "typing.Dict[str, typing.Tuple[str, str]]" = {
+    "BaseModel": ("prefect.pydantic", ".main"),
+    "PrefectBaseModel": ("prefect.pydantic", ".main"),
 }
 
 
@@ -20,14 +26,22 @@ def __getattr__(attr_name: str) -> object:
 
     if attr_name in _dynamic_imports:
         # If the attribute is in the dynamic imports, import it from the specified module
-        module_path, module_name = _dynamic_imports[attr_name]
-        module = import_module(module_path, module_name)
-        return getattr(module, module_name)
+        package, module_name = _dynamic_imports[attr_name]
+
+        # Prevent recursive import
+        if module_name == "__module__":
+            return import_module(f".{attr_name}", package=package)
+
+        # Import the module and return the attribute
+        else:
+            module = import_module(module_name, package=package)
+            return getattr(module, attr_name)
+
     elif HAS_PYDANTIC_V2 and not USE_PYDANTIC_V2:
         # In this case, we are using Pydantic v2 but it is not enabled, so we should import from pydantic.v1
-        module = import_module("pydantic.v1", attr_name)
-        return module
+        module = import_module("pydantic.v1")
+        return getattr(module, attr_name)
     else:
         # In this case, we are using either Pydantic v1 or Pydantic v2 is enabled, so we should import from pydantic
-        module = import_module("pydantic", attr_name)
-        return module
+        module = import_module("pydantic")
+        return getattr(module, attr_name)
