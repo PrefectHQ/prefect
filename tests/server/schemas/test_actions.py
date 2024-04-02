c33dfcda906df432fc78dab5600cf9f198f0d756
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-04-02 11:13:18 -0500

    fix tests

--- tests/server/schemas/test_actions.py
+++ tests/server/schemas/test_actions.py
@@ -44,7 +44,7 @@ class TestDeploymentCreate:
         with pytest.warns(
             UserWarning,
             match=(
-                "`worker_pool_queue_id` is no longer supported for creating or updating"
+                "`worker_pool_queue_id` is no longer supported for creating or updating "
                 "deployments. Please use `work_pool_name` and "
                 "`work_queue_name` instead."
             ),
@@ -69,7 +69,7 @@ class TestDeploymentCreate:
             match=(
                 "`worker_pool_name`, `worker_pool_queue_name`, and "
                 "`work_pool_name` are"
-                "no longer supported for creating or updating"
+                "no longer supported for creating or updating "
                 "deployments. Please use `work_pool_name` and "
                 "`work_queue_name` instead."
             ),
@@ -171,7 +171,7 @@ class TestDeploymentUpdate:
             match=(
                 "`worker_pool_name`, `worker_pool_queue_name`, and "
                 "`work_pool_name` are"
-                "no longer supported for creating or updating"
+                "no longer supported for creating or updating "
                 "deployments. Please use `work_pool_name` and "
                 "`work_queue_name` instead."
             ),
