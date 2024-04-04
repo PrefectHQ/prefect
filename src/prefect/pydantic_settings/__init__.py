17cfc34333b43ab172160f57da22395b4d7117b6
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-04 13:37:26 -0400

    Update __init__.py

--- src/prefect/pydantic_settings/__init__.py
+++ src/prefect/pydantic_settings/__init__.py
@@ -1,9 +1,6 @@
-from typing import TYPE_CHECKING
 from prefect._internal.pydantic._flags import HAS_PYDANTIC_V2, USE_PYDANTIC_V2
 
-if TYPE_CHECKING:
-    from pydantic_settings import BaseSettings
-elif HAS_PYDANTIC_V2 and USE_PYDANTIC_V2:
+if HAS_PYDANTIC_V2 and USE_PYDANTIC_V2:
     from pydantic_settings import BaseSettings
 elif HAS_PYDANTIC_V2 and not USE_PYDANTIC_V2:
     from pydantic.v1 import BaseSettings
