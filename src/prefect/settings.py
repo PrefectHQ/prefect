6dfa427777e7d3b08569c587eb72553c5248ba1d
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-05 18:00:25 -0400

    Update settings.py

--- src/prefect/settings.py
+++ src/prefect/settings.py
@@ -70,6 +70,7 @@ from pydantic_core import SchemaSerializer
 from typing_extensions import Literal
 
 from prefect._internal.compatibility.deprecated import generate_deprecation_message
+from prefect._internal.pydantic.utilities.model_dump import model_dump
 from prefect._internal.schemas.validators import validate_settings
 from prefect.exceptions import MissingProfileError
 from prefect.pydantic import BaseModel, Field, create_model, root_validator, validator
@@ -1771,7 +1772,7 @@ class Settings(SettingsFieldsMixin):
         return self.__class__(
             **{
                 **{setting.name: value for setting, value in set_defaults.items()},
-                **self.dict(exclude_unset=True, exclude=restore_defaults_names),
+                **model_dump(self, exclude_unset=True, exclude=restore_defaults_names),
                 **{setting.name: value for setting, value in updates.items()},
             }
         )
@@ -1827,7 +1828,7 @@ class Settings(SettingsFieldsMixin):
             set_keys = {
                 # Collect all of the "set" keys and cast to `Setting` objects
                 SETTING_VARIABLES[key]
-                for key in self.dict(exclude_unset=True)
+                for key in model_dump(self, exclude_unset=True)
             }
             include.intersection_update(set_keys)
 
