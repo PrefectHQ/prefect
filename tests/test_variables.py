953a5ba33b7b66e4c108adc39959352858db1b83
Will Raphaelson <will@prefect.io>
2024-04-08 11:38:02 -0500

    Variable set/get client methods (#12596)

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -1,22 +1,41 @@
 import pytest
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from prefect import flow, variables
-from prefect.server.models.variables import create_variable
-from prefect.server.schemas.actions import VariableCreate
 
 
 @pytest.fixture
-async def variable(
-    session: AsyncSession,
-):
-    model = await create_variable(
-        session,
-        VariableCreate(name="my_variable", value="my-value", tags=["123", "456"]),
+async def variable():
+    model = await variables.Variable.set(
+        name="my_variable", value="my-value", tags=["123", "456"]
     )
-    await session.commit()
-
     return model
+
+
+async def test_set_variable():
+    model = await variables.Variable.set(
+        name="my_new_variable", value="my_value", tags=["123", "456"]
+    )
+    assert model.name == "my_new_variable"
+    assert model.value == "my_value"
+    assert model.tags == ["123", "456"]
+
+
+def test_variable_with_same_name_and_not_overwrite_errors(variable):
+    with pytest.raises(
+        ValueError,
+        match="You are attempting to save a variable with a name that is already in use. If you would like to overwrite the values that are saved, then call .set with `overwrite=True`.",
+    ):
+        variables.Variable.set(name=variable.name, value="new_value", overwrite=False)
+
+
+async def test_variable_with_same_name_and_overwrite(variable):
+    new_value = "new_value"
+    new_tags = ["my", "new", "tags"]
+    overwritten_variable = await variables.Variable.set(
+        name=variable.name, value=new_value, tags=new_tags, overwrite=True
+    )
+    assert overwritten_variable.value == new_value
+    assert overwritten_variable.tags == new_tags
 
 
 def test_get(variable):
@@ -53,3 +72,23 @@ async def test_variables_work_in_async_flows(variable):
 
     res = await foo()
     assert res == variable.value
+
+
+def test_variable_class_work_in_sync_flows(variable):
+    @flow
+    def foo():
+        var = variables.Variable.get("my_variable")
+        return var
+
+    res = foo()
+    assert res.value == variable.value
+
+
+async def test_variable_class_work_in_async_flows(variable):
+    @flow
+    async def foo():
+        var = await variables.Variable.get("my_variable")
+        return var
+
+    res = await foo()
+    assert res.value == variable.value
