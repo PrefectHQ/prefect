ef3d842525a1d5d9ce45f8fa07e9fa1c60453414
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-04-10 14:32:41 -0500

    Add experimental Pydantic V2 `model_validator` (#12635)

--- src/prefect/_internal/pydantic/_compat.py
+++ src/prefect/_internal/pydantic/_compat.py
@@ -21,6 +21,7 @@ from .utilities.model_fields_set import ModelFieldsSetMixin, model_fields_set
 from .utilities.model_json_schema import ModelJsonSchemaMixin, model_json_schema
 from .utilities.model_validate import ModelValidateMixin, model_validate
 from .utilities.model_validate_json import ModelValidateJsonMixin, model_validate_json
+from .utilities.model_validator import model_validator
 from .utilities.type_adapter import TypeAdapter, validate_python
 
 if typing.TYPE_CHECKING:
@@ -68,6 +69,7 @@ __all__ = [
     "Field",
     "FieldInfo",
     "field_validator",
+    "model_validator",
     "PrivateAttr",
     "SecretStr",
     "ConfigDict",
