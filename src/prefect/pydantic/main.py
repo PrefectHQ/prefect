64c32528d0548b704251dcaf5e66635183a89ceb
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-04-09 23:32:56 -0400

    Update main.py

--- src/prefect/pydantic/main.py
+++ src/prefect/pydantic/main.py
@@ -11,9 +11,15 @@ from prefect._internal.pydantic._compat import (
     field_validator,
 )
 
+if typing.TYPE_CHECKING:
+    from rich.repr import Result
+
 
 class PrefectBaseModel(BaseModel):
-    model_config = ConfigDict()
+    model_config = ConfigDict(
+        revalidate_instances="never",
+    )
+
     __prefect_exclude__: typing.ClassVar[typing.Set[str]] = set()
 
     def reset(self: "typing.Self") -> "typing.Self":
@@ -23,6 +29,20 @@ class PrefectBaseModel(BaseModel):
         return self.model_construct().model_copy(
             deep=True, update=self.model_dump(exclude=self.__prefect_exclude__)
         )
+
+    def reveal_secrets(self: "typing.Self") -> "typing.Self":
+        return self.model_copy(
+            deep=True,
+            update={
+                field_name: getattr(self, field_name).get_secret_value()
+                for field_name, field in self.model_dump().items()
+                if hasattr(field, "get_secret_value")
+            },
+        )
+
+    def __rich_repr__(self: "typing.Self") -> "Result":
+        for name, value in self.model_dump(mode="json").items():
+            yield name, value
 
 
 __all__ = [
