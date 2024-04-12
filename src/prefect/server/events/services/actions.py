ccc91a2696459e89e624e61628076ee17fcc183f
Chris Guidry <chris.g@prefect.io>
2024-04-12 15:13:44 -0400

    Actions service (#12699)

--- src/prefect/server/events/services/actions.py
+++ src/prefect/server/events/services/actions.py
@@ -1,43 +1,35 @@
 import asyncio
 from typing import Optional
 
-import rich
-
 from prefect.logging import get_logger
-from prefect.server.events.schemas.automations import TriggeredAction
-from prefect.server.utilities.messaging import Message, create_consumer
+from prefect.server.events import actions
+from prefect.server.utilities.messaging import create_consumer
 
 logger = get_logger(__name__)
 
 
-class ActionLogger:
-    """A debugging service that logs actions to the console as they arrive."""
+class Actions:
+    """Runs actions triggered by Automatinos"""
 
-    name: str = "ActionLogger"
+    name: str = "Actions"
 
     consumer_task: Optional[asyncio.Task] = None
 
     async def start(self):
-        assert self.consumer_task is None, "Logger already started"
+        assert self.consumer_task is None, "Actions already started"
         self.consumer = create_consumer("actions")
 
-        console = rich.console.Console()
+        async with actions.consumer() as handler:
+            self.consumer_task = asyncio.create_task(self.consumer.run(handler))
+            logger.debug("Actions started")
 
-        async def handler(message: Message):
-            triggered_action: TriggeredAction = TriggeredAction.parse_raw(message.data)
-            console.print("Action:", triggered_action.action.type)
-            console.file.flush()
-
-        self.consumer_task = asyncio.create_task(self.consumer.run(handler))
-        logger.debug("Action logger started")
-
-        try:
-            await self.consumer_task
-        except asyncio.CancelledError:
-            pass
+            try:
+                await self.consumer_task
+            except asyncio.CancelledError:
+                pass
 
     async def stop(self):
-        assert self.consumer_task is not None, "Logger not started"
+        assert self.consumer_task is not None, "Actions not started"
         self.consumer_task.cancel()
         try:
             await self.consumer_task
@@ -45,4 +37,4 @@ class ActionLogger:
             pass
         finally:
             self.consumer_task = None
-        logger.debug("Action logger stopped")
+        logger.debug("Actions stopped")
