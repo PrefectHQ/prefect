d6e3472ba434d7fce5bb39b4fbd06802691c64ac
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-04-13 17:17:44 -0400

    [types] NonNegativeDuration / PositiveDuration (#12711)

--- src/prefect/types/__init__.py
+++ src/prefect/types/__init__.py
@@ -3,6 +3,7 @@ from typing import Any, Callable, ClassVar, Generator
 
 from pydantic_core import core_schema, CoreSchema, SchemaValidator
 from typing_extensions import Self
+from datetime import timedelta
 
 
 @dataclass
@@ -41,3 +42,49 @@ class PositiveInteger(int):
     @classmethod
     def validate(cls, v: Any) -> Self:
         return SchemaValidator(schema=cls.schema).validate_python(v)
+
+
+@dataclass
+class NonNegativeDuration(timedelta):
+    schema: ClassVar = core_schema.timedelta_schema(ge=timedelta(seconds=0))
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable[..., Any], None, None]:
+        yield cls.validate
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: Callable[..., Any]
+    ) -> CoreSchema:
+        return cls.schema
+
+    @classmethod
+    def validate(cls, v: Any) -> Self:
+        return SchemaValidator(schema=cls.schema).validate_python(v)
+
+
+@dataclass
+class PositiveDuration(timedelta):
+    schema: ClassVar = core_schema.timedelta_schema(gt=timedelta(seconds=0))
+
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable[..., Any], None, None]:
+        yield cls.validate
+
+    @classmethod
+    def __get_pydantic_core_schema__(
+        cls, source_type: Any, handler: Callable[..., Any]
+    ) -> CoreSchema:
+        return cls.schema
+
+    @classmethod
+    def validate(cls, v: Any) -> Self:
+        return SchemaValidator(schema=cls.schema).validate_python(v)
+
+
+__all__ = [
+    "NonNegativeInteger",
+    "PositiveInteger",
+    "NonNegativeDuration",
+    "PositiveDuration",
+]
