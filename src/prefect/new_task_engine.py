d52f3dac6976adea505a3c87010d203dbfb3f1f7
Chris White <chris@prefect.io>
2024-04-17 15:59:39 -0700

    Stub out a new task run interface

--- src/prefect/new_task_engine.py
+++ src/prefect/new_task_engine.py
@@ -15,70 +15,43 @@ P = ParamSpec("P")
 R = TypeVar("R")
 
 
-class TaskRunContext(BaseModel):
-    value: str = "Running"
-    history: List[str] = Field(
-        default_factory=list, description="Convenience store for history of states"
-    )
-    retries: int = 0
+class TaskRun:
+    def __init__(self, task, parameters, task_run_id, client):
+        self.task = task
+        self.paramters = parameters
+        self.task_run_id = task_run_id
+        self.client = client
 
-    @property
-    def is_running(self) -> bool:
-        # This doesn't need to be a property, obviously.
-        return self.value not in ["Completed", "Failed"]
+    @classmethod
+    async def create(self, task, parameters):
+        """
+        Constructs a class and registers with the API.
+        """
+        pass
 
-    @property
-    def is_retrying(self) -> bool:
-        # This doesn't need to be a property, obviously.
-        return self.value in ["Retrying"]
+    async def start(self):
+        """
+        - check for a cached state
+        - sets state to running
+        - initialize task run logger
+        - update task run name
+        """
+        return self
+
+    async def is_running(self):
+        pass
+
+    async def handle_success(self, result):
+        pass
+
+    async def handle_exception(self, exc):
+        pass
 
     async def __aenter__(self: "Self") -> "Self":
         return self
 
     async def __aexit__(self, *args: Any) -> None:
         pass
-
-    def update(self, response: TaskRun) -> Self:
-        self.value: str = cast(str, response.state_name)
-        self.history = self.history + [self.value]
-        return self
-
-
-class Client:
-    """
-    This is a fake client that simulates the behavior of the Prefect API.
-    """
-
-    async def create_task_run(self, task: Task[P, Coroutine[Any, Any, R]]) -> TaskRun:
-        return TaskRun(
-            task_key=str(uuid4()), dynamic_key=str(uuid4()), state_name="Running"
-        )
-
-    async def set_failed(self, task_run: TaskRun, exc: Exception) -> TaskRun:
-        return TaskRun(
-            task_key=task_run.task_key,
-            dynamic_key=task_run.dynamic_key,
-            state_name="Failed",
-            state_info=str(exc),
-        )
-
-    async def set_retrying(self, task_run: TaskRun) -> TaskRun:
-        return TaskRun(
-            task_key=task_run.task_key,
-            dynamic_key=task_run.dynamic_key,
-            state_name="Retrying",
-        )
-
-    async def finalize_run(self, task_run: TaskRun, result: Any) -> TaskRun:
-        return TaskRun(
-            task_key=task_run.task_key,
-            dynamic_key=task_run.dynamic_key,
-            state_name="Completed",
-            result=result,
-        )
-
-
-client = Client()
 
 
 async def run_task(
@@ -89,38 +62,29 @@ async def run_task(
 ) -> R:
     """
     Runs a task against the API.
+
+    We will most likely want to use this logic as a wrapper and return a coroutine for type inference.
     """
 
     if not task_run:
         # If no task run is provided, create a new one.
-        task_run = await client.create_task_run(task)
+        task_run = TaskRun.create(task, parameters)
 
     result = cast(R, None)  # Ask me about this sync.
 
-    async with TaskRunContext() as state:
+    async with task_run.start() as state:
         # This is a context manager that keeps track of the state of the task run.
-        while state.is_running:
+        while state.is_running():
             try:
                 # This is where the task is actually run.
                 result = await task.fn(**parameters)  # type: ignore
 
-                # This is a random error that occurs 30% of the time.
-                if random.random() < 0.7:
-                    raise ValueError("This is a random error")
-
                 # If the task run is successful, finalize it.
-                state = state.update(await client.finalize_run(task_run, result))
+                await state.handle_success(result)
 
             except Exception as exc:
                 # If the task fails, and we have retries left, set the task to retrying.
-                if task.retries > state.retries:
-                    state = state.update(await client.set_retrying(task_run))
-                    state.retries += 1
-                    await asyncio.sleep(0)
-
-                # If the task fails, and we have no retries left, set the task to failed.
-                else:
-                    state = state.update(await client.set_failed(task_run, exc))
+                await state.handle_exception(exc)
 
     return result  # type: ignore
 
