50b13e78ef0360c870725da20a363b5196a1f21e
zzstoatzz <thrast36@gmail.com>
2024-04-19 12:41:47 -0500

    assume existing tag

--- scripts/create_tags_for_changed_collections.py
+++ scripts/create_tags_for_changed_collections.py
@@ -6,14 +6,12 @@ import sys
 from pathlib import Path
 from typing import Dict, List
 
-import tomli
-from gh_util.functions import (
-    create_repo_tag,  # pip install gh-util, set GITHUB_TOKEN env var
-)
+from gh_util.functions import create_repo_tag
 from packaging.version import Version
 
 INTEGRATIONS_BASEPATH = "src/integrations"
-INTEGRATIONS_VERSIONS_FILE = "src/integrations/versions.toml"
+OWNER = "prefecthq"
+REPO = "prefect"
 
 
 def get_changed_files(previous_tag: str, current_commit: str) -> List[str]:
@@ -27,13 +25,24 @@ def increment_patch_version(version: str) -> str:
     return f"{v.major}.{v.minor}.{v.micro + 1}"
 
 
+def get_latest_tag_version(integration_name: str) -> str:
+    cmd = (
+        f"git tag --list '*-{integration_name}' "
+        "--sort=-v:refname --format='%(refname:short)' | head -n1"
+    )
+    output = subprocess.check_output(cmd, shell=True, text=True)
+    tag_name = output.strip()
+    if tag_name:
+        return tag_name.split("-")[-1]
+
+    raise ValueError(f"Tag not found for {integration_name}")
+
+
 def get_changed_integrations(
     changed_files: List[str], glob_pattern: str
 ) -> Dict[str, str]:
     integrations_base_path = Path(INTEGRATIONS_BASEPATH)
     changed_integrations = {}
-    with open(INTEGRATIONS_VERSIONS_FILE, "rb") as f:
-        versions: Dict[str, str] = tomli.load(f).get("integrations", {})
     modified_integrations_files = [
         file_path
         for file_path in changed_files
@@ -43,18 +52,18 @@ def get_changed_integrations(
     for file_path in modified_integrations_files:
         path = Path(file_path)
         integration_name = path.parent.name
-        if integration_name in versions:
-            current_version = versions[integration_name]
-            next_version = increment_patch_version(current_version)
-            changed_integrations[integration_name] = next_version
+        current_version = get_latest_tag_version(integration_name)
+        changed_integrations[integration_name] = increment_patch_version(
+            current_version
+        )
     return changed_integrations
 
 
 async def create_tags(changed_integrations: Dict[str, str]) -> None:
     for integration_name, version in changed_integrations.items():
         await create_repo_tag(
-            owner="zzstoatzz",
-            repo="integration-monorepo",
+            owner=OWNER,
+            repo=REPO,
             tag_name=f"{integration_name}-{version}",
             commit_sha=os.environ.get("CURRENT_COMMIT", ""),
             message=f"Release {integration_name} {version}",
