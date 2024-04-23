52aad77d958728918742c6c10cca3b0faa48d043
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-04-23 16:36:43 -0500

    add automation filter name

--- src/prefect/server/events/filters.py
+++ src/prefect/server/events/filters.py
@@ -56,9 +56,13 @@ class AutomationFilterName(PrefectFilterBaseModel):
         filters = []
         if self.any_ is not None:
             filters.append(db.Automation.name.in_(self.any_))
+        return filters
 
 
 class AutomationFilter(PrefectOperatorFilterBaseModel):
+    name: Optional[AutomationFilterName] = Field(
+        default=None, description="Filter criteria for `Automation.name`"
+    )
     created: Optional[AutomationFilterCreated] = Field(
         default=None, description="Filter criteria for `Automation.created`"
     )
@@ -66,6 +70,8 @@ class AutomationFilter(PrefectOperatorFilterBaseModel):
     def _get_filter_list(self, db: PrefectDBInterface) -> List:
         filters = []
 
+        if self.name is not None:
+            filters.append(self.name.as_sql_filter(db))
         if self.created is not None:
             filters.append(self.created.as_sql_filter(db))
 
