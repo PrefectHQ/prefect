481466368fece3d55a31545318fd2a4819d0ca0b
zzstoatzz <thrast36@gmail.com>
2024-04-24 15:54:01 -0500

    add prefect test harness

--- src/integrations/prefect-ray/tests/conftest.py
+++ src/integrations/prefect-ray/tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
 from prefect.settings import PREFECT_ASYNC_FETCH_STATE_RESULT, temporary_settings
+from prefect.testing.utilities import prefect_test_harness
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prefect_db():
+    with prefect_test_harness():
+        yield
 
 
 @pytest.fixture(autouse=True)
