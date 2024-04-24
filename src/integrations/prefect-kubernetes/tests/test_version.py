904937f9e43d12fe078c9a2ca9856b6f70747414
Alex Streed <alex.s@prefect.io>
2024-04-24 14:34:56 -0500

    Copypasta bad

--- src/integrations/prefect-kubernetes/tests/test_version.py
+++ src/integrations/prefect-kubernetes/tests/test_version.py
@@ -2,7 +2,7 @@ from packaging.version import Version
 
 
 def test_version():
-    from prefect_shell import __version__
+    from prefect_kubernetes import __version__
 
     assert isinstance(__version__, str)
     assert Version(__version__)
