32a42d12a530b86c5884a9d1bfe86896b10ad8b6
Craig Harshbarger <pleek91@gmail.com>
2024-04-25 16:29:51 -0500

    Fix types and lint (#12902)

--- ui/src/maps/csrfToken.ts
+++ ui/src/maps/csrfToken.ts
@@ -1,12 +1,12 @@
-import { CsrfToken } from "@/models/CsrfToken"
-import { CsrfTokenResponse } from "@/types/csrfTokenResponse"
+import { CsrfToken } from '@/models/CsrfToken'
 import { MapFunction } from '@/services/mapper'
+import { CsrfTokenResponse } from '@/types/csrfTokenResponse'
 
 export const mapCsrfTokenResponseToCsrfToken: MapFunction<CsrfTokenResponse, CsrfToken> = function(source) {
-    return {
-        token: source.token,
-        expiration: this.map('string', source.expiration, 'Date'),
-        issued: new Date(),
-    }
+  return {
+    token: source.token,
+    expiration: this.map('string', source.expiration, 'Date'),
+    issued: new Date(),
+  }
 }
 
