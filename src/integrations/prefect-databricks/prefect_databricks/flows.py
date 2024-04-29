d638bd51580173a6de5ed9ee509698b07d632883
Constantino Schillebeeckx <ConstantinoSchillebeeckx@users.noreply.github.com>
2024-04-29 15:43:23 -0500

    Log full output of databricks job (#13151)
    Co-authored-by: nate nowack <thrast36@gmail.com>

--- src/integrations/prefect-databricks/prefect_databricks/flows.py
+++ src/integrations/prefect-databricks/prefect_databricks/flows.py
@@ -4,7 +4,7 @@ Module containing flows for interacting with Databricks
 
 import asyncio
 from logging import Logger
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from prefect import flow, get_run_logger
 from prefect_databricks import DatabricksCredentials
@@ -20,6 +20,9 @@ from prefect_databricks.models.jobs import (
     RunResultState,
     RunSubmitTaskSettings,
 )
+
+JobMetadata = Dict[str, Any]
+NotebookOutput = Dict[str, Any]
 
 
 class DatabricksJobTerminated(Exception):
@@ -65,8 +68,9 @@ async def jobs_runs_submit_and_wait_for_completion(
     timeout_seconds: Optional[int] = None,
     idempotency_token: Optional[str] = None,
     access_control_list: Optional[List[AccessControlRequest]] = None,
+    return_metadata: bool = False,
     **jobs_runs_submit_kwargs: Dict[str, Any],
-) -> Dict:
+) -> Union[NotebookOutput, Tuple[NotebookOutput, JobMetadata]]:
     """
     Flow that triggers a job run and waits for the triggered run to complete.
 
@@ -178,10 +182,16 @@ async def jobs_runs_submit_and_wait_for_completion(
         max_wait_seconds: Maximum number of seconds to wait for the entire flow to complete.
         poll_frequency_seconds: Number of seconds to wait in between checks for
             run completion.
+        return_metadata: When True, method will return a tuple of notebook output as well as
+            job run metadata; by default though, the method only returns notebook output
         **jobs_runs_submit_kwargs: Additional keyword arguments to pass to `jobs_runs_submit`.
 
     Returns:
-        A dictionary of task keys to its corresponding notebook output.
+        Either a dict or a tuple (depends on `return_metadata`) comprised of
+        * task_notebook_outputs: dictionary of task keys to its corresponding notebook output;
+          this is the only object returned by default from this method
+        * jobs_runs_metadata: dictionary containing IDs of the jobs runs tasks; this is only
+          returned if `return_metadata=True`.
 
     Examples:
         Submit jobs runs and wait.
@@ -290,6 +300,8 @@ async def jobs_runs_submit_and_wait_for_completion(
                 run_name,
                 multi_task_jobs_runs_id,
             )
+            if return_metadata:
+                return task_notebook_outputs, jobs_runs_metadata
             return task_notebook_outputs
         else:
             raise DatabricksJobTerminated(
