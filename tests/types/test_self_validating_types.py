bb1dd3b25ec80a876ce3f90a896e00b65bbde671
zzstoatzz <thrast36@gmail.com>
2024-05-02 18:42:59 -0500

    more economic test cases

--- tests/types/test_self_validating_types.py
+++ tests/types/test_self_validating_types.py
@@ -12,48 +12,60 @@ from prefect.types import (
 
 
 class TestConstrainedIntegers:
-    @pytest.mark.parametrize("integer_type", [PositiveInteger, NonNegativeInteger])
-    @pytest.mark.parametrize("value", [0, 1, 2, 3])
-    def test_valid_integer(self, integer_type, value):
+    @pytest.mark.parametrize(
+        "integer_type,valid_value",
+        [
+            (PositiveInteger, 1),
+            (NonNegativeInteger, 0),
+        ],
+    )
+    def test_valid_integer(self, integer_type, valid_value):
         class Model(BaseModel):
             value: integer_type
 
-        if integer_type == PositiveInteger and value == 0:
-            with pytest.raises(ValidationError):
-                Model(value=value)
-        else:
-            m = Model(value=value)
-            assert m.value == value
+        m = Model(value=valid_value)
+        assert m.value == valid_value
 
-    @pytest.mark.parametrize("integer_type", [PositiveInteger, NonNegativeInteger])
-    @pytest.mark.parametrize("value", [-1, -2, -3])
-    def test_invalid_negative_integer(self, integer_type, value):
+    @pytest.mark.parametrize(
+        "integer_type,invalid_value",
+        [
+            (PositiveInteger, 0),
+            (NonNegativeInteger, -1),
+        ],
+    )
+    def test_invalid_integer(self, integer_type, invalid_value):
         class Model(BaseModel):
             value: integer_type
 
         with pytest.raises(ValidationError):
-            Model(value=value)
+            Model(value=invalid_value)
 
 
 class TestConstrainedDurations:
-    @pytest.mark.parametrize("duration_type", [PositiveDuration, NonNegativeDuration])
-    @pytest.mark.parametrize("value", [0, 1, 2, 3])
-    def test_valid_duration(self, duration_type, value):
+    @pytest.mark.parametrize(
+        "duration_type,valid_value",
+        [
+            (PositiveDuration, timedelta(seconds=1)),
+            (NonNegativeDuration, timedelta(seconds=0)),
+        ],
+    )
+    def test_valid_duration(self, duration_type, valid_value):
         class Model(BaseModel):
             value: duration_type
 
-        if duration_type == PositiveDuration and value == 0:
-            with pytest.raises(ValidationError):
-                Model(value=value)
-        else:
-            m = Model(value=value)
-            assert m.value == timedelta(seconds=value)
+        m = Model(value=valid_value)
+        assert m.value == valid_value
 
-    @pytest.mark.parametrize("duration_type", [PositiveDuration, NonNegativeDuration])
-    @pytest.mark.parametrize("value", [-1, -2, -3])
-    def test_invalid_negative_duration(self, duration_type, value):
+    @pytest.mark.parametrize(
+        "duration_type,invalid_value",
+        [
+            (PositiveDuration, timedelta(seconds=0)),
+            (NonNegativeDuration, timedelta(seconds=-1)),
+        ],
+    )
+    def test_invalid_duration(self, duration_type, invalid_value):
         class Model(BaseModel):
             value: duration_type
 
         with pytest.raises(ValidationError):
-            Model(value=value)
+            Model(value=invalid_value)
