f49cd668fbe6b28a9c10b53d786b4d397209f85c
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-05-03 09:03:32 -0500

    Add `prefect global-concurrency-limit` command group and `ls` cmd (#13194)

--- src/prefect/server/schemas/responses.py
+++ src/prefect/server/schemas/responses.py
@@ -504,3 +504,22 @@ class WorkerResponse(schemas.core.Worker):
             worker.status = schemas.statuses.WorkerStatus.OFFLINE
 
         return worker
+
+
+class GlobalConcurrencyLimitResponse(ORMBaseModel):
+    """
+    A response object for global concurrency limits.
+    """
+
+    active: bool = Field(
+        default=True, description="Whether the global concurrency limit is active."
+    )
+    name: str = Field(
+        default=..., description="The name of the global concurrency limit."
+    )
+    limit: int = Field(default=..., description="The concurrency limit.")
+    active_slots: int = Field(default=..., description="The number of active slots.")
+    slot_decay_per_second: float = Field(
+        default=2.0,
+        description="The decay rate for active slots when used as a rate limit.",
+    )
