f97fcb25434b1e6e638d07b64b2320313607a9ea
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-05-03 09:03:32 -0500

    Add `prefect global-concurrency-limit` command group and `ls` cmd (#13194)

---
+++ tests/cli/test_global_concurrency_limit.py
@@ -0,0 +1,78 @@
+from typing import Generator, List
+from unittest import mock
+from uuid import UUID
+
+import pytest
+
+from prefect.client.schemas.objects import GlobalConcurrencyLimit
+from prefect.testing.cli import invoke_and_assert
+
+
+@pytest.fixture
+def read_global_concurrency_limits() -> Generator[mock.AsyncMock, None, None]:
+    with mock.patch(
+        "prefect.client.PrefectClient.read_global_concurrency_limits",
+    ) as m:
+        yield m
+
+
+def test_listing_gcl_empty(read_global_concurrency_limits: mock.AsyncMock):
+    read_global_concurrency_limits.return_value = []
+
+    invoke_and_assert(
+        ["global-concurrency-limit", "ls"],
+        expected_output="No global concurrency limits found.",
+    )
+
+
+@pytest.fixture
+def various_global_concurrency_limits(
+    read_global_concurrency_limits: mock.AsyncMock,
+) -> List[GlobalConcurrencyLimit]:
+    global_concurrency_limits = [
+        GlobalConcurrencyLimit(
+            id=UUID("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa"),
+            created="2021-01-01T00:00:00Z",
+            updated="2021-01-01T00:00:00Z",
+            active=True,
+            name="here-the-buck-stops",
+            limit=1,
+            active_slots=1,
+            slot_decay_per_second=0.1,
+        ),
+        GlobalConcurrencyLimit(
+            id=UUID("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbbb"),
+            created="2021-01-01T00:00:00Z",
+            updated="2021-01-01T00:00:00Z",
+            active=True,
+            name="there-the-buck-stops",
+            limit=2,
+            active_slots=2,
+            slot_decay_per_second=0.1,
+        ),
+        GlobalConcurrencyLimit(
+            id=UUID("cccccccc-cccc-cccc-cccc-cccccccccccc"),
+            created="2021-01-01T00:00:00Z",
+            updated="2021-01-01T00:00:00Z",
+            active=True,
+            name="everywhere-the-buck-stops",
+            limit=3,
+            active_slots=3,
+            slot_decay_per_second=0.1,
+        ),
+    ]
+    read_global_concurrency_limits.return_value = global_concurrency_limits
+    return global_concurrency_limits
+
+
+def test_listing_gcl(various_global_concurrency_limits: List[GlobalConcurrencyLimit]):
+    invoke_and_assert(
+        ["global-concurrency-limit", "ls"],
+        expected_output_contains=(
+            # name check is truncated during tests as we can't match the full name without changing the width of the column
+            str(various_global_concurrency_limits[0].name[:14]),
+            str(various_global_concurrency_limits[1].name[:14]),
+            str(various_global_concurrency_limits[2].name[:14]),
+        ),
+        expected_code=0,
+    )
