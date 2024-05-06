d3f24085565bf0439c371196b777d4d94e540025
nate nowack <thrast36@gmail.com>
2024-05-06 11:24:44 -0500

    remove `dataclass` from custom constrained types (#13257)

--- src/prefect/types/__init__.py
+++ src/prefect/types/__init__.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Any, Callable, ClassVar, Generator
 
 from pydantic_core import core_schema, CoreSchema, SchemaValidator
@@ -6,7 +5,6 @@ from typing_extensions import Self
 from datetime import timedelta
 
 
-@dataclass
 class NonNegativeInteger(int):
     schema: ClassVar[CoreSchema] = core_schema.int_schema(ge=0)
 
@@ -25,7 +23,6 @@ class NonNegativeInteger(int):
         return SchemaValidator(schema=cls.schema).validate_python(v)
 
 
-@dataclass
 class PositiveInteger(int):
     schema: ClassVar[CoreSchema] = core_schema.int_schema(gt=0)
 
@@ -44,7 +41,6 @@ class PositiveInteger(int):
         return SchemaValidator(schema=cls.schema).validate_python(v)
 
 
-@dataclass
 class NonNegativeFloat(float):
     schema: ClassVar[CoreSchema] = core_schema.float_schema(ge=0)
 
@@ -63,7 +59,6 @@ class NonNegativeFloat(float):
         return SchemaValidator(schema=cls.schema).validate_python(v)
 
 
-@dataclass
 class NonNegativeDuration(timedelta):
     schema: ClassVar = core_schema.timedelta_schema(ge=timedelta(seconds=0))
 
@@ -82,7 +77,6 @@ class NonNegativeDuration(timedelta):
         return SchemaValidator(schema=cls.schema).validate_python(v)
 
 
-@dataclass
 class PositiveDuration(timedelta):
     schema: ClassVar = core_schema.timedelta_schema(gt=timedelta(seconds=0))
 
