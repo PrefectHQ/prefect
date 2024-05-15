f5be3ff315f876408be81c0a2cc3903c1b3a6481
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-05-15 13:22:01 -0500

    do the same for deployment_storage

--- flows/deployment_storage.py
+++ flows/deployment_storage.py
@@ -7,7 +7,6 @@ import anyio
 from packaging.version import Version
 
 import prefect
-from prefect.deployments import Deployment
 from prefect.filesystems import LocalFileSystem
 
 # The version oldest version this test runs with
@@ -47,38 +46,41 @@ async def read_flow_run(flow_run_id):
 
 
 def main():
-    # We must create an ignore file
-    file = Path(".prefectignore")
-    if not file.exists():
-        file.touch()
+    if Version(prefect.__version__) <= Version("2.19.0"):
+        from prefect.deployments import Deployment
 
-    # Create deployment
-    deployment = Deployment.build_from_flow(
-        flow=hello,
-        name="test-deployment",
-        storage=LocalFileSystem(basepath="/tmp/integration-flows/storage"),
-        path=None,
-    )
-    deployment_id = deployment.apply()
+        # We must create an ignore file
+        file = Path(".prefectignore")
+        if not file.exists():
+            file.touch()
 
-    # Create a flow run
-    flow_run = anyio.run(create_flow_run, deployment_id)
+        # Create deployment
+        deployment = Deployment.build_from_flow(
+            flow=hello,
+            name="test-deployment",
+            storage=LocalFileSystem(basepath="/tmp/integration-flows/storage"),
+            path=None,
+        )
+        deployment_id = deployment.apply()
 
-    os.makedirs("/tmp/integration-flows/execution", exist_ok=True)
+        # Create a flow run
+        flow_run = anyio.run(create_flow_run, deployment_id)
 
-    env = os.environ.copy()
-    env["PREFECT__FLOW_RUN_ID"] = str(flow_run.id)
-    subprocess.check_call(
-        [sys.executable, "-m", "prefect.engine"],
-        env=env,
-        timeout=30,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-        cwd="/tmp/integration-flows/execution",
-    )
+        os.makedirs("/tmp/integration-flows/execution", exist_ok=True)
 
-    flow_run = anyio.run(read_flow_run, flow_run.id)
-    assert flow_run.state.is_completed(), flow_run.state
+        env = os.environ.copy()
+        env["PREFECT__FLOW_RUN_ID"] = str(flow_run.id)
+        subprocess.check_call(
+            [sys.executable, "-m", "prefect.engine"],
+            env=env,
+            timeout=30,
+            stdout=sys.stdout,
+            stderr=sys.stderr,
+            cwd="/tmp/integration-flows/execution",
+        )
+
+        flow_run = anyio.run(read_flow_run, flow_run.id)
+        assert flow_run.state.is_completed(), flow_run.state
 
 
 if __name__ == "__main__":
