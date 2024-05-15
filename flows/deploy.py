6d7a7fba0733b9f031bcb9aadc42d32d3fbfc96b
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-05-15 15:17:49 -0500

    uncomment out deploy

--- flows/deploy.py
+++ flows/deploy.py
@@ -1,59 +1,59 @@
-# import os
-# import subprocess
-# import sys
+import os
+import subprocess
+import sys
 
-# from packaging.version import Version
+from packaging.version import Version
 
-# import prefect
-# import prefect.flow_runs
+import prefect
+import prefect.flow_runs
 
-# # The version oldest version this test runs with
-# SUPPORTED_VERSION = "2.6.0"
-
-
-# if Version(prefect.__version__) < Version(SUPPORTED_VERSION):
-#     raise NotImplementedError()
+# The version oldest version this test runs with
+SUPPORTED_VERSION = "2.6.0"
 
 
-# async def main():
-#     if Version(prefect.__version__) >= Version("2.19.0"):
-#         try:
-#             TEST_SERVER_VERSION = os.environ.get(
-#                 "TEST_SERVER_VERSION", prefect.__version__
-#             )
-#             # Work pool became GA in 2.8.0
-#             if Version(prefect.__version__) >= Version("2.8") and Version(
-#                 TEST_SERVER_VERSION
-#             ) >= Version("2.8"):
-#                 subprocess.check_call(
-#                     ["prefect", "work-pool", "create", "test-pool", "-t", "process"],
-#                     stdout=sys.stdout,
-#                     stderr=sys.stderr,
-#                 )
-
-#                 flow_instance = await prefect.flow.from_source(
-#                     source="https://github.com/PrefectHQ/prefect-recipes.git",
-#                     entrypoint="flows-starter/hello.py:hello",
-#                 )
-
-#                 await flow_instance.deploy(
-#                     name="demo-deployment",
-#                     work_pool_name="test-pool",
-#                 )
-
-#                 flow_run_state = flow_instance._run("demo-deployment")
-
-#                 assert flow_run_state.is_completed(), flow_run_state
-
-#         finally:
-#             subprocess.check_call(
-#                 ["prefect", "work-pool", "delete", "test-pool"],
-#                 stdout=sys.stdout,
-#                 stderr=sys.stderr,
-#             )
+if Version(prefect.__version__) < Version(SUPPORTED_VERSION):
+    raise NotImplementedError()
 
 
-# if __name__ == "__main__":
-#     import asyncio
+async def main():
+    if Version(prefect.__version__) >= Version("2.19.0"):
+        try:
+            TEST_SERVER_VERSION = os.environ.get(
+                "TEST_SERVER_VERSION", prefect.__version__
+            )
+            # Work pool became GA in 2.8.0
+            if Version(prefect.__version__) >= Version("2.8") and Version(
+                TEST_SERVER_VERSION
+            ) >= Version("2.8"):
+                subprocess.check_call(
+                    ["prefect", "work-pool", "create", "test-pool", "-t", "process"],
+                    stdout=sys.stdout,
+                    stderr=sys.stderr,
+                )
 
-#     asyncio.run(main())
+                flow_instance = await prefect.flow.from_source(
+                    source="https://github.com/PrefectHQ/prefect-recipes.git",
+                    entrypoint="flows-starter/hello.py:hello",
+                )
+
+                await flow_instance.deploy(
+                    name="demo-deployment",
+                    work_pool_name="test-pool",
+                )
+
+                flow_run_state = flow_instance._run("demo-deployment")
+
+                assert flow_run_state.is_completed(), flow_run_state
+
+        finally:
+            subprocess.check_call(
+                ["prefect", "work-pool", "delete", "test-pool"],
+                stdout=sys.stdout,
+                stderr=sys.stderr,
+            )
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(main())
