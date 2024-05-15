d7c446e7c51319425063fb7408735bedffeb0f9e
Alexander Streed <desertaxle@users.noreply.github.com>
2024-05-15 10:31:53 -0500

    Remove `prefect agent` CLI from `main` (#13365)

--- src/prefect/cli/__init__.py
+++ src/prefect/cli/__init__.py
@@ -5,7 +5,6 @@ from prefect.cli.root import app
 # Import CLI submodules to register them to the app
 # isort: split
 
-import prefect.cli.agent
 import prefect.cli.artifact
 import prefect.cli.block
 import prefect.cli.cloud
