95896d393c411ba3f04aff975f8b643e1b4e3944
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-05-15 15:09:56 -0500

    update min version for deploy

--- flows/deploy.py
+++ flows/deploy.py
@@ -16,40 +16,42 @@ if Version(prefect.__version__) < Version(SUPPORTED_VERSION):
 
 
 async def main():
-    # if Version(prefect.__version__) >= Version("2.19.0"):
-    # tk: let's just see where this fails in compatibility and set version then
-    try:
-        TEST_SERVER_VERSION = os.environ.get("TEST_SERVER_VERSION", prefect.__version__)
-        # Work pool became GA in 2.8.0
-        if Version(prefect.__version__) >= Version("2.8") and Version(
-            TEST_SERVER_VERSION
-        ) >= Version("2.8"):
+    if Version(prefect.__version__) >= Version("2.10.0"):
+        # tk: let's just see where this fails in compatibility and set version then
+        try:
+            TEST_SERVER_VERSION = os.environ.get(
+                "TEST_SERVER_VERSION", prefect.__version__
+            )
+            # Work pool became GA in 2.8.0
+            if Version(prefect.__version__) >= Version("2.8") and Version(
+                TEST_SERVER_VERSION
+            ) >= Version("2.8"):
+                subprocess.check_call(
+                    ["prefect", "work-pool", "create", "test-pool", "-t", "process"],
+                    stdout=sys.stdout,
+                    stderr=sys.stderr,
+                )
+
+                flow_instance = await prefect.flow.from_source(
+                    source="https://github.com/PrefectHQ/prefect-recipes.git",
+                    entrypoint="flows-starter/hello.py:hello",
+                )
+
+                await flow_instance.deploy(
+                    name="demo-deployment",
+                    work_pool_name="test-pool",
+                )
+
+                flow_run_state = flow_instance._run("demo-deployment")
+
+                assert flow_run_state.is_completed(), flow_run_state
+
+        finally:
             subprocess.check_call(
-                ["prefect", "work-pool", "create", "test-pool", "-t", "process"],
+                ["prefect", "work-pool", "delete", "test-pool"],
                 stdout=sys.stdout,
                 stderr=sys.stderr,
             )
-
-            flow_instance = await prefect.flow.from_source(
-                source="https://github.com/PrefectHQ/prefect-recipes.git",
-                entrypoint="flows-starter/hello.py:hello",
-            )
-
-            await flow_instance.deploy(
-                name="demo-deployment",
-                work_pool_name="test-pool",
-            )
-
-            flow_run_state = flow_instance._run("demo-deployment")
-
-            assert flow_run_state.is_completed(), flow_run_state
-
-    finally:
-        subprocess.check_call(
-            ["prefect", "work-pool", "delete", "test-pool"],
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-        )
 
 
 if __name__ == "__main__":
