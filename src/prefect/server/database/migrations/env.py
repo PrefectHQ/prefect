d7595ccb05c2cf8740645e656d4bfc1bc5291b45
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-05-18 20:37:51 -0400

    Revert "Update env.py"
    This reverts commit 870f8b060a220c2724a711ecc32e9a779972e758.

--- src/prefect/server/database/migrations/env.py
+++ src/prefect/server/database/migrations/env.py
@@ -1,7 +1,6 @@
 # Originally generated from `alembic init`
 # https://alembic.sqlalchemy.org/en/latest/tutorial.html#creating-an-environment
 
-import asyncio
 import contextlib
 
 import sqlalchemy
@@ -11,6 +10,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from prefect.server.database.configurations import SQLITE_BEGIN_MODE
 from prefect.server.database.dependencies import provide_database_interface
 from prefect.server.utilities.database import get_dialect
+from prefect.utilities.asyncutils import sync_compatible
 
 db_interface = provide_database_interface()
 config = context.config
@@ -157,6 +157,7 @@ def disable_sqlite_foreign_keys(context):
         context.execute("BEGIN IMMEDIATE")
 
 
+@sync_compatible
 async def apply_migrations() -> None:
     """
     Apply migrations to the database.
@@ -171,4 +172,4 @@ async def apply_migrations() -> None:
 if context.is_offline_mode():
     dry_run_migrations()
 else:
-    asyncio.run(apply_migrations())
+    apply_migrations()
