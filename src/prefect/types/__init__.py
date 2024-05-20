f055b1159d4cceecee6b27c3f91c2bc742d62a5c
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-05-20 14:49:32 -0400

    add TimeZone str type

--- src/prefect/types/__init__.py
+++ src/prefect/types/__init__.py
@@ -2,6 +2,9 @@ from typing import Annotated
 
 from pydantic import Field
 from datetime import timedelta
+from zoneinfo import available_timezones
+
+timezone_set = available_timezones()
 
 
 NonNegativeInteger = Annotated[int, Field(ge=0)]
@@ -16,6 +19,8 @@ NonNegativeFloat = Annotated[float, Field(ge=0.0)]
 NonNegativeDuration = Annotated[timedelta, Field(ge=0)]
 
 PositiveDuration = Annotated[timedelta, Field(gt=0)]
+
+TimeZone = Annotated[str, Field(default="UTC", pattern="|".join(timezone_set))]
 
 __all__ = [
     "NonNegativeInteger",
