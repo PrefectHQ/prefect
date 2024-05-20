ee5ce391b94e042df088113a37af2fe81e6a32f7
Alex Streed <alex.s@prefect.io>
2024-05-20 08:07:05 -0500

    Revert changes

--- src/prefect/server/database/migrations/env.py
+++ src/prefect/server/database/migrations/env.py
@@ -10,7 +10,7 @@ from sqlalchemy.ext.asyncio import AsyncEngine
 from prefect.server.database.configurations import SQLITE_BEGIN_MODE
 from prefect.server.database.dependencies import provide_database_interface
 from prefect.server.utilities.database import get_dialect
-from prefect.utilities.asyncutils import run_sync
+from prefect.utilities.asyncutils import sync_compatible
 
 db_interface = provide_database_interface()
 config = context.config
@@ -157,6 +157,7 @@ def disable_sqlite_foreign_keys(context):
         context.execute("BEGIN IMMEDIATE")
 
 
+@sync_compatible
 async def apply_migrations() -> None:
     """
     Apply migrations to the database.
@@ -171,4 +172,4 @@ async def apply_migrations() -> None:
 if context.is_offline_mode():
     dry_run_migrations()
 else:
-    run_sync(apply_migrations())
+    apply_migrations()
