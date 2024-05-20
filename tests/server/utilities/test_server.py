b289cbcf473c767060f7e868f0f8ee327f01af1b
Chris Guidry <chris.g@prefect.io>
2024-05-20 14:08:12 -0400

    Removing another scoped dependency test.

--- tests/server/utilities/test_server.py
+++ tests/server/utilities/test_server.py
@@ -2,42 +2,11 @@ import urllib.parse
 
 import pytest
 from fastapi import (
-    Depends,
     FastAPI,
-    HTTPException,
 )
 from fastapi.testclient import TestClient
 
 from prefect.server.utilities.server import PrefectRouter
-
-
-def test_request_scoped_dependency_cannot_raise_after_yield():
-    # Converse to the above test. This does not cover any of our code, just FastAPI
-    # behavior.
-    async def test():
-        yield
-        raise HTTPException(status_code=202)
-
-    app = FastAPI()
-    router = PrefectRouter()
-
-    @router.get("/")
-    def foo(
-        x=Depends(test),
-    ):
-        pass
-
-    app.include_router(router)
-
-    client = TestClient(app)
-
-    # In newer FastAPI versions, FastAPI raises a runtime error complaining about
-    # an exception during the response
-    expected_type = RuntimeError
-    match = "Caught handled exception, but response already started"
-
-    with pytest.raises(expected_type, match=match):
-        client.get("/")
 
 
 class TestParsing:
