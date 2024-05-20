485a985839331236846de37b0a8c80003d8d9fc2
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-05-20 13:46:51 -0500

    migrate remaining types to annotated (#13473)

--- src/prefect/types/__init__.py
+++ src/prefect/types/__init__.py
@@ -1,8 +1,6 @@
-from typing import Annotated, Any, Callable, ClassVar, Generator
+from typing import Annotated
 
-from pydantic_core import core_schema, CoreSchema, SchemaValidator
 from pydantic import Field
-from typing_extensions import Self
 from datetime import timedelta
 
 
@@ -15,29 +13,9 @@ PositiveInteger = Annotated[int, Field(gt=0)]
 NonNegativeFloat = Annotated[float, Field(ge=0.0)]
 
 
-class NonNegativeDuration(timedelta):
-    """A timedelta that must be greater than or equal to 0."""
+NonNegativeDuration = Annotated[timedelta, Field(ge=0)]
 
-    schema: ClassVar = core_schema.timedelta_schema(ge=timedelta(seconds=0))
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls, source_type: Any, handler: Callable[..., Any]
-    ) -> CoreSchema:
-        return cls.schema
-
-
-class PositiveDuration(timedelta):
-    """A timedelta that must be greater than 0."""
-
-    schema: ClassVar = core_schema.timedelta_schema(gt=timedelta(seconds=0))
-
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls, source_type: Any, handler: Callable[..., Any]
-    ) -> CoreSchema:
-        return cls.schema
-
+PositiveDuration = Annotated[timedelta, Field(gt=0)]
 
 __all__ = [
     "NonNegativeInteger",
