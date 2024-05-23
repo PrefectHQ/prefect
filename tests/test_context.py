237f4bebbf15747da435320db0f32c551c9391d2
Alexander Streed <desertaxle@users.noreply.github.com>
2024-05-23 08:07:57 -0500

    Add utilities to serialize context to a dictionary and hydrate context from a dictionary (#13529)

--- tests/test_context.py
+++ tests/test_context.py
@@ -13,13 +13,18 @@ from prefect.context import (
     ContextModel,
     FlowRunContext,
     SettingsContext,
+    TagsContext,
     TaskRunContext,
     get_run_context,
     get_settings_context,
+    hydrated_context,
     root_settings_context,
+    serialize_context,
+    tags,
     use_profile,
 )
 from prefect.exceptions import MissingContextError
+from prefect.new_task_runners import ThreadPoolTaskRunner
 from prefect.results import ResultFactory
 from prefect.settings import (
     DEFAULT_PROFILES_PATH,
@@ -394,3 +399,201 @@ class TestSettingsContext:
             result = portal.call(get_settings_context)
 
         assert result == GLOBAL_SETTINGS_CONTEXT
+
+
+class TestSerializeContext:
+    def test_empty(self):
+        serialized = serialize_context()
+        assert serialized == {
+            "flow_run_context": {},
+            "task_run_context": {},
+            "tags_context": {},
+            "settings_context": SettingsContext.get().serialize(),
+        }
+
+    async def test_with_flow_run_context(self, prefect_client):
+        @flow
+        def foo():
+            pass
+
+        test_task_runner = ThreadPoolTaskRunner()
+        flow_run = await prefect_client.create_flow_run(foo)
+        result_factory = await ResultFactory.from_flow(foo)
+
+        async with anyio.create_task_group() as task_group:
+            with FlowRunContext(
+                flow=foo,
+                flow_run=flow_run,
+                client=prefect_client,
+                task_runner=test_task_runner,
+                result_factory=result_factory,
+                background_tasks=task_group,
+                parameters={"x": "y"},
+            ) as flow_run_context:
+                serialized = serialize_context()
+                assert serialized == {
+                    "flow_run_context": flow_run_context.serialize(),
+                    "task_run_context": {},
+                    "tags_context": {},
+                    "settings_context": SettingsContext.get().serialize(),
+                }
+
+    async def test_with_task_run_context(self, prefect_client, flow_run):
+        @task
+        def bar():
+            pass
+
+        task_run = await prefect_client.create_task_run(
+            bar, flow_run.id, dynamic_key=""
+        )
+
+        with TaskRunContext(
+            task=bar,
+            task_run=task_run,
+            client=prefect_client,
+            result_factory=await ResultFactory.from_task(bar, client=prefect_client),
+            parameters={"foo": "bar"},
+        ) as task_ctx:
+            serialized = serialize_context()
+            assert serialized == {
+                "flow_run_context": {},
+                "task_run_context": task_ctx.serialize(),
+                "tags_context": {},
+                "settings_context": SettingsContext.get().serialize(),
+            }
+
+    def test_with_tags_context(self):
+        with tags("a", "b") as current_tags:
+            serialized = serialize_context()
+            assert serialized == {
+                "flow_run_context": {},
+                "task_run_context": {},
+                "tags_context": {"current_tags": current_tags},
+                "settings_context": SettingsContext.get().serialize(),
+            }
+
+    def test_with_multiple_contexts(self):
+        with tags("a", "b") as current_tags:
+            with temporary_settings(
+                updates={PREFECT_API_KEY: "test", PREFECT_API_URL: "test"}
+            ):
+                serialized = serialize_context()
+                assert serialized == {
+                    "flow_run_context": {},
+                    "task_run_context": {},
+                    "tags_context": {"current_tags": current_tags},
+                    "settings_context": SettingsContext.get().serialize(),
+                }
+                assert (
+                    serialized["settings_context"]["settings"]["PREFECT_API_KEY"]
+                    == "test"
+                )
+                assert (
+                    serialized["settings_context"]["settings"]["PREFECT_API_URL"]
+                    == "test"
+                )
+
+
+class TestHydratedContext:
+    def test_empty(self):
+        initial_settings_context = SettingsContext.get()
+        with hydrated_context({}):
+            assert FlowRunContext.get() is None
+            assert TaskRunContext.get() is None
+            assert TagsContext.get().current_tags == set()
+            assert SettingsContext.get() == initial_settings_context
+
+    async def test_with_flow_run_context(self, prefect_client):
+        @flow
+        def foo():
+            pass
+
+        test_task_runner = ThreadPoolTaskRunner()
+        flow_run = await prefect_client.create_flow_run(foo)
+        result_factory = await ResultFactory.from_flow(foo)
+        async with anyio.create_task_group() as task_group:
+            flow_run_context = FlowRunContext(
+                flow=foo,
+                flow_run=flow_run,
+                client=prefect_client,
+                task_runner=test_task_runner,
+                result_factory=result_factory,
+                background_tasks=task_group,
+                parameters={"x": "y"},
+            )
+
+        with hydrated_context(
+            {
+                "flow_run_context": flow_run_context.serialize(),
+            }
+        ):
+            hydrated_flow_run_context = FlowRunContext.get()
+            assert hydrated_flow_run_context.flow is foo
+            assert hydrated_flow_run_context.flow_run == flow_run
+            assert (
+                hydrated_flow_run_context.client is not None
+            )  # this won't be the same object as the original client
+            assert (
+                hydrated_flow_run_context.task_runner is not None
+            )  # this won't be the same object as the original task runner
+            assert (
+                hydrated_flow_run_context.result_factory is not None
+            )  # this won't be the same object as the original result factory
+            assert (
+                hydrated_flow_run_context.background_tasks is not None
+            )  # this won't be the same object as the original background tasks
+            assert isinstance(hydrated_flow_run_context.start_time, DateTime)
+            assert hydrated_flow_run_context.parameters == {"x": "y"}
+
+    async def test_with_task_run_context(self, prefect_client, flow_run):
+        @task
+        def bar():
+            pass
+
+        task_run = await prefect_client.create_task_run(
+            bar, flow_run.id, dynamic_key=""
+        )
+        task_ctx = TaskRunContext(
+            task=bar,
+            task_run=task_run,
+            client=prefect_client,
+            result_factory=await ResultFactory.from_task(bar, client=prefect_client),
+            parameters={"foo": "bar"},
+        )
+
+        with hydrated_context(
+            {
+                "task_run_context": task_ctx.serialize(),
+            }
+        ):
+            hydrated_task_ctx = TaskRunContext.get()
+            assert hydrated_task_ctx.task is bar
+            assert hydrated_task_ctx.task_run == task_run
+            assert hydrated_task_ctx.result_factory is not None
+            assert isinstance(hydrated_task_ctx.start_time, DateTime)
+            assert hydrated_task_ctx.parameters == {"foo": "bar"}
+
+    def test_with_tags_context(self):
+        with hydrated_context(
+            {
+                "tags_context": {"current_tags": {"a", "b"}},
+            }
+        ):
+            assert TagsContext.get().current_tags == {"a", "b"}
+
+    def test_with_settings_context(self):
+        with hydrated_context(
+            {
+                "settings_context": {
+                    "profile": {"name": "test", "settings": {}, "source": None},
+                    "settings": {"PREFECT_API_KEY": "test", "PREFECT_API_URL": "test"},
+                },
+            }
+        ):
+            assert SettingsContext.get().profile == Profile(
+                name="test",
+                settings={},
+                source=None,
+            )
+            assert SettingsContext.get().settings.PREFECT_API_KEY == "test"
+            assert SettingsContext.get().settings.PREFECT_API_URL == "test"
