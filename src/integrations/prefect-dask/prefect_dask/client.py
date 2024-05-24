256371151dd960a0cfb650d10a3880962ecf3a0d
Alex Streed <alex.s@prefect.io>
2024-05-24 07:01:27 -0500

    Unskip utils tests

--- src/integrations/prefect-dask/prefect_dask/client.py
+++ src/integrations/prefect-dask/prefect_dask/client.py
@@ -1,9 +1,11 @@
+import asyncio
+from functools import wraps
 from uuid import uuid4
 
 from distributed import Client, Future
 
 from prefect.context import serialize_context
-from prefect.new_task_engine import run_task
+from prefect.new_task_engine import run_task_async, run_task_sync
 from prefect.tasks import Task
 from prefect.utilities.callables import get_call_parameters
 from prefect.utilities.engine import collect_task_run_inputs_sync
@@ -30,7 +32,7 @@ class PrefectDistributedClient(Client):
             task_run_id = uuid4()
             context = serialize_context()
 
-            if not (parameters := kwargs.get("parameters")):
+            if (parameters := kwargs.get("parameters")) is None:
                 # If parameters are not provided, we need to extract them from the function.
                 # This case is when the PrefectDistributedClient is used directly without
                 # the DaskTaskRunner.
@@ -45,25 +47,51 @@ class PrefectDistributedClient(Client):
                     for k, v in dependencies.items()
                 }
 
-            future = super().submit(
-                run_task,
-                key=key,
-                workers=workers,
-                resources=resources,
-                retries=retries,
-                priority=priority,
-                fifo_timeout=fifo_timeout,
-                allow_other_workers=allow_other_workers,
-                actor=actor,
-                actors=actors,
-                pure=pure,
-                task=func,
-                context=context,
-                task_run_id=task_run_id,
-                wait_for=kwargs.get("wait_for"),
-                parameters=parameters,
-                dependencies=dependencies,
-            )
+            if func.isasync:
+
+                @wraps(run_task_sync)
+                def wrapper_func(*args, **kwargs):
+                    return asyncio.run(run_task_async(*args, **kwargs))
+
+                future = super().submit(
+                    wrapper_func,
+                    key=key,
+                    workers=workers,
+                    resources=resources,
+                    retries=retries,
+                    priority=priority,
+                    fifo_timeout=fifo_timeout,
+                    allow_other_workers=allow_other_workers,
+                    actor=actor,
+                    actors=actors,
+                    pure=pure,
+                    task=func,
+                    context=context,
+                    task_run_id=task_run_id,
+                    wait_for=kwargs.get("wait_for"),
+                    parameters=parameters,
+                    dependencies=dependencies,
+                )
+            else:
+                future = super().submit(
+                    run_task_sync,
+                    key=key,
+                    workers=workers,
+                    resources=resources,
+                    retries=retries,
+                    priority=priority,
+                    fifo_timeout=fifo_timeout,
+                    allow_other_workers=allow_other_workers,
+                    actor=actor,
+                    actors=actors,
+                    pure=pure,
+                    task=func,
+                    context=context,
+                    task_run_id=task_run_id,
+                    wait_for=kwargs.get("wait_for"),
+                    parameters=parameters,
+                    dependencies=dependencies,
+                )
             future.task_run_id = task_run_id
             return future
         else:
