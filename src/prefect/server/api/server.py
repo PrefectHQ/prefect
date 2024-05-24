ff7f6702eaada883d3902f787bbf5c9228c2b77a
Zach Angell <42625717+zangell44@users.noreply.github.com>
2024-05-24 10:05:46 -0400

    Add flow ui endpoints (#13544)

--- src/prefect/server/api/server.py
+++ src/prefect/server/api/server.py
@@ -91,6 +91,7 @@ API_ROUTERS = (
     api.events.router,
     api.automations.router,
     api.templates.router,
+    api.ui.flows.router,
     api.ui.flow_runs.router,
     api.ui.schemas.router,
     api.ui.task_runs.router,
