23d6e0fd14d87f6ab0a5d91332b339d6548365fd
Chris White <chris@prefect.io>
2024-05-26 10:53:45 -0700

    Add stub of result-store record-store

---
+++ src/prefect/records/result_store.py
@@ -0,0 +1,21 @@
+from .store import RecordStore
+
+from prefect.results import BaseResult, PersistedResult, ResultFactory
+
+
+class ResultFactoryStore(RecordStore):
+    result_factory: ResultFactory
+
+    def exists(self, key: str) -> bool:
+        return False
+
+    def read(self, key: str) -> BaseResult:
+        result = PersistedResult(
+            serializer_type=self.result_factory.serializer.type,
+            storage_block_id=self.result_factory.storage_block_id,
+            storage_key=key,
+        )
+        return result
+
+    def write(self, key: str, value: dict) -> BaseResult:
+        return self.result_factory.create_result(obj=value, key=key)
