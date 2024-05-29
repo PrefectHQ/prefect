92f815a44693e68289467bd97fd748f0f1b07878
Collin Choy <collin@prefect.io>
2024-05-29 15:27:45 -0400

    Add handler to submit form to getform.io.

--- ui/src/components/JoinTheCommunityModal.vue
+++ ui/src/components/JoinTheCommunityModal.vue
@@ -17,11 +17,15 @@
 
       <p-divider class="-my-3" />
 
-      <p-form :id="formId" @submit="console.dir($event)">
+      <p-form :id="formId" @submit="signUpForEmailUpdates">
         <p-label v-slot="{ id }" label="Notify me about Prefect updates">
           <p-text-input :id v-model="email" placeholder="hello@prefect.io" type="email" required />
         </p-label>
       </p-form>
+
+      <p-message v-if="error" error>
+        {{ error }}
+      </p-message>
     </template>
 
     <template #cancel="scope">
@@ -31,7 +35,7 @@
     </template>
 
     <template #actions>
-      <p-button primary type="submit" :form="formId">
+      <p-button primary type="submit" :form="formId" :loading>
         Sign up
       </p-button>
     </template>
@@ -45,4 +49,31 @@
 
   const formId = 'join-the-community-modal'
   const email = ref<string>()
+
+  const loading = ref(false)
+  const error = ref('')
+
+  const formEndpoint = 'https://getform.io/f/eapderva'
+  async function signUpForEmailUpdates(): Promise<void> {
+    error.value = ''
+    loading.value = true
+    try {
+      const response = await fetch(formEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email.value }),
+        // getform redirects to a thank-you page so this cancels that additional request
+        redirect: 'manual',
+      })
+
+      console.dir(response)
+    } catch (err) {
+      error.value = 'An error occurred. Please try again.'
+      console.error(err)
+    } finally {
+      loading.value = false
+    }
+  }
 </script>
