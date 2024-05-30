eede0c51668bd0a731de602b9f53c3184f3628ec
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-05-30 10:22:41 -0500

    migrate `prefect-gitlab` to pydantic v2 (#13656)

--- src/integrations/prefect-gitlab/prefect_gitlab/credentials.py
+++ src/integrations/prefect-gitlab/prefect_gitlab/credentials.py
@@ -1,14 +1,11 @@
 """Module used to enable authenticated interactions with GitLab"""
 
+from typing import Optional
+
 from gitlab import Gitlab
-from pydantic import VERSION as PYDANTIC_VERSION
+from pydantic import Field, SecretStr
 
 from prefect.blocks.core import Block
-
-if PYDANTIC_VERSION.startswith("2."):
-    from pydantic.v1 import Field, HttpUrl, SecretStr
-else:
-    from pydantic import Field, HttpUrl, SecretStr
 
 
 class GitLabCredentials(Block):
@@ -29,17 +26,14 @@ class GitLabCredentials(Block):
     """
 
     _block_type_name = "GitLab Credentials"
-    _logo_url = HttpUrl(
-        url="https://images.ctfassets.net/gm98wzqotmnx/55edIimT4g9gbjhkh5a3Sp/dfdb9391d8f45c2e93e72e3a4d350771/gitlab-logo-500.png?h=250",  # noqa
-        scheme="https",
-    )
+    _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/55edIimT4g9gbjhkh5a3Sp/dfdb9391d8f45c2e93e72e3a4d350771/gitlab-logo-500.png?h=250"
 
-    token: SecretStr = Field(
-        name="Personal Access Token",
+    token: Optional[SecretStr] = Field(
+        title="Personal Access Token",
         default=None,
         description="A GitLab Personal Access Token with read_repository scope.",
     )
-    url: str = Field(
+    url: Optional[str] = Field(
         default=None, title="URL", description="URL to self-hosted GitLab instances."
     )
 
