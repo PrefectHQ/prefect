c03ba86653cce5f94c74800ace47b19233d1cf3b
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-05-30 16:43:10 -0500

    migrate `prefect-shell` to pydantic 2 (#13675)

--- src/integrations/prefect-shell/tests/conftest.py
+++ src/integrations/prefect-shell/tests/conftest.py
@@ -2,6 +2,17 @@ import logging
 
 import pytest
 
+from prefect.testing.utilities import prefect_test_harness
+
+
+@pytest.fixture(scope="session", autouse=True)
+def prefect_db():
+    """
+    Sets up test harness for temporary DB during test runs.
+    """
+    with prefect_test_harness():
+        yield
+
 
 @pytest.fixture(scope="function")
 def prefect_caplog(caplog):
