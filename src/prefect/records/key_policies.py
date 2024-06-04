b205682f86321e43823ef4642f325745a70d561f
Chris White <chris@prefect.io>
2024-06-04 11:47:01 -0700

    Begin adding key policy tests

--- src/prefect/records/key_policies.py
+++ src/prefect/records/key_policies.py
@@ -1,8 +1,10 @@
+from dataclasses import dataclass
 from typing import Optional
 
 from prefect.utilities.hashing import hash_objects
 
 
+@dataclass
 class KeyPolicy:
     def from_function(cls, fn) -> "KeyPolicy":
         """
@@ -21,7 +23,59 @@ class KeyPolicy:
     ) -> Optional[str]:
         raise NotImplementedError
 
+    def __rsub__(self, other: str) -> "CompoundKeyPolicy":
+        if not isinstance(other, str):
+            raise TypeError("Can only subtract strings from key policies.")
+        if isinstance(self, Inputs):
+            if self.exclude is None:
+                self.exclude = [other]
+            else:
+                self.exclude.append(other)
 
+    def __add__(self, other: "KeyPolicy") -> "CompoundKeyPolicy":
+        if isinstance(self, CompoundKeyPolicy):
+            self.merge(other)
+            return self
+        elif isinstance(other, CompoundKeyPolicy):
+            other.merge(self)
+            return other
+        else:
+            return CompoundKeyPolicy(policies=[self, other])
+
+
+@dataclass
+class CompoundKeyPolicy:
+    policies: list = None
+
+    def merge(self, other):
+        """
+        Inplace addition of another policy to this compound policy
+        """
+        if not isinstance(other, _None):
+            # ignore _None policies
+            if self.policies is None:
+                self.policies = [other]
+            else:
+                self.policies.append(other)
+
+    def compute_key(
+        self, task, run, inputs, flow_parameters, **kwargs
+    ) -> Optional[str]:
+        keys = []
+        for policy in self.policies:
+            keys.append(
+                policy.compute_key(
+                    task=task,
+                    run=run,
+                    inputs=inputs,
+                    flow_parameters=flow_parameters,
+                    **kwargs,
+                )
+            )
+        return hash_objects(*keys)
+
+
+@dataclass
 class Default(KeyPolicy):
     "Execution run ID only"
 
@@ -29,10 +83,11 @@ class Default(KeyPolicy):
         return str(run.id)
 
 
+@dataclass
 class _None(KeyPolicy):
     "ignore key policies altogether, always run - prevents persistence"
 
-    def compute_key(self, task, run, parameters, flow_parameters, **kwargs) -> None:
+    def compute_key(self, task, run, inputs, flow_parameters, **kwargs) -> None:
         return None
 
 
@@ -44,6 +99,7 @@ class FlowParameters(KeyPolicy):
     pass
 
 
+@dataclass
 class Inputs(KeyPolicy):
     """
     Exposes flag for whether to include flow parameters as well.
@@ -51,13 +107,20 @@ class Inputs(KeyPolicy):
     And exclude/include config.
     """
 
-    def compute_key(self, task, run, parameters, flow_parameters, **kwargs) -> None:
-        # to_hash = flow_parameters.values() if self.include_flow_params
+    exclude: list = None
 
-        for parameter in parameters:
-            pass
+    def compute_key(self, task, run, inputs, flow_parameters, **kwargs) -> None:
+        hashed_inputs = {}
+        inputs = inputs or {}
+        exclude = self.exclude or []
 
-        return hash_objects()
+        for key, val in inputs.items():
+            if key not in exclude:
+                hashed_inputs[key] = val
+
+        return hash_objects(hashed_inputs)
 
 
 DEFAULT = Default()
+INPUTS = Inputs()
+NONE = _None()
