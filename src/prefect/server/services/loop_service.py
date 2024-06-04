ba1814d6de8d252e0752db25aeeab5ae3910bc55
jake@prefect.io <jake@prefect.io>
2024-06-04 11:39:45 -0400

    don't log retryable exceptions in loop services

--- src/prefect/server/services/loop_service.py
+++ src/prefect/server/services/loop_service.py
@@ -9,6 +9,7 @@ import anyio
 import pendulum
 
 from prefect.logging import get_logger
+from prefect.server.api.server import is_client_retryable_exception
 from prefect.server.database.dependencies import inject_db
 from prefect.server.database.interface import PrefectDBInterface
 from prefect.utilities.processutils import _register_signal
@@ -83,7 +84,10 @@ class LoopService:
 
             # if an error is raised, log and continue
             except Exception as exc:
-                self.logger.error(f"Unexpected error in: {repr(exc)}", exc_info=True)
+                if not is_client_retryable_exception(exc):
+                    self.logger.error(
+                        f"Unexpected error in: {repr(exc)}", exc_info=True
+                    )
 
             end_time = pendulum.now("UTC")
 
