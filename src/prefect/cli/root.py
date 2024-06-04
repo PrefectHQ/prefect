028c56a643333f4c372c42f4dad0da8394d53d12
Craig Harshbarger <pleek91@gmail.com>
2024-06-04 13:15:14 -0500

    Update method for getting the pydantic version

--- src/prefect/cli/root.py
+++ src/prefect/cli/root.py
@@ -95,7 +95,7 @@ def main(
 async def version():
     """Get the current Prefect version."""
     import sqlite3
-    import pkg_resources
+    from importlib.metadata import PackageNotFoundError, version
 
     from prefect.server.utilities.database import get_dialect
     from prefect.settings import PREFECT_API_DATABASE_CONNECTION_URL
@@ -124,14 +124,12 @@ async def version():
 
     version_info["Server type"] = server_type.lower()
 
-    pydantic_version: str
-
     try:
-        pydantic_version = pkg_resources.get_distribution("pydantic").version
-    except pkg_resources.DistributionNotFound:
+        pydantic_version = version("pydantic")
+    except PackageNotFoundError:
         pydantic_version = "Not installed"
 
-    version_info['Pydantic version'] = pydantic_version
+    version_info["Pydantic version"] = pydantic_version
 
     # TODO: Consider adding an API route to retrieve this information?
     if server_type == ServerType.EPHEMERAL.value:
