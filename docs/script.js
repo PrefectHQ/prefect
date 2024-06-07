aeeadac7339ba51ae13d9c4607a8d41e1a73b3ac
Jeff Hale <discdiver@users.noreply.github.com>
2024-06-07 08:16:21 -0400

    Remove script tag (#13852)

--- docs/script.js
+++ docs/script.js
@@ -1,20 +1,19 @@
-<script>
-    (function() {
+
+(function () {
     if (typeof window === 'undefined') return;
     if (typeof window.signals !== 'undefined') return;
     var script = document.createElement('script');
     script.src = 'https://cdn.cr-relay.com/v1/site/5c7cdf16-fbc0-4bb8-b39e-a8c6136687b9/signals.js';
     script.async = true;
     window.signals = Object.assign(
-    [],
-    ['page', 'identify', 'form'].reduce(function (acc, method){
-        acc[method] = function () {
-            signals.push([method, arguments]);
-            return signals;
-        };
-    return acc;
-    }, { })
+        [],
+        ['page', 'identify', 'form'].reduce(function (acc, method) {
+            acc[method] = function () {
+                signals.push([method, arguments]);
+                return signals;
+            };
+            return acc;
+        }, {})
     );
     document.head.appendChild(script);
-  })();
-</script>
+})();
