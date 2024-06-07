f6cd4d9c3136e18af98aa2a9527fe1ed8904fc4c
Chris Guidry <chris.g@prefect.io>
2024-06-07 14:50:05 -0400

    Resolving an infinite loop distributing events to a websocket subscriber (#13877)

--- src/prefect/server/utilities/subscriptions.py
+++ src/prefect/server/utilities/subscriptions.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import Optional
 
 from fastapi import (
@@ -40,3 +41,21 @@ async def accept_prefect_socket(websocket: WebSocket) -> Optional[WebSocket]:
     except NORMAL_DISCONNECT_EXCEPTIONS:
         # it's fine if a client disconnects either normally or abnormally
         return None
+
+
+async def still_connected(websocket: WebSocket) -> bool:
+    """Checks that a client websocket still seems to be connected during a period where
+    the server is expected to be sending messages."""
+    try:
+        await asyncio.wait_for(websocket.receive(), timeout=0.1)
+        return True  # this should never happen, but if it does, we're still connected
+    except asyncio.TimeoutError:
+        # The fact that we timed out rather than getting another kind of error
+        # here means we're still connected to our client, so we can continue to send
+        # events.
+        return True
+    except RuntimeError:
+        # starlette raises this if we test a client that's disconnected
+        return False
+    except NORMAL_DISCONNECT_EXCEPTIONS:
+        return False
