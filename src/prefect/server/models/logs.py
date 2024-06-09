f4fbc5901598d9e2364b71f13d7294e27a18ecbc
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2024-06-09 19:31:08 -0400

    Mute 3.12.2 error (#13831)

--- src/prefect/server/models/logs.py
+++ src/prefect/server/models/logs.py
@@ -42,7 +42,18 @@ async def create_logs(
     Returns:
         None
     """
-    await session.execute(db.insert(db.Log).values([log.model_dump() for log in logs]))
+    try:
+        await session.execute(
+            db.insert(db.Log).values([log.model_dump() for log in logs])
+        )
+    except RuntimeError as exc:
+        if "can't create new thread at interpreter shutdown" in str(exc):
+            # Background logs sometimes fail to write when the interpreter is shutting down.
+            # This is a known issue in Python 3.12.2 that can be ignored and is fixed in Python 3.12.3.
+            # see e.g. https://github.com/python/cpython/issues/113964
+            pass
+        else:
+            raise
 
 
 @inject_db
