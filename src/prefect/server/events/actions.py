039629b586492deb06739b945fb42bfa35888733
nate nowack <thrast36@gmail.com>
2024-06-11 15:49:14 -0500

    Update src/prefect/server/events/actions.py
    Co-authored-by: Chris Guidry <chris.g@prefect.io>

--- src/prefect/server/events/actions.py
+++ src/prefect/server/events/actions.py
@@ -268,7 +268,7 @@ class ExternalDataAction(Action):
         except Exception:
             error_detail = None
         if response.status_code in {409, 422}:
-            if error_detail:
+            if error_detail or response.status_code == 422:
                 return f"Validation error occurred for {self.type!r} - {error_detail}"
             return f"Conflict (409) occurred for {self.type!r} - {error_detail or response.text!r}"
         else:
