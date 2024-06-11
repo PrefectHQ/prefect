35599d85bf84950c87d1e167dbb9fe46077612cb
Alexander Streed <desertaxle@users.noreply.github.com>
2024-06-11 08:49:37 -0500

    Adds circuit breaker when a task depends on itself (#13882)

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -640,6 +640,28 @@ class TestTaskSubmit:
             assert y == i + 1
             assert exceptions_equal(z, ValueError("Fail task"))
 
+    async def test_raises_if_depends_on_itself(self):
+        @task
+        def say_hello(name):
+            return f"Hello {name}!"
+
+        @flow
+        def my_flow():
+            greeting_queue = []
+            for i in range(3):
+                if greeting_queue:
+                    wait_for = greeting_queue
+                else:
+                    wait_for = []
+                future = say_hello.submit(name=f"Person {i}", wait_for=wait_for)
+                greeting_queue.append(future)
+
+            for fut in greeting_queue:
+                print(fut.result())
+
+        with pytest.raises(ValueError, match="deadlock"):
+            my_flow()
+
     def test_logs_message_when_submitted_tasks_end_in_pending(self, caplog):
         """
         If submitted tasks aren't waited on before a flow exits, they may fail to run
