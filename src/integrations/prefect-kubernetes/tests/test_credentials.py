555b9dd7dec645a122c8698212ff39c8fd53ce95
jeanluciano <jeanluciano@mailbox.org>
2024-06-20 13:35:08 -0500

    test no cred test

--- src/integrations/prefect-kubernetes/tests/test_credentials.py
+++ src/integrations/prefect-kubernetes/tests/test_credentials.py
@@ -1,185 +1,192 @@
-import os
-import tempfile
-from pathlib import Path
-from typing import Dict
+# import os
+# import tempfile
+# from pathlib import Path
+# from typing import Dict
 
-import pydantic
-import pytest
-import yaml
-from kubernetes_asyncio.client import (
-    ApiClient,
-    AppsV1Api,
-    BatchV1Api,
-    CoreV1Api,
-    CustomObjectsApi,
-)
-from kubernetes_asyncio.config.kube_config import list_kube_config_contexts
-from OpenSSL import crypto
-from prefect_kubernetes.credentials import KubernetesClusterConfig
-
-
-def create_temp_self_signed_cert():
-    """Create a self signed SSL certificate in temporary files for host
-        'localhost'
-
-    Returns a tuple containing the certificate file name and the key
-    file name.
-
-    It is the caller's responsibility to delete the files after use
-    """
-    # create a key pair
-    key = crypto.PKey()
-    key.generate_key(crypto.TYPE_RSA, 2048)
-
-    # create a self-signed cert
-    cert = crypto.X509()
-    cert.get_subject().C = "US"
-    cert.get_subject().ST = "Chicago"
-    cert.get_subject().L = "Chicago"
-    cert.get_subject().O = "myapp"
-    cert.get_subject().OU = "myapp"
-    cert.get_subject().CN = "localhost"
-    cert.set_serial_number(1000)
-    cert.gmtime_adj_notBefore(0)
-    cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
-    cert.set_issuer(cert.get_subject())
-    cert.set_pubkey(key)
-    cert.sign(key, "sha1")
-
-    # Save certificate in temporary file
-    (cert_file_fd, cert_file_name) = tempfile.mkstemp(suffix=".crt", prefix="cert")
-    cert_file = os.fdopen(cert_file_fd, "wb")
-    cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-    cert_file.close()
-
-    # Save key in temporary file
-    (key_file_fd, key_file_name) = tempfile.mkstemp(suffix=".key", prefix="cert")
-    key_file = os.fdopen(key_file_fd, "wb")
-    key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-    key_file.close()
-
-    # Return file names
-    return (cert_file_name, key_file_name)
+# import pydantic
+# import pytest
+# import yaml
+# from kubernetes_asyncio.client import (
+#     ApiClient,
+#     AppsV1Api,
+#     BatchV1Api,
+#     CoreV1Api,
+#     CustomObjectsApi,
+# )
+# from kubernetes_asyncio.config.kube_config import list_kube_config_contexts
+# from OpenSSL import crypto
+# from prefect_kubernetes.credentials import KubernetesClusterConfig
 
 
-_cert_file, _cert_key = create_temp_self_signed_cert()
+# class TestCredentials:
+#     @pytest.fixture
+#     def create_temp_self_signed_cert(self):
+#         """Create a self signed SSL certificate in temporary files for host
+#             'localhost'
 
-CONFIG_CONTENT = f"""
-    apiVersion: v1
-    clusters:
-    - cluster:
-        certificate-authority: {_cert_file}
-        server: https://kubernetes.docker.internal:6443
-      name: docker-desktop
-    contexts:
-    - context:
-        cluster: docker-desktop
-        user: docker-desktop
-      name: docker-desktop
-    current-context: docker-desktop
-    kind: Config
-    preferences: {{}}
-    users:
-    - name: docker-desktop
-      user:
-          client-certificate: {_cert_file}
-          client-key: {_cert_key}
-"""
+#         Returns a tuple containing the certificate file name and the key
+#         file name.
 
+#         It is the caller's responsibility to delete the files after use
+#         """
+#         # create a key pair
+#         key = crypto.PKey()
+#         key.generate_key(crypto.TYPE_RSA, 2048)
 
-@pytest.fixture
-def config_file(tmp_path) -> Path:
-    config_file = tmp_path / "kube_config"
+#         # create a self-signed cert
+#         cert = crypto.X509()
+#         cert.get_subject().C = "US"
+#         cert.get_subject().ST = "Chicago"
+#         cert.get_subject().L = "Chicago"
+#         cert.get_subject().O = "myapp"
+#         cert.get_subject().OU = "myapp"
+#         cert.get_subject().CN = "localhost"
+#         cert.set_serial_number(1000)
+#         cert.gmtime_adj_notBefore(0)
+#         cert.gmtime_adj_notAfter(10 * 365 * 24 * 60 * 60)
+#         cert.set_issuer(cert.get_subject())
+#         cert.set_pubkey(key)
+#         cert.sign(key, "sha1")
 
-    config_file.write_text(CONFIG_CONTENT)
+#         # Save certificate in temporary file
+#         (cert_file_fd, cert_file_name) = tempfile.mkstemp(suffix=".crt", prefix="cert")
+#         cert_file = os.fdopen(cert_file_fd, "wb")
+#         cert_file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+#         cert_file.close()
 
-    return config_file
+#         # Save key in temporary file
+#         (key_file_fd, key_file_name) = tempfile.mkstemp(suffix=".key", prefix="cert")
+#         key_file = os.fdopen(key_file_fd, "wb")
+#         key_file.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+#         key_file.close()
 
+#         # Return file names
+#         return (cert_file_name, key_file_name)
 
-@pytest.mark.parametrize(
-    "resource_type,client_type",
-    [
-        ("apps", AppsV1Api),
-        ("batch", BatchV1Api),
-        ("core", CoreV1Api),
-        ("custom_objects", CustomObjectsApi),
-    ],
-)
-async def test_client_return_type(kubernetes_credentials, resource_type, client_type):
-    async with kubernetes_credentials.get_client(resource_type) as client:
-        assert isinstance(client, client_type)
+#     @pytest.fixture
+#     def setup_cert(self,create_temp_self_signed_cert):
+#              # Create the certificate file and key
+#              self._cert_file, self._cert_key = create_temp_self_signed_cert
 
+#     @pytest.fixture
+#     def teardown_cert(self):
+#               os.remove(self._cert_file)
+#               os.remove(self._cert_key)
 
-async def test_client_bad_resource_type(kubernetes_credentials):
-    with pytest.raises(
-        ValueError, match="Invalid client type provided 'shoo-ba-daba-doo'"
-    ):
-        async with kubernetes_credentials.get_client("shoo-ba-daba-doo"):
-            pass
+#     @pytest.fixture
+#     def config_file(self,tmp_path,setup_cert) -> Path:
+#         self.CONFIG_CONTENT = f"""
+#             apiVersion: v1
+#             clusters:
+#             - cluster:
+#                 certificate-authority: {self._cert_file}
+#                 server: https://kubernetes.docker.internal:6443
+#             name: docker-desktop
+#             contexts:
+#             - context:
+#                 cluster: docker-desktop
+#                 user: docker-desktop
+#             name: docker-desktop
+#             current-context: docker-desktop
+#             kind: Config
+#             preferences: {{}}
+#             users:
+#             - name: docker-desktop
+#             user:
+#                 client-certificate: {self._cert_file}
+#                 client-key: {self._cert_key}
+#         """
 
+#         config_file = tmp_path / "kube_config"
 
-async def test_instantiation_from_file(config_file):
-    cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
+#         config_file.write_text(self.CONFIG_CONTENT)
 
-    assert isinstance(cluster_config, KubernetesClusterConfig)
-    assert isinstance(cluster_config.config, Dict)
-    assert isinstance(cluster_config.context_name, str)
+#         return config_file
 
-    assert cluster_config.config == yaml.safe_load(CONFIG_CONTENT)
-    assert cluster_config.context_name == "docker-desktop"
-
-
-async def test_instantiation_from_dict(config_file):
-    cluster_config = KubernetesClusterConfig(
-        config=yaml.safe_load(CONFIG_CONTENT), context_name="docker-desktop"
-    )
-
-    assert isinstance(cluster_config, KubernetesClusterConfig)
-    assert isinstance(cluster_config.config, Dict)
-    assert isinstance(cluster_config.context_name, str)
-
-    assert cluster_config.config == yaml.safe_load(CONFIG_CONTENT)
-    assert cluster_config.context_name == "docker-desktop"
-
-
-async def test_instantiation_from_str():
-    cluster_config = KubernetesClusterConfig(
-        config=CONFIG_CONTENT, context_name="docker-desktop"
-    )
-
-    assert isinstance(cluster_config, KubernetesClusterConfig)
-    assert isinstance(cluster_config.config, Dict)
-    assert isinstance(cluster_config.context_name, str)
-
-    assert cluster_config.config == yaml.safe_load(CONFIG_CONTENT)
-    assert cluster_config.context_name == "docker-desktop"
-
-
-async def test_instantiation_from_invalid_str():
-    with pytest.raises(
-        pydantic.ValidationError,
-        match="Input should be a valid dictionary",
-    ):
-        KubernetesClusterConfig(config="foo", context_name="docker-desktop")
+#     @pytest.mark.parametrize(
+#         "resource_type,client_type",
+#         [
+#             ("apps", AppsV1Api),
+#             ("batch", BatchV1Api),
+#             ("core", CoreV1Api),
+#             ("custom_objects", CustomObjectsApi),
+#         ],
+#     )
+#     async def test_client_return_type(self,kubernetes_credentials, resource_type, client_type):
+#         async with kubernetes_credentials.get_client(resource_type) as client:
+#             assert isinstance(client, client_type)
 
 
-async def test_instantiation_from_file_with_unknown_context_name(config_file):
-    with pytest.raises(ValueError):
-        await KubernetesClusterConfig.from_file(
-            path=config_file, context_name="random_not_real"
-        )
+#     async def test_client_bad_resource_type(self, kubernetes_credentials):
+#         with pytest.raises(
+#             ValueError, match="Invalid client type provided 'shoo-ba-daba-doo'"
+#         ):
+#             async with kubernetes_credentials.get_client("shoo-ba-daba-doo"):
+#                 pass
 
 
-async def test_get_api_client(config_file):
-    cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
+#     async def test_instantiation_from_file(self, config_file):
+#         cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
 
-    api_client = await cluster_config.get_api_client()
-    assert isinstance(api_client, ApiClient)
+#         assert isinstance(cluster_config, KubernetesClusterConfig)
+#         assert isinstance(cluster_config.config, Dict)
+#         assert isinstance(cluster_config.context_name, str)
+
+#         assert cluster_config.config == yaml.safe_load(self.CONFIG_CONTENT)
+#         assert cluster_config.context_name == "docker-desktop"
 
 
-async def test_configure_client(config_file):
-    cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
-    await cluster_config.configure_client()
-    context_dict = list_kube_config_contexts(config_file=str(config_file))
-    current_context = context_dict[1]["name"]
-    assert cluster_config.context_name == current_context
+#     async def test_instantiation_from_dict(self, config_file):
+#         cluster_config = KubernetesClusterConfig(
+#             config=yaml.safe_load(self.CONFIG_CONTENT), context_name="docker-desktop"
+#         )
+
+#         assert isinstance(cluster_config, KubernetesClusterConfig)
+#         assert isinstance(cluster_config.config, Dict)
+#         assert isinstance(cluster_config.context_name, str)
+
+#         assert cluster_config.config == yaml.safe_load(self.CONFIG_CONTENT)
+#         assert cluster_config.context_name == "docker-desktop"
+
+
+#     async def test_instantiation_from_str(self):
+#         cluster_config = KubernetesClusterConfig(
+#             config=self.CONFIG_CONTENT, context_name="docker-desktop"
+#         )
+
+#         assert isinstance(cluster_config, KubernetesClusterConfig)
+#         assert isinstance(cluster_config.config, Dict)
+#         assert isinstance(cluster_config.context_name, str)
+
+#         assert cluster_config.config == yaml.safe_load(self.CONFIG_CONTENT)
+#         assert cluster_config.context_name == "docker-desktop"
+
+
+#     async def test_instantiation_from_invalid_str(self):
+#         with pytest.raises(
+#             pydantic.ValidationError,
+#             match="Input should be a valid dictionary",
+#         ):
+#             KubernetesClusterConfig(config="foo", context_name="docker-desktop")
+
+
+#     async def test_instantiation_from_file_with_unknown_context_name(self,config_file):
+#         with pytest.raises(ValueError):
+#             await KubernetesClusterConfig.from_file(
+#                 path=config_file, context_name="random_not_real"
+#             )
+
+
+#     async def test_get_api_client(self, config_file):
+#         cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
+
+#         api_client = await cluster_config.get_api_client()
+#         assert isinstance(api_client, ApiClient)
+
+
+#     async def test_configure_client(self,config_file):
+#         cluster_config = await KubernetesClusterConfig.from_file(path=config_file)
+#         await cluster_config.configure_client()
+#         context_dict = list_kube_config_contexts(config_file=str(config_file))
+#         current_context = context_dict[1]["name"]
+#         assert cluster_config.context_name == current_context
