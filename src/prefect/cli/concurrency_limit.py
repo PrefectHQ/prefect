9f201685d80010a31572e158ced06ae1e04b5d8e
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-06-21 21:22:25 +0200

    Move selfish imports off package __init__.py  (#14190)

--- src/prefect/cli/concurrency_limit.py
+++ src/prefect/cli/concurrency_limit.py
@@ -20,7 +20,7 @@ from rich.table import Table
 from prefect.cli._types import PrefectTyper
 from prefect.cli._utilities import exit_with_error, exit_with_success
 from prefect.cli.root import app, is_interactive
-from prefect.client import get_client
+from prefect.client.orchestration import get_client
 from prefect.exceptions import ObjectNotFound
 
 concurrency_limit_app = PrefectTyper(
