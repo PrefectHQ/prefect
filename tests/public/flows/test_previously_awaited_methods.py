155edd55542ad7a3e16ebdf434b431c654b688ca
zzstoatzz <thrast36@gmail.com>
2024-06-21 16:57:14 -0500

    more warnings on sqlite

--- tests/public/flows/test_previously_awaited_methods.py
+++ tests/public/flows/test_previously_awaited_methods.py
@@ -41,12 +41,16 @@ async def test_awaiting_formerly_async_methods():
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
         await get_some_numbers_old_await_syntax()
+        deprecation_warnings = [
+            _w for _w in w if issubclass(_w.category, DeprecationWarning)
+        ]
+
         assert (
-            len(w) == N * 2 + 5
+            len(deprecation_warnings) == N * 2 + 5
         )  # 1 return_state, 1 submit, 1 wait, 1 result, 1 map, N waits, N results
-        assert issubclass(w[0].category, DeprecationWarning)
         assert all(
-            "please remove the `await` keyword" in str(warning.message) for warning in w
+            "please remove the `await` keyword" in str(warning.message)
+            for warning in deprecation_warnings
         )
 
     # Test the new way (without await)
