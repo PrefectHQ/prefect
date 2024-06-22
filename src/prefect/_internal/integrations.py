7b681ddf1af9e3b4867cd0b2f810f9db163615e6
Alexander Streed <desertaxle@users.noreply.github.com>
2024-06-22 15:09:14 -0500

    Use prefect extras when adhoc installing integrations (#14242)

---
+++ src/prefect/_internal/integrations.py
@@ -0,0 +1,7 @@
+KNOWN_EXTRAS_FOR_PACKAGES = {
+    "prefect-kubernetes": "prefect[kubernetes]",
+    "prefect-aws": "prefect[aws]",
+    "prefect-gcp": "prefect[gcp]",
+    "prefect-azure": "prefect[azure]",
+    "prefect-docker": "prefect[docker]",
+}
