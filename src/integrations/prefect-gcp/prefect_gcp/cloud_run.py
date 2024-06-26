30dce1e1bce83f544869a654a150e1b4864d8614
Serina Grill <42048900+serinamarie@users.noreply.github.com>
2024-06-26 11:51:58 -0500

    delete unneeded file

--- src/integrations/prefect-gcp/prefect_gcp/cloud_run.py
+++
@@ -1,6 +0,0 @@
-"""
-2024-06-27: This helps to handle imports for moved or removed objects in Prefect 3.0 upgrade.
-"""
-from prefect._internal.compatibility.migration import getattr_migration
-
-__getattr__ = getattr_migration(__name__)
