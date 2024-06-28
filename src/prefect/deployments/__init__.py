de771e77e46fa066d7a67ad75bf050851e219205
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-06-28 17:55:02 +0200

    Update __init__.py

--- src/prefect/deployments/__init__.py
+++ src/prefect/deployments/__init__.py
@@ -1,20 +1,36 @@
-from prefect._internal.compatibility.migration import getattr_migration
-import prefect.deployments.base
-import prefect.deployments.steps
-from prefect.deployments.base import (
-    initialize_project,
-)
+from typing import TYPE_CHECKING
 
-from prefect.deployments.runner import (
-    RunnerDeployment,
-    deploy,
-    DockerImage,
-    EntrypointType,
-)
+if TYPE_CHECKING:
+    from .flow_runs import run_deployment
+    from .base import initialize_project
+    from .runner import deploy
 
 
-from prefect.deployments.flow_runs import (
-    run_deployment,
-)
+_public_api: dict[str, tuple[str, str]] = {
+    "initialize_project": (__spec__.parent, ".base"),
+    "deploy": (__spec__.parent, ".runner"),
+    "run_deployment": (__spec__.parent, ".flow_runs"),
+}
 
-__getattr__ = getattr_migration(__name__)
+# Declare API for type-checkers
+__all__ = [
+    "initialize_project",
+    "deploy",
+    "run_deployment",
+]
+
+
+def __getattr__(attr_name: str) -> object:
+    from importlib import import_module
+
+    dynamic_attr = _public_api.get(attr_name)
+    if dynamic_attr is None:
+        return import_module(f".{attr_name}", package=__name__)
+
+    package, module_name = dynamic_attr
+
+    if module_name == "__module__":
+        return import_module(f".{attr_name}", package=package)
+    else:
+        module = import_module(module_name, package=package)
+        return getattr(module, attr_name)
