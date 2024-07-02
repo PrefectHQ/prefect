9c6b14474da994c7e52ec2e81769da4c753f2903
Netanel Malka <netanelm@upstream.auto>
2024-07-02 15:41:41 +0300

    Added print_func for run_namespaced_job

--- src/integrations/prefect-kubernetes/prefect_kubernetes/flows.py
+++ src/integrations/prefect-kubernetes/prefect_kubernetes/flows.py
@@ -1,6 +1,6 @@
 """A module to define flows interacting with Kubernetes resources."""
 
-from typing import Any, Dict
+from typing import Any, Callable, Dict, Optional
 
 from prefect import flow, task
 from prefect_kubernetes.jobs import KubernetesJob
@@ -8,15 +8,16 @@ from prefect_kubernetes.jobs import KubernetesJob
 
 @flow
 async def run_namespaced_job(
-    kubernetes_job: KubernetesJob,
+    kubernetes_job: KubernetesJob, print_func: Optional[Callable] = None
 ) -> Dict[str, Any]:
     """Flow for running a namespaced Kubernetes job.
 
     Args:
         kubernetes_job: The `KubernetesJob` block that specifies the job to run.
+        print_func: A function to print the logs from the job pods.
 
     Returns:
-        The a dict of logs from each pod in the job, e.g. {'pod_name': 'pod_log_str'}.
+        A dict of logs from each pod in the job, e.g. {'pod_name': 'pod_log_str'}.
 
     Raises:
         RuntimeError: If the created Kubernetes job attains a failed status.
@@ -37,6 +38,8 @@ async def run_namespaced_job(
     """
     kubernetes_job_run = await task(kubernetes_job.trigger.aio)(kubernetes_job)
 
-    await task(kubernetes_job_run.wait_for_completion.aio)(kubernetes_job_run)
+    await task(kubernetes_job_run.wait_for_completion.aio)(kubernetes_job_run)(
+        print_func
+    )
 
     return await task(kubernetes_job_run.fetch_result.aio)(kubernetes_job_run)
