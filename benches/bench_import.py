190fb4e9e90d02e835491d332eb6741d1439f156
Alex Streed <alex.s@prefect.io>
2024-07-10 16:26:52 -0500

    Add additional logging for debugging

--- benches/bench_import.py
+++ benches/bench_import.py
@@ -7,3 +7,8 @@ import pytest
 @pytest.mark.benchmark
 def bench_import_prefect():
     subprocess.check_call([sys.executable, "-c", "import prefect"])
+
+
+@pytest.mark.benchmark
+def bench_import_prefect_flow():
+    subprocess.check_call([sys.executable, "-c", "from prefect import flow"])
