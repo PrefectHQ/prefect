850b54c60a0b29f404e896d47995d458107d800d
jeanluciano <jeanluciano@mailbox.org>
2024-07-11 15:01:21 -0500

    tests passing

--- src/integrations/prefect-kubernetes/prefect_kubernetes/flows.py
+++ src/integrations/prefect-kubernetes/prefect_kubernetes/flows.py
@@ -1,22 +1,24 @@
 """A module to define flows interacting with Kubernetes resources."""
 
-from typing import Any, Dict
+import inspect
+from typing import Any, Callable, Dict, Optional
 
 from prefect import flow, task
 from prefect_kubernetes.jobs import KubernetesJob
 
 
 @flow
-async def run_namespaced_job(
-    kubernetes_job: KubernetesJob,
+def run_namespaced_job(
+    kubernetes_job: KubernetesJob, print_func: Optional[Callable] = None
 ) -> Dict[str, Any]:
     """Flow for running a namespaced Kubernetes job.
 
     Args:
         kubernetes_job: The `KubernetesJob` block that specifies the job to run.
+        print_func: A function to print the logs from the job pods.
 
     Returns:
-        The a dict of logs from each pod in the job, e.g. {'pod_name': 'pod_log_str'}.
+        A dict of logs from each pod in the job, e.g. {'pod_name': 'pod_log_str'}.
 
     Raises:
         RuntimeError: If the created Kubernetes job attains a failed status.
@@ -35,8 +37,59 @@ async def run_namespaced_job(
         )
         ```
     """
-    kubernetes_job_run = await task(kubernetes_job.trigger.aio)(kubernetes_job)
+    kubernetes_job_run = task(kubernetes_job.trigger)()
 
-    await task(kubernetes_job_run.wait_for_completion.aio)(kubernetes_job_run)
+    task(kubernetes_job_run.wait_for_completion)(print_func)
 
-    return await task(kubernetes_job_run.fetch_result.aio)(kubernetes_job_run)
+    return task(kubernetes_job_run.fetch_result)()
+
+
+@flow
+async def run_namespaced_job_async(
+    kubernetes_job: KubernetesJob, print_func: Optional[Callable] = None
+) -> Dict[str, Any]:
+    """Flow for running a namespaced Kubernetes job.
+
+    Args:
+        kubernetes_job: The `KubernetesJob` block that specifies the job to run.
+        print_func: A function to print the logs from the job pods.
+
+    Returns:
+        A dict of logs from each pod in the job, e.g. {'pod_name': 'pod_log_str'}.
+
+    Raises:
+        RuntimeError: If the created Kubernetes job attains a failed status.
+
+    Example:
+
+        ```python
+        from prefect_kubernetes import KubernetesJob, run_namespaced_job
+        from prefect_kubernetes.credentials import KubernetesCredentials
+
+        run_namespaced_job(
+            kubernetes_job=KubernetesJob.from_yaml_file(
+                credentials=KubernetesCredentials.load("k8s-creds"),
+                manifest_path="path/to/job.yaml",
+            )
+        )
+        ```
+    """
+    kubernetes_job_run = (
+        await maybe_coro
+        if inspect.iscoroutine((maybe_coro := task(kubernetes_job.trigger)()))
+        else maybe_coro
+    )
+
+    (
+        await maybe_coro
+        if inspect.iscoroutine(
+            maybe_coro := task(kubernetes_job_run.wait_for_completion)(print_func)
+        )
+        else maybe_coro
+    )
+
+    return (
+        await maybe_coro
+        if inspect.iscoroutine(maybe_coro := task(kubernetes_job_run.fetch_result)())
+        else maybe_coro
+    )
