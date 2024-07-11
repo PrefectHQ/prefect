48b5ca4fa7f3149ead84811e1ecbd9e69bc3241e
Alex Streed <alex.s@prefect.io>
2024-07-11 08:45:16 -0500

    Final changes to initial suite to run with CodSpeed

--- benches/bench_cli.py
+++ benches/bench_cli.py
@@ -1,23 +1,21 @@
 import subprocess
 
-import pytest
+# It's hard to get a good reading on these in CodSpeed because they run in another process and
+# CodSpeed currently doesn't include system calls in the benchmark time.
+# TODO: Find a way to measure these in CodSpeed
 
 
-@pytest.mark.benchmark
-def bench_prefect_help():
-    subprocess.check_call(["prefect", "--help"])
+def bench_prefect_help(benchmark):
+    benchmark(subprocess.check_call, ["prefect", "--help"])
 
 
-@pytest.mark.benchmark
-def bench_prefect_version():
-    subprocess.check_call(["prefect", "version"])
+def bench_prefect_version(benchmark):
+    benchmark(subprocess.check_call, ["prefect", "version"])
 
 
-@pytest.mark.benchmark
-def bench_prefect_short_version():
-    subprocess.check_call(["prefect", "--version"])
+def bench_prefect_short_version(benchmark):
+    benchmark(subprocess.check_call, ["prefect", "--version"])
 
 
-@pytest.mark.benchmark
-def bench_prefect_profile_ls():
-    subprocess.check_call(["prefect", "profile", "ls"])
+def bench_prefect_profile_ls(benchmark):
+    benchmark(subprocess.check_call, ["prefect", "profile", "ls"])
