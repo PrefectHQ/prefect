a402a9a4b46106e2ea00db57d939a6c6d37149cb
Alex Streed <alex.s@prefect.io>
2024-07-10 19:34:18 -0500

    Refactor import benches

--- benches/bench_import.py
+++ benches/bench_import.py
@@ -3,30 +3,30 @@ import sys
 
 
 def bench_import_prefect(benchmark):
-    # To get an accurate result, we want to import the module from scratch each time
-    # Remove the module from sys.modules if it's there
-    if "prefect" in sys.modules:
-        del sys.modules["prefect"]
-
-    # Clear importlib cache
-    importlib.invalidate_caches()
-
     def import_prefect():
+        # To get an accurate result, we want to import the module from scratch each time
+        # Remove the module from sys.modules if it's there
+        if "prefect" in sys.modules:
+            del sys.modules["prefect"]
+
+        # Clear importlib cache
+        importlib.invalidate_caches()
+
         import prefect  # noqa
 
     benchmark(import_prefect)
 
 
 def bench_import_prefect_flow(benchmark):
-    # To get an accurate result, we want to import the module from scratch each time
-    # Remove the module from sys.modules if it's there
-    if "prefect" in sys.modules:
-        del sys.modules["prefect"]
-
-    # Clear importlib cache
-    importlib.invalidate_caches()
-
     def import_prefect_flow():
+        # To get an accurate result, we want to import the module from scratch each time
+        # Remove the module from sys.modules if it's there
+        if "prefect" in sys.modules:
+            del sys.modules["prefect"]
+
+        # Clear importlib cache
+        importlib.invalidate_caches()
+
         from prefect import flow  # noqa
 
     benchmark(import_prefect_flow)
