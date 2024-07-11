8a63e6caef9eb1d624f49f17cdeed346005ff54d
Netanel Malka <netanel246@gmail.com>
2024-07-11 16:39:07 +0300

    Added print_func for run_namespaced_job (#14463)
    Co-authored-by: Netanel Malka <netanelm@upstream.auto>
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>

--- src/integrations/prefect-kubernetes/tests/test_flows.py
+++ src/integrations/prefect-kubernetes/tests/test_flows.py
@@ -148,3 +148,35 @@ async def test_run_namespaced_job_successful_with_evictions(
     assert mock_read_namespaced_job_status.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 1
+
+
+async def test_run_namespaced_job_stream_logs(
+    valid_kubernetes_job_block,
+    mock_create_namespaced_job,
+    mock_read_namespaced_job_status,
+    mock_delete_namespaced_job,
+    successful_job_status,
+    mock_list_namespaced_pod,
+    read_pod_logs,
+    mock_pod_log,
+    capsys,
+):
+    successful_job_status.status.active = 0
+    successful_job_status.status.failed = 1
+    mock_read_namespaced_job_status.return_value = successful_job_status
+
+    await run_namespaced_job(
+        kubernetes_job=valid_kubernetes_job_block, print_func=print
+    )
+
+    assert mock_create_namespaced_job.call_count == 1
+    assert mock_create_namespaced_job.call_args[1]["namespace"] == "default"
+    assert mock_create_namespaced_job.call_args[1]["body"].metadata.name == "pi"
+
+    assert read_pod_logs.call_count == 1
+
+    assert mock_read_namespaced_job_status.call_count == 1
+
+    assert mock_delete_namespaced_job.call_count == 1
+
+    assert capsys.readouterr().out == "test log\n"
