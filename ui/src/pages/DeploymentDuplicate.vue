ef36a18b802a42a1ceb07e2efb6afba4df3cfe2b
Dylan Hughes <dylanbhughes@gmail.com>
2024-07-16 13:50:35 -0400

    update type for DeploymentDuplicate

--- ui/src/pages/DeploymentDuplicate.vue
+++ ui/src/pages/DeploymentDuplicate.vue
@@ -4,13 +4,13 @@
       <PageHeadingDeploymentDuplicate :deployment="deployment" />
     </template>
 
-    <DeploymentFormV2 :deployment="deployment" @cancel="cancel" :can-update-name="true" @submit="submit" />
+    <DeploymentFormV2 :deployment="deployment" @cancel="cancel" mode="duplicate" @submit="submit" />
   </p-layout-default>
 </template>
 
 <script lang="ts" setup>
   import { showToast } from '@prefecthq/prefect-design'
-  import { PageHeadingDeploymentDuplicate, useWorkspaceApi, DeploymentUpdateV2, getApiErrorMessage, DeploymentFormV2, DeploymentCreate } from '@prefecthq/prefect-ui-library'
+  import { PageHeadingDeploymentDuplicate, useWorkspaceApi, getApiErrorMessage, DeploymentFormV2, DeploymentCreate } from '@prefecthq/prefect-ui-library'
   import { useSubscription, useRouteParam } from '@prefecthq/vue-compositions'
   import { computed } from 'vue'
   import { usePageTitle } from '@/compositions/usePageTitle'
@@ -25,17 +25,12 @@
   const deploymentSubscription = useSubscription(api.deployments.getDeployment, [deploymentId.value], subscriptionOptions)
   const deployment = computed(() => deploymentSubscription.response)
 
-  async function submit(request: DeploymentUpdateV2): Promise<void> {
+  async function submit(request: DeploymentCreate): Promise<void> {
     try {
-      const deploymentRequest: DeploymentCreate = {
-        ...deployment.value, 
-        ...request,
-      }
-      const newDeployment = await api.deployments.createDeployement(deploymentRequest)
+      const newDeployment = await api.deployments.createDeployement(request)
       showToast('Deployment created', 'success')
       router.push(routes.deployment(newDeployment.id))
     } catch (error) {
-      debugger
       const message = getApiErrorMessage(error, 'Error creating deployment')
       showToast(message, 'error')
       console.warn(error)
