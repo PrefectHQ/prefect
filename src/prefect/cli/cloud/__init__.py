12386647c93f4c805d685b321bb7841641483497
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-07-16 23:49:56 +0200

    Update __init__.py

--- src/prefect/cli/cloud/__init__.py
+++ src/prefect/cli/cloud/__init__.py
@@ -29,7 +29,7 @@ from prefect.cli._types import PrefectTyper
 from prefect.cli._utilities import exit_with_error, exit_with_success
 from prefect.cli.root import app, is_interactive
 from prefect.client.cloud import CloudUnauthorizedError, get_cloud_client
-from prefect.client.schemas import Workspace
+from prefect.client.schemas.objects import Workspace
 from prefect.context import get_settings_context
 from prefect.settings import (
     PREFECT_API_KEY,
