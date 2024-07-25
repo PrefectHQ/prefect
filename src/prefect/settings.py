d9299125904cfe3961077cbc93ec5a06b6987645
Chris White <chris@prefect.io>
2024-07-25 09:44:34 -0700

    Remove stale settings (#14748)

--- src/prefect/settings.py
+++ src/prefect/settings.py
@@ -481,18 +481,6 @@ PREFECT_HOME = Setting(
 directory may be created automatically when required.
 """
 
-PREFECT_EXTRA_ENTRYPOINTS = Setting(
-    str,
-    default="",
-)
-"""
-Modules for Prefect to import when Prefect is imported.
-
-Values should be separated by commas, e.g. `my_module,my_other_module`.
-Objects within modules may be specified by a ':' partition, e.g. `my_module:my_object`.
-If a callable object is provided, it will be called with no arguments on import.
-"""
-
 PREFECT_DEBUG_MODE = Setting(
     bool,
     default=False,
@@ -1436,11 +1424,6 @@ PREFECT_TASK_SCHEDULING_PENDING_TASK_TIMEOUT = Setting(
 How long before a PENDING task are made available to another task worker.  In practice,
 a task worker should move a task from PENDING to RUNNING very quickly, so runs stuck in
 PENDING for a while is a sign that the task worker may have crashed.
-"""
-
-PREFECT_EXPERIMENTAL_DISABLE_SYNC_COMPAT = Setting(bool, default=False)
-"""
-Whether or not to disable the sync_compatible decorator utility.
 """
 
 PREFECT_EXPERIMENTAL_ENABLE_SCHEDULE_CONCURRENCY = Setting(bool, default=False)
