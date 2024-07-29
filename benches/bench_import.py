fd9d124bc191bc8e209c4769d0f98d9d720782c7
Chris Guidry <chris.g@prefect.io>
2024-07-29 14:55:36 -0400

    Using pytest_benchmark pedantic mode to move import clearing away from tests

--- benches/bench_import.py
+++ benches/bench_import.py
@@ -1,47 +1,37 @@
 import importlib
 import sys
 
-import prometheus_client
 import pytest
-from prometheus_client import CollectorRegistry
+from prometheus_client import REGISTRY
+from pytest_benchmark.fixture import BenchmarkFixture
+
+
+def reset_imports():
+    # Remove the module from sys.modules if it's there
+    prefect_modules = [key for key in sys.modules if key.startswith("prefect")]
+    for module in prefect_modules:
+        del sys.modules[module]
+
+    # Clear importlib cache
+    importlib.invalidate_caches()
+
+    # reset the prometheus registry to clear any previously measured metrics
+    for collector in list(REGISTRY._collector_to_names):
+        REGISTRY.unregister(collector)
 
 
 @pytest.mark.benchmark(group="imports")
-def bench_import_prefect(benchmark):
+def bench_import_prefect(benchmark: BenchmarkFixture):
     def import_prefect():
-        # To get an accurate result, we want to import the module from scratch each time
-        # Remove the module from sys.modules if it's there
-        prefect_modules = [key for key in sys.modules if key.startswith("prefect")]
-        for module in prefect_modules:
-            del sys.modules[module]
-
-        # Clear importlib cache
-        importlib.invalidate_caches()
-
-        # reset the prometheus registry to clear any previously registered metrics
-        prometheus_client.REGISTRY = CollectorRegistry(auto_describe=True)
-
         import prefect  # noqa
 
-    benchmark(import_prefect)
+    benchmark.pedantic(import_prefect, setup=reset_imports, rounds=50)
 
 
 @pytest.mark.timeout(180)
 @pytest.mark.benchmark(group="imports")
-def bench_import_prefect_flow(benchmark):
+def bench_import_prefect_flow(benchmark: BenchmarkFixture):
     def import_prefect_flow():
-        # To get an accurate result, we want to import the module from scratch each time
-        # Remove the module from sys.modules if it's there
-        prefect_modules = [key for key in sys.modules if key.startswith("prefect")]
-        for module in prefect_modules:
-            del sys.modules[module]
-
-        # Clear importlib cache
-        importlib.invalidate_caches()
-
-        # reset the prometheus registry to clear any previously registered metrics
-        prometheus_client.REGISTRY = CollectorRegistry(auto_describe=True)
-
         from prefect import flow  # noqa
 
-    benchmark(import_prefect_flow)
+    benchmark.pedantic(import_prefect_flow, setup=reset_imports, rounds=50)
