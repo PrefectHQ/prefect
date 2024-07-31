a8d4fdd720fdd54bdb31046150837aa317b7b6ff
zzstoatzz <thrast36@gmail.com>
2024-07-31 14:20:12 -0500

    bail on it

--- src/prefect/server/database/configurations.py
+++ src/prefect/server/database/configurations.py
@@ -421,8 +421,10 @@ class AioSqliteConfiguration(BaseDatabaseConfiguration):
         else:
             cursor.execute("PRAGMA busy_timeout = 60000;")  # 60s
 
-        # moves temporary tables from disk into RAM, speeds up reads
-        cursor.execute("PRAGMA temp_store = memory;")
+        # `PRAGMA temp_store = memory;`moves temporary tables from disk into RAM
+        # this supposedly speeds up reads, but it seems to actually
+        # decrease overall performance, see https://github.com/PrefectHQ/prefect/pull/14812
+        # cursor.execute("PRAGMA temp_store = memory;")
 
         cursor.close()
 
