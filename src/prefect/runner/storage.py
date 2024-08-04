d00f7a882917211cfcadab19aab93ed8eba471dd
Chris White <chris@prefect.io>
2024-08-04 16:36:00 -0500

    Persist include-submodules flag

--- src/prefect/runner/storage.py
+++ src/prefect/runner/storage.py
@@ -278,6 +278,7 @@ class GitRepository:
             "prefect.deployments.steps.git_clone": {
                 "repository": self._url,
                 "branch": self._branch,
+                "include_submodules": self._include_submodules,
             }
         }
         if isinstance(self._credentials, Block):
