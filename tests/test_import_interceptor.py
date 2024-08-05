26dc50155739470d4d22820af4759f5bd8a4cc88
Alexander Streed <desertaxle@users.noreply.github.com>
2024-08-05 15:44:30 -0500

    Fix syntax for client test suite `pytest` command (#14856)

--- tests/test_import_interceptor.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-def test_prefect_1_import_warning():
-    with pytest.raises(ImportError):
-        with pytest.warns(UserWarning, match="Attempted import of 'prefect.Client"):
-            from prefect import Client  # noqa
