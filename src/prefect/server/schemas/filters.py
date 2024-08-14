f8ac5a0d7ef3082d763f9aa4d6908d91ed176212
nicholas <znicholasbrown@gmail.com>
2024-08-14 12:20:27 -0400

    Fix filter

--- src/prefect/server/schemas/filters.py
+++ src/prefect/server/schemas/filters.py
@@ -1191,9 +1191,9 @@ class LogFilterTaskRunId(PrefectFilterBaseModel):
             filters.append(orm_models.Log.task_run_id.in_(self.any_))
         if self.is_null_ is not None:
             filters.append(
-                orm_models.Log.task_run_id is None
+                orm_models.Log.task_run_id.is_(None)
                 if self.is_null_
-                else orm_models.Log.task_run_id is not None
+                else orm_models.Log.task_run_id.is_not(None)
             )
         return filters
 
