7ea2dbd615c22893357c98e05885a7295e1c6c9e
Collin Choy <collin@prefect.io>
2024-08-16 19:30:04 -0400

    fix docstring.

--- src/prefect/cli/cloud/ip_allowlist.py
+++ src/prefect/cli/cloud/ip_allowlist.py
@@ -14,7 +14,7 @@ cloud_app.add_typer(ip_allowlist_app, aliases=["ip-allowlists"])
 @ip_allowlist_app.command()
 async def ls():
     """
-    Fetch and list all IP allowlist entries in your workspace
+    Fetch and list all IP allowlist entries in your account.
     """
     confirm_logged_in()
 
