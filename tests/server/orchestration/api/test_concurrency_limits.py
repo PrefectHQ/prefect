237c9e2a253ed595c10c4ba41be4739ddecd0ecc
Chris Guidry <chris.g@prefect.io>
2024-08-21 16:17:51 -0400

    Adds acquire/release endpoints for task run concurrency limits
    In support of client-side task run orchestration, we need to expose these two
    endpoints so that clients may acquire and release tag-based concurrency limits
    for the task runs they are orchestrating.
    
    The design of this API is intended to be closer to V2 concurrency terminology,
    hence the `increment`/`decrement` paths and the `names` parameter.

--- tests/server/orchestration/api/test_concurrency_limits.py
+++ tests/server/orchestration/api/test_concurrency_limits.py
@@ -1,9 +1,13 @@
+from typing import List
 from uuid import uuid4
 
+import pytest
+from httpx import AsyncClient
 from starlette import status
 
 from prefect.server import schemas
 from prefect.server.schemas.actions import ConcurrencyLimitCreate
+from prefect.settings import PREFECT_TASK_RUN_TAG_CONCURRENCY_SLOT_WAIT_SECONDS
 
 
 class TestConcurrencyLimits:
@@ -99,3 +103,170 @@ class TestConcurrencyLimits:
 
         post_reset = await client.get(f"/concurrency_limits/{cl_id}")
         assert len(post_reset.json()["active_slots"]) == 0
+
+
+class TestAcquiringAndReleasing:
+    @pytest.fixture
+    async def tags_with_limits(
+        self,
+        client: AsyncClient,
+    ) -> List[str]:
+        tags = ["tag1", "tag2"]
+
+        for tag in tags:
+            await client.post(
+                "/concurrency_limits/",
+                json=ConcurrencyLimitCreate(
+                    tag=tag,
+                    concurrency_limit=2,
+                ).model_dump(mode="json"),
+            )
+
+        return tags
+
+    async def test_acquiring_and_releasing_limits(
+        self,
+        client: AsyncClient,
+        tags_with_limits: List[str],
+    ):
+        task_run_id = uuid4()
+        tags = tags_with_limits + ["does-not-exist"]
+
+        response = await client.post(
+            "/concurrency_limits/increment",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        for tag in tags_with_limits:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert concurrency_limit.active_slots == [task_run_id]
+
+        response = await client.post(
+            "/concurrency_limits/decrement",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        # confirm the slots have been released
+
+        for tag in tags_with_limits:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert concurrency_limit.active_slots == []
+
+    async def test_failing_to_acquire_one_slot(
+        self,
+        client: AsyncClient,
+        tags_with_limits: List[str],
+    ):
+        task_run_id = uuid4()
+        tags = tags_with_limits + ["does-not-exist"]
+
+        # Acquire the slots by two random other task runs
+        for i in range(2):
+            response = await client.post(
+                "/concurrency_limits/increment",
+                json={"names": tags_with_limits[:1], "task_run_id": str(uuid4())},
+            )
+            assert response.status_code == status.HTTP_200_OK
+
+        response = await client.post(
+            "/concurrency_limits/increment",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_423_LOCKED
+        assert "Retry-After" in response.headers
+        assert (
+            0.0
+            <= float(response.headers["Retry-After"])
+            <= PREFECT_TASK_RUN_TAG_CONCURRENCY_SLOT_WAIT_SECONDS.value() * 2
+        )
+        assert (
+            "Concurrency limit for the tag1 tag has been reached"
+            in response.json()["detail"]
+        )
+
+        for tag in tags_with_limits:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert task_run_id not in concurrency_limit.active_slots
+
+        response = await client.post(
+            "/concurrency_limits/decrement",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        # confirm the slots have been released
+
+        for tag in tags_with_limits:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert task_run_id not in concurrency_limit.active_slots
+
+    @pytest.fixture
+    async def tag_with_zero_concurrency(
+        self,
+        client: AsyncClient,
+    ) -> str:
+        await client.post(
+            "/concurrency_limits/",
+            json=ConcurrencyLimitCreate(
+                tag="zero",
+                concurrency_limit=0,
+            ).model_dump(mode="json"),
+        )
+
+        return "zero"
+
+    async def test_setting_tag_to_zero_concurrency(
+        self,
+        client: AsyncClient,
+        tags_with_limits: List[str],
+        tag_with_zero_concurrency: str,
+    ):
+        task_run_id = uuid4()
+        tags = tags_with_limits + [tag_with_zero_concurrency] + ["does-not-exist"]
+
+        response = await client.post(
+            "/concurrency_limits/increment",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_423_LOCKED
+        assert "Retry-After" not in response.headers
+        assert (
+            'The concurrency limit on tag "zero" is 0 and will deadlock if the task '
+            "tries to run again." in response.json()["detail"]
+        )
+
+        for tag in tags_with_limits + [tag_with_zero_concurrency]:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert concurrency_limit.active_slots == []
+
+        response = await client.post(
+            "/concurrency_limits/decrement",
+            json={"names": tags, "task_run_id": str(task_run_id)},
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        # confirm the slots have been released
+
+        for tag in tags_with_limits + [tag_with_zero_concurrency]:
+            read_response = await client.get(f"/concurrency_limits/tag/{tag}")
+            concurrency_limit = schemas.core.ConcurrencyLimit.model_validate(
+                read_response.json()
+            )
+            assert concurrency_limit.active_slots == []
