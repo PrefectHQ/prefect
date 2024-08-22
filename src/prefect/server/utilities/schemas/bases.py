22c8a6e3de4db8c6393d7232e39577cd34dc7865
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-08-22 15:37:38 +0200

    Performance: Add defer_build to PrefectBaseModel (#15046)

--- src/prefect/server/utilities/schemas/bases.py
+++ src/prefect/server/utilities/schemas/bases.py
@@ -60,6 +60,7 @@ class PrefectBaseModel(BaseModel):
     _reset_fields: ClassVar[Set[str]] = set()
     model_config = ConfigDict(
         ser_json_timedelta="float",
+        defer_build=True,
         extra=(
             "ignore"
             if os.getenv("PREFECT_TEST_MODE", "0").lower() not in ["true", "1"]
