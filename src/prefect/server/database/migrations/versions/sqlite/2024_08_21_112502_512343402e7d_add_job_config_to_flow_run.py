64db538aa3b9389ac9757268bd140b9e5f6b7e41
Dylan Hughes <dylanbhughes@gmail.com>
2024-08-23 12:25:22 -0400

    put the infrastructure config on its own table

--- src/prefect/server/database/migrations/versions/sqlite/2024_08_21_112502_512343402e7d_add_job_config_to_flow_run.py
+++ src/prefect/server/database/migrations/versions/sqlite/2024_08_21_112502_512343402e7d_add_job_config_to_flow_run.py
@@ -19,17 +19,75 @@ depends_on = None
 
 
 def upgrade():
-    with op.batch_alter_table("flow_run", schema=None) as batch_op:
-        batch_op.add_column(
-            sa.Column(
-                "job_configuration",
-                prefect.server.utilities.database.JSON(astext_type=sa.Text()),
-                server_default="{}",
-                nullable=True,
-            )
+    op.create_table(
+        "flow_run_infrastructure_configuration",
+        sa.Column(
+            "flow_run_id",
+            prefect.server.utilities.database.UUID(),
+            nullable=False,
+        ),
+        sa.Column(
+            "job_configuration",
+            prefect.server.utilities.database.JSON(astext_type=sa.Text()),
+            nullable=False,
+            server_default="{}",
+        ),
+        sa.Column(
+            "id",
+            prefect.server.utilities.database.UUID(),
+            server_default=sa.text(
+                "(\n    (\n        lower(hex(randomblob(4)))\n        || '-'\n        || lower(hex(randomblob(2)))\n        || '-4'\n        || substr(lower(hex(randomblob(2))),2)\n        || '-'\n        || substr('89ab',abs(random()) % 4 + 1, 1)\n        || substr(lower(hex(randomblob(2))),2)\n        || '-'\n        || lower(hex(randomblob(6)))\n    )\n    )"
+            ),
+            nullable=False,
+        ),
+        sa.Column(
+            "created",
+            prefect.server.utilities.database.Timestamp(timezone=True),
+            server_default=sa.text("(strftime('%Y-%m-%d %H:%M:%f000', 'now'))"),
+            nullable=False,
+        ),
+        sa.Column(
+            "updated",
+            prefect.server.utilities.database.Timestamp(timezone=True),
+            server_default=sa.text("(strftime('%Y-%m-%d %H:%M:%f000', 'now'))"),
+            nullable=False,
+        ),
+        sa.PrimaryKeyConstraint(
+            "id", name=op.f("pk_flow_run_infrastructure_configuration")
+        ),
+        sa.ForeignKeyConstraint(
+            ["flow_run_id"],
+            ["flow_run.id"],
+            name=op.f(
+                "fk_flow_run_infrastructure_configuration__flow_run_id__flow_run"
+            ),
+            ondelete="CASCADE",
+        ),
+    )
+    with op.batch_alter_table(
+        "flow_run_infrastructure_configuration", schema=None
+    ) as batch_op:
+        batch_op.create_index(
+            batch_op.f("ix_flow_run_infrastructure_configuration__flow_run_id"),
+            ["flow_run_id"],
+            unique=False,
+        )
+        batch_op.create_index(
+            batch_op.f("ix_flow_run_infrastructure_configuration__updated"),
+            ["updated"],
+            unique=False,
         )
 
 
 def downgrade():
-    with op.batch_alter_table("flow_run", schema=None) as batch_op:
-        batch_op.drop_column("job_configuration")
+    with op.batch_alter_table(
+        "flow_run_infrastructure_configuration", schema=None
+    ) as batch_op:
+        batch_op.drop_index(
+            batch_op.f("ix_flow_run_infrastructure_configuration__updated")
+        )
+        batch_op.drop_index(
+            batch_op.f("ix_flow_run_infrastructure_configuration__flow_run_id")
+        )
+
+    op.drop_table("flow_run_infrastructure_configuration")
