fdf65e8e4e9d77e4938fe3f2dcd1ac052eef6a48
Alexander Streed <desertaxle@users.noreply.github.com>
2024-08-26 13:01:13 -0500

    Revert change to defer building the server-side version of `PrefectBaseModel` (#15072)

--- src/prefect/server/utilities/schemas/bases.py
+++ src/prefect/server/utilities/schemas/bases.py
@@ -61,8 +61,6 @@ class PrefectBaseModel(BaseModel):
 
     model_config = ConfigDict(
         ser_json_timedelta="float",
-        experimental_defer_build_mode=("model", "type_adapter"),
-        defer_build=True,
         extra=(
             "ignore"
             if os.getenv("PREFECT_TEST_MODE", "0").lower() not in ["true", "1"]
