7a6459554736f664a0b6ce0d1e6faf6ed6e6dee3
Alexander Streed <desertaxle@users.noreply.github.com>
2024-09-04 14:43:54 -0500

    Adds ability to write result metadata separate from data when using `ResultStore` (#15214)

--- tests/results/test_result_store.py
+++ tests/results/test_result_store.py
@@ -731,3 +731,66 @@ async def test_result_store_from_task_takes_precedence_from_task(persist_result)
     result_store = foo()
 
     assert result_store.persist_result is persist_result
+
+
+async def test_result_store_read_and_write_with_metadata_storage(tmp_path):
+    metadata_storage = LocalFileSystem(basepath=tmp_path / "metadata")
+    result_storage = LocalFileSystem(basepath=tmp_path / "results")
+    result_store = ResultStore(
+        metadata_storage=metadata_storage, result_storage=result_storage
+    )
+
+    key = "test"
+    value = "test"
+    await result_store.awrite(key=key, obj=value)
+    read_value = await result_store.aread(key=key)
+    assert read_value.result == value
+
+    # Check that the result is written to the result storage
+    assert (
+        result_store.serializer.loads((tmp_path / "results" / key).read_bytes())
+        == value
+    )
+
+    # Check that the metadata is written to the metadata storage
+    assert (
+        tmp_path / "metadata" / key
+    ).read_text() == read_value.metadata.model_dump_json(serialize_as_any=True)
+
+
+async def test_result_store_exists_with_metadata_storage(tmp_path):
+    metadata_storage = LocalFileSystem(basepath=tmp_path / "metadata")
+    result_storage = LocalFileSystem(basepath=tmp_path / "results")
+    result_store = ResultStore(
+        metadata_storage=metadata_storage, result_storage=result_storage
+    )
+
+    key = "test"
+    value = "test"
+    await result_store.awrite(key=key, obj=value)
+
+    assert await result_store.aexists(key=key) is True
+    assert await result_store.aexists(key="nonexistent") is False
+    assert result_store.exists(key=key) is True
+    assert result_store.exists(key="nonexistent") is False
+
+    # Remove the metadata file and check that the result is not found
+    (tmp_path / "metadata" / key).unlink()
+    assert await result_store.aexists(key=key) is False
+    assert result_store.exists(key=key) is False
+
+
+async def test_result_store_exists_with_no_metadata_storage(tmp_path):
+    result_storage = LocalFileSystem(basepath=tmp_path / "results")
+    result_store = ResultStore(result_storage=result_storage)
+
+    key = "test"
+    value = "test"
+    await result_store.awrite(key=key, obj=value)
+    assert await result_store.aexists(key=key) is True
+    assert result_store.exists(key=key) is True
+
+    # Remove the result file and check that the result is not found
+    (tmp_path / "results" / key).unlink()
+    assert await result_store.aexists(key=key) is False
+    assert result_store.exists(key=key) is False
