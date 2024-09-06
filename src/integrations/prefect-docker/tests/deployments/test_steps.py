6739b891483efd44c915b46393cf80e2e54eea45
nate nowack <thrast36@gmail.com>
2024-09-06 11:47:29 -0500

    add function name to `prefect-docker` cache key (#15262)

--- src/integrations/prefect-docker/tests/deployments/test_steps.py
+++ src/integrations/prefect-docker/tests/deployments/test_steps.py
@@ -503,3 +503,28 @@ class TestCachedSteps:
         assert mock_docker_client.login.call_count == 3
         expected_push_calls = 1 + len(additional_tags)
         assert mock_docker_client.api.push.call_count == expected_push_calls * 3
+
+    def test_avoids_aggressive_caching(self, mock_docker_client):
+        """this is a regression test for https://github.com/PrefectHQ/prefect/issues/15258
+        where all decorated functions were sharing a cache, so dict(image=..., tag=...) passed to
+        build_docker_image and push_docker_image would hit the cache for push_docker_image,
+        even though the function was different and should not have been cached.
+
+        here we test that the caches are distinct for each decorated function.
+        """
+        image_name = "registry/repo"
+        tag = "latest"
+
+        build_docker_image(
+            image_name=image_name,
+            tag=tag,
+        )
+
+        # Push the image (this should not hit the cache)
+        push_docker_image(
+            image_name=image_name,
+            tag=tag,
+        )
+
+        mock_docker_client.api.build.assert_called_once()
+        mock_docker_client.api.push.assert_called_once()
