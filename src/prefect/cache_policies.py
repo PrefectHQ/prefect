1a0e1712e1d3881a9640db69471ceeb107f224e1
Alex Streed <alex.s@prefect.io>
2024-09-13 13:31:21 -0500

    Refactors `__add__` and `__sub__` on cache policies

--- src/prefect/cache_policies.py
+++ src/prefect/cache_policies.py
@@ -98,6 +98,22 @@ class CompoundCachePolicy(CachePolicy):
             return None
         return hash_objects(*keys)
 
+    def __add__(self, other: "CachePolicy") -> "CachePolicy":
+        # adding _None is a no-op
+        if isinstance(other, _None):
+            return self
+        elif isinstance(other, CompoundCachePolicy):
+            return CompoundCachePolicy(policies=other.policies + self.policies)
+        else:
+            return CompoundCachePolicy(policies=self.policies + [other])
+
+    def __sub__(self, other: str) -> "CompoundCachePolicy":
+        if not isinstance(other, str):
+            raise TypeError("Can only subtract strings from key policies.")
+        new = Inputs(exclude=[other])
+        policies = self.policies
+        return CompoundCachePolicy(policies=policies + [new])
+
 
 @dataclass
 class _None(CachePolicy):
