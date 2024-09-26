759724691da3b36e97aa3bc05d85ff0d8c22c629
nicholas <znicholasbrown@gmail.com>
2024-09-26 11:15:43 -0400

    Fix linting issues

--- tests/server/schemas/test_filters.py
+++ tests/server/schemas/test_filters.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 import pendulum
 import sqlalchemy as sa
 
-from prefect.server.schemas.filters import LogFilter, FlowRunFilter
+from prefect.server.schemas.filters import FlowRunFilter, LogFilter
 
 NOW = pendulum.now("UTC")
 
