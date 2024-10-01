a446ba7f8cfdd75bad9d762fdb09d299debd7f9a
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-10-01 15:36:06 -0400

    Update flow.$flowId.tsx

--- prefect-ui/src/routes/flows/flow.$flowId.tsx
+++ prefect-ui/src/routes/flows/flow.$flowId.tsx
@@ -7,13 +7,24 @@ const queryParams = (id: string) => ({
   queryFn: () => Promise.all([
     QueryService.GET('/flows/{id}', { params: { path: { id } } }),
     QueryService.POST('/flow_runs/filter', { body: { flows: { operator: 'and_', id : {any_: [id]} }, offset: 0, limit: 10, sort: 'START_TIME_DESC' } }),
+    QueryService.POST('/flow_runs/count', { body: { flows: { operator: 'and_', id : {any_: [id]} } }}),
     QueryService.POST('/deployments/filter', { body: { flows: { operator: 'and_', id : {any_: [id]} }, offset: 0, limit: 10, sort: 'CREATED_DESC' } }),
+    QueryService.POST('/deployments/count', { body: { flows: { operator: 'and_', id : {any_: [id]} } }}),
   ]),
   staleTime: 1000 // Data will be considered stale after 1 second.
 })
 
 export const Route = createFileRoute('/flows/flow/$flowId')({
-  component: () => { const [flow, flowRuns] = Route.useLoaderData(); return <FlowDetail flow={flow?.data} flowRuns={flowRuns?.data}/> },
+  component: () => { 
+    const [flow, flowRuns, flowRunsCount, deployments, deploymentsCount] = Route.useLoaderData(); 
+    return <FlowDetail 
+              flow={flow?.data} 
+              flowRuns={flowRuns?.data}
+              flowRunsCount={flowRunsCount?.data}
+              deployments={deployments?.data}
+              deploymentsCount={deploymentsCount?.data}
+              /> 
+  },
   loader: async ({ params: { flowId }, context }) => await context.queryClient.ensureQueryData(queryParams(flowId)),
   wrapInSuspense: true,
 })
@@ -21,15 +32,21 @@ export const Route = createFileRoute('/flows/flow/$flowId')({
 function FlowDetail({
   flow, 
   flowRuns,
+  flowRunsCount,
+  deployments,
+  deploymentsCount,
 }: {
   flow: components['schemas']['Flow'] | undefined,
   flowRuns: components['schemas']['FlowRun'][] | undefined,
+  flowRunsCount: number | undefined,
+  deployments: components['schemas']['DeploymentResponse'][] | undefined,
+  deploymentsCount: number | undefined,
 }) {
 
   return (
     <div className="p-2">
       <h3>Flow</h3>
-      { JSON.stringify([flow, flowRuns]) }
+      { JSON.stringify([flow, flowRuns, flowRunsCount, deployments, deploymentsCount]) }
     </div>
   )
 }
