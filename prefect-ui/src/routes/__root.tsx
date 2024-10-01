1029b6aa125f462077347d066c0ff0b0b12ea079
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2024-10-01 15:37:05 -0400

    Use `openapi-ts` to autofetch fetching service layer.  (#15556)

--- prefect-ui/src/routes/__root.tsx
+++ prefect-ui/src/routes/__root.tsx
@@ -1,7 +1,12 @@
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { createRootRouteWithContext, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { QueryClient } from '@tanstack/react-query'
 
-export const Route = createRootRoute({
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <div className="p-2 flex gap-2">
--- prefect-ui/vite.config.ts
+++ prefect-ui/vite.config.ts
@@ -1,8 +1,15 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [react(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
