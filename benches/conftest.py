2c823547e6a259fe8f91fb2f0aa9e0e3c79855dc
zzstoatzz <thrast36@gmail.com>
2024-10-04 11:20:31 -0500

    try monkeypatch

--- benches/conftest.py
+++ benches/conftest.py
@@ -3,6 +3,8 @@ import traceback
 import pytest
 import pytest_benchmark.plugin
 
+from prefect.settings import Settings
+
 _handle_saving = pytest_benchmark.session.BenchmarkSession.handle_saving
 
 
@@ -26,4 +28,5 @@ pytest_benchmark.session.BenchmarkSession.handle_saving = handle_saving
 def disable_test_mode(monkeypatch):
     monkeypatch.delenv("PREFECT_TEST_MODE", raising=False)
     monkeypatch.delenv("PREFECT_UNIT_TEST_MODE", raising=False)
+    monkeypatch.setattr("prefect.settings.get_current_settings", lambda: Settings())
     yield
