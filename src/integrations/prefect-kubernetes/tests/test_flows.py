7db32874843bfc84af3db3985ebd3f8e6fd378e4
nate nowack <thrast36@gmail.com>
2024-10-17 09:14:09 -0500

    Capture pod logs for `run_namespaced_job` (#15732)

--- src/integrations/prefect-kubernetes/tests/test_flows.py
+++ src/integrations/prefect-kubernetes/tests/test_flows.py
@@ -8,7 +8,7 @@ from prefect import flow
 async def test_run_namespaced_job_timeout_respected(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_list_namespaced_pod,
     read_pod_logs,
     successful_job_status,
@@ -16,7 +16,7 @@ async def test_run_namespaced_job_timeout_respected(
     successful_job_status.status.active = 1
     successful_job_status.status.succeeded = None
     successful_job_status.status.conditions = []
-    mock_read_namespaced_job_status.return_value = successful_job_status
+    mock_read_namespaced_job.return_value = successful_job_status
 
     valid_kubernetes_job_block.timeout_seconds = 1
 
@@ -30,13 +30,13 @@ async def test_run_namespaced_job_timeout_respected(
         == "pi"
     )
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
 
 async def test_run_namespaced_job_successful(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     mock_list_namespaced_pod,
     read_pod_logs,
@@ -52,7 +52,7 @@ async def test_run_namespaced_job_successful(
 
     assert read_pod_logs.call_count == 1
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 1
 
@@ -60,13 +60,13 @@ async def test_run_namespaced_job_successful(
 async def test_run_namespaced_job_successful_no_delete_after_completion(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     successful_job_status,
     mock_list_namespaced_pod,
     read_pod_logs,
 ):
-    mock_read_namespaced_job_status.return_value = successful_job_status
+    mock_read_namespaced_job.return_value = successful_job_status
 
     valid_kubernetes_job_block.delete_after_completion = False
 
@@ -79,7 +79,7 @@ async def test_run_namespaced_job_successful_no_delete_after_completion(
         == "pi"
     )
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 0
 
@@ -87,13 +87,13 @@ async def test_run_namespaced_job_successful_no_delete_after_completion(
 async def test_run_namespaced_job_unsuccessful(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     unsuccessful_job_status,
     mock_list_namespaced_pod,
     read_pod_logs,
 ):
-    mock_read_namespaced_job_status.return_value = unsuccessful_job_status
+    mock_read_namespaced_job.return_value = unsuccessful_job_status
 
     with pytest.raises(RuntimeError, match=", check the Kubernetes pod logs"):
         await run_namespaced_job_async(kubernetes_job=valid_kubernetes_job_block)
@@ -105,7 +105,7 @@ async def test_run_namespaced_job_unsuccessful(
         == "pi"
     )
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 0
 
@@ -113,7 +113,7 @@ async def test_run_namespaced_job_unsuccessful(
 def test_run_namespaced_job_sync_subflow(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     successful_job_status,
     mock_list_namespaced_pod,
@@ -134,7 +134,7 @@ def test_run_namespaced_job_sync_subflow(
 
     assert read_pod_logs.call_count == 1
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 1
 
@@ -142,7 +142,7 @@ def test_run_namespaced_job_sync_subflow(
 async def test_run_namespaced_job_successful_with_evictions(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     successful_job_status,
     mock_list_namespaced_pod,
@@ -150,7 +150,7 @@ async def test_run_namespaced_job_successful_with_evictions(
 ):
     successful_job_status.status.active = 0
     successful_job_status.status.failed = 1
-    mock_read_namespaced_job_status.return_value = successful_job_status
+    mock_read_namespaced_job.return_value = successful_job_status
 
     await run_namespaced_job_async(kubernetes_job=valid_kubernetes_job_block)
 
@@ -163,7 +163,7 @@ async def test_run_namespaced_job_successful_with_evictions(
 
     assert read_pod_logs.call_count == 1
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 1
 
@@ -171,7 +171,7 @@ async def test_run_namespaced_job_successful_with_evictions(
 def test_run_namespaced_job_sync_stream_logs(
     valid_kubernetes_job_block,
     mock_create_namespaced_job,
-    mock_read_namespaced_job_status,
+    mock_read_namespaced_job,
     mock_delete_namespaced_job,
     successful_job_status,
     mock_list_namespaced_pod,
@@ -196,7 +196,7 @@ def test_run_namespaced_job_sync_stream_logs(
 
     assert read_pod_logs.call_count == 1
 
-    assert mock_read_namespaced_job_status.call_count == 1
+    assert mock_read_namespaced_job.call_count == 1
 
     assert mock_delete_namespaced_job.call_count == 1
 
