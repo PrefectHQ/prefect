2c9b857779ab245f59e311b80c0c10c8a0d2e7e5
Sam Phinizy <samuel@prefect.io>
2024-10-29 18:17:56 -0400

    Adds in the ability for the client to get a remote_id from the server (#15849)
    Co-authored-by: Alexander Streed <desertaxle@users.noreply.github.com>

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -228,6 +228,7 @@ SUPPORTED_SETTINGS = {
     },
     "PREFECT_EVENTS_WEBSOCKET_BACKFILL_PAGE_SIZE": {"test_value": 10, "legacy": True},
     "PREFECT_EXPERIMENTAL_WARN": {"test_value": True},
+    "PREFECT_EXPERIMENTS_WORKER_LOGGING_TO_API_ENABLED": {"test_value": False},
     "PREFECT_FLOW_DEFAULT_RETRIES": {"test_value": 10, "legacy": True},
     "PREFECT_FLOWS_DEFAULT_RETRIES": {"test_value": 10},
     "PREFECT_FLOW_DEFAULT_RETRY_DELAY_SECONDS": {"test_value": 10, "legacy": True},
