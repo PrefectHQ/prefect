c3512dc3d2ab1586218d4ddf03d2bd043af48108
Sam Phinizy <samuel@prefect.io>
2024-11-01 16:37:08 -0400

    Reverts back to backend_id

--- src/prefect/workers/base.py
+++ src/prefect/workers/base.py
@@ -827,7 +827,7 @@ class BaseWorker(abc.ABC):
                     f"Worker '{self.name}' submitting flow run '{flow_run.id}'"
                 )
                 run_logger.info(
-                    f"Flow run '{flow_run.id}' submitted by worker '{self.worker_id}'"
+                    f"Flow run '{flow_run.id}' submitted by worker '{self.backend_id}'"
                 )
                 self._submitting_flow_run_ids.add(flow_run.id)
                 self._runs_task_group.start_soon(
