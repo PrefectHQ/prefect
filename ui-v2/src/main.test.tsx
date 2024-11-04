ecafe5f5b06906d15767818a0ca0c9ce4c081ccd
Alex Streed <alex.s@prefect.io>
2024-11-04 16:30:37 -0600

    Start test setup

---
+++ ui-v2/src/main.test.tsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from 'vitest';
+
+describe('something truthy and falsy', () => {
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
+});
--- ui-v2/vite.config.ts
+++ ui-v2/vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), TanStackRouterVite()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
