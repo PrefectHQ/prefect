11c7a6f660e7f076440990e49363a5e79ddfa2b7
Alex Streed <alex.s@prefect.io>
2024-11-04 16:20:24 -0600

    Update sidebar styling

--- ui-v2/src/components/ui/app-sidebar.tsx
+++ ui-v2/src/components/ui/app-sidebar.tsx
@@ -9,6 +9,7 @@ import {
 	SidebarMenuButton,
 } from "@/components/ui/sidebar";
 import { Link } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 const items = [
 	{
@@ -60,7 +61,35 @@ const items = [
 export function AppSidebar() {
 	return (
 		<Sidebar>
-			<SidebarHeader>Prefect</SidebarHeader>
+			<SidebarHeader>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 76 76"
+					className="w-11 h-11"
+					aria-label="Prefect Logo"
+				>
+					<title>Prefect Logo</title>
+					<path
+						fill="currentColor"
+						fillRule="evenodd"
+						d="M15.89 15.07 38 26.543v22.935l22.104-11.47.007.004V15.068l-.003.001L38 3.598z"
+						clipRule="evenodd"
+					/>
+					<path
+						fill="currentColor"
+						fillRule="evenodd"
+						d="M15.89 15.07 38 26.543v22.935l22.104-11.47.007.004V15.068l-.003.001L38 3.598z"
+						clipRule="evenodd"
+					/>
+					<path
+						fill="currentColor"
+						fillRule="evenodd"
+						d="M37.987 49.464 15.89 38v22.944l.013-.006L38 72.402V49.457z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			</SidebarHeader>
 			<SidebarContent>
 				<SidebarGroup>
 					<SidebarMenu>
@@ -81,9 +110,18 @@ export function AppSidebar() {
 			<SidebarFooter>
 				<SidebarMenu>
 					<SidebarMenuItem>
-						<SidebarMenuButton asChild>
-							<span>Ready to scale?</span>
-						</SidebarMenuButton>
+						<a
+							href="https://prefect.io/cloud-vs-oss?utm_source=oss&utm_medium=oss&utm_campaign=oss&utm_term=none&utm_content=none"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<SidebarMenuButton asChild>
+								<div className="flex items-center justify-between">
+									<span>Ready to scale?</span>
+									<Button size="sm">Upgrade</Button>
+								</div>
+							</SidebarMenuButton>
+						</a>
 					</SidebarMenuItem>
 					<SidebarMenuItem>
 						<SidebarMenuButton asChild>
--- ui-v2/src/components/ui/sidebar/sidebar.tsx
+++ ui-v2/src/components/ui/sidebar/sidebar.tsx
@@ -344,7 +344,7 @@ const SidebarHeader = React.forwardRef<
 		<div
 			ref={ref}
 			data-sidebar="header"
-			className={cn("flex flex-col gap-2 p-2", className)}
+			className={cn("flex flex-col gap-2 p-4", className)}
 			{...props}
 		/>
 	);
@@ -359,7 +359,7 @@ const SidebarFooter = React.forwardRef<
 		<div
 			ref={ref}
 			data-sidebar="footer"
-			className={cn("flex flex-col gap-2 p-2", className)}
+			className={cn("flex flex-col gap-2 p-4", className)}
 			{...props}
 		/>
 	);
@@ -407,7 +407,7 @@ const SidebarGroup = React.forwardRef<
 		<div
 			ref={ref}
 			data-sidebar="group"
-			className={cn("relative flex w-full min-w-0 flex-col p-2", className)}
+			className={cn("relative flex w-full min-w-0 flex-col p-4", className)}
 			{...props}
 		/>
 	);
