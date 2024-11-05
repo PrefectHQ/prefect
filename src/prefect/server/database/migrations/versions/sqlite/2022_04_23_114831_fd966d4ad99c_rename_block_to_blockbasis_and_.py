2f86e865d608f610a0dc596e9cbf9bd1cd5e2912
zzstoatzz <thrast36@gmail.com>
2024-11-05 16:08:33 -0600

    revert potentially dubious sqlite

--- src/prefect/server/database/migrations/versions/sqlite/2022_04_23_114831_fd966d4ad99c_rename_block_to_blockbasis_and_.py
+++ src/prefect/server/database/migrations/versions/sqlite/2022_04_23_114831_fd966d4ad99c_rename_block_to_blockbasis_and_.py
@@ -16,8 +16,6 @@ depends_on = None
 
 
 def upgrade():
-    op.execute("PRAGMA legacy_alter_table = ON")
-
     op.rename_table("block_spec", "block_schema")
     op.rename_table("block", "block_document")
 
@@ -73,8 +71,6 @@ def upgrade():
             ["id"],
             ondelete="cascade",
         )
-
-    op.execute("PRAGMA legacy_alter_table = OFF")
 
 
 def downgrade():
