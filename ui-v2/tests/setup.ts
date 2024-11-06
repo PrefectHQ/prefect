d15135d28c0c9b70138f02f653dfbf46cd762d3c
Alex Streed <alex.s@prefect.io>
2024-11-06 11:53:28 -0600

    Fix build error

--- ui-v2/tests/setup.ts
+++ ui-v2/tests/setup.ts
@@ -1,3 +1,4 @@
+/// <reference lib="dom" />
 import { expect, afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
