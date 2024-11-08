ac9adbf38aee637d891898c2c20fd5f8876b9d92
Andrew Brookins <andrew.b@prefect.io>
2024-11-08 11:52:04 -0800

    Fix the direction of our integration string replacement

--- tests/workers/test_base_worker.py
+++ tests/workers/test_base_worker.py
@@ -1895,11 +1895,11 @@ class TestBaseWorkerHeartbeat:
                 "prefect.workers.base.distributions"
             ) as mock_distributions:
                 mock_load_prefect_collections.return_value = {
-                    "prefect-aws": "1.0.0",
+                    "prefect_aws": "1.0.0",
                 }
                 mock_distributions.return_value = [
                     mock.MagicMock(
-                        metadata={"Name": "prefect_aws"},
+                        metadata={"Name": "prefect-aws"},
                         version="1.0.0",
                     )
                 ]
@@ -1916,7 +1916,7 @@ class TestBaseWorkerHeartbeat:
                         "heartbeat_interval_seconds": worker.heartbeat_interval_seconds,
                         "worker_metadata": {
                             "integrations": [
-                                {"name": "prefect_aws", "version": "1.0.0"}
+                                {"name": "prefect-aws", "version": "1.0.0"}
                             ]
                         },
                         "return_id": True,
@@ -1938,7 +1938,7 @@ class TestBaseWorkerHeartbeat:
             async def _worker_metadata(self) -> WorkerMetadata:
                 return WorkerMetadata(
                     **{
-                        "integrations": [{"name": "prefect-aws", "version": "1.0.0"}],
+                        "integrations": [{"name": "prefect_aws", "version": "1.0.0"}],
                         "custom_field": "heya",
                     }
                 )
@@ -1974,7 +1974,7 @@ class TestBaseWorkerHeartbeat:
                         "heartbeat_interval_seconds": worker.heartbeat_interval_seconds,
                         "worker_metadata": {
                             "integrations": [
-                                {"name": "prefect-aws", "version": "1.0.0"}
+                                {"name": "prefect_aws", "version": "1.0.0"}
                             ],
                             "custom_field": "heya",
                         },
