b59ebf9eaab36537f73f3d0a2083baaf54137728
Alexander Streed <desertaxle@users.noreply.github.com>
2024-11-15 16:09:53 -0600

    [UI v2] Variables table - part 2 (#16025)

--- ui-v2/src/components/variables/data-table/cells.tsx
+++ ui-v2/src/components/variables/data-table/cells.tsx
@@ -49,7 +49,7 @@ export const ActionsCell = ({
 		<div className="flex flex-row justify-end">
 			<DropdownMenu>
 				<DropdownMenuTrigger asChild>
-					<Button variant="ghost" className="h-8 w-8 p-0">
+					<Button variant="outline" className="h-8 w-8 p-0">
 						<span className="sr-only">Open menu</span>
 						<MoreVerticalIcon className="h-4 w-4" />
 					</Button>
---
+++ ui-v2/src/components/variables/data-table/search.tsx
@@ -0,0 +1,57 @@
+import { IconInput } from "@/components/ui/input";
+import {
+	Select,
+	SelectTrigger,
+	SelectValue,
+	SelectItem,
+	SelectContent,
+} from "@/components/ui/select";
+import { TagsInput } from "@/components/ui/tags-input";
+import useDebounce from "@/hooks/use-debounce";
+import { SearchIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+type VariablesDataTableSearchProps = {
+	initialSearchValue: string;
+	onNameSearchChange: (value: string) => void;
+	onSortChange: (value: string) => void;
+};
+
+export const VariablesDataTableSearch = ({
+	initialSearchValue,
+	onNameSearchChange,
+	onSortChange,
+}: VariablesDataTableSearchProps) => {
+	const [searchValue, setSearchValue] = useState(initialSearchValue);
+	const debouncedSearchValue = useDebounce(searchValue, 500);
+
+	useEffect(() => {
+		onNameSearchChange(debouncedSearchValue);
+	}, [debouncedSearchValue, onNameSearchChange]);
+
+	return (
+		<div className="flex items-center gap-2 w-1/2 max-w-md">
+			<IconInput
+				Icon={SearchIcon}
+				placeholder="Search variables"
+				value={searchValue}
+				onChange={(e) => setSearchValue(e.target.value)}
+			/>
+			<TagsInput placeholder="Tags" />
+			<Select
+				onValueChange={(value) => onSortChange(value)}
+				defaultValue="CREATED_DESC"
+			>
+				<SelectTrigger className="w-1/4">
+					<SelectValue placeholder="Sort by" />
+				</SelectTrigger>
+				<SelectContent>
+					<SelectItem value="CREATED_DESC">Created</SelectItem>
+					<SelectItem value="UPDATED_DESC">Updated</SelectItem>
+					<SelectItem value="NAME_ASC">A to Z</SelectItem>
+					<SelectItem value="NAME_DESC">Z to A</SelectItem>
+				</SelectContent>
+			</Select>
+		</div>
+	);
+};
