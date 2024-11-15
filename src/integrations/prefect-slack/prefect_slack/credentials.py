23b7335b07e255c80ad9446fbba68724131058fa
Jeff Hale <jeffmshale@gmail.com>
2024-11-15 16:54:29 -0500

    updates

--- src/integrations/prefect-slack/prefect_slack/credentials.py
+++ src/integrations/prefect-slack/prefect_slack/credentials.py
@@ -36,7 +36,7 @@ class SlackCredentials(Block):
 
     _block_type_name = "Slack Credentials"
     _logo_url = "https://cdn.sanity.io/images/3ugk85nk/production/c1965ecbf8704ee1ea20d77786de9a41ce1087d1-500x500.png"  # noqa
-    _documentation_url = "https://prefecthq.github.io/prefect-slack/credentials/#prefect_slack.credentials.SlackCredentials"  # noqa
+    _documentation_url = "https://docs.prefect.io/integrations/prefect-slack"  # noqa
 
     token: SecretStr = Field(
         default=...,
@@ -90,7 +90,7 @@ class SlackWebhook(NotificationBlock):
 
     _block_type_name = "Slack Incoming Webhook"
     _logo_url = "https://images.ctfassets.net/gm98wzqotmnx/7dkzINU9r6j44giEFuHuUC/85d4cd321ad60c1b1e898bc3fbd28580/5cb480cd5f1b6d3fbadece79.png?h=250"  # noqa
-    _documentation_url = "https://prefecthq.github.io/prefect-slack/credentials/#prefect_slack.credentials.SlackWebhook"  # noqa
+    _documentation_url = "https://docs.prefect.io/integrations/prefect-slack"  # noqa
 
     url: SecretStr = Field(
         default=...,
