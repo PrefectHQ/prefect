b979ca5a8731554bf990e01fe75dea9e92d0742d
nate nowack <thrast36@gmail.com>
2024-11-21 13:05:47 -0600

    allow filtering by flow run tags via `any_` (#16072)

--- src/prefect/client/schemas/filters.py
+++ src/prefect/client/schemas/filters.py
@@ -129,6 +129,11 @@ class FlowRunFilterTags(PrefectBaseModel, OperatorMixin):
             " superset of the list"
         ),
     )
+    any_: Optional[List[str]] = Field(
+        default=None,
+        examples=[["tag-1", "tag-2"]],
+        description="A list of tags to include",
+    )
     is_null_: Optional[bool] = Field(
         default=None, description="If true, only include flow runs without tags"
     )
