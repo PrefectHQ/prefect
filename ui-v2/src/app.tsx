23a1bc05ea4fde09c31ec838a3a64eb3eb012404
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-11-27 09:26:47 -0800

    [UI v2] feat: Adds tanstack query dev tools for development mode (#16131)

--- ui-v2/src/app.tsx
+++ ui-v2/src/app.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "@tanstack/react-router";
 import { queryClient, router } from "./router";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const App = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
+			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);
 };
