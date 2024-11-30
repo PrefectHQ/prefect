9b4948f22c14cc6c8b2d48d0c460be8377d2bfc3
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-11-30 12:37:57 -0500

    detail data

--- ui-v2/src/routes/blocks/$id.tsx
+++ ui-v2/src/routes/blocks/$id.tsx
@@ -1,11 +1,37 @@
 import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/blocks/$id")({
-	component: RouteComponent,
-});
+import { useBlockDocuments, buildBlockDocumentsQuery } from "@/hooks/use-block-documents";
 
 function RouteComponent() {
 	const { id } = Route.useParams();
-	return <div>Block ID: {id}</div>;
+	const { blockDocuments: [block] } = useBlockDocuments({
+		blockDocuments: {
+			operator: "and_", 
+			is_anonymous: { eq_: false },
+			id: { any_: [id] },
+		},
+	});
+
+	return (
+		<div>
+			<h2>Block ID: {id}</h2>
+			<pre>{JSON.stringify(block, null, 2)}</pre>
+		</div>
+	);
 }
+
+export const Route = createFileRoute("/blocks/$id")({
+	component: RouteComponent,
+	loader: ({ context, params }) => {
+		return context.queryClient.ensureQueryData(
+			buildBlockDocumentsQuery({
+		blockDocuments: {
+			operator: "and_", 
+			is_anonymous: { eq_: false },
+			id: { any_: [params.id] },
+		},
+			})
+		);
+	},
+	wrapInSuspense: true,
+});
