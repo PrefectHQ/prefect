0984a3a36feddf333b9b61aed00bcf8f7aa50d9a
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2024-11-30 12:41:08 -0500

    create

--- ui-v2/src/routes/blocks/$id.tsx
+++ ui-v2/src/routes/blocks/$id.tsx
@@ -1,12 +1,17 @@
 import * as React from "react";
 import { createFileRoute } from "@tanstack/react-router";
-import { useBlockDocuments, buildBlockDocumentsQuery } from "@/hooks/use-block-documents";
+import {
+	useBlockDocuments,
+	buildBlockDocumentsQuery,
+} from "@/hooks/use-block-documents";
 
 function RouteComponent() {
 	const { id } = Route.useParams();
-	const { blockDocuments: [block] } = useBlockDocuments({
+	const {
+		blockDocuments: [block],
+	} = useBlockDocuments({
 		blockDocuments: {
-			operator: "and_", 
+			operator: "and_",
 			is_anonymous: { eq_: false },
 			id: { any_: [id] },
 		},
@@ -25,12 +30,12 @@ export const Route = createFileRoute("/blocks/$id")({
 	loader: ({ context, params }) => {
 		return context.queryClient.ensureQueryData(
 			buildBlockDocumentsQuery({
-		blockDocuments: {
-			operator: "and_", 
-			is_anonymous: { eq_: false },
-			id: { any_: [params.id] },
-		},
-			})
+				blockDocuments: {
+					operator: "and_",
+					is_anonymous: { eq_: false },
+					id: { any_: [params.id] },
+				},
+			}),
 		);
 	},
 	wrapInSuspense: true,
---
+++ ui-v2/src/routes/blocks/catalog_/$name_.create.tsx
@@ -0,0 +1,10 @@
+
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/blocks/catalog_/$name_/create')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return 'Hello /blocks/catalog_/$name_/create!'
+}
