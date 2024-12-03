c81238774341e72bb7bbcba7c44482270d21ec6e
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-03 14:48:01 -0800

    [UI v2] feat: Adding generic empty-state component, and replaced variables empty-state for it (#16193)

---
+++ ui-v2/src/components/ui/empty-state.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { PlusIcon } from "lucide-react";
+import { Button } from "./button";
+import { DocsLink } from "./docs-link";
+import {
+	EmptyState,
+	EmptyStateActions,
+	EmptyStateDescription,
+	EmptyStateIcon,
+	EmptyStateTitle,
+} from "./empty-state";
+
+const meta: Meta<typeof EmptyState> = {
+	title: "UI/EmptyState",
+	component: EmptyState,
+	parameters: {
+		docs: {
+			description: {
+				component:
+					"EmptyState is used to prompt to user to create a resource when there is none",
+			},
+		},
+	},
+};
+export default meta;
+
+type Story = StoryObj<typeof EmptyState>;
+export const Usage: Story = {
+	render: () => <EmptyStateExample />,
+};
+
+function EmptyStateExample(): JSX.Element {
+	return (
+		<EmptyState>
+			<EmptyStateIcon id="Variable" />
+			<EmptyStateTitle>Add a variable to get started</EmptyStateTitle>
+			<EmptyStateDescription>
+				Variables store non-sensitive pieces of JSON.
+			</EmptyStateDescription>
+			<EmptyStateActions>
+				<Button onClick={console.log}>
+					Add Variable <PlusIcon className="h-4 w-4 ml-2" />
+				</Button>
+				<DocsLink id="variables-guide" />
+			</EmptyStateActions>
+		</EmptyState>
+	);
+}
