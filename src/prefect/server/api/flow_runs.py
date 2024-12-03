975521635ac398d9466f5e4a23a3314b509a13ce
Chris Pickett <chris.pickett@prefect.io>
2024-12-03 09:51:50 -0500

    Explicitly use a transaction when updating labels

--- src/prefect/server/api/flow_runs.py
+++ src/prefect/server/api/flow_runs.py
@@ -846,7 +846,7 @@ async def update_flow_run_labels(
     labels: Dict[str, Any] = Body(..., description="The labels to update"),
     db: PrefectDBInterface = Depends(provide_database_interface),
 ):
-    async with db.session_context() as session:
+    async with db.session_context(begin_transaction=True) as session:
         logger.info(f"Updating flow run {flow_run_id} with labels {labels}")
         await models.flow_runs.update_flow_run_labels(
             session=session, flow_run_id=flow_run_id, labels=labels
