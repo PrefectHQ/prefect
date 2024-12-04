d24b85270800bd43cabf44d33f04f646cf2c4139
Evan Liu <evancoding@gmail.com>
2024-12-04 17:46:05 +1300

    Create stateBadgeVariants with cva()

--- ui-v2/src/components/ui/state-badge/index.tsx
+++ ui-v2/src/components/ui/state-badge/index.tsx
@@ -1,14 +1,15 @@
-import { Badge } from "../badge";
 import type { components } from "@/api/prefect";
+import { cva } from "class-variance-authority";
 import {
+	BanIcon,
+	CheckIcon,
 	ClockIcon,
 	PauseIcon,
-	XIcon,
-	CheckIcon,
-	ServerCrashIcon,
-	BanIcon,
 	PlayIcon,
+	ServerCrashIcon,
+	XIcon,
 } from "lucide-react";
+import { Badge } from "../badge";
 
 const ICONS = {
 	COMPLETED: CheckIcon,
@@ -25,29 +26,31 @@ const ICONS = {
 	React.ElementType
 >;
 
-const CLASSES = {
-	COMPLETED: "bg-green-50 text-green-600 hover:bg-green-50",
-	FAILED: "bg-red-50 text-red-600 hover:bg-red-50",
-	RUNNING: "bg-blue-100 text-blue-700 hover:bg-blue-100",
-	CANCELLED: "bg-gray-300 text-gray-800 hover:bg-gray-300",
-	CANCELLING: "bg-gray-300 text-gray-800 hover:bg-gray-300",
-	CRASHED: "bg-orange-50 text-orange-600 hover:bg-orange-50",
-	PAUSED: "bg-gray-300 text-gray-800 hover:bg-gray-300",
-	PENDING: "bg-gray-300 text-gray-800 hover:bg-gray-300",
-	SCHEDULED: "bg-yellow-100 text-yellow-700 hover:bg-yellow-100",
-} as const satisfies Record<components["schemas"]["StateType"], string>;
+const stateBadgeVariants = cva("gap-1", {
+	variants: {
+		state: {
+			COMPLETED: "bg-green-50 text-green-600 hover:bg-green-50",
+			FAILED: "bg-red-50 text-red-600 hover:bg-red-50",
+			RUNNING: "bg-blue-100 text-blue-700 hover:bg-blue-100",
+			CANCELLED: "bg-gray-300 text-gray-800 hover:bg-gray-300",
+			CANCELLING: "bg-gray-300 text-gray-800 hover:bg-gray-300",
+			CRASHED: "bg-orange-50 text-orange-600 hover:bg-orange-50",
+			PAUSED: "bg-gray-300 text-gray-800 hover:bg-gray-300",
+			PENDING: "bg-gray-300 text-gray-800 hover:bg-gray-300",
+			SCHEDULED: "bg-yellow-100 text-yellow-700 hover:bg-yellow-100",
+		} satisfies Record<components["schemas"]["StateType"], string>,
+	},
+});
 
 export const StateBadge = ({
 	state,
 }: { state: components["schemas"]["State"] }) => {
 	const Icon = ICONS[state.type];
 	return (
-		<Badge className={CLASSES[state.type]}>
-			<div className="flex items-center gap-1">
-				<Icon size={16} />
+		<Badge className={stateBadgeVariants({ state: state.type })}>
+			<Icon size={16} />
 
-				{state.name}
-			</div>
+			{state.name}
 		</Badge>
 	);
 };
