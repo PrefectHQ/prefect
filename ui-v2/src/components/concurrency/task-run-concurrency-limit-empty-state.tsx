08d93733f0858473b116b5f7675d562b1a4aa7bc
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-04 07:11:03 -0800

    [UI v2] feat: Adds EmptyState for Task Run Concurrency Limits (#16203)

---
+++ ui-v2/src/components/concurrency/task-run-concurrency-limit-empty-state.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import { DocsLink } from "@/components/ui/docs-link";
+import {
+	EmptyState,
+	EmptyStateActions,
+	EmptyStateDescription,
+	EmptyStateIcon,
+	EmptyStateTitle,
+} from "@/components/ui/empty-state";
+import { PlusIcon } from "lucide-react";
+
+type Props = {
+	onClick: () => void;
+};
+export const TaskRunConcurrencyLimitEmptyState = ({ onClick }: Props) => (
+	<EmptyState>
+		<EmptyStateIcon id="CircleArrowOutUpRight" />
+		<EmptyStateTitle>
+			Add a concurrency limit for your task runs
+		</EmptyStateTitle>
+		<EmptyStateDescription>
+			Creating a limit allows you to limit the number of tasks running
+			simultaneously with a given tag.
+		</EmptyStateDescription>
+		<EmptyStateActions>
+			<Button onClick={onClick}>
+				Add Concurrency Limit <PlusIcon className="h-4 w-4 ml-2" />
+			</Button>
+			<DocsLink id="task-concurrency-guide" />
+		</EmptyStateActions>
+	</EmptyState>
+);
