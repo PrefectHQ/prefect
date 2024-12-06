ad1cf8e733152d4f372d781dd353faf7a80cd42e
Devin Villarosa <devinvillarosa92.business@gmail.com>
2024-12-05 22:02:24 -0800

    [UI v2] Adds Create or edit dialog for concurrency limit

--- ui-v2/src/components/concurrency/global-concurrency-view/index.tsx
+++ ui-v2/src/components/concurrency/global-concurrency-view/index.tsx
@@ -1,28 +1,39 @@
+import { Flex } from "@/components/ui/flex";
 import { useListGlobalConcurrencyLimits } from "@/hooks/global-concurrency-limits";
 import { useState } from "react";
-
+import { CreateOrEditLimitDialog } from "./create-or-edit-limit-dialog";
+import { GlobalConcurrencyLimitEmptyState } from "./global-concurrency-limit-empty-state";
 import { GlobalConcurrencyLimitsHeader } from "./global-concurrency-limits-header";
 
 export const GlobalConcurrencyView = () => {
-	const [showAddDialog, setShowAddDialog] = useState(false);
+	const [openDialog, setOpenDialog] = useState(false);
 
 	const { data } = useListGlobalConcurrencyLimits();
 
-	const openAddDialog = () => setShowAddDialog(true);
-	const closeAddDialog = () => setShowAddDialog(false);
+	const openAddDialog = () => setOpenDialog(true);
+	const closeAddDialog = () => setOpenDialog(false);
 
 	return (
 		<>
-			<div className="flex flex-col gap-2">
-				<GlobalConcurrencyLimitsHeader onAdd={openAddDialog} />
-			</div>
-			<div>TODO</div>
-			<ul>
-				{data.map((limit) => (
-					<li key={limit.id}>{JSON.stringify(limit)}</li>
-				))}
-			</ul>
-			{showAddDialog && <div onClick={closeAddDialog}>TODO: DIALOG</div>}
+			{data.length === 0 ? (
+				<GlobalConcurrencyLimitEmptyState onAdd={openAddDialog} />
+			) : (
+				<Flex flexDirection="column" gap={2}>
+					<GlobalConcurrencyLimitsHeader onAdd={openAddDialog} />
+					<div>TODO</div>
+					<ul>
+						{data.map((limit) => (
+							<li key={limit.id}>{JSON.stringify(limit)}</li>
+						))}
+					</ul>
+				</Flex>
+			)}
+			<CreateOrEditLimitDialog
+				open={openDialog}
+				onOpenChange={setOpenDialog}
+				limitToUpdate={undefined /** TODO:  */}
+				onSubmit={closeAddDialog}
+			/>
 		</>
 	);
 };
