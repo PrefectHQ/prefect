a6cee357d99317801ad3d3182fe8e526c3ef9e2a
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-06 09:58:02 -0800

    [UI v2] feat: Loads data and url state to concurrency limit route for UX to consume (#16227)

--- ui-v2/src/components/concurrency/concurrency-tabs.tsx
+++ ui-v2/src/components/concurrency/concurrency-tabs.tsx
@@ -1,34 +1,81 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { TAB_OPTIONS, TabOptions } from "./concurrency-constants";
+import { TabOptions } from "@/routes/concurrency-limits";
+import { getRouteApi } from "@tanstack/react-router";
+
+const routeApi = getRouteApi("/concurrency-limits");
+
+type TabOptionValues = {
+	/** Value of search value in url */
+	tabSearchValue: TabOptions;
+	/** Display value for the UI */
+	displayValue: string;
+};
+
+/** Maps url tab option to visual name */
+const TAB_OPTIONS: Record<TabOptions, TabOptionValues> = {
+	global: {
+		tabSearchValue: "global",
+		displayValue: "Global",
+	},
+	["task-run"]: {
+		tabSearchValue: "task-run",
+		displayValue: "Task Run",
+	},
+} as const;
 
 type Props = {
 	globalView: React.ReactNode;
-	onValueChange: (value: TabOptions) => void;
 	taskRunView: React.ReactNode;
-	value: TabOptions;
 };
 
 // TODO: Move Tabs for navigation to a generic styled component
 
 export const ConcurrencyTabs = ({
 	globalView,
-	onValueChange,
 	taskRunView,
-	value,
 }: Props): JSX.Element => {
+	const { tab } = routeApi.useSearch();
+	const navigate = routeApi.useNavigate();
+
 	return (
-		<Tabs
-			defaultValue="Global"
-			className="w-[400px]"
-			value={value}
-			onValueChange={(value) => onValueChange(value as TabOptions)}
-		>
+		<Tabs defaultValue="Global" value={tab}>
 			<TabsList className="grid w-full grid-cols-2">
-				<TabsTrigger value="Global">{TAB_OPTIONS.Global}</TabsTrigger>
-				<TabsTrigger value="Task Run">{TAB_OPTIONS["Task Run"]}</TabsTrigger>
+				<TabsTrigger
+					value={TAB_OPTIONS.global.tabSearchValue}
+					onClick={() => {
+						void navigate({
+							to: "/concurrency-limits",
+							search: (prev) => ({
+								...prev,
+								tab: TAB_OPTIONS.global.tabSearchValue,
+							}),
+						});
+					}}
+				>
+					{TAB_OPTIONS.global.displayValue}
+				</TabsTrigger>
+
+				<TabsTrigger
+					value={TAB_OPTIONS["task-run"].tabSearchValue}
+					onClick={() => {
+						void navigate({
+							to: "/concurrency-limits",
+							search: (prev) => ({
+								...prev,
+								tab: TAB_OPTIONS["task-run"].tabSearchValue,
+							}),
+						});
+					}}
+				>
+					{TAB_OPTIONS["task-run"].displayValue}
+				</TabsTrigger>
 			</TabsList>
-			<TabsContent value={TAB_OPTIONS.Global}>{globalView}</TabsContent>
-			<TabsContent value={TAB_OPTIONS["Task Run"]}>{taskRunView}</TabsContent>
+			<TabsContent value={TAB_OPTIONS.global.tabSearchValue}>
+				{globalView}
+			</TabsContent>
+			<TabsContent value={TAB_OPTIONS["task-run"].tabSearchValue}>
+				{taskRunView}
+			</TabsContent>
 		</Tabs>
 	);
 };
