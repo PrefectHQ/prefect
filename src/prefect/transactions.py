4505a16d4512c8a519010ae1885905e2d78c912b
Andrew Brookins <andrew.b@prefect.io>
2024-12-06 00:47:21 -0800

    remove scaffolding

--- src/prefect/transactions.py
+++ src/prefect/transactions.py
@@ -316,21 +316,17 @@ class Transaction(ContextModel):
             if self.store and self.key and self.write_on_commit:
                 if isinstance(self.store, ResultStore):
                     if isinstance(self._staged_value, BaseResult):
-                        self.store.write(  # remote write event
+                        self.store.write(
                             key=self.key, obj=self._staged_value.get(_sync=True)
                         )
                     elif isinstance(self._staged_value, ResultRecord):
-                        self.store.persist_result_record(  # remote write event
+                        self.store.persist_result_record(
                             result_record=self._staged_value
                         )
                     else:
-                        self.store.write(
-                            key=self.key, obj=self._staged_value
-                        )  # remote write event
+                        self.store.write(key=self.key, obj=self._staged_value)
                 else:
-                    self.store.write(
-                        key=self.key, result=self._staged_value
-                    )  # remote write event -- ignore because this is the record store
+                    self.store.write(key=self.key, result=self._staged_value)
 
             self.state = TransactionState.COMMITTED
             if (
