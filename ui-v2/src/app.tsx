6931baef65078a209cd51a3cedaa9dac2f6edb31
Alex Streed <alex.s@prefect.io>
2024-12-09 10:33:42 -0600

    Remove unnecessary file changes

--- ui-v2/src/app.tsx
+++ ui-v2/src/app.tsx
@@ -1,10 +1,9 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import { queryClient, router } from "./router";
 
 export const App = () => {
-	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
