404090bb0970e617e81513be05aa9db9c9900178
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-10 14:09:59 -0800

    [UI v2] feat: Adds data and UX for task run concurrency limit (#16287)

--- ui-v2/src/components/concurrency/task-run-concurrenct-view/header.tsx
+++ ui-v2/src/components/concurrency/task-run-concurrenct-view/header.tsx
@@ -6,14 +6,14 @@ type Props = {
 	onAdd: () => void;
 };
 
-export const GlobalConcurrencyLimitsHeader = ({ onAdd }: Props) => {
+export const TaskRunConcurrencyLimitsHeader = ({ onAdd }: Props) => {
 	return (
 		<div className="flex gap-2 items-center">
-			<Typography variant="h4">Global Concurrency Limits</Typography>
+			<Typography variant="h4">Task Run Concurrency Limits</Typography>
 			<Button
 				onClick={onAdd}
 				size="icon"
-				aria-label="add global concurrency limit"
+				aria-label="add task run concurrency limit"
 			>
 				<Icon id="Plus" className="h-4 w-4" />
 			</Button>
