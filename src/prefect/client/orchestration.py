612106cb5ae855cd517008499a518812ea477b21
Paweł Należyty <155740333+paulusaptus@users.noreply.github.com>
2024-12-10 15:00:28 +0100

    Support flow deletion in Prefect Python client (#16308)
    Co-authored-by: nate nowack <nate@prefect.io>

--- src/prefect/client/orchestration.py
+++ src/prefect/client/orchestration.py
@@ -501,6 +501,24 @@ class PrefectClient:
         response = await self._client.get(f"/flows/{flow_id}")
         return Flow.model_validate(response.json())
 
+    async def delete_flow(self, flow_id: UUID) -> None:
+        """
+        Delete a flow by UUID.
+
+        Args:
+            flow_id: ID of the flow to be deleted
+        Raises:
+            prefect.exceptions.ObjectNotFound: If request returns 404
+            httpx.RequestError: If requests fail
+        """
+        try:
+            await self._client.delete(f"/flows/{flow_id}")
+        except httpx.HTTPStatusError as e:
+            if e.response.status_code == status.HTTP_404_NOT_FOUND:
+                raise prefect.exceptions.ObjectNotFound(http_exc=e) from e
+            else:
+                raise
+
     async def read_flows(
         self,
         *,
