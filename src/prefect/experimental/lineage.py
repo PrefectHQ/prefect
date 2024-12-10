4feda852e44cf64dfed84b7562260b09f3b162ea
Andrew Brookins <andrew.b@prefect.io>
2024-12-10 15:43:19 -0800

    try a different explanation about upstream/downstream

--- src/prefect/experimental/lineage.py
+++ src/prefect/experimental/lineage.py
@@ -49,7 +49,7 @@ async def emit_lineage_event(
     event_name: str,
     upstream_resources: Optional[UpstreamResources] = None,
     downstream_resources: Optional[DownstreamResources] = None,
-    direction_of_related_resources: Literal["upstream", "downstream"] = "upstream",
+    direction_of_run_from_event: Literal["upstream", "downstream"] = "downstream",
 ) -> None:
     """Emit lineage events showing relationships between resources.
 
@@ -59,34 +59,42 @@ async def emit_lineage_event(
             the event
         downstream_resources: Optional list of Resources that were downstream
             of the event
+        direction_of_run_from_event: The direction of the current run from
+            the event. E.g., if we're in a flow run and
+            `direction_of_run_from_event` is "downstream", then the flow run is
+            considered downstream of the resource's event.
     """
     if not get_current_settings().experiments.lineage_events_enabled:
         return
 
+    involved_resources = None
+
     upstream_resources = list(upstream_resources) if upstream_resources else []
     downstream_resources = list(downstream_resources) if downstream_resources else []
 
-    if direction_of_related_resources == "downstream":
-        # Set the involved resources to force the EventsWorker to exclude these
-        # from the related resources. Related resources are always considered
-        # "upstream," but in some cases, we instead want to include these as
-        # downstream resources.
+    # The EventsWorker automatically includes run-related resources as upstream
+    # resources for this event by adding them to `event.related`. However, if
+    # the current run is downstream of the event, run-related resources should
+    # be downstream, not upstream. So here, we set `involved_resources` to
+    # force the EventsWorker to avoid adding run-related resources to the event.
+    # This function will handle emitting a downstream event for each downstream
+    # resource instead.
+    if direction_of_run_from_event == "downstream":
         from prefect.client.orchestration import get_client  # Avoid a circular import
 
         async with get_client() as client:
             involved_resources = await related_resources_from_run_context(client)
         downstream_resources.extend(involved_resources)
-    else:
-        # The EventsWorker will automatically include these as as upstream
-        # resources for us, so no action is necessary.
-        involved_resources = None
 
     # Emit an event for each downstream resource. This is necessary because
     # our event schema allows one primary resource and many related resources,
     # and for the purposes of lineage, related resources can only represent
     # upstream resources.
     for resource in downstream_resources:
-        # Downstream resources need to have the prefect.resource.lineage-group label.
+        # Downstream lineage resources need to have the
+        # prefect.resource.lineage-group label. All upstram resources from a
+        # downstream resource with this label will be considered lineage-related
+        # resources.
         if "prefect.resource.lineage-group" not in resource:
             resource["prefect.resource.lineage-group"] = "global"
 
@@ -114,8 +122,8 @@ async def emit_result_read_event(
     Args:
         store: A `ResultStore` instance.
         result_key: The key of the result to generate a URI for.
-        downstream_resources: List of Resource objects that were
-            downstream of the event
+        downstream_resources: List of resources that were
+            downstream of the event's resource.
     """
     if not get_current_settings().experiments.lineage_events_enabled:
         return
@@ -138,7 +146,7 @@ async def emit_result_read_event(
             event_name=event_name,
             upstream_resources=upstream_resources,
             downstream_resources=downstream_resources,
-            direction_of_related_resources="downstream",
+            direction_of_run_from_event="downstream",
         )
 
 
@@ -148,13 +156,13 @@ async def emit_result_write_event(
     upstream_resources: Optional[UpstreamResources] = None,
 ) -> None:
     """
-    Emit a lineage event showing a result was written
+    Emit a lineage event showing a task or flow result was written.
 
     Args:
         store: A `ResultStore` instance.
         result_key: The key of the result to generate a URI for.
-        upstream_resources: Optional list of RelatedResources that were
-            upstream of the event
+        upstream_resources: Optional list of resources that were
+            upstream of the event's resource.
     """
     if not get_current_settings().experiments.lineage_events_enabled:
         return
@@ -172,4 +180,5 @@ async def emit_result_write_event(
             event_name="prefect.result.write",
             upstream_resources=upstream_resources,
             downstream_resources=downstream_resources,
+            direction_of_run_from_event="upstream",
         )
