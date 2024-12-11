ab420f4458eeb93855ad5502ec52aef7fe78d570
Alex Streed <alex.s@prefect.io>
2024-12-11 16:35:57 -0600

    Add `StatusBadge` component

--- ui-v2/src/components/deployments/data-table/data-table.stories.tsx
+++ ui-v2/src/components/deployments/data-table/data-table.stories.tsx
@@ -1,15 +1,32 @@
+import type { DeploymentWithFlow } from "@/hooks/deployments";
+import { faker } from "@faker-js/faker";
 import type { Meta, StoryObj } from "@storybook/react";
 import { DeploymentsDataTable } from ".";
 
 export default {
 	title: "Components/Deployments/DataTable",
 	component: DeploymentsDataTable,
-	parameters: {
-		layout: "centered",
-	},
 } satisfies Meta<typeof DeploymentsDataTable>;
+
+function createRandomDeployment(): DeploymentWithFlow {
+	return {
+		id: faker.string.uuid(),
+		name: faker.airline.airplane().name,
+		flow_id: faker.string.uuid(),
+		paused: faker.datatype.boolean(),
+		status: faker.helpers.arrayElement(["READY", "NOT_READY"]),
+		enforce_parameter_schema: faker.datatype.boolean(),
+		tags: Array.from({ length: faker.number.int({ min: 0, max: 3 }) }, () =>
+			faker.lorem.word(),
+		),
+		flow: {
+			id: faker.string.uuid(),
+			name: faker.company.catchPhrase().toLowerCase().replace(/\s+/g, "-"),
+		},
+	};
+}
 
 export const Default: StoryObj = {
 	name: "DataTable",
-	args: { deployments: [] },
+	args: { deployments: Array.from({ length: 10 }, createRandomDeployment) },
 };
