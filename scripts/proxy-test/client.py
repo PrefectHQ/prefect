6760f555a945bec46ce46754a57b26e63fe36487
jake@prefect.io <jake@prefect.io>
2024-12-12 00:10:46 -0500

    small changes

--- scripts/proxy-test/client.py
+++ scripts/proxy-test/client.py
@@ -9,13 +9,14 @@ WS_SERVER_URL = "ws://server:8000/ws"
 
 async def test_websocket_proxy_with_compat():
     """WebSocket through proxy with proxy compatibility code - should work"""
-    os.environ["HTTP_PROXY"] = "http://localhost:3128"
+    os.environ["HTTP_PROXY"] = PROXY_URL
 
-    async with websocket_connect("ws://server:8000/ws") as websocket:
-        await websocket.send("Hello!")
+    async with websocket_connect(WS_SERVER_URL) as websocket:
+        message = "Hello!"
+        await websocket.send(message)
         response = await websocket.recv()
         print("Response: ", response)
-        assert response == "Server received: Hello!"
+        assert response == f"Server received: {message}"
 
 
 async def main():
