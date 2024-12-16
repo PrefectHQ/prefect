7e07399151229d0bf197ee6658d6d78b75f5ab44
Chris Pickett <chris.pickett@prefect.io>
2024-12-16 10:03:26 -0500

    Correctly maintain span hierarchy when instrumenting nested tasks/flows (#16384)

--- src/prefect/flow_engine.py
+++ src/prefect/flow_engine.py
@@ -656,17 +656,12 @@ class FlowRunEngine(BaseFlowRunEngine[P, R]):
                     flow_version=self.flow.version,
                     empirical_policy=self.flow_run.empirical_policy,
                 )
-            parent_flow_run = FlowRunContext.get()
-            parent_labels = {}
-            if parent_flow_run and parent_flow_run.flow_run:
-                parent_labels = parent_flow_run.flow_run.labels
 
             self._telemetry.start_span(
                 name=self.flow.name,
                 run=self.flow_run,
                 client=self.client,
                 parameters=self.parameters,
-                parent_labels=parent_labels,
             )
 
             try:
@@ -1223,17 +1218,12 @@ class AsyncFlowRunEngine(BaseFlowRunEngine[P, R]):
                     flow_version=self.flow.version,
                     empirical_policy=self.flow_run.empirical_policy,
                 )
-            parent_flow_run = FlowRunContext.get()
-            parent_labels = {}
-            if parent_flow_run and parent_flow_run.flow_run:
-                parent_labels = parent_flow_run.flow_run.labels
 
             await self._telemetry.async_start_span(
                 name=self.flow.name,
                 run=self.flow_run,
                 client=self.client,
                 parameters=self.parameters,
-                parent_labels=parent_labels,
             )
 
             try:
