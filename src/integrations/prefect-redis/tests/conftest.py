984ca0c1fdfcc8dfdadc1576e1f41cc8fb7062e9
zangell44 <zachary.james.angell@gmail.com>
2024-12-17 11:53:06 -0800

    Fix pytest asyncio collection

--- src/integrations/prefect-redis/tests/conftest.py
+++ src/integrations/prefect-redis/tests/conftest.py
@@ -1,10 +1,28 @@
+import asyncio
+import sys
 from typing import AsyncGenerator, Generator
 
 import pytest
 from prefect_redis.client import close_all_cached_connections, get_async_redis_client
+from pytest_asyncio import is_async_test
 from redis.asyncio import Redis
 
 from prefect.testing.utilities import prefect_test_harness
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]):
+    # Ensure that all async tests are run with the session loop scope
+    pytest_asyncio_tests = [item for item in items if is_async_test(item)]
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
+
+
+@pytest.fixture(scope="session")
+def event_loop_policy():
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    return asyncio.get_event_loop_policy()
 
 
 @pytest.fixture(scope="session", autouse=True)
