61b3ad8484b67af505334d40d3e2c899db0f1318
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-20 14:38:11 -0800

    [UI v2] feat: Adds router and react-query storybook decorator (#16469)

---
+++ ui-v2/src/storybook/utils/router-decorator.tsx
@@ -0,0 +1,74 @@
+/* eslint-disable react-refresh/only-export-components */
+import {
+	RouterProvider,
+	createMemoryHistory,
+	createRootRoute,
+	createRoute,
+	createRouter,
+	useRouterState,
+} from "@tanstack/react-router";
+import { type ReactNode, createContext, useContext } from "react";
+
+//#region Dummy story router
+function RenderStory() {
+	const storyFn = useContext(CurrentStoryContext);
+	if (!storyFn) {
+		throw new Error("Storybook root not found");
+	}
+	return storyFn();
+}
+
+export const CurrentStoryContext = createContext<(() => ReactNode) | undefined>(
+	undefined,
+);
+
+function NotFoundComponent() {
+	const state = useRouterState();
+	return (
+		<div>
+			<i>Warning:</i> Simulated route not found for path{" "}
+			<code>{state.location.href}</code>
+		</div>
+	);
+}
+
+const storyPath = "/__story__";
+const storyRoute = createRoute({
+	path: storyPath,
+	getParentRoute: () => rootRoute,
+	component: RenderStory,
+});
+
+const rootRoute = createRootRoute({
+	notFoundComponent: NotFoundComponent,
+});
+rootRoute.addChildren([storyRoute]);
+
+export const storyRouter = createRouter({
+	history: createMemoryHistory({ initialEntries: [storyPath] }),
+	routeTree: rootRoute,
+});
+//#endregion
+
+/**
+ *
+ * @example
+ * ```ts
+ * import { routerDecorator } from '@/storybook/utils'
+ *
+ * const meta: Meta<typeof MyComponent> = {
+ * 	title: "UI/MyComponent",
+ * 	decorators: [routerDecorator],
+ * 	component: MyComponent,
+ * };
+ *
+ * export default meta;
+ */
+export function routerDecorator(storyFn: () => ReactNode) {
+	return (
+		<CurrentStoryContext.Provider value={storyFn}>
+			{/** @ts-expect-error Ignoring for now to get decorator working */}
+			<RouterProvider router={storyRouter} />
+		</CurrentStoryContext.Provider>
+	);
+}
