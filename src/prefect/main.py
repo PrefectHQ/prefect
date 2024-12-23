859e02d61d77469df0b22cf5128f95958dc44aa5
zzstoatzz <thrast36@gmail.com>
2024-12-23 13:35:25 -0600

    organize
    simplify
    
    revert testing changes

--- src/prefect/main.py
+++ src/prefect/main.py
@@ -1,11 +1,3 @@
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.instrumentation.sqlalchemy import SQLAlchemyInstrumentor
-
-instrumentor = FastAPIInstrumentor()
-instrumentor.instrument()
-SQLAlchemyInstrumentor().instrument()
-
-
 # Import user-facing API
 from typing import Any
 
@@ -57,9 +49,9 @@ prefect.logging.get_logger("profiles").debug(
 )
 
 # Configure telemetry
-# import prefect.telemetry.bootstrap
+import prefect.telemetry.bootstrap
 
-# prefect.telemetry.bootstrap.setup_telemetry()
+prefect.telemetry.bootstrap.setup_telemetry()
 
 
 from prefect._internal.compatibility.deprecated import (
