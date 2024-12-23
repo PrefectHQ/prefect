14d478993de13c9e205f29a0fd5992b70cc1f977
Dylan Hughes <dylanbhughes@gmail.com>
2024-12-23 11:59:22 -0500

    remove the notification block id and adjust duration for SLA

--- src/prefect/_experimental/sla.py
+++ src/prefect/_experimental/sla.py
@@ -1,6 +1,4 @@
-from datetime import timedelta
 from typing import Optional
-from uuid import UUID
 
 from pydantic import Field
 
@@ -14,10 +12,6 @@ class Sla(ActionBaseModel):
         default=...,
         description="The name of the SLA. Names must be unique on a per-deployment basis.",
     )
-    notification_block_id: Optional[UUID] = Field(
-        default=None,
-        description="The ID of the block document to use for notifications when the SLA is violated.",
-    )
     enabled: Optional[bool] = Field(
         default=True,
         description="Whether the SLA is enabled.",
@@ -27,7 +21,7 @@ class Sla(ActionBaseModel):
 class TimeToCompletionSla(Sla):
     """An SLA that triggers when a flow run takes longer than the specified duration."""
 
-    duration: timedelta = Field(
+    duration: int = Field(
         default=...,
-        description="The maximum flow run duration allowed before the SLA is violated.",
+        description="The maximum flow run duration allowed before the SLA is violated, expressed in seconds.",
     )
