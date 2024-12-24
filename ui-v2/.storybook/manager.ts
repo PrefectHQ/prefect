cbe7dd43d8459896345b2edf129d96cc7c2b39df
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2024-12-24 08:11:19 -0800

    [UI v2] feat: Adds prefect theming to storybook (#16482)

---
+++ ui-v2/.storybook/manager.ts
@@ -0,0 +1,14 @@
+import { addons } from "@storybook/manager-api";
+
+import favicon from "../public/ico/favicon-16x16-dark.png";
+import { prefectLightTheme } from "./prefect-theme";
+
+addons.setConfig({
+	theme: prefectLightTheme,
+});
+
+// custom favicon
+const link = document.createElement("link");
+link.setAttribute("rel", "shortcut icon");
+link.setAttribute("href", favicon);
+document.head.appendChild(link);
