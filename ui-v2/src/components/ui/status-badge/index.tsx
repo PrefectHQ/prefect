4d137668f53fd453334b36c172e9a900d1a12f6b
Alexander Streed <desertaxle@users.noreply.github.com>
2024-12-26 11:55:26 -0600

    `DeploymentsDataTable` - Part 1 (#16355)

---
+++ ui-v2/src/components/ui/status-badge/index.tsx
@@ -0,0 +1,45 @@
+import type { components } from "@/api/prefect";
+import { cva } from "class-variance-authority";
+import { Circle, Pause } from "lucide-react";
+import { Badge } from "../badge";
+
+type Status =
+	| components["schemas"]["DeploymentStatus"]
+	| components["schemas"]["WorkPoolStatus"];
+
+type StatusBadgeProps = {
+	status: Status;
+};
+
+const STATUS_ICONS = {
+	READY: <Circle size={12} fill="green" />,
+	NOT_READY: <Circle size={12} fill="red" />,
+	PAUSED: <Pause size={12} />,
+} as const satisfies Record<Status, React.ReactNode>;
+
+const statusBadgeVariants = cva(
+	"gap-2 px-2 text-black/80 font-mono font-light border border-black/10 shadow-none",
+	{
+		variants: {
+			status: {
+				READY: "bg-green-100 hover:bg-green-100",
+				NOT_READY: "bg-red-100 hover:bg-red-100",
+				PAUSED: "bg-gray-300 hover:bg-gray-300",
+			} satisfies Record<Status, string>,
+		},
+	},
+);
+
+export const StatusBadge = ({ status }: StatusBadgeProps) => {
+	const Icon = STATUS_ICONS[status];
+	const statusText = status
+		.split("_")
+		.map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+		.join(" ");
+	return (
+		<Badge className={statusBadgeVariants({ status })}>
+			{Icon}
+			{statusText}
+		</Badge>
+	);
+};
