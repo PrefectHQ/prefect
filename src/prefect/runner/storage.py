c9ee71de81bae84f029c8615849fac8520625f0f
jake@prefect.io <jake@prefect.io>
2024-12-27 10:35:27 -0500

    refactor methods

--- src/prefect/runner/storage.py
+++ src/prefect/runner/storage.py
@@ -173,42 +173,35 @@ class GitRepository:
 
         return _format_token_from_credentials(urlparse(self._url).netloc, credentials)
 
+    def _add_credentials_to_url(self, url: str) -> str:
+        """Add credentials to given url if possible."""
+        components = urlparse(url)
+        credentials = self._formatted_credentials
+
+        if components.scheme != "https" or not credentials:
+            return url
+
+        return urlunparse(
+            components._replace(netloc=f"{credentials}@{components.netloc}")
+        )
+
     @property
     def _repository_url_with_credentials(self) -> str:
-        if not self._credentials:
-            return self._url
+        return self._add_credentials_to_url(self._url)
 
-        url_components = urlparse(self._url)
-
-        if url_components.scheme == "https" and self._formatted_credentials is not None:
-            updated_components = url_components._replace(
-                netloc=f"{self._formatted_credentials}@{url_components.netloc}"
-            )
-            repository_url = urlunparse(updated_components)
-        else:
-            repository_url = self._url
-
-        return repository_url
-
+    @property
     def _git_config(self) -> list[str]:
-        """
-        Build a git configuration to use when running git commands.
-        """
+        """Build a git configuration to use when running git commands."""
         config = {}
 
         # Submodules can be private. The url in .gitmodules
         # will not include the credentials, we need to
-        # propagate them down here.
-        if self._include_submodules and self._formatted_credentials is not None:
-            url_components = urlparse(self._url)._replace(path="")
-            if url_components.scheme == "https":
-                with_auth = urlunparse(
-                    url_components._replace(
-                        netloc=f"{self._formatted_credentials}@{url_components.netloc}"
-                    )
-                )
-                without_auth = urlunparse(url_components)
-                config[f"url.{with_auth}.insteadOf"] = without_auth
+        # propagate them down here if they exist.
+        if self._include_submodules and self._formatted_credentials:
+            base_url = urlparse(self._url)._replace(path="")
+            without_auth = urlunparse(base_url)
+            with_auth = self._add_credentials_to_url(without_auth)
+            config[f"url.{with_auth}.insteadOf"] = without_auth
 
         return ["-c", " ".join(f"{k}={v}" for k, v in config.items())] if config else []
 
@@ -244,7 +237,7 @@ class GitRepository:
             # Update the existing repository
             cmd = ["git"]
             # Add the git configuration, must be given after `git` and before the command
-            cmd += self._git_config()
+            cmd += self._git_config
             # Add the pull command and parameters
             cmd += ["pull", "origin"]
             if self._branch:
@@ -274,7 +267,7 @@ class GitRepository:
         repository_url = self._repository_url_with_credentials
         cmd = ["git"]
         # Add the git configuration, must be given after `git` and before the command
-        cmd += self._git_config()
+        cmd += self._git_config
         # Add the clone command and its parameters
         cmd += ["clone", repository_url]
 
