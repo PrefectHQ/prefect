3721619316f8025f16364d840543c5ea40242b08
Devin Villarosa <devinvillarosa92.business@gmail.com>
2024-12-27 09:04:05 -0800

    [UI v2] feat: Adds active task runs component

---
+++ ui-v2/src/components/concurrency/task-run-concurrency-limits/task-run-concurrency-limit-active-task-runs/task-run-concurrency-limit-active-task-runs.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import {
+	createFakeFlow,
+	createFakeFlowRun,
+	createFakeTaskRun,
+	routerDecorator,
+} from "@/storybook/utils";
+
+import { TaskRunConcurrencyLimitActiveTaskRuns } from "./task-run-concurrency-limit-active-task-runs";
+
+const MOCK_DATA = [
+	{
+		flow: createFakeFlow(),
+		flowRun: createFakeFlowRun(),
+		taskRun: createFakeTaskRun(),
+	},
+	{
+		flow: createFakeFlow(),
+		flowRun: createFakeFlowRun(),
+		taskRun: createFakeTaskRun({ tags: ["foo", "bar", "baz"] }),
+	},
+	{
+		flow: createFakeFlow(),
+		flowRun: createFakeFlowRun(),
+		taskRun: createFakeTaskRun(),
+	},
+	{
+		taskRun: createFakeTaskRun(),
+	},
+	{
+		flow: createFakeFlow(),
+		flowRun: createFakeFlowRun(),
+		taskRun: createFakeTaskRun(),
+	},
+	{
+		taskRun: createFakeTaskRun({ tags: ["foo", "bar", "baz"] }),
+	},
+];
+
+const meta: Meta<typeof TaskRunConcurrencyLimitActiveTaskRuns> = {
+	title:
+		"Components/Concurrency/TaskRunConcurrencyLimits/TaskRunConcurrencyLimitActiveTaskRuns",
+	component: TaskRunConcurrencyLimitActiveTaskRuns,
+	decorators: [routerDecorator],
+	args: { data: MOCK_DATA },
+};
+export default meta;
+
+type Story = StoryObj<typeof TaskRunConcurrencyLimitActiveTaskRuns>;
+
+export const story: Story = {
+	name: "TaskRunConcurrencyLimitActiveTaskRuns",
+};
