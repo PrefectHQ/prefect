33c7f0ae777f92ccbb9c1c7993b5468b5d47ed2a
jbw-vtl <141396508+jbw-vtl@users.noreply.github.com>
2024-12-31 04:24:19 +0000

    feat: Add support for NO_PROXY for websocket connections (#16538)

--- tests/utilities/test_proxy.py
+++ tests/utilities/test_proxy.py
@@ -1,5 +1,7 @@
 from unittest.mock import Mock
 
+import pytest
+
 from prefect.events.clients import WebsocketProxyConnect
 
 
@@ -32,11 +34,41 @@ def test_init_ws_with_proxy(monkeypatch):
 
 
 def test_init_wss_with_proxy(monkeypatch):
-    monkeypatch.setenv("HTTPS_PROXY", "https://proxy:3128")
+    monkeypatch.setenv("HTTPS_PROXY", "http://proxy:3128")
     mock_proxy = Mock()
     monkeypatch.setattr("prefect.events.clients.Proxy", mock_proxy)
 
     client = WebsocketProxyConnect("wss://example.com")
 
-    mock_proxy.from_url.assert_called_once_with("https://proxy:3128")
+    mock_proxy.from_url.assert_called_once_with("http://proxy:3128")
     assert client._proxy is not None
+
+
+@pytest.mark.parametrize(
+    "no_proxy", ["example.com", ".com", "example.com,example2.com"]
+)
+def test_init_ws_with_no_proxy(monkeypatch, no_proxy: str):
+    # Validate http proxy is set
+    monkeypatch.setenv("HTTP_PROXY", "http://proxy:3128")
+    client = WebsocketProxyConnect("ws://example.com")
+    assert client._proxy is not None
+
+    # Keeping existing proxy configuration, validate we can disable it with NO_PROXY
+    monkeypatch.setenv("NO_PROXY", no_proxy)
+    client = WebsocketProxyConnect("ws://example.com")
+    assert client._proxy is None
+
+
+@pytest.mark.parametrize(
+    "no_proxy", ["example.com", ".com", "example.com,example2.com"]
+)
+def test_init_wss_with_no_proxy(monkeypatch, no_proxy: str):
+    # Validate https proxy is set
+    monkeypatch.setenv("HTTPS_PROXY", "http://proxy:3128")
+    client = WebsocketProxyConnect("wss://example.com")
+    assert client._proxy is not None
+
+    # Keeping existing proxy configuration, validate we can disable it with NO_PROXY
+    monkeypatch.setenv("NO_PROXY", no_proxy)
+    client = WebsocketProxyConnect("wss://example.com")
+    assert client._proxy is None
