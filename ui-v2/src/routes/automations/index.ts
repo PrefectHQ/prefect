7648cd9b62cc142268068541ab028cc8958c730e
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-01-02 10:55:16 -0800

    [UI v2] feat: Start automations data queries (#16569)

--- ui-v2/src/routes/automations/index.ts
+++ ui-v2/src/routes/automations/index.ts
@@ -1,7 +1,12 @@
+import { buildListAutomationsQuery } from "@/hooks/automations";
 import { createFileRoute } from "@tanstack/react-router";
 
+// nb: Currently there is no filtering or search params used on this page
 export const Route = createFileRoute("/automations/")({
 	component: RouteComponent,
+	loader: ({ context }) =>
+		context.queryClient.ensureQueryData(buildListAutomationsQuery()),
+	wrapInSuspense: true,
 });
 
 function RouteComponent() {
