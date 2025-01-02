62039fba7d979ec7c2fa5ed19a234423e5d92419
Dylan Hughes <dylanbhughes@gmail.com>
2025-01-02 12:46:20 -0500

    update typing for owner_resource

--- src/prefect/_experimental/sla.py
+++ src/prefect/_experimental/sla.py
@@ -32,7 +32,7 @@ class ServiceLevelAgreement(PrefectBaseModel, abc.ABC):
 
     @computed_field
     @property
-    def owner_resource(self) -> str | None:
+    def owner_resource(self) -> Union[str, None]:
         if self._deployment_id:
             return f"prefect.deployment.{self._deployment_id}"
         return None
