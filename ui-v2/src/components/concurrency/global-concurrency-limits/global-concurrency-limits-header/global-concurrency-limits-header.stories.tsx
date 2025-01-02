6682a29d55c0bb100e114527706c4f00818c7586
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-01-01 21:51:48 -0800

    [UI v2] chore: Use storybook fn() for handlers

--- ui-v2/src/components/concurrency/global-concurrency-limits/global-concurrency-limits-header/global-concurrency-limits-header.stories.tsx
+++ ui-v2/src/components/concurrency/global-concurrency-limits/global-concurrency-limits-header/global-concurrency-limits-header.stories.tsx
@@ -1,13 +1,12 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 import { GlobalConcurrencyLimitsHeader } from "./global-concurrency-limits-header";
 
 const meta = {
 	title:
 		"Components/Concurrency/GlobalConcurrencyLimits/GlobalConcurrencyLimitsHeader",
 	component: GlobalConcurrencyLimitsHeader,
-	args: {
-		onAdd: () => {},
-	},
+	args: { onAdd: fn() },
 } satisfies Meta<typeof GlobalConcurrencyLimitsHeader>;
 
 export default meta;
