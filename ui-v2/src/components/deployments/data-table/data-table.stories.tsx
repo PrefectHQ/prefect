2f3735bf1ee4f17520109a44c51f78ec24fc5167
Alex Streed <desertaxle@users.noreply.github.com>
2025-01-03 11:01:04 -0600

    Populate deployments list with data (#16578)

--- ui-v2/src/components/deployments/data-table/data-table.stories.tsx
+++ ui-v2/src/components/deployments/data-table/data-table.stories.tsx
@@ -1,9 +1,7 @@
-import type { DeploymentWithFlow } from "@/hooks/deployments";
+import { createFakeDeployment } from "@/mocks";
 import { toastDecorator } from "@/storybook/utils";
-import { faker } from "@faker-js/faker";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
-import { generateRandomSchedule } from "@tests/utils/mock-factories";
 import { DeploymentsDataTable } from ".";
 
 export default {
@@ -12,36 +10,10 @@ export default {
 	decorators: [toastDecorator],
 } satisfies Meta<typeof DeploymentsDataTable>;
 
-function createRandomDeployment(): DeploymentWithFlow {
-	return {
-		id: faker.string.uuid(),
-		created: faker.date.recent().toISOString(),
-		updated: faker.date.recent().toISOString(),
-		name: faker.airline.airplane().name,
-		flow_id: faker.string.uuid(),
-		paused: faker.datatype.boolean(),
-		status: faker.helpers.arrayElement(["READY", "NOT_READY"]),
-		enforce_parameter_schema: faker.datatype.boolean(),
-		tags: Array.from({ length: faker.number.int({ min: 0, max: 3 }) }, () =>
-			faker.lorem.word(),
-		),
-		schedules: Array.from(
-			{ length: faker.number.int({ min: 0, max: 3 }) },
-			() => generateRandomSchedule(),
-		),
-		flow: {
-			id: faker.string.uuid(),
-			created: faker.date.recent().toISOString(),
-			updated: faker.date.recent().toISOString(),
-			name: faker.company.catchPhrase().toLowerCase().replace(/\s+/g, "-"),
-		},
-	};
-}
-
 export const Default: StoryObj = {
 	name: "DataTable",
 	args: {
-		deployments: Array.from({ length: 10 }, createRandomDeployment),
+		deployments: Array.from({ length: 10 }, createFakeDeployment),
 		onQuickRun: fn(),
 		onCustomRun: fn(),
 		onEdit: fn(),
