8b371576239d435978908284bd0eb0874abaf7fd
Alex Streed <alex.s@prefect.io>
2025-01-06 14:08:37 -0600

    Use `msw` for story data

--- ui-v2/src/components/deployments/data-table/data-table.stories.tsx
+++ ui-v2/src/components/deployments/data-table/data-table.stories.tsx
@@ -1,17 +1,55 @@
-import { createFakeDeployment } from "@/mocks";
-import { routerDecorator, toastDecorator } from "@/storybook/utils";
+import { FlowRunWithDeploymentAndFlow } from "@/api/flow-runs";
+import {
+	createFakeDeployment,
+	createFakeFlow,
+	createFakeFlowRun,
+} from "@/mocks";
+import {
+	reactQueryDecorator,
+	routerDecorator,
+	toastDecorator,
+} from "@/storybook/utils";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
+import { buildApiUrl } from "@tests/utils/handlers";
+import { http, HttpResponse } from "msw";
 import { DeploymentsDataTable } from ".";
+
+const createFakeFlowRunWithDeploymentAndFlow =
+	(): FlowRunWithDeploymentAndFlow => {
+		const flowRun = createFakeFlowRun();
+
+		return {
+			...flowRun,
+			deployment: createFakeDeployment(),
+			flow: createFakeFlow(),
+		};
+	};
 
 export default {
 	title: "Components/Deployments/DataTable",
 	component: DeploymentsDataTable,
-	decorators: [toastDecorator, routerDecorator],
+	decorators: [toastDecorator, routerDecorator, reactQueryDecorator],
 } satisfies Meta<typeof DeploymentsDataTable>;
 
 export const Default: StoryObj = {
 	name: "Randomized Data",
+	parameters: {
+		msw: {
+			handlers: [
+				http.post(buildApiUrl("/flow_runs/filter"), async ({ request }) => {
+					const { limit } = (await request.json()) as { limit: number };
+
+					return HttpResponse.json(
+						Array.from(
+							{ length: limit },
+							createFakeFlowRunWithDeploymentAndFlow,
+						),
+					);
+				}),
+			],
+		},
+	},
 	args: {
 		deployments: Array.from({ length: 10 }, createFakeDeployment),
 		onQuickRun: fn(),
