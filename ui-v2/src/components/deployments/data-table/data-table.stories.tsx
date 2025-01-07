806823148239148b3bb9c60f1c391cf93a13ae08
Alex Streed <desertaxle@users.noreply.github.com>
2025-01-07 10:21:14 -0600

    Add flow run activity bar graph to deployments table (#16610)

--- ui-v2/src/components/deployments/data-table/data-table.stories.tsx
+++ ui-v2/src/components/deployments/data-table/data-table.stories.tsx
@@ -1,17 +1,40 @@
 import { createFakeDeployment } from "@/mocks";
-import { toastDecorator } from "@/storybook/utils";
+import { createFakeFlowRunWithDeploymentAndFlow } from "@/mocks/create-fake-flow-run";
+import {
+	reactQueryDecorator,
+	routerDecorator,
+	toastDecorator,
+} from "@/storybook/utils";
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
+import { buildApiUrl } from "@tests/utils/handlers";
+import { http, HttpResponse } from "msw";
 import { DeploymentsDataTable } from ".";
 
 export default {
 	title: "Components/Deployments/DataTable",
 	component: DeploymentsDataTable,
-	decorators: [toastDecorator],
+	decorators: [toastDecorator, routerDecorator, reactQueryDecorator],
 } satisfies Meta<typeof DeploymentsDataTable>;
 
 export const Default: StoryObj = {
-	name: "DataTable",
+	name: "Randomized Data",
+	parameters: {
+		msw: {
+			handlers: [
+				http.post(buildApiUrl("/flow_runs/filter"), async ({ request }) => {
+					const { limit } = (await request.json()) as { limit: number };
+
+					return HttpResponse.json(
+						Array.from(
+							{ length: limit },
+							createFakeFlowRunWithDeploymentAndFlow,
+						),
+					);
+				}),
+			],
+		},
+	},
 	args: {
 		deployments: Array.from({ length: 10 }, createFakeDeployment),
 		onQuickRun: fn(),
