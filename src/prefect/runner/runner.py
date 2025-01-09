c858e1ef51b880c15d240b27b31b3bc9c992fd36
Chris White <chris@prefect.io>
2025-01-09 11:22:03 -0500

    Update runner docstring for clarity of their universality (#16662)

--- src/prefect/runner/runner.py
+++ src/prefect/runner/runner.py
@@ -1,6 +1,8 @@
 """
-Runners are responsible for managing the execution of deployments created and managed by
-either `flow.serve` or the `serve` utility.
+Runners are responsible for managing the execution of all deployments.
+
+When creating a deployment using either `flow.serve` or the `serve` utility,
+they also will poll for scheduled runs.
 
 Example:
     ```python
