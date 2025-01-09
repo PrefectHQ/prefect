0bc23ac90d79bc900f038ee594e870456ebad406
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-01-08 16:59:18 -0800

    [UI v2] feat: Updates automation wizard action steps to be part of the automation wizard

--- ui-v2/src/components/automations/automations-wizard/details-step/details-step.test.tsx
+++ ui-v2/src/components/automations/automations-wizard/details-step/details-step.test.tsx
@@ -1,31 +1,51 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { DetailsStep } from "./details-step";
 
+import {
+	AutomationWizardSchema,
+	type AutomationWizardSchema as TAutomationWizardSchema,
+} from "@/components/automations/automations-wizard/automation-schema";
+import { Form } from "@/components/ui/form";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { describe, expect, it, vi } from "vitest";
+import { useForm } from "react-hook-form";
+import { describe, expect, it } from "vitest";
+
+const DetailsStepFormContainer = () => {
+	const form = useForm<TAutomationWizardSchema>({
+		resolver: zodResolver(AutomationWizardSchema),
+		defaultValues: { actions: [{ type: undefined }] },
+	});
+
+	return (
+		<Form {...form}>
+			<form>
+				<DetailsStep />
+			</form>
+		</Form>
+	);
+};
 
 describe("DetailsStep", () => {
 	it("able to add details about an automation", async () => {
 		const user = userEvent.setup();
 
 		// ------------ Setup
-		const mockOnSaveFn = vi.fn();
-		render(<DetailsStep onPrevious={vi.fn()} onSave={mockOnSaveFn} />);
+		render(<DetailsStepFormContainer />);
 
 		// ------------ Act
-
 		await user.type(screen.getByLabelText(/automation name/i), "My Automation");
 		await user.type(
 			screen.getByLabelText(/description \(optional\)/i),
 			"My Description",
 		);
 
-		await user.click(screen.getByRole("button", { name: /save/i }));
-
 		// ------------ Assert
-		expect(mockOnSaveFn).toBeCalledWith({
-			name: "My Automation",
-			description: "My Description",
-		});
+		expect(
+			screen.getByRole("textbox", { name: /automation name/i }),
+		).toHaveValue("My Automation");
+		expect(
+			screen.getByRole("textbox", { name: /description \(optional\)/i }),
+		).toHaveValue("My Description");
 	});
 });
