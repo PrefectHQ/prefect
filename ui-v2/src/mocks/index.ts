d1f4e25b0a3492d1c0f4df2dbf69fde43cbb865b
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-01-15 09:18:23 -0800

    [UI v2] feat: Adds hook to join deployment and flow data (#16711)

--- ui-v2/src/mocks/index.ts
+++ ui-v2/src/mocks/index.ts
@@ -1,5 +1,8 @@
 export { createFakeAutomation } from "./create-fake-automation";
-export { createFakeDeployment } from "./create-fake-deployment";
+export {
+	createFakeDeployment,
+	createFakeDeploymentWithFlow,
+} from "./create-fake-deployment";
 export { createFakeFlow } from "./create-fake-flow";
 export { createFakeFlowRun } from "./create-fake-flow-run";
 export { createFakeGlobalConcurrencyLimit } from "./create-fake-global-concurrency-limit";
