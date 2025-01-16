d3380839bca61129f44c88e6b861e9d5dc8de90b
Chris Guidry <chris.g@prefect.io>
2025-01-16 17:05:14 -0500

    Ensures that `QueueService` instances are unique within types (#16751)

--- tests/_internal/concurrency/test_services.py
+++ tests/_internal/concurrency/test_services.py
@@ -97,6 +97,14 @@ def test_instance_returns_new_instance_with_unique_key():
     assert isinstance(new_instance, MockService)
 
 
+def test_different_subclasses_have_unique_instances():
+    instance = MockService.instance()
+    assert isinstance(instance, MockService)
+    new_instance = MockBatchedService.instance()
+    assert new_instance is not instance
+    assert isinstance(new_instance, MockBatchedService)
+
+
 def test_instance_returns_same_instance_after_error():
     event = threading.Event()
 
