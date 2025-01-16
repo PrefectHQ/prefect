9e97343a50da0f0fbc786887894f8d50c8575a13
jeanluciano <jeanluciano@mailbox.org>
2025-01-16 12:18:34 -0600

    integration test added

---
+++ flows/setup_logging.py
@@ -0,0 +1,20 @@
+import logging
+from logging.config import dictConfig
+
+from prefect import flow
+
+logger = logging.getLogger(__name__)
+logger.setLevel("WARNING")
+
+dictConfig({"version": 1, "incremental": False})
+
+
+@flow
+def myflow():
+    return "Hello"
+
+
+if __name__ == "__main__":
+    result = myflow()
+
+    assert result == "Hello"
