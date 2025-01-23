d632bd0364d8d3d677686a09cb82838853da1f23
Kevin Cameron Grismore <kevin.grismore@prefect.io>
2025-01-23 11:53:21 -0600

    add `PrefectDbtSettings`

---
+++ src/integrations/prefect-dbt/prefect_dbt/core/settings.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from dbt_common.events.base_types import EventLevel
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class PrefectDbtSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="DBT_")
+
+    profiles_dir: Path = Field(default=Path.home() / ".dbt")
+    project_dir: Path = Field(default_factory=Path.cwd)
+    log_level: EventLevel = Field(default=EventLevel.INFO)
