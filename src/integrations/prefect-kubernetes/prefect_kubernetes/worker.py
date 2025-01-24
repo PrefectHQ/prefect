1bcefa70ca7f4bb8ec02216b8b362e67408328dd
jeanluciano <jeanluciano@mailbox.org>
2025-01-24 10:44:13 -0600

    changes deps

--- src/integrations/prefect-kubernetes/prefect_kubernetes/worker.py
+++ src/integrations/prefect-kubernetes/prefect_kubernetes/worker.py
@@ -916,7 +916,7 @@ class KubernetesWorker(BaseWorker):
                         func=batch_client.list_namespaced_job,
                         namespace=namespace,
                         field_selector=f"metadata.name={job_name}",
-                        _request_timeout=aiohttp.ClientTimeout(total=None),
+                        _request_timeout=aiohttp.ClientTimeout(),
                         **watch_kwargs,
                     ):
                         yield event
@@ -925,7 +925,7 @@ class KubernetesWorker(BaseWorker):
                         job_list = await batch_client.list_namespaced_job(
                             namespace=namespace,
                             field_selector=f"metadata.name={job_name}",
-                            _request_timeout=aiohttp.ClientTimeout(total=None),
+                            _request_timeout=aiohttp.ClientTimeout(),
                         )
 
                         resource_version = job_list.metadata.resource_version
