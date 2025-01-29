17990c78f4ceece0277b61c303aee366acf304a7
JBW <jbw@vitol.com>
2025-01-29 16:37:28 +0000

    docs: Add reasoning documentation to the new main.py

--- src/prefect/__main__.py
+++ src/prefect/__main__.py
@@ -1,3 +1,9 @@
+"""Exposes the typer app directly within the package, enabling the use-case of using
+`python -m prefect` in addition to the existing `prefect` console_scripts entrypoint.
+
+This is required to i.e. run prefect natively in PyCharm with debugger or simplifies running the CLI from within a
+python script.
+"""
 from .cli import app
 
 if __name__ == "__main__":
