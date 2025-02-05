076122ab9aef99abd6b1b00cc7d251d9d8e2c7c9
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-02-05 12:29:19 -0800

    [UI v2] feat: Adds search params for the deployment details route

--- ui-v2/src/routes/deployments/deployment.$id.tsx
+++ ui-v2/src/routes/deployments/deployment.$id.tsx
@@ -1,6 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { zodValidator } from "@tanstack/zod-adapter";
+import { z } from "zod";
+
+/**
+ * Schema for validating URL search parameters for the Deployment Details page
+ * @property {"Runs" | "Upcoming" | "Parameters" | "Configuration" | "Description"} tab used designate which tab view to display
+ */
+const searchParams = z.object({
+	tab: z
+		.enum(["Runs", "Upcoming", "Parameters", "Configuration", "Description"])
+		.default("Runs"),
+});
+
+export type DeploymentDetailsTabOptions = z.infer<typeof searchParams>["tab"];
 
 export const Route = createFileRoute("/deployments/deployment/$id")({
+	validateSearch: zodValidator(searchParams),
 	component: RouteComponent,
 });
 
