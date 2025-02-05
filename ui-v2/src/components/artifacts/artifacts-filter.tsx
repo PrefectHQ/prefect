abbbcd4fb2e5118d3bb4ac4c7500c31189c96608
Devan Grose <=>
2025-02-04 16:56:08 -0800

    [UI-v2] feat: add queries for artifacts

---
+++ ui-v2/src/components/artifacts/artifacts-filter.tsx
@@ -0,0 +1,106 @@
+import { SearchInput } from "@/components/ui/input";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import useDebounce from "@/hooks/use-debounce";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { Typography } from "../ui/typography";
+import { filterType } from "./types";
+
+interface ArtifactsFilterProps {
+	filters: filterType[];
+	onFilterChange: (newFilters: filterType[]) => void;
+	totalCount: number;
+}
+
+const artifactTypeOptions = [
+	{ value: "all", label: "All Types" },
+	{ value: "markdown", label: "Markdown" },
+	{ value: "progress", label: "Progress" },
+	{ value: "image", label: "Image" },
+	{ value: "table", label: "Table" },
+];
+
+export const ArtifactsFilterComponent = ({
+	filters,
+	onFilterChange,
+	totalCount,
+}: ArtifactsFilterProps) => {
+	const [searchInputValue, setSearchInput] = useState(
+		filters.find((val) => val.id == "name")?.value ?? "",
+	);
+
+	const debouncedSearchInputValue = useDebounce(searchInputValue, 500);
+
+	const changeArtifactName = useCallback(
+		(value: string) => {
+			onFilterChange([
+				...filters.filter((filter) => filter.id !== "name"),
+				{ id: "name", label: "Name", value },
+			]);
+		},
+		[filters, onFilterChange],
+	);
+
+	const changeArtifactType = useCallback(
+		(value: string) => {
+			onFilterChange([
+				...filters.filter((filter) => filter.id !== "type"),
+				{ id: "type", label: "Type", value },
+			]);
+		},
+		[filters, onFilterChange],
+	);
+
+	useEffect(() => {
+		changeArtifactName(debouncedSearchInputValue);
+	}, [debouncedSearchInputValue, changeArtifactName]);
+
+	const typeValue = useMemo(
+		() => filters.find((val) => val.id == "type")?.value ?? undefined,
+		[filters],
+	);
+	return (
+		<div
+			data-testid="artifact-filter"
+			className="flex justify-between items-center mt-4"
+		>
+			<div>
+				<Typography variant="body" className="text-sm text-muted-foreground">
+					{totalCount} artifacts
+				</Typography>
+			</div>
+			<div className="flex gap-4">
+				<SearchInput
+					data-testid="search-input"
+					value={searchInputValue}
+					onChange={(e) => setSearchInput(e.target.value)}
+				/>
+
+				<div className="xs:col-span-1 md:col-span-2 lg:col-span-2">
+					<Select
+						data-testid="type-select"
+						value={typeValue}
+						onValueChange={changeArtifactType}
+						defaultValue="undefined"
+					>
+						<SelectTrigger aria-label="Artifact type">
+							<SelectValue placeholder="Type" />
+						</SelectTrigger>
+						<SelectContent>
+							{artifactTypeOptions.map(({ value, label }) => (
+								<SelectItem key={value} value={value}>
+									{label}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+				</div>
+			</div>
+		</div>
+	);
+};
