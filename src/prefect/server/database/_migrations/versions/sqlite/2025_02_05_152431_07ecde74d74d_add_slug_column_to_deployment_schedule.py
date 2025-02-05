0e0c3236b2100b3399397b8bf20f71433767d4a3
Chris White <chris@prefect.io>
2025-02-05 15:26:13 -0800

    Add slug column to deployment schedules

---
+++ src/prefect/server/database/_migrations/versions/sqlite/2025_02_05_152431_07ecde74d74d_add_slug_column_to_deployment_schedule.py
@@ -0,0 +1,60 @@
+"""Add slug column to deployment_schedule
+
+Revision ID: 07ecde74d74d
+Revises: 67f886da208e
+Create Date: 2025-02-05 15:24:31.503016
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import prefect
+
+
+# revision identifiers, used by Alembic.
+revision = '07ecde74d74d'
+down_revision = '67f886da208e'
+branch_labels = None
+depends_on = None
+
+
+
+
+def upgrade():
+    with op.get_context().autocommit_block():
+        op.add_column(
+            "deployment_schedule",
+            sa.Column("slug", sa.String, nullable=True),
+        )
+
+        op.execute(
+            """
+            CREATE INDEX CONCURRENTLY IF NOT EXISTS ix_deployment_schedule__slug
+            ON deployment_schedule(slug)
+            """
+        )
+
+        op.execute(
+            """
+            CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS
+            ix_deployment_schedule__deployment_id__slug
+            ON deployment_schedule(deployment_id, slug)
+            WHERE slug IS NOT NULL;
+            """
+        )
+
+
+def downgrade():
+    with op.get_context().autocommit_block():
+        op.execute(
+            """
+            DROP INDEX CONCURRENTLY IF EXISTS ix_deployment_schedule__deployment_id__slug
+            """
+        )
+
+        op.execute(
+            """
+            DROP INDEX CONCURRENTLY IF EXISTS ix_deployment_schedule__slug
+            """
+        )
+
+        op.drop_column("deployment_schedule", "slug")
