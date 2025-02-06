25b5266288fd32fb2063d1d7feb27be68f2a491e
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-06 10:32:10 -0800

    [UI v2] feat: Adds deployment details RQ hook (#16994)

--- ui-v2/src/routes/deployments/deployment.$id.tsx
+++ ui-v2/src/routes/deployments/deployment.$id.tsx
@@ -1,7 +1,11 @@
+import { buildDeploymentDetailsQuery } from "@/api/deployments";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/deployments/deployment/$id")({
 	component: RouteComponent,
+	loader: ({ context, params }) =>
+		context.queryClient.ensureQueryData(buildDeploymentDetailsQuery(params.id)),
+	wrapInSuspense: true,
 });
 
 function RouteComponent() {
