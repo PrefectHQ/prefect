12bcec68e4f7d37f95a9c876f86d4175ab4817a5
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-06 12:16:46 -0800

    [UI v2] feat: Scaffolds UX components for deployment details page (#16999)

---
+++ ui-v2/src/components/deployments/deployment-details-header.tsx
@@ -0,0 +1,32 @@
+import { Deployment } from "@/api/deployments";
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+	BreadcrumbPage,
+	BreadcrumbSeparator,
+} from "../ui/breadcrumb";
+
+type DeploymentDetailsHeaderProps = {
+	deployment: Deployment;
+};
+export const DeploymentDetailsHeader = ({
+	deployment,
+}: DeploymentDetailsHeaderProps) => {
+	return (
+		<Breadcrumb>
+			<BreadcrumbList>
+				<BreadcrumbItem>
+					<BreadcrumbLink to="/deployments" className="text-xl font-semibold">
+						Deployments
+					</BreadcrumbLink>
+				</BreadcrumbItem>
+				<BreadcrumbSeparator />
+				<BreadcrumbItem className="text-xl font-semibold">
+					<BreadcrumbPage>{deployment.name}</BreadcrumbPage>
+				</BreadcrumbItem>
+			</BreadcrumbList>
+		</Breadcrumb>
+	);
+};
