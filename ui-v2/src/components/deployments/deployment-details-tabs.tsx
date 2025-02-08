e370de3ea985ca37d459a68ea7d3a54be37d4d95
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-02-07 16:25:35 -0800

    [UI v2] feat: Adds deployment parameter table

--- ui-v2/src/components/deployments/deployment-details-tabs.tsx
+++ ui-v2/src/components/deployments/deployment-details-tabs.tsx
@@ -5,6 +5,7 @@ import { Link, getRouteApi } from "@tanstack/react-router";
 import { type JSX } from "react";
 
 import { DeploymentDescription } from "./deployment-description";
+import { DeploymentParametersTable } from "./deployment-parameters-table";
 
 const routeApi = getRouteApi("/deployments/deployment/$id");
 
@@ -58,7 +59,7 @@ export const DeploymentDetailsTabs = ({
 			),
 			ViewComponent: () => (
 				<TabsContent value="Parameters">
-					<div className="border border-red-400">{"<ParametersView />"}</div>
+					<DeploymentParametersTable deployment={deployment} />
 				</TabsContent>
 			),
 		},
