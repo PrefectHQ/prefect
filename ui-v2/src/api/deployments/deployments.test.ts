72967a2a6d4b07d63652510b4cdb7444ee7e78cd
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-11 14:57:59 -0800

    [UI v2] feat: Adds create deployment schedule mutation (#17082)

--- ui-v2/src/api/deployments/deployments.test.ts
+++ ui-v2/src/api/deployments/deployments.test.ts
@@ -11,6 +11,7 @@ import {
 	buildFilterDeploymentsQuery,
 	buildPaginateDeploymentsQuery,
 	queryKeyFactory,
+	useCreateDeploymentSchedule,
 	useDeleteDeployment,
 	useUpdateDeploymentSchedule,
 } from "./index";
@@ -202,6 +203,71 @@ describe("deployments api", () => {
 				expect(useDeleteDeploymentResult.current.isSuccess).toBe(true),
 			);
 			expect(useListDeploymentsResult.current.data?.results).toHaveLength(0);
+		});
+	});
+
+	describe("useCreateDeploymentSchedule", () => {
+		const MOCK_DEPLOYMENT_ID = "deployment-id";
+		const MOCK_SCHEDULE_ID = "schedule-id";
+		const MOCK_SCHEDULE = {
+			id: MOCK_SCHEDULE_ID,
+			created: "2024-01-01T00:00:00.000Z",
+			updated: "2024-01-01T00:00:00.000Z",
+			deployment_id: "deployment-id",
+			schedule: {
+				interval: 3600.0,
+				anchor_date: "2024-01-01T00:00:00.000Z",
+				timezone: "UTC",
+			},
+			active: false,
+			max_scheduled_runs: null,
+		};
+		const MOCK_DEPLYOMENT = createFakeDeployment({
+			id: MOCK_DEPLOYMENT_ID,
+			schedules: [],
+		});
+		const MOCK_UPDATED_DEPLOYMENT = {
+			...MOCK_DEPLYOMENT,
+			schedules: [MOCK_SCHEDULE],
+		};
+
+		it("invalidates cache and fetches updated value", async () => {
+			const queryClient = new QueryClient();
+			// Original cached value
+			queryClient.setQueryData(queryKeyFactory.all(), [MOCK_DEPLYOMENT]);
+
+			// Updated fetch and cached value
+			mockFetchDeploymentsAPI([MOCK_UPDATED_DEPLOYMENT]);
+
+			const { result: useListDeploymentsResult } = renderHook(
+				() => useQuery(buildPaginateDeploymentsQuery()),
+				{ wrapper: createWrapper({ queryClient }) },
+			);
+
+			const { result: useCreateDeploymentScheduleResult } = renderHook(
+				useCreateDeploymentSchedule,
+				{ wrapper: createWrapper({ queryClient }) },
+			);
+
+			act(() =>
+				useCreateDeploymentScheduleResult.current.createDeploymentSchedule({
+					deployment_id: MOCK_DEPLOYMENT_ID,
+					schedule: {
+						interval: 3600.0,
+						anchor_date: "2024-01-01T00:00:00.000Z",
+						timezone: "UTC",
+					},
+					active: false,
+					max_scheduled_runs: null,
+				}),
+			);
+
+			await waitFor(() =>
+				expect(useCreateDeploymentScheduleResult.current.isSuccess).toBe(true),
+			);
+			expect(
+				useListDeploymentsResult.current.data?.results[0].schedules,
+			).toEqual([MOCK_SCHEDULE]);
 		});
 	});
 
