a825bc2fd94b10b144476078d74788cd7e083aad
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-11 15:10:11 -0800

    [UI v2] feat: Adds timezone component (#17101)

---
+++ ui-v2/src/components/ui/timezone-select/timezone-select.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { mockPointerEvents } from "@tests/utils/browser";
+import { expect, test, vi } from "vitest";
+
+import { TimezoneSelect } from "./timezone-select";
+
+test("TimezoneSelect can select an option", async () => {
+	mockPointerEvents();
+	const user = userEvent.setup();
+
+	// ------------ Setup
+	const mockOnSelectFn = vi.fn();
+
+	render(<TimezoneSelect onSelect={mockOnSelectFn} selectedValue="" />);
+
+	// ------------ Act
+	await user.click(screen.getByRole("combobox", { name: /select timezone/i }));
+
+	expect(screen.getByText(/suggested timezones/i)).toBeVisible();
+	expect(screen.getByText(/all timezones/i)).toBeVisible();
+
+	await user.click(screen.getByRole("option", { name: /africa \/ abidjan/i }));
+
+	// ------------ Assert
+	expect(mockOnSelectFn).toBeCalledWith("Africa/Abidjan");
+});
