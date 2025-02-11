0552b2b561af67ef0c4d7da6b606cf66150464e4
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-11 15:09:22 -0800

    [UI v2] feat: Adds deployment schedule list (except for adding end editing UX) (#17081)

---
+++ ui-v2/src/components/deployments/deployment-schedules/get-schedule-title.ts
@@ -0,0 +1,15 @@
+import cronstrue from "cronstrue";
+import humanizeDuration from "humanize-duration";
+import { rrulestr } from "rrule";
+import type { DeploymentSchedule } from "./types";
+
+export const getScheduleTitle = (deploymentSchedule: DeploymentSchedule) => {
+	const { schedule } = deploymentSchedule;
+	if ("interval" in schedule) {
+		return `Every ${humanizeDuration(schedule.interval * 1_000)}`;
+	}
+	if ("cron" in schedule) {
+		return cronstrue.toString(schedule.cron);
+	}
+	return rrulestr(schedule.rrule).toText();
+};
