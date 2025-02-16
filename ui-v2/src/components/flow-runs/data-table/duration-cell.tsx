38aa2413bd373fb141105a44ec76b7add341716b
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-02-16 12:40:46 -0800

    wip

---
+++ ui-v2/src/components/flow-runs/data-table/duration-cell.tsx
@@ -0,0 +1,29 @@
+import {
+	type FlowRunWithDeploymentAndFlow,
+	type FlowRunWithFlow,
+} from "@/api/flow-runs";
+import { Icon } from "@/components/ui/icons";
+import { Typography } from "@/components/ui/typography";
+import humanizeDuration from "humanize-duration";
+
+type DurationCellProps = {
+	flowRun: FlowRunWithFlow | FlowRunWithDeploymentAndFlow;
+};
+
+export const DurationCell = ({ flowRun }: DurationCellProps) => {
+	const { estimated_run_time, total_run_time } = flowRun;
+	const duration = estimated_run_time || total_run_time;
+	const durationLabel = humanizeDuration(duration, {
+		maxDecimalPoints: 3,
+		units: ["s"],
+	});
+
+	return (
+		<div className="flex items-center">
+			<div className="flex items-center gap-1">
+				<Icon id="Clock" className="h-4 w-4" />
+				<Typography variant="bodySmall">{durationLabel}</Typography>
+			</div>
+		</div>
+	);
+};
