6c781507006660e8988d955d5ab7a1241a9171d3
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-02-18 15:57:13 -0800

    [UI v2] feat: Adds task run columns to flow run data table

--- ui-v2/src/components/flow-runs/data-table/data-table.stories.tsx
+++ ui-v2/src/components/flow-runs/data-table/data-table.stories.tsx
@@ -6,18 +6,44 @@ import {
 	routerDecorator,
 	toastDecorator,
 } from "@/storybook/utils";
+import { faker } from "@faker-js/faker";
+import { buildApiUrl } from "@tests/utils/handlers";
+import { http, HttpResponse } from "msw";
 import { FlowRunsDataTable } from "./data-table";
 
-const MOCK_DATA = Array.from(
-	{ length: 5 },
-	createFakeFlowRunWithDeploymentAndFlow,
-);
+const MOCK_DATA = [
+	createFakeFlowRunWithDeploymentAndFlow({
+		id: "0",
+		state: { type: "SCHEDULED", id: "0" },
+	}),
+	createFakeFlowRunWithDeploymentAndFlow({ id: "1" }),
+	createFakeFlowRunWithDeploymentAndFlow({ id: "2" }),
+	createFakeFlowRunWithDeploymentAndFlow({ id: "3" }),
+	createFakeFlowRunWithDeploymentAndFlow({ id: "4" }),
+];
+
+const MOCK_FLOW_RUNS_TASK_COUNT = {
+	"0": faker.number.int({ min: 0, max: 5 }),
+	"1": faker.number.int({ min: 0, max: 5 }),
+	"2": faker.number.int({ min: 0, max: 5 }),
+	"3": faker.number.int({ min: 0, max: 5 }),
+	"4": faker.number.int({ min: 0, max: 5 }),
+};
 
 const meta: Meta<typeof FlowRunsDataTable> = {
 	title: "Components/FlowRuns/DataTable/FlowRunsDataTable",
 	decorators: [routerDecorator, reactQueryDecorator, toastDecorator],
 	args: { flowRuns: MOCK_DATA, flowRunsCount: MOCK_DATA.length },
 	component: FlowRunsDataTable,
+	parameters: {
+		msw: {
+			handlers: [
+				http.post(buildApiUrl("/ui/flow_runs/count-task-runs"), () => {
+					return HttpResponse.json(MOCK_FLOW_RUNS_TASK_COUNT);
+				}),
+			],
+		},
+	},
 };
 export default meta;
 
