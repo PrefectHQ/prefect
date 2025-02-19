945b896f8ae8dc6b8c2ca9d30c5cda5e32b1e29e
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-02-19 13:49:34 -0800

    [UI v2] feat: Adds search, sort, filtering, and pagination functionality to data table (#17205)

--- ui-v2/src/components/flow-runs/data-table/run-name-search.tsx
+++ ui-v2/src/components/flow-runs/data-table/run-name-search.tsx
@@ -4,7 +4,12 @@ import { Input, type InputProps } from "@/components/ui/input";
 export const RunNameSearch = (props: InputProps) => {
 	return (
 		<div className="relative">
-			<Input placeholder="Search by run name" className="pl-10" {...props} />
+			<Input
+				aria-label="search by run name"
+				placeholder="Search by run name"
+				className="pl-10"
+				{...props}
+			/>
 			<Icon
 				id="Search"
 				className="absolute left-3 top-2.5 text-muted-foreground"
