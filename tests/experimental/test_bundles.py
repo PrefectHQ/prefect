ae16331b94d9dcfb63d3da424a2e871c8869be55
Alex Streed <alex.s@prefect.io>
2025-02-18 22:01:40 -0600

    Add more tests

--- tests/experimental/test_bundles.py
+++ tests/experimental/test_bundles.py
@@ -1,3 +1,4 @@
+import signal
 from typing import Literal
 
 import pytest
@@ -10,6 +11,7 @@ from prefect._experimental.bundles import (
 from prefect.client.orchestration import PrefectClient
 from prefect.client.schemas.filters import FlowFilter, FlowFilterName
 from prefect.context import TagsContext
+from prefect.exceptions import Abort
 
 
 @pytest.mark.parametrize("engine_type", ["sync", "async"])
@@ -61,6 +63,32 @@ class TestExecuteBundleInSubprocess:
             == "I'm a simple flow, and as a simple flow I believe in speaking plainly."
         )
 
+    async def test_flow_ends_in_failed(
+        self, prefect_client: PrefectClient, engine_type: Literal["sync", "async"]
+    ):
+        if engine_type == "sync":
+
+            @flow(name="TestExecuteBundleInSubprocess.test_flow_ends_in_failed[sync]")
+            def foo():
+                raise ValueError("xyz")
+        else:
+
+            @flow(name="TestExecuteBundleInSubprocess.test_flow_ends_in_failed[async]")
+            async def foo():
+                raise ValueError("xyz")
+
+        flow_run = await prefect_client.create_flow_run(flow=foo)
+
+        bundle = create_bundle_for_flow_run(foo, flow_run)
+        process = execute_bundle_in_subprocess(bundle)
+
+        process.join()
+        assert process.exitcode == 1
+
+        flow_run = await self.get_flow_run_for_flow(prefect_client, foo.name)
+        assert flow_run.state is not None
+        assert flow_run.state.is_failed()
+
     async def test_with_parameters(
         self, prefect_client: PrefectClient, engine_type: Literal["sync", "async"]
     ):
@@ -109,7 +137,7 @@ class TestExecuteBundleInSubprocess:
                 persist_result=True,
             )
             def context_flow():
-                pass
+                return TagsContext.get().current_tags
         else:
 
             @flow(
@@ -117,7 +145,7 @@ class TestExecuteBundleInSubprocess:
                 persist_result=True,
             )
             async def context_flow():
-                pass
+                return TagsContext.get().current_tags
 
         context = {"tags_context": TagsContext(current_tags={"foo", "bar"}).serialize()}
 
@@ -136,3 +164,98 @@ class TestExecuteBundleInSubprocess:
         assert flow_run.state is not None
         assert flow_run.state.is_completed()
         assert await flow_run.state.result() == {"foo", "bar"}
+
+    async def test_flow_is_aborted(
+        self, engine_type: Literal["sync", "async"], prefect_client: PrefectClient
+    ):
+        if engine_type == "sync":
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_is_aborted[sync]",
+                persist_result=True,
+            )
+            def foo():
+                raise Abort()
+        else:
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_is_aborted[async]",
+                persist_result=True,
+            )
+            async def foo():
+                raise Abort()
+
+        flow_run = await prefect_client.create_flow_run(flow=foo)
+
+        bundle = create_bundle_for_flow_run(foo, flow_run)
+        process = execute_bundle_in_subprocess(bundle)
+        process.join()
+        assert process.exitcode == 0
+
+        flow_run = await prefect_client.read_flow_run(flow_run.id)
+        # Stays in running state because the flow run is aborted manually
+        assert flow_run.state is not None
+        assert flow_run.state.is_running()
+
+    async def test_flow_raises_a_base_exception(
+        self, prefect_client: PrefectClient, engine_type: Literal["sync", "async"]
+    ):
+        if engine_type == "sync":
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_raises_a_base_exception[sync]",
+                persist_result=True,
+            )
+            def foo():
+                raise BaseException()
+        else:
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_raises_a_base_exception[async]",
+                persist_result=True,
+            )
+            async def foo():
+                raise BaseException()
+
+        flow_run = await prefect_client.create_flow_run(flow=foo)
+
+        bundle = create_bundle_for_flow_run(foo, flow_run)
+        process = execute_bundle_in_subprocess(bundle)
+        process.join()
+        assert process.exitcode == 1
+
+        flow_run = await self.get_flow_run_for_flow(prefect_client, foo.name)
+        assert flow_run.state is not None
+        assert flow_run.state.is_crashed()
+
+    async def test_flow_process_is_killed(
+        self, prefect_client: PrefectClient, engine_type: Literal["sync", "async"]
+    ):
+        if engine_type == "sync":
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_process_is_killed[sync]",
+                persist_result=True,
+            )
+            def foo():
+                signal.raise_signal(signal.SIGKILL)
+        else:
+
+            @flow(
+                name="TestExecuteBundleInSubprocess.test_flow_process_is_killed[async]",
+                persist_result=True,
+            )
+            async def foo():
+                signal.raise_signal(signal.SIGKILL)
+
+        flow_run = await prefect_client.create_flow_run(flow=foo)
+
+        bundle = create_bundle_for_flow_run(foo, flow_run)
+        process = execute_bundle_in_subprocess(bundle)
+        process.join()
+        assert process.exitcode == -9
+
+        flow_run = await self.get_flow_run_for_flow(prefect_client, foo.name)
+        # Stays in running state because the process died
+        assert flow_run.state is not None
+        assert flow_run.state.is_running()
