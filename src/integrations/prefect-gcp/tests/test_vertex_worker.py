18c7475cae4ab0bec4812697e2ff812ea3e5619a
Jose Brache <jbrache@google.com>
2025-02-24 22:58:42 -0500

    Adding tests in test_vertex_worker.py

--- src/integrations/prefect-gcp/tests/test_vertex_worker.py
+++ src/integrations/prefect-gcp/tests/test_vertex_worker.py
@@ -1,9 +1,12 @@
+import datetime
 import uuid
 from unittest.mock import MagicMock
 
 import pydantic
 import pytest
+from google.cloud.aiplatform_v1.types.custom_job import Scheduling
 from google.cloud.aiplatform_v1.types.job_state import JobState
+from google.protobuf.duration_pb2 import Duration
 from prefect_gcp.workers.vertex import (
     VertexAIWorker,
     VertexAIWorkerJobConfiguration,
@@ -40,6 +43,41 @@ def job_config(service_account_info, gcp_credentials):
                     },
                 }
             ],
+        },
+    )
+
+@pytest.fixture
+def job_config_params(service_account_info, gcp_credentials):
+    return VertexAIWorkerJobConfiguration(
+        name="my-custom-ai-job",
+        region="ashenvale",
+        credentials=gcp_credentials,
+        job_spec={
+            "service_account_name": "my-service-account",
+            "maximum_run_time_hours": 1,
+            "worker_pool_specs": [
+                {
+                    "replica_count": 1,
+                    "container_spec": {
+                        "image_uri": "gcr.io/your-project/your-repo:latest",
+                        "command": ["python", "-m", "prefect.engine"],
+                    },
+                    "machine_spec": {
+                        "machine_type": "n1-standard-4",
+                        "accelerator_type": "NVIDIA_TESLA_K80",
+                        "accelerator_count": 1,
+                    },
+                    "disk_spec": {
+                        "boot_disk_type": "pd-ssd",
+                        "boot_disk_size_gb": 100,
+                    },
+                }
+            ],
+            "scheduling": {
+                "timeout": "7200s",
+                "strategy": "FLEX_START",
+                "max_wait_duration": "1800s"
+            },
         },
     )
 
@@ -150,6 +188,39 @@ class TestVertexAIWorker:
 
             assert (
                 job_config.credentials.job_service_async_client.get_custom_job.call_count
+                == 1
+            )
+            assert result == VertexAIWorkerResult(
+                status_code=0, identifier="mock_display_name"
+            )
+    
+    async def test_params_worker_run(self, flow_run, job_config_params):
+        async with VertexAIWorker("test-pool") as worker:
+            # Initialize scheduling parameters
+            maximum_run_time_hours = job_config_params.job_spec["maximum_run_time_hours"]
+            max_wait_duration = job_config_params.job_spec["scheduling"]["max_wait_duration"]
+            timeout = Duration()
+            timeout.FromTimedelta(
+                td=datetime.timedelta(
+                    hours=maximum_run_time_hours
+                )
+            )
+            scheduling = Scheduling(timeout=timeout, max_wait_duration=max_wait_duration)
+
+            job_config_params.prepare_for_flow_run(flow_run, None, None)
+            result = await worker.run(flow_run=flow_run, configuration=job_config_params)
+
+            custom_job_spec = job_config_params.credentials.job_service_async_client.create_custom_job.call_args[
+                1
+            ]["custom_job"].job_spec
+
+            # Assert scheduling parameters
+            assert custom_job_spec.scheduling.timeout == scheduling.timeout
+            assert custom_job_spec.scheduling.strategy == Scheduling.Strategy["FLEX_START"]
+            assert custom_job_spec.scheduling.max_wait_duration == scheduling.max_wait_duration
+
+            assert (
+                job_config_params.credentials.job_service_async_client.get_custom_job.call_count
                 == 1
             )
             assert result == VertexAIWorkerResult(
