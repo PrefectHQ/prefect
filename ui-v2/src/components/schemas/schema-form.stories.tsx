f1e9e6ccbaed3a3b4df8d5ff58a47b11151be2f8
Craig Harshbarger <pleek91@gmail.com>
2025-02-25 11:07:32 -0600

    Add multiple stories for different property types

--- ui-v2/src/components/schemas/schema-form.stories.tsx
+++ ui-v2/src/components/schemas/schema-form.stories.tsx
@@ -1,253 +1,60 @@
-import type { Meta, StoryFn } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
-import { SchemaForm } from "./schema-form";
+import { JsonInput } from "../ui/json-input";
+import { Typography } from "../ui/typography";
+import { SchemaForm, SchemaFormProps } from "./schema-form";
 import { SchemaFormErrors } from "./types/errors";
-import { PrefectKind } from "./types/prefect-kind";
 import { PrefectSchemaObject } from "./types/schemas";
 
-const schema: PrefectSchemaObject = {
+const userDefinition: PrefectSchemaObject = {
 	type: "object",
+	title: "User",
 	properties: {
-		none: {
-			type: "null",
-			title: "None",
-		},
-		string: {
-			type: "string",
-			title: "String",
-			description: "This is a description",
-			default: "hello",
-		},
-		string_required: {
-			type: "string",
-			title: "String Required",
-		},
-		string_enum: {
-			type: "string",
-			title: "String Enum",
-			enum: ["foo", "bar", "baz"],
-			description: "This is a description",
-		},
-		string_format_date: {
-			type: "string",
-			title: "String Format Date",
-			format: "date",
-		},
-		string_format_datetime: {
-			type: "string",
-			title: "String Format DateTime",
-			format: "date-time",
-		},
-		string_format_json: {
-			type: "string",
-			title: "String Format JSON",
-			format: "json-string",
-		},
-		integer: {
-			type: "integer",
-			title: "Integer",
-		},
-		integer_enum: {
-			type: "integer",
-			title: "Integer Enum",
-			enum: [1, 2, 3],
-		},
-		number: {
-			type: "number",
-			title: "Number",
-		},
-		number_enum: {
-			type: "number",
-			title: "Number Enum",
-			enum: [1.5, 2.5, 3.5, 4],
-		},
-		boolean: {
-			type: "boolean",
-			title: "Boolean",
-		},
-		boolean_enum: {
-			type: "boolean",
-			title: "Boolean Enum",
-			enum: [true, false],
-		},
-		array_string_enum: {
-			type: "array",
-			title: "Array of strings",
-			items: {
-				type: "string",
-				enum: ["tag1", "tag2", "tag3"],
-			},
-		},
-		array_integer_enum: {
-			type: "array",
-			title: "Array of integers",
-			items: {
-				type: "integer",
-				enum: [1, 2, 3],
-			},
-		},
-		array_number_enum: {
-			type: "array",
-			title: "Array of numbers",
-			items: {
-				type: "number",
-				enum: [1.5, 2.5, 3.5],
-			},
-		},
-		array_boolean_enum: {
-			type: "array",
-			title: "Array of booleans",
-			items: {
-				type: "boolean",
-				enum: [true, false],
-			},
-		},
-		array_reference_enum: {
-			type: "array",
-			title: "Array of references",
-			items: {
-				$ref: "#/definitions/type",
-			},
-		},
-		array_items: {
-			type: "array",
-			title: "Array of items",
-			items: {
-				type: "string",
-			},
-			minItems: 2,
-			maxItems: 4,
-		},
-		array_items_with_prefix: {
-			type: "array",
-			title: "Array of items with prefix",
-			prefixItems: [
-				{
-					type: "string",
-					title: "Prefix String",
-					enum: ["tag1", "tag2", "tag3"],
-				},
-				{
-					type: "boolean",
-					title: "Boolean",
-				},
-			],
-		},
-		array_items_with_any_of: {
-			type: "array",
-			title: "Array of items with anyOf",
-			default: [1, "foo"],
-			//@ts-expect-error pydantic can create properties without a type
-			items: {
-				anyOf: [{ type: "string" }, { type: "number" }],
-			},
-		},
-		object: {
-			type: "object",
-			title: "Object",
-			properties: {
-				foo: {
-					type: "string",
-					title: "Foo",
-				},
-			},
-			required: ["foo"],
-		},
-		reference: {
-			title: "User Reference",
-			$ref: "#/definitions/user",
-		},
-		preject_kind_json: {
-			type: "object",
-			title: "Prefect Kind JSON",
-		},
-		preject_kind_none: {
-			type: "string",
-			title: "Prefect Kind None",
-		},
-		//@ts-expect-error pydantic can create properties without a type
-		unknown: {
-			title: "Unknown",
-		},
-		//@ts-expect-error pydantic can create properties without a type
-		unknown_enum: {
-			title: "Unknown Enum",
-			enum: ["foo", "bar", "baz"],
-		},
-		//@ts-expect-error pydantic can create properties without a type
-		any_of: {
-			title: "Any Of",
-			default: true,
-			anyOf: [
-				{
-					type: "string",
-					title: "String",
-				},
-				{
-					type: "number",
-					title: "Number",
-				},
-				{
-					type: "boolean",
-					title: "Boolean",
-				},
-				{
-					type: "object",
-					title: "Object",
-					required: ["foo"],
-					properties: {
-						foo: { type: "string", title: "Foo" },
-						bar: { type: "number", title: "Bar" },
-						baz: { type: "boolean", title: "Baz" },
-					},
-				},
-				{
-					$ref: "#/definitions/user",
-				},
-			],
-		},
-		all_of: {
-			type: "object",
-			title: "All Of",
-			allOf: [
-				{
-					type: "object",
-					title: "Object",
-					properties: {
-						foo: { type: "string", title: "Foo" },
-					},
-				},
-				{
-					$ref: "#/definitions/user",
-				},
-			],
-		},
+		name: { type: "string", title: "Name" },
 	},
-	definitions: {
-		user: {
-			type: "object",
-			title: "User",
-			properties: {
-				name: {
-					type: "string",
-					title: "Name",
-				},
-			},
-		},
-		type: {
-			type: "string",
-			title: "Type",
-			enum: ["Type1", "Type2", "Type3"],
-		},
-	},
-	required: ["string_required"],
 };
 
-const kinds: PrefectKind[] = ["json", "jinja"];
+function TestSchemaForm({
+	schema,
+	kinds = ["json"],
+	errors = [],
+	values: initialValues = {},
+	onValuesChange = () => {},
+}: Partial<SchemaFormProps> & {
+	schema: PrefectSchemaObject;
+}) {
+	const [values, setValues] = useState<Record<string, unknown>>(initialValues);
+
+	function handleValuesChange(values: Record<string, unknown>) {
+		setValues(values);
+		onValuesChange(values);
+	}
+
+	return (
+		<div className="grid grid-cols-2 gap-4 p-4 size-full">
+			<div className="flex flex-col gap-4">
+				<Typography variant="h2">Form</Typography>
+				<SchemaForm
+					schema={schema}
+					kinds={kinds}
+					errors={errors}
+					values={values}
+					onValuesChange={handleValuesChange}
+				/>
+			</div>
+			<div className="flex flex-col gap-4">
+				<Typography variant="h2">Values</Typography>
+				<JsonInput value={JSON.stringify(values, null, 2)} />
+				<Typography variant="h2">Schema</Typography>
+				<JsonInput value={JSON.stringify(schema, null, 2)} />
+			</div>
+		</div>
+	);
+}
 
 const meta = {
 	title: "Components/SchemaForm",
-	component: SchemaForm,
+	component: TestSchemaForm,
 	parameters: {
 		layout: "fullscreen",
 	},
@@ -255,56 +62,446 @@ const meta = {
 
 export default meta;
 
-export const Default: StoryFn<typeof SchemaForm> = () => {
-	const [values, setValues] = useState<Record<string, unknown>>({
-		name: "John Doe",
-		age: 30,
-		preject_kind_json: {
-			__prefect_kind: "json",
-			value: JSON.stringify({
-				foo: "bar",
-			}),
-		},
-		preject_kind_none: {
-			__prefect_kind: "none",
-			value: "hello",
-		},
-		any_of: { name: "Prefect" },
-	});
+type Story = StoryObj<typeof meta>;
 
-	const errors: SchemaFormErrors = [
-		{ property: "string_required", errors: ["This is a required field"] },
-		{
-			property: "object",
-			errors: [
-				{ property: "foo", errors: ["This is a required field"] },
-				{ property: "foo", errors: ["This must be a string"] },
-			],
+export const TypeString: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: "John Doe",
+					title: "Name",
+					type: "string",
+				},
+			},
 		},
-		{
-			property: "any_of",
-			errors: [
-				"This must be a string or a number",
-				{ property: "name", errors: ["This must be a string"] },
-			],
-		},
-		{
-			property: "array_items",
-			errors: ["max 4", { index: 0, errors: ["This must be a string"] }],
-		},
-	];
-
-	return (
-		<div className="grid grid-cols-2 gap-4 p-16 w-full">
-			<SchemaForm
-				values={values}
-				schema={schema}
-				kinds={kinds}
-				errors={errors}
-				onValuesChange={setValues}
-			/>
-
-			<pre>{JSON.stringify(values, null, 2)}</pre>
-		</div>
-	);
+	},
 };
+TypeString.storyName = "type:string";
+
+export const TypeStringWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: "foo",
+					title: "Thing",
+					type: "string",
+					enum: ["foo", "bar", "baz"],
+				},
+			},
+		},
+	},
+};
+TypeStringWithEnum.storyName = "type:string & enum";
+
+export const TypeStringWithFormatDate: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: "2021-01-01",
+					title: "Date",
+					type: "string",
+					format: "date",
+				},
+			},
+		},
+	},
+};
+TypeStringWithFormatDate.storyName = "type:string & format:date";
+
+export const TypeStringWithFormatDateTime: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: "2021-01-01T00:00:00",
+					title: "Date & Time",
+					type: "string",
+					format: "date-time",
+				},
+			},
+		},
+	},
+};
+TypeStringWithFormatDateTime.storyName = "type:string & format:date-time";
+
+export const TypeStringWithFormatJson: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: JSON.stringify({ foo: "bar" }),
+					title: "JSON",
+					type: "string",
+					format: "json-string",
+				},
+			},
+		},
+	},
+};
+TypeStringWithFormatJson.storyName = "type:string & format:json-string";
+
+export const TypeBoolean: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: true,
+					title: "Is Active",
+					type: "boolean",
+				},
+			},
+		},
+	},
+};
+TypeBoolean.storyName = "type:boolean";
+
+export const TypeBooleanWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: true,
+					title: "Is Active",
+					type: "boolean",
+					enum: [true, false],
+				},
+			},
+		},
+	},
+};
+TypeBooleanWithEnum.storyName = "type:boolean & enum";
+
+export const TypeInteger: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: 30,
+					title: "Age",
+					type: "integer",
+				},
+			},
+		},
+	},
+};
+TypeInteger.storyName = "type:integer";
+
+export const TypeIntegerWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: 1,
+					title: "Age",
+					type: "integer",
+					enum: [1, 2, 3],
+				},
+			},
+		},
+	},
+};
+TypeIntegerWithEnum.storyName = "type:integer & enum";
+
+export const TypeNumber: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: 30.5,
+					title: "Age",
+					type: "number",
+				},
+			},
+		},
+	},
+};
+TypeNumber.storyName = "type:number";
+
+export const TypeNumberWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: 1.5,
+					title: "Age",
+					type: "number",
+					enum: [1.5, 2.5, 3.5],
+				},
+			},
+		},
+	},
+};
+TypeNumberWithEnum.storyName = "type:number & enum";
+
+export const TypeArray: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: ["foo", "bar", "baz"],
+					title: "Name",
+					type: "array",
+					items: {
+						type: "string",
+					},
+				},
+			},
+		},
+	},
+};
+TypeArray.storyName = "type:array";
+
+export const TypeArrayWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					title: "Name",
+					type: "array",
+					items: {
+						type: "string",
+						enum: ["foo", "bar", "baz"],
+					},
+				},
+			},
+		},
+	},
+};
+TypeArrayWithEnum.storyName = "type:array & enum";
+
+export const TypeArrayWithPrefix: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: ["foo", true],
+					title: "Name",
+					type: "array",
+					prefixItems: [
+						{
+							type: "string",
+							title: "Prefix String",
+							enum: ["tag1", "tag2", "tag3"],
+						},
+						{
+							type: "boolean",
+							title: "Boolean",
+						},
+					],
+				},
+			},
+		},
+	},
+};
+TypeArrayWithPrefix.storyName = "type:array & prefixItems";
+
+export const TypeArrayWithAnyOf: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: [1, "foo"],
+					title: "Name",
+					type: "array",
+					items: {
+						anyOf: [
+							{ type: "string" },
+							{ type: "number" },
+							{ type: "boolean" },
+							{
+								type: "object",
+								properties: {
+									foo: { type: "string" },
+									bar: { type: "number" },
+									baz: { type: "boolean" },
+								},
+							},
+						],
+					},
+				},
+			},
+		},
+	},
+};
+TypeArrayWithAnyOf.storyName = "type:array & anyOf";
+
+export const TypeObject: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				name: {
+					default: { foo: "bar", baz: true },
+					title: "User",
+					type: "object",
+					properties: {
+						first_name: { type: "string", title: "First Name" },
+						last_name: { type: "string", title: "Last Name" },
+						age: { type: "integer", title: "Age" },
+						birthday: { type: "string", title: "Birthday", format: "date" },
+					},
+					required: ["first_name", "last_name"],
+				},
+			},
+		},
+	},
+};
+TypeObject.storyName = "type:object";
+
+export const TypeObjectWithReference: Story = {
+	args: {
+		schema: {
+			definitions: {
+				user: userDefinition,
+			},
+			type: "object",
+			properties: {
+				user: {
+					title: "User Information",
+					$ref: "#/definitions/user",
+				},
+			},
+		},
+	},
+};
+TypeObjectWithReference.storyName = "type:object & reference";
+
+export const TypeObjectWithAnyOfAndReference: Story = {
+	args: {
+		schema: {
+			definitions: {
+				user: userDefinition,
+			},
+			type: "object",
+			properties: {
+				user: {
+					type: "object",
+					title: "Identification",
+					anyOf: [
+						{ $ref: "#/definitions/user" },
+						{
+							type: "object",
+							title: "SSN",
+							properties: { ssn: { type: "string", title: "SSN" } },
+						},
+						{
+							type: "object",
+							title: "Address",
+							properties: {
+								address: { type: "string", title: "Address" },
+								city: { type: "string", title: "City" },
+								state: { type: "string", title: "State" },
+								zip: { type: "string", title: "Zip" },
+							},
+						},
+					],
+				},
+			},
+		},
+	},
+};
+TypeObjectWithAnyOfAndReference.storyName = "type:object & anyOf & reference";
+
+export const TypeObjectWithAllOfAndReference: Story = {
+	args: {
+		schema: {
+			definitions: {
+				user: userDefinition,
+			},
+			type: "object",
+			properties: {
+				user: {
+					type: "object",
+					title: "User Information",
+					allOf: [
+						{ $ref: "#/definitions/user" },
+						{
+							type: "object",
+							title: "SSN",
+							properties: { ssn: { type: "string", title: "SSN" } },
+						},
+					],
+				},
+			},
+		},
+	},
+};
+TypeObjectWithAllOfAndReference.storyName = "type:object & allOf & reference";
+
+export const TypeUnknown: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				//@ts-expect-error pydantic can create properties without a type
+				name: {
+					title: "Unknown",
+				},
+			},
+		},
+	},
+};
+TypeUnknown.storyName = "type:unknown";
+
+export const TypeUnknownWithEnum: Story = {
+	args: {
+		schema: {
+			type: "object",
+			properties: {
+				//@ts-expect-error pydantic can create properties without a type
+				name: {
+					title: "Unknown",
+					enum: ["foo", "bar", "baz"],
+				},
+			},
+		},
+	},
+};
+TypeUnknownWithEnum.storyName = "type:unknown & enum";
+
+export const prefectKindJson: Story = {
+	args: {
+		schema: {
+			definitions: {
+				user: userDefinition,
+			},
+			type: "object",
+			properties: {
+				user: {
+					$ref: "#/definitions/user",
+				},
+			},
+		},
+		values: {
+			user: {
+				__prefect_kind: "json",
+				value: JSON.stringify(
+					{
+						name: "John Doe",
+					},
+					null,
+					2,
+				),
+			},
+		},
+	},
+};
+prefectKindJson.storyName = "prefect_kind:json";
