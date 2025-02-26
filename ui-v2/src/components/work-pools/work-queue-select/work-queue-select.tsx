37423bec2987e226684d22059a8504fca579b9d5
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-02-26 14:33:41 -0800

    [UI v2] feat: Adds Work Queues select component

---
+++ ui-v2/src/components/work-pools/work-queue-select/work-queue-select.tsx
@@ -0,0 +1,119 @@
+import { buildFilterWorkPoolWorkQueuesQuery } from "@/api/work-queues";
+
+import {
+	Combobox,
+	ComboboxCommandEmtpy,
+	ComboboxCommandGroup,
+	ComboboxCommandInput,
+	ComboboxCommandItem,
+	ComboboxCommandList,
+	ComboboxContent,
+	ComboboxTrigger,
+} from "@/components/ui/combobox";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
+import { useDeferredValue, useMemo, useState } from "react";
+
+type PresetOption = {
+	label: string;
+	value: string | null | undefined;
+};
+
+type WorkQueueSelectProps = {
+	presetOptions?: Array<PresetOption>;
+	selected: string | undefined | null;
+	onSelect: (name: string | undefined | null) => void;
+	workPoolName: string;
+};
+
+export const WorkQueueSelect = ({
+	presetOptions = [],
+	selected,
+	onSelect,
+	workPoolName,
+}: WorkQueueSelectProps) => {
+	const [search, setSearch] = useState("");
+	const { data, isSuccess } = useQuery(
+		buildFilterWorkPoolWorkQueuesQuery({ work_pool_name: workPoolName }),
+	);
+
+	// nb: because work queues API does not have filtering _like by name, do client-side filtering
+	const deferredSearch = useDeferredValue(search);
+	const filteredData = useMemo(() => {
+		if (!data) {
+			return [];
+		}
+		return data.filter((workQueue) =>
+			workQueue.name.toLowerCase().includes(deferredSearch.toLowerCase()),
+		);
+	}, [data, deferredSearch]);
+
+	const filteredPresetOptions = useMemo(() => {
+		return presetOptions.filter((option) =>
+			option.label.toLowerCase().includes(deferredSearch.toLowerCase()),
+		);
+	}, [deferredSearch, presetOptions]);
+
+	return (
+		<Combobox>
+			<ComboboxTrigger
+				selected={Boolean(selected)}
+				aria-label="Select a work queue"
+			>
+				{selected}
+			</ComboboxTrigger>
+			<ComboboxContent>
+				<ComboboxCommandInput
+					value={search}
+					onValueChange={setSearch}
+					placeholder="Search for a work queue..."
+				/>
+				<ComboboxCommandEmtpy>No work queue found</ComboboxCommandEmtpy>
+				<ComboboxCommandList>
+					<ComboboxCommandGroup>
+						{filteredPresetOptions.map((option) => (
+							<ComboboxCommandItem
+								key={option.label}
+								selected={selected === option.value}
+								onSelect={() => {
+									onSelect(option.value);
+									setSearch("");
+								}}
+								//nb: Stringifies label so that UX has a hoverable state
+								value={String(option.label)}
+							>
+								{option.label}
+							</ComboboxCommandItem>
+						))}
+						{isSuccess ? (
+							filteredData.map((workQueue) => (
+								<ComboboxCommandItem
+									key={workQueue.id}
+									selected={selected === workQueue.name}
+									onSelect={(value) => {
+										onSelect(value);
+										setSearch("");
+									}}
+									value={workQueue.name}
+								>
+									{workQueue.name}
+								</ComboboxCommandItem>
+							))
+						) : (
+							<LoadingSelectState />
+						)}
+					</ComboboxCommandGroup>
+				</ComboboxCommandList>
+			</ComboboxContent>
+		</Combobox>
+	);
+};
+
+type LoadingSelectStateProps = {
+	length?: number;
+};
+const LoadingSelectState = ({ length = 4 }: LoadingSelectStateProps) =>
+	Array.from({ length }, (_, index) => (
+		<Skeleton key={index} className="mt-2 p-4 h-2 w-full" />
+	));
