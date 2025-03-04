df4c0964be11c68d0f1bfe01358bf15e37b0dcc4
Craig Harshbarger <pleek91@gmail.com>
2025-03-04 11:45:28 -0600

    Upgrade toggle

--- ui-v2/src/components/ui/toggle-group.tsx
+++ ui-v2/src/components/ui/toggle-group.tsx
@@ -1,9 +1,8 @@
+import { toggleVariants } from "@/components/ui/toggle/styles";
+import { cn } from "@/lib/utils";
 import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 import { type VariantProps } from "class-variance-authority";
 import * as React from "react";
-
-import { toggleVariants } from "@/components/ui/toggle";
-import { cn } from "@/lib/utils";
 
 const ToggleGroupContext = React.createContext<
 	VariantProps<typeof toggleVariants>
