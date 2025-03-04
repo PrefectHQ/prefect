9d542384a590b55a4ba381e06b9c3a7309de5393
Devin Villarosa <devinvillarosa92.business@gmail.com>
2025-03-04 09:35:45 -0800

    badge

--- ui-v2/src/components/ui/badge/badge.tsx
+++ ui-v2/src/components/ui/badge/badge.tsx
@@ -1,21 +1,26 @@
-import type { VariantProps } from "class-variance-authority";
-
-import { cn } from "@/lib/utils";
-import React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { type VariantProps } from "class-variance-authority";
+import * as React from "react";
 import { badgeVariants } from "./styles";
 
-export interface BadgeProps
-	extends React.HTMLAttributes<HTMLDivElement>,
-		VariantProps<typeof badgeVariants> {}
+import { cn } from "@/lib/utils";
 
-export const Badge = React.forwardRef<HTMLDivElement, BadgeProps>(
-	({ className, variant, ...props }, ref) => (
-		<div
-			ref={ref}
+function Badge({
+	className,
+	variant,
+	asChild = false,
+	...props
+}: React.ComponentProps<"span"> &
+	VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+	const Comp = asChild ? Slot : "span";
+
+	return (
+		<Comp
+			data-slot="badge"
 			className={cn(badgeVariants({ variant }), className)}
 			{...props}
 		/>
-	),
-);
+	);
+}
 
-Badge.displayName = "Badge";
+export { Badge };
