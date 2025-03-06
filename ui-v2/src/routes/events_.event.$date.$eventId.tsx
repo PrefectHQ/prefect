ce2f30f8b70b18491d846fbca4f35fd69b4dd576
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-03-06 14:37:57 -0500

    events detail fetch

---
+++ ui-v2/src/routes/events_.event.$date.$eventId.tsx
@@ -0,0 +1,31 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { $api } from "../api/service";
+import { addDays, startOfDay } from "date-fns";
+
+
+export const Route = createFileRoute("/events_/event/$date/$eventId")({
+	component: RouteComponent,
+	loader: async ({context, params }) => {
+		return await context.queryClient.ensureQueryData(
+			$api.queryOptions("post", "/events/filter", {
+				body: {
+					limit: 1,
+					filter: {
+						order: "DESC",
+						id: {
+							id: [params.eventId]
+						},
+						occurred: {
+							since: startOfDay(params.date).toISOString(),
+							until: addDays(startOfDay(params.date), 1).toISOString()
+						}
+					}
+				}
+			})
+		);
+	},
+});
+
+function RouteComponent() {
+	return "🚧🚧 Pardon our dust! 🚧🚧";
+}
