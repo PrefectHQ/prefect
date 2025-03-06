1e7a8d29fc36c232eba1ebd0ddb670c1ae1a5aa1
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-03-06 16:28:41 -0500

    add Flow Run Graph label to graph endpoints to exclude them from orval

--- ui-v2/orval.config.cjs
+++ ui-v2/orval.config.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-   prefect: {
-     output: {
-       client: 'zod',
-       mode: "tags-split",
-       target: './src/api/zod',
-     },
-     input: {
-       target: './oss_schema.json',
-     },
-   },
- };
+	prefect: {
+		output: {
+			client: "zod",
+			mode: "tags-split",
+			target: "./src/api/zod",
+		},
+		input: {
+			target: "./oss_schema.json",
+			filters: {
+				mode: "exclude",
+				tags: ["Flow Run Graph"],
+			},
+		},
+	},
+};
