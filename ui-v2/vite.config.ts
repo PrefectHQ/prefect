891829f77c8a7635e5c420c909ccc06f5efeefe7
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-03-06 14:38:16 -0500

    do code splitting

--- ui-v2/vite.config.ts
+++ ui-v2/vite.config.ts
@@ -5,7 +5,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), TanStackRouterVite()],
+	plugins: [react(), TanStackRouterVite({
+		autoCodeSplitting: true,
+	})],
 	test: {
 		globals: true,
 		environment: "jsdom",
