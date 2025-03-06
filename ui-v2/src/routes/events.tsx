777163a340abe809fe9d5528005386984ca7daa3
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-03-06 14:37:52 -0500

    events data fetch

--- ui-v2/src/routes/events.tsx
+++ ui-v2/src/routes/events.tsx
@@ -1,7 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { $api } from "../api/service";
+import { components } from "@/api/prefect";
+import { z } from "zod";
+import { zodValidator } from "@tanstack/zod-adapter";
+import { readEventsEventsFilterPostBody } from "@/api/zod/events/events";
+
+const buildFilterFromSearchParams = (searchParams: z.infer<typeof readEventsEventsFilterPostBody>): components["schemas"]["Body_read_events_events_filter_post"] => {
+	return {
+		limit: searchParams.limit,
+	}
+}
 
 export const Route = createFileRoute("/events")({
 	component: RouteComponent,
+	validateSearch: zodValidator(readEventsEventsFilterPostBody),
+	loaderDeps: ({search}) => buildFilterFromSearchParams(search),
+	loader: async ({context, deps}) => {
+		return await context.queryClient.ensureQueryData(
+			$api.queryOptions("post", "/events/filter", {
+				body: deps
+			})
+		);
+	},
 });
 
 function RouteComponent() {
