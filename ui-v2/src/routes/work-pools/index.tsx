a5d8235b47e75df6f4f98efe92a8f9a742463876
nicholas <znicholasbrown@gmail.com>
2025-03-06 12:39:13 -0500

    Add count

--- ui-v2/src/routes/work-pools/index.tsx
+++ ui-v2/src/routes/work-pools/index.tsx
@@ -1,6 +1,10 @@
-import { buildFilterWorkPoolsQuery } from "@/api/work-pools";
+import {
+	buildCountWorkPoolsQuery,
+	buildFilterWorkPoolsQuery,
+} from "@/api/work-pools";
 import { WorkPoolsPageHeader } from "@/components/work-pools/header";
 import { WorkPoolCard } from "@/components/work-pools/work-pool-card/work-pool-card";
+import { pluralize } from "@/utils";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -9,6 +13,8 @@ export const Route = createFileRoute("/work-pools/")({
 });
 
 function RouteComponent() {
+	const { data: workPoolCount = 0 } = useQuery(buildCountWorkPoolsQuery());
+
 	const { data: workPools = [] } = useQuery(
 		buildFilterWorkPoolsQuery({
 			limit: 10,
@@ -20,6 +26,12 @@ function RouteComponent() {
 		// TODO: Should we just standardize a layout for all pages?
 		<div className="flex flex-col gap-4">
 			<WorkPoolsPageHeader />
+			<div className="flex justify-between">
+				<div className="text-sm text-muted-foreground">
+					{workPoolCount} {pluralize(workPoolCount, "work pool")}
+				</div>
+				<div className="flex gap-2">{/* Search goes here */}</div>
+			</div>
 			<div className="flex flex-col gap-4">
 				{workPools.map((workPool) => (
 					<WorkPoolCard key={workPool.id} workPool={workPool} />
