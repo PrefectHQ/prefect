2a7c6fa57664ae24df25f65ace9b84270c04e518
Adam Azzam <33043305+AAAZZAM@users.noreply.github.com>
2025-03-07 09:27:46 -0500

    fix linting lifecycle

--- ui-v2/orval.config.cjs
+++ ui-v2/orval.config.cjs
@@ -5,10 +5,14 @@ module.exports = {
 			mode: "tags-split",
 			target: "./src/api/zod",
 		},
+		hooks: {
+			afterAllFilesWrite: ["npm run format"],
+		},
 		input: {
 			target: "./oss_schema.json",
 			filters: {
 				mode: "exclude",
+				// See https://github.com/orval-labs/orval/issues/1961
 				tags: ["Flow Run Graph"],
 			},
 		},
