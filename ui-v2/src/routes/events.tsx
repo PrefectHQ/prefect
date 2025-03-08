441e32b6aac60efd046ae7975d8f97b3c9f1ebab
Adam Azzam <33043305+aaazzam@users.noreply.github.com>
2025-03-08 11:01:17 -0500

    add hooks

--- ui-v2/src/routes/events.tsx
+++ ui-v2/src/routes/events.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { getQueryHooks } from "@/api/query";
+import { readEventsEventsFilterPostBody } from "@/api/zod/events/events";
+import { zodValidator } from "@tanstack/zod-adapter";
+
 
 export const Route = createFileRoute("/events")({
 	component: RouteComponent,
+	validateSearch: zodValidator(readEventsEventsFilterPostBody),
+	loaderDeps: ({ search }) => search,
+	loader: async ({ context, deps }) => {
+		return await context.queryClient.ensureQueryData(
+			getQueryHooks().queryOptions("post", "/events/filter", {
+				body: deps as any,
+			}),
+		);
+	},
 });
 
 function RouteComponent() {
-	return "🚧🚧 Pardon our dust! 🚧🚧";
+	return 'lol'
 }
