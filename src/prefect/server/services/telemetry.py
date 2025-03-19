8b4d9e03646ba7a9713e770a05392771fa379f02
Chris White <chris@prefect.io>
2025-03-19 13:47:12 -0700

    Adds back telemetry service to all servers when enabled (#17528)

--- src/prefect/server/services/telemetry.py
+++ src/prefect/server/services/telemetry.py
@@ -16,14 +16,14 @@ from prefect.server.database import PrefectDBInterface
 from prefect.server.database.dependencies import db_injector
 from prefect.server.models import configuration
 from prefect.server.schemas.core import Configuration
-from prefect.server.services.base import LoopService
+from prefect.server.services.base import LoopService, RunInAllServers
 from prefect.settings import PREFECT_DEBUG_MODE
 from prefect.settings.context import get_current_settings
 from prefect.settings.models.server.services import ServicesBaseSetting
 from prefect.types._datetime import now
 
 
-class Telemetry(LoopService):
+class Telemetry(RunInAllServers, LoopService):
     """
     Sends anonymous data to Prefect to help us improve
 
