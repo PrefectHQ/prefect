c2d440c31676cecf2e0c6827817feaed81b27c62
Alex Streed <desertaxle@users.noreply.github.com>
2025-03-28 15:59:30 -0500

    Remove code past its marked deprecation date (#17632)

--- src/prefect/utilities/engine.py
+++ src/prefect/utilities/engine.py
@@ -50,7 +50,6 @@ from prefect.settings import PREFECT_LOGGING_LOG_PRINTS
 from prefect.states import State
 from prefect.tasks import Task
 from prefect.utilities.annotations import allow_failure, quote
-from prefect.utilities.asyncutils import run_coro_as_sync
 from prefect.utilities.collections import StopVisiting, visit_collection
 from prefect.utilities.text import truncated_to
 
@@ -221,9 +220,9 @@ async def resolve_inputs(
         finished_states = [state for state in states if state.is_final()]
 
         state_results = [
-            state.result(raise_on_failure=False, fetch=True)
-            for state in finished_states
+            state.aresult(raise_on_failure=False) for state in finished_states
         ]
+        state_results = await asyncio.gather(*state_results)
 
         for state, result in zip(finished_states, state_results):
             result_by_state[state] = result
@@ -749,9 +748,7 @@ def resolve_to_final_result(expr: Any, context: dict[str, Any]) -> Any:
             " 'COMPLETED' state."
         )
 
-    result = state.result(raise_on_failure=False, fetch=True)
-    if asyncio.iscoroutine(result):
-        result = run_coro_as_sync(result)
+    result: Any = state.result(raise_on_failure=False, _sync=True)  # pyright: ignore[reportCallIssue] _sync messes up type inference and can be removed once async_dispatch is removed
 
     if state.state_details.traceparent:
         parameter_context = propagate.extract(
