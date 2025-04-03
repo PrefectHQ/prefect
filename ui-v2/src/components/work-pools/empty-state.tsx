1a1856d2351be8415752d827bbd693c7854094fe
Nicholas Brown <znicholasbrown@gmail.com>
2025-04-03 10:47:49 -0400

    Feature: Work pools page (#17419)

---
+++ ui-v2/src/components/work-pools/empty-state.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { DocsLink } from "@/components/ui/docs-link";
+import {
+	EmptyState,
+	EmptyStateActions,
+	EmptyStateDescription,
+	EmptyStateIcon,
+	EmptyStateTitle,
+} from "@/components/ui/empty-state";
+import { Icon } from "@/components/ui/icons";
+import { Link } from "@tanstack/react-router";
+
+export const WorkPoolsEmptyState = () => (
+	<EmptyState>
+		<EmptyStateIcon id="Cpu" />
+		<EmptyStateTitle>Add a work pool to get started</EmptyStateTitle>
+		<EmptyStateDescription>
+			Work pools allow you to prioritize and manage deployments and control the
+			infrastructure they run on.
+		</EmptyStateDescription>
+		<EmptyStateActions>
+			<Link to="/work-pools/create">
+				<Button>
+					Add Work Pool <Icon id="Plus" className="size-4 ml-2" />
+				</Button>
+			</Link>
+			<DocsLink id="work-pools-guide" />
+		</EmptyStateActions>
+	</EmptyState>
+);
