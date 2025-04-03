603d71457c9d9cdbc2b57150173a899b66058be7
Alex Streed <alex.s@prefect.io>
2025-04-02 21:33:50 -0500

    Fix typing issue

--- src/prefect/server/schemas/ui.py
+++ src/prefect/server/schemas/ui.py
@@ -1,9 +1,11 @@
 """Schemas for UI endpoints."""
 
+from typing import Optional
+
 from prefect.server.schemas.core import TaskRun as CoreTaskRun
 
 
 class TaskRun(CoreTaskRun):
     """A task run with additional details for display in the UI."""
 
-    flow_run_name: str | None = None
+    flow_run_name: Optional[str] = None
