af63bd253c3cdd236a3f4b9a53c72e069724a6f1
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-04-07 07:23:09 -0700

    [UI v2] feat: Adds block type page component (#17739)

---
+++ ui-v2/src/components/blocks/block-type-page/block-type-page.stories.tsx
@@ -0,0 +1,15 @@
+import { createFakeBlockType } from "@/mocks";
+import { routerDecorator, toastDecorator } from "@/storybook/utils";
+import type { Meta, StoryObj } from "@storybook/react";
+import { BlockTypePage } from "./block-type-page";
+
+const meta = {
+	title: "Components/Blocks/BlockTypePage",
+	component: BlockTypePage,
+	decorators: [toastDecorator, routerDecorator],
+	args: { blockType: createFakeBlockType() },
+} satisfies Meta<typeof BlockTypePage>;
+
+export default meta;
+
+export const story: StoryObj = { name: "BlockTypePage" };
