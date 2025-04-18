d77271ad93c5ecdbb173666aab3c37145ec2b30a
nate nowack <thrast36@gmail.com>
2025-04-18 13:22:42 -0500

    serve all tasks in a module (#17858)

--- tests/cli/test_task.py
+++ tests/cli/test_task.py
@@ -80,3 +80,77 @@ async def test_multiple_entrypoints():
         assert len(served_tasks) == 2
         assert served_tasks[0].name == "do_the_dishes"
         assert served_tasks[1].name == "spacewalk"
+
+
+async def test_mutual_exclusivity():
+    await run_sync_in_worker_thread(
+        invoke_and_assert,
+        [
+            "task",
+            "serve",
+            f"{TEST_PROJECTS_DIR}/tasks/household.py:do_the_dishes",
+            "--module",
+            "tests.test-projects.tasks.household",
+        ],
+        expected_code=1,
+        expected_output_contains=[
+            "You may provide entrypoints or modules, but not both at the same time."
+        ],
+    )
+
+
+async def test_module_import_error():
+    await run_sync_in_worker_thread(
+        invoke_and_assert,
+        [
+            "task",
+            "serve",
+            "--module",
+            "tests.test-projects.tasks.doesnotexist",
+        ],
+        expected_code=1,
+        expected_output_contains=[
+            "Module 'tests.test-projects.tasks.doesnotexist' could not be imported. Please check the module name and try again."
+        ],
+    )
+
+
+async def test_single_module():
+    with mock.patch(
+        "prefect.cli.task.task_serve", new_callable=mock.AsyncMock
+    ) as task_serve:
+        await run_sync_in_worker_thread(
+            invoke_and_assert,
+            [
+                "task",
+                "serve",
+                "--module",
+                "tests.test-projects.tasks.household",
+            ],
+            expected_code=0,
+        )
+        task_serve.assert_awaited_once_with(mock.ANY, limit=10)
+        served_tasks = task_serve.call_args_list[0][0]
+        assert any(getattr(t, "name", None) == "do_the_dishes" for t in served_tasks)
+
+
+async def test_multiple_modules():
+    with mock.patch(
+        "prefect.cli.task.task_serve", new_callable=mock.AsyncMock
+    ) as task_serve:
+        await run_sync_in_worker_thread(
+            invoke_and_assert,
+            [
+                "task",
+                "serve",
+                "--module",
+                "tests.test-projects.tasks.household",
+                "--module",
+                "tests.test-projects.tasks.space",
+            ],
+            expected_code=0,
+        )
+        task_serve.assert_awaited_once_with(mock.ANY, mock.ANY, limit=10)
+        served_tasks = task_serve.call_args_list[0][0]
+        assert any(getattr(t, "name", None) == "do_the_dishes" for t in served_tasks)
+        assert any(getattr(t, "name", None) == "spacewalk" for t in served_tasks)
