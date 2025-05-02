bc7f6b29e6dd3b819d07f336884b59d02ea0e16c
nate nowack <thrast36@gmail.com>
2025-05-02 08:53:44 -0500

    add logfire dep group for performance exploration (#17860)

--- src/prefect/settings/base.py
+++ src/prefect/settings/base.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import inspect
 from functools import partial
-from typing import Any, Dict, Tuple, Type
+from typing import Any
 
 from pydantic import (
     AliasChoices,
@@ -32,12 +32,12 @@ class PrefectBaseSettings(BaseSettings):
     @classmethod
     def settings_customise_sources(
         cls,
-        settings_cls: Type[BaseSettings],
+        settings_cls: type[BaseSettings],
         init_settings: PydanticBaseSettingsSource,
         env_settings: PydanticBaseSettingsSource,
         dotenv_settings: PydanticBaseSettingsSource,
         file_secret_settings: PydanticBaseSettingsSource,
-    ) -> Tuple[PydanticBaseSettingsSource, ...]:
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
         """
         Define an order for Prefect settings sources.
 
@@ -93,9 +93,9 @@ class PrefectBaseSettings(BaseSettings):
         exclude_unset: bool = False,
         include_secrets: bool = True,
         include_aliases: bool = False,
-    ) -> Dict[str, str]:
+    ) -> dict[str, str]:
         """Convert the settings object to a dictionary of environment variables."""
-        env: Dict[str, Any] = self.model_dump(
+        env: dict[str, Any] = self.model_dump(
             exclude_unset=exclude_unset,
             mode="json",
             context={"include_secrets": include_secrets},
@@ -192,7 +192,7 @@ class PrefectSettingsConfigDict(SettingsConfigDict, total=False):
 
 
 def _add_environment_variables(
-    schema: Dict[str, Any], model: Type[PrefectBaseSettings]
+    schema: dict[str, Any], model: type[PrefectBaseSettings]
 ) -> None:
     for property in schema["properties"]:
         env_vars: list[str] = []
@@ -212,7 +212,7 @@ def _add_environment_variables(
 
 
 def build_settings_config(
-    path: Tuple[str, ...] = tuple(), frozen: bool = False
+    path: tuple[str, ...] = tuple(), frozen: bool = False
 ) -> PrefectSettingsConfigDict:
     env_prefix = f"PREFECT_{'_'.join(path).upper()}_" if path else "PREFECT_"
     return PrefectSettingsConfigDict(
