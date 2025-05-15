18b29c529211791b069256561891e6722c8cc212
Alex Streed <alex.s@prefect.io>
2025-05-15 08:42:20 -0500

    Update deploy script to create the smoke-test work pool if it doesn't exist

--- client/client_deploy.py
+++ client/client_deploy.py
@@ -9,11 +9,23 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 from prefect import Flow, get_client
+from prefect.client.schemas.actions import WorkPoolCreate
 from prefect.runner.runner import Runner
+from prefect.server.exceptions import ObjectNotFoundError
 
 
 async def main():
     async with get_client() as client:
+        # Check if the smoke-test work pool exists
+        try:
+            await client.read_work_pool("smoke-test")
+        except ObjectNotFoundError:
+            # Create the work pool if it doesn't exist
+            await client.create_work_pool(
+                WorkPoolCreate(name="smoke-test", type="process")
+            )
+
+        # Deploy the flow
         smoke_test_flow = await Flow.afrom_source(
             source=Path(__file__).resolve().parent,
             entrypoint="client_flow.py:smoke_test_flow",
@@ -29,6 +41,7 @@ async def main():
 
         deployment_id = await coro
 
+        # Execute a run via a runner
         flow_run = await client.create_flow_run_from_deployment(
             deployment_id=deployment_id
         )
