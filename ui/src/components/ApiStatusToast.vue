caaa1078b7b9f8c5308c20c148f3a762a2c70361
Nicholas Brown <znicholasbrown@gmail.com>
2025-06-24 17:46:11 -0400

    Enhancement: Add an axios middleware for intercepting errors  (#18365)

---
+++ ui/src/components/ApiStatusToast.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <PHeading :heading="6">
+      Oops. Something went wrong.
+    </PHeading>
+    <div>
+      You can use Prefect Cloud for free, anytime.
+    </div>
+    <div>
+      <PButton to="https://app.prefect.cloud/auth/sign-up?utm_source=prefect-oss&utm_medium=toast" variant="default" target="_blank">
+        Try it now
+      </PButton>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PButton, PHeading } from '@prefecthq/prefect-design'
+</script>
--- ui/src/utilities/api.ts
+++ ui/src/utilities/api.ts
@@ -3,9 +3,9 @@ import { createActions } from '@prefecthq/vue-compositions'
 import { AxiosInstance } from 'axios'
 import { InjectionKey } from 'vue'
 import { AdminApi } from '@/services/adminApi'
+import { setupApiStatusInterceptor } from '@/services/apiStatus'
 import { AutomationsApi } from '@/services/automationsApi'
 import { CsrfTokenApi, setupCsrfInterceptor } from '@/services/csrfTokenApi'
-
 
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
@@ -13,11 +13,12 @@ export function createPrefectApi(config: PrefectConfig) {
   const csrfTokenApi = createActions(new CsrfTokenApi(config))
 
   function axiosInstanceSetupHook(axiosInstance: AxiosInstance): void {
+    setupApiStatusInterceptor(axiosInstance)
     setupCsrfInterceptor(csrfTokenApi, axiosInstance)
-    
+
     const password = localStorage.getItem('prefect-password')
     if (password) {
-      axiosInstance.defaults.headers.common['Authorization'] = `Basic ${password}`
+      axiosInstance.defaults.headers.common.Authorization = `Basic ${password}`
     }
   }
 
