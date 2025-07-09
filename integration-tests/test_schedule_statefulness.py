e1df0a70452a424554e8f41a11622676e7daf19d
Alex Streed <alex.s@prefect.io>
2025-07-09 11:07:55 -0500

    Add more checks to schedule statefulness test

--- integration-tests/test_schedule_statefulness.py
+++ integration-tests/test_schedule_statefulness.py
@@ -16,7 +16,7 @@ this integration test should
 """
 
 import signal
-from typing import Any
+from typing import Any, Callable
 from uuid import uuid4
 
 import pytest
@@ -24,7 +24,7 @@ import pytest
 from prefect import flow, get_client
 from prefect.client.schemas.objects import DeploymentSchedule
 from prefect.client.schemas.schedules import CronSchedule
-from prefect.schedules import Cron
+from prefect.schedules import Cron, Schedule
 
 
 @pytest.fixture()
@@ -37,17 +37,43 @@ def my_flow():
     print("Hello, world!")
 
 
-def _handler(signum: int, frame: Any):
-    raise KeyboardInterrupt("Simulating user interruption")
+def create_check_handler(
+    deployment_name: str,
+    check_function: Callable[[list[DeploymentSchedule]], bool] = lambda schedules: True,
+    num_checks: int = 5,
+    timeout: int = 2,
+):
+    def handler(signum: int, frame: Any):
+        schedules = check_deployment_schedules(f"my-flow/{deployment_name}")
+        if check_function(schedules) or num_checks <= 0:
+            raise KeyboardInterrupt("Simulating user interruption")
+        else:
+            print(f"Checking {num_checks} more times")
+            signal.signal(
+                signal.SIGALRM,
+                create_check_handler(
+                    deployment_name, check_function, num_checks - 1, timeout
+                ),
+            )
+            signal.alarm(timeout)
+
+    return handler
 
 
 def run_serve_with_schedule(
-    timeout: int = 5, serve_kwargs: dict[str, Any] | None = None
+    deployment_name: str,
+    check_function: Callable[[list[DeploymentSchedule]], bool] = lambda schedules: True,
+    schedules: list[Schedule] | None = None,
+    timeout: int = 2,
+    num_checks: int = 3,
 ):
-    signal.signal(signal.SIGALRM, _handler)
+    signal.signal(
+        signal.SIGALRM,
+        create_check_handler(deployment_name, check_function, num_checks, timeout),
+    )
     signal.alarm(timeout)
     try:
-        my_flow.serve(**(serve_kwargs or {}))
+        my_flow.serve(name=deployment_name, schedules=schedules)
     except KeyboardInterrupt:
         print("Serve interrupted")
     finally:
@@ -64,13 +90,19 @@ def test_schedule_statefulness(deployment_name: str):
     # case 1: Schedule without slug
     print("\nTest case 1: Schedule without slug")
     run_serve_with_schedule(
-        serve_kwargs={"name": deployment_name, "schedules": [Cron("0 9 * * *")]}
+        deployment_name,
+        schedules=[Cron("0 9 * * *")],
+        check_function=lambda schedules: any(
+            s.schedule == CronSchedule(cron="0 9 * * *") for s in schedules
+        ),
     )
     schedules = check_deployment_schedules(f"my-flow/{deployment_name}")
     assert any(s.schedule == CronSchedule(cron="0 9 * * *") for s in schedules), (
         f"Expected schedule to persist: {schedules}"
     )
-    run_serve_with_schedule(serve_kwargs={"name": deployment_name, "schedules": []})
+    run_serve_with_schedule(
+        deployment_name, schedules=[], check_function=lambda schedules: not schedules
+    )
     schedules = check_deployment_schedules(f"my-flow/{deployment_name}")
     assert not schedules, (
         f"Expected no schedules after removing unnamed schedule: {schedules}"
@@ -79,12 +111,13 @@ def test_schedule_statefulness(deployment_name: str):
     # case 2: Schedule with slug
     print("\nTest case 2: Schedule with slug")
     run_serve_with_schedule(
-        serve_kwargs={
-            "name": deployment_name,
-            "schedules": [Cron("0 9 * * *", slug="every-day-at-9am")],
-        }
+        deployment_name,
+        schedules=[Cron("0 9 * * *", slug="every-day-at-9am")],
+        check_function=lambda schedules: any(
+            s.slug == "every-day-at-9am" for s in schedules
+        ),
     )
-    run_serve_with_schedule(serve_kwargs={"schedules": []})
+    run_serve_with_schedule(deployment_name, schedules=[])
     schedules = check_deployment_schedules(f"my-flow/{deployment_name}")
     assert any(s.slug == "every-day-at-9am" for s in schedules), (
         f"Expected named schedule to persist: {schedules}"
