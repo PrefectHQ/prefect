903b573497e38ae186ca8897eecf7fa9a8438cc6
Alex Streed <alex.s@prefect.io>
2025-07-10 16:09:04 -0500

    Add in-memory concurrency lease storage

---
+++ src/prefect/server/concurrency/lease_storage/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import timedelta
+from uuid import UUID
+
+from prefect.server.utilities.leasing import LeaseStorage, ResourceLease
+
+
+@dataclass
+class ConcurrencyLimitLeaseMetadata:
+    slots: int
+
+
+class ConcurrencyLeaseStorage(LeaseStorage[ConcurrencyLimitLeaseMetadata]):
+    async def create_lease(
+        self,
+        resource_ids: list[UUID],
+        ttl: timedelta,
+        metadata: ConcurrencyLimitLeaseMetadata | None = None,
+    ) -> ResourceLease[ConcurrencyLimitLeaseMetadata]: ...
+
+    async def read_lease(self, lease_id: UUID) -> None: ...
+
+    async def renew_lease(self, lease_id: UUID, ttl: timedelta) -> None: ...
+
+    async def release_lease(self, lease_id: UUID) -> None: ...
+
+    async def read_expired_lease_ids(self, limit: int = 100) -> list[UUID]: ...
