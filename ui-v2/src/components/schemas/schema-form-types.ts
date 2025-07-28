c1cd8208a12bee585546f39ff2bea920f6107e4f
Sam Chaudry <sam.chaudry@hotmail.co.uk>
2025-07-28 13:20:06 +0100

    Update schema form types to match test and schema requirements

--- ui-v2/src/components/schemas/schema-form-types.ts
+++ ui-v2/src/components/schemas/schema-form-types.ts
@@ -1,2 +1,9 @@
-export type MyModelType = { shared_parameter: string; unique_1?: number };
-export type MyModel2Type = { shared_parameter: string; unique_2?: string };
+export type MyModelType = {
+  shared_parameter: string;
+  unique_1: number;
+};
+
+export type MyModel2Type = {
+  shared_parameter: string;
+  unique_2: number;
+};
