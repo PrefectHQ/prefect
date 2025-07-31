5dd815ff5a7ca4ea72052ee39c197f50ddba2889
Alex Streed <alex.s@prefect.io>
2025-07-31 14:18:25 -0500

    Add filter and event handler structure to ECS observer

--- src/integrations/prefect-aws/test.py
+++ src/integrations/prefect-aws/test.py
@@ -1,11 +1,17 @@
 import asyncio
+from typing import Any
 
-from prefect_aws.observers.ecs import EcsObserver, SqsSubscriber
+from prefect_aws.observers.ecs import EcsObserver, FilterCase, SqsSubscriber
 
 
 async def main():
     sub = SqsSubscriber(queue_name="prefect-ecs-task-events", queue_region="us-east-2")
     observer = EcsObserver(sqs_subscriber=sub)
+
+    @observer.on_event("task", tags={"prefect.io/flow-run-id": FilterCase.PRESENT})
+    def on_task_event(event: dict[str, Any], tags: dict[str, str]):
+        print(f"Received task event with tags: {tags}")
+
     await observer.run()
 
 
