71e2a038693e710c27664a8c25bf4a38d8a6a4e2
Joonas Nissinen <joonas.nissinen@smartly.io>
2025-08-19 17:00:31 +0300

    fix: initialize _exceptions for runner sessions for anyio 4.10+

--- src/prefect/runner/runner.py
+++ src/prefect/runner/runner.py
@@ -1165,6 +1165,13 @@ class Runner:
         self._client = get_client()
         self._tmp_dir.mkdir(parents=True)
         await self._client.__aenter__()
+
+        # Re-initialize both TaskGroups' _exceptions attributes each time to avoid reuse
+        # issues with anyio >4.4.0. The __aexit__ method of the TaskGroup deletes the
+        # _exceptions attribute and this causes issues.
+        self._runs_task_group._exceptions = []
+        self._loops_task_group._exceptions = []
+
         await self._runs_task_group.__aenter__()
 
         self.started = True
