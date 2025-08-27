18cb8165ee711954f8999078193c627be003b91b
Devin Villarosa <102188207+devinvillarosa@users.noreply.github.com>
2025-08-27 15:47:50 -0700

    [UI-v2] feat: Properly implements dark mode with next-themes (#18803)

--- ui-v2/src/components/layouts/MainLayout.tsx
+++ ui-v2/src/components/layouts/MainLayout.tsx
@@ -1,13 +1,22 @@
 import { AppSidebar } from "@/components/ui/app-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "../ui/theme-provider";
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<main className="flex-1 overflow-auto p-4">{children}</main>
-			<Toaster />
-		</SidebarProvider>
+		<ThemeProvider
+			attribute="class"
+			defaultTheme="system"
+			enableSystem
+			disableTransitionOnChange
+			storageKey="vite-ui-theme"
+		>
+			<SidebarProvider>
+				<AppSidebar />
+				<main className="flex-1 overflow-auto p-4">{children}</main>
+				<Toaster />
+			</SidebarProvider>
+		</ThemeProvider>
 	);
 }
