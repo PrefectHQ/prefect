f95e384c9ebdca085fe690f2402db079d7d774bf
Alex Streed <ajstreed1@gmail.com>
2025-10-27 15:56:25 -0500

    Remove stale compat code

--- src/prefect/utilities/compat.py
+++ src/prefect/utilities/compat.py
@@ -1,23 +1,21 @@
 """
 Utilities for Python version compatibility
 """
+
 # Please organize additions to this file by version
+import warnings
+import importlib.metadata
+from importlib.metadata import (
+    EntryPoint as EntryPoint,
+    EntryPoints as EntryPoints,
+    entry_points as entry_points,
+)
 
-import sys
+importlib_metadata = importlib.metadata
 
-if sys.version_info < (3, 10):
-    import importlib_metadata as importlib_metadata
-    from importlib_metadata import (
-        EntryPoint as EntryPoint,
-        EntryPoints as EntryPoints,
-        entry_points as entry_points,
-    )
-else:
-    import importlib.metadata
-    from importlib.metadata import (
-        EntryPoint as EntryPoint,
-        EntryPoints as EntryPoints,
-        entry_points as entry_points,
-    )
-
-    importlib_metadata = importlib.metadata
+warnings.warn(
+    "The prefect.utilities.compat module is deprecated. "
+    "Use importlib.metadata directly instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
