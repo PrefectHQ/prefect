40813576f520253efca858da39173b5e767dfbf2
zzstoatzz <thrast36@gmail.com>
2025-10-30 11:59:06 -0500

    fix: update tests for removed LoopService classes
    - updated test_service_subsets.py to remove references to deleted service classes
    - updated test_server_services.py to check for TaskRunRecorder instead of MarkLateRuns
    - all local tests now passing

--- tests/server/services/test_service_subsets.py
+++ tests/server/services/test_service_subsets.py
@@ -1,40 +1,24 @@
 from prefect.server.events.services.actions import Actions
 from prefect.server.events.services.event_logger import EventLogger
 from prefect.server.events.services.event_persister import EventPersister
-from prefect.server.events.services.triggers import ProactiveTriggers, ReactiveTriggers
+from prefect.server.events.services.triggers import ReactiveTriggers
 from prefect.server.events.stream import Distributor
 from prefect.server.logs.stream import LogDistributor
 from prefect.server.services.base import RunInEphemeralServers, RunInWebservers, Service
-from prefect.server.services.cancellation_cleanup import CancellationCleanup
-from prefect.server.services.foreman import Foreman
-from prefect.server.services.late_runs import MarkLateRuns
-from prefect.server.services.pause_expirations import FailExpiredPauses
-from prefect.server.services.repossessor import Repossessor
-from prefect.server.services.scheduler import RecentDeploymentsScheduler, Scheduler
 from prefect.server.services.task_run_recorder import TaskRunRecorder
-from prefect.server.services.telemetry import Telemetry
 
 
 def test_the_all_service_subset():
     """The following services should be enabled on background servers or full-featured
     API servers"""
     assert set(Service.all_services()) == {
-        Telemetry,
         # Orchestration services
-        CancellationCleanup,
-        FailExpiredPauses,
-        Foreman,
-        MarkLateRuns,
-        RecentDeploymentsScheduler,
-        Repossessor,
-        Scheduler,
         TaskRunRecorder,
         # Events services
         Actions,
         Distributor,
         EventLogger,
         EventPersister,
-        ProactiveTriggers,
         ReactiveTriggers,
         # Logs services
         LogDistributor,
@@ -44,7 +28,6 @@ def test_the_all_service_subset():
 def test_run_in_ephemeral_servers():
     """The following services should be enabled on ephemeral servers"""
     assert set(RunInEphemeralServers.all_services()) == {
-        Telemetry,
         # Orchestration services
         TaskRunRecorder,
         # Events services
@@ -52,7 +35,6 @@ def test_run_in_ephemeral_servers():
         Distributor,
         EventLogger,
         EventPersister,
-        ProactiveTriggers,
         ReactiveTriggers,
         # Logs services
         LogDistributor,
@@ -62,7 +44,6 @@ def test_run_in_ephemeral_servers():
 def test_run_in_webservers():
     """The following services should be enabled on webservers"""
     assert set(RunInWebservers.all_services()) == {
-        Telemetry,
         # Events services
         Distributor,
         # Logs services
