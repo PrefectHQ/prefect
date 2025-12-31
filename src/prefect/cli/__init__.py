263bac02a15123510fb383e80e88655cdbed767f
jake@prefect.io <jake@prefect.io>
2025-12-30 20:20:01 -0500

    add send log command

--- src/prefect/cli/__init__.py
+++ src/prefect/cli/__init__.py
@@ -21,6 +21,7 @@ import prefect.cli.events
 import prefect.cli.experimental
 import prefect.cli.flow
 import prefect.cli.flow_run
+import prefect.cli.logs
 import prefect.cli.global_concurrency_limit
 import prefect.cli.profile
 import prefect.cli.server
