3163148692b423b60cdd1db69f83aeffffd1954a
zzstoatzz <thrast36@gmail.com>
2026-03-03 12:49:04 -0600

    chore: sync ui openapi schema

--- ui-v2/src/api/prefect.ts
+++ ui-v2/src/api/prefect.ts
@@ -8310,7 +8310,7 @@ export interface components {
          */
         IntervalSchedule: {
             /** Interval */
-            interval: number;
+            interval: number | string;
             /**
              * Anchor Date
              * Format: date-time
