497e28d2eb131254a76b59b1ed3179b20ed67f14
zzstoatzz <thrast36@gmail.com>
2026-05-05 15:25:03 -0500

    Sync UI v2 OpenAPI client

--- ui-v2/src/api/prefect.ts
+++ ui-v2/src/api/prefect.ts
@@ -8472,7 +8472,7 @@ export interface components {
          */
         IntervalSchedule: {
             /** Interval */
-            interval: number;
+            interval: number | string;
             /**
              * Anchor Date
              * Format: date-time
