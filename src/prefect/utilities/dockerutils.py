51d127db9f5acb1ffb2caa8c1f208bac1b51e0c4
ChidiebereNjoku <144056487+ChidiebereNjoku@users.noreply.github.com>
2026-08-21 16:49:15 +0100

    Update dockerutils.py

--- src/prefect/utilities/dockerutils.py
+++ src/prefect/utilities/dockerutils.py
@@ -79,7 +79,7 @@ def get_prefect_image_name(
         regex_pattern=r"[^a-zA-Z0-9_.-]+",
     )
 
-    image = "prefect" if is_prod_build else "prefect-dev" 
+    image = "prefect" if is_prod_build else "prefect-dev"
     return f"prefecthq/{image}:{tag}"
 
 
